e54a344a2be7ccb174128c55f414a44dfc35579f
Paul In&In <darezik@gmail.com>
2004-05-27 11:50:20 +0000

    missing fixes

--- include/connection.h
+++ include/connection.h
@@ -7,10 +7,11 @@
 class eConnection: public virtual iObject, public Connection
 {
 	int ref;
+	ePtr<iObject> m_owner;
 public:
 DEFINE_REF(eConnection);
 public:
-	eConnection(const Connection &conn): Connection(conn), ref(0) { };
+	eConnection(iObject *owner, const Connection &conn): Connection(conn), ref(0), m_owner(owner) { };
 	virtual ~eConnection() { disconnect(); }
 };
 
