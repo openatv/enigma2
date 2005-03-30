c94f9e4edd506a3c0f091082c9a9d104854e7f65
Paul In&In <darezik@gmail.com>
2005-03-30 08:47:01 +0000

    - widget instance is now part of screen  - skins can now contain cosmetic widgets (widgets without functionality)    please don't abuse them! Use them only for backgrounds etc.

--- screens.py
+++ screens.py
@@ -15,17 +15,22 @@ class Screen(dict, HTMLSkin, GUISkin):
 		GUISkin.__init__(self)
 		
 	def execBegin(self):
+#		assert self.session == None, "a screen can only exec one per time"
+#		self.session = session
 		for (name, val) in self.items():
 			val.execBegin()
 	
 	def execEnd(self):
 		for (name, val) in self.items():
 			val.execEnd()
+#		assert self.session != None, "execEnd on non-execing screen!"
+#		self.session = None
 	
 	# never call this directly - it will be called from the session!
 	def doClose(self):
 		GUISkin.close(self)
 		
+		del self.session
 		for (name, val) in self.items():
 			print "%s -> %d" % (name, sys.getrefcount(val))
 			del self[name]
