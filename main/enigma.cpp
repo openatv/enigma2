60e5fe14d01b84bbef35d5286183a85ff72002dd
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2005-05-20 19:42:13 +0000

    undo shitty stuff.. sorry

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -1,3 +1,4 @@
+#define SDLDC
 #include <stdio.h>
 #include <libsig_comp.h>
 #include <lib/base/ebase.h>
@@ -9,9 +10,7 @@
 
 #include <lib/gdi/grc.h>
 #include <lib/gdi/gfbdc.h>
-#ifdef SDLDC
 #include <lib/gdi/sdl.h>
-#endif
 #include <lib/gdi/font.h> 
 
 #include <lib/gui/ewidget.h>
@@ -145,7 +144,7 @@ int main(int argc, char **argv)
 	gFBDC::getInstance(my_dc);
 #endif
 
-	fontRenderClass::getInstance()->AddFont("/usr/share/fonts/arial.ttf", "Arial", 100);
+	fontRenderClass::getInstance()->AddFont("/home/tmbinc/enigma2/fonts/arial.ttf", "Arial", 100);
 
 	eWidgetDesktop dsk(eSize(720, 576));
 	
