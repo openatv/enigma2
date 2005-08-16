01b94f4679c691fe89bd7340cf2fb87a5d2b1e0c
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-08-16 02:06:28 +0000

    fixed

--- lib/python/Components/InputDevice.py
+++ lib/python/Components/InputDevice.py
@@ -1,26 +1,30 @@
 from config import configElement
-from config import config
+from config import config				#global config instance
+from config import ConfigSubsection
 
 class inputDevices:
 	def __init__(self):
 		pass
 	def setRepeat(self, value):
+		print "setup rc repeat"
 		pass
 	def setDelay(self, value):
+		print "setup rc delay"
 		pass
 
 def InitInputDevices():
-	config.inputDevices = config;
+	config.inputDevices = ConfigSubsection;
 	config.inputDevices.repeat = configElement("config.inputDevices.repeat", config.Slider, 10);
 	config.inputDevices.delay = configElement("config.inputDevices.delay", config.Slider, 10);
+
+	#this instance anywhere else needed?	
+	iDevices = inputDevices();	
 	
 	def inputDevicesRepeatChanged(configElement):
-		print "setup rc repeat"
-		#inputDevices.setRepeat(configElement.value);
+		iDevices.setRepeat(configElement.value);
 
 	def inputDevicesDelayChanged(configElement):
-		print "setup rc delay"
-		#inputDevices.setDelay(configElement.value);
+		iDevices.setDelay(configElement.value);
 
 	# this will call the "setup-val" initial
 	config.inputDevices.repeat.addNotifier(inputDevicesRepeatChanged);
