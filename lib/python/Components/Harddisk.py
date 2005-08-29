b29f0af566c8c1fd25de259ba35f5130a7efe8ca
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-08-29 22:00:47 +0000

    add harddisk stuff

---
+++ lib/python/Components/Harddisk.py
@@ -0,0 +1,44 @@
+
+
+def tryOpen(filename):
+	try:
+		procFile = open(filename)
+	except IOError:
+		return ""
+	return procFile
+
+class Harddisk:
+	def __init__(self, index):
+		self.index = index
+		#perhaps this is easier?
+		self.prochdx = "/proc/ide/hd" + ("a","b","c","d","e","f","g","h")[index] + "/"
+		
+	def capacity(self):
+		procfile = tryOpen(self.prochdx + "capacity")
+		
+		if procfile == "":
+			return -1
+
+		line = procfile.readline()
+		procfile.close()
+		
+		if line == "":
+			return -1
+
+		return int(line)
+						
+	def model(self):
+		procfile = tryOpen(self.prochdx + "model")
+		
+		if procfile == "":
+			return ""
+
+		line = procfile.readline()
+		procfile.close()
+
+		return line
+
+	def free(self):
+		pass
+		
+	
