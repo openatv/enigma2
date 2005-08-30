f87a1c53d4278df70582195e112a3cc901c21ea5
Paul In&In <darezik@gmail.com>
2005-08-30 12:04:19 +0000

    add HarddiskSetup

---
+++ lib/python/Screens/HarddiskSetup.py
@@ -0,0 +1,11 @@
+from Screen import Screen
+from Components.ActionMap import ActionMap
+
+class HarddiskSelection(Screen):
+  def __init__(self, session):
+    Screen.__init__(self, session)
+
+    self["actions"] = ActionMap(["OkCancelActions"],
+      {
+        "cancel": self.close
+      })
