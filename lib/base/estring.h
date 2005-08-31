4269ecde13580077c4d234b21784f704bf43e8fc
Felix Domke <tmbinc@elitedvb.net>
2005-08-31 18:20:45 +0000

    - fix character conversion for DVB

--- lib/base/estring.h
+++ lib/base/estring.h
@@ -9,8 +9,9 @@
 int strnicmp(const char*, const char*, int);
 
 std::string getNum(int num, int base=10);
-std::string convertDVBUTF8(unsigned char *data, int len, int table=5);
+std::string convertDVBUTF8(const unsigned char *data, int len, int table=5);
 std::string convertUTF8DVB(const std::string &string);  // with default ISO8859-5
+std::string convertDVBUTF8(const std::string &s, int table=5);
 std::string convertLatin1UTF8(const std::string &string);
 int isUTF8(const std::string &string);
 
