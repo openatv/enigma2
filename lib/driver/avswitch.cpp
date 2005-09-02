9458c6fcf07885c97e8b627b035c848267ee2d0c
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-09-02 21:40:11 +0000

    avswitch

---
+++ lib/driver/avswitch.cpp
@@ -0,0 +1,41 @@
+#include <lib/driver/avswitch.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+#include <lib/base/init.h>
+#include <lib/base/init_num.h>
+#include <lib/base/econfig.h>
+#include <lib/base/eerror.h>
+
+eAVSwitch *eAVSwitch::instance = 0;
+
+eAVSwitch::eAVSwitch()
+{
+	ASSERT(!instance);
+	instance = this;
+	
+	avsfd = open("/dev/dbox/avs0", O_RDWR);
+}
+
+eAVSwitch::~eAVSwitch()
+{
+	if(avsfd > 0)
+		close(avsfd);
+}
+
+eAVSwitch *eAVSwitch::getInstance()
+{
+	return instance;
+}
+
+void eAVSwitch::setColorFormat(int format)
+{
+	printf("eAVSwitch::setColorFormat(%d)\n",format);
+	/*there are no ioctl for controling this in avs - scart api needed 
+		no, not the gillem one */
+}
+
+//FIXME: correct "run/startlevel"
+eAutoInitP0<eAVSwitch> init_avswitch(eAutoInitNumbers::rc, "AVSwitch Driver");
