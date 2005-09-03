aed130dec1e35bf1907d82b6ee18fee965bde091
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-09-03 00:18:08 +0000

    configBoolean is now configSelection

--- lib/python/Components/SetupDevices.py
+++ lib/python/Components/SetupDevices.py
@@ -5,7 +5,7 @@ from config import config				#global config instance
 from config import configElement
 from config import ConfigSubsection
 from config import ConfigSlider
-from config import configBoolean
+from config import configSelection
 
 #temp. class for exhibition
 
@@ -21,25 +21,25 @@ class LCD:
 
 def InitSetupDevices():
 	config.timezone = ConfigSubsection();
-	config.timezone.val = configElement("config.timezone.val", configBoolean, 1, ("GMT", "GMT+1", "GMT+2", "GMT+3", "GMT+4", "GMT+5", "GMT+6", "GMT+7", "GMT+8", "GMT+9") );
+	config.timezone.val = configElement("config.timezone.val", configSelection, 1, ("GMT", "GMT+1", "GMT+2", "GMT+3", "GMT+4", "GMT+5", "GMT+6", "GMT+7", "GMT+8", "GMT+9") );
 
 	config.rc = ConfigSubsection();
-	config.rc.map = configElement("config.rc.map", configBoolean, 0, ("Default", "Classic") );
+	config.rc.map = configElement("config.rc.map", configSelection, 0, ("Default", "Classic") );
 
 	config.keyboard = ConfigSubsection();
-	config.keyboard.keymap = configElement("config.keyboard.keymap", configBoolean, 1, ("English", "German") );
+	config.keyboard.keymap = configElement("config.keyboard.keymap", configSelection, 1, ("English", "German") );
 
 	config.osd = ConfigSubsection();
 	config.osd.alpha = configElement("config.osd.alpha", ConfigSlider, 0, "");
 	config.osd.bright = configElement("config.osd.bright", ConfigSlider, 5, "");
 	config.osd.contrast = configElement("config.osd.contrast", ConfigSlider, 5, "");
-	config.osd.language = configElement("config.osd.language", configBoolean, 0, ("English", "English US") );
+	config.osd.language = configElement("config.osd.language", configSelection, 0, ("English", "English US") );
 
 	config.lcd = ConfigSubsection();
 	config.lcd.bright = configElement("config.lcd.bright", ConfigSlider, 7, "");
 	config.lcd.contrast = configElement("config.lcd.contrast", ConfigSlider, 2, "");
 	config.lcd.standby = configElement("config.lcd.standby", ConfigSlider, 1, "");
-	config.lcd.invert = configElement("config.lcd.invert", configBoolean, 1, ("Enable", "Disable") );
+	config.lcd.invert = configElement("config.lcd.invert", configSelection, 1, ("Enable", "Disable") );
 
 	ilcd = LCD()
 
@@ -53,27 +53,27 @@ def InitSetupDevices():
 	config.lcd.contrast.addNotifier(setLCDcontrast);
 
 	config.parental = ConfigSubsection();
-	config.parental.lock = configElement("config.parental.lock", configBoolean, 1, ("Enable", "Disable") );
-	config.parental.setuplock = configElement("config.parental.setuplock", configBoolean, 1, ("Enable", "Disable") );
+	config.parental.lock = configElement("config.parental.lock", configSelection, 1, ("Enable", "Disable") );
+	config.parental.setuplock = configElement("config.parental.setuplock", configSelection, 1, ("Enable", "Disable") );
 
 	config.expert = ConfigSubsection();
-	config.expert.splitsize = configElement("config.expert.splitsize", configBoolean, 1, ("0.5Gbyte", "1.0 GByte", "1.5 GByte", "2.0 GByte") );
-	config.expert.satpos = configElement("config.expert.satpos", configBoolean, 1, ("Enable", "Disable") );
-	config.expert.fastzap = configElement("config.expert.fastzap", configBoolean, 0, ("Enable", "Disable") );
-	config.expert.skipconfirm = configElement("config.expert.skipconfirm", configBoolean, 1, ("Enable", "Disable") );
-	config.expert.hideerrors = configElement("config.expert.hideerrors", configBoolean, 1, ("Enable", "Disable") );
-	config.expert.autoinfo = configElement("config.expert.autoinfo", configBoolean, 1, ("Enable", "Disable") );
+	config.expert.splitsize = configElement("config.expert.splitsize", configSelection, 1, ("0.5Gbyte", "1.0 GByte", "1.5 GByte", "2.0 GByte") );
+	config.expert.satpos = configElement("config.expert.satpos", configSelection, 1, ("Enable", "Disable") );
+	config.expert.fastzap = configElement("config.expert.fastzap", configSelection, 0, ("Enable", "Disable") );
+	config.expert.skipconfirm = configElement("config.expert.skipconfirm", configSelection, 1, ("Enable", "Disable") );
+	config.expert.hideerrors = configElement("config.expert.hideerrors", configSelection, 1, ("Enable", "Disable") );
+	config.expert.autoinfo = configElement("config.expert.autoinfo", configSelection, 1, ("Enable", "Disable") );
 
 	config.sat = ConfigSubsection();
-	config.sat.satA = configElement("config.sat.satA", configBoolean, 1, ("Disabled" ,"Astra 19.2", "Hotbird 13.0") );
-	config.sat.satB = configElement("config.sat.satB", configBoolean, 0, ("Disabled" ,"Astra 19.2", "Hotbird 13.0") );
-	config.sat.diseqcA = configElement("config.sat.diseqcA", configBoolean, 3, ("no DiSEqC", "DiSEqC 1.0", "DiSEqC 1.1", "DiSEqC 1.2") );
-	config.sat.diseqcB = configElement("config.sat.diseqcB", configBoolean, 0, ("no DiSEqC", "DiSEqC 1.0", "DiSEqC 1.1", "DiSEqC 1.2") );
-	config.sat.posA = configElement("config.sat.posA", configBoolean, 0, ("DiSEqC A", "DiSEqC B", "DiSEqC C", "DiSEqC D") );
-	config.sat.posB = configElement("config.sat.posB", configBoolean, 1, ("DiSEqC A", "DiSEqC B", "DiSEqC C", "DiSEqC D") );
+	config.sat.satA = configElement("config.sat.satA", configSelection, 1, ("Disabled" ,"Astra 19.2", "Hotbird 13.0") );
+	config.sat.satB = configElement("config.sat.satB", configSelection, 0, ("Disabled" ,"Astra 19.2", "Hotbird 13.0") );
+	config.sat.diseqcA = configElement("config.sat.diseqcA", configSelection, 3, ("no DiSEqC", "DiSEqC 1.0", "DiSEqC 1.1", "DiSEqC 1.2") );
+	config.sat.diseqcB = configElement("config.sat.diseqcB", configSelection, 0, ("no DiSEqC", "DiSEqC 1.0", "DiSEqC 1.1", "DiSEqC 1.2") );
+	config.sat.posA = configElement("config.sat.posA", configSelection, 0, ("DiSEqC A", "DiSEqC B", "DiSEqC C", "DiSEqC D") );
+	config.sat.posB = configElement("config.sat.posB", configSelection, 1, ("DiSEqC A", "DiSEqC B", "DiSEqC C", "DiSEqC D") );
 
 	#config.blasel = ConfigSubsection();
-	#config.blasel.val = configElement("", configBoolean, 0, ("bunt", "s/w", "gruen") );
+	#config.blasel.val = configElement("", configSelection, 0, ("bunt", "s/w", "gruen") );
 	#config.inputDevices.delay = configElement("config.inputDevices.delay", ConfigSlider, 3);
 
 	#this instance anywhere else needed?	
