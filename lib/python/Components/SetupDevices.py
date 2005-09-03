e4ef86473060528115c4469d3e912f398a68a397
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-09-03 00:02:15 +0000

    add rfmod

--- lib/python/Components/SetupDevices.py
+++ lib/python/Components/SetupDevices.py
@@ -26,13 +26,13 @@ def InitSetupDevices():
 	config.rc = ConfigSubsection();
 	config.rc.map = configElement("config.rc.map", configBoolean, 0, ("Default", "Classic") );
 
-	config.rfmod = ConfigSubsection();
-	config.rfmod.enable = configElement("config.rfmod.enable", configBoolean, 1, ("Enable", "Disable") );
-	config.rfmod.test = configElement("config.rfmod.test", configBoolean, 1, ("Enable", "Disable") );
-	config.rfmod.sound = configElement("config.rfmod.sound", configBoolean, 0, ("Enable", "Disable") );
-	config.rfmod.soundcarrier = configElement("config.rfmod.soundcarrier", configBoolean, 1, ("4.5 MHz", "5.5 MHz", "6.0 MHz", "6.5 MHz") );
-	config.rfmod.channel = configElement("config.rfmod.channel", configBoolean, 6, ("30", "31", "32", "33", "34", "35", "36", "37", "38", "39") );
-	config.rfmod.finetune = configElement("config.rfmod.finetune", ConfigSlider, 5, "");
+	#config.rfmod = ConfigSubsection();
+	#config.rfmod.enable = configElement("config.rfmod.enable", configBoolean, 1, ("Enable", "Disable") );
+	#config.rfmod.test = configElement("config.rfmod.test", configBoolean, 1, ("Enable", "Disable") );
+	#config.rfmod.sound = configElement("config.rfmod.sound", configBoolean, 0, ("Enable", "Disable") );
+	#config.rfmod.soundcarrier = configElement("config.rfmod.soundcarrier", configBoolean, 1, ("4.5 MHz", "5.5 MHz", "6.0 MHz", "6.5 MHz") );
+	#config.rfmod.channel = configElement("config.rfmod.channel", configBoolean, 6, ("30", "31", "32", "33", "34", "35", "36", "37", "38", "39") );
+	#config.rfmod.finetune = configElement("config.rfmod.finetune", ConfigSlider, 5, "");
 
 	config.keyboard = ConfigSubsection();
 	config.keyboard.keymap = configElement("config.keyboard.keymap", configBoolean, 1, ("English", "German") );
