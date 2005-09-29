7e78afcb4cb69c310b23b31ae64567c69315806f
Paul In&In <darezik@gmail.com>
2005-09-29 22:10:55 +0000

    add FixedMenu

---
+++ lib/python/Screens/FixedMenu.py
@@ -0,0 +1,23 @@
+from Screen import Screen
+from Menu import MenuList
+from Components.ActionMap import ActionMap
+from Components.Header import Header
+
+class FixedMenu(Screen):
+	def okbuttonClick(self):
+		selection = self["menu"].getCurrent()
+		selection[1]()
+
+	def __init__(self, session, title, list):
+		Screen.__init__(self, session)
+		
+		self["menu"] = MenuList(list)	
+							
+		self["actions"] = ActionMap(["OkCancelActions"], 
+			{
+				"ok": self.okbuttonClick,
+				"cancel": self.close
+			})
+		
+		self["title"] = Header(title)
+
