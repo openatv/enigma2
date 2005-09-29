cdeb634affac7c3083937825bbf2c89cd11e84f9
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-09-29 00:50:33 +0000

    a bit more/nicer fakes

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -7,15 +7,16 @@ class About(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		
-		self["text"] = Label("Enigma v2.0a")
+		self["text"] = Label("Enigma v2.0b")
 
 		self["tuner"] = Label("Detected NIMs:")
-		self["tunerA"] = Label("   Tuner A: Fujitsu QST (DVB-S)")
-		self["tunerB"] = Label("   Tuner B: Fujitsu QST (DVB-S)")
+		self["tunerA"] = Label("   Tuner A: Alps BSBE1 (DVB-S)")
+		self["tunerB"] = Label("   Tuner B: Alps BSBE1 (DVB-S)")
 
 		self["hdd"] = Label("Detected HDD:")
 		hdd = Harddisk(0)
-		self["hddA"] = Label("%s (%s, %d MB free)" % (hdd.model(), hdd.capacity(),hdd.free()))
+		#self["hddA"] = Label("%s (%s, %d MB free)" % (hdd.model(), hdd.capacity(),hdd.free()))
+		self["hddA"] = Label("Seagate 398 GByte (323 GByte free)")
 
 		self["actions"] = ActionMap(["SetupActions"], 
 			{
