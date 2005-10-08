ee34dc9997d200a8021332dd639c2cf3a92b21cd
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2005-10-08 23:09:53 +0000

    added nims to the about box

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -2,6 +2,8 @@ from Screen import Screen
 from Components.ActionMap import ActionMap
 from Components.Label import Label
 from Components.Harddisk import Harddisk
+from Components.NimManager import nimmanager
+from Components.MenuList import MenuList
 
 class About(Screen):
 	def __init__(self, session):
@@ -10,13 +12,17 @@ class About(Screen):
 		self["text"] = Label("Enigma v2.0b")
 
 		self["tuner"] = Label("Detected NIMs:")
-		self["tunerA"] = Label("   Tuner A: Alps BSBE1 (DVB-S)")
-		self["tunerB"] = Label("   Tuner B: Alps BSBE1 (DVB-S)")
+		
+		nims = nimmanager.nimList()
+		count = 0
+		for i in nims:
+			self["tuner" + str(count)] = Label(i[0])
+			count += 1
 
 		self["hdd"] = Label("Detected HDD:")
 		hdd = Harddisk(0)
-		#self["hddA"] = Label("%s (%s, %d MB free)" % (hdd.model(), hdd.capacity(),hdd.free()))
-		self["hddA"] = Label("Seagate 398 GByte (323 GByte free)")
+		self["hddA"] = Label("%s (%s, %d MB free)" % (hdd.model(), hdd.capacity(),hdd.free()))
+		#self["hddA"] = Label("Seagate 398 GByte (323 GByte free)")
 
 		self["actions"] = ActionMap(["SetupActions"], 
 			{
