2e8c4f0e8cc34d491734356c9498cfa6166720ea
Paul In&In <darezik@gmail.com>
2005-10-11 21:22:48 +0000

    reenable editing of network settings by adding an action for every number

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -13,7 +13,17 @@ class NetworkSetup(Screen):
             "ok": self.keySave,
             "cancel": self.keyCancel,
             "left": self.keyLeft,
-            "right": self.keyRight
+            "right": self.keyRight,
+            "1": self.keyNumber1,
+            "2": self.keyNumber2,
+            "3": self.keyNumber3,
+            "4": self.keyNumber4,
+            "5": self.keyNumber5,
+            "6": self.keyNumber6,
+            "7": self.keyNumber7,
+            "8": self.keyNumber8,
+            "9": self.keyNumber9,
+            "0": self.keyNumber0
         }, -1)
 
         self.list = []
@@ -45,6 +55,32 @@ class NetworkSetup(Screen):
     def keyRight(self):
         self["config"].handleKey(config.key["nextElement"])
         self.newConfig()
+    
+    def keyNumberGlobal(self, number):
+        print "You pressed number " + str(number)
+        if (self["config"].getCurrent()[1].parent.enabled == True):
+            self["config"].handleKey(config.key[str(number)])
+        
+    def keyNumber1(self):
+        self.keyNumberGlobal(1)
+    def keyNumber2(self):
+        self.keyNumberGlobal(2)
+    def keyNumber3(self):
+        self.keyNumberGlobal(3)
+    def keyNumber4(self):
+        self.keyNumberGlobal(4)
+    def keyNumber5(self):
+        self.keyNumberGlobal(5)
+    def keyNumber6(self):
+        self.keyNumberGlobal(6)
+    def keyNumber7(self):
+        self.keyNumberGlobal(7)
+    def keyNumber8(self):
+        self.keyNumberGlobal(8)
+    def keyNumber9(self):
+        self.keyNumberGlobal(9)
+    def keyNumber0(self):
+        self.keyNumberGlobal(0)        
 
     def keySave(self):
         #for x in self["config"].list:
