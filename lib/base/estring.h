33388f98fd5499197f5309befd2106ad7b65699c
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2005-11-09 16:51:26 +0000

    utf8 changes

--- lib/base/estring.h
+++ lib/base/estring.h
@@ -9,13 +9,18 @@
 int strnicmp(const char*, const char*, int);
 
 std::string getNum(int num, int base=10);
-std::string convertDVBUTF8(const unsigned char *data, int len, int table=5);
-std::string convertUTF8DVB(const std::string &string);  // with default ISO8859-5
-std::string convertDVBUTF8(const std::string &s, int table=5);
+
+std::string convertDVBUTF8(const unsigned char *data, int len, int table=0, int tsidonid=0); // with default ISO8859-1/Latin1
+std::string convertUTF8DVB(const std::string &string, int table=0); // with default ISO8859-1/Latin1
 std::string convertLatin1UTF8(const std::string &string);
 int isUTF8(const std::string &string);
 
 std::string removeDVBChars(const std::string &s);
 void makeUpper(std::string &s);
+
+inline std::string convertDVBUTF8(const std::string &string, int table=0, int tsidonid=0) // with default ISO8859-1/Latin1
+{
+	return convertDVBUTF8((const unsigned char*)string.c_str(), string.length(), table, tsidonid);
+}
 
 #endif // __E_STRING__
