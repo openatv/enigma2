cae001777c7b32bdb4f6f4199e9f49058946fe9f
Ronny Strutz <ronny.strutz@multimedia-labs.de>
2005-11-13 13:59:57 +0000

    fixme: stopService()

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -4,18 +4,21 @@ from Components.ActionMap import ActionMap
 class Standby(Screen):
 	def Power(self):
 		print "leave standby"
+		#start last played service
+		self.infobar.servicelist.zap()
 		self.close()
 
-	def __init__(self, session):
+	def __init__(self, session, infobar):
 		Screen.__init__(self, session)
-		
+		self.infobar = infobar
 		print "enter standby"
 
 		self["actions"] = ActionMap( [ "StandbyActions" ],
 		{
 			"power": self.Power
 		})
-		
+
+		self.session.nav.stopService()
 		#stop/pause? playing services
 		#switch off avs
 		
