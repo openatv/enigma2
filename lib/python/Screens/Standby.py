5359ef974668fac118d01fae4ea8930ac3235de0
Paul In&In <darezik@gmail.com>
2005-11-14 02:37:13 +0000

    add mute to standby

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -1,12 +1,26 @@
 from Screen import Screen
 from Components.ActionMap import ActionMap
+from enigma import *
 
 class Standby(Screen):
 	def Power(self):
 		print "leave standby"
 		#start last played service
 		self.infobar.servicelist.zap()
+		self.leaveMute()
 		self.close()
+
+	def setMute(self):
+		if (eDVBVolumecontrol.getInstance().isMuted()):
+			self.wasMuted = 1
+			print "mute already active"
+		else:	
+			self.wasMuted = 0
+			eDVBVolumecontrol.getInstance().volumeToggleMute()
+
+	def leaveMute(self):
+		if self.wasMuted == 0:
+			eDVBVolumecontrol.getInstance().volumeToggleMute()
 
 	def __init__(self, session, infobar):
 		Screen.__init__(self, session)
@@ -18,7 +32,9 @@ class Standby(Screen):
 			"power": self.Power
 		})
 
+		self.setMute()
 		self.session.nav.stopService()
+		
 		#stop/pause? playing services
 		#switch off avs
 		
