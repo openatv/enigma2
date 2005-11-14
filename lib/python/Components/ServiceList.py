a4048ce604ef796f0cab302f972e711c405f2584
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2005-11-14 00:39:46 +0000

    modifications for working edit mode

--- lib/python/Components/ServiceList.py
+++ lib/python/Components/ServiceList.py
@@ -7,16 +7,17 @@ class ServiceList(HTMLComponent, GUIComponent):
 
 	MODE_NORMAL = 0
 	MODE_FAVOURITES = 1
-	
+
 	def __init__(self):
 		GUIComponent.__init__(self)
 		self.l = eListboxServiceContent()
-		
+		self.root = None
+
 	def getCurrent(self):
 		r = eServiceReference()
 		self.l.getCurrent(r)
 		return r
-		
+
 	def moveUp(self):
 		self.instance.moveSelection(self.instance.moveUp)
 
@@ -30,23 +31,37 @@ class ServiceList(HTMLComponent, GUIComponent):
 	def GUIdelete(self):
 		self.instance = None
 
+	def getRoot(self):
+		return self.root
+
+	def getRootServices(self):
+		serviceHandler = eServiceCenter.getInstance()
+		list = serviceHandler.list(self.root)
+		dest = [ ]
+		if list is not None:
+			while 1:
+				s = list.getNext()
+				if s.valid():
+					dest.append(s.toString())
+				else:
+					break
+		return dest
+
 	def setRoot(self, root):
+		self.root = root
 		self.l.setRoot(root)
 		self.l.sort()
-		
-		# mark stuff
-	def clearMarked(self):
-		self.l.clearMarked()
-		
+
+# stuff for multiple marks (edit mode / later multiepg)
 	def clearMarks(self):
 		self.l.initMarked()
-	
+
 	def isMarked(self, ref):
 		return self.l.isMarked(ref)
 
 	def addMarked(self, ref):
 		self.l.addMarked(ref)
-	
+
 	def removeMarked(self, ref):
 		self.l.removeMarked(ref)
 
@@ -56,11 +71,11 @@ class ServiceList(HTMLComponent, GUIComponent):
 		ref = eServiceReference()
 		marked = [ ]
 		while i.markedQueryNext(ref) == 0:
-			marked.append(ref)
+			marked.append(ref.toString())
 			ref = eServiceReference()
-
 		return marked
 
+#just for movemode.. only one marked entry..
 	def setCurrentMarked(self, state):
 		self.l.setCurrentMarked(state)
 
@@ -74,5 +89,4 @@ class ServiceList(HTMLComponent, GUIComponent):
 			self.l.setElementPosition(self.l.celServiceName, eRect(40, 0, self.instance.size().width(), 40))
 			self.l.setElementFont(self.l.celServiceNumber, gFont("Arial", 20))
 			self.l.setElementPosition(self.l.celServiceNumber, eRect(0, 10, 40, 30))
-
 			self.l.setVisualMode(eListboxServiceContent.visModeComplex)
