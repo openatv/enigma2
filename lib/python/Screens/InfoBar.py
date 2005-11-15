c88a496f48c1eed2d2bb1243e17c5125d353b63e
Paul In&In <darezik@gmail.com>
2005-11-15 03:57:19 +0000

    add CMoviePlayer

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -1,13 +1,16 @@
 from Screen import Screen
 
+from Screens.MovieSelection import MovieSelection
+from Screens.MessageBox import MessageBox
+
 from Components.Clock import Clock
 from Components.ActionMap import ActionMap
-from Screens.AudioSelection import AudioSelection
-from Screens.MovieSelection import MovieSelection
+from Components.ServicePosition import ServicePosition
 
 from Screens.InfoBarGenerics import InfoBarVolumeControl, InfoBarShowHide, \
 	InfoBarPowerKey, InfoBarNumberZap, InfoBarChannelSelection, InfoBarMenu, \
-	InfoBarEPG, InfoBarEvent, InfoBarServiceName, InfoBarPVR, InfoBarInstantRecord
+	InfoBarEPG, InfoBarEvent, InfoBarServiceName, InfoBarPVR, InfoBarInstantRecord, \
+	InfoBarAudioSelection
 
 from enigma import *
 
@@ -15,32 +18,55 @@ import time
 
 class InfoBar(Screen, InfoBarVolumeControl, InfoBarShowHide, InfoBarPowerKey, \
 	InfoBarNumberZap, InfoBarChannelSelection, InfoBarMenu, InfoBarEPG, \
-	InfoBarEvent, InfoBarServiceName, InfoBarPVR, InfoBarInstantRecord):
+	InfoBarEvent, InfoBarServiceName, InfoBarInstantRecord, InfoBarAudioSelection):
+
 	def __init__(self, session):
 		Screen.__init__(self, session)
+
+		self["actions"] = ActionMap( [ "InfobarActions" ],
+			{
+				"showMovies": self.showMovies,
+			})
 		
 		for x in InfoBarVolumeControl, InfoBarShowHide, InfoBarPowerKey, \
 			InfoBarNumberZap, InfoBarChannelSelection, InfoBarMenu, InfoBarEPG, \
-			InfoBarEvent, InfoBarServiceName, InfoBarPVR, InfoBarInstantRecord:
+			InfoBarEvent, InfoBarServiceName, InfoBarInstantRecord, InfoBarAudioSelection:
 			x.__init__(self)
 
-		self["actions"] = ActionMap( [ "InfobarActions" ], 
+		self["CurrentTime"] = Clock()
+
+	def showMovies(self):
+		self.session.openWithCallback(self.movieSelected, MovieSelection)
+	
+	def movieSelected(self, service):
+		if service is not None:
+			self.session.open(MoviePlayer, service)
+
+class MoviePlayer(Screen, InfoBarVolumeControl, InfoBarShowHide, InfoBarPowerKey, \
+		InfoBarMenu, \
+		InfoBarServiceName, InfoBarPVR, InfoBarAudioSelection):
+		
+	def __init__(self, session, service):
+		Screen.__init__(self, session)
+		
+		self["actions"] = ActionMap( [ "MoviePlayerActions" ],
 			{
-				"showMovies": self.showMovies,
-				#"quit": self.quit,
-				"audioSelection": self.audioSelection,
+				"leavePlayer": self.leavePlayer
 			})
+		
+		for x in InfoBarVolumeControl, InfoBarShowHide, InfoBarPowerKey, InfoBarMenu, InfoBarServiceName, InfoBarPVR, InfoBarAudioSelection:
+			x.__init__(self)
 
 		self["CurrentTime"] = Clock()
 		# ServicePosition(self.session.nav, ServicePosition.TYPE_REMAINING)
-		# Clock()
+		
+		self.lastservice = self.session.nav.getCurrentlyPlayingServiceReference()
+		self.session.nav.playService(service)
 
-	def showMovies(self):
-		self.session.open(MovieSelection)
-
-	def audioSelection(self):
-		service = self.session.nav.getCurrentService()
-		audio = service.audioTracks()
-		n = audio.getNumberOfTracks()
-		if n > 0:
-			self.session.open(AudioSelection, audio)
+	def leavePlayer(self):
+		self.session.openWithCallback(self.leavePlayerConfirmed, MessageBox, "Stop playing this movie?")
+	
+	def leavePlayerConfirmed(self, answer):
+		if answer == True:
+			self.session.nav.playService(self.lastservice)
+			self.close()
