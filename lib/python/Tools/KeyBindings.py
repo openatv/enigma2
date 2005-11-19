f513b8747b6fb84fa3225b5244529c4b6e387348
Paul In&In <darezik@gmail.com>
2005-11-19 02:16:16 +0000

    help: improve key description, a bit.

--- lib/python/Tools/KeyBindings.py
+++ lib/python/Tools/KeyBindings.py
@@ -1,6 +1,15 @@
 
 keyBindings = { }
 
+from keyids import KEYIDS
+
+keyDescriptions = {
+		KEYIDS["KEY_RED"]: "red",
+		KEYIDS["KEY_BLUE"]: "blue",
+		KEYIDS["KEY_MENU"]: "menu",
+		KEYIDS["KEY_VIDEO"]: "video"
+	}
+
 def addKeyBinding(key, context, action):
 	if (context, action) in keyBindings:
 		keyBindings[(context, action)].append(key)
@@ -14,4 +23,6 @@ def queryKeyBinding(context, action):
 		return [ ]
 
 def getKeyDescription(key):
+	if key in keyDescriptions:
+		return keyDescriptions[key]
 	return "key_%0x" % key
