4ae10f76135d53a4285fd458a87f485695c9fbd7
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2005-11-26 03:00:38 +0000

    get the file for the BlinkingPixmap from skin.xml

--- lib/python/Components/Pixmap.py
+++ lib/python/Components/Pixmap.py
@@ -3,23 +3,22 @@ import skin
 from enigma import *
 
 class Pixmap:
-	"""Pixmap can be used for components which use a pixmap"""
+	"""Pixmap can be used for components which diplay a pixmap"""
 	
 	def __init__(self):
 		self.instance = None
 	
 	def GUIcreate(self, parent):
 		self.instance = self.createWidget(parent)
-		#self.instance.setText(self.message)
 	
 	def GUIdelete(self):
 		self.removeWidget(self.instance)
 		self.instance = None
 	
-	def getePixmap(self, parent, filename):
-		pixmap = ePixmap(parent)
-		pixmap.setPixmapFromFile(filename)
-		return pixmap
+	def getePixmap(self, parent):
+		#pixmap = ePixmap(parent)
+		#pixmap.setPixmapFromFile(self.filename)
+		return ePixmap(parent)
 	
 	def removeWidget(self, instance):
 		pass
