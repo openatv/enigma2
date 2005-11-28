116be9bd37c5df45beb57c1e896e7158b7868cc3
Paul In&In <darezik@gmail.com>
2005-11-28 01:53:20 +0000

    audio: add language display

--- lib/python/Screens/AudioSelection.py
+++ lib/python/Screens/AudioSelection.py
@@ -2,17 +2,19 @@ from Screen import Screen
 from Components.ActionMap import ActionMap
 from Components.MenuList import MenuList
 from Components.Label import Label
+from Tools.ISO639 import LanguageCodes
 
 class AudioSelection(Screen):
 	def KeyOk(self):
 		selection = self["tracks"].getCurrent()
 		print "select track " + str(selection[1])
+		
 		self.audio.selectTrack(selection[1])
 		self.close()
 	def __init__(self, session, audio):
 		Screen.__init__(self, session)
 
-		self["actions"] = ActionMap(["SetupActions"], 
+		self["actions"] = ActionMap(["OkCancelActions"], 
 			{
 				"cancel": self.close,
 				"ok": self.KeyOk,
@@ -24,8 +26,17 @@ class AudioSelection(Screen):
 		n = audio.getNumberOfTracks()
 		for x in range(n):
 			i = audio.getTrackInfo(x)
-			tlist.append((i.getDescription(), x))
+			langCode = i.getLanguage()
+			
+			description = i.getDescription();
+			
+			if langCode in LanguageCodes:
+				language = LanguageCodes[langCode][0]
+				if len(description):
+					description += " (" + language + ")"
+				else:
+					description = language
+
+			tlist.append((description, x))
 
 		self["tracks"] = MenuList(tlist)
-
-		
