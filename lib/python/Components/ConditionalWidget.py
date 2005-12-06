775c7252e078a0fbc4226bfabf8a09b64e34c848
Paul In&In <darezik@gmail.com>
2005-12-06 19:35:47 +0000

    show red record button only if hdd is present

--- lib/python/Components/ConditionalWidget.py
+++ lib/python/Components/ConditionalWidget.py
@@ -40,7 +40,7 @@ class ConditionalWidget(Widget):
 		if (withTimer):
 			self.conditionCheckTimer = eTimer()
 			self.conditionCheckTimer.timeout.get().append(self.update)
-			self.conditionCheckTimer.start(1000)
+			self.conditionCheckTimer.start(500)
 		
 	def setConnect(self, conditionalFunction):
 		self.conditionalFunction = conditionalFunction
