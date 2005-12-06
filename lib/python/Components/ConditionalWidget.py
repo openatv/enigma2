7aaa13924d6d60f6fb66a262ce6ecd8088aee9b6
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2005-12-06 20:02:48 +0000

    the long awaited feature "blinking label" is now available

--- lib/python/Components/ConditionalWidget.py
+++ lib/python/Components/ConditionalWidget.py
@@ -62,3 +62,51 @@ class ConditionalWidget(Widget):
 				self.activateCondition(False)
 			
 			self.activateCondition(self.conditionalFunction())
+
+			
+			
+import time
+
+class BlinkingWidget(Widget):
+	def __init__(self):
+		Widget.__init__(self)
+		
+		self.blinking = True
+		
+		self.setBlinkTime(500)
+
+		self.timer = eTimer()
+		self.timer.timeout.get().append(self.blink)
+	
+	def setBlinkTime(self, time):
+		self.blinktime = time
+		
+	def blink(self):
+		if self.blinking == True:
+			if (self.state == self.SHOWN):
+				self.hideWidget()
+			elif (self.state == self.HIDDEN):
+				self.showWidget()
+			
+	def startBlinking(self):
+		self.blinking = True
+		self.timer.start(self.blinktime)
+		
+	def stopBlinking(self):
+		self.blinking = False
+		if (self.state == self.SHOWN):
+			self.hideWidget()
+		self.timer.stop()
+		
+class BlinkingWidgetConditional(BlinkingWidget, ConditionalWidget):
+	def __init__(self):
+		BlinkingWidget.__init__(self)
+		ConditionalWidget.__init__(self)
+		
+	def activateCondition(self, condition):
+		if (condition):
+			if not self.blinking: # we are already blinking
+				self.startBlinking()
+		else:
+			if self.blinking: # we are blinking
+				self.stopBlinking()			
