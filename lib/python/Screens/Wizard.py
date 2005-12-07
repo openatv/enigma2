53e4c40efd933ba8823246ef98a90ce3535b5f90
Paul In&In <darezik@gmail.com>
2005-12-07 13:31:28 +0000

    work on startup-wizard

--- lib/python/Screens/Wizard.py
+++ lib/python/Screens/Wizard.py
@@ -4,14 +4,17 @@ from Screens.HelpMenu import HelpableScreen
 from Components.Label import Label
 from Components.ActionMap import HelpableActionMap
 from Components.config import config, configElementBoolean
+from Components.Pixmap import Pixmap
 
 config.misc.firstrun = configElementBoolean("config.misc.firstrun", 1);
 
 class WelcomeWizard(Screen, HelpableScreen):
 
 	skin = """
-		<screen position="140,125" size="460,350" title="Welcome...">
-			<widget name="text" position="20,20" size="440,300" font="Arial;30" />
+		<screen position="0,0" size="720,560" title="Welcome..." flags="wfNoBorder">
+			<widget name="text" position="50,50" size="440,300" font="Arial;20" />
+			<widget name="rc" pixmap="/usr/share/enigma2/rc.png" position="500,50" size="154,475" alphatest="on" />
+			<widget name="circle" pixmap="/usr/share/enigma2/mute-fs8.png" position="520,200" zPosition="1" size="100,100" alphatest="on" />
 		</screen>"""
 
 	def __init__(self, session):
@@ -21,7 +24,9 @@ class WelcomeWizard(Screen, HelpableScreen):
 		HelpableScreen.__init__(self)
 
 
-		self["text"] = Label(_("Welcome!\n\nYou can always press the help key!\n\nPlease Note: Do a service search first!"));
+		self["text"] = Label(_("Hello User.\n\nThis start-wizard will guide you through the basic setup of your Dreambox."));
+		self["rc"] = Pixmap()
+		self["circle"] = Pixmap()
 		
 		self["actions"] = HelpableActionMap(self, "OkCancelActions",
 			{
