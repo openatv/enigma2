1024b449f8548eb7a77361ff9d8113ab4ab7a27f
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2005-12-11 23:14:20 +0000

    moving the list generation to a seperate method

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -12,18 +12,15 @@ class LanguageSelection(Screen):
 		Screen.__init__(self, session)
 		
 		self.list = []
-		if len(language.lang) == 0: # no language available => display only english
-			self.list.append(LanguageEntryComponent("en", _("English")))
-		else:
-			for x in language.lang:
-				self.list.append(LanguageEntryComponent(x[2], x[0]))
-		
 		self["list"] = LanguageList(self.list)
+		self.updateList()
 		
-		self["actions"] = ActionMap(["OkCancelActions"], 
+		self["actions"] = ActionMap(["ListboxActions", "OkCancelActions"], 
 		{
 			"ok": self.save,
-			"cancel": self.close
+			"cancel": self.close,
+			"moveUp": self.up,
+			"moveDown": self.down
 		})
 		
 	def save(self):
@@ -34,3 +31,19 @@ class LanguageSelection(Screen):
 		language.activateLanguage(self["list"].l.getCurrentSelectionIndex())
 		config.osd.language.value = self["list"].l.getCurrentSelectionIndex()
 		config.osd.language.save()
+
+	def updateList(self):
+		self.list = []
+		if len(language.lang) == 0: # no language available => display only english
+			self.list.append(LanguageEntryComponent("en", _("English")))
+		else:
+			for x in language.lang:
+				self.list.append(LanguageEntryComponent(x[2], _(x[0])))
+		
+		self["list"].l.setList(self.list)
+		
+	def up(self):
+		print "up"
+		
+	def down(self):
+		print "down"
