df6123056e2b40f44b8233bc63e923c0d7cf0577
Paul In&In <darezik@gmail.com>
2005-12-17 18:00:02 +0000

    tutorial manager proof-of-concept

--- lib/python/Screens/TutorialWizard.py
+++ lib/python/Screens/TutorialWizard.py
@@ -10,7 +10,7 @@ config.misc.firstruntutorial = configElementBoolean("config.misc.firstruntutoria
 
 class TutorialWizard(Wizard):
 	skin = """
-		<screen position="0,0" size="720,560" title="Welcome..." flags="wfNoBorder" >
+		<screen position="0,0" size="720,300" title="Welcome..." flags="wfNoBorder" >
 			<widget name="text" position="50,100" size="440,200" font="Arial;23" />
 			<widget name="rc" pixmap="/usr/share/enigma2/rc.png" position="500,600" zPosition="10" size="154,475" transparent="1" alphatest="on"/>
 			<widget name="arrowdown" pixmap="/usr/share/enigma2/arrowdown.png" position="0,0" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
