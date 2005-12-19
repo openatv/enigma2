c11e4777be508ebbed677c9e8e3d11eedb73ee22
Paul In&In <darezik@gmail.com>
2005-12-19 20:34:58 +0000

    wizardize the start wizard

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -10,11 +10,11 @@ config.misc.firstrun = configElementBoolean("config.misc.firstrun", 1);
 class StartWizard(Wizard):
 	skin = """
 		<screen position="0,0" size="720,560" title="Welcome..." flags="wfNoBorder" >
-			<widget name="text" position="50,100" size="440,200" font="Arial;23" />
+			<widget name="text" position="150,50" size="340,270" font="Arial;23" />
 			<widget name="list" position="50,300" zPosition="1" size="440,200" />
 			<widget name="config" position="50,300" zPosition="1" size="440,200" transparent="1" />			
-			<widget name="step" position="50,50" size="440,25" font="Arial;23" />
 			<widget name="stepslider" position="50,500" zPosition="1" borderWidth="2" size="440,20" backgroundColor="dark" />
+			<widget name="wizard" pixmap="/usr/share/enigma2/wizard.png" position="40,50" zPosition="10" size="110,174" transparent="1" alphatest="on"/>
 			<widget name="rc" pixmap="/usr/share/enigma2/rc.png" position="500,600" zPosition="10" size="154,475" transparent="1" alphatest="on"/>
 			<widget name="arrowdown" pixmap="/usr/share/enigma2/arrowdown.png" position="0,0" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
 			<widget name="arrowup" pixmap="/usr/share/enigma2/arrowup.png" position="-100,-100" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
@@ -25,7 +25,8 @@ class StartWizard(Wizard):
 		self.skin = StartWizard.skin
 		self.xmlfile = "startwizard.xml"
 		
-		Wizard.__init__(self, session)
+		Wizard.__init__(self, session, showSteps = False)
+		self["wizard"] = Pixmap()
 		self["rc"] = MovingPixmap()
 		self["arrowdown"] = MovingPixmap()
 		self["arrowup"] = MovingPixmap()
