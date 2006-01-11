22414818f32ccf04fa6f6805b0e2fde56e1563ea
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2006-01-11 15:13:34 +0000

    finish the tuxbox plugin parser plugin

--- lib/python/Plugins/tuxboxplugins/plugin.py
+++ lib/python/Plugins/tuxboxplugins/plugin.py
@@ -7,8 +7,26 @@ from Components.Label import Label
 import os
 
 def getPlugins():
-	return [("Tuxbox-Plugin1", "function", "main", 0),
-			("Tuxbox-Plugin2", "function", "main", 1)]
+	dir = os.listdir("/usr/lib/tuxbox/plugins/")
+	
+	pluginlist = []
+	for x in dir:
+		try:
+			if x[-3:] == "cfg":
+				pluginlist.append((getPluginParams(x)["name"], "function", "main", x))
+		except:
+			pass
+	return pluginlist
+
+def getPluginParams(file):
+	file = open("/usr/lib/tuxbox/plugins/" + file, "r")
+	lines = file.readlines()
+	file.close()
+	params = {}
+	for x in lines:
+		split = x.split("=")
+		params[split[0]] = split[1]
+	return params
 
 def main(session, args):
 	print "Running plugin with number", args
