d375bc39f479e6d2cb52ea8ea6222f07f9c9b842
Paul In&In <darezik@gmail.com>
2006-01-15 15:29:39 +0000

    set wss according to av settings

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -17,8 +17,18 @@ class AVSwitch:
 		eAVSwitch.getInstance().setVideomode(value)
 
 	def setWSS(self, value):
-		#print "wss:" + str(value)
-		pass
+		if value == 0:
+			writevalue = "off"
+		elif value == 1:
+			writevalue = "auto"
+		elif value == 2:
+			writevalue = "auto(4:3_off)"
+		try:
+			file = open("/proc/stb/denc/0/wss", "w")
+			file.write(writevalue)
+			file.close()
+		except:
+			print "[AVSwitch.py] Error writing to /proc/stb/denc/0/wss"
 	
 	def setInput(self, input):
 		eAVSwitch.getInstance().setInput(self.INPUT[input])
@@ -27,13 +37,13 @@ class AVSwitch:
 
 def InitAVSwitch():
 	config.av = ConfigSubsection();
-	config.av.colorformat = configElement("config.av.colorformat", configSelection, 1, (("cvbs", _("CVBS")), ("rgb", _("RGB")), ("svideo", _("S-Video")) ));
-	config.av.aspectratio = configElement("config.av.aspectratio", configSelection, 0, (("4_3_letterbox", _("4:3 Letterbox")), ("4_3_panscan", _("4:3 PanScan")), ("16_9", _("16:9")), ("16_9_always", _("16:9 always"))) );
-	#config.av.tvsystem = configElement("config.av.tvsystem", configSelection, 0, ("PAL", "PAL + PAL60", "Multi", "NTSC") );
-	config.av.tvsystem = configElement("config.av.tvsystem", configSelection, 0, (("pal", _("PAL")), ("ntsc", _("NTSC"))) );
-	config.av.wss = configElement("config.av.wss", configSelection, 0, (("enable", _("Enable")), ("disable", _("Disable"))) );
-	config.av.defaultac3 = configElement("config.av.defaultac3", configSelection, 1, (("enable", _("Enable")), ("disable", _("Disable"))));
-	config.av.vcrswitch = configElement("config.av.vcrswitch", configSelection, 1, (("enable", _("Enable")), ("disable", _("Disable"))));
+	config.av.colorformat = configElement("config.av.colorformat", configSelection, 1, (("cvbs", _("CVBS")), ("rgb", _("RGB")), ("svideo", _("S-Video")) ))
+	config.av.aspectratio = configElement("config.av.aspectratio", configSelection, 0, (("4_3_letterbox", _("4:3 Letterbox")), ("4_3_panscan", _("4:3 PanScan")), ("16_9", _("16:9")), ("16_9_always", _("16:9 always"))) )
+	#config.av.tvsystem = configElement("config.av.tvsystem", configSelection, 0, ("PAL", "PAL + PAL60", "Multi", "NTSC") )
+	config.av.tvsystem = configElement("config.av.tvsystem", configSelection, 0, (("pal", _("PAL")), ("ntsc", _("NTSC"))) )
+	config.av.wss = configElement("config.av.wss", configSelection, 0, (("off", _("Off")), ("auto", _("Auto")), ("auto_no4_3", _("Auto (disable on 4:3)"))) )
+	config.av.defaultac3 = configElement("config.av.defaultac3", configSelection, 1, (("enable", _("Enable")), ("disable", _("Disable"))))
+	config.av.vcrswitch = configElement("config.av.vcrswitch", configSelection, 1, (("enable", _("Enable")), ("disable", _("Disable"))))
 
 	iAVSwitch = AVSwitch()
 
