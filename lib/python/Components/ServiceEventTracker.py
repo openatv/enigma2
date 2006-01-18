69382ab48558b9305b27e464857deebdfa52b6b9
Felix Domke <tmbinc@elitedvb.net>
2006-01-18 02:34:36 +0000

    add ServiceEventTracker

---
+++ lib/python/Components/ServiceEventTracker.py
@@ -0,0 +1,17 @@
+from enigma import pNavigation
+
+class ServiceEventTracker:
+	"""Tracks service events into a screen"""
+	
+	def __init__(self, screen, eventmap):
+		self.__eventmap = eventmap
+		self.session = screen.session
+		self.session.nav.event.append(self.__event)
+		screen.onClose.append(self.__del_event)
+
+	def __del_event(self):
+		self.session.nav.event.remove(self.__event)
+
+	def __event(self, ev):
+		if ev in self.__eventmap:
+			self.__eventmap[ev]()
