9e8504f3bb1d87717ee84da934836a063c77cba4
Paul In&In <darezik@gmail.com>
2006-01-19 17:06:27 +0000

    add non working multi bouquet switch

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -2,7 +2,19 @@ from config import *
 import os
 from enigma import *
 
+from Screens.ChannelSelection import USE_MULTIBOUQUETS
+global USE_MULTIBOUQUETS
+
 def InitUsageConfig():
 	config.usage = ConfigSubsection();
 	config.usage.epgtoggle = configElement("config.usage.epgtoggle", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
 	config.usage.showdish = configElement("config.usage.showdish", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
+	config.usage.multibouquet = configElement("config.usage.multibouquet", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
+	
+	def setMultiBouquet(configElement):
+		if currentConfigSelectionElement(configElement) == "no":
+			USE_MULTIBOUQUETS = False
+		else:
+			USE_MULTIBOUQUETS = True
+		
+	config.usage.multibouquet.addNotifier(setMultiBouquet);
