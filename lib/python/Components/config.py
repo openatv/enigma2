324b8434d9652a89596079c50f42fce33685ab65
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2006-01-21 17:21:20 +0000

    disable debug info

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -478,8 +478,8 @@ class configElement:
 		elif control == configText:
 			return str(data.strip())
 		elif control == configSequence:
-			print self.vals
-			print self.value
+#			print self.vals
+#			print self.value
 			try:
 				value = ""
 				count = 0
