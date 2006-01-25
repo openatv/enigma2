34de9dad5810732ca9b1b56009dd96dff88883e2
Paul In&In <darezik@gmail.com>
2006-01-25 16:15:44 +0000

    many info handling changes (nownext/multi/single epg)

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -4,7 +4,6 @@ from enigma import *
 
 def InitUsageConfig():
 	config.usage = ConfigSubsection();
-	config.usage.epgtoggle = configElement("config.usage.epgtoggle", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
 	config.usage.showdish = configElement("config.usage.showdish", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
 	config.usage.multibouquet = configElement("config.usage.multibouquet", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
 
