653246824859d28a8310eb1a52d91e05cce27489
Paul In&In <darezik@gmail.com>
2006-01-28 16:54:41 +0000

    fix no more working multi epg since previous BouquetSelector change

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -36,7 +36,6 @@ class BouquetSelector(Screen):
 
 	def okbuttonClick(self):
 		self.selectedFunc(self["menu"].getCurrent()[1])
-		self.close(True)
 
 	def cancelClick(self):
 		self.close(False)
@@ -107,11 +106,15 @@ class ChannelContextMenu(Screen):
 		else:
 			cnt = len(bouquets)
 		if cnt > 1: # show bouquet list
-			self.session.openWithCallback(self.bouquetSelClosed, BouquetSelector, bouquets, self.addCurrentServiceToBouquet)
+			self.session.openWithCallback(self.bouquetSelClosed, BouquetSelector, bouquets, self.bouquetSelected)
 		elif cnt == 1: # add to only one existing bouquet
 			self.addCurrentServiceToBouquet(bouquets[0][1])
 		else: #no bouquets in root.. so assume only one favourite list is used
 			self.addCurrentServiceToBouquet(self.csel.bouquet_root)
+
+	def bouquetSelected(self, bouquet):
+		self.addCurrentServiceToBouquet(bouquet)
+		self.close(True) # close bouquet selection
 
 	def bouquetSelClosed(self, recursive):
 		if recursive:
