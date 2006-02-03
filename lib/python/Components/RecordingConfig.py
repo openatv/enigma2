1963e01e7ffb86fe0e6942d3630222eddf1ebc67
Felix Domke <tmbinc@elitedvb.net>
2006-02-03 21:30:49 +0000

    add record margin

--- lib/python/Components/RecordingConfig.py
+++ lib/python/Components/RecordingConfig.py
@@ -4,6 +4,6 @@ from enigma import *
 
 def InitRecordingConfig():
 	config.recording = ConfigSubsection();
-	config.recording.asktozap = configElement("config.recording.asktozap", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) );
-
-
+	config.recording.asktozap = configElement("config.recording.asktozap", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
+	config.recording.margin_before = configElement("config.recording.margin_before", configSequence, [0], configsequencearg.get("INTEGER", (0, 30)))
+	config.recording.margin_after = configElement("config.recording.margin_after", configSequence, [0], configsequencearg.get("INTEGER", (0, 30)))
