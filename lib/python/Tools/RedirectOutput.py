9b31d0bf9be2d677fb5d82d5e9faad64167abf9d
Felix Domke <tmbinc@elitedvb.net>
2006-02-13 20:40:51 +0000

    encode unicode strings as UTF8 for console output

--- lib/python/Tools/RedirectOutput.py
+++ lib/python/Tools/RedirectOutput.py
@@ -3,8 +3,10 @@ from enigma import ePythonOutput
 
 class EnigmaOutput:
 	def write(self, data):
+		if type(data) is unicode:
+			data = data.encode("UTF-8")
 		ePythonOutput(data)
-	
+
 	def flush():
 		pass
 
