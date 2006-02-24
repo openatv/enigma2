4c6226bbf4d32d3abdc640a63581d963dd94937b
Felix Domke <tmbinc@elitedvb.net>
2006-02-24 22:37:30 +0000

    use MultiContent wraps in plugin list

--- lib/python/Components/MultiContent.py
+++ lib/python/Components/MultiContent.py
@@ -11,5 +11,8 @@ RT_WRAP = 32
 
 from enigma import eListboxPythonMultiContent
 
-def MultiContentEntryText(pos = (0, 0), size = (0, 0), font = 0, flags = 0, text = "", private = ()):
-	return (eListboxPythonMultiContent.TYPE_TEXT, pos[0], pos[1], size[0], size[1], font, flags, text) + private
+def MultiContentEntryText(pos = (0, 0), size = (0, 0), font = 0, flags = RT_HALIGN_LEFT | RT_VALIGN_TOP, text = ""):
+	return (eListboxPythonMultiContent.TYPE_TEXT, pos[0], pos[1], size[0], size[1], font, flags, text)
+
+def MultiContentEntryPixmap(pos = (0, 0), size = (0, 0), png = None):
+	return (eListboxPythonMultiContent.TYPE_PIXMAP, pos[0], pos[1], size[0], size[1], png)
