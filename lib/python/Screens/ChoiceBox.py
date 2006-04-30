e8f4b9242fc67bcea9b561d3e7bc34c25b3ea0a1
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2006-04-30 20:22:36 +0000

    use color actions in ChoiceBox

--- lib/python/Screens/ChoiceBox.py
+++ lib/python/Screens/ChoiceBox.py
@@ -16,7 +16,7 @@ class ChoiceBox(Screen):
 		self["text"] = Label(title)
 		self.list = []
 		if keys is None:
-			self.keys = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" ] + (len(list) - 10) * [""]
+			self.keys = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "red", "green", "yellow", "blue" ] + (len(list) - 10) * [""]
 		else:
 			self.keys = keys
 			
@@ -30,7 +30,7 @@ class ChoiceBox(Screen):
 			pos += 1
 		self["list"] = ChoiceList(self.list)
 				
-		self["actions"] = NumberActionMap(["WizardActions", "InputActions"], 
+		self["actions"] = NumberActionMap(["WizardActions", "InputActions", "ColorActions"], 
 		{
 			"ok": self.go,
 			"back": self.cancel,
@@ -43,7 +43,11 @@ class ChoiceBox(Screen):
 			"7": self.keyNumberGlobal,
 			"8": self.keyNumberGlobal,
 			"9": self.keyNumberGlobal,
-			"0": self.keyNumberGlobal
+			"0": self.keyNumberGlobal,
+			"red": self.keyRed,
+			"green": self.keyGreen,
+			"yellow": self.keyYellow,
+			"blue": self.keyBlue
 		}, -1)
 		
 	def keyLeft(self):
@@ -60,6 +64,22 @@ class ChoiceBox(Screen):
 	def go(self):
 		self.close(self["list"].l.getCurrentSelection()[0])
 		#self.close(self["input"].getText())
-		
+
+	def keyRed(self):
+		if self.keymap.has_key("red"):
+			self.close(self.keymap["red"])
+
+	def keyGreen(self):
+		if self.keymap.has_key("green"):
+			self.close(self.keymap["green"])
+	
+	def keyYellow(self):
+		if self.keymap.has_key("yellow"):
+			self.close(self.keymap["yellow"])
+			
+	def keyBlue(self):
+		if self.keymap.has_key("blue"):
+			self.close(self.keymap["blue"])
+			
 	def cancel(self):
 		self.close(None)
