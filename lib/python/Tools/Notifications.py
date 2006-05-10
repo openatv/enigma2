cb3e4c3b6d7760434c65a75fefb7624fa5ef30a4
Paul In&In <darezik@gmail.com>
2006-05-10 23:02:07 +0000

    add kwargs support for notifications

--- lib/python/Tools/Notifications.py
+++ lib/python/Tools/Notifications.py
@@ -3,11 +3,10 @@ notifications = [ ]
 
 notificationAdded = [ ]
 
-def AddNotification(screen, *args):
-	AddNotificationWithCallback(None, screen, *args)
+def AddNotification(screen, *args, **kwargs):
+	AddNotificationWithCallback(None, screen, *args, **kwargs)
 
-def AddNotificationWithCallback(fnc, screen, *args):
-	notifications.append((fnc, screen) + args)
+def AddNotificationWithCallback(fnc, screen, *args, **kwargs):
+	notifications.append((fnc, screen, args, kwargs))
 	for x in notificationAdded:
 		x()
-
