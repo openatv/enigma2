59f97885223c5c1b07c6125ae99a0d9c3f6a1b55
Felix Domke <tmbinc@elitedvb.net>
2006-05-24 21:14:05 +0000

    increase rfmod tuning range.

--- lib/python/Components/RFmod.py
+++ lib/python/Components/RFmod.py
@@ -1,6 +1,10 @@
 from config import *
 from enigma import *
 
+# CHECK ME.
+RFMOD_CHANNEL_MIN = 21
+RFMOD_CHANNEL_MAX = 69 + 1
+
 class RFmod:
 	def __init__(self):
 		pass
@@ -25,7 +29,7 @@ def InitRFmod():
 	config.rfmod.test = configElement("config.rfmod.test", configSelection, 0, (("disable", _("Disable")), ("enable", _("Enable"))) );
 	config.rfmod.sound = configElement("config.rfmod.sound", configSelection, 0, (("enable", _("Enable")), ("disable", _("Disable"))) );
 	config.rfmod.soundcarrier = configElement("config.rfmod.soundcarrier", configSelection, 1, ("4.5 MHz", "5.5 MHz", "6.0 MHz", "6.5 MHz") );
-	config.rfmod.channel = configElement("config.rfmod.channel", configSelection, 6, ("30", "31", "32", "33", "34", "35", "36", "37", "38", "39") );
+	config.rfmod.channel = configElement("config.rfmod.channel", configSelection, 36 - RFMOD_CHANNEL_MIN, tuple(["%d" % x for x in range(RFMOD_CHANNEL_MIN, RFMOD_CHANNEL_MAX)]))
 	config.rfmod.finetune = configElement("config.rfmod.finetune", configSlider, 5, (1, 10));
 
 	iRFmod = RFmod()
