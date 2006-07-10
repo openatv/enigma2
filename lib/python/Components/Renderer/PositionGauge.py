2855d90bf48292650e7565f09089040c6f2d6e8a
Paul In&In <darezik@gmail.com>
2006-07-10 16:19:41 +0000

    add PositionGauge renderer

---
+++ lib/python/Components/Renderer/PositionGauge.py
@@ -0,0 +1,51 @@
+from Renderer import Renderer
+from enigma import ePositionGauge
+
+class PositionGauge(Renderer):
+	def __init__(self):
+		Renderer.__init__(self)
+		self.__position = 0
+		self.__length = 0
+		self.__cutlist = [ ]
+	
+	GUI_WIDGET = ePositionGauge
+	
+	def postWidgetCreate(self, instance):
+		self.changed()
+		instance.setInOutList(self.__cutlist)
+
+	def changed(self):
+		(self.length, self.position) = (self.source.length or 0, self.source.position or 0)
+
+	def cutlist_changed(self):
+		self.cutlist = self.source.cutlist or [ ]
+
+	def getPosition(self):
+		return self.__position
+	
+	def setPosition(self, pos):
+		self.__position = pos
+		if self.instance is not None:
+			self.instance.setPosition(pos)
+	
+	position = property(getPosition, setPosition)
+
+	def getLength(self):
+		return self.__length
+	
+	def setLength(self, len):
+		self.__length = len
+		if self.instance is not None:
+			self.instance.setLength(len)
+
+	length = property(getLength, setLength)
+
+	def getCutlist(self):
+		return self.__cutlist
+	
+	def setCutlist(self, cutlist):
+		self.__cutlist = cutlist
+		if self.instance is not None:
+			self.instance.setInOutList(cutlist)
+
+	cutlist = property(getCutlist, setCutlist)
