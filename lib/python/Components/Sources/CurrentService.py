834bbf9162cfd90cd063e25aa45fc3c28fdee742
Felix Domke <tmbinc@elitedvb.net>
2006-07-10 16:22:44 +0000

    generate 'changed'-events with event argument

--- lib/python/Components/Sources/CurrentService.py
+++ lib/python/Components/Sources/CurrentService.py
@@ -7,14 +7,18 @@ class CurrentService(PerServiceBase, Source):
 		Source.__init__(self)
 		PerServiceBase.__init__(self, navcore, 
 			{ 
-				iPlayableService.evStart: self.changed,
-				iPlayableService.evEnd: self.changed,
+				iPlayableService.evStart: self.serviceEvent,
+				iPlayableService.evEnd: self.serviceEvent,
 				# FIXME: we should check 'interesting_events'
 				# which is not always provided.
-				iPlayableService.evUpdatedInfo: self.changed,
-				iPlayableService.evUpdatedEventInfo: self.changed
-			})
+				iPlayableService.evUpdatedInfo: self.serviceEvent,
+				iPlayableService.evUpdatedEventInfo: self.serviceEvent,
+				iPlayableService.evCuesheetChanged: self.serviceEvent
+			}, with_event=True)
 		self.navcore = navcore
+
+	def serviceEvent(self, event):
+		self.changed(event)
 
 	def getCurrentService(self):
 		return self.navcore.getCurrentService()
