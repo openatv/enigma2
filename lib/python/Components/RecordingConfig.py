f0a0f6b79c99fb346c32e435a543c1649a197f54
Felix Domke <tmbinc@elitedvb.net>
2006-07-28 11:58:31 +0000

    fix sense

--- lib/python/Components/RecordingConfig.py
+++ lib/python/Components/RecordingConfig.py
@@ -4,6 +4,7 @@ from enigma import *
 
 def InitRecordingConfig():
 	config.recording = ConfigSubsection();
-	config.recording.asktozap = configElement("config.recording.asktozap", configSelection, 1, (("yes", _("yes")), ("no", _("no"))) )
+	# actually this is "recordings always have priority". "Yes" does mean: don't ask. The RecordTimer will ask when value is 0.
+	config.recording.asktozap = configElement("config.recording.asktozap", configSelection, 1, (("no", _("no")), ("yes", _("yes"))) )
 	config.recording.margin_before = configElement("config.recording.margin_before", configSequence, [0], configsequencearg.get("INTEGER", (0, 30)))
 	config.recording.margin_after = configElement("config.recording.margin_after", configSequence, [0], configsequencearg.get("INTEGER", (0, 30)))
