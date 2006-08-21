ab27e94e1aae7dcae312b115031029c7efcae4ce
Paul In&In <darezik@gmail.com>
2006-08-21 23:37:27 +0000

    fix non working code

--- lib/python/Tools/NumericalTextInput.py
+++ lib/python/Tools/NumericalTextInput.py
@@ -3,43 +3,44 @@ from enigma import *
 from Components.Language import language
 
 class NumericalTextInput:
-	mapping = []
-	lang = language.getLanguage()
-	if lang == 'de_DE':
-		mapping.append (".,?'\"0-()@/:_") # 0
-		mapping.append (" 1") # 1
-		mapping.append ("aäbc2AABC") # 2
-		mapping.append ("def3DEF") # 3
-		mapping.append ("ghi4GHI") # 4
-		mapping.append ("jkl5JKL") # 5
-		mapping.append ("mnoö6MNOÖ") # 6
-		mapping.append ("pqrsß7PQRSß") # 7
-		mapping.append ("tuüv8TUÜV") # 8
-		mapping.append ("wxyz9WXYZ") # 9
-	elif lang == 'es_ES':
-		mapping.append (".,?'\"0-()@/:_") # 0
-		mapping.append (" 1") # 1
-		mapping.append ("abcáà2ABCÁÀ") # 2
-		mapping.append ("deéèf3DEFÉÈ") # 3
-		mapping.append ("ghiíì4GHIÍÌ") # 4
-		mapping.append ("jkl5JKL") # 5
-		mapping.append ("mnñoóò6MNÑOÓÒ") # 6
-		mapping.append ("pqrs7PQRS") # 7
-		mapping.append ("tuvúù8TUVÚÙ") # 8
-		mapping.append ("wxyz9WXYZ") # 9
-	else:
-		mapping.append (".,?'\"0-()@/:_") # 0
-		mapping.append (" 1") # 1
-		mapping.append ("abc2ABC") # 2
-		mapping.append ("def3DEF") # 3
-		mapping.append ("ghi4GHI") # 4
-		mapping.append ("jkl5JKL") # 5
-		mapping.append ("mno6MNO") # 6
-		mapping.append ("pqrs7PQRS") # 7
-		mapping.append ("tuv8TUV") # 8
-		mapping.append ("wxyz9WXYZ") # 9
-
 	def __init__(self, nextFunction = None):
+		self.mapping = []
+		self.lang = language.getLanguage()
+		
+		if self.lang == 'de_DE':
+			self.mapping.append (".,?'\"0-()@/:_") # 0
+			self.mapping.append (" 1") # 1
+			self.mapping.append ("aäbc2AABC") # 2
+			self.mapping.append ("def3DEF") # 3
+			self.mapping.append ("ghi4GHI") # 4
+			self.mapping.append ("jkl5JKL") # 5
+			self.mapping.append ("mnoö6MNOÖ") # 6
+			self.mapping.append ("pqrsß7PQRSß") # 7
+			self.mapping.append ("tuüv8TUÜV") # 8
+			self.mapping.append ("wxyz9WXYZ") # 9
+		elif self.lang == 'es_ES':
+			self.mapping.append (".,?'\"0-()@/:_") # 0
+			self.mapping.append (" 1") # 1
+			self.mapping.append ("abcáà2ABCÁÀ") # 2
+			self.mapping.append ("deéèf3DEFÉÈ") # 3
+			self.mapping.append ("ghiíì4GHIÍÌ") # 4
+			self.mapping.append ("jkl5JKL") # 5
+			self.mapping.append ("mnñoóò6MNÑOÓÒ") # 6
+			self.mapping.append ("pqrs7PQRS") # 7
+			self.mapping.append ("tuvúù8TUVÚÙ") # 8
+			self.mapping.append ("wxyz9WXYZ") # 9
+		else:
+			self.mapping.append (".,?'\"0-()@/:_") # 0
+			self.mapping.append (" 1") # 1
+			self.mapping.append ("abc2ABC") # 2
+			self.mapping.append ("def3DEF") # 3
+			self.mapping.append ("ghi4GHI") # 4
+			self.mapping.append ("jkl5JKL") # 5
+			self.mapping.append ("mno6MNO") # 6
+			self.mapping.append ("pqrs7PQRS") # 7
+			self.mapping.append ("tuv8TUV") # 8
+			self.mapping.append ("wxyz9WXYZ") # 9
+		
 		self.nextFunction = nextFunction
 		self.Timer = eTimer()
 		self.Timer.timeout.get().append(self.nextChar)
