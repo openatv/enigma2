aac38dc40762a906d42e626dcdd06a8dc3745da1
Paul In&In <darezik@gmail.com>
2006-10-10 03:00:49 +0000

    add parental control (still somehow buggy and some minor features missing... don't trust it yet to protect your children)

---
+++ lib/python/Components/ParentalControlList.py
@@ -0,0 +1,55 @@
+from HTMLComponent import *
+from GUIComponent import *
+
+from MenuList import MenuList
+from Components.ParentalControl import parentalControl
+from Tools.Directories import *
+
+from enigma import *
+
+RT_HALIGN_LEFT = 0
+RT_HALIGN_RIGHT = 1
+RT_HALIGN_CENTER = 2
+RT_HALIGN_BLOCK = 4
+
+RT_VALIGN_TOP = 0
+RT_VALIGN_CENTER = 8
+RT_VALIGN_BOTTOM = 16
+
+lockPicture = loadPNG(resolveFilename(SCOPE_SKIN_IMAGE, "lock-fs8.png"))
+
+def ParentalControlEntryComponent(service, name, locked = True):
+	res = [ (service, name, locked) ]
+	res.append((eListboxPythonMultiContent.TYPE_TEXT, 80, 5, 200, 50, 0, RT_HALIGN_LEFT, name))
+	if locked:
+		res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHATEST, 0, 0, 32, 32, lockPicture))
+	
+	return res
+
+class ParentalControlList(MenuList, HTMLComponent, GUIComponent):
+	def __init__(self, list):
+		GUIComponent.__init__(self)
+		self.l = eListboxPythonMultiContent()
+		self.list = list
+		self.l.setList(list)
+		self.l.setFont(0, gFont("Regular", 20))
+
+	GUI_WIDGET = eListbox
+	
+	def setList(self, list):
+		self.list = list
+		self.l.setList(list)
+		
+	def postWidgetCreate(self, instance):
+		instance.setContent(self.l)
+		instance.setItemHeight(32)
+
+	def toggleSelectedLock(self):
+		print "self.l.getCurrentSelection():", self.l.getCurrentSelection()
+		print "self.l.getCurrentSelectionIndex():", self.l.getCurrentSelectionIndex()
+		self.list[self.l.getCurrentSelectionIndex()] = ParentalControlEntryComponent(self.l.getCurrentSelection()[0][0], self.l.getCurrentSelection()[0][1], not self.l.getCurrentSelection()[0][2]);
+		if self.l.getCurrentSelection()[0][2]:
+			parentalControl.protectService(self.l.getCurrentSelection()[0][0])
+		else:
+			parentalControl.unProtectService(self.l.getCurrentSelection()[0][0])	
+		self.l.setList(self.list)
--- lib/python/Screens/InputBox.py
+++ lib/python/Screens/InputBox.py
@@ -5,15 +5,17 @@ from Components.ActionMap import NumberActionMap
 from Components.Label import Label
 from Components.Input import Input
 from Components.GUIComponent import *
+from Tools.BoundFunction import boundFunction
 
 import os
 
 class InputBox(Screen):
-	def __init__(self, session, title = "", **kwargs):
+	def __init__(self, session, title = "", windowTitle = _("Input"), **kwargs):
 		Screen.__init__(self, session)
 
 		self["text"] = Label(title)
 		self["input"] = Input(**kwargs)
+		self.onShown.append(boundFunction(self.setTitle, windowTitle))
 
 		self["actions"] = NumberActionMap(["WizardActions", "InputBoxActions", "InputAsciiActions", "KeyboardInputActions"], 
 		{
@@ -85,3 +87,50 @@ class InputBox(Screen):
 
 	def keyInsert(self):
 		self["input"].toggleOverwrite()
+
+class PinInput(InputBox):
+	def __init__(self, session, service = "", tries = 1, pinList = [], *args, **kwargs):
+		InputBox.__init__(self, session = session, text="9876", maxSize=True, type=Input.PIN, *args, **kwargs)
+		
+		self.showTries = True
+		if tries == 1:
+			self.showTries = False
+
+		self.pinList = pinList
+		self["service"] = Label(service)
+		
+		self["tries"] = Label("")
+		self.onShown.append(boundFunction(self.setTries, tries))
+				
+	def keyNumberGlobal(self, number):
+		if self["input"].currPos == len(self["input"]) - 1:
+			InputBox.keyNumberGlobal(self, number)
+			self.go()
+		else:
+			InputBox.keyNumberGlobal(self, number)
+		
+	def checkPin(self, pin):
+		if pin is not None and int(pin) in self.pinList:
+			return True
+		return False
+		
+	def go(self):
+		if self.checkPin(self["input"].getText()):
+			self.close((True, self.tries))
+		else:
+			self.keyHome()
+			self.setTries(self.tries - 1)
+			if self.tries == 0:
+				self.close((False, self.tries))
+			else:
+				pass
+			
+	def cancel(self):
+		rcinput = eRCInput.getInstance()
+		rcinput.setKeyboardMode(rcinput.kmNone)
+		self.close((None, self.tries))
+	
+	def setTries(self, tries):
+		self.tries = tries
+		if self.showTries:
+			self["tries"].setText(_("Tries left:") + " " + str(tries))
