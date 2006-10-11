9c6d304651186630ad19f9975cb0b66dc2220123
Paul In&In <darezik@gmail.com>
2006-10-11 12:29:11 +0000

    convert radiotext to utf8

--- lib/python/Components/Converter/RadioText.py
+++ lib/python/Components/Converter/RadioText.py
@@ -4,12 +4,16 @@ from Components.Element import cached
 class RadioText(Converter, object):
 	def __init__(self, type):
 		Converter.__init__(self, type)
+		self.type = type
 
 	@cached
 	def getText(self):
 		rt = self.source.radiotext
 		if rt is None:
 			return "N/A"
-		return rt
-
+		text = rt.getRadioText()
+		if self.type == "RadioText-UTF8":
+			return text.decode("latin-1").encode("utf-8")
+		else:
+			return text
 	text = property(getText)
