d54cb733870ac099e9474888f8efb9c3fc573b75
Paul In&In <darezik@gmail.com>
2006-10-14 09:59:34 +0000

    small fix

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -6,9 +6,9 @@ def InitUsageConfig():
 	config.usage.multibouquet = ConfigYesNo(default = False)
 	config.usage.quickzap_bouquet_change = ConfigYesNo(default = False)
 	config.usage.e1like_radio_mode = ConfigYesNo(default = False)
-	config.usage.infobar_timeout = ConfigSelection(default = _("5 seconds"), choices = [
-		_("no timeout"), _("1 second"), _("2 seconds"), _("3 seconds"), _("4 seconds"),
-		_("5 seconds"), _("6 seconds"), _("7 seconds"), _("8 seconds"),
-		_("9 seconds"), _("10 seconds")])
+	config.usage.infobar_timeout = ConfigSelection(default = "5", choices = [
+		("0", _("no timeout")), ("1", _("1 second")), ("2", _("2 seconds")), ("3", _("3 seconds")),
+		("4", _("4 seconds")), ("5", _("5 seconds")), ("6", _("6 seconds")), ("7", _("7 seconds")),
+		("8", _("8 seconds")), ("9", _("9 seconds")), ("10", _("10 seconds"))])
 	config.usage.show_infobar_on_zap = ConfigYesNo(default = True)
 	config.usage.show_infobar_on_skip = ConfigYesNo(default = True)
