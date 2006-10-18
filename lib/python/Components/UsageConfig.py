8dcb4dd235735c42695805d6a5621bf6a7719323
Paul In&In <darezik@gmail.com>
2006-10-18 23:39:11 +0000

    make harddisk standby configurable

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1,4 +1,5 @@
 from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection
+import os
 
 def InitUsageConfig():
 	config.usage = ConfigSubsection();
@@ -7,8 +8,19 @@ def InitUsageConfig():
 	config.usage.quickzap_bouquet_change = ConfigYesNo(default = False)
 	config.usage.e1like_radio_mode = ConfigYesNo(default = False)
 	config.usage.infobar_timeout = ConfigSelection(default = "5", choices = [
-		("0", _("no timeout")), ("1", _("1 second")), ("2", _("2 seconds")), ("3", _("3 seconds")),
-		("4", _("4 seconds")), ("5", _("5 seconds")), ("6", _("6 seconds")), ("7", _("7 seconds")),
-		("8", _("8 seconds")), ("9", _("9 seconds")), ("10", _("10 seconds"))])
+		("0", _("no timeout")), ("1", "1 " + _("second")), ("2", "2 " + _("seconds")), ("3", "3 " + _("seconds")),
+		("4", "4 " + _("seconds")), ("5", "5 " + _("seconds")), ("6", "6 " + _("seconds")), ("7", "7 " + _("seconds")),
+		("8", "8 " + _("seconds")), ("9", "9 " + _("seconds")), ("10", "10 " + _("seconds"))])
 	config.usage.show_infobar_on_zap = ConfigYesNo(default = True)
 	config.usage.show_infobar_on_skip = ConfigYesNo(default = True)
+	config.usage.hdd_standby = ConfigSelection(default = "120", choices = [
+		("0", _("no standby")), ("2", "10 " + _("seconds")), ("6", "30 " + _("seconds")),
+		("12", "1 " + _("minute")), ("24", "2 " + _("minutes")),
+		("60", "5 " + _("minutes")), ("120", "10 " + _("minutes")), ("240", "20 " + _("minutes")),
+		("241", "30 " + _("minutes")), ("242", "1 " + _("hour")), ("244", "2 " + _("hours")),
+		("248", "4 " + _("hours")) ])
+	
+	def setHDDStandby(configElement):
+		os.system("hdparm -S" + configElement.value + " /dev/ide/host0/bus0/target0/lun0/disc")
+
+	config.usage.hdd_standby.addNotifier(setHDDStandby);
