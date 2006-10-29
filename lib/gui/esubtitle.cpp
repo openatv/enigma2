1ac36b6e99c0b4196f4e9358c2810b9aba6ced33
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2006-10-29 01:06:00 +0000

    fix displaying teletext subtitle characters > 0x80

--- lib/gui/esubtitle.cpp
+++ lib/gui/esubtitle.cpp
@@ -1,5 +1,6 @@
 #include <lib/gui/esubtitle.h>
 #include <lib/gdi/grc.h>
+#include <lib/base/estring.h>
 
 	/*
 		ok, here's much room for improvements.
@@ -18,12 +19,35 @@ eSubtitleWidget::eSubtitleWidget(eWidget *parent)
 	CONNECT(m_hide_subtitles_timer.timeout, eSubtitleWidget::clearPage);
 }
 
+#define startX 50
 void eSubtitleWidget::setPage(const eDVBTeletextSubtitlePage &p)
 {
 	m_page = p;
 	m_page_ok = 1;
-	m_hide_subtitles_timer.start(5000, true);
-	invalidate();  // FIXME
+	invalidate(m_visible_region);  // invalidate old visible regions
+	m_visible_region.rects.clear();
+
+	int elements = m_page.m_elements.size();
+	if (elements)
+	{
+		int startY = elements > 1
+			? size().height() / 2
+			: size().height() / 3 * 2;
+		int width = size().width() - startX * 2;
+		int height = size().height() - startY;
+		int size_per_element = height / (elements ? elements : 1);
+		for (int i=0; i<elements; ++i)
+		{
+			eRect &area = m_page.m_elements[i].m_area;
+			area.setLeft(startX);
+			area.setTop(size_per_element * i + startY);
+			area.setWidth(width);
+			area.setHeight(size_per_element);
+			m_visible_region.rects.push_back(area);
+		}
+	}
+	m_hide_subtitles_timer.start(7500, true);
+	invalidate(m_visible_region);  // invalidate new regions
 }
 
 void eSubtitleWidget::setPage(const eDVBSubtitlePage &p)
@@ -34,7 +58,7 @@ void eSubtitleWidget::setPage(const eDVBSubtitlePage &p)
 	for (std::list<eDVBSubtitleRegion>::iterator it(m_dvb_page.m_regions.begin()); it != m_dvb_page.m_regions.end(); ++it)
 		m_visible_region.rects.push_back(eRect(it->m_position, it->m_pixmap->size()));
 	m_dvb_page_ok = 1;
-	m_hide_subtitles_timer.start(5000, true);
+	m_hide_subtitles_timer.start(7500, true);
 	invalidate(m_visible_region);  // invalidate new regions
 }
 
@@ -59,22 +83,23 @@ int eSubtitleWidget::event(int event, void *data, void *data2)
 		getStyle(style);
 		
 		eWidget::event(event, data, data2);
-		ePtr<gFont> font = new gFont("Regular", 30);
+		ePtr<gFont> font = new gFont("Regular", 38);
 		painter.setFont(font);
 		
-/*		if (!m_page_ok && !m_dvb_page_ok)
-			painter.renderText(eRect(ePoint(0, 0), size()), "waiting for subtitles...", gPainter::RT_WRAP);
-		else */if (m_page_ok)
+		if (m_page_ok)
 		{
 			int elements = m_page.m_elements.size();
-			int height = size().height();
-			int size_per_element = height / (elements ? elements : 1);
 			for (int i=0; i<elements; ++i)
 			{
+				eDVBTeletextSubtitlePageElement &element = m_page.m_elements[i];
+				eRect &area = element.m_area;
+				eRect shadow = area;
+				shadow.moveBy(3,3);
+				std::string text = convertLatin1UTF8(element.m_text);
 				painter.setForegroundColor(gRGB(0,0,0));
-				painter.renderText(eRect(2, size_per_element * i + 2, size().width(), size_per_element), m_page.m_elements[i].m_text, gPainter::RT_WRAP|gPainter::RT_VALIGN_CENTER|gPainter::RT_HALIGN_CENTER);
-				painter.setForegroundColor(m_page.m_elements[i].m_color);
-				painter.renderText(eRect(0, size_per_element * i, size().width(), size_per_element), m_page.m_elements[i].m_text, gPainter::RT_WRAP|gPainter::RT_VALIGN_CENTER|gPainter::RT_HALIGN_CENTER);
+				painter.renderText(shadow, text, gPainter::RT_WRAP|gPainter::RT_VALIGN_CENTER|gPainter::RT_HALIGN_CENTER);
+				painter.setForegroundColor(element.m_color);
+				painter.renderText(area, text, gPainter::RT_WRAP|gPainter::RT_VALIGN_CENTER|gPainter::RT_HALIGN_CENTER);
 			}
 		}
 		else if (m_dvb_page_ok)
