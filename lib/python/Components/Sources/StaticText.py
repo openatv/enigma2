3e35c703607b5f135828fa5efe2beee669c7a4c2
Felix Domke <tmbinc@elitedvb.net>
2007-01-09 02:08:11 +0000

    add StaticText source, which can be used as a simple way to forward values between web pages

---
+++ lib/python/Components/Sources/StaticText.py
@@ -0,0 +1,16 @@
+from Source import Source
+from Components.Element import cached
+
+class StaticText(Source):
+	# filter is a function which filters external, untrusted strings
+	# this must be done to avoid XSS attacks!
+	
+	# (and is probably not done yet. For this reason, be careful when
+	# using this on HTML pages. *DO* provide your filter function.)
+	def __init__(self, text = "", filter = lambda x: x):
+		Source.__init__(self)
+		self.text = text
+		self.filter = filter
+
+	def handleCommand(self, cmd):
+		self.text = self.filter(cmd)
