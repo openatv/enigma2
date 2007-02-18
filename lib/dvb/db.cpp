1691bb139e9379f40099aa7b7945f495dda341e9
Paul In&In <darezik@gmail.com>
2007-02-18 09:59:53 +0000

    remove unneeded code

--- lib/dvb/db.cpp
+++ lib/dvb/db.cpp
@@ -668,11 +668,8 @@ void eDVBDB::loadBouquet(const char *path)
 				e->name = line+offs;
 				read_descr=false;
 			}
-			else if (!strncmp(line, "#NAME ", 6) || !strncmp(line, "#NAME: ", 7))
-			{
-				int offs = line[5] == ':' ? 7 : 6;
-				bouquet.m_bouquet_name=line+offs;
-			}
+			else if (!strncmp(line, "#NAME ", 6))
+				bouquet.m_bouquet_name=line+6;
 			continue;
 		}
 	}
