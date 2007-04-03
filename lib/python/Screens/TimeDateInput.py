68f99807c390af77ffae9a2581b9ccea7939037c
Paul In&In <darezik@gmail.com>
2007-04-03 18:03:10 +0000

    better fix for previous commit store last entered begin time in multi epg

--- lib/python/Screens/TimeDateInput.py
+++ lib/python/Screens/TimeDateInput.py
@@ -9,15 +9,14 @@ import time
 import datetime
 
 class TimeDateInput(Screen, ConfigListScreen):
-	def __init__(self, session):
+	def __init__(self, session, config_time=None, config_date=None):
 		Screen.__init__(self, session)
-
 		self["oktext"] = Label(_("OK"))
 		self["canceltext"] = Label(_("Cancel"))
 		self["ok"] = Pixmap()
 		self["cancel"] = Pixmap()
 
-		self.createConfig()
+		self.createConfig(config_date, config_time)
 
 		self["actions"] = NumberActionMap(["SetupActions"],
 		{
@@ -30,10 +29,18 @@ class TimeDateInput(Screen, ConfigListScreen):
 		ConfigListScreen.__init__(self, self.list)
 		self.createSetup(self["config"])
 
-	def createConfig(self):
-		nowtime = time.time()
-		self.timeinput_date = ConfigDateTime(default = nowtime, formatstring = _("%d.%B %Y"), increment = 86400)
-		self.timeinput_time = ConfigClock(default = nowtime)
+	def createConfig(self, conf_date, conf_time):
+		self.save_mask = 0
+		if conf_time:
+			self.save_mask |= 1
+		else:
+			conf_time = ConfigClock(default = time.time()),
+		if conf_date:
+			self.save_mask |= 2
+		else:
+			conf_date = ConfigDateTime(default = time.time(), formatstring = _("%d.%B %Y"), increment = 86400)
+		self.timeinput_date = conf_date
+		self.timeinput_time = conf_time
 
 	def createSetup(self, configlist):
 		self.list = []
@@ -52,7 +59,15 @@ class TimeDateInput(Screen, ConfigListScreen):
 
 	def keyGo(self):
 		time = self.getTimestamp(self.timeinput_date.value, self.timeinput_time.value)
+		if self.save_mask & 1:
+			self.timeinput_time.save()
+		if self.save_mask & 2:
+			self.timeinput_date.save()
 		self.close((True, time))
 
 	def keyCancel(self):
+		if self.save_mask & 1:
+			self.timeinput_time.cancel()
+		if self.save_mask & 2:
+			self.timeinput_date.cancel()
 		self.close((False,))
