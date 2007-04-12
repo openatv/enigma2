c1234f5fe6458979d4a7b846383dcadf390d84ea
Felix Domke <tmbinc@elitedvb.net>
2007-04-12 19:53:28 +0000

    use menu source/converter

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -1,11 +1,24 @@
 from Screen import Screen
 
-from Components.MenuList import MenuList
 from Components.ActionMap import ActionMap
 from Components.Language import language
 from Components.LanguageList import *
 from Components.config import config
+from Components.Sources.List import List
 
+from Tools.Directories import *
+
+from enigma import eListboxPythonMultiContent, loadPNG, eListbox, gFont, RT_HALIGN_LEFT
+
+def LanguageEntryComponent(file, name, index):
+	res = [ index ]
+	res.append((eListboxPythonMultiContent.TYPE_TEXT, 80, 10, 200, 50, 0, RT_HALIGN_LEFT ,name))
+	png = loadPNG(resolveFilename(SCOPE_SKIN_IMAGE, "countries/" + file + ".png"))
+	if png == None:
+		png = loadPNG(resolveFilename(SCOPE_SKIN_IMAGE, "countries/missing.png"))
+	res.append((eListboxPythonMultiContent.TYPE_PIXMAP, 10, 5, 60, 40, png))
+	
+	return res
 
 class LanguageSelection(Screen):
 	def __init__(self, session):
@@ -14,18 +27,16 @@ class LanguageSelection(Screen):
 		self.oldActiveLanguage = language.getActiveLanguage()
 
 		self.list = []
-		self["list"] = LanguageList(self.list)
+		self["languages"] = List(self.list, item_height=50, fonts = [gFont("Regular", 20)])
+		self["languages"].onSelectionChanged.append(self.changed)
+
 		self.updateList()
 		self.onLayoutFinish.append(self.selectActiveLanguage)
 
-		self["actions"] = ActionMap(["OkCancelActions", "DirectionActions"], 
+		self["actions"] = ActionMap(["OkCancelActions"], 
 		{
 			"ok": self.save,
 			"cancel": self.cancel,
-			"up": self.up,
-			"down": self.down,
-			"left": self.left,
-			"right": self.right
 		}, -1)
 
 	def selectActiveLanguage(self):
@@ -33,7 +44,7 @@ class LanguageSelection(Screen):
 		pos = 0
 		for x in self.list:
 			if x[0] == activeLanguage:
-				self["list"].instance.moveSelectionTo(pos)
+				self["languages"].index = pos
 				break
 			pos += 1
 
@@ -46,14 +57,20 @@ class LanguageSelection(Screen):
 		self.close()
 
 	def run(self):
-		language.activateLanguage(self["list"].l.getCurrentSelection()[0])
-		config.osd.language.value = self["list"].l.getCurrentSelection()[0]
+		print "updating language..."
+		lang = self["languages"].getCurrent()[0]
+		language.activateLanguage(lang)
+		config.osd.language.value = lang
 		config.osd.language.save()
 		config.misc.languageselected.value = 0
 		config.misc.languageselected.save()
 		self.setTitle(_("Language selection"))
+		print "ok"
 
 	def updateList(self):
+		print "update list"
+		first_time = len(self.list) == 0
+
 		self.list = []
 		if len(language.getLanguageList()) == 0: # no language available => display only english
 			self.list.append(LanguageEntryComponent("en", _("English"), "en_EN"))
@@ -61,24 +78,14 @@ class LanguageSelection(Screen):
 			for x in language.getLanguageList():
 				self.list.append(LanguageEntryComponent(file = x[1][3].lower(), name = _(x[1][0]), index = x[0]))
 		#self.list.sort(key=lambda x: x[1][7])
-		self["list"].l.setList(self.list)
+		
+		print "updateList"
+		if first_time:
+			self["languages"].list = self.list
+		else:
+			self["languages"].updateList(self.list)
+		print "done"
 
-	def up(self):
-		self["list"].instance.moveSelection(self["list"].instance.moveUp)
-		self.run()
-		self.updateList()
-
-	def down(self):
-		self["list"].instance.moveSelection(self["list"].instance.moveDown)
-		self.run()
-		self.updateList()
-
-	def left(self):
-		self["list"].instance.moveSelection(self["list"].instance.pageUp)
-		self.run()
-		self.updateList()
-
-	def right(self):
-		self["list"].instance.moveSelection(self["list"].instance.pageDown)
+	def changed(self):
 		self.run()
 		self.updateList()
