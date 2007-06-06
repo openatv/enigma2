70608ea260f899f0c072b294f7f231ae072f7d6c
Felix Domke <tmbinc@elitedvb.net>
2007-06-06 14:16:16 +0000

    add ServiceReference to ServiceName converter, event id to EventName (patch by reichi)

--- lib/python/Components/Converter/EventName.py
+++ lib/python/Components/Converter/EventName.py
@@ -5,13 +5,16 @@ class EventName(Converter, object):
 	NAME = 0
 	SHORT_DESCRIPTION = 1
 	EXTENDED_DESCRIPTION = 2
-
+	ID = 3
+	
 	def __init__(self, type):
 		Converter.__init__(self, type)
 		if type == "Description":
 			self.type = self.SHORT_DESCRIPTION
 		elif type == "ExtendedDescription":
 			self.type = self.EXTENDED_DESCRIPTION
+		elif type == "ID":
+			self.type = self.ID
 		else:
 			self.type = self.NAME
 
@@ -27,5 +30,7 @@ class EventName(Converter, object):
 			return event.getShortDescription()
 		elif self.type == self.EXTENDED_DESCRIPTION:
 			return event.getExtendedDescription()
-			
+		elif self.type == self.ID:
+			return str(event.getEventId())
+		
 	text = property(getText)
