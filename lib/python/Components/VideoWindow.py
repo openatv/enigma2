21e2d23ad571ed98335c594ce0c4a210681d903e
Paul In&In <darezik@gmail.com>
2007-06-19 23:38:42 +0000

    make VideoWindow usable for main decoder too

--- lib/python/Components/VideoWindow.py
+++ lib/python/Components/VideoWindow.py
@@ -2,7 +2,11 @@ from GUIComponent import GUIComponent
 from enigma import eVideoWidget
 
 class VideoWindow(GUIComponent):
-	def __init__(self):
+	def __init__(self, decoder = 1):
 		GUIComponent.__init__(self)
+		self.decoder = decoder
 
 	GUI_WIDGET = eVideoWidget
+
+	def postWidgetCreate(self, instance):
+		instance.setDecoder(self.decoder)
