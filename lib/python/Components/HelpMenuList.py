6081de115fc664514cd6cb834548a3fc3e02af6f
Paul In&In <darezik@gmail.com>
2007-07-11 13:31:10 +0000

    whitespace fixup

--- lib/python/Components/HelpMenuList.py
+++ lib/python/Components/HelpMenuList.py
@@ -13,16 +13,16 @@ class HelpMenuList(GUIComponent):
 		self.onSelChanged = [ ]
 		self.l = eListboxPythonMultiContent()
 		self.callback = callback
-		
+
 		l = [ ]
 		for (actionmap, context, actions) in list:
 			for (action, help) in actions:
 				entry = [ ]
-				
+
 				buttons = queryKeyBinding(context, action)
 
 				name = None
-				
+
 				for n in buttons:
 					name = getKeyDescription(n)
 					if name is not None:
@@ -30,14 +30,14 @@ class HelpMenuList(GUIComponent):
 
 				entry.append( (actionmap, context, action, name ) )
 				entry.append( (eListboxPythonMultiContent.TYPE_TEXT, 0, 0, 400, 28, 0, 0, help) )
-				
+
 				l.append(entry)
-		
+
 		self.l.setList(l)
-		
+
 		self.l.setFont(0, gFont("Regular", 26))
 		self.l.setItemHeight(42)
-	
+
 	def ok(self):
 		# a list entry has a "private" tuple as first entry...
 		l = self.getCurrent()
@@ -46,7 +46,7 @@ class HelpMenuList(GUIComponent):
 		# ...containing (Actionmap, Context, Action, keydata).
 		# we returns this tuple to the callback.
 		self.callback(l[0], l[1], l[2])
-	
+
 	def getCurrent(self):
 		sel = self.l.getCurrentSelection()
 		return sel and sel[0]
