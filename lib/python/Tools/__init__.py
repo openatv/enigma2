982da3d2688264865730416dcd0892e524811adb
Paul In&In <darezik@gmail.com>
2007-07-22 09:56:06 +0000

    cleanup __init__.py files ....

--- lib/python/Tools/__init__.py
+++ lib/python/Tools/__init__.py
@@ -1,5 +1,2 @@
-all = ["RedirectTime.py", "FuzzyDate.py", "XMLTools.py", "Directories.py", "KeyBindings.py", "BoundFunction.py", "ISO639.py", "Notifications",
-	"RedirectOutput.py", "DreamboxHardware.py"]
-
 import RedirectTime
 import RedirectOutput
