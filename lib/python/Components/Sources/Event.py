da1a7e22f07e692576886da5cd2505d4e207a509
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2007-07-28 09:18:50 +0000

    fixes

--- lib/python/Components/Sources/Event.py
+++ lib/python/Components/Sources/Event.py
@@ -1,25 +1,25 @@
 from Components.VariableText import VariableText
 from Components.GUIComponent import GUIComponent
-from enigma import eEPGCache, eServiceReference as Ref, eLabel
+from enigma import eLabel
 from Source import Source
 
 class Event(VariableText, GUIComponent, Source, object):
-	def __init__(self, timer=None):
+	def __init__(self):
 		Source.__init__(self)
 		GUIComponent.__init__(self)
 		VariableText.__init__(self)
-		self.event = None
+		self.evt = None
 
 	GUI_WIDGET = eLabel
 
 	def getCurrentEvent(self):
-		return self.event
+		return self.evt
 
 	event = property(getCurrentEvent)
 
 	def newEvent(self, event):
-		if not self.event or self.event != event:
-			self.event = event
+		if not self.evt or self.evt != event:
+			self.evt = event
 			if not event:
 				self.changed((self.CHANGED_CLEAR,))
 			else:
