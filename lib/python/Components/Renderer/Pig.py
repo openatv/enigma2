79cab0403882bf14a34828b75f50a34d445851a6
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2007-09-21 20:15:18 +0000

    add a "Picture in Graphics" source.. its usable to show a small embedded tv picture in every screen.. simply by add a line like "<widget source="fake" render="Pig" backgroundColor="transparent" size="160,120" position="50,50" />

---
+++ lib/python/Components/Renderer/Pig.py
@@ -0,0 +1,37 @@
+##
+## P(icture)i(n)g(raphics) renderer
+##
+from Renderer import Renderer
+from enigma import eVideoWidget, eSize, ePoint
+
+class Pig(Renderer):
+	def __init__(self):
+		Renderer.__init__(self)
+		self.Position = self.Size = None
+
+	GUI_WIDGET = eVideoWidget
+
+	def postWidgetCreate(self, instance):
+		instance.setDecoder(0)
+
+	def applySkin(self, desktop):
+		ret = Renderer.applySkin(self, desktop)
+		if ret:
+			self.Position = self.instance.position()
+			self.Size = self.instance.size()
+		return ret
+
+	def preWidgetRemove(self, instance):
+		instance.resize(eSize(720,576))
+		instance.move(ePoint(0,0))
+
+	def onShow(self):
+		if self.instance:
+			if self.Size:
+				self.instance.resize(self.Size)
+			if self.Position:
+				self.instance.move(self.Position)
+
+	def onHide(self):
+		if self.instance:
+			self.preWidgetRemove(self.instance)
