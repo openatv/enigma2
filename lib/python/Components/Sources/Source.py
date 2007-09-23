f5202ca9fa45a85f14d6fdf95f48caf78871dd8a
Paul In&In <darezik@gmail.com>
2007-09-23 23:26:41 +0000

    add ObsoleteSource for compatibility redirects

--- lib/python/Components/Sources/Source.py
+++ lib/python/Components/Sources/Source.py
@@ -15,3 +15,10 @@ class Source(Element):
 	
 	def destroy(self):
 		self.__dict__.clear()
+
+class ObsoleteSource(Source):
+	def __init__(self, new_source, description = None, removal_date = "as soon as possible"):
+		self.new_source = new_source
+		self.description = description
+		self.removal_date = removal_date
+
