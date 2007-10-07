20706bda37359411c6e53600763bdcbf8bc0ea53
Felix Domke <tmbinc@elitedvb.net>
2007-10-07 09:22:47 +0000

    don't crash if boolean source returns None

--- lib/python/Components/Converter/ConditionalShowHide.py
+++ lib/python/Components/Converter/ConditionalShowHide.py
@@ -5,10 +5,18 @@ class ConditionalShowHide(Converter, object):
 		Converter.__init__(self, type)
 		self.invert = type == "Invert"
 
+	def calcVisibility(self):
+		b = self.source.boolean
+		if b is None:
+			return True
+		b ^= self.invert
+		return b
+
 	def changed(self, what):
+		vis = self.calcVisibility()
 		for x in self.downstream_elements:
-			x.visible = self.source.boolean ^ self.invert
+			x.visible = vis
 
 	def connectDownstream(self, downstream):
 		Converter.connectDownstream(self, downstream)
-		downstream.visible = self.source.boolean ^ self.invert
+		downstream.visible = self.calcVisibility()
