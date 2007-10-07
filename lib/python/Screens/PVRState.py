af399f0121b6b53944f9a09a83e6ced078dc0ef3
Felix Domke <tmbinc@elitedvb.net>
2007-10-07 09:10:23 +0000

    use session.CurrentService in PVRState

--- lib/python/Screens/PVRState.py
+++ lib/python/Screens/PVRState.py
@@ -1,16 +1,12 @@
 from Screen import Screen
 
 from Components.Label import Label
-from Components.Sources.CurrentService import CurrentService
 
 class PVRState(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
-		
 		self["state"] = Label(text="")
 
 class TimeshiftState(PVRState):
 	def __init__(self, session):
 		PVRState.__init__(self, session)
-		
-		self["CurrentService"] = CurrentService(self.session.nav)
