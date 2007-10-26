d7ade1df1e1d8e0259378d783a38ae49e3271fd6
Felix Domke <tmbinc@elitedvb.net>
2007-10-26 17:00:29 +0000

    convert About screen to source/renderer system. Change names a bit, add enigma version to about screen

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -1,6 +1,7 @@
 from Screen import Screen
 from Components.ActionMap import ActionMap
-from Components.Label import Label
+from Components.Sources.StaticText import StaticText
+from Components.Sources.Source import ObsoleteSource
 from Components.Harddisk import Harddisk
 from Components.NimManager import nimmanager
 from Components.About import about
@@ -10,32 +11,33 @@ from Tools.DreamboxHardware import getFPVersion
 class About(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
-		
-		self["text"] = Label("Enigma v" + about.getVersionString())
 
-		self["tuner"] = Label(_("Detected NIMs:"))
+		self["EnigmaVersion"] = StaticText("Enigma: " + about.getEnigmaVersionString())
+		self["ImageVersion"] = StaticText("Image: " + about.getImageVersionString())
+
+		self["TunerHeader"] = StaticText(_("Detected NIMs:"))
 
 		fp_version = getFPVersion()
 		if fp_version is None:
 			fp_version = ""
 		else:
 			fp_version = _("Frontprocessor version: %d") % fp_version
-		
-		self["fpVersion"] = Label(fp_version)
-		
+
+		self["FPVersion"] = StaticText(fp_version)
+
 		nims = nimmanager.nimList()
 		for count in range(4):
 			if count < len(nims):
-				self["tuner" + str(count)] = Label(nims[count])
+				self["Tuner" + str(count)] = StaticText(nims[count])
 			else:
-				self["tuner" + str(count)] = Label("")
+				self["Tuner" + str(count)] = StaticText("")
 
-		self["hdd"] = Label(_("Detected HDD:"))
+		self["HDDHeader"] = StaticText(_("Detected HDD:"))
 		hdd = Harddisk(0)
 		if hdd.model() != "":
-			self["hddA"] = Label(_("%s\n(%s, %d MB free)") % (hdd.model(), hdd.capacity(),hdd.free()))
+			self["hddA"] = StaticText(_("%s\n(%s, %d MB free)") % (hdd.model(), hdd.capacity(),hdd.free()))
 		else:			
-			self["hddA"] = Label(_("none"))
+			self["hddA"] = StaticText(_("none"))
 
 		self["actions"] = ActionMap(["SetupActions"], 
 			{
