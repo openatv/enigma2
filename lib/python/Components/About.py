e59b2eb9c6e05f39a6a1723528e8570082dfdf13
Felix Domke <tmbinc@elitedvb.net>
2007-10-26 16:59:34 +0000

    add getEnigmaVersionString to Components.About

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,10 +1,14 @@
 from Tools.Directories import resolveFilename, SCOPE_SYSETC
+from enigma import getEnigmaVersionString
 
 class About:
 	def __init__(self):
 		pass
-	
+
 	def getVersionString(self):
+		return self.getImageVersionString()
+
+	def getImageVersionString(self):
 		try:
 			file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
 			lines = file.readlines()
@@ -18,12 +22,15 @@ class About:
 					year = version[4:8]
 					month = version[8:10]
 					day = version[10:12]
-					
+
 					return '-'.join(["dev", year, month, day])
 			file.close()
 		except IOError:
 			pass
 
 		return "unavailable"
-	
+
+	def getEnigmaVersionString(self):
+		return getEnigmaVersionString()
+
 about = About()
