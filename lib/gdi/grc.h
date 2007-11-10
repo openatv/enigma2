65fc65b5c86c16419eed6c2b0ca9263f0d6d9d67
Paul In&In <darezik@gmail.com>
2007-11-10 13:54:48 +0000

    just export gFBDC.getInstance and setResolution to python

--- lib/gdi/grc.h
+++ lib/gdi/grc.h
@@ -23,7 +23,6 @@
 #include <lib/gdi/region.h>
 #include <lib/gdi/gfont.h>
 
-#ifndef SWIG
 class eTextPara;
 
 class gDC;
@@ -256,12 +255,10 @@ public:
 	void flip();
 	void notify();
 };
-#endif
 
 class gDC: public iObject
 {
 DECLARE_REF(gDC);
-#ifndef SWIG
 protected:
 	ePtr<gPixmap> m_pixmap;
 
@@ -279,9 +276,6 @@ protected:
 	int m_spinner_num, m_spinner_i;
 public:
 	virtual void exec(gOpcode *opcode);
-#else
-public:
-#endif
 	gDC(gPixmap *pixmap);
 	gDC();
 	virtual ~gDC();
