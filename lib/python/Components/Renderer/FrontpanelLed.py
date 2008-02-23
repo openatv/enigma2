4bfde32534913bee3a87462751cf550c9a909999
Paul In&In <darezik@gmail.com>
2008-02-23 10:08:15 +0000

    blink frontpanel led when record is running

---
+++ lib/python/Components/Renderer/FrontpanelLed.py
@@ -0,0 +1,22 @@
+from Components.Element import Element
+
+# this is not a GUI renderer.
+class FrontpanelLed(Element):
+	def __init__(self):
+		Element.__init__(self)
+
+	def changed(self, *args, **kwargs):
+		print ">> Frontpanel LED changed"
+		print ">> FP led:", self.source.value
+		if self.source.value:
+			pattern = 0x55555555
+			speed = 20
+		else:
+			pattern = 0
+			speed = 1
+
+		try:
+			open("/proc/stb/fp/led0_pattern", "w").write("%08x" % pattern)
+			open("/proc/stb/fp/led_pattern_speed", "w").write("%d" % speed)
+		except IOError:
+			pass
