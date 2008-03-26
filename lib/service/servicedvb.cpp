ce1e31199159636b5c62fbaf63b3ebc655d7917b
Paul In&In <darezik@gmail.com>
2008-03-26 00:35:34 +0000

    fix: comparison is always true due to limited range of data type

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -809,7 +809,7 @@ PyObject *eDVBServiceList::getContent(const char* format, bool sorted)
 							sptr->getName(ref, name);
 
 							// filter short name brakets
-							unsigned int pos;
+							size_t pos;
 							while((pos = name.find("\xc2\x86")) != std::string::npos)
 								name.erase(pos,2);
 							while((pos = name.find("\xc2\x87")) != std::string::npos)
