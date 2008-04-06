133fb520c2ed186863c88fa58f45e197e2bcd6d6
Paul In&In <darezik@gmail.com>
2008-04-06 21:57:53 +0000

    add translation choser (on pressing the red button) to the StartWizard

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,4 +1,5 @@
-from Wizard import Wizard, wizardManager
+from Wizard import wizardManager
+from Screens.WizardLanguage import WizardLanguage
 
 from Components.Pixmap import Pixmap, MovingPixmap
 from Components.config import config, ConfigBoolean, configfile
@@ -9,14 +10,16 @@ from LanguageSelection import LanguageSelection
 config.misc.firstrun = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
 
-class StartWizard(Wizard):
+class StartWizard(WizardLanguage):
 	skin = """
 		<screen position="0,0" size="720,576" title="Welcome..." flags="wfNoBorder" >
 			<widget name="text" position="153,50" size="340,270" font="Regular;23" />
 			<widget source="list" render="Listbox" position="50,300" size="440,200" scrollbarMode="showOnDemand" >
 				<convert type="StringList" />
 			</widget>
-			<widget name="config" position="50,300" zPosition="1" size="440,200" transparent="1" scrollbarMode="showOnDemand" />			
+			<widget name="config" position="50,300" zPosition="1" size="440,200" transparent="1" scrollbarMode="showOnDemand" />
+			<ePixmap pixmap="skin_default/button_red.png" position="40,225" zPosition="0" size="15,16" transparent="1" alphatest="on" />
+			<widget name="languagetext" position="55,225" size="95,30" font="Regular;18" />			
 			<widget name="stepslider" position="50,500" zPosition="1" borderWidth="2" size="440,20" backgroundColor="dark" />
 			<widget name="wizard" pixmap="wizard.png" position="40,50" zPosition="10" size="110,174" transparent="1" alphatest="on"/>
 			<widget name="rc" pixmap="rc.png" position="500,600" zPosition="10" size="154,475" transparent="1" alphatest="on"/>
@@ -29,7 +32,7 @@ class StartWizard(Wizard):
 		self.skin = StartWizard.skin
 		self.xmlfile = "startwizard.xml"
 		
-		Wizard.__init__(self, session, showSteps = False)
+		WizardLanguage.__init__(self, session, showSteps = False)
 		self["wizard"] = Pixmap()
 		self["rc"] = MovingPixmap()
 		self["arrowdown"] = MovingPixmap()
