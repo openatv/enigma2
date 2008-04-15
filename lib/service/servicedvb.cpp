0cf336f653b6dfbd9b1de93cb63e560b26f8d402
Paul In&In <darezik@gmail.com>
2008-04-15 12:38:43 +0000

    add seekChapter method to iSeekableService interface

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1446,6 +1446,12 @@ RESULT eDVBServicePlay::isCurrentlySeekable()
 	return m_is_pvr || m_timeshift_active;
 }
 
+RESULT eDVBServicePlay::seekChapter(int chapter)
+{
+		/* seekChapter N/I. */
+	return -1;
+}
+
 RESULT eDVBServicePlay::frontendInfo(ePtr<iFrontendInformation> &ptr)
 {
 	ptr = this;
