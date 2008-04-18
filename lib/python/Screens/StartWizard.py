1f7d8546313bc9e1c86fc08b95b8c52fb731f21b
Paul In&In <darezik@gmail.com>
2008-04-18 19:06:21 +0000

    no need to define skin in skin_default.xml and .py files

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -11,27 +11,8 @@ config.misc.firstrun = ConfigBoolean(default = True)
 config.misc.languageselected = ConfigBoolean(default = True)
 
 class StartWizard(WizardLanguage):
-	skin = """
-		<screen position="0,0" size="720,576" title="Welcome..." flags="wfNoBorder" >
-			<widget name="text" position="153,50" size="340,270" font="Regular;23" />
-			<widget source="list" render="Listbox" position="50,300" size="440,200" scrollbarMode="showOnDemand" >
-				<convert type="StringList" />
-			</widget>
-			<widget name="config" position="50,300" zPosition="1" size="440,200" transparent="1" scrollbarMode="showOnDemand" />
-			<ePixmap pixmap="skin_default/buttons/button_red.png" position="40,225" zPosition="0" size="15,16" transparent="1" alphatest="on" />
-			<widget name="languagetext" position="55,225" size="95,30" font="Regular;18" />			
-			<widget name="stepslider" position="50,500" zPosition="1" borderWidth="2" size="440,20" backgroundColor="dark" />
-			<widget name="wizard" pixmap="skin_default/wizard.png" position="40,50" zPosition="10" size="110,174" transparent="1" alphatest="on"/>
-			<widget name="rc" pixmap="skin_default/rc.png" position="500,600" zPosition="10" size="154,475" transparent="1" alphatest="on"/>
-			<widget name="arrowdown" pixmap="skin_default/arrowdown.png" position="0,0" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
-			<widget name="arrowup" pixmap="skin_default/arrowup.png" position="-100,-100" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
-			<widget name="arrowup2" pixmap="skin_default/arrowup.png" position="-100,-100" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
-		</screen>"""
-	
 	def __init__(self, session):
-		self.skin = StartWizard.skin
 		self.xmlfile = "startwizard.xml"
-		
 		WizardLanguage.__init__(self, session, showSteps = False)
 		self["wizard"] = Pixmap()
 		self["rc"] = MovingPixmap()
--- lib/python/Screens/TutorialWizard.py
+++ lib/python/Screens/TutorialWizard.py
@@ -6,20 +6,8 @@ from Components.Pixmap import MovingPixmap
 config.misc.firstruntutorial = ConfigBoolean(default = True)
 
 class TutorialWizard(Wizard):
-	skin = """
-		<screen position="0,0" size="720,576" title="Welcome..." flags="wfNoBorder">
-			<widget name="text" position="50,100" size="440,200" font="Regular;23" />
-			<widget name="list" position="50,300" zPosition="1" size="440,200" />
-			<widget name="rc" pixmap="skin_default/rc.png" position="500,600" zPosition="10" size="154,475" transparent="1" alphatest="on"/>
-			<widget name="arrowdown" pixmap="skin_default/arrowdown.png" position="0,0" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
-			<widget name="arrowup" pixmap="skin_default/arrowup.png" position="-100,-100" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
-			<widget name="arrowup2" pixmap="skin_default/arrowup.png" position="-100,-100" zPosition="11" size="37,70" transparent="1" alphatest="on"/>
-		</screen>"""
-	
 	def __init__(self, session):
-		self.skin = TutorialWizard.skin
 		self.xmlfile = "tutorialwizard.xml"
-		
 		Wizard.__init__(self, session, showSteps=False, showStepSlider=False, showList=True, showConfig=False)
 		self["rc"] = MovingPixmap()
 		self["arrowdown"] = MovingPixmap()
