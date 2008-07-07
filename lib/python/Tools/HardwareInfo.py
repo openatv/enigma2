fefaf0d0f2b71ee47ffe0c227e1778f7d99e9430
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2008-07-07 10:37:26 +0000

    make model detection optional

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -1,10 +1,17 @@
 class HardwareInfo:
 	def __init__(self):
 		self.device = "unknown"
-		file = open("/proc/stb/info/model", "r")
-		self.device = file.readline().strip()
+		try:
+			file = open("/proc/stb/info/model", "r")
+			self.device = file.readline().strip()
+			file.close()
+		except:
+			print "----------------"
+			print "you should upgrade to new drivers for the hardware detection to work properly"
+			print "----------------"
 		
 	def get_device_name(self):
 		return self.device
 	
 	device_name = property(get_device_name)
+
