f864b8d52978a36cc0b56e7873ccbbd95f2a4a08
Andreas Monzner <andreas.monzner@multimedia-labs.de>
2008-08-21 16:22:05 +0000

    add box detection as fallback when /proc/stb/info is not avail (old drivers are used)

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -9,7 +9,21 @@ class HardwareInfo:
 			print "----------------"
 			print "you should upgrade to new drivers for the hardware detection to work properly"
 			print "----------------"
-		
+			print "fallback to detect hardware via /proc/cpuinfo!!"
+			try:
+				rd = open("/proc/cpuinfo", "r").read()
+				if rd.find("Brcm4380 V4.2") != 0:
+					self.device = "dm8000"
+					print "dm8000 detected!"
+				elif rd.find("Brcm7401 V0.0") != 0:
+					self.device = "dm800"
+					print "dm800 detected!"
+				elif rd.find("MIPS 4KEc V4.8") != 0:
+					self.device = "dm7025"
+					print "dm7025 detected!"
+			except:
+				pass
+
 	def get_device_name(self):
 		return self.device
 	
