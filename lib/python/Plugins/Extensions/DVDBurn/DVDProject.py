8702b819c8b1606983118a47c5fe42989c28f0b5
Paul In&In <darezik@gmail.com>
2008-09-03 12:24:05 +0000

    fix cuesheets, add multiple menu pages support, add preview support, add project saveing ...

--- lib/python/Plugins/Extensions/DVDBurn/DVDProject.py
+++ lib/python/Plugins/Extensions/DVDBurn/DVDProject.py
@@ -1,20 +1,68 @@
+from Tools.Directories import resolveFilename, fileExists, SCOPE_FONTS, SCOPE_PLUGINS, SCOPE_SKIN
 class DVDProject:
 	def __init__(self):
 		self.titles = [ ]
 		self.target = None
-		self.name = _("New DVD")
+		self.name = _("Dreambox DVD record")
+		self.vmgm = resolveFilename(SCOPE_PLUGINS,"Extensions/DVDBurn/dreamvmgm.mpg")
+		self.menuaudio = resolveFilename(SCOPE_PLUGINS,"Extensions/DVDBurn/silence.mp2")
+		self.menubg = resolveFilename(SCOPE_SKIN, "dreamdvd_02.jpg")
+		# tuples with R, G, B values
+		self.color_button	= ( 0x08, 0x00, 0x00 )
+		self.color_highlight	= ( 0x00, 0xC0, 0xC0 )
+		self.color_headline	= ( 0x00, 0x00, 0x80 )
+		self.font_face = resolveFilename(SCOPE_FONTS, "nmsbd.ttf")
+		# tuple with three pixel values ( headline, title, subtitle )
+		self.font_size = ( 48, 28, 16 )
+		# please supply even numbers for all dimensions
+		self.space_left = 30
+		self.space_top = 120
+		self.space_rows = 36
 
 	def addService(self, service):
 		import DVDTitle
-		t = DVDTitle.DVDTitle()
-		t.source = service
+		title = DVDTitle.DVDTitle()
+		title.addService(service)
+		self.titles.append(title)
+		return title
+	
+	def saveProject(self, path):
+		import xml.dom.minidom
+		from Tools.XMLTools import elementsWithTag, mergeText, stringToXML
+		list = []
+		list.append('<?xml version="1.0" encoding="utf-8" ?>\n')
+		list.append('<DreamDVDBurnerProject>\n')
+		list.append('\t<config')
+		list.append(' name="' + self.name + '"')
+		list.append(' vmgm="' + self.vmgm + '"')
+		list.append(' />\n')
+		list.append('\t<menu')
+		list.append(' bg="' + self.menubg + '"')
+		list.append(' audio="' + self.menuaudio + '"')
+		list.append(' color_button="' + str(self.color_button) + '"')
+		list.append(' color_highlight="' + str(self.color_highlight) + '"')
+		list.append(' color_headline="' + str(self.color_headline) + '"')
+		list.append(' font_face="' + self.font_face + '"')
+		list.append(' font_size="' + str(self.font_size) + '"')
+		list.append(' space_left="' + str(self.space_left) + '"')
+		list.append(' space_top="' + str(self.space_top) + '"')
+		list.append(' space_rows="' + str(self.space_rows) + '"')
+		list.append(' />\n')
+		list.append('\t<titles>\n')
+		for title in self.titles:
+			list.append('\t\t<path>')
+			list.append(stringToXML(title.source.getPath()))
+			list.append('</path>\n')
+		list.append('\t</titles>\n')
+		list.append('</DreamDVDBurnerProject>\n')
 
-		from enigma import eServiceCenter, iServiceInformation
-		serviceHandler = eServiceCenter.getInstance()
-
-		info = serviceHandler.info(service)
-		descr = info and " " + info.getInfoString(service, iServiceInformation.sDescription) or ""
-		t.name = info and info.getName(service) or "Title" + descr
-
-		self.titles.append(t)
-		return t
+		i = 0
+		filename = path + "/" + self.name + ".ddvdp.xml"
+		while fileExists(filename):
+			i = i+1
+			filename = path + "/" + self.name + str(i).zfill(3) + ".ddvdp.xml"
+				
+		file = open(filename, "w")
+		for x in list:
+			file.write(x)
+		file.close()
--- lib/python/Plugins/Extensions/DVDBurn/DVDTitle.py
+++ lib/python/Plugins/Extensions/DVDBurn/DVDTitle.py
@@ -1,9 +1,78 @@
 
 class DVDTitle:
 	def __init__(self):
-		self.cutlist = [ ]
+		self.cuesheet = [ ]
 		self.source = None
 		self.name = ""
+		self.descr = ""
+		self.filesize = 0
+		self.estimatedDiskspace = 0
+		self.inputfile = ""
+		self.cutlist = [ ]
+		self.chaptermarks = [ ]
 
-	def estimateDiskspace(self):
-		return 0
+	def addService(self, service):
+		from os import path
+		from enigma import eServiceCenter, iServiceInformation
+		self.source = service
+		serviceHandler = eServiceCenter.getInstance()
+		info = serviceHandler.info(service)
+		self.descr = info and " " + info.getInfoString(service, iServiceInformation.sDescription) or ""
+		self.name = info and info.getName(service) or "Title" + t.descr
+		self.inputfile = service.getPath()
+		self.filesize = path.getsize(self.inputfile)
+		self.estimatedDiskspace = self.filesize
+
+	def produceFinalCuesheet(self):
+		print "[produceFinalCuesheet] >>> ", self.inputfile, self.cuesheet
+
+		CUT_TYPE_IN = 0
+		CUT_TYPE_OUT = 1
+		CUT_TYPE_MARK = 2
+		CUT_TYPE_LAST = 3
+
+		accumulated_in = 0
+		accumulated_at = 0
+		last_in = 0
+
+		self.cutlist = [ ]
+		self.chaptermarks = [ ]
+
+		# our demuxer expects *strictly* IN,OUT lists.
+		currently_in = not any(type == CUT_TYPE_IN for pts, type in self.cuesheet)
+		if currently_in:
+			self.cutlist.append(0) # emulate "in" at first		
+
+		for (pts, type) in self.cuesheet:
+			#print "pts=", pts, "type=", type, "accumulated_in=", accumulated_in, "accumulated_at=", accumulated_at, "last_in=", last_in
+			if type == CUT_TYPE_IN and not currently_in:
+				self.cutlist.append(pts)
+				last_in = pts
+				currently_in = True
+
+			if type == CUT_TYPE_OUT and currently_in:
+				self.cutlist.append(pts)
+
+				# accumulate the segment
+				accumulated_in += pts - last_in 
+				accumulated_at = pts
+				currently_in = False
+
+			if type == CUT_TYPE_MARK and currently_in:
+				# relocate chaptermark against "in" time. This is not 100% accurate,
+				# as the in/out points are not.
+				reloc_pts = pts - last_in + accumulated_in
+				self.chaptermarks.append(reloc_pts)
+				
+		print "cutlist =", self.cutlist, "chaptermarks =", self.chaptermarks, "accumulated_in =", accumulated_in
+
+		if len(self.cutlist) > 1:
+			from enigma import eServiceCenter, iServiceInformation
+			serviceHandler = eServiceCenter.getInstance()
+			service = self.source
+			info = serviceHandler.info(service)
+			lenpts = info.getLength(service) * 90000
+			part = accumulated_in / float(lenpts)
+			usedsize = int ( part * self.filesize )
+			print "lenpts=", lenpts, "part=", part, "filesize=", self.filesize, "estimatedDiskspace=", usedsize
+			self.estimatedDiskspace = usedsize
