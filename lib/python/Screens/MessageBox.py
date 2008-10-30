a3aa2b3184b6175bd1d5d1d91f45d555f40483d3
Paul In&In <darezik@gmail.com>
2008-10-30 18:54:24 +0100

    add possibility to disable user input in Messageboxes

--- lib/python/Screens/MessageBox.py
+++ lib/python/Screens/MessageBox.py
@@ -12,7 +12,7 @@ class MessageBox(Screen):
 	TYPE_WARNING = 2
 	TYPE_ERROR = 3
 
-	def __init__(self, session, text, type = TYPE_YESNO, timeout = -1, close_on_any_key = False, default = True):
+	def __init__(self, session, text, type = TYPE_YESNO, timeout = -1, close_on_any_key = False, default = True, enable_input = True):
 		self.type = type
 		Screen.__init__(self, session)
 
@@ -47,20 +47,21 @@ class MessageBox(Screen):
 			self["selectedChoice"].setText(self.list[0][0])
 		self["list"] = MenuList(self.list)
 
-		self["actions"] = ActionMap(["MsgBoxActions", "DirectionActions"], 
-			{
-				"cancel": self.cancel,
-				"ok": self.ok,
-				"alwaysOK": self.alwaysOK,
-				"up": self.up,
-				"down": self.down,
-				"left": self.left,
-				"right": self.right,
-				"upRepeated": self.up,
-				"downRepeated": self.down,
-				"leftRepeated": self.left,
-				"rightRepeated": self.right
-			}, -1)
+		if enable_input:
+			self["actions"] = ActionMap(["MsgBoxActions", "DirectionActions"], 
+				{
+					"cancel": self.cancel,
+					"ok": self.ok,
+					"alwaysOK": self.alwaysOK,
+					"up": self.up,
+					"down": self.down,
+					"left": self.left,
+					"right": self.right,
+					"upRepeated": self.up,
+					"downRepeated": self.down,
+					"leftRepeated": self.left,
+					"rightRepeated": self.right
+				}, -1)
 
 	def initTimeout(self, timeout):
 		self.timeout = timeout
