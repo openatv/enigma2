79023728001c40267a239257e0a268a39434caf0
Paul In&In <darezik@gmail.com>
2008-11-18 00:39:59 +0100

    experimental dm8000 recording fix, doesn't belong here

--- lib/python/Components/Renderer/FrontpanelLed.py
+++ lib/python/Components/Renderer/FrontpanelLed.py
@@ -6,15 +6,24 @@ class FrontpanelLed(Element):
 		Element.__init__(self)
 
 	def changed(self, *args, **kwargs):
-		if self.source.value:
+		if self.source.value or 1:
 			pattern = 0x55555555
+			pattern_4bit = 0x84fc8c04
 			speed = 20
 		else:
 			pattern = 0
+			pattern_4bit = 0xffffffff
 			speed = 1
 
 		try:
 			open("/proc/stb/fp/led0_pattern", "w").write("%08x" % pattern)
+		except IOError:
+			pass
+		try:
+			open("/proc/stb/fp/led_pattern", "w").write("%08x" % pattern_4bit)
+		except IOError:
+			pass
+		try:
 			open("/proc/stb/fp/led_pattern_speed", "w").write("%d" % speed)
 		except IOError:
 			pass
