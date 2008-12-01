34c1ccf6eaef6f2e8535cb3c0cbe6390835086ef
ghost <andreas.monzner@multimedia-labs.de>
2008-12-01 20:30:34 +0100

    graphmultiepg: also show plugin in extensions menu

--- lib/python/Plugins/Extensions/GraphMultiEPG/plugin.py
+++ lib/python/Plugins/Extensions/GraphMultiEPG/plugin.py
@@ -93,4 +93,5 @@ def main(session, servicelist, **kwargs):
 def Plugins(**kwargs):
 	name = _("Graphical Multi EPG")
 	descr = _("A graphical EPG for all services of an specific bouquet")
- 	return [ PluginDescriptor(name=name, description=descr, where = PluginDescriptor.WHERE_EVENTINFO, fnc=main) ]
+ 	return [ PluginDescriptor(name=name, description=descr, where = PluginDescriptor.WHERE_EVENTINFO, fnc=main),
+	  PluginDescriptor(name=name, description=descr, where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=main) ]
