37b2b2a211731bdcaf935d297a942d6e02229818
Paul In&In <darezik@gmail.com>
2009-02-10 22:29:43 +0100

    Add MultoContentTemplateColor, which takes the color out of a templated argument

--- lib/python/Components/MultiContent.py
+++ lib/python/Components/MultiContent.py
@@ -1,5 +1,7 @@
 from enigma import eListboxPythonMultiContent, RT_HALIGN_LEFT, RT_VALIGN_TOP
 
+def MultiContentTemplateColor(n): return 0xff000000 | n
+
 def MultiContentEntryText(pos = (0, 0), size = (0, 0), font = 0, flags = RT_HALIGN_LEFT | RT_VALIGN_TOP, text = "", color = None, color_sel = None, backcolor = None, backcolor_sel = None, border_width = None, border_color = None):
 	return (eListboxPythonMultiContent.TYPE_TEXT, pos[0], pos[1], size[0], size[1], font, flags, text, color, color_sel, backcolor, backcolor_sel, border_width, border_color)
 
