28a0e997d1d28a6d057ede5e8de70f26ceed0e8e
Felix Domke <tmbinc@elitedvb.net>
2009-05-25 01:37:44 +0200

    Fix frontpanel support for DM8000, modified behaviour on hardware with single frontpanel led (disabled in standby)

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -1,10 +1,10 @@
 from enigma import eDVBResourceManager
+from Tools.Directories import fileExists
 
 SystemInfo = { }
 
 #FIXMEE...
 def getNumVideoDecoders():
-	from Tools.Directories import fileExists
 	idx = 0
 	while fileExists("/dev/dvb/adapter0/video%d"%(idx), 'f'):
 		idx += 1
@@ -12,3 +12,18 @@ def getNumVideoDecoders():
 
 SystemInfo["NumVideoDecoders"] = getNumVideoDecoders()
 SystemInfo["CanMeasureFrontendInputPower"] = eDVBResourceManager.getInstance().canMeasureFrontendInputPower()
+
+
+def countFrontpanelLEDs():
+	leds = 0
+	if fileExists("/proc/stb/fp/led_set_pattern"):
+		leds += 1
+
+	while fileExists("/proc/stb/fp/led%d_pattern" % leds):
+		leds += 1
+
+	return leds
+
+SystemInfo["NumFrontpanelLEDs"] = countFrontpanelLEDs()
+SystemInfo["FrontpanelDisplay"] = fileExists("/dev/dbox/oled0") or fileExists("/dev/dbox/lcd0")
+SystemInfo["FrontpanelDisplayGrayscale"] = fileExists("/dev/dbox/oled0")
