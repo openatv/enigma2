7f3c248018d22453f9992e78e0f6064cd9315c47
Paul In&In <darezik@gmail.com>
2009-06-11 12:46:49 +0200

    add a format converter, i.e. to convert from lamedb to sat.xml files the main destination format of this converter is a sat.xml file easily extendable with further datasources

---
+++ tools/host_tools/FormatConverter/main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+from datasource import genericdatasource
+from satxml import satxml
+from lamedb import lamedb
+from input import *
+import sys
+import os
+
+maindata = genericdatasource()
+
+sources = [satxml, lamedb]
+
+datasources = [maindata]
+
+for source in sources:
+	datasources.append(source())
+	
+for source in datasources:
+	source.setDatasources(datasources)
+
+while(True):
+	os.system("/usr/bin/clear")
+	list = []
+	for index in range(len(datasources)):
+		list.append(datasources[index].getName() + (" (%d sats)" % len(datasources[index].transponderlist.keys())))
+	index = inputChoices(list, "q", "quit")
+	if index is None:
+		break		
+	
+	while(True):
+		print datasources[index].getStatus()
+		list = []
+		for action in datasources[index].getCapabilities():
+			list.append(action[0])
+		action = inputChoices(list)
+		if action is None:
+			break
+		
+		datasources[index].getCapabilities()[action][1]()
+		#except:
+		#	print sys.exc_info()
+		#	print "sorry, could not execute that command"
+		
