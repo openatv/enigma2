9e0b367cd7017f8abf3e3ea804966ce6e23a768f
ghost <andreas.monzner@multimedia-labs.de>
2009-07-31 23:42:32 +0200

    fix convertDVBUTF8 default table (0 is now iso6397 but we want iso8859-1)

--- lib/base/estring.h
+++ lib/base/estring.h
@@ -12,7 +12,7 @@ int strnicmp(const char*, const char*, int);
 
 std::string getNum(int num, int base=10);
 
-std::string convertDVBUTF8(const unsigned char *data, int len, int table=1, int tsidonid=0); // with default ISO8859-1 / Latin1
+std::string convertDVBUTF8(const unsigned char *data, int len, int table=1, int tsidonid=1); // with default ISO8859-1 / Latin1
 std::string convertLatin1UTF8(const std::string &string);
 int isUTF8(const std::string &string);
 
@@ -20,7 +20,7 @@ std::string removeDVBChars(const std::string &s);
 void makeUpper(std::string &s);
 std::string replace_all(const std::string &in, const std::string &entity, const std::string &symbol);
 
-inline std::string convertDVBUTF8(const std::string &string, int table=1, int tsidonid=0) // with default ISO8859-1 / Latin1
+inline std::string convertDVBUTF8(const std::string &string, int table=1, int tsidonid=1) // with default ISO8859-1 / Latin1
 {
 	return convertDVBUTF8((const unsigned char*)string.c_str(), string.length(), table, tsidonid);
 }
