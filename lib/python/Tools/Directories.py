1dc03ac8f376954d7734214089e56b22f22c7a91
acid-burn <acidburn@opendreambox.org>
2009-09-08 14:26:40 +0200

    Tools/Directories.py: SCOPE_METADIR is now /usr/share/meta

--- lib/python/Tools/Directories.py
+++ lib/python/Tools/Directories.py
@@ -57,7 +57,7 @@ defaultPaths = {
 		SCOPE_DEFAULTDIR: ("/usr/share/enigma2/defaults/", PATH_CREATE),
 		SCOPE_DEFAULTPARTITION: ("/dev/mtdblock/6", PATH_DONTCREATE),
 		SCOPE_DEFAULTPARTITIONMOUNTDIR: ("/usr/share/enigma2/dealer", PATH_CREATE),
-		SCOPE_METADIR: ("/usr/lib/enigma2/python/Plugins/meta", PATH_CREATE),
+		SCOPE_METADIR: ("/usr/share/meta", PATH_CREATE),
 	}
 
 FILE_COPY = 0 # copy files from fallback dir to the basedir
