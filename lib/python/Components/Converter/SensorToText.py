710b3ea19993c9fc15e38a57101494eb962c606c
thedoc <thedoc@atom.(none)>
2009-12-10 14:52:36 +0100

    support up to 8 fan/temp sensors

--- lib/python/Components/Converter/SensorToText.py
+++ lib/python/Components/Converter/SensorToText.py
@@ -5,6 +5,8 @@ class SensorToText(Converter, object):
 		Converter.__init__(self, arguments)
         
 	def getText(self):
+		if self.source.getValue() is None:
+			return ""
 		return "%d %s" % (self.source.getValue(), self.source.getUnit())
 	
 	text = property(getText)
