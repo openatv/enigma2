2701861d66fd17ed825de7bbfdea8e000fdb30b4
thedoc <thedoc@atom.(none)>
2009-12-23 11:42:22 +0100

    fixes bug #322 change default visibility of blinking dish icon to true

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -7,7 +7,7 @@ import os
 
 def InitUsageConfig():
 	config.usage = ConfigSubsection();
-	config.usage.showdish = ConfigYesNo(default = False)
+	config.usage.showdish = ConfigYesNo(default = True)
 	config.usage.multibouquet = ConfigYesNo(default = False)
 	config.usage.quickzap_bouquet_change = ConfigYesNo(default = False)
 	config.usage.e1like_radio_mode = ConfigYesNo(default = False)
