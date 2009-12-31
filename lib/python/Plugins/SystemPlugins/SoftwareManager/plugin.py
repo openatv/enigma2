aea461bb9dc35ab20a78bd141c785bcb5967e616
acid-burn <acid-burn@opendreambox.org>
2009-12-31 08:29:47 +0100

    SoftwareManager/plugin.py: - small cleanup and fix. This is for #112

--- lib/python/Plugins/SystemPlugins/SoftwareManager/plugin.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/plugin.py
@@ -567,9 +567,7 @@ class PluginManager(Screen, DreamInfoHandler):
 					for foundtag in prerequisites["tag"]:
 						if categorytag == foundtag:
 							attributes = package[0]["attributes"]
-							print "ATTRIBUTES",attributes
 							if attributes.has_key("packagetype"):
-								print "PACKAGETYPE",attributes["packagetype"]
 								if attributes["packagetype"] == "internal":
 									continue
 								self.packetlist.append([attributes["name"], attributes["details"], attributes["shortdescription"], attributes["packagename"]])
@@ -696,11 +694,12 @@ class PluginManager(Screen, DreamInfoHandler):
 		self.session.openWithCallback(self.ExecuteReboot, MessageBox, _("Install or remove finished.") +" "+_("Do you want to reboot your Dreambox?"), MessageBox.TYPE_YESNO)
 
 	def ExecuteReboot(self, result):
+		print "RESULT im ExecuteReboot",result
 		if result is None:
 			return
 		if result is False:
 			self.reloadPluginlist()
-			self.selectedFiles = None
+			self.selectedFiles = []
 			self.detailsClosed(True)
 		if result:
 			quitMainloop(3)
