e5ea5996f6f641d5f66f23f9cfba273722740582
thedoc <thedoc@atom.(none)>
2010-01-01 14:00:47 +0100

    fixes bug #362 allow assigning standby function to long power button press

--- mytest.py
+++ mytest.py
@@ -383,6 +383,8 @@ class PowerKey:
 						menu_screen = self.session.openWithCallback(self.MenuClosed, MainMenu, x)
 						menu_screen.setTitle(_("Standby / Restart"))
 						return
+		elif action == "standby":
+			self.standby()
 
 	def powerdown(self):
 		self.standbyblocked = 0
