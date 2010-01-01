a12200f49b725b5c6b662c9eb32107542c531f75
Paul In&In <darezik@gmail.com>
2010-01-01 20:22:14 +0100

    Revert "Derive movie list size from the position of the description border, not the size"
    This reverts commit 22964ab30124b361cc86a950d3f4941c5fc6a386.
    With HD skins, this results in an empty movielist!

--- lib/python/Screens/MovieSelection.py
+++ lib/python/Screens/MovieSelection.py
@@ -200,8 +200,7 @@ class MovieSelection(Screen, HelpableScreen, SelectionEventInfo):
 	def updateDescription(self):
 		if config.movielist.description.value == MovieList.SHOW_DESCRIPTION:
 			self["DescriptionBorder"].show()
-			height = self["DescriptionBorder"].instance.position().y() - self["list"].instance.position().y()
-			self["list"].instance.resize(eSize(self.listWidth, height))
+			self["list"].instance.resize(eSize(self.listWidth, self.listHeight-self["DescriptionBorder"].instance.size().height()))
 		else:
 			self["Service"].newService(None)
 			self["DescriptionBorder"].hide()
