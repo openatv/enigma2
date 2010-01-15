cf5061f4f8fb4314419b9ccda4aa763ad1fb993d
pieterg <pieterg@users.sourceforge.net>
2010-01-15 16:23:29 +0100

    painter: implemented RT_VALIGN_BOTTOM
    Though it has been available for some time, this flag was not yet implemented

--- lib/gdi/grc.cpp
+++ lib/gdi/grc.cpp
@@ -664,7 +664,12 @@ void gDC::exec(gOpcode *o)
 			int correction = vcentered_top - bbox.top();
 			offset += ePoint(0, correction);
 		}
-		
+		else if (o->parm.renderText->flags & gPainter::RT_VALIGN_BOTTOM)
+		{
+			eRect bbox = para->getBoundBox();
+			int correction = o->parm.renderText->area.height() - bbox.height();
+			offset += ePoint(0, correction);
+		}
 		para->blit(*this, offset, m_background_color_rgb, m_foreground_color_rgb);
 		delete o->parm.renderText;
 		break;
