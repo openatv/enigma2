openatv/enigma2

commit d20257c0c70023334f2f9292b57a21e7a7876466

Background file eraser: Allow erasing files with long filenames
Note: Message class seems unable to handle string members, causing segfaults. So
that's why the char* stuff is still in there.

lib/components/file_eraser.cpp
@@ -47,17 +47,21 @@ void eBackgroundFileEraser::thread()
 	stop_thread_timer->stop();
 }
 
-void eBackgroundFileEraser::erase(const char *filename)
+void eBackgroundFileEraser::erase(const std::string& filename)
 {
-	if (filename)
+	if (!filename.empty())
 	{
-		char buf[255];
-		snprintf(buf, 255, "%s.del", filename);
-		if (rename(filename, buf)<0)
-			;/*perror("rename file failed !!!");*/
+		std::string delname(filename);
+		delname.append(".del");
+		if (rename(filename.c_str(), delname.c_str())<0)
+		{
+			// if rename fails, try deleting in foreground.
+			eDebug("Rename %s -> %s failed.", filename.c_str(), delname.c_str());
+			::unlink(filename.c_str());
+		}
 		else
 		{
-			messages.send(Message(Message::erase, strdup(buf)));
+			messages.send(Message(Message::erase, strdup(delname.c_str())));
 			run();
 		}
 	}
@@ -68,14 +72,10 @@ void eBackgroundFileEraser::gotMessage(const Message &msg )
 	switch (msg.type)
 	{
 		case Message::erase:
-			if ( msg.filename )
-			{
-				if ( ::unlink(msg.filename) < 0 )
-					eDebug("remove file %s failed (%m)", msg.filename);
-				else
-					eDebug("file %s erased", msg.filename);
-				free((char*)msg.filename);
-			}
+			if ( ::unlink(msg.filename) < 0 )
+				eDebug("remove file %s failed (%m)", msg.filename);
+			else
+				eDebug("file %s erased", msg.filename);
 			stop_thread_timer->start(1000, true); // stop thread in one seconds
 			break;
 		case Message::quit:

lib/components/file_eraser.h
@@ -10,14 +10,14 @@ class eBackgroundFileEraser: public eMainloop, private eThread, public Object
 	struct Message
 	{
 		int type;
-		const char *filename;
+		const char* filename;
 		enum
 		{
 			erase,
 			quit
 		};
-		Message(int type=0, const char *filename=0)
-			:type(type), filename(filename)
+		Message(int type=0, const char* afilename=NULL)
+			:type(type), filename(afilename)
 		{}
 	};
 	eFixedMessagePump<Message> messages;
@@ -34,7 +34,7 @@ public:
 #ifdef SWIG
 public:
 #endif
-	void erase(const char * filename);
+	void erase(const std::string& filename);
 	static eBackgroundFileEraser *getInstance() { return instance; }
 };
 

lib/service/servicedvb.cpp
@@ -452,7 +452,7 @@ RESULT eDVBPVRServiceOfflineOperations::deleteFromDisk(int simulate)
 		{
 			eDebug("Removing %s...", i->c_str());
 			if (eraser)
-				eraser->erase(i->c_str());
+				eraser->erase(*i);
 			else
 				::unlink(i->c_str());
 		}

lib/service/servicemp3.cpp
@@ -127,11 +127,10 @@ RESULT eMP3ServiceOfflineOperations::deleteFromDisk(int simulate)
 		{
 			eDebug("Removing %s...", i->c_str());
 			if (eraser)
-				eraser->erase(i->c_str());
+				eraser->erase(*i);
 			else
 				::unlink(i->c_str());
 		}
-		
 		return 0;
 	}
 }

commit 28c5800a147ee94d8d0bc8be519c9c83bf47eb0c

Fix memleak, if rename fails, just add the original
Memleak introduced in d20257c0c70023334f2f9292b57a21e7a7876466
When the ".del" rename fails, simply add the original filename instead. Better than silently ignoring.

lib/components/file_eraser.cpp
@@ -55,15 +55,12 @@ void eBackgroundFileEraser::erase(const std::string& filename)
 		delname.append(".del");
 		if (rename(filename.c_str(), delname.c_str())<0)
 		{
-			// if rename fails, try deleting in foreground.
+			// if rename fails, try deleting the file itself without renaming.
 			eDebug("Rename %s -> %s failed.", filename.c_str(), delname.c_str());
-			::unlink(filename.c_str());
+			delname = filename;
 		}
-		else
-		{
-			messages.send(Message(Message::erase, strdup(delname.c_str())));
-			run();
-		}
+		messages.send(Message(Message::erase, strdup(delname.c_str())));
+		run();
 	}
 }
 
@@ -74,8 +71,7 @@ void eBackgroundFileEraser::gotMessage(const Message &msg )
 		case Message::erase:
 			if ( ::unlink(msg.filename) < 0 )
 				eDebug("remove file %s failed (%m)", msg.filename);
-			else
-				eDebug("file %s erased", msg.filename);
+			free(msg.filename);
 			stop_thread_timer->start(1000, true); // stop thread in one seconds
 			break;
 		case Message::quit:

lib/components/file_eraser.h
@@ -16,7 +16,10 @@ class eBackgroundFileEraser: public eMainloop, private eThread, public Object
 			erase,
 			quit
 		};
-		Message(int type=0, const char* afilename=NULL)
+		Message(int type=0)
+			:type(type), filename(NULL)
+		{}
+		Message(int type, const char* afilename)
 			:type(type), filename(afilename)
 		{}
 	};