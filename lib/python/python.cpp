f3268c1647d94e569171f37f35bc279d64317dc8
pieterg <pieterg@users.sourceforge.net>
2010-01-22 22:19:45 +0100

    ePython: make sure we quit after a PyObject_CallObject error
    Since we no longer kill ourselves after a bsod, we have to
    make sure we quit (with python errorcode 5).
    This causes another bsod, with a useless message,
    which is ignored (we show only the first occuring bsod)

--- lib/python/python.cpp
+++ lib/python/python.cpp
@@ -6,6 +6,7 @@ extern "C" void init_enigma();
 extern "C" void eBaseInit(void);
 extern "C" void eConsoleInit(void);
 extern void bsodFatal(const char *component);
+extern void quitMainloop(int exitCode);
 
 #define SKIP_PART2
 #include <lib/python/python.h>
@@ -207,7 +208,10 @@ int ePython::call(ePyObject pFunc, ePyObject pArgs)
 		 	eDebug("(PyObject_CallObject(%s,%s) failed)", PyString_AS_STRING(FuncStr), PyString_AS_STRING(ArgStr));
 			Py_DECREF(FuncStr);
 			Py_DECREF(ArgStr);
+			/* immediately show BSOD, so we have the actual error at the bottom */
 		 	bsodFatal(0);
+			/* and make sure we quit (which would also eventually cause a bsod, but with useless termination messages) */
+			quitMainloop(5);
 		}
 	}
 	return res;
