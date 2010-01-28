8d0845f1ca2c9e0e8383b8fe637ad0aac7646909
Mike Looijmans <milo-software@users.sourceforge.net>
2010-01-28 19:50:03 +0100

    Movie list: Ask less
    By default disable the questions that require more button presses
    than simply performing the action.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -43,11 +43,11 @@ def InitUsageConfig():
 
 	config.usage.movielist_trashcan = ConfigYesNo(default=True)
 	config.usage.movielist_trashcan_days = ConfigNumber(default=8)
-	config.usage.on_movie_start = ConfigSelection(default = "ask", choices = [
+	config.usage.on_movie_start = ConfigSelection(default = "resume", choices = [
 		("ask", _("Ask user")), ("resume", _("Resume from last position")), ("beginning", _("Start from the beginning")) ])
-	config.usage.on_movie_stop = ConfigSelection(default = "ask", choices = [
+	config.usage.on_movie_stop = ConfigSelection(default = "movielist", choices = [
 		("ask", _("Ask user")), ("movielist", _("Return to movie list")), ("quit", _("Return to previous service")) ])
-	config.usage.on_movie_eof = ConfigSelection(default = "ask", choices = [
+	config.usage.on_movie_eof = ConfigSelection(default = "movielist", choices = [
 		("ask", _("Ask user")), ("movielist", _("Return to movie list")), ("quit", _("Return to previous service")), ("pause", _("Pause movie at end")) ])
 
 	config.usage.setup_level = ConfigSelection(default = "expert", choices = [
