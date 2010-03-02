d0d208975bc9fb90984900d983f4a6ded4b12719
Mike Looijmans <milo-software@users.sourceforge.net>
2010-03-02 19:32:15 +0100

    Use file ctime as recording time for enigma1 recordings
    Also some refactoring of inefficient code (no need
    to call strlen four times, for example). Removed a
    255 char filename length limitation.

--- lib/dvb/metaparser.h
+++ lib/dvb/metaparser.h
@@ -12,7 +12,6 @@ public:
 	int parseMeta(const std::string &filename);
 	int parseRecordings(const std::string &filename);
 	int updateMeta(const std::string &basename);
-	long long fileSize(const std::string &basename);
 
 	eServiceReferenceDVB m_ref;
 	int m_data_ok, m_time_create, m_length;
