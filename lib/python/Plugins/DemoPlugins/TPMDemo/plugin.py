b451e9ca5fac13989e123d45b173cb053ee6180a
Stefan Pluecken <stefan.pluecken@multimedia-labs.de>
2010-04-09 20:49:15 +0200

    refs bug #467 read proc device instead of using internal methods

--- lib/python/Plugins/DemoPlugins/TPMDemo/plugin.py
+++ lib/python/Plugins/DemoPlugins/TPMDemo/plugin.py
@@ -1,6 +1,5 @@
 from Screens.Screen import Screen
 from Plugins.Plugin import PluginDescriptor
-from Tools.HardwareInfo import HardwareInfo
 from enigma import eTPM
 import sha
 
@@ -44,7 +43,10 @@ def read_random():
 		return None
 
 def main(session, **kwargs):
-	device = HardwareInfo().get_device_name()
+	try:
+		device = open("/proc/stb/info/model", "r").readline().strip()
+	except:
+		device = ""	
 	if device != "dm7025":
 		rootkey = ['\x9f', '|', '\xe4', 'G', '\xc9', '\xb4', '\xf4', '#', '&', '\xce', '\xb3', '\xfe', '\xda', '\xc9', 'U', '`', '\xd8', '\x8c', 's', 'o', '\x90', '\x9b', '\\', 'b', '\xc0', '\x89', '\xd1', '\x8c', '\x9e', 'J', 'T', '\xc5', 'X', '\xa1', '\xb8', '\x13', '5', 'E', '\x02', '\xc9', '\xb2', '\xe6', 't', '\x89', '\xde', '\xcd', '\x9d', '\x11', '\xdd', '\xc7', '\xf4', '\xe4', '\xe4', '\xbc', '\xdb', '\x9c', '\xea', '}', '\xad', '\xda', 't', 'r', '\x9b', '\xdc', '\xbc', '\x18', '3', '\xe7', '\xaf', '|', '\xae', '\x0c', '\xe3', '\xb5', '\x84', '\x8d', '\r', '\x8d', '\x9d', '2', '\xd0', '\xce', '\xd5', 'q', '\t', '\x84', 'c', '\xa8', ')', '\x99', '\xdc', '<', '"', 'x', '\xe8', '\x87', '\x8f', '\x02', ';', 'S', 'm', '\xd5', '\xf0', '\xa3', '_', '\xb7', 'T', '\t', '\xde', '\xa7', '\xf1', '\xc9', '\xae', '\x8a', '\xd7', '\xd2', '\xcf', '\xb2', '.', '\x13', '\xfb', '\xac', 'j', '\xdf', '\xb1', '\x1d', ':', '?']
 		
