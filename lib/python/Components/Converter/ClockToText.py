43089e1b9a28c05a4ecf2eea8b095d0f74fc1565
Mike Looijmans <milo-software@users.sourceforge.net>
2010-05-14 15:19:14 +0200

    ClockToText: Change Full order to day-dd/mm-hh:mm

--- lib/python/Components/Converter/ClockToText.py
+++ lib/python/Components/Converter/ClockToText.py
@@ -75,7 +75,7 @@ class ClockToText(Converter, object):
 		elif self.type == self.DATE:
 			return _(strftime("%A",t)) + " " + str(t[2]) + " " + MONTHS[t[1]-1] + " " + str(t[0])
 		elif self.type == self.FULL:
-			return "%2d:%02d" % (t.tm_hour, t.tm_min) + "  " + dayOfWeek[t[6]] + " %d/%d" % (t[2],t[1])  
+			return dayOfWeek[t[6]] + " %d/%d  %2d:%02d" % (t[2],t[1], t.tm_hour, t.tm_min)  
 		elif self.type == self.FORMAT:
 			spos = self.fmt_string.find('%')
 			if spos > 0:
