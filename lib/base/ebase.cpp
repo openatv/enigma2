362b74b468183005be2cc4d96c9bd1d130f46c83
Paul In&In <darezik@gmail.com>
2010-05-17 21:58:45 +0200

    lib/base/ebase.h/cpp: dont crash when try to start eTimers, eSocketNotifiers with no more existing Mainloops (on e2 shutdown)

--- lib/base/ebase.cpp
+++ lib/base/ebase.cpp
@@ -26,8 +26,11 @@ void eSocketNotifier::start()
 	if (state)
 		stop();
 
-	context.addSocketNotifier(this);
-	state=2;  // running but not in poll yet
+	if (eMainloop::isValid(&context))
+	{
+		context.addSocketNotifier(this);
+		state=2;  // running but not in poll yet
+	}
 }
 
 void eSocketNotifier::stop()
@@ -46,29 +49,35 @@ void eTimer::start(long msek, bool singleShot)
 	if (bActive)
 		stop();
 
-	bActive = true;
-	bSingleShot = singleShot;
-	interval = msek;
-	clock_gettime(CLOCK_MONOTONIC, &nextActivation);
-//	eDebug("this = %p\nnow sec = %d, nsec = %d\nadd %d msec", this, nextActivation.tv_sec, nextActivation.tv_nsec, msek);
-	nextActivation += (msek<0 ? 0 : msek);
-//	eDebug("next Activation sec = %d, nsec = %d", nextActivation.tv_sec, nextActivation.tv_nsec );
-	context.addTimer(this);
+	if (eMainloop::isValid(&context))
+	{
+		bActive = true;
+		bSingleShot = singleShot;
+		interval = msek;
+		clock_gettime(CLOCK_MONOTONIC, &nextActivation);
+//		eDebug("this = %p\nnow sec = %d, nsec = %d\nadd %d msec", this, nextActivation.tv_sec, nextActivation.tv_nsec, msek);
+		nextActivation += (msek<0 ? 0 : msek);
+//		eDebug("next Activation sec = %d, nsec = %d", nextActivation.tv_sec, nextActivation.tv_nsec );
+		context.addTimer(this);
+	}
 }
 
-void eTimer::startLongTimer( int seconds )
+void eTimer::startLongTimer(int seconds)
 {
 	if (bActive)
 		stop();
 
-	bActive = bSingleShot = true;
-	interval = 0;
-	clock_gettime(CLOCK_MONOTONIC, &nextActivation);
-//	eDebug("this = %p\nnow sec = %d, nsec = %d\nadd %d sec", this, nextActivation.tv_sec, nextActivation.tv_nsec, seconds);
-	if ( seconds > 0 )
-		nextActivation.tv_sec += seconds;
-//	eDebug("next Activation sec = %d, nsec = %d", nextActivation.tv_sec, nextActivation.tv_nsec );
-	context.addTimer(this);
+	if (eMainloop::isValid(&context))
+	{
+		bActive = bSingleShot = true;
+		interval = 0;
+		clock_gettime(CLOCK_MONOTONIC, &nextActivation);
+//		eDebug("this = %p\nnow sec = %d, nsec = %d\nadd %d sec", this, nextActivation.tv_sec, nextActivation.tv_nsec, seconds);
+		if ( seconds > 0 )
+			nextActivation.tv_sec += seconds;
+//		eDebug("next Activation sec = %d, nsec = %d", nextActivation.tv_sec, nextActivation.tv_nsec );
+		context.addTimer(this);
+	}
 }
 
 void eTimer::stop()
@@ -113,6 +122,11 @@ void eTimer::activate()   // Internal Funktion... called from eApplication
 
 // mainloop
 ePtrList<eMainloop> eMainloop::existing_loops;
+
+bool eMainloop::isValid(eMainloop *ml)
+{
+	return std::find(existing_loops.begin(), existing_loops.end(), ml) != existing_loops.end();
+}
 
 eMainloop::~eMainloop()
 {
