f7cfded1962e1230afd8b047a3263bc9cae1618a
Paul In&In <darezik@gmail.com>
2010-07-06 15:41:11 +0200

    Softwaremanager: * properly count processed packages. * make configfile owerwrite question while upgrading configurable over new setupscreen on Menu Button. * show files that will be backed up inside own infoscreen and not inside the menu description. * add possibility to navigate with the remote keys (a-z) inside packagemanager. thx emanuel fixes #550

--- lib/python/Plugins/SystemPlugins/SoftwareManager/SoftwareTools.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/SoftwareTools.py
@@ -1,4 +1,5 @@
-from enigma import eConsoleAppContainer
+# -*- coding: iso-8859-1 -*-
+from enigma import eConsoleAppContainer,eTPM
 from Components.Console import Console
 from Components.About import about
 from Components.DreamInfoHandler import DreamInfoHandler
@@ -8,8 +9,48 @@ from Components.Ipkg import IpkgComponent
 from Components.Network import iNetwork
 from Tools.Directories import pathExists, fileExists, resolveFilename, SCOPE_METADIR
 from Tools.HardwareInfo import HardwareInfo
+import sha
 
 from time import time
+
+def bin2long(s):
+	return reduce( lambda x,y:(x<<8L)+y, map(ord, s))
+
+def long2bin(l):
+	res = ""
+	for byte in range(128):
+		res += chr((l >> (1024 - (byte + 1) * 8)) & 0xff)
+	return res
+
+def rsa_pub1024(src, mod):
+	return long2bin(pow(bin2long(src), 65537, bin2long(mod)))
+	
+def decrypt_block(src, mod):
+	if len(src) != 128 and len(src) != 202:
+		return None
+	dest = rsa_pub1024(src[:128], mod)
+	hash = sha.new(dest[1:107])
+	if len(src) == 202:
+		hash.update(src[131:192])	
+	result = hash.digest()
+	if result == dest[107:127]:
+		return dest
+	return None
+
+def validate_cert(cert, key):
+	buf = decrypt_block(cert[8:], key) 
+	if buf is None:
+		return None
+	return buf[36:107] + cert[139:196]
+
+def read_random():
+	try:
+		fd = open("/dev/urandom", "r")
+		buf = fd.read(8)
+		fd.close()
+		return buf
+	except:
+		return None
 
 class SoftwareTools(DreamInfoHandler):
 	lastDownloadDate = None
@@ -58,24 +99,71 @@ class SoftwareTools(DreamInfoHandler):
 				self.getUpdates()
 
 	def getUpdates(self, callback = None):
-		if SoftwareTools.NetworkConnectionAvailable == True:
-			SoftwareTools.lastDownloadDate = time()
-			if SoftwareTools.list_updating is False and callback is None:
-				SoftwareTools.list_updating = True
-				self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
-			elif SoftwareTools.list_updating is False and callback is not None:
-				SoftwareTools.list_updating = True
-				self.NotifierCallback = callback
-				self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
-			elif SoftwareTools.list_updating is True and callback is not None:
-				#update info collecting already in progress
-				self.NotifierCallback = callback
+		if SoftwareTools.lastDownloadDate is None:
+			if  self.hardware_info.device_name != "dm7025":
+				rootkey = ['\x9f', '|', '\xe4', 'G', '\xc9', '\xb4', '\xf4', '#', '&', '\xce', '\xb3', '\xfe', '\xda', '\xc9', 'U', '`', '\xd8', '\x8c', 's', 'o', '\x90', '\x9b', '\\', 'b', '\xc0', '\x89', '\xd1', '\x8c', '\x9e', 'J', 'T', '\xc5', 'X', '\xa1', '\xb8', '\x13', '5', 'E', '\x02', '\xc9', '\xb2', '\xe6', 't', '\x89', '\xde', '\xcd', '\x9d', '\x11', '\xdd', '\xc7', '\xf4', '\xe4', '\xe4', '\xbc', '\xdb', '\x9c', '\xea', '}', '\xad', '\xda', 't', 'r', '\x9b', '\xdc', '\xbc', '\x18', '3', '\xe7', '\xaf', '|', '\xae', '\x0c', '\xe3', '\xb5', '\x84', '\x8d', '\r', '\x8d', '\x9d', '2', '\xd0', '\xce', '\xd5', 'q', '\t', '\x84', 'c', '\xa8', ')', '\x99', '\xdc', '<', '"', 'x', '\xe8', '\x87', '\x8f', '\x02', ';', 'S', 'm', '\xd5', '\xf0', '\xa3', '_', '\xb7', 'T', '\t', '\xde', '\xa7', '\xf1', '\xc9', '\xae', '\x8a', '\xd7', '\xd2', '\xcf', '\xb2', '.', '\x13', '\xfb', '\xac', 'j', '\xdf', '\xb1', '\x1d', ':', '?']
+				etpm = eTPM()
+				l2cert = etpm.getCert(eTPM.TPMD_DT_LEVEL2_CERT)
+				if l2cert is None:
+					return
+				l2key = validate_cert(l2cert, rootkey)
+				if l2key is None:
+					return
+				l3cert = etpm.getCert(eTPM.TPMD_DT_LEVEL3_CERT)
+				if l3cert is None:
+					print "please run the genuine dreambox plugin"
+					return
+				l3key = validate_cert(l3cert, l2key)
+				if l3key is None:
+					return
+				rnd = read_random()
+				if rnd is None:
+					return
+				val = etpm.challenge(rnd)
+				result = decrypt_block(val, l3key)
+			if self.hardware_info.device_name == "dm7025" or result[80:88] == rnd:
+				if SoftwareTools.NetworkConnectionAvailable == True:
+					SoftwareTools.lastDownloadDate = time()
+					if SoftwareTools.list_updating is False and callback is None:
+						SoftwareTools.list_updating = True
+						self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
+					elif SoftwareTools.list_updating is False and callback is not None:
+						SoftwareTools.list_updating = True
+						self.NotifierCallback = callback
+						self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
+					elif SoftwareTools.list_updating is True and callback is not None:
+						self.NotifierCallback = callback
+				else:
+					SoftwareTools.list_updating = False
+					if callback is not None:
+						callback(False)
+					elif self.NotifierCallback is not None:
+						self.NotifierCallback(False)
+			else:
+				SoftwareTools.NetworkConnectionAvailable = False
+				SoftwareTools.list_updating = False
+				if callback is not None:
+					callback(False)
+				elif self.NotifierCallback is not None:
+					self.NotifierCallback(False)		
 		else:
-			SoftwareTools.list_updating = False
-			if callback is not None:
-				callback(False)
-			elif self.NotifierCallback is not None:
-				self.NotifierCallback(False)
+			if SoftwareTools.NetworkConnectionAvailable == True:
+				SoftwareTools.lastDownloadDate = time()
+				if SoftwareTools.list_updating is False and callback is None:
+					SoftwareTools.list_updating = True
+					self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
+				elif SoftwareTools.list_updating is False and callback is not None:
+					SoftwareTools.list_updating = True
+					self.NotifierCallback = callback
+					self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
+				elif SoftwareTools.list_updating is True and callback is not None:
+					self.NotifierCallback = callback
+			else:
+				SoftwareTools.list_updating = False
+				if callback is not None:
+					callback(False)
+				elif self.NotifierCallback is not None:
+					self.NotifierCallback(False)
 
 	def ipkgCallback(self, event, param):
 		if event == IpkgComponent.EVENT_ERROR:
