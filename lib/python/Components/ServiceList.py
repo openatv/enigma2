bde26474d216c3937ae926f2f8dfdcadf55bb38c
Mike Looijmans <milo-software@users.sourceforge.net>
2010-10-16 15:59:22 +0200

    Fix servicelist merge
    ServiceList.py was broken since merge
    Few minor changes in cpp

--- lib/python/Components/ServiceList.py
+++ lib/python/Components/ServiceList.py
@@ -249,6 +249,7 @@ class ServiceList(HTMLComponent, GUIComponent):
 				self.l.setElementPosition(self.l.celServiceEventProgressbar, eRect(60, 0, 52, self.ItemHeight))
 			else:
 				self.l.setElementPosition(self.l.celServiceEventProgressbar, eRect(60, 0, 0, 0))
+			self.l.setElementFont(self.l.celServiceName, self.ServiceNameFont)
 			self.l.setElementFont(self.l.celServiceNumber, self.ServiceNumberFont)
 			self.l.setElementPosition(self.l.celServiceNumber, eRect(0, 0, 50, self.ItemHeight))
 			self.l.setElementPosition(self.l.celServiceName, eRect(60, 0, self.instance.size().width()-60, self.ItemHeight))
