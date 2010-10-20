7948d65556bce7927d79798fa9288947624eba5d
Paul In&In <darezik@gmail.com>
2010-10-20 18:09:10 +0200

    Revert "refs bug #587"
    This reverts commit d21824019fe93603f1c05c7263ef7570aa60e135.
    
    Conflicts:
    
    	lib/python/Components/NimManager.py

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -474,7 +474,7 @@ class SecConfigure:
 		self.update()
 
 class NIM(object):
-	def __init__(self, slot, type, description, has_outputs = True, internally_connectable = None, multi_type = {}, frontend_id = None, i2c = None, is_empty = False):
+	def __init__(self, slot, type, description, has_outputs = True, internally_connectable = None, multi_type = {}, frontend_id = None):
 		self.slot = slot
 
 		if type not in ("DVB-S", "DVB-C", "DVB-T", "DVB-S2", None):
@@ -488,7 +488,6 @@ class NIM(object):
 		self.multi_type = multi_type
 		self.i2c = i2c
 		self.frontend_id = frontend_id
-		self.__is_empty = is_empty
 
 	def isCompatible(self, what):
 		if not self.isSupported():
@@ -547,12 +546,8 @@ class NIM(object):
 	def isMultiType(self):
 		return (len(self.multi_type) > 0)
 	
-	def isEmpty(self):
-		return self.__is_empty
-	
-	# empty tuners are supported!
 	def isSupported(self):
-		return (self.frontend_id is not None) or self.__is_empty
+		return (self.frontend_id is not None)
 	
 	# returns dict {<slotid>: <type>}
 	def getMultiTypeList(self):
@@ -698,10 +693,8 @@ class NimManager:
 				entries[current_slot] = {}
 			elif line.startswith("Type:"):
 				entries[current_slot]["type"] = str(line[6:])
-				entries[current_slot]["isempty"] = False
 			elif line.startswith("Name:"):
 				entries[current_slot]["name"] = str(line[6:])
-				entries[current_slot]["isempty"] = False
 			elif line.startswith("Has_Outputs:"):
 				input = str(line[len("Has_Outputs:") + 1:])
 				entries[current_slot]["has_outputs"] = (input == "yes")
@@ -725,7 +718,6 @@ class NimManager:
 			elif line.startswith("empty"):
 				entries[current_slot]["type"] = None
 				entries[current_slot]["name"] = _("N/A")
-				entries[current_slot]["isempty"] = True
 		nimfile.close()
 		
 		from os import path
@@ -747,7 +739,7 @@ class NimManager:
 				entry["frontend_device"] = entry["internally_connectable"] = None
 			if not (entry.has_key("multi_type")):
 				entry["multi_type"] = {}
-			self.nim_slots.append(NIM(slot = id, description = entry["name"], type = entry["type"], has_outputs = entry["has_outputs"], internally_connectable = entry["internally_connectable"], multi_type = entry["multi_type"], frontend_id = entry["frontend_device"], i2c = entry["i2c"], is_empty = entry["isempty"]))
+			self.nim_slots.append(NIM(slot = id, description = entry["name"], type = entry["type"], has_outputs = entry["has_outputs"], internally_connectable = entry["internally_connectable"], multi_type = entry["multi_type"], frontend_id = entry["frontend_device"]))
 
 	def hasNimType(self, chktype):
 		for slot in self.nim_slots:
