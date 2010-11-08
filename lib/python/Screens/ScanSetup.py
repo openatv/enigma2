57f0f06b1e3235dfb6739a43bb95ab92c02f9574
ghost <andreas.monzner@multimedia-labs.de>
2010-11-08 19:14:46 +0100

    ScanSetup.py: remove dvb_shutdown_timeout workaround

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -115,7 +115,6 @@ class CableTransponderSearchSupport:
 
 	def cableTransponderSearchSessionClosed(self, *val):
 		print "cableTransponderSearchSessionClosed, val", val
-		self.resetTimeout()
 		self.cable_search_container.appClosed.remove(self.cableTransponderSearchClosed)
 		self.cable_search_container.dataAvail.remove(self.getCableTransponderData)
 		if val and len(val):
@@ -167,19 +166,6 @@ class CableTransponderSearchSupport:
 			tmpstr += " kHz "
 			tmpstr += data[0]
 			self.cable_search_session["text"].setText(tmpstr)
-
-	def setTimeout(self):
-		try:
-			self.oldtimeoutvalue = open("/sys/module/dvb_core/parameters/dvb_shutdown_timeout", "r").readline()
-			open("/sys/module/dvb_core/parameters/dvb_shutdown_timeout", "w").write("0")
-		except:
-			print "[info] no /sys/module/dvb_core/parameters/dvb_shutdown_timeout available"		
-		
-	def resetTimeout(self):
-		try:
-			open("/sys/module/dvb_core/parameters/dvb_shutdown_timeout", "w").write(self.oldtimeoutvalue)
-		except:
-			print "[info] no /sys/module/dvb_core/parameters/dvb_shutdown_timeout available"
 
 	def startCableTransponderSearch(self, nim_idx):
 		if not self.tryGetRawFrontend(nim_idx):
@@ -265,7 +251,6 @@ class CableTransponderSearchSupport:
 		self.cable_search_container.execute(cmd)
 		tmpstr = _("Try to find used transponders in cable network.. please wait...")
 		tmpstr += "\n\n..."
-		self.setTimeout()
 		self.cable_search_session = self.session.openWithCallback(self.cableTransponderSearchSessionClosed, MessageBox, tmpstr, MessageBox.TYPE_INFO)
 
 class DefaultSatLists(DefaultWizard):
