f9e33f13445c24303275677b7a5b64bb5a6938ff
Paul In&In <darezik@gmail.com>
2010-11-10 20:30:42 +0100

    use newer eDVBServicePMTHandler::tuneExt call, add a virtual createDataSource function to make the code more re-usable

--- lib/service/servicedvb.h
+++ lib/service/servicedvb.h
@@ -289,6 +289,8 @@ protected:
 
 	ePtr<eConnection> m_video_event_connection;
 	void video_event(struct iTSMPEGDecoder::videoEvent);
+
+	virtual ePtr<iDataSource> createDataSource(eServiceReferenceDVB &ref);
 };
 
 class eStaticServiceDVBBouquetInformation: public iStaticServiceInformation
