openatv/enigma2

commit 73f929390ed2b40b35571acd08485465181065df

MovieSelection: Rename e2settings.pkl to .e2settings.pkl
Makes the files hidden on most systems, see
http://www.pli-images.org/forum/viewthread.php?forum_id=64&thread_id=15746&pid=167924#post_167924

lib/python/Screens/MovieSelection.py
@@ -459,8 +459,17 @@ class MovieSelection(Screen, HelpableScreen, SelectionEventInfo):
 	def loadLocalSettings(self):
 		'Load settings, called when entering a directory'
 		try:
-			path = os.path.join(config.movielist.last_videodir.value, "e2settings.pkl")
-			updates = pickle.load(open(path, "rb"))
+			try:
+				path = os.path.join(config.movielist.last_videodir.value, ".e2settings.pkl")
+				updates = pickle.load(open(path, "rb"))
+			except:
+				# load old settings file and rename it (this code should be removed in a week or so)
+				oldpath = os.path.join(config.movielist.last_videodir.value, "e2settings.pkl")
+				updates = pickle.load(open(oldpath, "rb"))
+				try:
+					os.rename(oldpath, path)
+				except:
+					pass
 			needUpdateDesc = ("description" in updates) and (updates["description"] != self.settings["description"]) 
 			self.settings.update(updates)
 			if needUpdateDesc:

commit 9cda124c46bf990f404f104772304378d98c379e

MovieSelection: Fix non-working preferences
sort order no longer functioned (because of the reload), wrote the
wrong settings file and loading the old file silently failed.

Broken since
73f929390ed2b40b35571acd08485465181065df
and also clean up old e2settings files.

lib/python/Screens/MovieSelection.py
@@ -447,7 +447,7 @@ class MovieSelection(Screen, HelpableScreen, SelectionEventInfo):
 
 	def saveLocalSettings(self):
 		try:
-			path = os.path.join(config.movielist.last_videodir.value, "e2settings.pkl")
+			path = os.path.join(config.movielist.last_videodir.value, ".e2settings.pkl")
 			pickle.dump(self.settings, open(path, "wb"))
 		except Exception, e:
 			print "Failed to save settings:", e
@@ -459,15 +459,21 @@ class MovieSelection(Screen, HelpableScreen, SelectionEventInfo):
 	def loadLocalSettings(self):
 		'Load settings, called when entering a directory'
 		try:
+			oldpath = os.path.join(config.movielist.last_videodir.value, "e2settings.pkl")
+			path = os.path.join(config.movielist.last_videodir.value, ".e2settings.pkl")
 			try:
-				path = os.path.join(config.movielist.last_videodir.value, ".e2settings.pkl")
 				updates = pickle.load(open(path, "rb"))
 			except:
-				# load old settings file and rename it (this code should be removed in a week or so)
-				oldpath = os.path.join(config.movielist.last_videodir.value, "e2settings.pkl")
-				updates = pickle.load(open(oldpath, "rb"))
+				# load old settings file and rename it (this code should be removed in a few weeks
+				if os.path.isfile(oldpath):
+					try:
+						updates = pickle.load(open(oldpath, "rb"))
+						os.rename(oldpath, path)
+					except:
+						pass
+			if os.path.isfile(oldpath):
 				try:
-					os.rename(oldpath, path)
+					os.unlink(oldpath)
 				except:
 					pass
 			needUpdateDesc = ("description" in updates) and (updates["description"] != self.settings["description"]) 