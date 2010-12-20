ef35eeebd55877fa9d1037a685f90b4027a0426e
Andreas Oberritter <obi@opendreambox.org>
2010-12-20 12:26:53 +0100

    version: use compile date if commit date is unavailable. default to "(no branch)"
    Please update your BitBake! (cd bb && git pull)

--- main/version_info.cpp
+++ main/version_info.cpp
@@ -1,13 +1,13 @@
 #include "version_info.h"
 #include "version.h"
 
-#ifndef ENIGMA2_LAST_CHANGE_DATE
-#define ENIGMA2_LAST_CHANGE_DATE __DATE__
+#ifndef ENIGMA2_COMMIT_DATE
+#define ENIGMA2_COMMIT_DATE __DATE__
 #endif
-const char *enigma2_date = ENIGMA2_LAST_CHANGE_DATE;
+const char *enigma2_date = ENIGMA2_COMMIT_DATE;
 
 #ifndef ENIGMA2_BRANCH
-#define ENIGMA2_BRANCH "HEAD"
+#define ENIGMA2_BRANCH "(no branch)"
 #endif
 const char *enigma2_branch = ENIGMA2_BRANCH;
 
