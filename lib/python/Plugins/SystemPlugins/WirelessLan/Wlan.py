openatv/enigma2

commit 12c50782b1db0f56f4efeab68c8a716d03b7f191

WirelessLan: avoid a crash, parsing iwconfig output

lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
@@ -469,7 +469,7 @@ class Status:
 						enc = _("Unsupported")
 					else:
 						enc = _("Disabled")
-				else:
+				else if '   Security' in line:
 					enc = line[line.index('Encryption key')+15 :line.index('   Security')]
 					if enc is not None:
 						enc = _("Enabled")

commit 5c722abc10e7150471c2f4ab482a6180d5147b0e

WirelessLan: fix typo introduced by 12c50782b1db0f56f4efeab68c8a716d03b7f191

lib/python/Plugins/SystemPlugins/WirelessLan/Wlan.py
@@ -469,7 +469,7 @@ class Status:
 						enc = _("Unsupported")
 					else:
 						enc = _("Disabled")
-				else if '   Security' in line:
+				elif '   Security' in line:
 					enc = line[line.index('Encryption key')+15 :line.index('   Security')]
 					if enc is not None:
 						enc = _("Enabled")