73803066c3759530b91858693844540594960e57
Littlesat <Littlesat99@yahoo.com>
2011-02-09 15:32:36 +0100

    Change snr in dB from two digits behind the dot to just one
    As 2 digits is excessive

--- lib/python/Components/Converter/FrontendInfo.py
+++ lib/python/Components/Converter/FrontendInfo.py
@@ -43,7 +43,7 @@ class FrontendInfo(Converter, object):
 			percent = self.source.snr
 		elif self.type == self.SNRdB:
 			if self.source.snr_db is not None:
-				return "%3.02f dB" % (self.source.snr_db / 100.0)
+				return "%3.01f dB" % (self.source.snr_db / 100.0)
 			elif self.source.snr is not None: #fallback to normal SNR...
 				percent = self.source.snr
 		elif self.type == self.TUNER_TYPE:
