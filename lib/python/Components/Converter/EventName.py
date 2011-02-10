290e64dab6eeb2afe8039bdac8d26d90d7db2979
pieterg <pieterg@users.sourceforge.net>
2011-02-10 22:52:23 +0100

    EventName converter: add ShortAndExtendedDescription support
    Returns combined short and extended descriptor data

--- lib/python/Components/Converter/EventName.py
+++ lib/python/Components/Converter/EventName.py
@@ -5,7 +5,8 @@ class EventName(Converter, object):
 	NAME = 0
 	SHORT_DESCRIPTION = 1
 	EXTENDED_DESCRIPTION = 2
-	ID = 3
+	SHORT_AND_EXTENDED_DESCRIPTION = 3
+	ID = 4
 	
 	def __init__(self, type):
 		Converter.__init__(self, type)
@@ -13,6 +14,8 @@ class EventName(Converter, object):
 			self.type = self.SHORT_DESCRIPTION
 		elif type == "ExtendedDescription":
 			self.type = self.EXTENDED_DESCRIPTION
+		elif type == "ShortAndExtendedDescription":
+			self.type == SHORT_AND_EXTENDED_DESCRIPTION:
 		elif type == "ID":
 			self.type = self.ID
 		else:
@@ -30,6 +33,12 @@ class EventName(Converter, object):
 			return event.getShortDescription()
 		elif self.type == self.EXTENDED_DESCRIPTION:
 			return event.getExtendedDescription()
+		elif self.type == self.SHORT_AND_EXTENDED_DESCRIPTION:
+			description = event.getShortDescription()
+			extended = event.getExtendedDescription()
+			if description and extended:
+				description += '\n\n'
+			return description + event.getExtendedDescription()
 		elif self.type == self.ID:
 			return str(event.getEventId())
 		
