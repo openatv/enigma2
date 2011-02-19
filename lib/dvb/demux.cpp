ca8ce05d854f44f9ee27eae3c2efcf4369f8b2fb
Paul In&In <darezik@gmail.com>
2011-02-19 17:17:07 +0100

    demux: re-introduce HAVE_OLDPVR in openDVR
    openDVR is public, and can be used by (external) plugins.
    So it should use HAVE_OLDPVR, to open the correct device.

--- lib/dvb/demux.cpp
+++ lib/dvb/demux.cpp
@@ -104,9 +104,13 @@ int eDVBDemux::openDemux(void)
 
 int eDVBDemux::openDVR(int flags)
 {
+#ifdef HAVE_OLDPVR
+	return ::open("/dev/misc/pvr", flags);
+#else
 	char filename[32];
 	snprintf(filename, sizeof(filename), "/dev/dvb/adapter%d/dvr%d", adapter, demux);
 	return ::open(filename, flags);
+#endif
 }
 
 DEFINE_REF(eDVBDemux)
