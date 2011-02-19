682b39c4f6abee9091a4907d4ec22e03f77bec7a
andyblac <andyblac@o2.co.uk>
2011-02-19 14:09:43 +0000

    Make ViX HD the skin default

--- skin.py
+++ skin.py
@@ -53,7 +53,7 @@ def addSkin(name, scope = SCOPE_SKIN):
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "Magic/skin.xml"
+DEFAULT_SKIN = "ViX_HD/skin.xml"
 config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
 
 profile("LoadSkin")
