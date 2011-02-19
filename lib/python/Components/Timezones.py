c08946d662a5b787544a91045d6840c3adc13732
andyblac <andyblac@o2.co.uk>
2011-02-19 16:13:12 +0000

    Change default Time Zone to "(GMT) Greenwich Mean Time : Dublin, Lisbon, London"

--- lib/python/Components/Timezones.py
+++ lib/python/Components/Timezones.py
@@ -56,7 +56,7 @@ class Timezones:
 
 	def getDefaultTimezone(self):
 		# TODO return something more useful - depending on country-settings?
-		t = "(GMT+01:00) Amsterdam, Berlin, Bern, Rome, Vienna"
+		t = "(GMT) Greenwich Mean Time : Dublin, Lisbon, London"
 		for (a,b) in self.timezones:
 			if a == t:
 				return a
