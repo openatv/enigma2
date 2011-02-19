a9d6ae27b7ad1bf639a73056ed2b6d0bab530c9a
andyblac <andyblac@o2.co.uk>
2011-02-19 14:45:12 +0000

    Add support to the About screen to read the ViX image-version file

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -11,6 +11,19 @@ class About:
 
 	def getImageVersionString(self):
 		try:
+			fd = open("/proc/stb/info/version","r")
+			version = fd.read()
+			print 'int val',int(version.split()[0],16)
+			if int(version,16) < 0x130000 and int(version,16) > 0x110000:
+				box_type="Duo"
+			elif int(version,16) > 0x140000:
+				box_type="Solo"
+			elif int(version,16) > 0x1 and int(version,16) < 0x3:
+				box_type="ET9000"
+		except:
+			box_type=""
+
+		try:
 			file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
 			lines = file.readlines()
 			for x in lines:
@@ -20,12 +33,22 @@ class About:
 					#0120 2005 11 29 01 16
 					#0123 4567 89 01 23 45
 					version = splitted[1]
-					year = version[-13:-9]
-					month = version[-9:-7]
-					day = version[-7:-5]
-					name = version[1:-13]
-
-					return '-'.join(([name, year, month, day]))
+					image_type = version[0] # 0 = release, 1 = experimental
+					major = version[1]
+					minor = version[2]
+					revision = version[3]
+					year = version[4:8]
+					month = version[8:10]
+					day = version[10:12]
+					date = '-'.join((year, month, day))
+					if image_type == '0':
+						image_type = "Release v"
+						version = '.'.join((major, minor, revision))
+						ver = ''.join((image_type, version))
+						return ' '.join((box_type, ver, ' - ', date))
+					else:
+						image_type = "Experimental"
+						return ' '.join((box_type, image_type, ' - ', date))
 			file.close()
 		except IOError:
 			pass
