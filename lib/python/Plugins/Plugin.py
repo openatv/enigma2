78d856f73bbb06cb1fa8796943081049f869bca7
ghost <andreas.monzner@multimedia-labs.de>
2011-03-03 18:43:41 +0100

    add plugin weight to PluginDescriptor... so the plugin start order can be changed now

--- lib/python/Plugins/Plugin.py
+++ lib/python/Plugins/Plugin.py
@@ -63,7 +63,7 @@ class PluginDescriptor:
 	WHERE_SOFTWAREMANAGER = 14
 
 
-	def __init__(self, name = "Plugin", where = [ ], description = "", icon = None, fnc = None, wakeupfnc = None, needsRestart = None, internal = False):
+	def __init__(self, name = "Plugin", where = [ ], description = "", icon = None, fnc = None, wakeupfnc = None, needsRestart = None, internal = False, weight = 0):
 		self.name = name
 		self.internal = internal
 		self.needsRestart = needsRestart
@@ -78,6 +78,8 @@ class PluginDescriptor:
 			self.icon = None
 		else:
 			self.icon = icon
+
+		self.weight = weight
 
 		self.wakeupfnc = wakeupfnc
 
