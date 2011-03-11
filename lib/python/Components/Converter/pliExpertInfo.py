c21ddddb83ab4d6df8e233b4ab5f45c69d97bc07
Paul In&In <darezik@gmail.com>
2011-03-11 23:05:16 +0100

    Add more improvements in pliExpertInfo.py
    But still a lot of improvements required.

--- lib/python/Components/Converter/pliExpertInfo.py
+++ lib/python/Components/Converter/pliExpertInfo.py
@@ -57,145 +57,150 @@ class pliExpertInfo(Poll, Converter, object):
 		else:
 			return ""	# unsupported orientation
 		
+		prvd = info.getInfoString(iServiceInformation.sProvider)
+		Ret_Text = self.short(prvd)
+
+		frontendData = (feinfo and feinfo.getAll(True))
+		if (frontendData is not None):
+			if ((frontendData.get("tuner_type") == "DVB-S") or (frontendData.get("tuner_type") == "DVB-C")):
+				frequency = (str((frontendData.get("frequency") / 1000)) + " MHz")
+				symbolrate = (str((frontendData.get("symbol_rate") / 1000)) + " KS/s")
+				try:
+					if (frontendData.get("tuner_type") == "DVB-S"):
+						polarisation_i = frontendData.get("polarization")
+					else:
+						polarisation_i = 0
+					fec_i = frontendData.get("fec_inner")
+					Ret_Text += sep + frequency + " - " + self.ar_pol[polarisation_i] + sep2 + self.ar_fec[fec_i] + " - " + symbolrate 
+				except:
+					Ret_Text += sep + frequency + sep + symbolrate
+				orb_pos = ""
+				if (frontendData.get("tuner_type") == "DVB-S"):
+					orbital_pos = int(frontendData["orbital_position"])
+					if orbital_pos > 1800:
+						orb_pos = str((float(3600 - orbital_pos)) / 10.0) + "W"
+					elif orbital_pos > 0:
+						orb_pos = str((float(orbital_pos)) / 10.0) + "E"
+				Ret_Text += sep + "Pos: " + orb_pos
+			elif (frontendData.get("tuner_type") == "DVB-T"):
+				frequency = (str((frontendData.get("frequency") / 1000)) + " MHz")
+				Ret_Text += sep + "Frequency:" + sep + frequency
+
 		if (feinfo is not None) and (xresol > 0):
 			if (yresol > 580):
-				Ret_Text = "HD " 
+				Ret_Text += sep + "HD "
 			else:
-				Ret_Text = "SD "
+				Ret_Text += sep + "SD "
 			Ret_Text += str(xresol) + "x" + str(yresol)
-			frontendData = (feinfo and feinfo.getAll(True))
-			if (frontendData is not None):
-				if ((frontendData.get("tuner_type") == "DVB-S") or (frontendData.get("tuner_type") == "DVB-C")):
-					frequency = (str((frontendData.get("frequency") / 1000)) + " MHz")
-					symbolrate = (str((float(frontendData.get("symbol_rate")) / float(1000000))) + " MS/s")
-					try:
-						if (frontendData.get("tuner_type") == "DVB-S"):
-							polarisation_i = frontendData.get("polarization")
-						else:
-							polarisation_i = 0
-						fec_i = frontendData.get("fec_inner")
-						Ret_Text += sep + frequency + " - " + self.ar_pol[polarisation_i] + sep2 + self.ar_fec[fec_i] + " - " + symbolrate 
-					except:
-						Ret_Text += sep + frequency + sep + symbolrate
-					orb_pos = ""
-					if (frontendData.get("tuner_type") == "DVB-S"):
-						orbital_pos = int(frontendData["orbital_position"])
-						if orbital_pos > 1800:
-							orb_pos = str((float(3600 - orbital_pos)) / 10.0) + "W"
-						elif orbital_pos > 0:
-							orb_pos = str((float(orbital_pos)) / 10.0) + "E"
-					Ret_Text += sep + "Pos: " + orb_pos
-				elif (frontendData.get("tuner_type") == "DVB-T"):
-					frequency = (str((frontendData.get("frequency") / 1000)) + " MHz")
-					Ret_Text += sep + "Frequency:" + sep + frequency
-			prvd = info.getInfoString(iServiceInformation.sProvider)
-			Ret_Text = self.short(prvd) + sep + Ret_Text
-
-		try:
-			f = open("/tmp/ecm.info", "r")
-			flines = f.readlines()
-			f.close()
-		except:
-			Sec_Text = "secEmpty"
-			flines = None
-
-		if (flines is not None):
-			ePid = ""
-			decID = ""
-			decFrom = ""
-			eMasTime = ""
-			eHops = ""
-			for cell in flines:
-				icell = cell.lower()
-				cellmembers = cell.split()
-				if ("caid" in icell):
-					for x in range(len(cellmembers)):
-						if ("caid" in cellmembers[x].lower()):
-							if x<(len(cellmembers) - 1):
-								if cellmembers[x+1] != "0x000":
-									decID = cellmembers[x + 1]
-									decID = decID.lstrip("0x")
-									decID = decID.strip(",;.:-*_<>()[]{}")
-									if (len(decID)<4):
-										decID = "0" + decID
-									decCI = decID
-							break
-				elif ("using:" in cell) or ("source:" in cell):
-					for x in range(len(cellmembers)):
-						if ("using:" in cellmembers[x]) or ("source:" in cellmembers[x]):
-							if x < (len(cellmembers) - 1):
-								if cellmembers[x + 1] != "fta":
-									decFrom = cellmembers[x + 1]
-							break
-				elif ("ecm time:" in cell):
-					for x in range(len(cellmembers)):
-						if ("time:" in cellmembers[x]):
-							if x < (len(cellmembers) - 1):
-								eMasTime = str(cellmembers[x + 1])
-							break
-				elif ("address:" in cell):
-					for x in range(len(cellmembers)):
-						if ("address:" in cellmembers[x]):
-							if x < (len(cellmembers) - 1):
-								adrFrom = cellmembers[x + 1]
-								if ("sci0" in adrFrom):
-									decFrom = "Slot-1"
-								elif ("sci1" in adrFrom):
-									decFrom = "Slot-2"
-								elif ("sci2" in adrFrom):
-									decFrom = "Slot-3"
-								else:
-									decFrom = adrFrom
-							break
-				elif ("hops:" in cell):
-					for x in range(len(cellmembers)):
-						if ("hops:" in cellmembers[x]):
-							if x<(len(cellmembers) - 1):
-								eHops = str(cellmembers[x + 1])
-							break
-				elif ("pid:" in cell):
-					for x in range(len(cellmembers)):
-						if ("pid:" in cellmembers[x]):
-							if x<(len(cellmembers) - 1):
-								ePid = str(cellmembers[x + 1])
-								ePid = ePid.lstrip("0x")
-								if (len(ePid) == 3):
-									ePid = "0" + ePid
-								elif (len(ePid) == 2):
-									ePid = "00" + ePid
-								elif (len(ePid) == 1):
-									ePid = "000" + ePid
-								ePid = ePid.upper()
-							break
-			if decID != "":
-				for idline in self.idnames:
-					IDlist = idline.split(",")
-					try:
-						if (int(decID, 16) >= int(IDlist[0], 16)) and (int(decID, 16) <= int(IDlist[1], 16)):
-							decID = IDlist[2] + ":" + decID.upper()
-							break
-					except:
-						pass
-
-			Sec_Text = self.short(decFrom) + sep + decID + sep + "pid: " + ePid + sep + eHops + " hops" + sep + eMasTime + " sec."
-
-		try:
-			searchIDs = (info.getInfoObject(iServiceInformation.sCAIDs))
-			for idline in self.idnames:
-				IDlist = idline.split(",")
-				color = "\c007?7?7?"
-				for oneID in searchIDs:
-					if (oneID >= int(IDlist[0], 16)) and (oneID <= int(IDlist[1], 16)):
-						color="\c00????00"
-						if (oneID == int(decCI, 16)):
-							color="\c0000??00"
-							break
-				res = res + color + IDlist[3] + " "
-		except:
-			pass
 
 		if (info.getInfo(iServiceInformation.sIsCrypted) == 1):
-			Ret_Text += "\n" + res + "\c00??????" + sep + Sec_Text
+
+			try:
+				f = open("/tmp/ecm.info", "r")
+				flines = f.readlines()
+				f.close()
+			except:
+				Sec_Text = "secEmpty"
+				flines = None
+
+			if (flines is not None):
+				ePid = ""
+				decID = ""
+				decFrom = ""
+				eMasTime = ""
+				eHops = ""
+				for cell in flines:
+					icell = cell.lower()
+					cellmembers = cell.split()
+					if ("caid" in icell):
+						for x in range(len(cellmembers)):
+							if ("caid" in cellmembers[x].lower()):
+								if x<(len(cellmembers) - 1):
+									if cellmembers[x+1] != "0x000":
+										decID = cellmembers[x + 1]
+										decID = decID.lstrip("0x")
+										decID = decID.strip(",;.:-*_<>()[]{}")
+										if (len(decID)<4):
+											decID = "0" + decID
+										decCI = decID
+								break
+					elif ("using:" in cell) or ("source:" in cell):
+						for x in range(len(cellmembers)):
+							if ("using:" in cellmembers[x]) or ("source:" in cellmembers[x]):
+								if x < (len(cellmembers) - 1):
+									if cellmembers[x + 1] != "fta":
+										decFrom = cellmembers[x + 1]
+								break
+					elif ("ecm time:" in cell):
+						for x in range(len(cellmembers)):
+							if ("time:" in cellmembers[x]):
+								if x < (len(cellmembers) - 1):
+									eMasTime = str(cellmembers[x + 1])
+								break
+					elif ("address:" in cell):
+						for x in range(len(cellmembers)):
+							if ("address:" in cellmembers[x]):
+								if x < (len(cellmembers) - 1):
+									adrFrom = cellmembers[x + 1]
+									if ("sci0" in adrFrom):
+										decFrom = "Slot-1"
+									elif ("sci1" in adrFrom):
+										decFrom = "Slot-2"
+									elif ("sci2" in adrFrom):
+										decFrom = "Slot-3"
+									else:
+										decFrom = adrFrom
+								break
+					elif ("hops:" in cell):
+						for x in range(len(cellmembers)):
+							if ("hops:" in cellmembers[x]):
+								if x<(len(cellmembers) - 1):
+									eHops = str(cellmembers[x + 1])
+								break
+					elif ("pid:" in cell):
+						for x in range(len(cellmembers)):
+							if ("pid:" in cellmembers[x]):
+								if x<(len(cellmembers) - 1):
+									ePid = str(cellmembers[x + 1])
+									ePid = ePid.lstrip("0x")
+									if (len(ePid) == 3):
+										ePid = "0" + ePid
+									elif (len(ePid) == 2):
+										ePid = "00" + ePid
+									elif (len(ePid) == 1):
+										ePid = "000" + ePid
+									ePid = ePid.upper()
+								break
+				if decID != "":
+					for idline in self.idnames:
+						IDlist = idline.split(",")
+						try:
+							if (int(decID, 16) >= int(IDlist[0], 16)) and (int(decID, 16) <= int(IDlist[1], 16)):
+								decID = IDlist[2] + ":" + decID.upper()
+								break
+						except:
+							pass
+				Sec_Text = decFrom + " @" + eHops + " (%ss) " % eMasTime + decID + " pid:" + ePid
+	
+			try:
+				searchIDs = (info.getInfoObject(iServiceInformation.sCAIDs))
+				for idline in self.idnames:
+					IDlist = idline.split(",")
+					color = "\c007?7?7?"
+					for oneID in searchIDs:
+						if (oneID >= int(IDlist[0], 16)) and (oneID <= int(IDlist[1], 16)):
+							color="\c00????00"
+							if (oneID == int(decCI, 16)):
+								color="\c0000??00"
+								break
+					res = res + color + IDlist[3] + " "
+			except:
+				pass
+	
+			Ret_Text += "\n" + res + "\c00?????? " + Sec_Text
+		else:
+			Ret_Text += "\n\c007?7?7?S V I Nd Co Cw B N" + "\c00?????? FTA" 
+
 
 		return Ret_Text
 
