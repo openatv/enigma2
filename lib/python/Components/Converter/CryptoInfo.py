47d980be389750ef6e5e0be6a71134c2722e3346
Paul In&In <darezik@gmail.com>
2011-03-17 17:40:21 +0100

    Use one please to parse /tmp/ecm.info
    This was parsed in two places, CryptoInfo.py and pliExpertInfo.py.
    In additional some other small optimalizations

--- lib/python/Components/Converter/CryptoInfo.py
+++ lib/python/Components/Converter/CryptoInfo.py
@@ -1,127 +1,26 @@
 from Components.Converter.Converter import Converter
-from enigma import eTimer
 from Components.Element import cached
 from Components.config import config
-import os
+from Tools.GetEcmInfo import GetEcmInfo 
+from Poll import Poll
 
-ECM_INFO = '/tmp/ecm.info'
 
-class CryptoInfo(Converter, object):
+class CryptoInfo(Poll, Converter, object):
 	def __init__(self, type):
 		Converter.__init__(self, type)
-		# type is ignored, expected to be "verboseInfo"
+		Poll.__init__(self)
+
 		self.active = False
 		self.visible = config.usage.show_cryptoinfo.value
 		self.textvalue = ""
-		self.clock_timer = eTimer()
-		self.clock_timer.callback.append(self.poll)
-		if self.visible:
-			self.clock_timer.start(1000)
-		self.ecm_mtime = None
-
-	def destroy(self):
-		self.clock_timer.callback.remove(self.poll)
-		Converter.destroy(self)
-
-	def poll(self):
-		try:
-			ecm_mtime = os.stat(ECM_INFO).st_mtime
-		except:
-			ecm_mtime = None
-		if ecm_mtime != self.ecm_mtime:
-			self.ecm_mtime = ecm_mtime
-			self.changed((self.CHANGED_POLL,))
-
-	def doSuspend(self, suspended):
-		if suspended:
-			self.clock_timer.stop()
-		else:
-			self.clock_timer.start(1000)
-			self.poll()
-
+		self.poll_interval = 1000
+		self.poll_enabled = True
+		
 	@cached
 	def getText(self):
 		if not self.visible:
 			return ''
-		# file changed.
-		try:
-			ecm = open(ECM_INFO, 'rb').readlines()
-			info = {}
-			for line in ecm:
-				d = line.split(':', 1)
-				if len(d) > 1:
-					info[d[0].strip()] = d[1].strip()
-			# info is dictionary
-			using = info.get('using', '')
-			if using:
-				# CCcam
-				if using == 'fta':
-					self.textvalue = _("FTA")
-				elif using == 'emu':
-					self.textvalue = "EMU (%ss)" % (info.get('ecm time', '?'))
-				else:
-					hops = info.get('hops', None)
-					if hops and hops != '0':
-						hops = ' @' + hops
-					else:
-						hops = ''
-					self.textvalue = info.get('address', '?') + hops + " (%ss)" % info.get('ecm time', '?')
-			else:
-				decode = info.get('decode', None)
-				if decode:
-					# gbox (untested)
-					if info['decode'] == 'Network':
-						cardid = 'id:' + info.get('prov', '')
-						try:
-							share = open('/tmp/share.info', 'rb').readlines()
-							for line in share:
-								if cardid in line:
-									self.textvalue = line.strip()
-									break
-							else:
-								self.textvalue = cardid
-						except:
-							self.textvalue = decode
-					else:
-						self.textvalue = decode
-				else:
-					source = info.get('source', '')
-					if source:
-						# MGcam
-						eEnc  = ""
-						eCaid = ""
-						eSrc = ""
-						eTime = ""
-						for line in ecm:
-							line = line.strip() 
-							if line.find('ECM') != -1:
-								line = line.split(' ')
-								eEnc = line[1]
-								eCaid = line[5][2:-1]
-								continue
-							if line.find('source') != -1:
-								line = line.split(' ')
-								eSrc = line[4][:-1]
-								continue
-							if line.find('msec') != -1:
-								line = line.split(' ')
-								eTime = line[0]
-								continue
-						self.textvalue = "(%s %s %.3f @ %s)" % (eEnc,eCaid,(float(eTime)/1000),eSrc)
-					else:
-						reader = info.get('reader', '')
-						if reader:
-							hops = info.get('hops', None)
-							if hops and hops != '0':
-								hops = ' @' + hops
-							else:
-								hops = ''
-							self.textvalue = reader + hops + " (%ss)" % info.get('ecm time', '?')
-						else:
-							self.textvalue = ""
-		except:
-			ecm = None
-			self.textvalue = ""
-		return self.textvalue
-
+		ecmdata = GetEcmInfo()
+		data = ecmdata.getEcmData()
+		return data[0]
 	text = property(getText)
--- lib/python/Components/Converter/pliExpertInfo.py
+++ lib/python/Components/Converter/pliExpertInfo.py
@@ -7,9 +7,8 @@ from Components.Converter.Converter import Converter
 from Components.Element import cached
 from Components.config import config
 from Tools.Transponder import ConvertToHumanReadable
+from Tools.GetEcmInfo import GetEcmInfo 
 from Poll import Poll
-
-ECM_INFO = '/tmp/ecm.info'
 
 class pliExpertInfo(Poll, Converter, object):
 	SMART_LABEL = 0
@@ -26,7 +25,7 @@ class pliExpertInfo(Poll, Converter, object):
 		try:
 			self.poll_interval = config.plugins.ValiKSSetup.pollTime.value*1000
 		except:
-			self.poll_interval = 30000
+			self.poll_interval = 1000
 		self.poll_enabled = True
 		self.idnames = (
 			( "0x100", "0x1FF","Seca"   ,"S" ),
@@ -94,121 +93,44 @@ class pliExpertInfo(Poll, Converter, object):
 				Ret_Text += sep + "SD "
 			Ret_Text += str(xresol) + "x" + str(yresol)
 
+		decCI = "0" 
+		Sec_Text = ""
 		if (info.getInfo(iServiceInformation.sIsCrypted) == 1):
+			ecmdata = GetEcmInfo() 
+			data = ecmdata.getEcmData()
+			Sec_Text = data[0]	
+			decCI = data[1]
+			provid = data[2]
+			pid = data[3]	
+
+			decCIfull = ''
+			if decCI != '0':
+				for idline in self.idnames:
+					try:
+						if int(decCI, 16) >= int(idline[0], 16) and int(decCI, 16) <= int(idline[1], 16):
+							decCIfull = idline[2] + ":" + decCI
+							break
+					except:
+						pass
+				Sec_Text += sep + decCIfull
+			if pid != '0':
+				Sec_Text += sep + "pid:" + pid
 			
-			try:
-				ecm = open(ECM_INFO, 'rb').readlines()
-	                        ecminfo = {}
-				for line in ecm:
-					d = line.split(':', 1)
-					if len(d) > 1:
-						ecminfo[d[0].strip()] = d[1].strip()
-				
-				using = ecminfo.get('using', '')
-				if using:
-					# CCcam
-					if using == 'fta':
-						Sec_Text = _("FTA")
-					elif using == 'emu':
-						Sec_Text = "EMU (%ss)" % (ecminfo.get('ecm time', '?'))
-					else:
-						hops = ecminfo.get('hops', None)
-						if hops and hops != '0':
-							hops = ' @' + hops
-						else:
-							hops = ''
-						Sec_Text = ecminfo.get('address', '?') + hops + " (%ss)" % ecminfo.get('ecm time', '?')
-				else:
-					decode = ecminfo.get('decode', None)
-					if decode:
-						# gbox (untested)
-						if ecminfo['decode'] == 'Network':
-							cardid = 'id:' + ecminfo.get('prov', '')
-							try:
-								share = open('/tmp/share.info', 'rb').readlines()
-								for line in share:
-									if cardid in line:
-										Sec_Text = line.strip()
-										break
-								else:
-									Sec_Text = cardid
-							except:
-								Sec_Text = decode
-						else:
-							Sec_Text = decode
-					else:
-						source = ecminfo.get('source', '')
-						if source:
-							# MGcam
-							eEnc  = ""
-							eCaid = ""
-							eSrc = ""
-							eTime = ""
-							for line in ecm:
-								line = line.strip() 
-								if line.find('ECM') != -1:
-									line = line.split(' ')
-									eEnc = line[1]
-									eCaid = line[5][2:-1]
-									continue
-								if line.find('source') != -1:
-									line = line.split(' ')
-									eSrc = line[4][:-1]
-									continue
-								if line.find('msec') != -1:
-									line = line.split(' ')
-									eTime = line[0]
-									continue
-							Sec_Text = "(%s %s %.3f @ %s)" % (eEnc,eCaid,(float(eTime)/1000),eSrc)
-						else:
-							reader = ecminfo.get('reader', '')
-							if reader:
-								#Oscam
-								hops = ecminfo.get('hops', None)
-								if hops and hops != '0':
-									hops = ' @' + hops
-								else:
-									hops = ''
-								Sec_Text = reader + hops + " (%ss)" % ecminfo.get('ecm time', '?')
-							else:
-								Sec_Text = ""
-	
-				pid = ecminfo.get('pid', None)
-				decCI = ecminfo.get('caid', None)
-				
-				decCIfull = ""
-				if decCI != "":
-					for idline in self.idnames:
-						try:
-							if int(decCI, 16) >= int(idline[0], 16) and int(decCI, 16) <= int(idline[1], 16):
-								decCIfull = idline[2] + ":" + decCI
-								break
-						except:
-							pass
-			
-				Sec_Text += sep + decCIfull + sep + "pid:" + pid
-	
-				res = ""			
-				try:
-					searchIDs = (info.getInfoObject(iServiceInformation.sCAIDs))
-					for idline in self.idnames:
-						if int(decCI, 16) >= int(idline[0], 16) and int(decCI, 16) <= int(idline[1], 16):                    
-							color="\c0000??00"
-						else:
-							color = "\c007?7?7?"
-							for oneID in searchIDs:
-								if oneID >= int(idline[0], 16) and oneID <= int(idline[1], 16):
-									color="\c00????00"
-						res += color + idline[3] + " "
-				except:
-					pass
-		
-				Ret_Text += "\n" + res + "\c00?????? " + Sec_Text
-			except:
-				Ret_Text += "\n\c007?7?7?S V I Nd Co Cw B N" + "\c00?????? No expert cryptinfo available" 
-				pass
 		else:
-			Ret_Text += "\n\c007?7?7?S V I Nd Co Cw B N" + "\c00?????? FTA"
+			Sec_Text = "FTA"
+		res = ""			
+		searchIDs = (info.getInfoObject(iServiceInformation.sCAIDs))
+		for idline in self.idnames:
+			if int(decCI, 16) >= int(idline[0], 16) and int(decCI, 16) <= int(idline[1], 16):                    
+				color="\c0000??00"
+			else:
+				color = "\c007?7?7?"
+				for oneID in searchIDs:
+					if oneID >= int(idline[0], 16) and oneID <= int(idline[1], 16):
+						color="\c00????00"
+			res += color + idline[3] + " "
+
+		Ret_Text += "\n" + res + "\c00?????? " + Sec_Text
 
 		return Ret_Text
 
