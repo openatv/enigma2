4e7f00a7a88ebb3adab2c50868a35a7449e581af
Paul In&In <darezik@gmail.com>
2011-07-01 13:00:50 +0100

    TimeShift: update default timeshift infobar.

--- lib/python/Screens/PVRState.py
+++ lib/python/Screens/PVRState.py
@@ -21,9 +21,9 @@ class PTSTimeshiftState(Screen):
 			<widget source="session.CurrentService" render="Label" position="345,5" size="60,27" font="Regular;20" halign="left" foregroundColor="white" backgroundColor="transpBlack">
 				<convert type="ServicePosition">Length</convert>
 			</widget>
-			<widget name="PTSSeekPointer" position="8,30" zPosition="3" size="19,50" pixmap="/usr/share/enigma2/skin_default/timeline-now.png" alphatest="on" />
-			<ePixmap position="10,33" size="840,15" zPosition="1" pixmap="/usr/share/enigma2/skin_default/slider_back.png" alphatest="on"/>
-			   <widget source="session.CurrentService" render="Progress" position="10,33" size="390,15" zPosition="2" pixmap="/usr/share/enigma2/skin_default/slider.png" transparent="1">
+			<widget name="PTSSeekPointer" position="0,30" zPosition="3" size="19,20" pixmap="/usr/share/enigma2/skin_default/timeline-now.png" alphatest="on" />
+			<ePixmap position="15,33" size="390,15" zPosition="1" pixmap="/usr/share/enigma2/skin_default/slider_back.png" alphatest="on"/>
+			   <widget source="session.CurrentService" render="Progress" position="15,33" size="390,15" zPosition="2" pixmap="/usr/share/enigma2/skin_default/slider.png" transparent="1">
 				<convert type="ServicePosition">Position</convert>
 			</widget>
 			<widget name="eventname" position="10,49" zPosition="4" size="420,20" font="Regular;18" halign="center" backgroundColor="transpBlack" />
