779723f31670459cf6a849d2bdf202a7588c42d3
Paul In&In <darezik@gmail.com>
2011-07-26 17:08:35 +0200

    About: Check date validity, no popen, singleton is silly
    Dates before 2011 are invalid
    Don't use popen to get kernel version
    Singleton class is useless in a language where modules are objects

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,38 +1,32 @@
-from Tools.Directories import resolveFilename, SCOPE_SYSETC
-from enigma import getEnigmaVersionString
+import sys
 import os
 import time
 
-class About:
-	def __init__(self):
-		pass
+def getVersionString():
+	return getImageVersionString()
 
-	def getVersionString(self):
-		return self.getImageVersionString()
-
-	def getImageVersionString(self):
-		try:
-			st = os.stat('/usr/lib/ipkg/status')
-			tm = time.localtime(st.st_mtime)
+def getImageVersionString():
+	try:
+		st = os.stat('/usr/lib/ipkg/status')
+		tm = time.localtime(st.st_mtime)
+		if tm.tm_year >= 2011:
 			return time.strftime("%b %e %Y %H:%M:%S", tm)
-		except:
-			pass
-		return "unavailable"
+	except:
+		pass
+	return "unavailable"
 
-	def getEnigmaVersionString(self):
-		enigma_version = getEnigmaVersionString()
-		if '-(no branch)' in enigma_version:
-			enigma_version = enigma_version [:-12]
-		return enigma_version
+def getEnigmaVersionString():
+	import enigma
+	enigma_version = enigma.getEnigmaVersionString()
+	if '-(no branch)' in enigma_version:
+		enigma_version = enigma_version [:-12]
+	return enigma_version
 
-	def getKernelVersionString(self):
-		try:
-			result = popen("uname -r","r").read().strip("\n").split('-')
-			kernel_version = result[0]
-			return kernel_version
-		except:
-			pass
-
+def getKernelVersionString():
+	try:
+		return open("/proc/version","r").read().split(' ', 4)[2].split('-',2)[0]
+	except:
 		return "unknown"
 
-about = About()
+# For modules that do "from About import about"
+about = sys.modules[__name__]
