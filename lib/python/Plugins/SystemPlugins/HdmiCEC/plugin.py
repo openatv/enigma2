a492d8869c9b4f42cba448468337a9fee39b505e
Paul In&In <darezik@gmail.com>
2011-08-13 22:08:03 +0200

    add HdmiCEC setup plugin
    configure which CEC messages are sent to the TV when the stb
    switches between standby/wakup status

---
+++ lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
@@ -0,0 +1,64 @@
+from Screens.Screen import Screen
+from Components.ConfigList import ConfigListScreen
+from Components.config import config, getConfigListEntry
+
+class HdmiCECSetupScreen(Screen, ConfigListScreen):
+	skin = """
+	<screen position="c-200,c-100" size="400,200" title="HDMI CEC setup">
+		<widget name="config" position="c-175,c-75" size="350,150" />
+		<ePixmap pixmap="skin_default/buttons/green.png" position="c-145,e-45" zPosition="0" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/red.png" position="c+5,e-45" zPosition="0" size="140,40" alphatest="on" />
+		<widget name="ok" position="c-145,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="green" />
+		<widget name="cancel" position="c+5,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="red" />
+	</screen>"""
+
+	def __init__(self, session):
+		self.skin = HdmiCECSetupScreen.skin
+		Screen.__init__(self, session)
+
+		from Components.ActionMap import ActionMap
+		from Components.Button import Button
+
+		self["ok"] = Button(_("OK"))
+		self["cancel"] = Button(_("Cancel"))
+
+		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
+		{
+			"ok": self.keyGo,
+			"save": self.keyGo,
+			"cancel": self.keyCancel,
+			"green": self.keyGo,
+			"red": self.keyCancel,
+		}, -2)
+
+		self.list = []
+		ConfigListScreen.__init__(self, self.list, session = self.session)
+		self.list.append(getConfigListEntry(_("Enabled"), config.hdmicec.enabled))
+		self.list.append(getConfigListEntry(_("Standby message"), config.hdmicec.standby_message))
+		self.list.append(getConfigListEntry(_("Wakeup message"), config.hdmicec.wakeup_message))
+		self["config"].list = self.list
+		self["config"].l.setList(self.list)
+
+	def keyLeft(self):
+		ConfigListScreen.keyLeft(self)
+
+	def keyRight(self):
+		ConfigListScreen.keyRight(self)
+
+	def keyGo(self):
+		config.hdmicec.save()
+		self.close()
+
+	def keyCancel(self):
+		self.close()
+
+def main(session, **kwargs):
+	session.open(HdmiCECSetupScreen)
+
+def Plugins(**kwargs):
+	from os import path
+	if path.exists("/dev/hdmi_cec"):
+		import Components.HdmiCec
+		from Plugins.Plugin import PluginDescriptor
+		return [PluginDescriptor(name = "HDMI CEC setup", description = _("Adjust HDMI CEC settings"), where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main)]
+	return []
