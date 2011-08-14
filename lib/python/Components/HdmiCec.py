e131b52401fcb9088368f22dfd2b5d6ce466e480
Paul In&In <darezik@gmail.com>
2011-08-14 13:41:05 +0200

    HdmiCec: add menu active / inactive support
    menu active/inactive enables/disables rc passthrough

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -6,15 +6,21 @@ config.hdmicec = ConfigSubsection()
 config.hdmicec.enabled = ConfigYesNo(default = True)
 config.hdmicec.standby_message = ConfigSelection(
 	choices = {
-	"standby": _("TV standby"),
+	"standby": _("TV off"),
 	"inactive": _("Source inactive"),
+	"menuinactive": _("Menu inactive"),
+	"menuinactive,standby": _("Menu inactive, TV off"),
+	"menuinactive,inactive": _("Menu inactive, source inactive"),
 	"nothing": _("Nothing"),
 	},
 	default = "standby")
 config.hdmicec.wakeup_message = ConfigSelection(
 	choices = {
-	"wakeup": _("TV wakeup"),
+	"wakeup": _("TV on"),
 	"active": _("Source active"),
+	"menuactive": _("Menu active"),
+	"wakeup,menuactive": _("TV on, menu active"),
+	"active,menuactive": _("Source active, menu active"),
 	"nothing": _("Nothing"),
 	},
 	default = "wakeup")
@@ -37,6 +43,10 @@ class HdmiCec:
 			elif message == "inactive":
 				physicaladdress = eHdmiCEC.getInstance().getPhysicalAddress()
 				cmd = struct.pack('BBB', 0x9d, int(physicaladdress/256), int(physicaladdress%256))
+			elif message == "menuactive":
+				cmd = struct.pack('BB', 0x8e, 0x00)
+			elif message == "menuinactive":
+				cmd = struct.pack('BB', 0x8e, 0x01)
 			if cmd:
 				eHdmiCEC.getInstance().sendMessage(address, len(cmd), str(cmd))
 
