5e28edfee9d4a8af74f2ecc3b8807739f15ab1dc
pieterg <pieterg@users.sourceforge.net>
2011-08-15 15:59:52 +0200

    iDVBChannel: rewrite requestTsidOnid
    Use a PSignal instead of a callback

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -1925,31 +1925,17 @@ RESULT eDVBChannel::setCIRouting(const eDVBCIRouting &routing)
 
 void eDVBChannel::SDTready(int result)
 {
-	ePyObject args = PyTuple_New(2), ret;
-	bool ok=false;
+	int tsid = -1, onid = -1;
 	if (!result)
 	{
 		for (std::vector<ServiceDescriptionSection*>::const_iterator i = m_SDT->getSections().begin(); i != m_SDT->getSections().end(); ++i)
 		{
-			ok = true;
-			PyTuple_SET_ITEM(args, 0, PyInt_FromLong((*i)->getTransportStreamId()));
-			PyTuple_SET_ITEM(args, 1, PyInt_FromLong((*i)->getOriginalNetworkId()));
+			tsid = (*i)->getTransportStreamId();
+			onid = (*i)->getOriginalNetworkId();
 			break;
 		}
 	}
-	if (!ok)
-	{
-		PyTuple_SET_ITEM(args, 0, Py_None);
-		PyTuple_SET_ITEM(args, 1, Py_None);
-		Py_INCREF(Py_None);
-		Py_INCREF(Py_None);
-	}
-	ret = PyObject_CallObject(m_tsid_onid_callback, args);
-	if (ret)
-		Py_DECREF(ret);
-	Py_DECREF(args);
-	Py_DECREF(m_tsid_onid_callback);
-	m_tsid_onid_callback = ePyObject();
+	/* emit */ receivedTsidOnid(tsid, onid);
 	m_tsid_onid_demux = 0;
 	m_SDT = 0;
 }
@@ -1966,25 +1952,20 @@ int eDVBChannel::reserveDemux()
 	return -1;
 }
 
-RESULT eDVBChannel::requestTsidOnid(ePyObject callback)
+RESULT eDVBChannel::requestTsidOnid()
 {
-	if (PyCallable_Check(callback))
+	if (!getDemux(m_tsid_onid_demux, 0))
 	{
-		if (!getDemux(m_tsid_onid_demux, 0))
+		m_SDT = new eTable<ServiceDescriptionSection>;
+		CONNECT(m_SDT->tableReady, eDVBChannel::SDTready);
+		if (m_SDT->start(m_tsid_onid_demux, eDVBSDTSpec()))
 		{
-			m_SDT = new eTable<ServiceDescriptionSection>;
-			CONNECT(m_SDT->tableReady, eDVBChannel::SDTready);
-			if (m_SDT->start(m_tsid_onid_demux, eDVBSDTSpec()))
-			{
-				m_tsid_onid_demux = 0;
-				m_SDT = 0;
-			}
-			else
-			{
-				Py_INCREF(callback);
-				m_tsid_onid_callback = callback;
-				return 0;
-			}
+			m_tsid_onid_demux = 0;
+			m_SDT = 0;
+		}
+		else
+		{
+			return 0;
 		}
 	}
 	return -1;
