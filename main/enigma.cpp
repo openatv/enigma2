e51ca18a89b434a33875c1e0f35fff2f42b29dc9
Andy Blackburn <andyblac@o2.co.uk>
2011-10-03 20:56:04 +0100

    move spinner closer to corner, now we dont need to allow room for overscan.

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -219,9 +219,9 @@ int main(int argc, char **argv)
 			}
 		}
 		if (i)
-			my_dc->setSpinner(eRect(ePoint(100, 100), wait[0]->size()), wait, i);
+			my_dc->setSpinner(eRect(ePoint(25, 25), wait[0]->size()), wait, i);
 		else
-			my_dc->setSpinner(eRect(100, 100, 0, 0), wait, 1);
+			my_dc->setSpinner(eRect(25, 25, 0, 0), wait, 1);
 	}
 	
 	gRC::getInstance()->setSpinnerDC(my_dc);
