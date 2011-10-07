c2bfc38fb5054f578ef88210bdf7bb999f01f3bc
Captain <captain.onboard@web.de>
2011-10-07 22:33:41 +0200

    update backup 6.4

--- lib/python/Plugins/SystemPlugins/SoftwareManager/backup-usb.sh
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/backup-usb.sh
@@ -2,19 +2,28 @@ TARGET="XX"
 
 for candidate in  /media/usb /media/sdb1 /media/sdc1 /media/sdd1 /media/sde1 /media/mmc1 
 do
-   if [ -f ${candidate}/vubackupstick -o -f ${candidate}/vubackupstick.txt -o -f ${candidate}/backupstick -o -f ${candidate}/backupstick.txt ]
+   if [ -f ${candidate}/*backupstick* ]
    then
      TARGET=${candidate}
    fi    
 done
 
 if [ $TARGET = "XX" ] ; then
-    echo "There is no valid USB-stick found, so I've got nothing to do"
+    echo "There is NO valid USB-stick found, so I've got nothing to do"
 	echo " "
-    echo "If you place an USB-stick containing a file with the name: "
-    echo "'backupstick' then the back-up will be automatically "
-    echo "made on the USB-stick and can be used to restore the "
-    echo "current image if necessary. "
+    echo "0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0"
+	echo " "
+	echo "WARNING    WARNING    WARNING    WARNING    WARNING"
+	echo "To back-up directly to the USB-stick, the USB-stick MUST "
+	echo "contain a file with the name: "
+	echo "backupstick or "
+	echo "backupstick.txt "
+	echo " "
+	echo "0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0"
+	echo " "
+	echo "If you place an USB-stick containing this file then the "
+    echo "back-up will be automatically made on the USB-stick and" 
+    echo "can be used to restore the current image if necessary. "
     echo
     echo "The program will exit now. "
 	echo " "
