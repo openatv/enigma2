e541fc00eee5d1f2739681ef0b129c5d1c808cfc
Paul In&In <darezik@gmail.com>
2011-10-22 13:45:07 +0100

    UserConfig: change EPG reload sched Default to Disabled.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -233,7 +233,7 @@ def InitUsageConfig():
 	config.epg.freesat.addNotifier(EpgSettingsChanged)
 	config.epg.viasat.addNotifier(EpgSettingsChanged)
 	config.epg.netmed.addNotifier(EpgSettingsChanged)
-	config.epg.cachesched = ConfigYesNo(default = True)
+	config.epg.cachesched = ConfigYesNo(default = False)
 	config.epg.cachetimer = ConfigSelection(default = 24, choices = [
 		("1", "1"),("2", "2"),("3", "3"),("4", "4"),("5", "5"),("6", "6"),("7", "7"),("8", "8"),("9", "9"),("10", "10"),
 		("11", "11"),("12", "12"),("13", "13"),("14", "14"),("15", "15"),("16", "16"),("17", "17"),("18", "18"),("19", "19"),("20", "20"),
