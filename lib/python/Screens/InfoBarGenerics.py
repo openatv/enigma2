d960d50f58e9939b461cf88af34879de3f0f914d
Paul In&In <darezik@gmail.com>
2011-11-12 16:07:51 +0100

    InfoBar: add support for activation of 'red button' services
    For now, just check for HBBTV info

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1940,6 +1940,25 @@ class InfoBarSubserviceSelection:
 		else:
 			del self.selectedSubservice
 
+class InfoBarRedButton:
+	def __init__(self):
+		self["RedButtonActions"] = HelpableActionMap(self, "InfobarRedButtonActions",
+			{
+				"activateRedButton": (self.activateRedButton, _("Red button...")),
+			})
+		self.onHBBTVActivation = [ ]
+		self.onRedButtonActivation = [ ]
+
+	def activateRedButton(self):
+		service = self.session.nav.getCurrentService()
+		info = service and service.info()
+		if info and info.getInfoString(iServiceInformation.sHBBTVUrl) != "":
+			for x in self.onHBBTVActivation:
+				x()
+		elif False: # TODO: other red button services
+			for x in self.onRedButtonActivation:
+				x()
+
 class InfoBarAdditionalInfo:
 	def __init__(self):
 
