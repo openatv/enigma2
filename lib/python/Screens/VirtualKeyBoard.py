f7a3b8b416d0ff84b7af180e2881ffa059251725
Paul In&In <darezik@gmail.com>
2011-12-02 21:18:59 +0000

    update keyboard support.

--- lib/python/Screens/VirtualKeyBoard.py
+++ lib/python/Screens/VirtualKeyBoard.py
@@ -97,7 +97,7 @@ class VirtualKeyBoard(Screen):
 		self["text"] = Label(self.text)
 		self["list"] = VirtualKeyBoardList([])
 		
-		self["actions"] = ActionMap(["OkCancelActions", "WizardActions", "DirectionActions", "ColorActions", "KeyboardInputActions"],
+		self["actions"] = ActionMap(["OkCancelActions", "WizardActions", "DirectionActions", "ColorActions", "KeyboardInputActions", "InputBoxActions", "InputAsciiActions"],
 			{
 				"gotAsciiCode": self.keyGotAscii,
 				"ok": self.okClicked,
@@ -109,50 +109,7 @@ class VirtualKeyBoard(Screen):
 				"red": self.backClicked,
 				"green": self.ok,
 				"yellow": self.switchLang,
-				"blue": self.shiftClicked,
 				"deleteBackward": self.backClicked,
-				"key_minus": self.key_minus,
-				"key_equal": self.key_equal,
-				"key_backspace": self.key_backspace,
-				"key_q": self.key_q,
-				"key_w": self.key_w,
-				"key_e": self.key_e,
-				"key_r": self.key_r,
-				"key_t": self.key_t,
-				"key_y": self.key_y,
-				"key_u": self.key_u,
-				"key_i": self.key_i,
-				"key_o": self.key_o,
-				"key_p": self.key_p,
-				"key_leftbrace": self.key_leftbrace,
-				"key_enter": self.key_enter,
-				"key_a": self.key_a,
-				"key_s": self.key_s,
-				"key_d": self.key_d,
-				"key_f": self.key_f,
-				"key_g": self.key_g,
-				"key_h": self.key_h,
-				"key_j": self.key_j,
-				"key_k": self.key_k,
-				"key_l": self.key_l,
-				"key_semicolon": self.key_semicolon,
-				"key_apostrophe": self.key_apostrophe,
-				"key_backslash": self.key_backslash,
-				"key_leftshift": self.key_leftshift,
-				"key_z": self.key_z,
-				"key_x": self.key_x,
-				"key_c": self.key_c,
-				"key_v": self.key_v,
-				"key_b": self.key_b,
-				"key_n": self.key_n,
-				"key_m": self.key_m,
-				"key_comma": self.key_comma,
-				"key_dot": self.key_dot,
-				"key_slash": self.key_slash,
-				"key_rightshift": self.key_rightshift,
-				"key_space": self.key_space,
-				"key_capslock": self.key_capslock,
-				"key_esc": self.key_esc,
 				"back": self.exit				
 			}, -2)
 		self.setLang()
@@ -168,88 +125,88 @@ class VirtualKeyBoard(Screen):
 		if self.lang == 'de_DE':
 			self.keys_list = [
 				[u"EXIT", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"0", u"BACKSPACE"],
-				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"ü", u"+"],
-				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"ö", u"ä", u"#"],
+				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"Ã¼", u"+"],
+				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"Ã¶", u"Ã¤", u"#"],
 				[u"<", u"y", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"-", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"@", u"ß", u"OK"]]
+				[u"SHIFT", u"SPACE", u"@", u"ÃŸ", u"OK"]]
 			self.shiftkeys_list = [
-				[u"EXIT", u"!", u'"', u"§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Ü", u"*"],
-				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ö", u"Ä", u"'"],
+				[u"EXIT", u"!", u'"', u"Â§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Ãœ", u"*"],
+				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ã–", u"Ã„", u"'"],
 				[u">", u"Y", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
 				[u"SHIFT", u"SPACE", u"?", u"\\", u"OK"]]
 			self.nextLang = 'es_ES'
 		elif self.lang == 'es_ES':
-			#still missing keys (u"ùÙ")
+			#still missing keys (u"Ã¹Ã™")
 			self.keys_list = [
 				[u"EXIT", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"0", u"BACKSPACE"],
-				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"ú", u"+"],
-				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"ó", u"á", u"#"],
+				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"Ãº", u"+"],
+				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"Ã³", u"Ã¡", u"#"],
 				[u"<", u"y", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"-", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"@", u"Ł", u"ŕ", u"é", u"č", u"í", u"ě", u"ń", u"ň", u"OK"]]
+				[u"SHIFT", u"SPACE", u"@", u"Å", u"Å•", u"Ã©", u"Ä", u"Ã­", u"Ä›", u"Å„", u"Åˆ", u"OK"]]
 			self.shiftkeys_list = [
-				[u"EXIT", u"!", u'"', u"§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Ú", u"*"],
-				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ó", u"Á", u"'"],
+				[u"EXIT", u"!", u'"', u"Â§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Ãš", u"*"],
+				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ã“", u"Ã", u"'"],
 				[u">", u"Y", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"?", u"\\", u"Ŕ", u"É", u"Č", u"Í", u"Ě", u"Ń", u"Ň", u"OK"]]
+				[u"SHIFT", u"SPACE", u"?", u"\\", u"Å”", u"Ã‰", u"ÄŒ", u"Ã", u"Äš", u"Åƒ", u"Å‡", u"OK"]]
 			self.nextLang = 'fi_FI'
 		elif self.lang == 'fi_FI':
 			self.keys_list = [
 				[u"EXIT", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"0", u"BACKSPACE"],
-				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"é", u"+"],
-				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"ö", u"ä", u"#"],
+				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"Ã©", u"+"],
+				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"Ã¶", u"Ã¤", u"#"],
 				[u"<", u"y", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"-", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"@", u"ß", u"ĺ", u"OK"]]
+				[u"SHIFT", u"SPACE", u"@", u"ÃŸ", u"Äº", u"OK"]]
 			self.shiftkeys_list = [
-				[u"EXIT", u"!", u'"', u"§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"É", u"*"],
-				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ö", u"Ä", u"'"],
+				[u"EXIT", u"!", u'"', u"Â§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Ã‰", u"*"],
+				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ã–", u"Ã„", u"'"],
 				[u">", u"Y", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"?", u"\\", u"Ĺ", u"OK"]]
+				[u"SHIFT", u"SPACE", u"?", u"\\", u"Ä¹", u"OK"]]
 			self.nextLang = 'sv_SE'
 		elif self.lang == 'sv_SE':
 			self.keys_list = [
 				[u"EXIT", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"0", u"BACKSPACE"],
-				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"é", u"+"],
-				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"ö", u"ä", u"#"],
+				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"Ã©", u"+"],
+				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"Ã¶", u"Ã¤", u"#"],
 				[u"<", u"y", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"-", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"@", u"ß", u"ĺ", u"OK"]]
+				[u"SHIFT", u"SPACE", u"@", u"ÃŸ", u"Äº", u"OK"]]
 			self.shiftkeys_list = [
-				[u"EXIT", u"!", u'"', u"§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"É", u"*"],
-				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ö", u"Ä", u"'"],
+				[u"EXIT", u"!", u'"', u"Â§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Ã‰", u"*"],
+				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Ã–", u"Ã„", u"'"],
 				[u">", u"Y", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"?", u"\\", u"Ĺ", u"OK"]]
+				[u"SHIFT", u"SPACE", u"?", u"\\", u"Ä¹", u"OK"]]
 			self.nextLang = 'sk_SK'
 		elif self.lang =='sk_SK':
 			self.keys_list = [
 				[u"EXIT", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"0", u"BACKSPACE"],
-				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"ú", u"+"],
-				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"ľ", u"@", u"#"],
+				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"Ãº", u"+"],
+				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"Ä¾", u"@", u"#"],
 				[u"<", u"y", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"-", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"š", u"č", u"ž", u"ý", u"á", u"í", u"é", u"OK"]]
+				[u"SHIFT", u"SPACE", u"Å¡", u"Ä", u"Å¾", u"Ã½", u"Ã¡", u"Ã­", u"Ã©", u"OK"]]
 			self.shiftkeys_list = [
-				[u"EXIT", u"!", u'"', u"§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"ť", u"*"],
-				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"ň", u"ď", u"'"],
-				[u"Á", u"É", u"Ď", u"Í", u"Ý", u"Ó", u"Ú", u"Ž", u"Š", u"Č", u"Ť", u"Ň"],
+				[u"EXIT", u"!", u'"', u"Â§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Å¥", u"*"],
+				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Åˆ", u"Ä", u"'"],
+				[u"Ã", u"Ã‰", u"ÄŽ", u"Ã", u"Ã", u"Ã“", u"Ãš", u"Å½", u"Å ", u"ÄŒ", u"Å¤", u"Å‡"],
 				[u">", u"Y", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"?", u"\\", u"ä", u"ö", u"ü", u"ô", u"ŕ", u"ĺ", u"OK"]]
+				[u"SHIFT", u"SPACE", u"?", u"\\", u"Ã¤", u"Ã¶", u"Ã¼", u"Ã´", u"Å•", u"Äº", u"OK"]]
 			self.nextLang = 'cs_CZ'
 		elif self.lang == 'cs_CZ':
 			self.keys_list = [
 				[u"EXIT", u"1", u"2", u"3", u"4", u"5", u"6", u"7", u"8", u"9", u"0", u"BACKSPACE"],
-				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"ú", u"+"],
-				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"ů", u"@", u"#"],
+				[u"q", u"w", u"e", u"r", u"t", u"z", u"u", u"i", u"o", u"p", u"Ãº", u"+"],
+				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u"Å¯", u"@", u"#"],
 				[u"<", u"y", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"-", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"ě", u"š", u"č", u"ř", u"ž", u"ý", u"á", u"í", u"é", u"OK"]]
+				[u"SHIFT", u"SPACE", u"Ä›", u"Å¡", u"Ä", u"Å™", u"Å¾", u"Ã½", u"Ã¡", u"Ã­", u"Ã©", u"OK"]]
 			self.shiftkeys_list = [
-				[u"EXIT", u"!", u'"', u"§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"ť", u"*"],
-				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"ň", u"ď", u"'"],
+				[u"EXIT", u"!", u'"', u"Â§", u"$", u"%", u"&", u"/", u"(", u")", u"=", u"BACKSPACE"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Z", u"U", u"I", u"O", u"P", u"Å¥", u"*"],
+				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"Åˆ", u"Ä", u"'"],
 				[u">", u"Y", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"?", u"\\", u"Č", u"Ř", u"Š", u"Ž", u"Ú", u"Á", u"É", u"OK"]]
+				[u"SHIFT", u"SPACE", u"?", u"\\", u"ÄŒ", u"Å˜", u"Å ", u"Å½", u"Ãš", u"Ã", u"Ã‰", u"OK"]]
 			self.nextLang = 'en_EN'
 		else:
 			self.keys_list = [
@@ -291,13 +248,6 @@ class VirtualKeyBoard(Screen):
 		
 		self["list"].setList(list)
 	
-	def shiftClicked(self):
-		if self.shiftMode:
-			self.shiftMode = False
-		else:
-			self.shiftMode = True
-		self.buildVirtualKeyBoard(self.selectedKey)
-			
 	def backClicked(self):
 		self.text = self.text[:-1]
 		self["text"].setText(self.text.encode("utf-8"))
@@ -354,182 +304,6 @@ class VirtualKeyBoard(Screen):
 		else:
 			self.text += text
 			self["text"].setText(self.text.encode("utf-8"))
-
-	def key_backspace(self):
-		self.selectedKey = 11
-		self.okClicked()
-
-	def key_minus(self):
-		self.selectedKey = 22
-		self.okClicked()
-
-	def key_equal(self):
-		self.selectedKey = 23
-		self.okClicked()
-
-	def key_q(self):
-		self.selectedKey = 12
-		self.okClicked()
-
-	def key_w(self):
-		self.selectedKey = 13
-		self.okClicked()
-
-	def key_e(self):
-		self.selectedKey = 14
-		self.okClicked()
-
-	def key_r(self):
-		self.selectedKey = 15
-		self.okClicked()
-
-	def key_t(self):
-		self.selectedKey = 16
-		self.okClicked()
-
-	def key_y(self):
-		self.selectedKey = 17
-		self.okClicked()
-
-	def key_u(self):
-		self.selectedKey = 18
-		self.okClicked()
-
-	def key_i(self):
-		self.selectedKey = 19
-		self.okClicked()
-
-	def key_o(self):
-		self.selectedKey = 20
-		self.okClicked()
-
-	def key_p(self):
-		self.selectedKey = 21
-		self.okClicked()
-
-	def key_leftbrace(self):
-		self.selectedKey = 36
-		self.okClicked()
-
-	def key_rightbrace(self):
-		self.selectedKey = 35
-		self.okClicked()
-
-	def key_enter(self):
-		self.selectedKey = 23
-		self.okClicked()
-
-	def key_a(self):
-		self.selectedKey = 24
-		self.okClicked()
-
-	def key_s(self):
-		self.selectedKey = 25
-		self.okClicked()
-
-	def key_d(self):
-		self.selectedKey = 26
-		self.okClicked()
-
-	def key_f(self):
-		self.selectedKey = 27
-		self.okClicked()
-
-	def key_g(self):
-		self.selectedKey = 28
-		self.okClicked()
-
-	def key_h(self):
-		self.selectedKey = 29
-		self.okClicked()
-
-	def key_j(self):
-		self.selectedKey = 30
-		self.okClicked()
-
-	def key_k(self):
-		self.selectedKey = 31
-		self.okClicked()
-
-	def key_l(self):
-		self.selectedKey = 32
-		self.okClicked()
-
-	def key_semicolon(self):
-		self.selectedKey = 33
-		self.okClicked()
-
-	def key_apostrophe(self):
-		self.selectedKey = 34
-		self.okClicked()
-
-	def key_grave(self):
-		self.selectedKey = 35
-		self.okClicked()
-
-	def key_leftshift(self):
-		self.selectedKey = 48
-		self.okClicked()
-
-	def key_backslash(self):
-		self.selectedKey = 35
-		self.okClicked()
-
-	def key_z(self):
-		self.selectedKey = 37
-		self.okClicked()
-
-	def key_x(self):
-		self.selectedKey = 38
-		self.okClicked()
-
-	def key_c(self):
-		self.selectedKey = 39
-		self.okClicked()
-
-	def key_v(self):
-		self.selectedKey = 40
-		self.okClicked()
-
-	def key_b(self):
-		self.selectedKey = 41
-		self.okClicked()
-
-	def key_n(self):
-		self.selectedKey = 42
-		self.okClicked()
-
-	def key_m(self):
-		self.selectedKey = 43
-		self.okClicked()
-
-	def key_comma(self):
-		self.selectedKey = 44
-		self.okClicked()
-
-	def key_dot(self):
-		self.selectedKey = 45
-		self.okClicked()
-
-	def key_slash(self):
-		self.selectedKey = 46
-		self.okClicked()
-
-	def key_rightshift(self):
-		self.selectedKey = 48
-		self.okClicked()
-
-	def key_space(self):
-		self.selectedKey = 49
-		self.okClicked()
-
-	def key_capslock(self):
-		self.selectedKey = 52
-		self.okClicked()
-
-	def key_esc(self):
-		self.selectedKey = 50
-		self.okClicked()
 
 	def ok(self):
 		self.close(self.text.encode("utf-8"))
