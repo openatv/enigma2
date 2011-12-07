b051f97cce61e946a4cf8cd10a2d1faf20127d5f
Paul In&In <darezik@gmail.com>
2011-12-07 21:26:14 +0100

    Add EPG next renderer for channel selection screen
    And small cosmetic change to the VideoSize Renderer

---
+++ lib/python/Components/Renderer/NextEpgInfo.py
@@ -0,0 +1,22 @@
+from Components.VariableText import VariableText
+from Renderer import Renderer
+from enigma import eLabel, eEPGCache
+from time import localtime
+
+class NextEpgInfo(Renderer, VariableText):
+	def __init__(self):
+		Renderer.__init__(self)
+		VariableText.__init__(self)
+		self.epgcache = eEPGCache.getInstance()
+	GUI_WIDGET = eLabel
+	
+	def changed(self, what):
+		self.text = ""
+		reference = self.source.service
+		info = reference and self.source.info
+		if info is None:	
+			return
+		nextEvent = self.epgcache.lookupEvent(['IBDCTSERNX', (reference.toString(), 1, -1)])
+		if nextEvent:
+			if nextEvent[0][4]:
+				self.text = nextEvent[0][4]
