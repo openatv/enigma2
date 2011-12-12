4726189f39913fca08003b44d5e63e88e49250ff
Andy Blackburn <andyblac@o2.co.uk>
2011-12-12 01:59:37 +0000

    EventName: Add EPG next for channel selection screen, this is better than using a single render (NextEpgInfo), as this also allows Next Description to use used.
    <eLabel text="NEXT:" position="695,570" size="70,25" font="Bold;24" foregroundColor="infobar_service_frgrnd" backgroundColor="black" noWrap="1" zPosition="2" transparent="0" valign="top" />
    <widget source="ServiceEvent" render="Label" position="775,570" size="330,25" font="Bold;24" foregroundColor="infobar_service_frgrnd" backgroundColor="black" noWrap="1" zPosition="2" transparent="0" valign="top">
    	<convert type="EventName">NextName</convert>
    </widget>
    <widget source="ServiceEvent" render="Label" position="695,600" size="575,75" font="Regular;22" backgroundColor="black" foregroundColor="window-fg" transparent="0" valign="top" zPosition="2">
    	<convert type="EventName">NextDescription</convert>
    </widget>

--- lib/python/Components/Converter/EventName.py
+++ lib/python/Components/Converter/EventName.py
@@ -1,5 +1,6 @@
 from Components.Converter.Converter import Converter
 from Components.Element import cached
+from enigma import eEPGCache
 
 class EventName(Converter, object):
 	NAME = 0
@@ -7,9 +8,12 @@ class EventName(Converter, object):
 	EXTENDED_DESCRIPTION = 2
 	FULL_DESCRIPTION = 3
 	ID = 4
+	NEXT_NAME = 5
+	NEXT_DESCRIPTION = 6
 	
 	def __init__(self, type):
 		Converter.__init__(self, type)
+		self.epgcache = eEPGCache.getInstance()
 		if type == "Description":
 			self.type = self.SHORT_DESCRIPTION
 		elif type == "ExtendedDescription":
@@ -18,6 +22,10 @@ class EventName(Converter, object):
 			self.type = self.FULL_DESCRIPTION
 		elif type == "ID":
 			self.type = self.ID
+		elif type == "NextName":
+			self.type = self.NEXT_NAME
+		elif type == "NextDescription":
+			self.type = self.NEXT_DESCRIPTION
 		else:
 			self.type = self.NAME
 
@@ -41,5 +49,16 @@ class EventName(Converter, object):
 			return description + extended
 		elif self.type == self.ID:
 			return str(event.getEventId())
+		elif self.type == self.NEXT_NAME or self.type == self.NEXT_DESCRIPTION:
+			reference = self.source.service
+			info = reference and self.source.info
+			if info is None:	
+				return
+			nextEvent = self.epgcache.lookupEvent(['IBDCTSERNX', (reference.toString(), 1, -1)])
+			if nextEvent:
+				if self.type == self.NEXT_NAME and nextEvent[0][4]:
+					return nextEvent[0][4]
+				elif self.type == self.NEXT_DESCRIPTION and nextEvent[0][6]:
+					return nextEvent[0][6]
 		
 	text = property(getText)
