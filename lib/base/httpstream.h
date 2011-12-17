133bce8bbf67c9b615d2d494510a80252ff5b7d5
Paul In&In <darezik@gmail.com>
2011-12-17 16:04:10 +0100

    httpstream: split socket helper functions into baseclass

--- lib/base/httpstream.h
+++ lib/base/httpstream.h
@@ -4,19 +4,13 @@
 #include <string>
 #include <lib/base/ebase.h>
 #include <lib/base/itssource.h>
+#include <lib/base/socketbase.h>
 
-class eHttpStream: public iTsSource, public Object
+class eHttpStream: public iTsSource, public eSocketBase, public Object
 {
 	DECLARE_REF(eHttpStream);
 
 	int streamSocket;
-
-	ssize_t singleRead(int fd, void *buf, size_t count);
-	ssize_t timedRead(int fd, void *buf, size_t count, int initialtimeout, int interbytetimeout);
-	ssize_t readLine(int fd, char** buffer, size_t* bufsize);
-	ssize_t write(int fd, const void *buf, size_t count);
-	int select(int maxfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
-	int connect(const char *hostname, int port, int timeoutsec);
 
 	/* iTsSource */
 	off_t lseek(off_t offset, int whence);
