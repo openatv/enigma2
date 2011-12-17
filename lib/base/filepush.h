dcc618f36e9124fdb4cfbb2104b367265dd5619c
Paul In&In <darezik@gmail.com>
2011-12-17 16:02:30 +0100

    filepush: Refactor into recording and playback classes
    They do different things, so should be different classes.
    The playback is unchanged for now, the recorder is much, much
    simpler now.

--- lib/base/filepush.h
+++ lib/base/filepush.h
@@ -17,7 +17,6 @@ public:
 
 class eFilePushThread: public eThread, public Object
 {
-	int prio_class, prio;
 public:
 	eFilePushThread(int prio_class=IOPRIO_CLASS_BE, int prio_level=0, int blocksize=188, size_t buffersize=188*1024);
 	~eFilePushThread();
@@ -43,14 +42,13 @@ public:
 	enum { evtEOF, evtReadError, evtWriteError, evtUser };
 	Signal1<void,int> m_event;
 
-	void installSigUSR1Handler();
-	void before_set_thread_alive();
-
 		/* you can send private events if you want */
 	void sendEvent(int evt);
 protected:
 	virtual int filterRecordData(const unsigned char *data, int len, size_t &current_span_remaining);
 private:
+	int prio_class;
+	int prio;
 	iFilePushScatterGather *m_sg;
 	int m_stop;
 	int m_buf_start, m_buf_end, m_filter_end;
@@ -66,6 +64,35 @@ private:
 
 	eFixedMessagePump<int> m_messagepump;
 
+	void recvEvent(const int &evt);
+};
+
+class eFilePushThreadRecorder: public eThread, public Object
+{
+public:
+	eFilePushThreadRecorder(int prio_class=IOPRIO_CLASS_BE, int prio_level=0, int blocksize=188, size_t buffersize=188*1024);
+	~eFilePushThreadRecorder();
+	void thread();
+	void stop();
+	void start(int sourcefd, int destfd);
+
+	enum { evtEOF, evtReadError, evtWriteError, evtUser };
+	Signal1<void,int> m_event;
+
+	void sendEvent(int evt);
+protected:
+	virtual void filterRecordData(const unsigned char *data, int len) = 0;
+private:
+	int prio_class;
+	int prio;
+	int m_stop;
+	int m_fd_dest;
+	int m_fd_source;
+	int m_blocksize;
+	size_t m_buffersize;
+	unsigned char* m_buffer;
+	off_t m_current_position;
+	eFixedMessagePump<int> m_messagepump;
 	void recvEvent(const int &evt);
 };
 
