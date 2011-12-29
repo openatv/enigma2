62b7da475bbe84f5f4106d2e496da2856beab49a
black_64 <mail>
2011-12-29 08:31:56 +0100

    add install softcams

--- lib/python/Plugins/Extensions/Aafpanel/SoftcamPanel.py
+++ lib/python/Plugins/Extensions/Aafpanel/SoftcamPanel.py
@@ -4,6 +4,12 @@ from Components.MenuList import MenuList
 from Components.Label import Label
 from Screens.Screen import Screen
 from Components.ActionMap import ActionMap
+from Components.Sources.List import List
+from Components.Sources.StaticText import StaticText
+from Tools.LoadPixmap import LoadPixmap
+from Tools.Directories import resolveFilename, SCOPE_CURRENT_PLUGIN, SCOPE_CURRENT_SKIN
+from Screens.MessageBox import MessageBox
+from Screens.Console import Console
 from enigma import *
 import os
 
@@ -70,6 +76,7 @@ class SoftcamPanel(Screen):
 
 		self.skin = SOFTCAM_SKIN
 		self.onShown.append(self.setWindowTitle)
+		self.partyfeed = None
 
 		self.mlist = []
 		self["Mlist"] = MenuList(self.mlist)
@@ -77,7 +84,11 @@ class SoftcamPanel(Screen):
 		self["key_green"] = Label(_("Restart"))
 		self["key_red"] = Label(_("Stop"))
 		self["key_yellow"] = Label(_("Refresh"))
-		self["key_blue"]= Label(_("Exit"))
+		self.partyfeed = os.path.exists("/etc/opkg/3rd-party-feed.conf")
+		if self.partyfeed:
+			self["key_blue"]= Label(_("Install"))
+		else:
+			self["key_blue"]= Label(_("Exit"))
 		self["ecminfo"] = Label(_("No ECM info"))
 		self["camcount"] = Label("(0/0)")
 		self["actifcam"] = Label(_("no CAM active"))
@@ -159,7 +170,7 @@ class SoftcamPanel(Screen):
 			"up": self.up,
 			"down": self.down,
 			"ok": self.ok,
-			"blue": self.Exit,
+			"blue": self.Blue,
 			"red": self.Red,
 			"green": self.Green,
 			"yellow": self.Yellow,
@@ -404,6 +415,16 @@ class SoftcamPanel(Screen):
 	def Exit(self):
 		self.Timer.stop()
 		self.close()
+		
+	def Blue(self):
+		if not self.partyfeed:
+			self.exit()
+		else:
+			self.Timer.stop()
+			self.session.openWithCallback(self.ShowSoftcamCallback, ShowSoftcamPackages)
+
+	def ShowSoftcamCallback(self):
+		self.Timer.start(2000, True)
 
 	def ok(self):
 		#// Exit Softcam when pressing the OK button
@@ -518,3 +539,164 @@ class SoftcamPanel(Screen):
 					if k[1][:-1] == "y":
 						return True
 						break
+
+class ShowSoftcamPackages(Screen):
+	skin = """
+		<screen name="ShowSoftcamPackages" position="center,center" size="630,500" title="Install Softcams" >
+			<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on" />
+			<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on" />
+			<widget source="key_red" render="Label" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
+			<widget source="key_green" render="Label" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+			<widget source="key_ok" render="Label" position="240,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+			<widget source="list" render="Listbox" position="5,50" size="620,420" scrollbarMode="showOnDemand">
+				<convert type="TemplatedMultiContent">
+					{"template": [
+							MultiContentEntryText(pos = (5, 1), size = (540, 28), font=0, flags = RT_HALIGN_LEFT, text = 0), # index 0 is the name
+							MultiContentEntryText(pos = (5, 26), size = (540, 20), font=1, flags = RT_HALIGN_LEFT, text = 2), # index 2 is the description
+							MultiContentEntryPixmapAlphaTest(pos = (545, 2), size = (48, 48), png = 4), # index 4 is the status pixmap
+							MultiContentEntryPixmapAlphaTest(pos = (5, 50), size = (510, 2), png = 5), # index 4 is the div pixmap
+						],
+					"fonts": [gFont("Regular", 22),gFont("Regular", 14)],
+					"itemHeight": 52
+					}
+				</convert>
+			</widget>
+		</screen>"""
+	
+	def __init__(self, session, args = None):
+		Screen.__init__(self, session)
+		self.session = session
+		
+		self["actions"] = ActionMap(["OkCancelActions", "DirectionActions", "ColorActions"],
+		{
+			"red": self.exit,
+			"ok": self.go,
+			"cancel": self.exit,
+			"green": self.startupdateList,
+		}, -1)
+		
+		self.list = []
+		self.statuslist = []
+		self["list"] = List(self.list)
+		self["key_red"] = StaticText(_("Close"))
+		self["key_green"] = StaticText(_("Reload"))
+		self["key_ok"] = StaticText(_("Install"))
+
+		self.oktext = _("\nPress OK on your remote control to continue.")
+		self.onShown.append(self.setWindowTitle)
+		self.setStatus('list')
+		self.Timer1 = eTimer()
+		self.Timer1.callback.append(self.rebuildList)
+		self.Timer1.start(1000, True)
+		self.Timer2 = eTimer()
+		self.Timer2.callback.append(self.updateList)
+
+	def go(self, returnValue = None):
+		cur = self["list"].getCurrent()
+		if cur:
+			status = cur[3]
+			self.package = cur[2]
+			if status == "installable":
+				self.session.openWithCallback(self.runInstall, MessageBox, _("Do you want to install the package:\n") + self.package + "\n" + self.oktext)
+
+	def runInstall(self, result):
+		if result:
+			self.session.openWithCallback(self.runInstallCont, Console, cmdlist = ['opkg install ' + self.package], closeOnSuccess = True)
+
+	def runInstallCont(self):
+			ret = command('opkg list-installed | grep ' + self.package + ' | cut -d " " -f1')
+
+			if ret != self.package:
+				self.session.open(MessageBox, _("Install Failed !!"), MessageBox.TYPE_ERROR, timeout = 10)
+			else:
+				self.session.open(MessageBox, _("Install Finished."), MessageBox.TYPE_INFO, timeout = 10)
+				self.setStatus('list')
+				self.Timer1.start(1000, True)
+
+	def UpgradeReboot(self, result):
+		if result is None:
+			return
+		
+	def exit(self):
+		self.close()
+			
+	def setWindowTitle(self):
+		self.setTitle(_("Install Softcams"))
+
+	def setStatus(self,status = None):
+		if status:
+			self.statuslist = []
+			divpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, "skin_default/div-h.png"))
+			if status == 'update':
+				statuspng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_PLUGIN, "Extensions/Aafpanel/pics/upgrade.png"))
+				self.statuslist.append(( _("Package list update"), '', _("Trying to download a new updatelist. Please wait..." ),'', statuspng, divpng ))
+				self['list'].setList(self.statuslist)
+			if status == 'list':
+				statuspng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_PLUGIN, "Extensions/Aafpanel/pics/upgrade.png"))
+				self.statuslist.append(( _("Package list"), '', _("Getting Softcam list. Please wait..." ),'', statuspng, divpng ))
+				self['list'].setList(self.statuslist)
+			elif status == 'error':
+				statuspng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_PLUGIN, "Extensions/Aafpanel/pics/remove.png"))
+				self.statuslist.append(( _("Error"), '', _("There was an error downloading the updatelist. Please try again." ),'', statuspng, divpng ))
+				self['list'].setList(self.statuslist)				
+
+	def startupdateList(self):
+		self.setStatus('update')
+		self.Timer2.start(1000, True)
+
+	def updateList(self):
+		self.container = eConsoleAppContainer()
+		self.container.appClosed.append(self.doneupdateList)
+		self.setStatus('list')
+		self.container.execute('opkg update')
+
+	def doneupdateList(self, answer):
+		print answer
+		self.container.appClosed.remove(self.doneupdateList)
+		self.Timer1.start(1000, True)
+
+	def rebuildList(self):
+		self.list = []
+		self.Flist = []
+		self.Elist = []
+		t = command('opkg list | grep "enigma2-plugin-softcams-"')
+		self.Flist = t.split('\n')
+		tt = command('opkg list-installed | grep "enigma2-plugin-softcams-"')
+		self.Elist = tt.split('\n')
+
+		if len(self.Flist) > 0:
+			self.buildPacketList()
+		else:
+			self.setStatus('error')
+
+	def buildEntryComponent(self, name, version, description, state):
+		divpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_SKIN, "skin_default/div-h.png"))
+		if not description:
+			description = ""
+		installedpng = LoadPixmap(cached=True, path=resolveFilename(SCOPE_CURRENT_PLUGIN, "Extensions/Aafpanel/pics/installed.png"))
+		return((name, version, _(description), state, installedpng, divpng))
+
+	def buildPacketList(self):
+		fetchedList = self.Flist
+		excludeList = self.Elist
+
+		if len(fetchedList) > 0:
+			for x in fetchedList:
+				x_installed = False
+				Fx = x.split(' - ')
+				try:
+					if Fx[0].find('-softcams-') > -1:
+						for exc in excludeList:
+							Ex = exc.split(' - ')
+							if Fx[0] == Ex[0]:
+								x_installed = True
+								break
+						if x_installed == False:
+							self.list.append(self.buildEntryComponent(Fx[2], Fx[1], Fx[0], "installable"))
+				except:
+					pass
+
+			self['list'].setList(self.list)
+	
+		else:
+			self.setStatus('error')
