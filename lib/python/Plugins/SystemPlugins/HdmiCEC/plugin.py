da14975a43ee9d57fbc23b8c4d7b791c0b6c5478
pieterg <pieterg@users.sourceforge.net>
2012-01-02 21:35:21 +0100

    HdmiCEC: remove control_tv_deepstandby setting
    1. setting name was ambiguous; tv's don't have deepstandby mode
    2. the way the setting was used, suggested we would need similar
    settings configuring how to control an AVR when the stb goes in
    and out of deep standby
    
    Instead, just use the 'handle_deepstandby_events' setting.
    If we handle deepstandby events, the behaviour is the same as with
    normal standby events, no need to make things more complicated.

--- lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
+++ lib/python/Plugins/SystemPlugins/HdmiCEC/plugin.py
@@ -49,9 +49,8 @@ class HdmiCECSetupScreen(Screen, ConfigListScreen):
 
 		self.list.append(getConfigListEntry(_("Enabled"), config.hdmicec.enabled))
 		self.list.append(getConfigListEntry(_("Put TV in standby"), config.hdmicec.control_tv_standby))
-		self.list.append(getConfigListEntry(_("Put TV in standby when deepstandby"), config.hdmicec.control_tv_deepstandby))
 		self.list.append(getConfigListEntry(_("Wakeup TV from standby"), config.hdmicec.control_tv_wakeup))
-		self.list.append(getConfigListEntry(_("Wakeup TV after deepstandby"), config.hdmicec.handle_deepstandby_events))
+		self.list.append(getConfigListEntry(_("Regard deep standby as standby"), config.hdmicec.handle_deepstandby_events))
 		self.list.append(getConfigListEntry(_("Switch TV to correct input"), config.hdmicec.report_active_source))
 		self.list.append(getConfigListEntry(_("Use TV remote control"), config.hdmicec.report_active_menu))
 		self.list.append(getConfigListEntry(_("Handle standby from TV"), config.hdmicec.handle_tv_standby))
