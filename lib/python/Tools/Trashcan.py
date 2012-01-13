fa2b3e8238d0029397c7aa00e6014101dbce5680
Paul In&In <darezik@gmail.com>
2012-01-13 20:15:17 +0000

    TrashCan: add multiple trashcan support.
    the trash can has been updated to allow each device to have it own trashcan,
    
    also moving and copying file/folders between devices is now supported.
    
    also moving folders and it's content to trashcan is now supported.
    
    when in a trashcan you can now permanently delete a file/folder by press pressing the delete button.
    
    Signed-off-by: Andy Blackburn <andyblac@o2.co.uk>

--- lib/python/Tools/Trashcan.py
+++ lib/python/Tools/Trashcan.py
@@ -1,5 +1,8 @@
+import Components.Task
+from twisted.internet import reactor, threads, task
 import time
 import os
+import fnmatch
 
 try:
 	import enigma
@@ -9,12 +12,24 @@ except:
 
 from Directories import resolveFilename, SCOPE_HDD
 
-def getTrashFolder():
+def getTrashFolder(path=None):
 	# Returns trash folder without symlinks
-	return os.path.realpath(os.path.join(resolveFilename(SCOPE_HDD), ".Trash"))
+	if path is None:
+		print 'path is none'
+	else:
+		trash_path = os.path.split(path)
+		if trash_path[0].endswith('movie'):
+			trashcan = trash_path[0]
+		else:
+			trash_path = os.path.split(trash_path[0])
+			if trash_path[0].endswith('net'):
+				trashcan = os.path.join(trash_path[0],trash_path[1])
+			else:
+				trashcan = trash_path[0]
+	return os.path.realpath(os.path.join(trashcan, ".Trash"))
 
-def createTrashFolder():
-	trash = getTrashFolder()
+def createTrashFolder(path=None):
+	trash = getTrashFolder(path)
 	if not os.path.isdir(trash):
 		os.mkdir(trash)
 	return trash
@@ -53,53 +68,14 @@ class Trashcan:
 			print "[Trashcan] Weirdness:", e
 
 def clean(ctimeLimit, reserveBytes):
-		# Remove expired items from trash, and attempt to have
-		# reserveBytes of free disk space. 
-		trash = getTrashFolder()
-		if not os.path.isdir(trash):
-			print "[Trashcan] No trash.", trash
-			return 0
-		diskstat = os.statvfs(trash)
-		free = diskstat.f_bfree * diskstat.f_bsize
-		bytesToRemove = reserveBytes - free 
-		candidates = []
-		print "[Trashcan] bytesToRemove", bytesToRemove
-		size = 0
-		for root, dirs, files in os.walk(trash, topdown=False):
-			for name in files:
-				try:
-					fn = os.path.join(root, name)
-					st = os.stat(fn)
-					if st.st_ctime < ctimeLimit:
-						print "[Trashcan] Too old:", name, st.st_ctime
-						enigma.eBackgroundFileEraser.getInstance().erase(fn)
-						bytesToRemove -= st.st_size
-					else:
-						candidates.append((st.st_ctime, fn, st.st_size))
-						size += st.st_size
-				except Exception, e:
-					print "[Trashcan] Failed to stat %s:"% name, e 
-			# Remove empty directories if possible
-			for name in dirs:
-				try:
-					os.rmdir(os.path.join(root, name))
-				except:
-					pass
-		candidates.sort()
-		# Now we have a list of ctime, candidates, size. Sorted by ctime (=deletion time)
-		print "[Trashcan] Bytes to remove:", bytesToRemove
-		print "[Trashcan] Size now:", size
-		for st_ctime, fn, st_size in candidates:
-			if bytesToRemove < 0:
-				break
-			enigma.eBackgroundFileEraser.getInstance().erase(fn)
-			bytesToRemove -= st_size
-			size -= st_size
-		print "[Trashcan] Size now:", size
-		 
+	name = _("Cleaing Trashes")
+	job = Components.Task.Job(name)
+	task = CleanTrashTask(job, name)
+	task.openFiles(ctimeLimit, reserveBytes)
+	Components.Task.job_manager.AddJob(job)
 		
-def cleanAll():
-		trash = getTrashFolder()
+def cleanAll(path=None):
+		trash = getTrashFolder(path)
 		if not os.path.isdir(trash):
 			print "[Trashcan] No trash.", trash
 			return 0
@@ -163,3 +139,54 @@ if __name__ == '__main__':
 	cleanAll()
 	instance.destroy()
 	s.destroy()
+
+class CleanTrashTask(Components.Task.PythonTask):
+	def openFiles(self, ctimeLimit, reserveBytes):
+		self.ctimeLimit = ctimeLimit
+		self.reserveBytes = reserveBytes
+
+	def work(self):
+		matches = []
+		for root, dirnames, filenames in os.walk('/media/'):
+		  for filename in fnmatch.filter(dirnames, '.Trash'):
+			  matches.append(os.path.join(root, filename))
+
+		for trashfolder in matches:
+			trash = getTrashFolder(trashfolder)
+			diskstat = os.statvfs(trash)
+			free = diskstat.f_bfree * diskstat.f_bsize
+			bytesToRemove = self.reserveBytes - free 
+			candidates = []
+			print "[Trashcan] " + str(trashfolder) + ": bytesToRemove",bytesToRemove
+			size = 0
+			for root, dirs, files in os.walk(trash, topdown=False):
+				for name in files:
+					try:
+						fn = os.path.join(root, name)
+						st = os.stat(fn)
+						if st.st_ctime < self.ctimeLimit:
+							print "[Trashcan] " + str(trashfolder) + ": Too old:",name, st.st_ctime
+							enigma.eBackgroundFileEraser.getInstance().erase(fn)
+							bytesToRemove -= st.st_size
+						else:
+							candidates.append((st.st_ctime, fn, st.st_size))
+							size += st.st_size
+					except Exception, e:
+						print "[Trashcan] Failed to stat %s:"% name, e 
+				# Remove empty directories if possible
+				for name in dirs:
+					try:
+						os.rmdir(os.path.join(root, name))
+					except:
+						pass
+			candidates.sort()
+			# Now we have a list of ctime, candidates, size. Sorted by ctime (=deletion time)
+			print "[Trashcan] " + str(trashfolder) + ": Bytes to remove:",bytesToRemove
+			print "[Trashcan] " + str(trashfolder) + ": Size now:",size
+			for st_ctime, fn, st_size in candidates:
+				if bytesToRemove < 0:
+					break
+				enigma.eBackgroundFileEraser.getInstance().erase(fn)
+				bytesToRemove -= st_size
+				size -= st_size
+			print "[Trashcan] " + str(trashfolder) + ": Size now:",size
