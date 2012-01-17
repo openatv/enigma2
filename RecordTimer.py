04192f8294de980b4e0f55ea7899d0b0d9ffb448
Paul In&In <darezik@gmail.com>
2012-01-17 21:52:45 +0000

    RecordTimer: fix always not finding freespace.
    Signed-off-by: Andy Blackburn <andyblac@o2.co.uk>

--- RecordTimer.py
+++ RecordTimer.py
@@ -188,6 +188,8 @@ class RecordTimerEntry(timer.TimerEntry, object):
 			return True
 		else:
 			self.calculateFilename()
+			if not self.freespace():
+				return False
 			rec_ref = self.service_ref and self.service_ref.ref
 			if rec_ref and rec_ref.flags & eServiceReference.isGroup:
 				rec_ref = getBestPlayableServiceReference(rec_ref, eServiceReference())
@@ -266,7 +268,13 @@ class RecordTimerEntry(timer.TimerEntry, object):
 				self.next_activation = self.begin
 				self.backoff = 0
 				return True
-
+			if not self.freespace():
+				Notifications.AddPopup(text = _("Write error while recording. Disk full?\n%s") % self.name, type = MessageBox.TYPE_ERROR, timeout = 5, id = "DiskFullMessage")
+				self.state = 4
+# 				self.disabled = True
+				self.failed = True
+				return False
+				
 			self.log(7, "prepare failed")
 			if self.first_try_prepare:
 				self.first_try_prepare = False
@@ -284,9 +292,13 @@ class RecordTimerEntry(timer.TimerEntry, object):
 				else:
 					self.log(8, "currently no service running... so we dont need to stop it")
 			return False
+
 		elif next_state == self.StateRunning:
 			# if this timer has been cancelled, just go to "end" state.
 			if self.cancelled:
+				return True
+
+			if self.failed:
 				return True
 
 			if self.justplay:
@@ -310,8 +322,8 @@ class RecordTimerEntry(timer.TimerEntry, object):
 					# retry
 					self.begin = time() + self.backoff
 					return False
-
 				return True
+
 		elif next_state == self.StateEnded:
 			old_end = self.end
 			if self.setAutoincreaseEnd():
@@ -479,24 +491,10 @@ class RecordTimer(timer.Timer):
 		except IOError:
 			print "unable to load timers from file!"
 
-	def freespace(self):
-		import os
-		s = os.statvfs(self.Filename)
-		if (s.f_bavail * s.f_bsize) / 1000000 < 1024:
-			print "[TIMER] Not enough free space to record:"
-			return False
-		else:
-			return True
-
 	def doActivate(self, w):
 		# when activating a timer which has already passed,
 		# simply abort the timer. don't run trough all the stages.
-		if not self.freespace():
-			Notifications.AddPopup(text = _("Write error while recording. Disk full?\n"), type = MessageBox.TYPE_ERROR, timeout = 5, id = "DiskFullMessage")
-			w.state = RecordTimerEntry.StateFailed
-			w.disabled = True
-			w.failed = True
-		elif w.shouldSkip():
+		if w.shouldSkip():
 			w.state = RecordTimerEntry.StateEnded
 		else:
 			# when active returns true, this means "accepted".
