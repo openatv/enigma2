ea84153dd5d6e9ddcce58fcf43998c86a970ecdd
Mike Looijmans <milo-software@users.sourceforge.net>
2012-02-11 19:15:01 +0100

    Minor changes (C++ style)

--- lib/base/itssource.h
+++ lib/base/itssource.h
@@ -19,7 +19,7 @@ public:
 	virtual off_t length()=0;
 	virtual int valid()=0;
 	virtual off_t offset() = 0;
-	int getPacketSize() { return packetSize; }
+	int getPacketSize() const { return packetSize; }
 };
 
 #endif
