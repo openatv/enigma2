ded6ce4f3026978f4ff50bb8fc26d1a0ebe74673
Littlesat <Littlesat99@yahoo.com>
2012-02-24 11:27:51 +0100

    Usage of a simple Messagebox for Notifications
    Add to Messagebox.py a possiblity to use a simple skin that can be used
    for notification so that the notification will not be shown anymore with
    a PIG.

--- lib/python/Tools/Notifications.py
+++ lib/python/Tools/Notifications.py
@@ -42,4 +42,4 @@ def AddPopup(text, type, timeout, id = None):
 	if id is not None:
 		RemovePopup(id)
 	print "AddPopup, id =", id
-	AddNotificationWithID(id, MessageBox, text = text, type = type, timeout = timeout, close_on_any_key = True)
+	AddNotificationWithID(id, MessageBox, text = text, type = type, timeout = timeout, close_on_any_key = True, simple = True)
