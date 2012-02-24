9f65d43f5f9677b4930838ceb5aa8619eb9de58d
Paul In&In <darezik@gmail.com>
2012-02-24 11:27:51 +0100

    Usage of a simple Messagebox for Notifications
    Add to Messagebox.py a possiblity to use a simple skin that can be used
    for notification so that the notification will not be shown anymore with
    a PIG.

--- lib/python/Screens/MessageBox.py
+++ lib/python/Screens/MessageBox.py
@@ -12,9 +12,12 @@ class MessageBox(Screen):
 	TYPE_WARNING = 2
 	TYPE_ERROR = 3
 
-	def __init__(self, session, text, type = TYPE_YESNO, timeout = -1, close_on_any_key = False, default = True, enable_input = True, msgBoxID = None, picon = None):
+	def __init__(self, session, text, type = TYPE_YESNO, timeout = -1, close_on_any_key = False, default = True, enable_input = True, msgBoxID = None, picon = None, simple = False):
 		self.type = type
 		Screen.__init__(self, session)
+
+		if simple:
+			self.skinName="MessageBoxSimple"
 		
 		self.msgBoxID = msgBoxID
 
