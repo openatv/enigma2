42a6ff934aa8052e84ae1ceed9bf9bbc5e86a33d
Andy Blackburn <andyblac@o2.co.uk>
2012-03-01 17:43:37 +0000

    MessageBox: add Title support.

--- lib/python/Screens/MessageBox.py
+++ lib/python/Screens/MessageBox.py
@@ -12,10 +12,11 @@ class MessageBox(Screen):
 	TYPE_WARNING = 2
 	TYPE_ERROR = 3
 
-	def __init__(self, session, text, type = TYPE_YESNO, timeout = -1, close_on_any_key = False, default = True, enable_input = True, msgBoxID = None, picon = None, simple = False):
+	def __init__(self, session, text, type = TYPE_YESNO, title = None, timeout = -1, close_on_any_key = False, default = True, enable_input = True, msgBoxID = None, picon = None, simple = False):
 		self.type = type
 		Screen.__init__(self, session)
 
+		self.setTitle(_(title))
 		if simple:
 			self.skinName="MessageBoxSimple"
 		
