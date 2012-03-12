fc2a478c3f437405b8c0f54e6aa67c4cfcd06669
Andy Blackburn <andyblac@o2.co.uk>
2012-03-12 10:35:48 +0000

    Time Remaining/Elapsed: tweak code to allow the 2 to be set independently of each other, so you can have Elapsed time on OSB infobar but remaining in VFD, you can all so use what is called in skin (this is default). this give the user more control other how it is displayed. you can also now set what is to be used for positive time (was always remaining time).

--- lib/python/Components/Converter/EventTime.py
+++ lib/python/Components/Converter/EventTime.py
@@ -9,15 +9,17 @@ class EventTime(Poll, Converter, object):
 	STARTTIME = 0
 	ENDTIME = 1
 	REMAINING = 2
-	PROGRESS = 3
-	DURATION = 4
-	ELAPSED = 5
-	NEXT_START_TIME = 6
-	NEXT_END_TIME = 7
-	NEXT_DURATION = 8 
-	THIRD_START_TIME = 9
-	THIRD_END_TIME = 10
-	THIRD_DURATION = 11 
+	REMAINING_VFD = 3
+	PROGRESS = 4
+	DURATION = 5
+	ELAPSED = 6
+	ELAPSED_VFD = 7
+	NEXT_START_TIME = 8
+	NEXT_END_TIME = 9
+	NEXT_DURATION = 10
+	THIRD_START_TIME = 11
+	THIRD_END_TIME = 12
+	THIRD_DURATION = 13
 
 	def __init__(self, type):
 		Converter.__init__(self, type)
@@ -27,6 +29,10 @@ class EventTime(Poll, Converter, object):
 			self.type = self.ENDTIME
 		elif type == "Remaining":
 			self.type = self.REMAINING
+			self.poll_interval = 60*1000
+			self.poll_enabled = True
+		elif type == "VFDRemaining":
+			self.type = self.REMAINING_VFD
 			self.poll_interval = 60*1000
 			self.poll_enabled = True
 		elif type == "StartTime":
@@ -39,6 +45,10 @@ class EventTime(Poll, Converter, object):
 			self.poll_enabled = True
 		elif type == "Elapsed":
 			self.type = self.ELAPSED
+			self.poll_interval = 60*1000
+			self.poll_enabled = True
+		elif type == "VFDElapsed":
+			self.type = self.ELAPSED_VFD
 			self.poll_interval = 60*1000
 			self.poll_enabled = True
 		elif type == "NextStartTime":
@@ -70,42 +80,49 @@ class EventTime(Poll, Converter, object):
 			return event.getBeginTime() + event.getDuration()
 		elif self.type == self.DURATION:
 			return event.getDuration()
-		elif self.type == self.REMAINING:
+		elif self.type == self.REMAINING or self.type == self.REMAINING_VFD or self.type == self.ELAPSED or self.type == self.ELAPSED_VFD:
 			now = int(time())
 			start_time = event.getBeginTime()
 			duration = event.getDuration()
 			end_time = start_time + duration
 			elapsed = now - start_time
 			if start_time <= now <= end_time:
-				if config.usage.swap_time_remaining_on_osd.value == "1" or config.usage.swap_time_remaining_on_vfd.value == "1":
-					return (duration, elapsed)
-				elif config.usage.swap_time_remaining_on_osd.value == "2" or config.usage.swap_time_remaining_on_vfd.value == "2":
-					return (duration, elapsed, end_time - now)
-				elif config.usage.swap_time_remaining_on_osd.value == "3" or config.usage.swap_time_remaining_on_vfd.value == "3":
-					return (duration, end_time - now, elapsed)
-				else:
+				if self.type == self.REMAINING and config.usage.swap_time_remaining_on_osd.value == "0":
 					return (duration, end_time - now)
-			else:
-				return (duration, None)
-		elif self.type == self.ELAPSED:
-			now = int(time())
-			start_time = event.getBeginTime()
-			duration = event.getDuration()
-			end_time = start_time + duration
-			elapsed = now - start_time
-			if start_time <= now <= end_time:
-				if config.usage.swap_time_remaining_on_osd.value == "1" or config.usage.swap_time_remaining_on_vfd.value == "1":
-					return (duration, end_time - now)
-				elif config.usage.swap_time_remaining_on_osd.value == "2" or config.usage.swap_time_remaining_on_vfd.value == "2":
-					return (duration, elapsed, end_time - now)
-				elif config.usage.swap_time_remaining_on_osd.value == "3" or config.usage.swap_time_remaining_on_vfd.value == "3":
-					return (duration, end_time - now, elapsed)
-				else:
+				elif self.type == self.REMAINING and config.usage.swap_time_remaining_on_osd.value == "1":
 					return (duration, elapsed)
+				elif self.type == self.REMAINING and config.usage.swap_time_remaining_on_osd.value == "2":
+					return (duration, elapsed, end_time - now)
+				elif self.type == self.REMAINING and config.usage.swap_time_remaining_on_osd.value == "3":
+					return (duration, end_time - now, elapsed)
+				elif self.type == self.ELAPSED and config.usage.swap_time_remaining_on_osd.value == "0":
+					return (duration, elapsed)
+				elif self.type == self.ELAPSED and config.usage.swap_time_remaining_on_osd.value == "1":
+					return (duration, end_time - now)
+				elif self.type == self.ELAPSED and config.usage.swap_time_remaining_on_osd.value == "2":
+					return (duration, elapsed, end_time - now)
+				elif self.type == self.ELAPSED and config.usage.swap_time_remaining_on_osd.value == "3":
+					return (duration, end_time - now, elapsed)
+				elif self.type == self.REMAINING_VFD and config.usage.swap_time_remaining_on_vfd.value == "0":
+					return (duration, end_time - now)
+				elif self.type == self.REMAINING_VFD and config.usage.swap_time_remaining_on_vfd.value == "1":
+					return (duration, elapsed)
+				elif self.type == self.REMAINING_VFD and config.usage.swap_time_remaining_on_vfd.value == "2":
+					return (duration, elapsed, end_time - now)
+				elif self.type == self.REMAINING_VFD and config.usage.swap_time_remaining_on_vfd.value == "3":
+					return (duration, end_time - now, elapsed)
+				elif self.type == self.ELAPSED_VFD and config.usage.swap_time_remaining_on_vfd.value == "0":
+					return (duration, elapsed)
+				elif self.type == self.ELAPSED_VFD and config.usage.swap_time_remaining_on_vfd.value == "1":
+					return (duration, end_time - now)
+				elif self.type == self.ELAPSED_VFD and config.usage.swap_time_remaining_on_vfd.value == "2":
+					return (duration, elapsed, end_time - now)
+				elif self.type == self.ELAPSED_VFD and config.usage.swap_time_remaining_on_vfd.value == "3":
+					return (duration, end_time - now, elapsed)
 			else:
 				return (duration, None)
 
-		elif int(self.type) > 5:
+		elif self.type == self.NEXT_START_TIME or self.type == self.NEXT_END_TIME or self.type == self.NEXT_DURATION or self.type == self.THIRD_START_TIME or self.type == self.THIRD_END_TIME or self.type == self.THIRD_DURATION:
 			reference = self.source.service
 			info = reference and self.source.info
 			if info is None:	
