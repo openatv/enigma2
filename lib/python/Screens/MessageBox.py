5582c224bdfc4dab78992e1b6718da579e799762
Andy Blackburn <andyblac@o2.co.uk>
2012-03-19 00:08:15 +0000

    MessageBox: update code

--- lib/python/Screens/MessageBox.py
+++ lib/python/Screens/MessageBox.py
@@ -12,7 +12,7 @@ class MessageBox(Screen):
 	TYPE_WARNING = 2
 	TYPE_ERROR = 3
 
-	def __init__(self, session, text, type = TYPE_YESNO, title = None, timeout = -1, close_on_any_key = False, default = True, enable_input = True, msgBoxID = None, picon = None, simple = False):
+	def __init__(self, session, text, type = TYPE_YESNO, timeout = -1, close_on_any_key = False, default = True, enable_input = True, msgBoxID = None, picon = None, simple = False):
 		self.type = type
 		Screen.__init__(self, session)
 
@@ -21,16 +21,15 @@ class MessageBox(Screen):
 		
 		self.msgBoxID = msgBoxID
 
-		if not title and len(_(text)) < 55:
+		if len(_(text)) < 55:
 			Screen.setTitle(self, _(text))
 			self["text"] = Label("")
 		else:
-			self.setTitle(_(title))
 			self["text"] = Label(_(text))
 		self["Text"] = StaticText(_(text))
 		self["selectedChoice"] = StaticText()
 
-		self.text = text
+		self.text = _(text)
 		self.close_on_any_key = close_on_any_key
 
 		self["ErrorPixmap"] = Pixmap()
