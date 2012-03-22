0a8461167ab383fc92e6d84e4361b3e0cc61a761
Paul In&In <darezik@gmail.com>
2012-03-22 16:05:09 +0100

    UsageConfig: add Thai language to the selection choices

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -321,6 +321,7 @@ def InitUsageConfig():
 		("slv", _("Slovenian")),
 		("spa", _("Spanish")),
 		("swe", _("Swedish")),
+		("tha", _("Thai")),
 		("tur Audio_TUR", _("Turkish"))]
 
 	def setEpgLanguage(configElement):
