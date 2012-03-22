5b5cae5998f27264aa29ec13d43e8ce8bd1ab424
pieterg <pieterg@users.sourceforge.net>
2012-03-22 16:06:51 +0100

    fix compiler warnings

--- lib/service/servicedvb.h
+++ lib/service/servicedvb.h
@@ -308,7 +308,7 @@ public:
 	eServiceReference &getPlayableService() { return m_playable_service; }
 	RESULT getName(const eServiceReference &ref, std::string &name);
 	int getLength(const eServiceReference &ref);
-	int isPlayable(const eServiceReference &ref, const eServiceReference &ignore, bool simulate=false);
+	int isPlayable(const eServiceReference &ref, const eServiceReference &ignore, bool simulate);
 	RESULT getEvent(const eServiceReference &ref, ePtr<eServiceEvent> &ptr, time_t start_time);
 };
 
