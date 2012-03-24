56f4180da2d524fda877f440d9ce1ef9e33563b4
Paul In&In <darezik@gmail.com>
2012-03-24 15:38:11 +0100

    ClockToText: date representatieons are translatable
    strftime() does not have the proper options to order date elements
    to the specific language for all cases. Therefor make the strftime
    formats translatable so the order and other interpunction will be
    language specific.

--- lib/python/Components/Converter/ClockToText.py
+++ lib/python/Components/Converter/ClockToText.py
@@ -63,32 +63,30 @@ class ClockToText(Converter, object):
 		t = localtime(time)
 		
 		if self.type == self.WITH_SECONDS:
-			return "%2d:%02d:%02d" % (t.tm_hour, t.tm_min, t.tm_sec)
+			# TRANSLATORS: full time representation hour:minute:seconds 
+			return _("%2d:%02d:%02d") % (t.tm_hour, t.tm_min, t.tm_sec)
 		elif self.type == self.DEFAULT:
-			return "%2d:%02d" % (t.tm_hour, t.tm_min)
+			# TRANSLATORS: short time representation hour:minute
+			return _("%2d:%02d") % (t.tm_hour, t.tm_min)
 		elif self.type == self.DATE:
-			# TRANSLATORS: full date representations dayname daynum monthname year. This is a strftime() format! See 'man strftime'
+			# TRANSLATORS: full date representation dayname daynum monthname year in strftime() format! See 'man strftime'
 			d = _("%A %e %B %Y")
-			return strftime(d, t)
 		elif self.type == self.FULL:
-			# TRANSLATORS: full representations dayname daynum monthnum hours minutes. This is a strftime() format! See 'man strftime'
+			# TRANSLATORS: long date representation short dayname daynum short monthname hour:minute in strftime() format! See 'man strftime'
 			d = _("%a %e/%m  %-H:%M")
-			return strftime(d, t)
 		elif self.type == self.SHORT_DATE:
-			# TRANSLATORS: short date representations dayname daynum monthnum. This is a strftime() format! See 'man strftime'
+			# TRANSLATORS: short date representation short dayname daynum short monthname in strftime() format! See 'man strftime'
 			d = _("%a %e/%m")
-			return strftime(d, t)
 		elif self.type == self.LONG_DATE:
-			# TRANSLATORS: long date representations dayname daynum monthname. This is a strftime() format! See 'man strftime'
+			# TRANSLATORS: long date representations dayname daynum monthname in strftime() format! See 'man strftime'
 			d = _("%A %e %B")
-			return strftime(d, t)
 		elif self.type == self.VFD:
-			# TRANSLATORS: VFD representations hours minuts daynum monthnum. This is a strftime() format! See 'man strftime'
+			# TRANSLATORS: VFD hour:minute daynum short monthname in strftime() format! See 'man strftime'
 			d = _("%k:%M %e/%m")
-			return strftime(d, t)
 		elif self.type == self.FORMAT:
-			return strftime(self.fmt_string, t)
+			d = self.fmt_string
 		else:
 			return "???"
+		return strftime(d, t)
 
 	text = property(getText)
