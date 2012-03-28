a749e946c9ecf05eef6adfdb7bd77935444d8738
pieterg <pieterg@users.sourceforge.net>
2012-03-29 01:15:40 +0200

    Harddisk: move initialisation of last_access and last_stat into the constructor
    Fancontrol2 seems to be using various (private) Harddisk members.
    So if some are not initialised, because we are not running a sleep
    timer on a device, it would crash.

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -59,6 +59,8 @@ class Harddisk:
 
 		self.max_idle_time = 0
 		self.idle_running = False
+		self.last_access = time.time()
+		self.last_stat = 0
 		self.timer = None
 		self.is_sleeping = False
 
@@ -438,8 +440,6 @@ class Harddisk:
 		return (int(data[0]), int(data[4]))
 
 	def startIdle(self):
-		self.last_access = time.time()
-		self.last_stat = 0
 		from enigma import eTimer
 
 		# disable HDD standby timer
