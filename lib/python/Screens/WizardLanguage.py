5d70906ca2cd442aff4d6e1c55222536fb33cee4
Paul In&In <darezik@gmail.com>
2012-03-31 20:09:39 +0200

    Make VideoWizard screen automatically fit to screen
    VideoWizard is the first thing to appear on a new system. Now it fills
    the screen, regardless of the resolution it's in. It's a nice demonstration
    of <panel> capabilities.
    
    Moved a few summary screens to where they belong.

--- lib/python/Screens/WizardLanguage.py
+++ lib/python/Screens/WizardLanguage.py
@@ -5,10 +5,9 @@ from Components.Language import language
 class WizardLanguage(Wizard):
 	def __init__(self, session, showSteps = True, showStepSlider = True, showList = True, showConfig = True):
 		Wizard.__init__(self, session, showSteps, showStepSlider, showList, showConfig)
-		
 		self["languagetext"] = Label()
 		self.updateLanguageDescription()
-		
+
 	def red(self):
 		self.resetCounter()
 		self.languageSelect()
@@ -19,13 +18,12 @@ class WizardLanguage(Wizard):
 		if newlanguage >= len(language.getLanguageList()):
 			newlanguage = 0
 		language.activateLanguageIndex(newlanguage)
-		
 		self.updateTexts()
 
 	def updateLanguageDescription(self):
 		print language.getLanguageList()[language.getActiveLanguageIndex()]
 		self["languagetext"].setText(self.getTranslation(language.getLanguageList()[language.getActiveLanguageIndex()][1][0]))
-		
+
 	def updateTexts(self):
 		print "updateTexts"
 		self.updateText(firstset = True)
