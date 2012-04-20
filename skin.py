55e539e47669e7a5cce8cb61a54475afdbb63277
babsy98 <babsy98@gmx.net>
2012-04-20 18:15:23 +0200

    switch back to openpli default skin for testing only

--- skin.py
+++ skin.py
@@ -59,11 +59,11 @@ def addSkin(name, scope = SCOPE_SKIN):
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "BlackShadow3/skin.xml"
+DEFAULT_SKIN = "PLi-HD/skin.xml"
 # on SD hardware, PLi-HD will not be available
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
-	# in that case, fallback to org_skin (which is an SD skin)
-	DEFAULT_SKIN = "skin.xml"
+	# in that case, fallback to Magic (which is an SD skin)
+	DEFAULT_SKIN = "Magic/skin.xml"
 config.skin.primary_skin = ConfigText(default=DEFAULT_SKIN)
 
 profile("LoadSkin")
