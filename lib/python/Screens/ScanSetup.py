openatv/enigma2

commit 7df3473a562c184628042f772c34756b1f4fc641

remove legacy dvbapi 3
-add support for DVB-T2
-add support for DVB-C ANNEX C

frontend: use pure dvb api 5 to tune / get frontend status
db: parse 'system' for terrestial/cable (DVB-T/T2, DVB-C-ANNEX-A/C)

lib/python/Screens/ScanSetup.py
@@ -15,9 +15,9 @@ from enigma import eTimer, eDVBFrontendParametersSatellite, eComponentScan, \
 	eDVBFrontendParametersCable, eConsoleAppContainer, eDVBResourceManager
 
 def buildTerTransponder(frequency, 
-		inversion=2, bandwidth = 3, fechigh = 6, feclow = 6,
+		inversion=2, bandwidth = 7000000, fechigh = 6, feclow = 6,
 		modulation = 2, transmission = 2, guard = 4,
-		hierarchy = 4):
+		hierarchy = 4, system = 0):
 #	print "freq", frequency, "inv", inversion, "bw", bandwidth, "fech", fechigh, "fecl", feclow, "mod", modulation, "tm", transmission, "guard", guard, "hierarchy", hierarchy
 	parm = eDVBFrontendParametersTerrestrial()
 	parm.frequency = frequency
@@ -29,6 +29,7 @@ def buildTerTransponder(frequency,
 	parm.transmission_mode = transmission
 	parm.guard_interval = guard
 	parm.hierarchy = hierarchy
+	parm.system = system
 	return parm
 
 def getInitialTransponderList(tlist, pos):
@@ -46,6 +47,7 @@ def getInitialTransponderList(tlist, pos):
 			parm.modulation = x[6]
 			parm.rolloff = x[8]
 			parm.pilot = x[9]
+			parm.system = x[10]
 			tlist.append(parm)
 
 def getInitialCableTransponderList(tlist, nim):
@@ -57,12 +59,8 @@ def getInitialCableTransponderList(tlist, nim):
 			parm.symbol_rate = x[2]
 			parm.modulation = x[3]
 			parm.fec_inner = x[4]
-			parm.inversion = parm.Inversion_Unknown
-			#print "frequency:", x[1]
-			#print "symbol_rate:", x[2]
-			#print "modulation:", x[3]
-			#print "fec_inner:", x[4]
-			#print "inversion:", 2
+			parm.inversion = x[5]
+			parm.system = x[6]
 			tlist.append(parm)
 
 def getInitialTerrestrialTransponderList(tlist, region):
@@ -75,7 +73,7 @@ def getInitialTerrestrialTransponderList(tlist, region):
 
 	for x in list:
 		if x[0] == 2: #TERRESTRIAL
-			parm = buildTerTransponder(x[1], x[9], x[2], x[4], x[5], x[3], x[7], x[6], x[8])
+			parm = buildTerTransponder(x[1], x[9], x[2], x[4], x[5], x[3], x[7], x[6], x[8], x[10])
 			tlist.append(parm)
 
 cable_bands = {
@@ -504,17 +502,19 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 				"inversion": eDVBFrontendParametersCable.Inversion_Unknown,
 				"modulation": eDVBFrontendParametersCable.Modulation_QAM64,
 				"fec": eDVBFrontendParametersCable.FEC_Auto,
-				"symbolrate": 6900 }
+				"symbolrate": 6900,
+				"system": eDVBFrontendParametersCable.System_DVB_C_ANNEX_A }
 			defaultTer = {
 				"frequency" : 466000,
 				"inversion" : eDVBFrontendParametersTerrestrial.Inversion_Unknown,
-				"bandwidth" : eDVBFrontendParametersTerrestrial.Bandwidth_7MHz,
+				"bandwidth" : 7000000,
 				"fechigh" : eDVBFrontendParametersTerrestrial.FEC_Auto,
 				"feclow" : eDVBFrontendParametersTerrestrial.FEC_Auto,
 				"modulation" : eDVBFrontendParametersTerrestrial.Modulation_Auto,
 				"transmission_mode" : eDVBFrontendParametersTerrestrial.TransmissionMode_Auto,
 				"guard_interval" : eDVBFrontendParametersTerrestrial.GuardInterval_Auto,
-				"hierarchy": eDVBFrontendParametersTerrestrial.Hierarchy_Auto }
+				"hierarchy": eDVBFrontendParametersTerrestrial.Hierarchy_Auto,
+				"system": eDVBFrontendParametersTerrestrial.System_DVB_T }
 
 			if frontendData is not None:
 				ttype = frontendData.get("tuner_type", "UNKNOWN")
@@ -538,16 +538,18 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 					defaultCab["inversion"] = frontendData.get("inversion", eDVBFrontendParametersCable.Inversion_Unknown)
 					defaultCab["fec"] = frontendData.get("fec_inner", eDVBFrontendParametersCable.FEC_Auto)
 					defaultCab["modulation"] = frontendData.get("modulation", eDVBFrontendParametersCable.Modulation_QAM16)
+					defaultTer["system"] = frontendData.get("system", eDVBFrontendParametersCable.System_DVB_C_ANNEX_A)
 				elif ttype == "DVB-T":
 					defaultTer["frequency"] = frontendData.get("frequency", 0)
 					defaultTer["inversion"] = frontendData.get("inversion", eDVBFrontendParametersTerrestrial.Inversion_Unknown)
-					defaultTer["bandwidth"] = frontendData.get("bandwidth", eDVBFrontendParametersTerrestrial.Bandwidth_7MHz)
+					defaultTer["bandwidth"] = frontendData.get("bandwidth", 7000000)
 					defaultTer["fechigh"] = frontendData.get("code_rate_hp", eDVBFrontendParametersTerrestrial.FEC_Auto)
 					defaultTer["feclow"] = frontendData.get("code_rate_lp", eDVBFrontendParametersTerrestrial.FEC_Auto)
 					defaultTer["modulation"] = frontendData.get("constellation", eDVBFrontendParametersTerrestrial.Modulation_Auto)
 					defaultTer["transmission_mode"] = frontendData.get("transmission_mode", eDVBFrontendParametersTerrestrial.TransmissionMode_Auto)
 					defaultTer["guard_interval"] = frontendData.get("guard_interval", eDVBFrontendParametersTerrestrial.GuardInterval_Auto)
 					defaultTer["hierarchy"] = frontendData.get("hierarchy_information", eDVBFrontendParametersTerrestrial.Hierarchy_Auto)
+					defaultTer["system"] = frontendData.get("system", eDVBFrontendParametersTerrestrial.System_DVB_T)
 
 			self.scan_sat = ConfigSubsection()
 			self.scan_cab = ConfigSubsection()
@@ -647,10 +649,14 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 				(eDVBFrontendParametersCable.FEC_2_3, "2/3"),
 				(eDVBFrontendParametersCable.FEC_3_4, "3/4"),
 				(eDVBFrontendParametersCable.FEC_5_6, "5/6"),
+				(eDVBFrontendParametersCable.FEC_6_7, "6/7"),
 				(eDVBFrontendParametersCable.FEC_7_8, "7/8"),
 				(eDVBFrontendParametersCable.FEC_8_9, "8/9"),
 				(eDVBFrontendParametersCable.FEC_None, _("None"))])
 			self.scan_cab.symbolrate = ConfigInteger(default = defaultCab["symbolrate"], limits = (1, 99999))
+			self.scan_cab.system = ConfigSelection(default = defaultCab["system"], choices = [
+				(eDVBFrontendParametersCable.System_DVB_C_ANNEX_A, _("DVB-C")),
+				(eDVBFrontendParametersCable.System_DVB_C_ANNEX_C, _("DVB-C ANNEX C"))])
 
 			# terrestial
 			self.scan_ter.frequency = ConfigInteger(default = 466000, limits = (50000, 999000))
@@ -660,23 +666,31 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 				(eDVBFrontendParametersTerrestrial.Inversion_Unknown, _("Auto"))])
 			# WORKAROUND: we can't use BW-auto
 			self.scan_ter.bandwidth = ConfigSelection(default = defaultTer["bandwidth"], choices = [
-				(eDVBFrontendParametersTerrestrial.Bandwidth_8MHz, "8MHz"),
-				(eDVBFrontendParametersTerrestrial.Bandwidth_7MHz, "7MHz"),
-				(eDVBFrontendParametersTerrestrial.Bandwidth_6MHz, "6MHz")])
+				(10000000, "10MHz"),
+				(8000000, "8MHz"),
+				(7000000, "7MHz"),
+				(6000000, "6MHz"),
+				(5000000, "5MHz"),
+				(1712000, "1.712MHz")
+				])
 			#, (eDVBFrontendParametersTerrestrial.Bandwidth_Auto, _("Auto"))))
 			self.scan_ter.fechigh = ConfigSelection(default = defaultTer["fechigh"], choices = [
 				(eDVBFrontendParametersTerrestrial.FEC_1_2, "1/2"),
 				(eDVBFrontendParametersTerrestrial.FEC_2_3, "2/3"),
 				(eDVBFrontendParametersTerrestrial.FEC_3_4, "3/4"),
 				(eDVBFrontendParametersTerrestrial.FEC_5_6, "5/6"),
+				(eDVBFrontendParametersTerrestrial.FEC_6_7, "6/7"),
 				(eDVBFrontendParametersTerrestrial.FEC_7_8, "7/8"),
+				(eDVBFrontendParametersTerrestrial.FEC_8_9, "8/9"),
 				(eDVBFrontendParametersTerrestrial.FEC_Auto, _("Auto"))])
 			self.scan_ter.feclow = ConfigSelection(default = defaultTer["feclow"], choices = [
 				(eDVBFrontendParametersTerrestrial.FEC_1_2, "1/2"),
 				(eDVBFrontendParametersTerrestrial.FEC_2_3, "2/3"),
 				(eDVBFrontendParametersTerrestrial.FEC_3_4, "3/4"),
 				(eDVBFrontendParametersTerrestrial.FEC_5_6, "5/6"),
+				(eDVBFrontendParametersTerrestrial.FEC_6_7, "6/7"),
 				(eDVBFrontendParametersTerrestrial.FEC_7_8, "7/8"),
+				(eDVBFrontendParametersTerrestrial.FEC_8_9, "8/9"),
 				(eDVBFrontendParametersTerrestrial.FEC_Auto, _("Auto"))])
 			self.scan_ter.modulation = ConfigSelection(default = defaultTer["modulation"], choices = [
 				(eDVBFrontendParametersTerrestrial.Modulation_QPSK, "QPSK"),
@@ -685,6 +699,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 				(eDVBFrontendParametersTerrestrial.Modulation_Auto, _("Auto"))])
 			self.scan_ter.transmission = ConfigSelection(default = defaultTer["transmission_mode"], choices = [
 				(eDVBFrontendParametersTerrestrial.TransmissionMode_2k, "2K"),
+				(eDVBFrontendParametersTerrestrial.TransmissionMode_4k, "4K"),
 				(eDVBFrontendParametersTerrestrial.TransmissionMode_8k, "8K"),
 				(eDVBFrontendParametersTerrestrial.TransmissionMode_Auto, _("Auto"))])
 			self.scan_ter.guard = ConfigSelection(default = defaultTer["guard_interval"], choices = [
@@ -699,6 +714,9 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 				(eDVBFrontendParametersTerrestrial.Hierarchy_2, "2"),
 				(eDVBFrontendParametersTerrestrial.Hierarchy_4, "4"),
 				(eDVBFrontendParametersTerrestrial.Hierarchy_Auto, _("Auto"))])
+			self.scan_ter.system = ConfigSelection(default = defaultTer["system"], choices = [
+				(eDVBFrontendParametersTerrestrial.System_DVB_T, _("DVB-T")),
+				(eDVBFrontendParametersTerrestrial.System_DVB_T2, _("DVB-T2"))])
 
 			self.scan_scansat = {}
 			for sat in nimmanager.satList:

lib/dvb/db.cpp
@@ -411,9 +411,20 @@ void eDVBDB::loadServicelist(const char *file)
 			{
 				eDVBFrontendParametersTerrestrial ter;
 				int frequency, bandwidth, code_rate_HP, code_rate_LP, modulation, transmission_mode, guard_interval, hierarchy, inversion, flags=0;
-				sscanf(line+3, "%d:%d:%d:%d:%d:%d:%d:%d:%d:%d", &frequency, &bandwidth, &code_rate_HP, &code_rate_LP, &modulation, &transmission_mode, &guard_interval, &hierarchy, &inversion, &flags);
+				int system = eDVBFrontendParametersTerrestrial::System_DVB_T;
+				sscanf(line+3, "%d:%d:%d:%d:%d:%d:%d:%d:%d:%d:%d", &frequency, &bandwidth, &code_rate_HP, &code_rate_LP, &modulation, &transmission_mode, &guard_interval, &hierarchy, &inversion, &flags, &system);
 				ter.frequency = frequency;
-				ter.bandwidth = bandwidth;
+				switch (bandwidth)
+				{
+				case eDVBFrontendParametersTerrestrial::Bandwidth_8MHz: ter.bandwidth = 8000000; break;
+				case eDVBFrontendParametersTerrestrial::Bandwidth_7MHz: ter.bandwidth = 7000000; break;
+				case eDVBFrontendParametersTerrestrial::Bandwidth_6MHz: ter.bandwidth = 6000000; break;
+				default:
+				case eDVBFrontendParametersTerrestrial::Bandwidth_Auto: ter.bandwidth = 0; break;
+				case eDVBFrontendParametersTerrestrial::Bandwidth_5MHz: ter.bandwidth = 5000000; break;
+				case eDVBFrontendParametersTerrestrial::Bandwidth_1_712MHz: ter.bandwidth = 1712000; break;
+				case eDVBFrontendParametersTerrestrial::Bandwidth_10MHz: ter.bandwidth = 10000000; break;
+				}
 				ter.code_rate_HP = code_rate_HP;
 				ter.code_rate_LP = code_rate_LP;
 				ter.modulation = modulation;
@@ -421,6 +432,7 @@ void eDVBDB::loadServicelist(const char *file)
 				ter.guard_interval = guard_interval;
 				ter.hierarchy = hierarchy;
 				ter.inversion = inversion;
+				ter.system = system;
 				feparm->setDVBT(ter);
 				feparm->setFlags(flags);
 			} else if (line[1]=='c')
@@ -430,13 +442,15 @@ void eDVBDB::loadServicelist(const char *file)
 					inversion=eDVBFrontendParametersCable::Inversion_Unknown,
 					modulation=eDVBFrontendParametersCable::Modulation_Auto,
 					fec_inner=eDVBFrontendParametersCable::FEC_Auto,
+					system = eDVBFrontendParametersCable::System_DVB_C_ANNEX_A,
 					flags=0;
-				sscanf(line+3, "%d:%d:%d:%d:%d:%d", &frequency, &symbol_rate, &inversion, &modulation, &fec_inner, &flags);
+				sscanf(line+3, "%d:%d:%d:%d:%d:%d", &frequency, &symbol_rate, &inversion, &modulation, &fec_inner, &flags, &system);
 				cab.frequency = frequency;
 				cab.fec_inner = fec_inner;
 				cab.inversion = inversion;
 				cab.symbol_rate = symbol_rate;
 				cab.modulation = modulation;
+				cab.system = system;
 				feparm->setDVBC(cab);
 				feparm->setFlags(flags);
 			}
@@ -552,10 +566,22 @@ void eDVBDB::saveServicelist(const char *file)
 		}
 		else if (!ch.m_frontendParameters->getDVBT(ter))
 		{
-			fprintf(f, "\tt %d:%d:%d:%d:%d:%d:%d:%d:%d:%d\n",
-				ter.frequency, ter.bandwidth, ter.code_rate_HP,
+			int bandwidth;
+			switch (bandwidth)
+			{
+			case 8000000: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_8MHz; break;
+			case 7000000: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_7MHz; break;
+			case 6000000: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_6MHz; break;
+			default:
+			case 0: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_Auto; break;
+			case 5000000: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_5MHz; break;
+			case 1712000: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_1_712MHz; break;
+			case 10000000: bandwidth = eDVBFrontendParametersTerrestrial::Bandwidth_10MHz; break;
+			}
+			fprintf(f, "\tt %d:%d:%d:%d:%d:%d:%d:%d:%d:%d:%d\n",
+				ter.frequency, bandwidth, ter.code_rate_HP,
 				ter.code_rate_LP, ter.modulation, ter.transmission_mode,
-				ter.guard_interval, ter.hierarchy, ter.inversion, flags);
+				ter.guard_interval, ter.hierarchy, ter.inversion, flags, ter.system);
 		}
 		else if (!ch.m_frontendParameters->getDVBC(cab))
 		{
@@ -965,7 +991,7 @@ PyObject *eDVBDB::readCables(ePyObject cab_list, ePyObject tp_dict)
 	}
 	const Attribute *at;
 	int tmp, *dest,
-		modulation, fec, freq, sr;
+		modulation, fec, freq, sr, inversion, system;
 	std::string name;
 	char *end_ptr;
 	const ElementList &root_elements = root->getElementList();
@@ -1009,6 +1035,8 @@ PyObject *eDVBDB::readCables(ePyObject cab_list, ePyObject tp_dict)
 				AttributeConstIterator end = tp_attributes.end();
 				modulation = eDVBFrontendParametersCable::Modulation_QAM64;
 				fec = eDVBFrontendParametersCable::FEC_Auto;
+				system = eDVBFrontendParametersCable::System_DVB_C_ANNEX_A;
+				inversion = eDVBFrontendParametersCable::Inversion_Unknown;
 				freq = 0;
 				sr = 0;
 				for (AttributeConstIterator it(tp_attributes.begin()); it != end; ++it)
@@ -1021,6 +1049,8 @@ PyObject *eDVBDB::readCables(ePyObject cab_list, ePyObject tp_dict)
 					else if (name == "frequency") dest = &freq;
 					else if (name == "symbol_rate") dest = &sr;
 					else if (name == "fec_inner") dest = &fec;
+					else if (name == "inversion") dest = &inversion;
+					else if (name == "system") dest = &system;
 					else continue;
 					if (dest)
 					{
@@ -1033,12 +1063,14 @@ PyObject *eDVBDB::readCables(ePyObject cab_list, ePyObject tp_dict)
 				{
 					while (freq > 999999)
 						freq /= 10;
-					tuple = PyTuple_New(5);
+					tuple = PyTuple_New(7);
 					PyTuple_SET_ITEM(tuple, 0, PyInt_FromLong(1));
 					PyTuple_SET_ITEM(tuple, 1, PyInt_FromLong(freq));
 					PyTuple_SET_ITEM(tuple, 2, PyInt_FromLong(sr));
 					PyTuple_SET_ITEM(tuple, 3, PyInt_FromLong(modulation));
 					PyTuple_SET_ITEM(tuple, 4, PyInt_FromLong(fec));
+					PyTuple_SET_ITEM(tuple, 5, PyInt_FromLong(inversion));
+					PyTuple_SET_ITEM(tuple, 6, PyInt_FromLong(system));
 					PyList_Append(tplist, tuple);
 					Py_DECREF(tuple);
 				}
@@ -1080,7 +1112,7 @@ PyObject *eDVBDB::readTerrestrials(ePyObject ter_list, ePyObject tp_dict)
 	const Attribute *at;
 	std::string name;
 	int tmp, *dest,
-		freq, bw, constellation, crh = 5, crl = 5, guard = 4, transm, hierarchy, inv = 2;
+		freq, bw, constellation, crh, crl, guard, transm, hierarchy, inv, system;
 	char *end_ptr;
 	const ElementList &root_elements = root->getElementList();
 	for (ElementConstIterator it(root_elements.begin()); it != root_elements.end(); ++it)
@@ -1130,6 +1162,7 @@ PyObject *eDVBDB::readTerrestrials(ePyObject ter_list, ePyObject tp_dict)
 				transm = eDVBFrontendParametersTerrestrial::TransmissionMode_Auto;
 				hierarchy = eDVBFrontendParametersTerrestrial::Hierarchy_Auto;
 				inv = eDVBFrontendParametersTerrestrial::Inversion_Unknown;
+				system = eDVBFrontendParametersTerrestrial::System_DVB_T;
 				for (AttributeConstIterator it(tp_attributes.begin()); it != end; ++it)
 				{
 //					eDebug("\t\tattr: %s", at->name().c_str());
@@ -1145,6 +1178,7 @@ PyObject *eDVBDB::readTerrestrials(ePyObject ter_list, ePyObject tp_dict)
 					else if (name == "transmission_mode") dest = &transm;
 					else if (name == "hierarchy_information") dest = &hierarchy;
 					else if (name == "inversion") dest = &inv;
+					else if (name == "system") dest = &system;
 					else continue;
 					if (dest)
 					{
@@ -1155,11 +1189,22 @@ PyObject *eDVBDB::readTerrestrials(ePyObject ter_list, ePyObject tp_dict)
 				}
 				if (freq)
 				{
-					if (crh > 5) // our terrestrial.xml is buggy... 6 for AUTO
-						crh = 5;
-					if (crl > 5) // our terrestrial.xml is buggy... 6 for AUTO
-						crl = 5;
-					tuple = PyTuple_New(10);
+					switch (bw)
+					{
+					case eDVBFrontendParametersTerrestrial::Bandwidth_8MHz: bw = 8000000; break;
+					case eDVBFrontendParametersTerrestrial::Bandwidth_7MHz: bw = 7000000; break;
+					case eDVBFrontendParametersTerrestrial::Bandwidth_6MHz: bw = 6000000; break;
+					default:
+					case eDVBFrontendParametersTerrestrial::Bandwidth_Auto: bw = 0; break;
+					case eDVBFrontendParametersTerrestrial::Bandwidth_5MHz: bw = 5000000; break;
+					case eDVBFrontendParametersTerrestrial::Bandwidth_1_712MHz: bw = 1712000; break;
+					case eDVBFrontendParametersTerrestrial::Bandwidth_10MHz: bw = 10000000; break;
+					}
+					if (crh > eDVBFrontendParametersTerrestrial::FEC_8_9)
+						crh = eDVBFrontendParametersTerrestrial::FEC_Auto;
+					if (crl > eDVBFrontendParametersTerrestrial::FEC_8_9)
+						crl = eDVBFrontendParametersTerrestrial::FEC_Auto;
+					tuple = PyTuple_New(11);
 					PyTuple_SET_ITEM(tuple, 0, PyInt_FromLong(2));
 					PyTuple_SET_ITEM(tuple, 1, PyInt_FromLong(freq));
 					PyTuple_SET_ITEM(tuple, 2, PyInt_FromLong(bw));
@@ -1170,6 +1215,7 @@ PyObject *eDVBDB::readTerrestrials(ePyObject ter_list, ePyObject tp_dict)
 					PyTuple_SET_ITEM(tuple, 7, PyInt_FromLong(transm));
 					PyTuple_SET_ITEM(tuple, 8, PyInt_FromLong(hierarchy));
 					PyTuple_SET_ITEM(tuple, 9, PyInt_FromLong(inv));
+					PyTuple_SET_ITEM(tuple, 10, PyInt_FromLong(system));
 					PyList_Append(tplist, tuple);
 					Py_DECREF(tuple);
 				}

commit 167fae7563e9917ce7eb63eb0715bc86e797873a

ScanSetup: fix setting invalid sat transponder system
broken by 7df3473a562c184628042f772c34756b1f4fc641

lib/python/Screens/ScanSetup.py
@@ -47,7 +47,6 @@ def getInitialTransponderList(tlist, pos):
 			parm.modulation = x[6]
 			parm.rolloff = x[8]
 			parm.pilot = x[9]
-			parm.system = x[10]
 			tlist.append(parm)
 
 def getInitialCableTransponderList(tlist, nim):