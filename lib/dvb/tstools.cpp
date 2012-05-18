openatv/enigma2

commit 4301e468e6c4f530e85e3aaa1b2a89b3235a4487

tstool: Seek to sequence start when (re)winding MPEG2 streams
This fixes non-working rewinding on a few transponders, and seems to
have no visible effect on streams that already worked fine.

lib/dvb/tstools.cpp
@@ -705,6 +705,7 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 
 	off_t offset = _offset;
 	int nr_frames = 0;
+	bool is_mpeg2 = false;
 
 		/* let's find the iframe before the given offset */
 	if (direction < 0)
@@ -736,10 +737,13 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 				--nr_frames;
 			else
 				++nr_frames;
-			if ( // is start
-				((data & 0xE0FF) == 0x0009) ||		/* H.264 NAL unit access delimiter with I-frame*/
-				((data & 0x3800FF) == 0x080000))	/* MPEG2 picture start code with I-frame */
+			if ((data & 0xE0FF) == 0x0009)		/* H.264 NAL unit access delimiter with I-frame*/
 			{
+				break;
+			}
+			if ((data & 0x3800FF) == 0x080000)	/* MPEG2 picture start code with I-frame */
+			{
+				is_mpeg2 = true;
 				break;
 			}
 		}
@@ -748,22 +752,7 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 	}
 	off_t start = offset;
 
-#if 0
-			/* backtrack to find the previous sequence start, in case of MPEG2 */
-	if ((data & 0xFF) == 0x00) {
-		do {
-			--start;
-			if (m_streaminfo.getStructureEntry(start, data, 0))
-			{
-				eDebug("get previous failed");
-				return -1;
-			}
-		} while (((data & 0xFF) != 9) && ((data & 0xFF) != 0x00) && ((data & 0xFF) != 0xB3)); /* sequence start or previous frame */
-		if ((data & 0xFF) != 0xB3)
-			start = offset;  /* Failed to find corresponding sequence start, so never mind */
-	}
-#endif
-		/* let's find the next frame after the given offset */
+	/* let's find the next frame after the given offset */
 	unsigned int data;
 	do
 	{
@@ -772,9 +761,23 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 			eDebug("get next failed");
 			return -1;
 		}
-		data = (unsigned int)longdata;
+		data = ((unsigned int)longdata) & 0xFF;
 	}
-	while (((data & 0xFF) != 9) && ((data & 0xFF) != 0x00)); /* next frame */
+	while ((data != 0x09) && (data != 0x00)); /* next frame */
+
+	if (is_mpeg2)
+	{
+		// Seek back to sequence start (appears to be needed for some...)
+		do
+		{
+			if (m_streaminfo.getStructureEntryNext(start, longdata, -1))
+			{
+				eDebug("Failed to find MPEG2 start frame");
+				break;
+			}
+		}
+		while ((((unsigned int)longdata) & 0xFF) != 0xB3); /* sequence start or previous frame */
+	}
 
 	len = offset - start;
 	_offset = start;

commit 111d7fade55ce9592384434d063c881251738648

tstools: Fix fast-forward of MPEG2 streams at >16x
The "backsearch" sometimes would cause the stream to go back in time.
This is now prevented by limiting the search for a start frame to the
number of frames skipped. Also reports the correct number of frames
back to the caller, and a tiny perfomance optimization by moving the
"if" outside the loop.
The limited search also applies to rewinding, which isn't entirely
correct.

Problem was introduced in 4301e468e6c4f530e85e3aaa1b2a89b3235a4487

lib/dvb/tstools.cpp
@@ -733,10 +733,7 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 		// is_frame
 		if (((data & 0xFF) == 0x0009) || ((data & 0xFF) == 0x00)) /* H.264 UAD or MPEG2 start code */
 		{
-			if (direction < 0)
-				--nr_frames;
-			else
-				++nr_frames;
+			++nr_frames;
 			if ((data & 0xE0FF) == 0x0009)		/* H.264 NAL unit access delimiter with I-frame*/
 			{
 				break;
@@ -767,21 +764,26 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 
 	if (is_mpeg2)
 	{
-		// Seek back to sequence start (appears to be needed for some...)
-		do
+		// Seek back to sequence start (appears to be needed for e.g. a few TCM streams)
+		while (nr_frames)
 		{
 			if (m_streaminfo.getStructureEntryNext(start, longdata, -1))
 			{
 				eDebug("Failed to find MPEG2 start frame");
 				break;
 			}
+			if ((((unsigned int)longdata) & 0xFF) == 0xB3) /* sequence start or previous frame */
+				break;
+			--nr_frames;
 		}
-		while ((((unsigned int)longdata) & 0xFF) != 0xB3); /* sequence start or previous frame */
 	}
 
 	len = offset - start;
 	_offset = start;
-	direction = nr_frames;
+	if (direction < 0)
+		direction = -nr_frames;
+	else
+		direction = nr_frames;
 //	eDebug("result: offset=%llu, len: %ld", offset, (int)len);
 	return 0;
 }