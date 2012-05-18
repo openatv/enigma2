a5eafff781dad18ce99a318acb99e90e7d88d267
Andy Blackburn <andyblac@o2.co.uk>
2012-05-18 10:26:49 +0100

    HelpMenu: fix possible BSOD.

--- lib/python/Screens/HelpMenu.py
+++ lib/python/Screens/HelpMenu.py
@@ -53,9 +53,11 @@ class HelpableScreen:
 			})
 
 	def showHelp(self):
-		if self.secondInfoBarScreen and self.secondInfoBarScreen.shown:
-			self.secondInfoBarScreen.hide()
-			self.secondInfoBarWasShown = False
+		try:
+			if self.secondInfoBarScreen and self.secondInfoBarScreen.shown:
+				self.secondInfoBarScreen.hide()
+		except:
+			pass
 		self.session.openWithCallback(self.callHelpAction, HelpMenu, self.helpList)
 
 	def callHelpAction(self, *args):
