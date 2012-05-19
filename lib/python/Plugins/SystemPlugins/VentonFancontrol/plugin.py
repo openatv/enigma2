5c86a8b59b9c99eb1a6827947199ff8522c2636a
black_64 <mail>
2012-05-19 10:37:38 +0200

    ventonfancontrol: add fancontrol in for standby

--- lib/python/Plugins/SystemPlugins/VentonFancontrol/plugin.py
+++ lib/python/Plugins/SystemPlugins/VentonFancontrol/plugin.py
@@ -1,20 +1,23 @@
 from Screens.Screen import Screen
 from Components.ConfigList import ConfigListScreen
 from Components.config import config, ConfigSubsection, ConfigInteger, ConfigSelection, getConfigListEntry
+from Components.FanControl import fancontrol
 
 modelist = {"0": _("Off"), "1": _("On")}
+standbylist = [("false", _("no")), ("true", _("yes")), ("trueRec", _("yes, Except for Recording or HDD"))]
 
-config.plugins.FanSetup = ConfigSubsection()
-config.plugins.FanSetup.mode = ConfigSelection(choices = modelist, default = "0")
+config.plugins.FanControl = ConfigSubsection()
+config.plugins.FanControl.mode = ConfigSelection(choices = modelist, default = "0")
+config.plugins.FanControl.StandbyOff = ConfigSelection(choices = standbylist, default="false")
 
 class FanSetupScreen(Screen, ConfigListScreen):
 	skin = """
-	<screen position="c-200,c-100" size="400,200" title="Fan setup">
-		<widget name="config" position="c-175,c-75" size="350,150" />
-		<ePixmap pixmap="skin_default/buttons/green.png" position="c-145,e-45" zPosition="0" size="140,40" alphatest="on" />
-		<ePixmap pixmap="skin_default/buttons/red.png" position="c+5,e-45" zPosition="0" size="140,40" alphatest="on" />
-		<widget name="ok" position="c-145,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="green" />
-		<widget name="cancel" position="c+5,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="red" />
+	<screen position="c-200,c-100" size="600,200" title="Fan setup">
+		<widget name="config" position="c-275,c-75" size="550,150" />
+		<ePixmap pixmap="skin_default/buttons/green.png" position="c-245,e-45" zPosition="0" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/red.png" position="c+105,e-45" zPosition="0" size="140,40" alphatest="on" />
+		<widget name="ok" position="c-245,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="green" />
+		<widget name="cancel" position="c+105,e-45" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="red" />
 	</screen>"""
 
 	def __init__(self, session):
@@ -39,10 +42,13 @@ class FanSetupScreen(Screen, ConfigListScreen):
 		self.list = []
 		ConfigListScreen.__init__(self, self.list, session = self.session)
 
-		mode = config.plugins.FanSetup.mode.value
+		mode = config.plugins.FanControl.mode.value
+		modestandby = config.plugins.FanControl.StandbyOff.value
 
 		self.mode = ConfigSelection(choices = modelist, default = mode)
 		self.list.append(getConfigListEntry(_("Fan mode"), self.mode))
+		self.modestandby = ConfigSelection(choices = standbylist, default = modestandby)
+		self.list.append(getConfigListEntry(_("Fan control in Standby off"), self.modestandby))
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
 
@@ -55,33 +61,40 @@ class FanSetupScreen(Screen, ConfigListScreen):
 		self.setPreviewSettings()
 
 	def setPreviewSettings(self):
-		applySettings(int(self.mode.value))
+		applySettings(int(self.mode.value), False, self.modestandby.value)
 
 	def keyGo(self):
-		config.plugins.FanSetup.mode.value = self.mode.value
-		config.plugins.FanSetup.save()
+		config.plugins.FanControl.mode.value = self.mode.value
+		config.plugins.FanControl.StandbyOff.value = self.modestandby.value
+		config.plugins.FanControl.save()
 		self.close()
 
 	def keyCancel(self):
 		setConfiguredSettings()
 		self.close()
 
-def applySettings(mode):
-	setMode = ""
+def applySettings(mode, firststart = None, modestandby = None):
 	if mode == 1:
-		setMode = "255"
+		fancontrol.getConfig(0).pwm.value = 255
 	else:
-		setMode = "0"
+		fancontrol.getConfig(0).pwm.value = 0
 
-	try:
-		file = open("/proc/stb/fp/fan_pwm", "w")
-		file.write('%s' % setMode)
-		file.close()
-	except:
-		return
+	if firststart:
+		if config.plugins.FanControl.StandbyOff.value == "false":
+			fancontrol.getConfig(0).pwm_standby.value = 255
+		else:
+			fancontrol.getConfig(0).pwm_standby.value = 0
+	else:
+		if modestandby == "false":
+			fancontrol.getConfig(0).pwm_standby.value = 255
+		else:
+			fancontrol.getConfig(0).pwm_standby.value = 0
+
+	fancontrol.getConfig(0).pwm.save()
+	fancontrol.getConfig(0).pwm_standby.save()
 
 def setConfiguredSettings():
-	applySettings(int(config.plugins.FanSetup.mode.value))
+	applySettings(int(config.plugins.FanControl.mode.value), True)
 
 def main(session, **kwargs):
 	session.open(FanSetupScreen)
