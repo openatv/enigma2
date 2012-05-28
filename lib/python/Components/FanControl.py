2a3d999c2107ee5440e2c052b1b598c6dde888ee
Paul In&In <darezik@gmail.com>
2012-05-28 13:59:22 +0100

    FanControl: set default speed minimum speed.

--- lib/python/Components/FanControl.py
+++ lib/python/Components/FanControl.py
@@ -63,7 +63,7 @@ class FanControl:
 		for fanid in range(self.getFanCount()):
 			fan = ConfigSubsection()
 			fan.vlt = ConfigSlider(default = 15, increment = 5, limits = (0, 255))
-			fan.pwm = ConfigSlider(default = 0, increment = 5, limits = (0, 255))
+			fan.pwm = ConfigSlider(default = 1, increment = 5, limits = (0, 255))
 			fan.vlt_standby = ConfigSlider(default = 5, increment = 5, limits = (0, 255))
 			fan.pwm_standby = ConfigSlider(default = 0, increment = 5, limits = (0, 255))
 			fan.vlt.addNotifier(boundFunction(setVlt, self, fanid))
