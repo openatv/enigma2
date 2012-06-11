7472be9a4e78b332cd53eb0d3ebdd2d11ab4b8e6
Paul In&In <darezik@gmail.com>
2012-06-11 16:09:18 +0200

    enigma2: rc_models removed duplicate pictures
    et9x fix model detection (thanks daddelfalk)
    
    vu fix rcpositions (thanks Pedro_Newbie)

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -25,7 +25,7 @@ class RcModel:
 	def readRcTypeFromProc(self):
 		if os.path.exists('/proc/stb/info/boxtype'):
 			model = self.readFile('/proc/stb/info/boxtype')
-			if model == 'et9000' or model == 'et5000' or model == 'et6000':
+			if len(model) == 6 and model[:2] == 'et':
 				rc = self.readFile('/proc/stb/ir/rc/type')
 				if rc == '4':
 					self.currentRcType = self.RCTYPE_DMM
