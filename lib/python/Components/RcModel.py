e579963f86f2769549f190ba4611df18e5de2955
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-07-03 13:16:13 +0100

    remote models: update, some may need positions tweaking.

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -6,8 +6,12 @@ class RcModel:
 	RCTYPE_ET6X00 = 2
 	RCTYPE_ET9500 = 3
 	RCTYPE_VU = 4
-	RCTYPE_TM = 5
-	RCTYPE_GB = 6
+	RCTYPE_GB = 5
+	RCTYPE_INI3000 = 6
+	RCTYPE_INI5000 = 7
+	RCTYPE_INI7000 = 8
+	RCTYPE_ODIN = 9
+	RCTYPE_TM = 10
 
 	def __init__(self):
 		self.currentRcType = self.RCTYPE_DMM
@@ -35,7 +39,9 @@ class RcModel:
 				rc = self.readFile('/proc/stb/ir/rc/type')
 				if rc == '4':
 					self.currentRcType = self.RCTYPE_DMM
-				elif rc == '5' or rc == '11':
+				elif rc == '5' and model == 'et9200':
+					self.currentRcType = self.RCTYPE_ODIN
+				elif rc == '5':
 					self.currentRcType = self.RCTYPE_ET9X00
 				elif rc == '6':
 					self.currentRcType = self.RCTYPE_DMM
@@ -45,22 +51,38 @@ class RcModel:
 					self.currentRcType = self.RCTYPE_VU
 				elif rc == '9':
 					self.currentRcType = self.RCTYPE_ET9500
+				elif rc == '11' and model == 'et9200':
+					self.currentRcType = self.RCTYPE_ET9500
+				elif rc == '11' and model == 'et9000':
+					self.currentRcType = self.RCTYPE_ET9x00
+			elif model == 'gigablue':
+				self.currentRcType = self.RCTYPE_GB
+			elif model == 'ini-3000':
+				self.currentRcType = self.RCTYPE_INI3000
+			elif model == 'ini-5000':
+				self.currentRcType = self.RCTYPE_INI5000
+			elif model == 'ini-7000':
+				self.currentRcType = self.RCTYPE_INI7000
 		elif os.path.exists('/proc/stb/info/vumodel'):
 			self.currentRcType = self.RCTYPE_VU
-		elif os.path.exists('/proc/stb/info/model'):
-			model = self.readFile('/proc/stb/info/model')
-			if model == 'Gigablue':
-				self.currentRcType = self.RCTYPE_GB
 
 	def getRcLocation(self):
 		if self.currentRcType == self.RCTYPE_ET9X00:
 			return '/usr/share/enigma2/rc_models/et9x00/'
-		elif self.currentRcType == self.RCTYPE_ET9500:
-			return '/usr/share/enigma2/rc_models/et9500/'
 		elif self.currentRcType == self.RCTYPE_ET6X00:
 			return '/usr/share/enigma2/rc_models/et6x00/'
+		elif self.currentRcType == self.RCTYPE_ET9500:
+			return '/usr/share/enigma2/rc_models/et9500/'
 		elif self.currentRcType == self.RCTYPE_GB:
 			return '/usr/share/enigma2/rc_models/gb/'
+		elif self.currentRcType == self.RCTYPE_INI3000:
+			return '/usr/share/enigma2/rc_models/ini3000/'
+		elif self.currentRcType == self.RCTYPE_INI5000:
+			return '/usr/share/enigma2/rc_models/ini5000/'
+		elif self.currentRcType == self.RCTYPE_INI7000:
+			return '/usr/share/enigma2/rc_models/ini7000/'
+		elif self.currentRcType == self.RCTYPE_ODIN:
+			return '/usr/share/enigma2/rc_models/odin/'
 		elif self.currentRcType == self.RCTYPE_TM:
 			return '/usr/share/enigma2/rc_models/tm/'
 		elif self.currentRcType == self.RCTYPE_VU:
