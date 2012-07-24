9e12e1b45684a172cc79148f7bcb8447bab56a4b
Paul In&In <darezik@gmail.com>
2012-07-24 20:33:06 +0200

    Revert "counting channelnumber from service reference not from service name"
    As it has other side effects with stream services do not
    get numbers anymore on the InfoBar and something went incorrect
    with alternative services

--- lib/python/Components/Renderer/ChannelNumber.py
+++ lib/python/Components/Renderer/ChannelNumber.py
@@ -20,11 +20,10 @@ class ChannelNumber(Renderer, VariableText):
 		if info is None:
 			self.text = ""
 			return
-
-		serviceref = info.getInfoString(iServiceInformation.sServiceref)
-		if serviceref in self.list:
+		name = info.getName().replace('\xc2\x86', '').replace('\xc2\x87', '')
+		if name in self.list:
 			for idx in range(1, len(self.list)):
-				if serviceref == self.list[idx-1]:
+				if name == self.list[idx-1]:
 					self.text = str(idx)
 					break
 		else:
@@ -39,4 +38,5 @@ class ChannelNumber(Renderer, VariableText):
 			channels = services and services.getContent("SN", True)
 			for channel in channels:
 				if not channel[0].startswith("1:64:"):
-					self.list.append(channel[0])
+					self.list.append(channel[1].replace('\xc2\x86', '').replace('\xc2\x87', ''))
+
