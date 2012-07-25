eb04520e0ad068c3c24a62f0564d69b68aee57d1
Paul In&In <darezik@gmail.com>
2012-07-25 21:27:39 +0200

    ChannelNumber: Save some resources
    Only search for a new number when the service realy changes

--- lib/python/Components/Renderer/ChannelNumber.py
+++ lib/python/Components/Renderer/ChannelNumber.py
@@ -3,30 +3,30 @@ from enigma import eLabel, eServiceCenter
 from Renderer import Renderer
 from Screens.InfoBar import InfoBar
 
-oldroot = None
-Offset = None
-
 class ChannelNumber(Renderer, VariableText):
 	def __init__(self):
 		Renderer.__init__(self)
 		VariableText.__init__(self)
+		self.lastService = None
 
 	GUI_WIDGET = eLabel
 
 	def changed(self, what):
-		self.text="---"
 		service = self.source.service
 		if service and service.info():
 			serviceHandler = eServiceCenter.getInstance()
 			CurrentServiceList = InfoBar.instance.servicelist
 			root = CurrentServiceList.servicelist.getRoot()
 			if 'userbouquet.' in root.toCompareString():
-				services = serviceHandler.list(root)
-				channels = services and services.getContent("SN", True)
-				channelIndex = CurrentServiceList.servicelist.l.lookupService(CurrentServiceList.servicelist.getCurrent())
-				markersCounter = 0
-				for i in range(channelIndex):
-					if channels[i][0].startswith("1:64:"):
-						markersCounter = markersCounter + 1
-				self.text = str(CurrentServiceList.getBouquetNumOffset(root)+channelIndex+1-markersCounter)
-				CurrentServiceList = None
+				if self.lastService is None or service != self.lastService:
+					self.lastService = service
+					services = serviceHandler.list(root)
+					channels = services and services.getContent("SN", True)
+					channelIndex = CurrentServiceList.servicelist.l.lookupService(CurrentServiceList.servicelist.getCurrent())
+					markersCounter = 0
+					for i in range(channelIndex):
+						if channels[i][0].startswith("1:64:"):
+							markersCounter = markersCounter + 1
+					self.text = str(CurrentServiceList.getBouquetNumOffset(root)+channelIndex+1-markersCounter)
+				return
+		self.text = "---"
