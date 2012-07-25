1ff39f5d9896941fbf531e6da05c336c690a9a28
Paul In&In <darezik@gmail.com>
2012-07-25 17:24:29 +0200

    ChannelNumber: Completely rewrite a new Renderer
    Now it does really count for the service selected in an bouquet
    and is not searching for a service name or reference via a shadow
    database

--- lib/python/Components/Renderer/ChannelNumber.py
+++ lib/python/Components/Renderer/ChannelNumber.py
@@ -1,42 +1,28 @@
 from Components.VariableText import VariableText
-from enigma import eLabel, iServiceInformation, eServiceReference, eServiceCenter
+from enigma import eLabel, eServiceCenter
 from Renderer import Renderer
+from Screens.InfoBar import InfoBar
 
-#
-# borrowed from vali, adapted for openpli
-#
 class ChannelNumber(Renderer, VariableText):
 	def __init__(self):
 		Renderer.__init__(self)
 		VariableText.__init__(self)
-		self.list = []
-		self.getList()
 
 	GUI_WIDGET = eLabel
 
 	def changed(self, what):
+		self.text=""
 		service = self.source.service
-		info = service and service.info()
-		if info is None:
-			self.text = ""
-			return
-		name = info.getName().replace('\xc2\x86', '').replace('\xc2\x87', '')
-		if name in self.list:
-			for idx in range(1, len(self.list)):
-				if name == self.list[idx-1]:
-					self.text = str(idx)
-					break
-		else:
-			self.text = '---'
-
-	def getList(self):
-		serviceHandler = eServiceCenter.getInstance()
-		services = serviceHandler.list(eServiceReference('1:7:1:0:0:0:0:0:0:0:(type == 1) || (type == 17) || (type == 195) || (type == 25) FROM BOUQUET "bouquets.tv" ORDER BY bouquet'))
-		bouquets = services and services.getContent("SN", True)
-		for bouquet in bouquets:
-			services = serviceHandler.list(eServiceReference(bouquet[0]))
+		if service and service.info():
+			serviceHandler = eServiceCenter.getInstance()
+			CurrentServiceList = InfoBar.instance.servicelist
+			root = CurrentServiceList.servicelist.getRoot()
+			services = serviceHandler.list(root)
 			channels = services and services.getContent("SN", True)
-			for channel in channels:
-				if not channel[0].startswith("1:64:"):
-					self.list.append(channel[1].replace('\xc2\x86', '').replace('\xc2\x87', ''))
-
+			channelIndex = CurrentServiceList.servicelist.l.lookupService(CurrentServiceList.servicelist.getCurrent())
+			markersCounter = 0
+			for i in range(channelIndex):
+				if channels[i][0].startswith("1:64:"):
+					markersCounter = markersCounter + 1
+			self.text = str(CurrentServiceList.getBouquetNumOffset(root)+channelIndex+1-markersCounter)
+			CurrentServiceList = None
