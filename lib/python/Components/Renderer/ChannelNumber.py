011fea77ababe50f7b39316c5e8f7c5b4f6f6249
Paul In&In <darezik@gmail.com>
2012-07-25 23:13:45 +0200

    ChannelNumber: Far more better way to save resourses
    In the previous patch I made one of my classic mistakes with
    the lastService
    Why do update this with every event. Do it only when required

--- lib/python/Components/Renderer/ChannelNumber.py
+++ lib/python/Components/Renderer/ChannelNumber.py
@@ -1,32 +1,44 @@
 from Components.VariableText import VariableText
-from enigma import eLabel, eServiceCenter
+from enigma import eLabel, eServiceCenter, iPlayableService
 from Renderer import Renderer
 from Screens.InfoBar import InfoBar
+
+firstChannelNumberClass = True
+text = ""
 
 class ChannelNumber(Renderer, VariableText):
 	def __init__(self):
 		Renderer.__init__(self)
 		VariableText.__init__(self)
-		self.lastService = None
+		self.text = "---"
+		global firstChannelNumberClass
+		self.firstChannelNumberClass  = firstChannelNumberClass
+		firstChannelNumberClass  = False
 
 	GUI_WIDGET = eLabel
 
 	def changed(self, what):
+		global text
+		if what[0] != self.CHANGED_SPECIFIC:
+			return
+		if what[1] != iPlayableService.evStart:
+			return
+		if not self.firstChannelNumberClass:
+			self.text = text
+			return
+		self.text = "---"
 		service = self.source.service
 		if service and service.info():
 			serviceHandler = eServiceCenter.getInstance()
 			CurrentServiceList = InfoBar.instance.servicelist
 			root = CurrentServiceList.servicelist.getRoot()
 			if 'userbouquet.' in root.toCompareString():
-				if self.lastService is None or service != self.lastService:
-					self.lastService = service
-					services = serviceHandler.list(root)
-					channels = services and services.getContent("SN", True)
-					channelIndex = CurrentServiceList.servicelist.l.lookupService(CurrentServiceList.servicelist.getCurrent())
-					markersCounter = 0
-					for i in range(channelIndex):
-						if channels[i][0].startswith("1:64:"):
-							markersCounter = markersCounter + 1
-					self.text = str(CurrentServiceList.getBouquetNumOffset(root)+channelIndex+1-markersCounter)
-				return
-		self.text = "---"
+				services = serviceHandler.list(root)
+				channels = services and services.getContent("SN", True)
+				channelIndex = CurrentServiceList.servicelist.l.lookupService(CurrentServiceList.servicelist.getCurrent())
+				markersCounter = 0
+				for i in range(channelIndex):
+					if channels[i][0].startswith("1:64:"):
+						markersCounter = markersCounter + 1
+				self.text = str(CurrentServiceList.getBouquetNumOffset(root)+channelIndex+1-markersCounter)
+		text = self.text
