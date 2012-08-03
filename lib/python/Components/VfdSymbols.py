882cf3907e685a19985e5c13e4bf33d602d1eddc
Black_64 <black6464@gmail.com>
2012-08-03 13:57:27 +0200

    add vfd symbols recording, subtitels and parental control

---
+++ lib/python/Components/VfdSymbols.py
@@ -0,0 +1,97 @@
+from twisted.internet import threads
+from config import config
+from enigma import eDBoxLCD, eTimer, iPlayableService
+import NavigationInstance
+from Tools.Directories import fileExists
+from Components.ParentalControl import parentalControl
+from Components.ServiceEventTracker import ServiceEventTracker
+
+POLLTIME = 5 # seconds
+
+def SymbolsCheck(session, **kwargs):
+		global symbolspoller
+		symbolspoller = SymbolsCheckPoller(session)
+		symbolspoller.start()
+
+class SymbolsCheckPoller:
+	def __init__(self, session):
+		self.session = session
+		self.timer = eTimer()
+		self.onClose = []
+		self.__event_tracker = ServiceEventTracker(screen=self,eventmap=
+			{
+				iPlayableService.evUpdatedInfo: self.__evUpdatedInfo,
+			})
+
+	def start(self):
+		if self.symbolscheck not in self.timer.callback:
+			self.timer.callback.append(self.symbolscheck)
+		self.timer.startLongTimer(0)
+
+	def stop(self):
+		if self.symbolscheck in self.timer.callback:
+			self.timer.callback.remove(self.symbolscheck)
+		self.timer.stop()
+
+	def symbolscheck(self):
+		threads.deferToThread(self.JobTask)
+		self.timer.startLongTimer(POLLTIME)
+
+	def JobTask(self):
+		self.Recording()
+		self.timer.startLongTimer(POLLTIME)
+
+	def __evUpdatedInfo(self):
+		self.service = self.session.nav.getCurrentService()
+		self.Subtitle()
+		self.ParentalControl()
+
+	def Recording(self):
+		if not fileExists("/proc/stb/lcd/symbol_recording") or not fileExists("/proc/stb/lcd/symbol_record_1") or not fileExists("/proc/stb/lcd/symbol_record_2"):
+			return
+	
+		recordings = len(NavigationInstance.instance.getRecordings())
+
+		if recordings > 0:
+			open("/proc/stb/lcd/symbol_recording", "w").write("1")
+			if recordings == 1:
+				open("/proc/stb/lcd/symbol_record_1", "w").write("1")
+				open("/proc/stb/lcd/symbol_record_2", "w").write("0")
+			elif recordings >= 2:
+				open("/proc/stb/lcd/symbol_record_1", "w").write("1")
+				open("/proc/stb/lcd/symbol_record_2", "w").write("1")
+		else:
+			open("/proc/stb/lcd/symbol_recording", "w").write("0")
+			open("/proc/stb/lcd/symbol_record_1", "w").write("0")
+			open("/proc/stb/lcd/symbol_record_2", "w").write("0")
+
+
+	def Subtitle(self):
+		if not fileExists("/proc/stb/lcd/symbol_smartcard"):
+			return
+
+		subtitle = self.service and self.service.subtitle()
+		subtitlelist = subtitle and subtitle.getSubtitleList()
+
+		if subtitlelist:
+			subtitles = len(subtitlelist)
+			if subtitles > 0:
+				open("/proc/stb/lcd/symbol_smartcard", "w").write("1")
+			else:
+				open("/proc/stb/lcd/symbol_smartcard", "w").write("0")
+		else:
+			open("/proc/stb/lcd/symbol_smartcard", "w").write("0")
+
+	def ParentalControl(self):
+		if not fileExists("/proc/stb/lcd/symbol_parent_rating"):
+			return
+
+		service = self.session.nav.getCurrentlyPlayingServiceReference()
+
+		if service:
+			if parentalControl.getProtectionLevel(service.toCompareString()) == -1:
+				open("/proc/stb/lcd/symbol_parent_rating", "w").write("0")
+			else:
+				open("/proc/stb/lcd/symbol_parent_rating", "w").write("1")
+		else:
+			open("/proc/stb/lcd/symbol_parent_rating", "w").write("0")
