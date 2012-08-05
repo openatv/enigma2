3f46460d5dfcf56959b5f461b7349882bc82a478
Paul In&In <darezik@gmail.com>
2012-08-05 09:49:51 +0200

    change 'satellite receiver' to receiver'
    As it turns out our receivers can do much more than receiving
    from satallite. We are creating a more or les versatile box
    able to recevie not only from satellite but also from cable.
    the air and even internet!

--- lib/python/Plugins/SystemPlugins/SoftwareUpdate/plugin.py
+++ lib/python/Plugins/SystemPlugins/SoftwareUpdate/plugin.py
@@ -30,7 +30,7 @@ class UpdatePlugin(Screen):
 		self["slider"] = self.slider
 		self.activityslider = Slider(0, 100)
 		self["activityslider"] = self.activityslider
-		self.status = StaticText(_("Upgrading Satellite receiver... Please wait"))
+		self.status = StaticText(_("Upgrading receiver... Please wait"))
 		self["status"] = self.status
 		self.package = StaticText()
 		self["package"] = self.package
@@ -128,7 +128,7 @@ class UpdatePlugin(Screen):
 	def exit(self):
 		if not self.ipkg.isRunning():
 			if self.packages != 0 and self.error == 0:
-				self.session.openWithCallback(self.exitAnswer, MessageBox, _("Upgrade finished. Do you want to reboot your Satellite receiver?"))
+				self.session.openWithCallback(self.exitAnswer, MessageBox, _("Upgrade finished. Do you want to reboot your receiver?"))
 			else:
 				self.close()
 			
