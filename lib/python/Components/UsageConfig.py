658683914312a903f29cf19bbf60e7e46735d15c
Paul In&In <darezik@gmail.com>
2012-08-08 10:59:14 +0200

    ServiceList: Add option to display percentage values instead of progressbars
    As this was a big wish on our forum

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -92,9 +92,11 @@ def InitUsageConfig():
 	config.usage.frontend_priority = ConfigSelection(default = "-1", choices = nims)
 	config.misc.disable_background_scan = ConfigYesNo(default = False)
 
-	config.usage.show_event_progress_in_servicelist = ConfigSelection(default = 'right', choices = [
-		('left', _("Left")),
-		('right', _("Right")),
+	config.usage.show_event_progress_in_servicelist = ConfigSelection(default = 'barright', choices = [
+		('barleft', _("Progress Bar Left")),
+		('barright', _("Progress Bar Right")),
+		('percleft', _("Percentage Left")),
+		('percright', _("Percentage Right")),
 		('no', _("No")) ])
 	config.usage.show_channel_numbers_in_servicelist = ConfigYesNo(default = True)
 
