ac544992c1a9fad9235148b244047e34efe73926
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-08-12 00:30:23 +0100

    [hint text] reword some items

--- lib/python/Screens/Recordings_Timershift.py
+++ lib/python/Screens/Recordings_Timershift.py
@@ -136,7 +136,7 @@ class RecordingSettings(Screen,ConfigListScreen):
 			self.list.append(getConfigListEntry(_("Disk space to reserve for recordings (in GB)"), config.usage.movielist_trashcan_reserve, _("Itmes in trashcan will be deleted if less then the set space is available.")))
 		if config.usage.setup_level.index >= 2:
 			self.list.append(getConfigListEntry(_("Recording data sync size"), config.misc.flush_size, _("Only change for debugging; default is 'Off'.")))
-			self.list.append(getConfigListEntry(_("Background delete option"), config.misc.erase_flags, _("Only change for debugging; default is 'Off'.")))
+			self.list.append(getConfigListEntry(_("Background delete option"), config.misc.erase_flags, _("Only change for debugging; default is 'Internal hdd only'.")))
 			self.list.append(getConfigListEntry(_("Background delete speed"), config.misc.erase_speed, _("Only change for debugging; default is '20 MB/s'.")))
 			self.list.append(getConfigListEntry(_("Offline decode delay (ms)"), config.recording.offline_decode_delay, _("Change this value if your smartcard can't doesn't handle off-line decoding well; default is '1000'.")))
 		self["config"].setList(self.list)
