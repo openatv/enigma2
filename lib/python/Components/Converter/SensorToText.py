045607333ad2f88a469919a3b672c9725f21386d
ims <ims21@users.sourceforge.net>
2012-08-15 20:36:35 +0200

    grade for temperature in C and F
    Signed-off-by: Mirakels <mirakels@users.sourceforge.net>

--- lib/python/Components/Converter/SensorToText.py
+++ lib/python/Components/Converter/SensorToText.py
@@ -3,12 +3,14 @@ from Components.Converter.Converter import Converter
 class SensorToText(Converter, object):
 	def __init__(self, arguments):
 		Converter.__init__(self, arguments)
-        
+
 	def getText(self):
 		if self.source.getValue() is None:
 			return ""
-		return "%d %s" % (self.source.getValue(), self.source.getUnit())
-	
+		mark = " "
+		unit = self.source.getUnit()
+		if unit in ('C','F'):
+			mark = str('\xc2\xb0')
+		return "%d%s%s" % (self.source.getValue(), mark, unit)
+
 	text = property(getText)
-        
-        
