53ffd06385232c252c61317240c7d51c42f6ba8e
Black_64 <black6464@gmail.com>
2012-09-08 14:28:21 +0200

    usageconfig: fix merge issue that caused a E2 restart loop

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -192,13 +192,7 @@ def InitUsageConfig():
 					("alpha", _("Alpha")),
 					("number", _("Number"))])
 
-	def refreshServiceList(configElement):
-		from Screens.InfoBar import InfoBar
-		InfoBarInstance = InfoBar.instance
-		if InfoBarInstance is not None:
-			servicelist = InfoBarInstance.servicelist
-			if servicelist:
-				servicelist.setMode()
+	
 	config.usage.show_event_progress_in_servicelist.addNotifier(refreshServiceList)
 	config.usage.show_channel_numbers_in_servicelist.addNotifier(refreshServiceList)
 
@@ -720,3 +714,10 @@ def preferredInstantRecordPath():
 def defaultMoviePath():
 	return config.usage.default_path.value
 
+def refreshServiceList(configElement = None):
+	from Screens.InfoBar import InfoBar
+	InfoBarInstance = InfoBar.instance
+	if InfoBarInstance is not None:
+		servicelist = InfoBarInstance.servicelist
+		if servicelist:
+			servicelist.setMode()
