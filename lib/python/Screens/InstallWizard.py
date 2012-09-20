2213ec7216c757ced76ac5c1260f4399d63818e6
pieterg <pieterg@users.sourceforge.net>
2012-09-20 14:26:09 +0200

    InstallWizard: fix typo

--- lib/python/Screens/InstallWizard.py
+++ lib/python/Screens/InstallWizard.py
@@ -51,7 +51,7 @@ class InstallWizard(Screen, ConfigListScreen):
 			self.createMenu()
 		elif self.index == self.STATE_CHOISE_SOFTCAM:
 			self.enabled = ConfigYesNo(default = True)
-			modes = {"cccam": _("default") + " (CCcam)"), "scam": "scam"}
+			modes = {"cccam": _("default") + " (CCcam)", "scam": "scam"}
 			self.softcam_type = ConfigSelection(choices = modes, default = "cccam")
 			self.createMenu()
 
