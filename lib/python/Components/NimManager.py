09787aa0a75cd78efc685391d89877c90f1e8bf2
Paul In&In <darezik@gmail.com>
2012-09-20 20:42:24 +0200

    NimManager: use 'simple' mode as default for DVB-S (instead of 'nothing')

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1458,7 +1458,7 @@ def InitNimManager(nimmgr):
 			if len(nimmgr.canConnectTo(x)) > 0:
 				config_mode_choices.append(("loopthrough", _("loopthrough to")))
 			nim.advanced = ConfigNothing()
-			tmp = ConfigSelection(config_mode_choices, "nothing")
+			tmp = ConfigSelection(config_mode_choices, "simple")
 			tmp.slot_id = x
 			tmp.addNotifier(configModeChanged, initial_call = False)
 			nim.configMode = tmp
