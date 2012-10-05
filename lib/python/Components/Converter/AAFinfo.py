84f6791d77bf836572bf77c30edcfb28243b60e3
Black_64 <black6464@gmail.com>
2012-10-05 18:12:58 +0200

    add scalevalue to fit the gauge

--- lib/python/Components/Converter/AAFinfo.py
+++ lib/python/Components/Converter/AAFinfo.py
@@ -17,7 +17,7 @@ class AAFinfo(Converter, object):
 	@cached
 	def getValue(self):
 		if self.type == 1:
-			return (100 - self.Harddiskspace())
+			return self.scaleValue(self.Harddiskspace())
 
 	def getText(self):
 		if self.type == 2:
@@ -26,6 +26,25 @@ class AAFinfo(Converter, object):
 
 	value = property(getValue)
 	text = property(getText)
+
+	def scaleValue(self, value):
+		if value > 100:
+			return 100
+		elif value < 0:
+			return 0
+		elif 0 <= value <= 100:
+			# scale to use the upper half of the gauge (0>100 == 75>25)
+			tmp = value / 2
+			if 0 <= tmp <= 25:
+				# 0>25 = 75>100
+				return (tmp + 75)
+			elif 25 < tmp <= 50:
+				# 26>50 = 1>25
+				return (tmp - 25)
+			else:
+				return 0
+		else:
+			return 0
 
 	def Harddiskspace(self):
 		list2 = []
