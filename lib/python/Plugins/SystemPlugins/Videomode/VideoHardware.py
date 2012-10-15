ea71e3d995ee9469ba893dcc198ac61253da1423
Captain <captain.onboard@web.de>
2012-10-15 18:41:42 +0200

    disable yuv for xp1000

--- lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoHardware.py
@@ -133,7 +133,7 @@ class VideoHardware:
 		if self.modes.has_key("DVI-PC") and not self.getModeList("DVI-PC"):
 			print "remove DVI-PC because of not existing modes"
 			del self.modes["DVI-PC"]
-		if getBoxType() == 'et4x00':
+		if getBoxType() == 'et4x00' or getBoxType() == 'xp1000':
 			del self.modes["YPbPr"]
 		if getBoxType() == 'gbquad' or getBoxType() == 'et5x00':
 			del self.modes["Scart"]
