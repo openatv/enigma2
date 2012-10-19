c3d4fd0a912496d087abdf41ba3e95d874f35a94
Paul In&In <darezik@gmail.com>
2012-10-19 19:34:33 +0200

    filepush: Stop sending data to mux after thread termination
    This caused the hangup on the 8000, when paused, it will block
    on write(), so just pressing pause and then jumping would hang.

--- lib/base/filepush.cpp
+++ lib/base/filepush.cpp
@@ -82,8 +82,9 @@ void eFilePushThread::thread()
 		{
 			buf_end = 0;
 			/* Check m_stop after interrupted syscall. */
-			if (m_stop)
+			if (m_stop) {
 				break;
+			}
 			if (errno == EINTR || errno == EBUSY || errno == EAGAIN)
 				continue;
 			if (errno == EOVERFLOW)
@@ -130,6 +131,8 @@ void eFilePushThread::thread()
 				   in stream_mode, think of evtEOF as "buffer underrun occured". */
 			sendEvent(evtEOF);
 
+			if (m_stop)
+				break;
 			if (m_stream_mode)
 			{
 				eDebug("reached EOF, but we are in stream mode. delaying 1 second.");
@@ -148,15 +151,19 @@ void eFilePushThread::thread()
 			/* Write data to mux */
 			int buf_start = 0;
 			filterRecordData(m_buffer, buf_end);
-			while (buf_start != buf_end)
+			while ((buf_start != buf_end) && !m_stop)
 			{
 				int w = write(m_fd_dest, m_buffer + buf_start, buf_end - buf_start);
 
 				if (w <= 0)
 				{
 					/* Check m_stop after interrupted syscall. */
-					if (m_stop)
+					if (m_stop) {
+						w = 0;
+						buf_start = 0;
+						buf_end = 0;
 						break;
+					}
 					if (w < 0 && (errno == EINTR || errno == EAGAIN || errno == EBUSY))
 						continue;
 					eDebug("eFilePushThread WRITE ERROR");
@@ -193,7 +200,7 @@ void eFilePushThread::stop()
 
 	m_stop = 1;
 
-	eDebug("stopping thread."); /* just do it ONCE. it won't help to do this more than once. */
+	eDebug("eFilePushThread stopping thread");
 	sendSignal(SIGUSR1);
 	kill(0);
 }
