162c1c520d41ae32c35a1e64fa4289515ab7eda9
Paul In&In <darezik@gmail.com>
2012-11-04 23:24:01 +0100

    moved to center a spinner

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -212,9 +212,9 @@ int main(int argc, char **argv)
 			}
 		}
 		if (i)
-			my_dc->setSpinner(eRect(ePoint(25, 25), wait[0]->size()), wait, i);
+			my_dc->setSpinner(eRect(ePoint(640, 360), wait[0]->size()), wait, i);
 		else
-			my_dc->setSpinner(eRect(25, 25, 0, 0), wait, 1);
+			my_dc->setSpinner(eRect(640, 360, 0, 0), wait, 1);
 	}
 	
 	gRC::getInstance()->setSpinnerDC(my_dc);
