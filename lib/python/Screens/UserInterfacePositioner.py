3fa736a9fac69831a1a461e3b42f07a473fa7970
Captain <captain.onboard@web.de>
2012-11-11 22:22:02 +0100

    update osd position wizard and setup

--- lib/python/Screens/UserInterfacePositioner.py
+++ lib/python/Screens/UserInterfacePositioner.py
@@ -10,29 +10,119 @@ from os import path
 from enigma import getDesktop, getBoxType
 
 class UserInterfacePositioner(Screen, ConfigListScreen):
-	skin = """
-	<screen name="OSDSetup" position="c-250,c-200" size="500,400">
-		<widget name="config" position="c-175,30" size="350,150" foregroundColor="white" />
-		<ePixmap pixmap="skin_default/buttons/red.png" position="c-230,e-50" zPosition="0" size="140,40" alphatest="on" />
-		<ePixmap pixmap="skin_default/buttons/green.png" position="c-70,e-50" zPosition="0" size="140,40" alphatest="on" />
-		<ePixmap pixmap="skin_default/buttons/yellow.png" position="c+90,e-50" zPosition="0" size="140,40" alphatest="on" />
-		<widget source="key_red" render="Label" position="c-230,e-50" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="red" />
-		<widget source="key_green" render="Label" position="c-70,e-50" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="green" />
-		<widget source="key_yellow" render="Label" position="c+90,e-50" size="140,40" valign="center" halign="center" zPosition="1" font="Regular;20" transparent="1" backgroundColor="yellow" />
-		<ePixmap pixmap="skin_default/div-h.png" position="c-250,e-100" zPosition="1" size="500,2" />
-		<widget source="status" render="Label" position="c-200,e-90" size="400,30" zPosition="10" font="Regular;21" halign="center" valign="center" foregroundColor="white" transparent="1" />
-	</screen>"""
+	if (getDesktop(0).size().width() == 1280):
+		skin = """
+			<screen position="center,center" size="1280,720" backgroundColor="#000000" title="OSD Adjustment" >
+
+				<widget source="title" render="Label" position="200,130" zPosition="1" size="880,60" font="Regular;58" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget name="config" position="150,250" zPosition="1" size="980,150" font="Regular;20" halign="center" valign="center" transparent="1" />
+				
+				<eLabel backgroundColor="red" position="0,0" size="1280,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,719" size="1280,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,0" size="1,720" zPosition="0" />
+				<eLabel backgroundColor="red" position="1279,0" size="1,720" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1230,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,694" size="1230,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1,670" zPosition="0" />
+				<eLabel backgroundColor="green" position="1254,25" size="1,670" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1180,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,669" size="1180,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1,620" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="1229,50" size="1,620" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1130,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,644" size="1130,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1,570" zPosition="0" />
+				<eLabel backgroundColor="blue" position="1204,75" size="1,570" zPosition="0" />
+
+				<eLabel backgroundColor="red" position="189,627" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="green" position="443,627" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="697,627" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="blue" position="951,627" size="140,3" zPosition="0" />
+				<widget source="key_red" render="Label" position="189,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_green" render="Label" position="443,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_yellow" render="Label" position="697,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_blue" render="Label" position="951,605" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+
+			</screen>"""
+
+	elif (getDesktop(0).size().width() == 1024):
+		skin = """
+			<screen position="center,center" size="1024,576" backgroundColor="#000000" title="OSD Adjustment" >
+
+				<widget source="title" render="Label" position="200,100" zPosition="1" size="624,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="config" render="Label" position="100,180" zPosition="1" size="824,50" font="Regular;24" halign="center" valign="center" transparent="1" />
+				
+				<eLabel backgroundColor="red" position="0,0" size="1024,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,575" size="1024,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="red" position="1023,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="974,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,551" size="974,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="green" position="999,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="924,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,526" size="924,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="974,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="874,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,501" size="874,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1,426" zPosition="0" />
+				<eLabel backgroundColor="blue" position="949,75" size="1,426" zPosition="0" />
+
+				<eLabel backgroundColor="red" position="138,477" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="green" position="341,477" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="544,477" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="blue" position="747,477" size="140,3" zPosition="0" />
+				<widget source="key_red" render="Label" position="138,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_green" render="Label" position="341,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_yellow" render="Label" position="544,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_blue" render="Label" position="747,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+			</screen>"""
+
+	else:
+		skin = """
+			<screen position="center,center" size="720,576" backgroundColor="#000000" title="OSD Adjustment" >
+
+				<widget source="title" render="Label" position="75,100" zPosition="1" size="570,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="config" render="Label" position="75,180" zPosition="1" size="570,50" font="Regular;21" halign="center" valign="center" transparent="1" />
+				
+				<eLabel backgroundColor="red" position="0,0" size="720,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,575" size="720,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="red" position="719,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="670,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,551" size="670,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="green" position="694,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="620,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,526" size="620,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="670,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="570,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,501" size="570,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1,426" zPosition="0" />
+				<eLabel backgroundColor="blue" position="645,75" size="1,426" zPosition="0" />
+
+				<eLabel backgroundColor="red" position="80,477" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="green" position="220,477" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="360,477" size="140,3" zPosition="0" />
+				<eLabel backgroundColor="blue" position="500,477" size="140,3" zPosition="0" />
+				<widget source="key_red" render="Label" position="80,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_green" render="Label" position="220,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_yellow" render="Label" position="360,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+				<widget source="key_blue" render="Label" position="500,455" zPosition="1" size="140,22" font="Regular;18" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+
+			</screen>"""
 
 	def __init__(self, session):
-		self.skin = OSDSetup.skin
 		Screen.__init__(self, session)
-		self.setup_title = _("OSD Position Setup")
+		self.setup_title = _("Position Setup")
 		self["status"] = StaticText()
 		self["key_red"] = StaticText(_("Cancel"))
-		self["key_green"] = StaticText(_("OK"))
+		self["key_green"] = StaticText(_("save"))
 		self["key_yellow"] = StaticText(_("Defaults"))
-		self["info1"] = Label(_("Use arrows Up/Down to select"))
-		self["info2"] = Label(_("Use arrows Left/Right to adjust"))
+		
+		self["title"] = StaticText(_("OSD Adjustment"))
 
 		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
 			{
@@ -47,12 +137,12 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 		self.list = []
 		ConfigListScreen.__init__(self, self.list, session = self.session, on_change = self.changedEntry)
 		if SystemInfo["CanChangeOsdAlpha"] == True:
-			self.list.append(getConfigListEntry(_("OSD visibility"), config.osd.alpha, _("This option lets you adjust the transparency of the OSD")))
+			self.list.append(getConfigListEntry(_("User interface visibility"), config.osd.alpha, _("This option lets you adjust the transparency of the user interface")))
 		if SystemInfo["CanChangeOsdPosition"] == True:
-			self.list.append(getConfigListEntry(_("Move Left/Right"), config.osd.dst_left, _("Use the Left/Right buttons on your remote to move the OSD left/right")))
-			self.list.append(getConfigListEntry(_("Width"), config.osd.dst_width, _("Use the Left/Right buttons on your remote to adjust the size of the OSD. Left button decreases the size, Right increases the size.")))
-			self.list.append(getConfigListEntry(_("Move Up/Down"), config.osd.dst_top, _("Use the Left/Right buttons on your remote to move the OSD up/down")))
-			self.list.append(getConfigListEntry(_("Height"), config.osd.dst_height, _("Use the Left/Right buttons on your remote to adjust the size of the OSD. Left button decreases the size, Right increases the size.")))
+			self.list.append(getConfigListEntry(_("Move Left/Right"), config.osd.dst_left, _("Use the Left/Right buttons on your remote to move the user interface left/right")))
+			self.list.append(getConfigListEntry(_("Width"), config.osd.dst_width, _("Use the Left/Right buttons on your remote to adjust the size of the user interface. Left button decreases the size, Right increases the size.")))
+			self.list.append(getConfigListEntry(_("Move Up/Down"), config.osd.dst_top, _("Use the Left/Right buttons on your remote to move the user interface up/down")))
+			self.list.append(getConfigListEntry(_("Height"), config.osd.dst_height, _("Use the Left/Right buttons on your remote to adjust the size of the user interface. Left button decreases the size, Right increases the size.")))
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
 
@@ -60,7 +150,7 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 		if not self.selectionChanged in self["config"].onSelectionChanged:
 			self["config"].onSelectionChanged.append(self.selectionChanged)
 		self.selectionChanged()
-
+		
 	def selectionChanged(self):
 		self["status"].setText(self["config"].getCurrent()[2])
 
--- lib/python/Screens/WizardUserInterfacePositioner.py
+++ lib/python/Screens/WizardUserInterfacePositioner.py
@@ -6,31 +6,107 @@ from Components.Pixmap import Pixmap, MovingPixmap, MultiPixmap
 from Components.Sources.Boolean import Boolean
 from Tools.Directories import resolveFilename, SCOPE_SKIN
 from Components.config import config, configfile
+from enigma import getDesktop, getBoxType
+from Components.Sources.StaticText import StaticText
 
 class WizardUserInterfacePositioner(WizardLanguage, Rc):
-	skin = """
-		<screen position="0,0" size="e,e" backgroundColor="blue">
-			<widget name="config" position="c-175,c-75" size="350,150" foregroundColor="black" backgroundColor="blue" />
-			<widget source="status" render="Label" position="c-300,e-170" size="600,60" zPosition="10" font="Regular;21" halign="center" valign="center" foregroundColor="black" backgroundColor="blue" transparent="1" />
-			<widget name="text" position="153,40" size="340,300" font="Regular;22"  foregroundColor="black" backgroundColor="blue" />
-			<widget source="list" render="Listbox" position="53,340" size="440,180"  foregroundColor="black" backgroundColor="blue" scrollbarMode="showOnDemand" >
-				<convert type="StringList" />
-			</widget>
-			<widget name="config" position="53,340" zPosition="1" size="440,180"  foregroundColor="black" backgroundColor="blue" transparent="1" scrollbarMode="showOnDemand" />
-			<widget name="wizard" pixmap="skin_default/wizard.png" position="40,50" zPosition="10" size="110,174" alphatest="on" />
-			<widget name="rc" pixmaps="skin_default/rc.png,skin_default/rcold.png" position="e-300,c-250" zPosition="10" size="154,500" alphatest="on" />
-			<widget name="arrowdown" pixmap="skin_default/arrowdown.png" position="-100,-100" zPosition="11" size="37,70" alphatest="on" />
-			<widget name="arrowup" pixmap="skin_default/arrowup.png" position="-100,-100" zPosition="11" size="37,70" alphatest="on" />
-		</screen>"""
+	if (getDesktop(0).size().width() == 1280):
+		skin = """
+			<screen position="center,center" size="1280,720" backgroundColor="#000000" title="OSD Adjustment" >
+
+				<widget source="title" render="Label" position="200,130" zPosition="1" size="880,60" font="Regular;58" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget name="config" position="150,250" zPosition="1" size="980,150" font="Regular;20" halign="center" valign="center" transparent="1" />
+				<widget source="info1" render="Label" position="200,450" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info2" render="Label" position="200,480" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info3" render="Label" position="200,510" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				
+				<eLabel backgroundColor="red" position="0,0" size="1280,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,719" size="1280,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,0" size="1,720" zPosition="0" />
+				<eLabel backgroundColor="red" position="1279,0" size="1,720" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1230,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,694" size="1230,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1,670" zPosition="0" />
+				<eLabel backgroundColor="green" position="1254,25" size="1,670" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1180,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,669" size="1180,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1,620" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="1229,50" size="1,620" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1130,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,644" size="1130,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1,570" zPosition="0" />
+				<eLabel backgroundColor="blue" position="1204,75" size="1,570" zPosition="0" />
+
+			</screen>"""
+
+	elif (getDesktop(0).size().width() == 1024):
+		skin = """
+			<screen position="center,center" size="1024,576" backgroundColor="#000000" title="OSD Adjustment" >
+
+				<widget source="title" render="Label" position="200,100" zPosition="1" size="624,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="config" render="Label" position="100,180" zPosition="1" size="824,50" font="Regular;24" halign="center" valign="center" transparent="1" />
+				<widget source="info1" render="Label" position="200,450" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info2" render="Label" position="200,480" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info3" render="Label" position="200,510" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				
+				<eLabel backgroundColor="red" position="0,0" size="1024,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,575" size="1024,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="red" position="1023,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="974,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,551" size="974,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="green" position="999,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="924,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,526" size="924,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="974,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="874,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,501" size="874,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1,426" zPosition="0" />
+				<eLabel backgroundColor="blue" position="949,75" size="1,426" zPosition="0" />
+
+			</screen>"""
+
+	else:
+		skin = """
+			<screen position="center,center" size="720,576" backgroundColor="#000000" title="OSD Adjustment" >
+
+				<widget source="title" render="Label" position="75,100" zPosition="1" size="570,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="config" render="Label" position="75,180" zPosition="1" size="570,50" font="Regular;21" halign="center" valign="center" transparent="1" />
+				<widget source="info1" render="Label" position="75,450" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info2" render="Label" position="75,480" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info3" render="Label" position="75,510" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				
+				<eLabel backgroundColor="red" position="0,0" size="720,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,575" size="720,1" zPosition="0" />
+				<eLabel backgroundColor="red" position="0,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="red" position="719,0" size="1,576" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="670,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,551" size="670,1" zPosition="0" />
+				<eLabel backgroundColor="green" position="25,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="green" position="694,25" size="1,526" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="620,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,526" size="620,1" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="50,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="yellow" position="670,50" size="1,476" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="570,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,501" size="570,1" zPosition="0" />
+				<eLabel backgroundColor="blue" position="75,75" size="1,426" zPosition="0" />
+				<eLabel backgroundColor="blue" position="645,75" size="1,426" zPosition="0" />
+
+			</screen>"""
 	def __init__(self, session, interface = None):
 		self.xmlfile = resolveFilename(SCOPE_SKIN, "userinterfacepositionerwizard.xml")
 		WizardLanguage.__init__(self, session, showSteps = False, showStepSlider = False)
 		Rc.__init__(self)
 		self.session = session
 		self["wizard"] = Pixmap()
-		self["HelpWindow"] = Pixmap()
-		self["HelpWindow"].hide()
-		self["VKeyIcon"] = Boolean(False)
+		
+		self["title"] = StaticText(_("OSD Adjustment"))
+		self["info1"] = StaticText(_("Use arrows Up/Down to select"))
+		self["info2"] = StaticText(_("Use arrows Left/Right to adjust"))
+		self["info3"] = StaticText(_("Press OK for save"))
 
 		self.NextStep = None
 		self.Text = None
