262a99f126d1127a5e0a4cd8fd52d27919947379
Captain <captain.onboard@web.de>
2012-11-11 23:33:05 +0100

    fix 3d menu update german translations

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,6 +1,5 @@
 from Wizard import wizardManager
 from Screens.WizardLanguage import WizardLanguage
-from Screens.WizardUserInterfacePositioner import WizardUserInterfacePositioner
 from Screens.Rc import Rc
 from Screens.Screen import Screen
 
@@ -37,6 +36,5 @@ class StartWizard(WizardLanguage, Rc):
 		config.misc.firstrun.save()
 		configfile.save()
 
-wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.getValue(), priority = 2)
-wizardManager.registerWizard(WizardUserInterfacePositioner, config.misc.firstrun.getValue(), priority = 1)
+wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.getValue(), priority = 0)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.getValue(), priority = 20)
--- lib/python/Screens/UserInterfacePositioner.py
+++ lib/python/Screens/UserInterfacePositioner.py
@@ -16,6 +16,8 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 
 				<widget source="title" render="Label" position="200,130" zPosition="1" size="880,60" font="Regular;58" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
 				<widget name="config" position="150,250" zPosition="1" size="980,150" font="Regular;20" halign="center" valign="center" transparent="1" />
+				<widget source="info1" render="Label" position="200,450" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info2" render="Label" position="200,480" zPosition="1" size="880,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
 				
 				<eLabel backgroundColor="red" position="0,0" size="1280,1" zPosition="0" />
 				<eLabel backgroundColor="red" position="0,719" size="1280,1" zPosition="0" />
@@ -51,6 +53,8 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 
 				<widget source="title" render="Label" position="200,100" zPosition="1" size="624,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
 				<widget source="config" render="Label" position="100,180" zPosition="1" size="824,50" font="Regular;24" halign="center" valign="center" transparent="1" />
+				<widget source="info1" render="Label" position="200,450" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info2" render="Label" position="200,480" zPosition="1" size="624,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
 				
 				<eLabel backgroundColor="red" position="0,0" size="1024,1" zPosition="0" />
 				<eLabel backgroundColor="red" position="0,575" size="1024,1" zPosition="0" />
@@ -85,6 +89,8 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 
 				<widget source="title" render="Label" position="75,100" zPosition="1" size="570,40" font="Regular;38" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
 				<widget source="config" render="Label" position="75,180" zPosition="1" size="570,50" font="Regular;21" halign="center" valign="center" transparent="1" />
+				<widget source="info1" render="Label" position="75,450" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
+				<widget source="info2" render="Label" position="75,480" zPosition="1" size="570,40" font="Regular;21" halign="center" valign="center" foregroundColor="yellow" backgroundColor="#1f771f" transparent="1" />
 				
 				<eLabel backgroundColor="red" position="0,0" size="720,1" zPosition="0" />
 				<eLabel backgroundColor="red" position="0,575" size="720,1" zPosition="0" />
@@ -123,6 +129,8 @@ class UserInterfacePositioner(Screen, ConfigListScreen):
 		self["key_yellow"] = StaticText(_("Defaults"))
 		
 		self["title"] = StaticText(_("OSD Adjustment"))
+		self["info1"] = StaticText(_("Use arrows Up/Down to select"))
+		self["info2"] = StaticText(_("Use arrows Left/Right to adjust"))
 
 		self["actions"] = ActionMap(["SetupActions", "ColorActions"],
 			{
