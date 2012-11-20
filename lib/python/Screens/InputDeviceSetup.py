ef444239e99a13bca58204a92d6250e9bcb64d75
Black_64 <black6464@gmail.com>
2012-11-20 09:52:44 +0100

    [inputdevicesetup] add maram9 and xp1000 rc

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -297,9 +297,14 @@ class InputDeviceSetup(Screen, ConfigListScreen):
 class RemoteControlType(Screen, ConfigListScreen):
 	rcList = [
 			("0", _("Default")),
-			("4", _("DMM normal")), ("6", _("DMM advanced")),
-			("11", "et9x00/6500"), ("7", "et5000/6000"), ("13", "et4000"),
-			("8", "VU+"),
+			("3", _("MaraM9")),
+			("4", _("DMM normal")),
+			("6", _("DMM advanced")),
+			("7", _("et5000/6000")),
+			("8", _("VU+")),
+			("11", _("et9x00/6500")),
+			("13", _("et4000")),
+			("14", _("XP1000")),
 		]
 
 	defaultRcList = [
