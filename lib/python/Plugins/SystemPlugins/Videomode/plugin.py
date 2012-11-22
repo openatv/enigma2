34eaf142d7846ff79ab239c08c4e476970ea77ea
Paul In&In <darezik@gmail.com>
2012-11-22 15:31:32 +0100

    Videomode: fix vcrswitch config description

--- lib/python/Plugins/SystemPlugins/Videomode/plugin.py
+++ lib/python/Plugins/SystemPlugins/Videomode/plugin.py
@@ -95,7 +95,7 @@ class VideoSetup(Screen, ConfigListScreen):
 			if level >= 1:
 				self.list.append(getConfigListEntry(_("WSS on 4:3"), config.av.wss, _("When enabled, content with an aspect ratio of 4:3 will be stretched to fit the screen.")))
 				if SystemInfo["ScartSwitch"]:
-					self.list.append(getConfigListEntry(_("Auto scart switching"), config.av.vcrswitch, _("When enabled, your TV will automatically select the SCART input when you switch on your receiver.")))
+					self.list.append(getConfigListEntry(_("Auto scart switching"), config.av.vcrswitch, _("When enabled, your receiver will detect activity on the VCR SCART input.")))
 
 		if level >= 1:
 			if SystemInfo["CanDownmixAC3"]:
