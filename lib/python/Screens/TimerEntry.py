64eea4d81adde3f6761d595233aa2484b4b97fb4
Paul In&In <darezik@gmail.com>
2012-11-25 19:16:10 +0000

    fix typo.

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -89,7 +89,7 @@ class TimerEntry(Screen, ConfigListScreen):
 					count = 0
 					for x in (0, 1, 2, 3, 4, 5, 6):
 						if flags == 1: # weekly
-÷							print "Set to weekday " + str(x)
+# 							print "Set to weekday " + str(x)
 							weekday = x
 						if flags & 1 == 1: # set user defined flags
 							day[x] = 1
