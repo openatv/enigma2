88f0a3080a4909012783252f5b03f4345629f435
Andy Blackburn <andrew.blackburn644@btinternet.com>
2012-12-04 19:31:35 +0000

    [Trashcan] fix possible tread crash.

--- lib/python/Tools/Trashcan.py
+++ lib/python/Tools/Trashcan.py
@@ -32,8 +32,11 @@ def get_size(start_path = '.'):
 	total_size = 0
 	for dirpath, dirnames, filenames in os.walk(start_path):
 		for f in filenames:
-			fp = os.path.join(dirpath, f)
-			total_size += os.path.getsize(fp)
+			try:
+				fp = os.path.join(dirpath, f)
+				total_size += os.path.getsize(fp)
+			except:
+				pass
 	return total_size
 
 class Trashcan:
