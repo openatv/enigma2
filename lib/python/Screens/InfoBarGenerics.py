openatv/enigma2

commit 35ff379b69e7743dd9d15542fa439c03fcfa7444

Navigation: Make true selected reference default
As this is most required after all. Also revert the previous commits
where true was set.

lib/python/Screens/InfoBarGenerics.py
@@ -1653,7 +1653,7 @@ class InfoBarPiP:
 				del self.session.pip
 
 	def swapPiP(self):
-		swapservice = self.session.nav.getCurrentlyPlayingServiceReference(True)
+		swapservice = self.session.nav.getCurrentlyPlayingServiceReference()
 		pipref = self.session.pip.getCurrentService()
 		if swapservice and pipref and pipref.toString() != swapservice.toString():
 			currentServicePath = self.servicelist.getCurrentServicePath()

Navigation.py
@@ -95,7 +95,7 @@ class Navigation:
 			self.stopService()
 		return 1
 	
-	def getCurrentlyPlayingServiceReference(self, selected = False):
+	def getCurrentlyPlayingServiceReference(self, selected = True):
 		if selected and self.currentlyPlayingServiceReference:
 			return self.currentlyPlayingSelectedServiceReference
 		return self.currentlyPlayingServiceReference

lib/python/Screens/ScanSetup.py
@@ -319,7 +319,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 		del self.feinfo
 		del self.service
 
-		self.session.postScanService = session.nav.getCurrentlyPlayingServiceReference(True)
+		self.session.postScanService = session.nav.getCurrentlyPlayingServiceReference()
 
 		self["actions"] = NumberActionMap(["SetupActions", "MenuActions"],
 		{

lib/python/Screens/ServiceInfo.py
@@ -105,8 +105,8 @@ class ServiceInfo(Screen):
 	def information(self):
 		if self.type == TYPE_SERVICE_INFO:
 			if self.session.nav.getCurrentlyPlayingServiceReference():
-				name = ServiceReference(self.session.nav.getCurrentlyPlayingServiceReference()).getServiceName()
-				refstr = self.session.nav.getCurrentlyPlayingServiceReference().toString()
+				name = ServiceReference(self.session.nav.getCurrentlyPlayingServiceReference(False)).getServiceName()
+				refstr = self.session.nav.getCurrentlyPlayingServiceReference(False).toString()
 			else:
 				name = _("N/A")
 				refstr = _("N/A")

commit 22a6226a63dba2839fec2fb51df401b409798962

InfoBarGenerics: fix zapping through a list of services with alternatives
broken since 35ff379b69e7743dd9d15542fa439c03fcfa7444

TODO: check all other users of getCurrentlyPlayingServiceReference()
With the default arguments, this function might return a group reference
instead of a service reference, e.g.:
1:134:1:0:0:0:0:0:0:0:FROM BOUQUET "alternatives.ned1_hd.tv" ORDER BY bouquet
instead of
1:0:19:1B6C:C88:3:EB0000:0:0:0:
Which might not be what the caller expects; this function used to
return the currently playing service reference, not the group reference
of which the currently playing service happens to be a member.

lib/python/Screens/InfoBarGenerics.py
@@ -525,7 +525,7 @@ class InfoBarChannelSelection:
 
 	def isPlayable(self, ref):
 		if not (ref.flags & eServiceReference.isMarker):
-			cur_running = self.session.nav.getCurrentlyPlayingServiceReference()
+			cur_running = self.session.nav.getCurrentlyPlayingServiceReference(False)
 			if not cur_running:
 				cur_running = eServiceReference()
 			info = eServiceCenter.getInstance().info(ref)