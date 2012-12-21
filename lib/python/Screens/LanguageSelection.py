cf2f27e17df3f0ff97b8bc6e11f01a720430ce37
Mike Looijmans <milo-software@users.sourceforge.net>
2012-12-21 20:47:20 +0100

    LanguageSelection: Make it look faster
    Only load a language when the bar lingers for more than half a second.
    Makes it smoother to browse through the list.

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -1,19 +1,15 @@
 import gettext
-
 from Screen import Screen
-
 from Components.ActionMap import ActionMap
 from Components.Language import language
 from Components.config import config
 from Components.Sources.List import List
 from Components.Label import Label
 from Components.Pixmap import Pixmap
-
 from Screens.Rc import Rc
-
 from Tools.Directories import resolveFilename, SCOPE_CURRENT_SKIN, SCOPE_LANGUAGE
-
 from Tools.LoadPixmap import LoadPixmap
+import enigma
 
 def LanguageEntryComponent(file, name, index):
 	png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "countries/" + index + ".png"))
@@ -43,25 +39,26 @@ class LanguageSelection(Screen):
 			"ok": self.save,
 			"cancel": self.cancel,
 		}, -1)
+		self.selectDelay = enigma.eTimer()
+		self.selectDelay.callback.append(self.run)
 
 	def selectActiveLanguage(self):
 		activeLanguage = language.getActiveLanguage()
 		pos = 0
-		for x in self.list:
+		for pos, x in enumerate(self.list):
 			if x[0] == activeLanguage:
 				self["languages"].index = pos
 				break
-			pos += 1
 
 	def save(self):
-		self.run()
+		self.commit(self.run())
 		self.close()
 
 	def cancel(self):
 		language.activateLanguage(self.oldActiveLanguage)
 		self.close()
 
-	def run(self, justlocal = False):
+	def run(self):
 		print "updating language..."
 		lang = self["languages"].getCurrent()[0]
 		if lang != config.osd.language.value:
@@ -69,14 +66,13 @@ class LanguageSelection(Screen):
 			config.osd.language.save()
 			self.catalog = gettext.translation('enigma2', resolveFilename(SCOPE_LANGUAGE, ""), languages=[config.osd.language.value])
 		self.setTitle(self.catalog.gettext("Language selection"))
+		return lang
 
-		if justlocal:
-			return
-
+	def commit(self, lang):
+		print "commit language"
 		language.activateLanguage(lang)
 		config.misc.languageselected.value = 0
 		config.misc.languageselected.save()
-		print "ok"
 
 	def updateList(self):
 		languageList = language.getLanguageList()
@@ -88,14 +84,13 @@ class LanguageSelection(Screen):
 		self["languages"].list = list
 
 	def changed(self):
-		self.run(justlocal = True)
+		self.selectDelay.start(500, True)
 
 class LanguageWizard(LanguageSelection, Rc):
 	def __init__(self, session):
 		LanguageSelection.__init__(self, session)
 		Rc.__init__(self)
 		self.onLayoutFinish.append(self.selectKeys)
-
 		self["wizard"] = Pixmap()
 		self["text"] = Label()
 		self.setText()
@@ -106,7 +101,7 @@ class LanguageWizard(LanguageSelection, Rc):
 		self.selectKey("DOWN")
 
 	def changed(self):
-		self.run(justlocal = True)
+		self.run()
 		self.setText()
 
 	def setText(self):
