bf7f810098712f8e2ed3c688e04fe9385a1ac537
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-01-18 15:19:07 +0000

    [ViX] add new VIXMENU plug location.

--- lib/python/Plugins/Plugin.py
+++ lib/python/Plugins/Plugin.py
@@ -66,6 +66,8 @@ class PluginDescriptor:
 	# or return a function which is called with session and the interface name for extended setup of this interface
 	WHERE_NETWORKMOUNTS = 15
 
+	WHERE_VIXMENU = 16
+
 	def __init__(self, name = "Plugin", where = [ ], description = "", icon = None, fnc = None, wakeupfnc = None, needsRestart = None, internal = False, weight = 0):
 		self.name = name
 		self.internal = internal
