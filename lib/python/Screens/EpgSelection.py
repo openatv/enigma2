842ca493889d511b72b40f6df0c33d906327a532
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-01-24 17:40:54 +0000

    [EpgSelection] fix issue where exiting out of SingleEPG, that was opened inside ChannelSelection, would not return you to ChannelSelection.

--- lib/python/Screens/EpgSelection.py
+++ lib/python/Screens/EpgSelection.py
@@ -562,7 +562,10 @@ class EPGSelection(Screen, HelpableScreen):
 					self.setServicelistSelection(self.StartBouquet, self.StartRef)
 				else:
 					self.session.nav.playService(self.StartRef)
-		self.close(True)
+		if self.type == EPG_TYPE_SINGLE:
+			self.close(False)
+		else:
+			self.close(True)
 
 	def infoKeyPressed(self):
 		cur = self['list'].getCurrent()
