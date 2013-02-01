a00790057764103cc9b05dff5ddcd518a7b41e22
Paul In&In <darezik@gmail.com>
2013-02-01 13:29:49 +0000

    [skin] fix default skin name.

--- skin.py
+++ skin.py
@@ -68,7 +68,7 @@ def skin_user_skinname():
 
 # example: loadSkin("nemesis_greenline/skin.xml")
 config.skin = ConfigSubsection()
-DEFAULT_SKIN = "ViX_Night_HD/skin.xml"
+DEFAULT_SKIN = "ViX-Night-HD/skin.xml"
 # on SD hardware, ViX Night HD will not be available
 if not fileExists(resolveFilename(SCOPE_SKIN, DEFAULT_SKIN)):
 	# in that case, fallback to Magic (which is an SD skin)
