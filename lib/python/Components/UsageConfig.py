c4cf9d67b4ad7544561a97e95bb6ee585789627e
Black_64 <black6464@gmail.com>
2013-02-02 23:30:53 +0100

    [UsageConfig] fix translation

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -524,7 +524,7 @@ def InitUsageConfig():
 		("hrv", _("Croatian")),
 		("ces cze", _("Czech")),
 		("dan", _("Danish")),
-		("dut ndl Deutsch", _("Dutch")),
+		("dut ndl Dutch", _("Dutch")),
 		("eng qaa Englisch", _("English")),
 		("est", _("Estonian")),
 		("fin", _("Finnish")),
