5adc67177457a5e55591ef2af04934b3d18a129e
Paul In&In <darezik@gmail.com>
2013-02-28 23:03:42 +0100

    flag was_timerwakeup

--- Navigation.py
+++ Navigation.py
@@ -30,7 +30,9 @@ class Navigation:
 		self.currentlyPlayingServiceOrGroup = None
 		self.currentlyPlayingService = None
 		self.RecordTimer = RecordTimer.RecordTimer()
+		self.__wasTimerWakeup = False
 		if getFPWasTimerWakeup():
+			self.__wasTimerWakeup = True
 			if nextRecordTimerAfterEventActionAuto:
 				# We need to give the systemclock the chance to sync with the transponder time,
 				# before we will make the decision about whether or not we need to shutdown
@@ -38,6 +40,9 @@ class Navigation:
 				self.recordshutdowntimer = eTimer()
 				self.recordshutdowntimer.callback.append(self.checkShutdownAfterRecording)
 				self.recordshutdowntimer.start(30000, True)
+
+	def wasTimerWakeup(self):
+		return self.__wasTimerWakeup
 
 	def checkShutdownAfterRecording(self):
 		if len(self.getRecordings()) or abs(self.RecordTimer.getNextRecordingTime() - time()) <= 360:
