ff918438f10cc39cdc2bd327f5c4374ef6409d45
Paul In&In <darezik@gmail.com>
2013-03-12 18:53:23 +0100

    Added progress bar on update

--- lib/python/Screens/SoftwareUpdate.py
+++ lib/python/Screens/SoftwareUpdate.py
@@ -1,119 +1,29 @@
-import Components.Task
 from Screens.ChoiceBox import ChoiceBox
 from Screens.MessageBox import MessageBox
 from Screens.Screen import Screen
-from Screens.Standby import TryQuitMainloop
-from Components.About import about
-from Components.ActionMap import ActionMap
-from Components.Button import Button
-from Components.config import config
-from Components.Console import Console
+from Screens.Standby import TryQuitMainloop 
+from Components.ActionMap import ActionMap, NumberActionMap
 from Components.Ipkg import IpkgComponent
-from Components.ScrollLabel import ScrollLabel
 from Components.Sources.StaticText import StaticText
 from Components.Slider import Slider
-from enigma import eTimer, eDVBDB
-from os import rename, path, remove
-from gettext import dgettext
-import urllib
-
-class SoftwareUpdateChanges(Screen):
-	def __init__(self, session, args = None):
-		Screen.__init__(self, session)
-		self.setTitle(_("OE Changes"))
-		if path.exists('/tmp/oe-git.log'):
-			remove('/tmp/oe-git.log')
-		if path.exists('/tmp/e2-git.log'):
-			remove('/tmp/e2-git.log')
-		self.logtype = 'oe'
-		self["text"] = ScrollLabel()
-		self['title_summary'] = StaticText()
-		self['text_summary'] = StaticText()
-		self["key_red"] = Button(_("Close"))
-		self["key_green"] = Button(_("Update"))
-		self["key_yellow"] = Button(_("Show E2 Log"))
-		self["myactions"] = ActionMap(['ColorActions', 'OkCancelActions', 'DirectionActions'],
-		{
-			'cancel': self.closeRecursive,
-			"red": self.closeRecursive,
-			"green": self.unattendedupdate,
-			"yellow": self.changelogtype,
-			"left": self.pageUp,
-			"right": self.pageDown,
-			"down": self.pageDown,
-			"up": self.pageUp
-		},-1)
-		self.onLayoutFinish.append(self.getlog)
-
-	def changelogtype(self):
-		if self.logtype == 'oe':
-			self["key_yellow"].setText(_("Show OE Log"))
-			self.setTitle(_("Enimga2 Changes"))
-			self.logtype = 'e2'
-		else:
-			self["key_yellow"].setText(_("Show E2 Log"))
-			self.setTitle(_("OE Changes"))
-			self.logtype = 'oe'
-		self.getlog()
-
-	def pageUp(self):
-		self["text"].pageUp()
-
-	def pageDown(self):
-		self["text"].pageDown()
-
-	def getlog(self):
-		try:
-			sourcefile = 'http://enigma2.world-of-satellite.com/feeds/' + about.getImageVersionString() + '/' + self.logtype + '-git.log'
-			sourcefile,headers = urllib.urlretrieve(sourcefile)
-			rename(sourcefile,'/tmp/' + self.logtype + '-git.log')
-
-			fd = open('/tmp/' + self.logtype + '-git.log', 'r')
-			releasenotes = fd.read()
-			fd.close()
-		except:
-			releasenotes = '404 Not Found'
-		if releasenotes.find('404 Not Found') == -1:
-			releasenotes = releasenotes.replace('\nopenvix: build',"\n\nopenvix: build")
-			releasenotes = releasenotes.split('\n\n')
-			ver = -1
-			releasever = ""
-			viewrelease=""
-			while not releasever.isdigit():
-				ver += 1
-				releasever = releasenotes[int(ver)].split('\n')
-				releasever = releasever[0].split(' ')
-				releasever = releasever[2].replace(':',"")
-
-			while int(releasever) > int(about.getBuildVersionString()):
-				viewrelease += releasenotes[int(ver)]+'\n\n'
-				ver += 1
-				releasever = releasenotes[int(ver)].split('\n')
-				releasever = releasever[0].split(' ')
-				releasever = releasever[2].replace(':',"")
-			self["text"].setText(viewrelease)
-			summarytext = viewrelease.split(':\n')
-			try:
-				self['title_summary'].setText(summarytext[0]+':')
-				self['text_summary'].setText(summarytext[1])
-			except:
-				self['title_summary'].setText("")
-				self['text_summary'].setText("")
-		else:
-			self['title_summary'].setText("")
-			self['text_summary'].setText(_("Error downloading change log."))
-			self['text'].setText(_("Error downloading change log."))
-
-	def unattendedupdate(self):
-		self.close((_("Unattended upgrade without GUI and reboot system"), "cold"))
-
-	def closeRecursive(self):
-		self.close(("menu", "menu"))
+from enigma import eTimer, getBoxType, eDVBDB
+from urllib import urlopen
+import socket
+import os
+import re
+import time
 
 class UpdatePlugin(Screen):
+	skin = """
+		<screen name="UpdatePlugin" position="center,center" size="550,300">
+			<widget name="activityslider" position="0,0" size="550,5"  />
+			<widget name="slider" position="0,150" size="550,30"  />
+			<widget source="package" render="Label" position="10,30" size="540,20" font="Regular;18" halign="center" valign="center" backgroundColor="#25062748" transparent="1" />
+			<widget source="status" render="Label" position="10,180" size="540,100" font="Regular;20" halign="center" valign="center" backgroundColor="#25062748" transparent="1" />
+		</screen>"""
+
 	def __init__(self, session, *args):
 		Screen.__init__(self, session)
-		Screen.setTitle(self, _("Software Update"))
 
 		self.sliderPackages = { "dreambox-dvb-modules": 1, "enigma2": 2, "tuxbox-image-info": 3 }
 
@@ -128,52 +38,64 @@ class UpdatePlugin(Screen):
 		self["package"] = self.package
 		self.oktext = _("Press OK on your remote control to continue.")
 
-		self.channellist_only = 0
-		self.channellist_name = ''
-		self.SettingsBackupDone = False
-		self.ImageBackupDone = False
-		self.autobackuprunning = False
-
 		self.packages = 0
 		self.error = 0
 		self.processed_packages = []
 		self.total_packages = None
-		self.checkNetworkState()
 
-	def checkNetworkState(self):
-		cmd1 = "opkg update"
-		self.CheckConsole = Console()
-		self.CheckConsole.ePopen(cmd1, self.checkNetworkStateFinished)
-
-	def checkNetworkStateFinished(self, result, retval,extra_args=None):
-		if result.find('bad address') != -1:
-			self.session.openWithCallback(self.close, MessageBox, _("Your STB_BOX is not connected to the internet, please check your network settings and try again."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
-		elif result.find('wget returned 1') != -1 or result.find('wget returned 255') != -1 or result.find('404 Not Found') != -1:
-			self.session.openWithCallback(self.close, MessageBox, _("Sorry feeds are down for maintenance, please try again later."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
-		elif result.find('Collected errors') != -1:
-			self.session.openWithCallback(self.close, MessageBox, _("A background update check is is progress, please wait a few minutes and try again."), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
-		else:
-			self.startCheck()
-
-	def startCheck(self):
-		self.activity = 0
-		self.activityTimer = eTimer()
-		self.activityTimer.callback.append(self.doActivityTimer)
-
+		self.channellist_only = 0
+		self.channellist_name = ''
+		self.updating = False
 		self.ipkg = IpkgComponent()
 		self.ipkg.addCallback(self.ipkgCallback)
+		self.onClose.append(self.__close)
 
-		self.updating = False
-
-		self["actions"] = ActionMap(["WizardActions"],
+		self["actions"] = ActionMap(["WizardActions"], 
 		{
 			"ok": self.exit,
 			"back": self.exit
 		}, -1)
+		
+		self.activity = 0 
+		self.activityTimer = eTimer()
+		self.activityTimer.callback.append(self.checkTraficLight)
+		self.activityTimer.callback.append(self.doActivityTimer)
+		self.activityTimer.start(100, True)
 
-		self.updating = True
+	def checkTraficLight(self):
+	
+		self.activityTimer.callback.remove(self.checkTraficLight)
 		self.activityTimer.start(100, False)
-		self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
+
+		currentTimeoutDefault = socket.getdefaulttimeout()
+		socket.setdefaulttimeout(3)
+		message = ""
+		picon = None
+		default = True
+		try:
+			# TODO: Use Twisted's URL fetcher, urlopen is evil. And it can
+			# run in parallel to the package update.
+			if getBoxType() in urlopen("http://openpli.org/status").read().split(','):
+				message = _("The current beta image might not be stable.\nFor more information see %s.") % ("www.openpli.org")
+				picon = MessageBox.TYPE_ERROR
+				default = False
+		except:
+			message = _("The status of the current beta image could not be checked because %s can not be reached.") % ("www.openpli.org")
+			picon = MessageBox.TYPE_ERROR
+			default = False
+		socket.setdefaulttimeout(currentTimeoutDefault)
+		if default:
+			self.startActualUpdate(True)
+		else:
+			message += "\n" + _("Do you want to update your STB_BOX?")
+			self.session.openWithCallback(self.startActualUpdate, MessageBox, message, default = default, picon = picon)
+
+	def startActualUpdate(self,answer):
+		if answer:
+			self.updating = True
+			self.ipkg.startCmd(IpkgComponent.CMD_UPDATE)
+		else:
+			self.close()
 
 	def doActivityTimer(self):
 		self.activity += 1
@@ -210,10 +132,9 @@ class UpdatePlugin(Screen):
 		elif event == IpkgComponent.EVENT_CONFIGURING:
 			self.package.setText(param)
 			self.status.setText(_("Configuring"))
-
 		elif event == IpkgComponent.EVENT_MODIFIED:
-			if config.plugins.softwaremanager.overwriteConfigFiles.getValue() in ("N", "Y"):
-				self.ipkg.write(True and config.plugins.softwaremanager.overwriteConfigFiles.getValue())
+			if config.plugins.softwaremanager.overwriteConfigFiles.value in ("N", "Y"):
+				self.ipkg.write(True and config.plugins.softwaremanager.overwriteConfigFiles.value)
 			else:
 				self.session.openWithCallback(
 					self.modificationCallback,
@@ -227,34 +148,17 @@ class UpdatePlugin(Screen):
 				self.updating = False
 				self.ipkg.startCmd(IpkgComponent.CMD_UPGRADE_LIST)
 			elif self.ipkg.currentCommand == IpkgComponent.CMD_UPGRADE_LIST:
-				from urllib import urlopen
-				import socket
-				currentTimeoutDefault = socket.getdefaulttimeout()
-				socket.setdefaulttimeout(3)
-				try:
-					config.softwareupdate.updateisunstable.setValue(urlopen("http://enigma2.world-of-satellite.com/feeds/" + about.getImageVersionString() + "/status").read())
-				except:
-					config.softwareupdate.updateisunstable.setValue(1)
-				socket.setdefaulttimeout(currentTimeoutDefault)
-				self.total_packages = None
-				if config.softwareupdate.updateisunstable.getValue() == '1' and config.softwareupdate.updatebeta.getValue():
-					self.total_packages = len(self.ipkg.getFetchedList())
-					message = _("The current update maybe unstable") + "\n" + _("Are you sure you want to update your STB_BOX?") + "\n(" + (ngettext("%s updated package available", "%s updated packages available", self.total_packages) % self.total_packages) + ")"
-				elif config.softwareupdate.updateisunstable.getValue() == '0':
-					self.total_packages = len(self.ipkg.getFetchedList())
-					message = _("Do you want to update your STB_BOX?") + "\n(" + (ngettext("%s updated package available", "%s updated packages available", self.total_packages) % self.total_packages) + ")"
+				self.total_packages = len(self.ipkg.getFetchedList())
 				if self.total_packages:
-					config.softwareupdate.updatefound.setValue(True)
-					choices = [(_("Upgrade and reboot system"), "cold")]
-					#choices.append((_("View the changes"), "changes"))
-					choices.append((_("Perform a setting backup,") + '\n\t' + _("making a backup before updating") + '\n\t' +_("is strongly advised."), "backup"))
-					choices.append((_("Update channel list only"), "channels"))
-					choices.append((_("Cancel"), ""))
-					upgrademessage = self.session.openWithCallback(self.startActualUpgrade, ChoiceBox, title=message, list=choices, skin_name = "SoftwareUpdateChoices")
-					upgrademessage.setTitle(_('Software update'))
+					message = _("Do you want to update your STB_BOX?") + "\n"
+					message = message + "(" + (ngettext("%s updated package available", "%s updated packages available", self.total_packages) % self.total_packages) + ")"
+					choices = [(_("Update and reboot (recommended)"), "cold"),
+						(_("Update and ask to reboot"), "hot"),
+						(_("Update channel list only"), "channels"),
+						(_("Cancel"), "")]
+					self.session.openWithCallback(self.startActualUpgrade, ChoiceBox, title=message, list=choices)
 				else:
-					upgrademessage = self.session.openWithCallback(self.close, MessageBox, _("Nothing to upgrade"), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
-					upgrademessage.setTitle(_('Software update'))
+					self.session.openWithCallback(self.close, MessageBox, _("No updates available"), type=MessageBox.TYPE_INFO, timeout=10, close_on_any_key=True)
 			elif self.channellist_only > 0:
 				if self.channellist_only == 1:
 					self.setEndMessage(_("Could not find installed channel list."))
@@ -299,65 +203,23 @@ class UpdatePlugin(Screen):
 		if not answer or not answer[1]:
 			self.close()
 			return
-
-		if answer[1] == "menu":
-			if config.softwareupdate.updateisunstable.getValue() == '1':
-				message = _("The current update maybe unstable") + "\n" + _("Are you sure you want to update your STB_BOX?") + "\n(%s " % self.total_packages + _("Packages") + ")"
-			elif config.softwareupdate.updateisunstable.getValue() == '0':
-				message = _("Do you want to update your STB_BOX?") + "\n(%s " % self.total_packages + _("Packages") + ")"
-			choices = [(_("Upgrade and reboot system"), "cold")]
-			#choices.append((_("View the changes"), "changes"))
-			choices.append((_("Perform a setting backup, making a backup before updating is strongly advised."), "backup"))
-			choices.append((_("Update channel list only"), "channels"))
-			choices.append((_("Cancel"), ""))
-			upgrademessage = self.session.openWithCallback(self.startActualUpgrade, ChoiceBox, title=message, list=choices, skin_name = "SoftwareUpdateChoices")
-			upgrademessage.setTitle(_('Software update'))
-		elif answer[1] == "changes":
-			self.session.openWithCallback(self.startActualUpgrade,SoftwareUpdateChanges)
-		elif answer[1] == "backup":
-			self.doSettingsBackup()
+		if answer[1] == "cold":
+			self.session.open(TryQuitMainloop,retvalue=42)
+			self.close()
 		elif answer[1] == "channels":
 			self.channellist_only = 1
 			self.slider.setValue(1)
 			self.ipkg.startCmd(IpkgComponent.CMD_LIST, args = {'installed_only': True})
-		elif answer[1] == "cold":
-			if config.softwareupdate.autosettingsbackup.getValue():
-				self.doAutoBackup()
-			else:
-				self.session.open(TryQuitMainloop,retvalue=42)
-				self.close()
+		else:
+			self.ipkg.startCmd(IpkgComponent.CMD_UPGRADE, args = {'test_only': False})
 
 	def modificationCallback(self, res):
 		self.ipkg.write(res and "N" or "Y")
-	
-	def doSettingsBackup(self):
-		from Plugins.SystemPlugins.SoftwareManager.BackupRestore import BackupScreen
-		if not self.SettingsBackupDone:
-		  self.SettingsBackupDone = True
-		  self.session.openWithCallback(self.startActualUpgrade(("menu", "menu")), BackupScreen, runBackup = True)
-		else:
-		  self.session.openWithCallback(self.startActualUpgrade(("cold", "cold")), BackupScreen, runBackup = True)
-
-	def doAutoBackup(self, val = False):
-		self.autobackuprunning = True
-		if config.softwareupdate.autosettingsbackup.getValue() and config.backupmanager.backuplocation.getValue() and not self.SettingsBackupDone:
-			self.doSettingsBackup()
-		else:
-			self.session.open(TryQuitMainloop,retvalue=42)
-			self.close()
-
-	def showJobView(self, job):
-		from Screens.TaskView import JobView
-		Components.Task.job_manager.in_background = False
-		if not self.autobackuprunning:
-			self.session.openWithCallback(self.startActualUpgrade(("menu", "menu")), JobView, job,  cancelable = False, backgroundable = False, afterEventChangeable = False, afterEvent="close")
-		else:
-			self.session.openWithCallback(self.doAutoBackup, JobView, job,  cancelable = False, backgroundable = False, afterEventChangeable = False, afterEvent="close")
 
 	def exit(self):
 		if not self.ipkg.isRunning():
 			if self.packages != 0 and self.error == 0 and self.channellist_only == 0:
-				self.session.openWithCallback(self.exitAnswer, MessageBox, _("Upgrade finished.") +" "+_("Do you want to reboot your STB_BOX?"))
+				self.session.openWithCallback(self.exitAnswer, MessageBox, _("Update completed. Do you want to reboot your STB_BOX?"))
 			else:
 				self.close()
 		else:
@@ -366,5 +228,8 @@ class UpdatePlugin(Screen):
 
 	def exitAnswer(self, result):
 		if result is not None and result:
-			self.session.open(TryQuitMainloop, retvalue=2)
+			self.session.open(TryQuitMainloop,retvalue=2)
 		self.close()
+
+	def __close(self):
+		self.ipkg.removeCallback(self.ipkgCallback)
