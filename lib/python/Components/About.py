840d0a3292fb605849e3a3655cdd6995581dea3f
Paul In&In <darezik@gmail.com>
2013-03-23 13:20:47 +0000

    [About] code cleanup

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -1,10 +1,9 @@
-from Tools.Directories import resolveFilename, SCOPE_SYSETC
-from Tools.HardwareInfo import HardwareInfo
-from os import path
-import sys, enigma
+from enigma import getImageVersionString
+from sys import modules
+import socket, fcntl, struct
 
 def getVersionString():
-	return engima.getImageVersionString()
+	return getImageVersionString()
 
 def getKernelVersionString():
 	try:
@@ -14,91 +13,6 @@ def getKernelVersionString():
 		return kernelversion
 	except:
 		return _("unknown")
-
-def getDriversString():
-	try:
-		file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
-		lines = file.readlines()
-		for x in lines:
-			splitted = x.split('=')
-			if splitted[0] == "drivers":
-				#YYYY MM DD hh mm
-				#2005 11 29 01 16
-				string = splitted[1].replace('\n','')
-				year = string[0:4]
-				month = string[4:6]
-				day = string[6:8]
-				date = '-'.join((year, month, day))
-		file.close()
-		return date
-	except IOError:
-		return "unavailable"
-
-def getImageTypeString():
-	try:
-		file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
-		lines = file.readlines()
-		for x in lines:
-			splitted = x.split('=')
-			if splitted[0] == "build_type":
-				image_type = splitted[1].replace('\n','') # 0 = release, 1 = experimental
-		file.close()
-		if image_type == '0':
-			image_type = _("Release")
-		else:
-			image_type = _("Experimental")
-		return image_type
-	except IOError:
-		return "unavailable"
-
-def getImageDistroString():
-	try:
-		file = open(resolveFilename(SCOPE_SYSETC, 'image-version'), 'r')
-		lines = file.readlines()
-		file.close()
-		for x in lines:
-			splitted = x.split('=')
-			if splitted[0] == "comment":
-				distro =  splitted[1].replace('\n','')
-		return distro
-	except IOError:
-		return "unavailable"
-
-import socket, fcntl, struct
-
-def _ifinfo(sock, addr, ifname):
-	iface = struct.pack('256s', ifname[:15])
-	info  = fcntl.ioctl(sock.fileno(), addr, iface)
-	if addr == 0x8927:
-		return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1].upper()
-	else:
-		return socket.inet_ntoa(info[20:24])
-
-def getIfConfig(ifname):
-	ifreq = {'ifname': ifname}
-	infos = {}
-	sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	# offsets defined in /usr/include/linux/sockios.h on linux 2.6
-	infos['addr']    = 0x8915 # SIOCGIFADDR
-	infos['brdaddr'] = 0x8919 # SIOCGIFBRDADDR
-	infos['hwaddr']  = 0x8927 # SIOCSIFHWADDR
-	infos['netmask'] = 0x891b # SIOCGIFNETMASK
-	try:
-		for k,v in infos.items():
-			ifreq[k] = _ifinfo(sock, v, ifname)
-	except:
-		pass
-	sock.close()
-	return ifreq
-
-def getIfTransferredData(ifname):
-	f = open('/proc/net/dev', 'r')
-	for line in f:
-		if ifname in line:
-			data = line.split('%s:' % ifname)[1].split()
-			rx_bytes, tx_bytes = (data[0], data[8])
-			f.close()
-			return (rx_bytes, tx_bytes)
 
 def getChipSetString():
 	try:
@@ -142,5 +56,39 @@ def getCpuCoresString():
 	except IOError:
 		return "unavailable"
 
+def _ifinfo(sock, addr, ifname):
+	iface = struct.pack('256s', ifname[:15])
+	info  = fcntl.ioctl(sock.fileno(), addr, iface)
+	if addr == 0x8927:
+		return ''.join(['%02x:' % ord(char) for char in info[18:24]])[:-1].upper()
+	else:
+		return socket.inet_ntoa(info[20:24])
+
+def getIfConfig(ifname):
+	ifreq = {'ifname': ifname}
+	infos = {}
+	sock  = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# offsets defined in /usr/include/linux/sockios.h on linux 2.6
+	infos['addr']    = 0x8915 # SIOCGIFADDR
+	infos['brdaddr'] = 0x8919 # SIOCGIFBRDADDR
+	infos['hwaddr']  = 0x8927 # SIOCSIFHWADDR
+	infos['netmask'] = 0x891b # SIOCGIFNETMASK
+	try:
+		for k,v in infos.items():
+			ifreq[k] = _ifinfo(sock, v, ifname)
+	except:
+		pass
+	sock.close()
+	return ifreq
+
+def getIfTransferredData(ifname):
+	f = open('/proc/net/dev', 'r')
+	for line in f:
+		if ifname in line:
+			data = line.split('%s:' % ifname)[1].split()
+			rx_bytes, tx_bytes = (data[0], data[8])
+			f.close()
+			return (rx_bytes, tx_bytes)
+
 # For modules that do "from About import about"
-about = sys.modules[__name__]
+about = modules[__name__]
