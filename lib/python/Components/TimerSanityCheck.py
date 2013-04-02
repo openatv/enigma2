53fa9a0a4ee52155c1a4b26a003a7cc52a65e3ae
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-04-02 16:54:09 +0100

    [TimerSanityCheck] fix possible BSOD.

--- lib/python/Components/TimerSanityCheck.py
+++ lib/python/Components/TimerSanityCheck.py
@@ -118,7 +118,7 @@ class TimerSanityCheck:
 			weeks = (interval_end - offset_0) / 604800
 			if ((interval_end - offset_0) % 604800):
 				weeks += 1
-			for cnt in range(weeks):
+			for cnt in range(int(weeks)):
 				for event in self.rep_eventlist:
 					if event[1] == -1: # -1 is the identifier of the changed timer
 						event_begin = self.newtimer.begin
