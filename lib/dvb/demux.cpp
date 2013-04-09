openatv/enigma2

commit 869a8d0cee9c39a8c4b3baca9a6355ed505a27b6

pvrparse: do not attempt to parse audio streams
Currently, eMPEGStreamParserTS can only parse mpeg2 (type 0)
and h264 (type 1).
Do not try to parse other stream types, which might lead to
false hits, and waste cpu time.

lib/dvb/pvrparse.cpp
@@ -1145,8 +1145,22 @@ void eMPEGStreamParserTS::addAccessPoint(off_t offset, pts_t pts, timespec &now,
 void eMPEGStreamParserTS::setPid(int _pid, int type)
 {
 	m_pktptr = 0;
-	m_pid = _pid;
-	m_streamtype = type;
+	/*
+	 * Currently, eMPEGStreamParserTS can only parse mpeg2 (type 0) and h264 (type 1).
+	 * Do not try to parse other stream types, which might lead to false hits,
+	 * and waste cpu time.
+	 */
+	if (type >= 0 && type <= 1)
+	{
+		m_pid = _pid;
+		m_streamtype = type;
+	}
+	else
+	{
+		/* invalidate pid */
+		m_pid = -1;
+		m_streamtype = -1;
+	}
 }
 
 int eMPEGStreamParserTS::getLastPTS(pts_t &last_pts)

commit 85c69f8ff001135ffa4d67041459587c7f407852

Fix eDVBPVRServiceOfflineOperations::reindex()
(used by the ReconstructScAp plugin)

869a8d0cee9c39a8c4b3baca9a6355ed505a27b6 broke
eDVBPVRServiceOfflineOperations::reindex, which relies
on eMPEGStreamParserTS::setPid to autodetect the
streamtype, when the streamtype argument equals -1

Signed-off-by: pieterg <pieterg@users.sourceforge.net>

lib/dvb/demux.cpp
@@ -386,7 +386,7 @@ class eDVBRecordFileThread: public eFilePushThreadRecorder
 public:
 	eDVBRecordFileThread(int packetsize, int bufferCount);
 	~eDVBRecordFileThread();
-	void setTimingPID(int pid, int type);
+	void setTimingPID(int pid, iDVBTSRecorder::timing_pid_type pidtype, int streamtype);
 	void startSaveMetaInformation(const std::string &filename);
 	void stopSaveMetaInformation();
 	int getLastPTS(pts_t &pts);
@@ -452,9 +452,9 @@ eDVBRecordFileThread::~eDVBRecordFileThread()
 	::munmap(m_allocated_buffer, m_aio.size() * m_buffersize);
 }
 
-void eDVBRecordFileThread::setTimingPID(int pid, int type)
+void eDVBRecordFileThread::setTimingPID(int pid, iDVBTSRecorder::timing_pid_type pidtype, int streamtype)
 {
-	m_ts_parser.setPid(pid, type);
+	m_ts_parser.setPid(pid, pidtype, streamtype);
 }
 
 void eDVBRecordFileThread::startSaveMetaInformation(const std::string &filename)
@@ -808,9 +808,9 @@ RESULT eDVBTSRecorder::removePID(int pid)
 	return 0;
 }
 
-RESULT eDVBTSRecorder::setTimingPID(int pid, int type)
+RESULT eDVBTSRecorder::setTimingPID(int pid, timing_pid_type pidtype, int streamtype)
 {
-	m_thread->setTimingPID(pid, type);
+	m_thread->setTimingPID(pid, pidtype, streamtype);
 	return 0;
 }
 

lib/dvb/demux.h
@@ -99,7 +99,7 @@ public:
 	RESULT addPID(int pid);
 	RESULT removePID(int pid);
 	
-	RESULT setTimingPID(int pid, int type);
+	RESULT setTimingPID(int pid, timing_pid_type pidtype, int streamtype);
 	
 	RESULT setTargetFD(int fd);
 	RESULT setTargetFilename(const std::string& filename);

lib/dvb/idemux.h
@@ -32,8 +32,9 @@ public:
 	virtual RESULT start() = 0;
 	virtual RESULT addPID(int pid) = 0;
 	virtual RESULT removePID(int pid) = 0;
-	
-	virtual RESULT setTimingPID(int pid, int type) = 0;
+
+	enum timing_pid_type { none = -1, video_pid, audio_pid };
+	virtual RESULT setTimingPID(int pid, timing_pid_type pidtype, int streamtype) = 0;
 	
 	virtual RESULT setTargetFD(int fd) = 0;
 		/* for saving additional meta data. */

lib/dvb/pvrparse.cpp
@@ -1142,18 +1142,19 @@ void eMPEGStreamParserTS::addAccessPoint(off_t offset, pts_t pts, timespec &now,
 	m_last_access_point = now;
 }
 
-void eMPEGStreamParserTS::setPid(int _pid, int type)
+void eMPEGStreamParserTS::setPid(int _pid, iDVBTSRecorder::timing_pid_type pidtype, int streamtype)
 {
 	m_pktptr = 0;
 	/*
-	 * Currently, eMPEGStreamParserTS can only parse mpeg2 (type 0) and h264 (type 1).
-	 * Do not try to parse other stream types, which might lead to false hits,
+	 * Currently, eMPEGStreamParserTS can only parse video, mpeg2 (streamtype 0) and h264 (streamtype 1).
+	 * Also, streamtype -1 should be accepted, which will cause the streamtype to be autodetected.
+	 * Do not try to parse audio pids, which might lead to false hits,
 	 * and waste cpu time.
 	 */
-	if (type >= 0 && type <= 1)
+	if (pidtype == iDVBTSRecorder::video_pid && streamtype < 2)
 	{
 		m_pid = _pid;
-		m_streamtype = type;
+		m_streamtype = streamtype;
 	}
 	else
 	{

lib/dvb/pvrparse.h
@@ -2,6 +2,7 @@
 #define __include_lib_dvb_pvrparse_h
 
 #include <lib/dvb/idvb.h>
+#include <lib/dvb/idemux.h>
 #include <map>
 #include <set>
 #include <deque>
@@ -115,7 +116,7 @@ class eMPEGStreamParserTS: public eMPEGStreamInformationWriter
 public:
 	eMPEGStreamParserTS(int packetsize = 188);
 	void parseData(off_t offset, const void *data, unsigned int len);
-	void setPid(int pid, int streamtype);
+	void setPid(int pid, iDVBTSRecorder::timing_pid_type pidtype, int streamtype);
 	int getLastPTS(pts_t &last_pts);
 	void enableAccessPoints(bool enable) { m_enable_accesspoints = enable; }
 private:

lib/service/servicedvb.cpp
@@ -563,7 +563,7 @@ static int reindex_work(const std::string& filename)
 			return err;
 		}
 		eDebug("reindex: pcr_pid=0x%x", pcr_pid);
-		parser.setPid(pcr_pid, -1); /* -1 = automatic MPEG2/h264 detection */
+		parser.setPid(pcr_pid, iDVBTSRecorder::video_pid, -1); /* -1 = automatic MPEG2/h264 detection */
 	}
 
 	parser.startSave(filename);
@@ -2496,7 +2496,8 @@ void eDVBServicePlay::updateTimeshiftPids()
 	else
 	{
 		int timing_pid = -1;
-		int timing_pid_type = -1;
+		int timing_stream_type = -1;
+		iDVBTSRecorder::timing_pid_type timing_pid_type = iDVBTSRecorder::none;
 		std::set<int> pids_to_record;
 		pids_to_record.insert(0); // PAT
 		if (program.pmtPid != -1)
@@ -2512,7 +2513,8 @@ void eDVBServicePlay::updateTimeshiftPids()
 			if (timing_pid == -1)
 			{
 				timing_pid = i->pid;
-				timing_pid_type = i->type;
+				timing_stream_type = i->type;
+				timing_pid_type = iDVBTSRecorder::video_pid;
 			}
 			pids_to_record.insert(i->pid);
 		}
@@ -2524,7 +2526,8 @@ void eDVBServicePlay::updateTimeshiftPids()
 			if (timing_pid == -1)
 			{
 				timing_pid = i->pid;
-				timing_pid_type = -1;
+				timing_stream_type = i->type;
+				timing_pid_type = iDVBTSRecorder::audio_pid;
 			}
 			pids_to_record.insert(i->pid);
 		}
@@ -2553,7 +2556,7 @@ void eDVBServicePlay::updateTimeshiftPids()
 			m_record->removePID(*i);
 
 		if (timing_pid != -1)
-			m_record->setTimingPID(timing_pid, timing_pid_type);
+			m_record->setTimingPID(timing_pid, timing_pid_type, timing_stream_type);
 	}
 }
 

lib/service/servicedvbrecord.cpp
@@ -353,7 +353,8 @@ int eDVBServiceRecord::doRecord()
 			if (program.pmtPid != -1)
 				pids_to_record.insert(program.pmtPid); // PMT
 
-			int timing_pid = -1, timing_pid_type = -1;
+			int timing_pid = -1, timing_stream_type = -1;
+			iDVBTSRecorder::timing_pid_type timing_pid_type = iDVBTSRecorder::none;
 
 			eDebugNoNewLine("RECORD: have %zd video stream(s)", program.videoStreams.size());
 			if (!program.videoStreams.empty())
@@ -368,7 +369,8 @@ int eDVBServiceRecord::doRecord()
 					if (timing_pid == -1)
 					{
 						timing_pid = i->pid;
-						timing_pid_type = i->type;
+						timing_stream_type = i->type;
+						timing_pid_type = iDVBTSRecorder::video_pid;
 					}
 					
 					if (i != program.videoStreams.begin())
@@ -390,7 +392,8 @@ int eDVBServiceRecord::doRecord()
 					if (timing_pid == -1)
 					{
 						timing_pid = i->pid;
-						timing_pid_type = -1;
+						timing_stream_type = i->type;
+						timing_pid_type = iDVBTSRecorder::audio_pid;
 					}
 				
 					if (i != program.audioStreams.begin())
@@ -456,7 +459,7 @@ int eDVBServiceRecord::doRecord()
 			}
 
 			if (timing_pid != -1)
-				m_record->setTimingPID(timing_pid, timing_pid_type);
+				m_record->setTimingPID(timing_pid, timing_pid_type, timing_stream_type);
 
 			m_pids_active = pids_to_record;
 

lib/service/servicedvbstream.cpp
@@ -165,7 +165,8 @@ int eDVBServiceStream::doRecord()
 		if (program.pmtPid != -1)
 			pids_to_record.insert(program.pmtPid); // PMT
 
-		int timing_pid = -1, timing_pid_type = -1;
+		int timing_pid = -1, timing_stream_type = -1;
+		iDVBTSRecorder::timing_pid_type timing_pid_type = iDVBTSRecorder::none;
 
 		eDebugNoNewLine("STREAM: have %zd video stream(s)", program.videoStreams.size());
 		if (!program.videoStreams.empty())
@@ -180,7 +181,8 @@ int eDVBServiceStream::doRecord()
 				if (timing_pid == -1)
 				{
 					timing_pid = i->pid;
-					timing_pid_type = i->type;
+					timing_stream_type = i->type;
+					timing_pid_type = iDVBTSRecorder::video_pid;
 				}
 				
 				if (i != program.videoStreams.begin())
@@ -202,7 +204,8 @@ int eDVBServiceStream::doRecord()
 				if (timing_pid == -1)
 				{
 					timing_pid = i->pid;
-					timing_pid_type = -1;
+					timing_stream_type = i->type;
+					timing_pid_type = iDVBTSRecorder::audio_pid;
 				}
 			
 				if (i != program.audioStreams.begin())
@@ -281,7 +284,7 @@ int eDVBServiceStream::doRecord()
 		}
 
 		if (timing_pid != -1)
-			m_record->setTimingPID(timing_pid, timing_pid_type);
+			m_record->setTimingPID(timing_pid, timing_pid_type, timing_stream_type);
 
 		m_pids_active = pids_to_record;
 