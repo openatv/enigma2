8006e84275d557a29ebd6a37dcdfb9fae8877252
atvcaptain <captain@web.de>
2013-04-16 22:44:44 +0200

    [gbquad] fix pink screen

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -129,7 +129,7 @@ def InitAVSwitch():
 	iAVSwitch = AVSwitch()
 
 	def setColorFormat(configElement):
-		if getBoxType().startswith('et'):
+		if getBoxType() == 'gbquad' or getBoxType().startswith('et'):
 			map = {"cvbs": 0, "rgb": 3, "svideo": 2, "yuv": 3}
 		else:
 			map = {"cvbs": 0, "rgb": 1, "svideo": 2, "yuv": 3}
