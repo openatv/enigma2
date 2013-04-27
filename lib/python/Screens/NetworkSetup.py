ae3d8d72561d1b474501572ce1aad804869070ad
atvcaptain <captain@web.de>
2013-04-27 23:27:45 +0200

    fix info of sabnzbd_process

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -1785,7 +1785,7 @@ class NetworkSABnzbd(Screen):
 	def updateService(self,result = None, retval = None, extra_args = None):
 		import process
 		p = process.ProcessList()
-		sabnzbd_process = str(p.named('SABnzbd.py ')).strip('[]')
+		sabnzbd_process = str(p.named('SABnzbd.py')).strip('[]')
 		self['labrun'].hide()
 		self['labstop'].hide()
 		self['labactive'].setText(_("Disabled"))
