3e9495f021c2483b5046e24a31484f047d2db809
a4tech <Dev1@dev>
2013-04-30 19:48:11 +0200

    Added weather

---
+++ lib/python/Plugins/Extensions/AccuWeather/__init__.py
@@ -0,0 +1,3 @@
+# 2013.03.25 16:03:01 CET
+#Embedded file name: /home/morser/Azbox/OpenSPA2/build-vuultimo/tmp/work/mips32el-oe-linux/enigma2-plugin-extensions-spzweather-1.0-r3/plugin/__init__.py
+pass
