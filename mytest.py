6c9bc1ae0c13a85302aff300e3ad7b68730a065a
aafcaptain <captain@web.de>
2013-05-02 21:44:31 +0200

    add infos

--- mytest.py
+++ mytest.py
@@ -492,11 +492,16 @@ def runScreenTest():
 	profile("RunReactor")
 	profile_final()
 	
-	if config.misc.boxtype.value == 'gb800se' or config.misc.boxtype.value == 'gb800solo' or config.misc.boxtype.value == 'gb800ue':
-		from enigma import evfd
-		evfd.getInstance().vfd_write_string("_E2_")
-		evfd.getInstance().vfd_led(str(1)) 
-		
+	if enigma.getBoxType() == 'gb800se' or enigma.getBoxType() == 'gb800solo':
+		from enigma import evfd, eConsoleAppContainer
+		try:
+			cmd = 'vfdctl "    openmips starting e2"'
+			container = eConsoleAppContainer()
+			container.execute(cmd)
+		except:
+			evfd.getInstance().vfd_write_string("-E2-")
+		evfd.getInstance().vfd_led(str(1))
+	
 	runReactor()
 
 	config.misc.startCounter.save()
