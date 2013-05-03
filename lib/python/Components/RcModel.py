13e8d970a6f6ee88c5ddf6fb40ebd4e49c69744f
Paul In&In <darezik@gmail.com>
2013-05-03 18:29:19 +0200

    rcu fixes

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -1,4 +1,5 @@
 from enigma import getBoxType
+from Tools.StbHardware import getFPVersion
 import os
 
 class RcModel:
@@ -12,20 +13,19 @@ class RcModel:
 	RCTYPE_ET9X00 = 7	
 	RCTYPE_ET9500 = 8
 	RCTYPE_GB = 9
-	RCTYPE_INI1000 = 10
-	RCTYPE_INI3000 = 11
-	RCTYPE_INI5000 = 12
-	RCTYPE_INI5000R = 13
-	RCTYPE_INI7000 = 14
-	RCTYPE_IQON = 15	
-	RCTYPE_IXUSSONE = 16
-	RCTYPE_IXUSSZERO = 17
-	RCTYPE_ODINM7 = 18
-	RCTYPE_ODINM9 = 19	
-	RCTYPE_TM = 20
-	RCTYPE_VU = 21	
-	RCTYPE_VU2 = 22
-	RCTYPE_XP1000 = 23
+	RCTYPE_INI0 = 10
+	RCTYPE_INI1 = 11
+	RCTYPE_INI2 = 12
+	RCTYPE_INI3 = 13
+	RCTYPE_IQON = 14
+	RCTYPE_IXUSSONE = 15
+	RCTYPE_IXUSSZERO = 16
+	RCTYPE_ODINM7 = 17
+	RCTYPE_ODINM9 = 18
+	RCTYPE_TM = 19
+	RCTYPE_VU = 20
+	RCTYPE_VU2 = 21
+	RCTYPE_XP1000 = 22
 
 
 	def __init__(self):
@@ -44,67 +44,78 @@ class RcModel:
 		return out.split()[0]
 
 	def readRcTypeFromProc(self):
-		if getBoxType() == 'dm8000':
-			self.currentRcType = self.RCTYPE_DMM
+		if os.path.exists('/proc/stb/info/hwmodel'):
+			model = self.readFile('/proc/stb/info/hwmodel')
+			if model == 'tmtwinoe' or model == 'tm2toe' or model == 'tmsingle':
+				self.currentRcType = self.RCTYPE_TM
+			elif model == 'ios100hd' or model == 'ios200hd' or model == 'ios300hd':
+				self.currentRcType = self.RCTYPE_IQON
+		elif getBoxType() == 'dm8000':
+				self.currentRcType = self.RCTYPE_DMM
 		elif getBoxType() == 'dm7020hd':
-			self.currentRcType = self.RCTYPE_DMM2
+				self.currentRcType = self.RCTYPE_DMM2
 		elif getBoxType() == 'dm800' or getBoxType() == 'dm800se' or getBoxType() == 'dm500hd':
-			self.currentRcType = self.RCTYPE_DMM1
-		elif getBoxType() == 'ebox5000':
-			self.currentRcType = self.RCTYPE_EBOX5000
-		elif getBoxType().startswith('et'):
+				self.currentRcType = self.RCTYPE_DMM1
+		elif os.path.exists('/proc/stb/info/boxtype'):
 			model = self.readFile('/proc/stb/info/boxtype')
-			rc = self.readFile('/proc/stb/ir/rc/type')
-			if rc == '5':
-				self.currentRcType = self.RCTYPE_ET9X00
-			elif rc == '7':
-				self.currentRcType = self.RCTYPE_ET6X00
-			elif rc == '9' and model == 'et9500':
-				self.currentRcType = self.RCTYPE_ET9500
-			elif rc == '9' and model == 'et6500':
-				self.currentRcType = self.RCTYPE_ET6500
-			elif rc == '11' and model == 'et9200':
-				self.currentRcType = self.RCTYPE_ET9500
-			elif rc == '11' and model == 'et9000':
-				self.currentRcType = self.RCTYPE_ET9X00
-			elif rc == '13' and model == 'et4000':
-				self.currentRcType = self.RCTYPE_ET4X00
-		elif getBoxType().startswith('gb'):
-			self.currentRcType = self.RCTYPE_GB
-		elif getBoxType().startswith('iqon'):
-			self.currentRcType = self.RCTYPE_IQON
-		elif getBoxType() == 'ixussone':
-			self.currentRcType = self.RCTYPE_IXUSSONE
-		elif getBoxType() == 'ixusszero':
-			self.currentRcType = self.RCTYPE_IXUSSZERO
-		elif getBoxType().startswith('odin'):
-			model = self.readFile('/proc/stb/info/boxtype')
-			if model == 'odinm7':
-				self.currentRcType = self.RCTYPE_ODINM7
+			if model.startswith('et') or model.startswith('xp'):
+				rc = self.readFile('/proc/stb/ir/rc/type')
+				if rc == '3':
+					self.currentRcType = self.RCTYPE_ODINM9
+				if rc == '4':
+					self.currentRcType = self.RCTYPE_DMM
+				elif rc == '5':
+					self.currentRcType = self.RCTYPE_ET9X00
+				elif rc == '6':
+					self.currentRcType = self.RCTYPE_DMM
+				elif rc == '7':
+					self.currentRcType = self.RCTYPE_ET6X00
+				elif rc == '8':
+					self.currentRcType = self.RCTYPE_VU
+				elif rc == '9' and model == 'et9500':
+					self.currentRcType = self.RCTYPE_ET9500
+				elif rc == '9' and model == 'et6500':
+					self.currentRcType = self.RCTYPE_ET6500
+				elif rc == '11' and model == 'et9200':
+					self.currentRcType = self.RCTYPE_ET9500
+				elif rc == '11' and model == 'et9000':
+					self.currentRcType = self.RCTYPE_ET9x00
+				elif rc == '13' and model == 'et4000':
+					self.currentRcType = self.RCTYPE_ET4X00
+				elif rc == '14':
+					self.currentRcType = self.RCTYPE_XP1000
+			elif model == 'ebox5000':
+				self.currentRcType = self.RCTYPE_EBOX5000	
+			elif model == 'gigablue':
+				self.currentRcType = self.RCTYPE_GB
+			elif model == 'ini-3000':
+				fp_version = str(getFPVersion())
+				if fp_version.startswith('1'):
+					self.currentRcType = self.RCTYPE_INI0
+				else:
+					self.currentRcType = self.RCTYPE_INI2
+			elif model == 'ini-5000' or model == 'ini-7000' or model == 'ini-7012':
+				self.currentRcType = self.RCTYPE_INI1
+			elif model == 'ini-1000' or model == 'ini-1000ru' or model == 'ini-5000ru':
+				self.currentRcType = self.RCTYPE_INI2
+			elif model == 'ini-1000sv' or model == 'ini-5000sv':
+				self.currentRcType = self.RCTYPE_INI3
 			elif model == 'odinm9':
 				self.currentRcType = self.RCTYPE_ODINM9
-		elif getBoxType().startswith('tm'):
-			self.currentRcType = self.RCTYPE_TM
-		elif getBoxType().startswith('venton'):
-			model = self.readFile('/proc/stb/info/boxtype')
-			if model == 'ini-1000' or model == 'ini-1000ru' or model == 'ini-1000sv':
-				self.currentRcType = self.RCTYPE_INI1000
-			elif model == 'ini-3000':
-				self.currentRcType = self.RCTYPE_INI3000
-			elif model == 'ini-5000' or model == 'ini-5000sv':
-				self.currentRcType = self.RCTYPE_INI5000
-			elif model == 'ini-5000ru':
-				self.currentRcType = self.RCTYPE_INI5000R
-			elif model == 'ini-7000' or model == 'ini-7012':
-				self.currentRcType = self.RCTYPE_INI7000
-		elif getBoxType().startswith('vu'):
-			if getBoxType() == 'vuultimo':
+			elif model == 'odinm7':
+				self.currentRcType = self.RCTYPE_ODINM7
+			elif model.startswith('Ixuss'):
+				if getBoxType() == 'ixussone':
+					self.currentRcType = self.RCTYPE_IXUSSONE
+				elif getBoxType() == 'ixusszero':
+					self.currentRcType = self.RCTYPE_IXUSSZERO
+		elif os.path.exists('/proc/stb/info/vumodel'):
+			model = self.readFile('/proc/stb/info/vumodel')
+			if model == 'ultimo':
 				self.currentRcType = self.RCTYPE_VU2
 			else:
 				self.currentRcType = self.RCTYPE_VU
-		elif getBoxType().startswith('xp'):
-			self.currentRcType = self.RCTYPE_XP1000
-
+		
 	def getRcLocation(self):
 		if self.currentRcType == self.RCTYPE_DMM:
 			return '/usr/share/enigma2/rc_models/dmm0/'
@@ -126,26 +137,24 @@ class RcModel:
 			return '/usr/share/enigma2/rc_models/et9500/'
 		elif self.currentRcType == self.RCTYPE_GB:
 			return '/usr/share/enigma2/rc_models/gb/'
-		elif self.currentRcType == self.RCTYPE_INI1000:
-			return '/usr/share/enigma2/rc_models/inix000/'
-		elif self.currentRcType == self.RCTYPE_INI3000:
-			return '/usr/share/enigma2/rc_models/inix000/'
-		elif self.currentRcType == self.RCTYPE_INI5000:
-			return '/usr/share/enigma2/rc_models/inix000/'
-		elif self.currentRcType == self.RCTYPE_INI5000R:
-			return '/usr/share/enigma2/rc_models/inix000/'
-		elif self.currentRcType == self.RCTYPE_INI7000:
-			return '/usr/share/enigma2/rc_models/inix000/'
+		elif self.currentRcType == self.RCTYPE_INI0:
+			return '/usr/share/enigma2/rc_models/ini0/'
+		elif self.currentRcType == self.RCTYPE_INI1:
+			return '/usr/share/enigma2/rc_models/ini1/'
+		elif self.currentRcType == self.RCTYPE_INI2:
+			return '/usr/share/enigma2/rc_models/ini2/'
+		elif self.currentRcType == self.RCTYPE_INI3:
+			return '/usr/share/enigma2/rc_models/ini3/'      
 		elif self.currentRcType == self.RCTYPE_IQON:
 			return '/usr/share/enigma2/rc_models/iqon/'
 		elif self.currentRcType == self.RCTYPE_IXUSSONE:
 			return '/usr/share/enigma2/rc_models/ixussone/'
 		elif self.currentRcType == self.RCTYPE_IXUSSZERO:
 			return '/usr/share/enigma2/rc_models/ixusszero/'
-		elif self.currentRcType == self.RCTYPE_ODINM7:
-			return '/usr/share/enigma2/rc_models/odinm7/'
 		elif self.currentRcType == self.RCTYPE_ODINM9:
 			return '/usr/share/enigma2/rc_models/odinm9/'
+		elif self.currentRcType == self.RCTYPE_ODINM7:
+			return '/usr/share/enigma2/rc_models/odinm7/'
 		elif self.currentRcType == self.RCTYPE_TM:
 			return '/usr/share/enigma2/rc_models/tm/'
 		elif self.currentRcType == self.RCTYPE_VU:
