openatv/enigma2

commit 578dc6957156b2e4225f9310236b39cca315692e

introduce eConfigManager baseclass
move python implementation into derived ePythonConfigQuery class

lib/python/pythonconfig.h
@@ -1,0 +1,20 @@
+#ifndef __lib_python_pythonconfig_h_
+#define __lib_python_pythonconfig_h_
+
+#include <lib/base/nconfig.h>
+#include <lib/python/python.h>
+
+class ePythonConfigQuery : public eConfigManager
+{
+	static ePyObject m_queryFunc;
+	ePythonConfigQuery() {}
+	~ePythonConfigQuery() {}
+#ifndef SWIG
+	RESULT getConfigValue(const char *key, std::string &value);
+	std::string getConfig(const char *key);
+#endif
+public:
+	static void setQueryFunc(SWIG_PYOBJECT(ePyObject) func);
+};
+
+#endif // __lib_python_pythonconfig_h_

lib/base/nconfig.cpp
@@ -1,35 +1,37 @@
 #include <lib/base/nconfig.h>
-#include <lib/python/python.h>
 
-ePyObject ePythonConfigQuery::m_queryFunc;
+eConfigManager *eConfigManager::instance = NULL;
 
-void ePythonConfigQuery::setQueryFunc(ePyObject queryFunc)
-{
-	if (m_queryFunc)
-		Py_DECREF(m_queryFunc);
-	m_queryFunc = queryFunc;
-	if (m_queryFunc)
-		Py_INCREF(m_queryFunc);
+eConfigManager::eConfigManager() 
+{ 
+	instance = this; 
 }
 
-RESULT ePythonConfigQuery::getConfigValue(const char *key, std::string &value)
+eConfigManager::~eConfigManager() 
+{ 
+	instance = NULL; 
+}
+
+eConfigManager *eConfigManager::getInstance() 
+{ 
+	return instance; 
+}
+
+std::string eConfigManager::getConfigValue(const char *key) 
 {
-	if (key && PyCallable_Check(m_queryFunc))
-	{
-		ePyObject pArgs = PyTuple_New(1);
-		PyTuple_SET_ITEM(pArgs, 0, PyString_FromString(key));
-		ePyObject pRet = PyObject_CallObject(m_queryFunc, pArgs);
-		Py_DECREF(pArgs);
-		if (pRet)
-		{
-			if (PyString_Check(pRet))
-			{
-				value.assign(PyString_AS_STRING(pRet));
-				Py_DECREF(pRet);
-				return 0;
-			}
-			Py_DECREF(pRet);
-		}
-	}
-	return -1;
+	return instance ? instance->getConfig(key) : "";
+}
+
+int eConfigManager::getConfigIntValue(const char *key, int defaultvalue) 
+{
+	std::string value = getConfigValue(key);
+	return (value != "") ? atoi(value.c_str()) : defaultvalue;
+}
+
+bool eConfigManager::getConfigBoolValue(const char *key, bool defaultvalue) 
+{
+	std::string value = getConfigValue(key);
+	if (value == "True") return true;
+	if (value == "False") return false;
+	return defaultvalue;
 }

lib/base/nconfig.h
@@ -1,18 +1,24 @@
 #ifndef __lib_base_nconfig_h_
 #define __lib_base_nconfig_h_
 
-#include <lib/python/python.h>
+#include <string>
+#include <stdbool.h>
 
-class ePythonConfigQuery
+class eConfigManager
 {
-	static ePyObject m_queryFunc;
-	ePythonConfigQuery() {}
-	~ePythonConfigQuery() {}
+protected:
+	static eConfigManager *instance;
+	static eConfigManager *getInstance();
+
+	virtual std::string getConfig(const char *key) = 0;
+
 public:
-	static void setQueryFunc(SWIG_PYOBJECT(ePyObject) func);
-#ifndef SWIG
-	static RESULT getConfigValue(const char *key, std::string &value);
-#endif
+	eConfigManager();
+	virtual ~eConfigManager();
+
+	static std::string getConfigValue(const char *key);
+	static int getConfigIntValue(const char *key, int defaultvalue = 0);
+	static bool getConfigBoolValue(const char *key, bool defaultvalue = false);
 };
 
-#endif // __lib_base_nconfig_h_
+#endif /* __lib_base_nconfig_h_ */

lib/dvb/cablescan.cpp
@@ -345,9 +345,7 @@ void eCableScan::createBouquets()
 	}
 	bouquetFilename = replace_all(providerName, " ", "");
 
-	bool multibouquet = false;
-	std::string value;
-	multibouquet = (ePythonConfigQuery::getConfigValue("config.usage.multibouquet", value) >= 0 && value == "True");
+	bool multibouquet = eConfigManager::getConfigBoolValue("config.usage.multibouquet");
 
 	if (multibouquet)
 	{

lib/dvb/fastscan.cpp
@@ -619,9 +619,7 @@ void eFastScan::parseResult()
 		}
 	}
 
-	bool multibouquet = false;
-	std::string value;
-	multibouquet = (ePythonConfigQuery::getConfigValue("config.usage.multibouquet", value) >= 0 && value == "True");
+	bool multibouquet = eConfigManager::getConfigBoolValue("config.usage.multibouquet");
 
 	if (multibouquet)
 	{

lib/dvb/frontend.cpp
@@ -2568,12 +2568,10 @@ RESULT eDVBFrontend::tune(const iDVBFrontendParameters &where)
 		if (res)
 			goto tune_error;
 
-		std::string enable_5V;
 		char configStr[255];
 		snprintf(configStr, 255, "config.Nims.%d.terrestrial_5V", m_slotid);
 		m_sec_sequence.push_back( eSecCommand(eSecCommand::START_TUNE_TIMEOUT, timeout) );
-		ePythonConfigQuery::getConfigValue(configStr, enable_5V);
-		if (enable_5V == "True")
+		if (eConfigManager::getConfigBoolValue(configStr))
 			m_sec_sequence.push_back( eSecCommand(eSecCommand::SET_VOLTAGE, iDVBFrontend::voltage13) );
 		else
 			m_sec_sequence.push_back( eSecCommand(eSecCommand::SET_VOLTAGE, iDVBFrontend::voltageOff) );

lib/dvb/pmt.cpp
@@ -388,13 +388,17 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 
 		std::string configvalue;
 		std::vector<std::string> autoaudio_languages;
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.audio_autoselect1", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.audio_autoselect1");
+		if (configvalue != "" && configvalue != "None")
 			autoaudio_languages.push_back(configvalue);
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.audio_autoselect2", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.audio_autoselect2");
+		if (configvalue != "" && configvalue != "None")
 			autoaudio_languages.push_back(configvalue);
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.audio_autoselect3", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.audio_autoselect3");
+		if (configvalue != "" && configvalue != "None")
 			autoaudio_languages.push_back(configvalue);
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.audio_autoselect4", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.audio_autoselect4");
+		if (configvalue != "" && configvalue != "None")
 			autoaudio_languages.push_back(configvalue);
 
 		int autosub_txt_normal = -1;
@@ -404,13 +408,17 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		int autosub_level =4;
 
 		std::vector<std::string> autosub_languages;
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_autoselect1", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.subtitle_autoselect1");
+		if (configvalue != "" && configvalue != "None")
 			autosub_languages.push_back(configvalue);
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_autoselect2", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.subtitle_autoselect2");
+		if (configvalue != "" && configvalue != "None")
 			autosub_languages.push_back(configvalue);
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_autoselect3", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.subtitle_autoselect3");
+		if (configvalue != "" && configvalue != "None")
 			autosub_languages.push_back(configvalue);
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_autoselect4", configvalue) && configvalue != "None")
+		configvalue = eConfigManager::getConfigValue("config.autolanguage.subtitle_autoselect4");
+		if (configvalue != "" && configvalue != "None")
 			autosub_languages.push_back(configvalue);
 
 		m_dsmcc_pid = program.dsmccPid;
@@ -494,13 +502,8 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 			}
 		}
 
-		bool defaultac3 = false;
-		bool useaudio_cache = false;
-
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.audio_defaultac3", configvalue))
-			defaultac3 = configvalue == "True";
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.audio_usecache", configvalue))
-			useaudio_cache = configvalue == "True";
+		bool defaultac3 = eConfigManager::getConfigBoolValue("config.autolanguage.audio_defaultac3");
+		bool useaudio_cache = eConfigManager::getConfigBoolValue("config.autolanguage.audio_usecache");
 
 		if (useaudio_cache && audio_cached != -1)
 			program.defaultAudioStream = audio_cached;
@@ -521,19 +524,10 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 				program.defaultAudioStream = autoaudio_ac3;
 		}
 
-		bool allow_hearingimpaired = false;
-		bool default_hearingimpaired = false;
-		bool defaultdvb = false;
-		int equallanguagemask = false;
-
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_hearingimpaired", configvalue))
-			allow_hearingimpaired = configvalue == "True";
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_defaultimpaired", configvalue))
-			default_hearingimpaired = configvalue == "True";
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.subtitle_defaultdvb", configvalue))
-			defaultdvb = configvalue == "True";
-		if (!ePythonConfigQuery::getConfigValue("config.autolanguage.equal_languages", configvalue))
-			equallanguagemask = atoi(configvalue.c_str());
+		bool allow_hearingimpaired = eConfigManager::getConfigBoolValue("config.autolanguage.subtitle_hearingimpaired");
+		bool default_hearingimpaired = eConfigManager::getConfigBoolValue("config.autolanguage.subtitle_defaultimpaired");
+		bool defaultdvb = eConfigManager::getConfigBoolValue("config.autolanguage.subtitle_defaultdvb");
+		int equallanguagemask = eConfigManager::getConfigIntValue("config.autolanguage.equal_languages");
 
 		if (defaultdvb)
 		{
@@ -788,9 +782,7 @@ int eDVBServicePMTHandler::tuneExt(eServiceReferenceDVB &ref, int use_decode_dem
 			if (ref.path.empty())
 			{
 				m_dvb_scan = new eDVBScan(m_channel, true, false);
-				std::string disable_background_scan;
-				if (ePythonConfigQuery::getConfigValue("config.misc.disable_background_scan", disable_background_scan) < 0
-					|| disable_background_scan != "True")
+				if (!eConfigManager::getConfigBoolValue("config.misc.disable_background_scan"))
 				{
 					/*
 					 * not starting a dvb scan triggers what appears to be a
@@ -822,9 +814,7 @@ int eDVBServicePMTHandler::tuneExt(eServiceReferenceDVB &ref, int use_decode_dem
 
 			if (m_service_type == offline) 
 			{
-				std::string delay;
-				ePythonConfigQuery::getConfigValue("config.recording.offline_decode_delay", delay);
-				m_pvr_channel->setOfflineDecodeMode(atoi(delay.c_str()));
+				m_pvr_channel->setOfflineDecodeMode(eConfigManager::getConfigIntValue("config.recording.offline_decode_delay"));
 			}
 		}
 	}

lib/dvb/subtitle.cpp
@@ -51,8 +51,7 @@ static int extract_pts(pts_t &pts, __u8 *pkt)
 
 void eDVBSubtitleParser::subtitle_process_line(subtitle_region *region, subtitle_region_object *object, int line, __u8 *data, int len)
 {
-	std::string configvalue;
-	bool subcentered = (ePythonConfigQuery::getConfigValue("config.subtitles.dvb_subtitles_centered", configvalue) >= 0 && configvalue == "True");
+	bool subcentered = eConfigManager::getConfigBoolValue("config.subtitles.dvb_subtitles_centered");
 	int x = subcentered ? (region->width - len) /2 : object->object_horizontal_position;
 	int y = object->object_vertical_position + line;
 	if (x + len > region->width)
@@ -1176,13 +1175,8 @@ void eDVBSubtitleParser::subtitle_redraw(int page_id)
 					break;
 			}
 
-			std::string configvalue;
-			int bcktrans = 0;
-			if (!ePythonConfigQuery::getConfigValue("config.subtitles.dvb_subtitles_backtrans", configvalue))
-			{
-				bcktrans = atoi(configvalue.c_str());
-			}
-			bool yellow = (ePythonConfigQuery::getConfigValue("config.subtitles.dvb_subtitles_yellow", configvalue) >= 0 && configvalue == "True");
+			int bcktrans = eConfigManager::getConfigIntValue("config.subtitles.dvb_subtitles_backtrans");
+			bool yellow = eConfigManager::getConfigBoolValue("config.subtitles.dvb_subtitles_yellow");
 
 			for (int i=0; i<clut_size; ++i)
 			{

commit 12cd3217cca951fa37d7e9c77fad224a5c109275

pythonconfig: create a config object
this fixes getting config from c++ code,
broken since 578dc6957156b2e4225f9310236b39cca315692e

lib/python/pythonconfig.h
@@ -7,13 +7,13 @@
 class ePythonConfigQuery : public eConfigManager
 {
 	static ePyObject m_queryFunc;
-	ePythonConfigQuery() {}
-	~ePythonConfigQuery() {}
 #ifndef SWIG
 	RESULT getConfigValue(const char *key, std::string &value);
 	std::string getConfig(const char *key);
 #endif
 public:
+	ePythonConfigQuery() {}
+	~ePythonConfigQuery() {}
 	static void setQueryFunc(SWIG_PYOBJECT(ePyObject) func);
 };
 

main/enigma.cpp
@@ -27,6 +27,7 @@
 #include <lib/gui/evideo.h>
 #include <lib/python/connections.h>
 #include <lib/python/python.h>
+#include <lib/python/pythonconfig.h>
 
 #include "bsod.h"
 #include "version_info.h"
@@ -96,6 +97,7 @@ void keyEvent(const eRCKey &key)
 class eMain: public eApplication, public Object
 {
 	eInit init;
+	ePythonConfigQuery config;
 
 	ePtr<eDVBDB> m_dvbdb;
 	ePtr<eDVBResourceManager> m_mgr;