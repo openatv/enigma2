870c8d8c8b8e0e05af5a7a8d223fc570fa308824
Paul In&In <darezik@gmail.com>
2013-05-24 14:38:59 +0200

    InfoBarGenerics: clear selected_subtitles on evStart instead of evEnd
    evEnd only occurs when we explicitly stop playback,
    selecting the next media file while the current
    service is still playing, we do not receive evEnd
    for the current service, but we do receive evStart
    for the next service.
    So evStart is a better choice to clear our status.

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2724,7 +2724,7 @@ class InfoBarSubtitleSupport(object):
 
 		self.__event_tracker = ServiceEventTracker(screen=self, eventmap=
 			{
-				iPlayableService.evEnd: self.__serviceStopped,
+				iPlayableService.evStart: self.__serviceStarted,
 				iPlayableService.evUpdatedInfo: self.__updatedInfo
 			})
 
@@ -2739,7 +2739,7 @@ class InfoBarSubtitleSupport(object):
 			from Screens.AudioSelection import SubtitleSelection
 			self.session.open(SubtitleSelection, self)
 
-	def __serviceStopped(self):
+	def __serviceStarted(self):
 		self.selected_subtitle = None
 		self.subtitle_window.hide()
 
