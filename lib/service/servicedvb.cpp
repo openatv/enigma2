fdb55fd4f11edde5b27e1b4952d0ed3fef1d86bc
Littlesat <littlesat99@yahoo.com>
2013-05-25 21:05:28 +0200

    Fix for getcachedsubtitle from servicedvb.cpp

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -3021,7 +3021,6 @@ RESULT eDVBServicePlay::getCachedSubtitle(struct SubtitleTrack &track)
 {
 	if (m_dvb_service)
 	{
-		struct SubtitleTrack track;
 		eDVBServicePMTHandler::program program;
 		eDVBServicePMTHandler &h = m_timeshift_active ? m_service_handler_timeshift : m_service_handler;
 		if (!h.getProgramInfo(program))
