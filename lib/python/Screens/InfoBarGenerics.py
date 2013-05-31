5a3f25e0970c528490d981a706f030d59d189172
Erik Slagter <erik@slagter.name>
2013-05-31 10:11:10 +0200

    InfoBarGenerics: fix for unintended endless timer recordings.
    Patch contributed by dima73:
    
    button REC->Add recording (indefinitely) or Add recording (stop after current event) and not event
    timer end = 1440 min
    
    Open Timer Edit->change timer end
    The recording time is over, but she did not stop!
    + 1 day

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1987,6 +1987,7 @@ class InfoBarInstantRecord:
 		simulTimerList = self.session.nav.RecordTimer.record(recording)
 
 		if simulTimerList is None:	# no conflict
+			recording.autoincrease = False
 			self.recording.append(recording)
 		else:
 			if len(simulTimerList) > 1: # with other recording
