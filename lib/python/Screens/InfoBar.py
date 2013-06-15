f4fc1d778200e9a3cf47986294a24b331d25bb11
Paul In&In <darezik@gmail.com>
2013-06-15 17:09:27 +0100

    [InfoBar] fix typo.

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -236,7 +236,7 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, \
 		self.allowPiP = True
 
 		for x in HelpableScreen, InfoBarShowHide, InfoBarMenu, InfoBarEPG, \
-				InfoBarBase, InfoBarSeek, InfoBarShowMovies, InfoBarInstantRecord \
+				InfoBarBase, InfoBarSeek, InfoBarShowMovies, InfoBarInstantRecord, \
 				InfoBarAudioSelection, InfoBarNotifications, \
 				InfoBarServiceNotifications, InfoBarPVRState, InfoBarCueSheetSupport, \
 				InfoBarMoviePlayerSummarySupport, InfoBarSubtitleSupport, \
