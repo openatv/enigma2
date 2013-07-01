b35a0f557f5aefbe36745528ba6c32c70935f8d4
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-07-02 00:58:59 +0100

    [NimManager] tweak multi-type tuner detection.

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -154,7 +154,7 @@ class SecConfigure:
 
 		for slot in nim_slots:
 			if slot.frontend_id is not None:
-				types = [type for type in ["DVB-T2", "DVB-T", "DVB-C", "DVB-S2", "DVB-S", "ATSC"] if eDVBResourceManager.getInstance().frontendIsCompatible(slot.frontend_id, type)]
+				types = [type for type in ["DVB-C", "DVB-T", "DVB-T2", "DVB-S", "DVB-S2", "ATSC"] if eDVBResourceManager.getInstance().frontendIsCompatible(slot.frontend_id, type)]
 				if "DVB-T2" in types:
 					# DVB-T2 implies DVB-T support
 					types.remove("DVB-T")
