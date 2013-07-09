dbb6928276c5a81b28f35e2baae9d4fb28ff2346
Black_64 <black6464@gmail.com>
2013-07-09 22:19:53 +0200

    [ChannelSelection] add "reload servicelist" to context menu

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -226,6 +226,8 @@ class ChannelContextMenu(Screen):
 					append_when_current_valid(current, menu, (_("end alternatives edit"), self.bouquetMarkEnd), level = 0)
 					append_when_current_valid(current, menu, (_("abort alternatives edit"), self.bouquetMarkAbort), level = 0)
 
+		menu.append(ChoiceEntryComponent(text = (_("Reload Services"), self.reloadServices)))
+
 		self["menu"] = ChoiceList(menu)
 
 	def playMain(self):
@@ -244,6 +246,11 @@ class ChannelContextMenu(Screen):
 
 	def cancelClick(self):
 		self.close(False)
+
+	def reloadServices(self):
+		eDVBDB.getInstance().reloadBouquets()
+		eDVBDB.getInstance().reloadServicelist()
+		self.session.openWithCallback(self.close, MessageBox, _("The servicelist is reloaded."), MessageBox.TYPE_INFO, timeout = 5)
 
 	def showServiceInformations(self):
 		self.session.open( ServiceInfo, self.csel.getCurrentSelection() )
