4c33321922a4015c2f0b56a927360247bbf149a0
Paul In&In <darezik@gmail.com>
2013-07-12 15:04:28 +0200

    less code for repeat current item in movielist
    Signed-off-by: Littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -262,7 +262,7 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, \
 			self.doSeek(0)
 			self.setSeekState(self.SEEK_STATE_PLAY)
 		elif answer in ("playlist","playlistquit","loop"):
-			( next_service, item , lenght ) = self.nextPlaylistService(self.cur_service)
+			( next_service, item , lenght ) = self.getPlaylistServiceInfo(self.cur_service)
 			if next_service is not None:
 				if config.usage.next_movie_msg.value:
 					self.displayPlayedName(next_service, item, lenght)
@@ -277,7 +277,7 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, \
 					self.leavePlayerConfirmed([True,"quit"])
 		elif answer in ("repeatcurrent"):
 			if config.usage.next_movie_msg.value:
-				(item, lenght) = self.currentPlaylistService(self.cur_service)
+				(item, lenght) = self.getPlaylistServiceInfo(self.cur_service)
 				self.displayPlayedName(self.cur_service, item, lenght)
 			self.session.nav.stopService()
 			self.session.nav.playService(self.cur_service)
@@ -391,23 +391,18 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, \
 			if ref and not self.session.nav.getCurrentlyPlayingServiceOrGroup():
 				self.session.nav.playService(ref)
 
-	def nextPlaylistService(self, service):
+	def getPlaylistServiceInfo(self, service):
 		from MovieSelection import playlist
 		for i, item in enumerate(playlist):
 			if item == service:
+				if config.usage.on_movie_eof.value == "repeatcurrent":
+					return (i+1, len(playlist))
 				i += 1
 				if i < len(playlist):
 					return (playlist[i], i+1, len(playlist))
 				elif config.usage.on_movie_eof.value == "loop":
 					return (playlist[0], 1, len(playlist))
 		return ( None, 0, 0 )
-
-	def currentPlaylistService(self, service):
-		from MovieSelection import playlist
-		for i, item in enumerate(playlist):
-			if item == service:
-				return (i+1, len(playlist))
-		return ( 0, 0 )
 
 	def displayPlayedName(self, ref, index, n):
 		from Tools import Notifications
