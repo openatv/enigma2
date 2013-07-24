30d9b93a67ca8432494eea60cd04477b7a9d1e65
Paul In&In <darezik@gmail.com>
2013-07-24 11:38:28 +0200

    Always ensure that subtitles are started when in timeshift

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1644,7 +1644,7 @@ class InfoBarTimeshift:
 		self["SeekActions"].setEnabled(state)
 		if not state:
 			self.setSeekState(self.SEEK_STATE_PLAY)
-			self.restartSubtitle()
+		self.restartSubtitle()
 
 	def __serviceStarted(self):
 		self.pvrStateDialog.hide()
