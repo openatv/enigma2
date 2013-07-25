94caf2a8b4a03bd8aacf160485437c8bc0b8ee98
Littlesat <littlesat99@yahoo.com>
2013-07-25 19:10:25 +0200

    For pango subs remove italic, bold, underscore codes
    When color is fixed to white or yellow

--- lib/gui/esubtitle.cpp
+++ lib/gui/esubtitle.cpp
@@ -43,10 +43,10 @@ void eSubtitleWidget::setPage(const eDVBTeletextSubtitlePage &p)
 			default:
 			case 1: /* white */
 				color = gRGB(255, 255, 255);
-			break;
+				break;
 			case 2: /* yellow */
 				color = gRGB(255, 255, 0);
-			break;
+				break;
 		}
 
 		if (!original_position)
@@ -303,6 +303,7 @@ int eSubtitleWidget::event(int event, void *data, void *data2)
 
 				switch (eConfigManager::getConfigIntValue("config.subtitles.pango_subtitle_colors", 1))
 				{
+					default:
 					case 0: /* use yellow for italic, cyan for bold and green for underscore */
 						text = replace_all(text, "<i>", (std::string) gRGB(255,255,0));
 						text = replace_all(text, "<b>", (std::string) gRGB(0,255,255));
@@ -313,6 +314,13 @@ int eSubtitleWidget::event(int event, void *data, void *data2)
 						break;
 					case 2: /* yellow */
 						text = (std::string) gRGB(255, 255, 0) + text;
+					case 1: /* remove italic, bold, underscore */
+						text = replace_all(text, "<i>", "");
+						text = replace_all(text, "<b>", "");
+						text = replace_all(text, "<u>", "");
+						text = replace_all(text, "</i>", "");
+						text = replace_all(text, "</b>", "");
+						text = replace_all(text, "</u>", "");
 						break;
 				}
 
