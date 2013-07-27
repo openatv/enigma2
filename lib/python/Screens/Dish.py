33a7d7905fbdd7fad8a8ec5546acdddff1afcf8f
ims <ims21@users.sourceforge.net>
2013-07-27 17:42:00 +0200

    added rotor speed into skin fixed GSOD for fresh instalation optimalized code added tuner name and speed into skin_default
    Signed-off-by: Littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/Dish.py
+++ lib/python/Screens/Dish.py
@@ -18,14 +18,15 @@ class Dish(Screen):
 	STATE_HIDDEN = 0
 	STATE_SHOWN  = 1
 	skin = """
-		<screen name="Dish" flags="wfNoBorder" position="86,100" size="130,220" title="Dish" zPosition="1" backgroundColor="#11396D" >
+		<screen name="Dish" flags="wfNoBorder" position="86,100" size="130,200" title="Dish" zPosition="1" backgroundColor="#11396D" >
 			<widget name="Dishpixmap" position="0,0"  size="130,160" zPosition="-1" pixmap="skin_default/icons/dish.png" transparent="1" alphatest="on" />
 			<widget name="turnTime"   position="5,0"   size="120,20" zPosition="1" font="Regular;20" halign="right" shadowColor="black" shadowOffset="-2,-2" transparent="1" />
-			<widget name="From"       position="5,164"  size="50,17" zPosition="1" font="Regular;17" halign="left"  shadowColor="black" shadowOffset="-2,-1" transparent="1"  />
+			<widget name="From"       position="5,162" size="50,17" zPosition="1" font="Regular;17" halign="left"  shadowColor="black" shadowOffset="-2,-1" transparent="1"  />
 			<widget name="posFrom"    position="57,160" size="70,20" zPosition="1" font="Regular;20" halign="left"  shadowColor="black" shadowOffset="-2,-2" transparent="1" />
-			<widget name="Goto"       position="5,184"  size="50,17" zPosition="1" font="Regular;17" halign="left"  shadowColor="black" shadowOffset="-2,-1" transparent="1" />
+			<widget name="Goto"       position="5,182"  size="50,17" zPosition="1" font="Regular;17" halign="left"  shadowColor="black" shadowOffset="-2,-1" transparent="1" />
 			<widget name="posGoto"    position="57,180" size="70,20" zPosition="1" font="Regular;20" halign="left"  shadowColor="black" shadowOffset="-2,-2" transparent="1" />
-			<widget name="tunerName"  position="5,204"  size="120,17" zPosition="1" font="Regular;17" halign="left" shadowColor="black" shadowOffset="-2,-2" transparent="1" />
+			<widget name="tunerName"  position="5,144"  size="90,16" zPosition="2" font="Regular;14" halign="left"  shadowColor="black" shadowOffset="-2,-2" transparent="1" />
+			<widget name="turnSpeed"  position="75,95" size="50,16" zPosition="2" font="Regular;14" halign="right" shadowColor="black" shadowOffset="-2,-2" transparent="1" />
 		</screen>"""
 
 	def __init__(self, session):
@@ -36,9 +37,10 @@ class Dish(Screen):
 		self["turnTime"] = Label("")
 		self["posFrom"] = Label("")
 		self["posGoto"] = Label("")
-		self["From"] = Label (_("From :"))
-		self["Goto"] = Label (_("Goto :"))
+		self["From"] = Label(_("From :"))
+		self["Goto"] = Label(_("Goto :"))
 		self["tunerName"] = Label("")
+		self["turnSpeed"] = Label("")
 
 		self.rotorTimer = eTimer()
 		self.rotorTimer.callback.append(self.updateRotorMovingState)
@@ -93,6 +95,7 @@ class Dish(Screen):
 		self["posGoto"].setText(self.OrbToStr(self.rotor_pos))
 		self["turnTime"].setText(self.FormatTurnTime(self.turn_time))
 		self["tunerName"].setText(self.getTunerName())
+		self["turnSpeed"].setText(str(self.getTurningSpeed(self.cur_polar)) + chr(176) + _("/s"))
 
 		self.turnTimer.start(1000, False)
 
@@ -137,32 +140,37 @@ class Dish(Screen):
 				mrt = 3600 - mrt
 			if (mrt % 10):
 				mrt += 10
-			( turningspeedH, turningspeedV ) = self.getTurningSpeed()
-			if pol in (1, 3):	# vertical
-				mrt = (mrt * 1000 / turningspeedV ) / 10000
-			else:			# horizontal
-				mrt = (mrt * 1000 / turningspeedH ) / 10000
+			mrt = (mrt * 1000 / self.getTurningSpeed(pol) ) / 10000
 		return mrt + 3
 
-	def getTurningSpeed(self):
-		nim = config.Nims[self.getCurrentTuner()]
-		return (nim.turningspeedH.float, nim.turningspeedV.float)
+	def getTurningSpeed(self, pol=0):
+		tuner = self.getCurrentTuner()
+		if tuner is not None:
+			nim = config.Nims[tuner]
+			if pol in (1, 3): # vertical
+				return nim.turningspeedV.float
+			return nim.turningspeedH.float
+		if pol in (1, 3):
+			return 1.0
+		return 1.5
 
 	def getCurrentTuner(self):
-		return self.currentTunerInfo().get("tuner_number")
-
-	def currentTunerInfo(self):
 		service = self.session.nav.getCurrentService()
 		feinfo = service and service.frontendInfo()
-		return feinfo and feinfo.getFrontendData()
+		tuner = feinfo and feinfo.getFrontendData()
+		if tuner is not None:
+			return tuner.get("tuner_number")
+		return None
 
 	def getTunerName(self):
-		from Components.NimManager import nimmanager
-		nims = nimmanager.nimList()
 		nr = self.getCurrentTuner()
-		if nr < 4:
-			return "".join(nims[nr].split(':')[:1])
-		return " ".join((_("Tuner"),str(nr)))
+		if nr is not None:
+			from Components.NimManager import nimmanager
+			nims = nimmanager.nimList()
+			if nr < 4:
+				return "".join(nims[nr].split(':')[:1])
+			return " ".join((_("Tuner"),str(nr)))
+		return ""
 
 	def OrbToStr(self, orbpos):
 		if orbpos == INVALID_POSITION:
