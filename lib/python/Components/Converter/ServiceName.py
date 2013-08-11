3ffee4869144bb2d500b1ae2712ca20927ecc94a
Paul In&In <darezik@gmail.com>
2013-08-11 10:44:00 +0100

    [ServiceName] add 'NameAndEvent' to render options.

--- lib/python/Components/Converter/ServiceName.py
+++ lib/python/Components/Converter/ServiceName.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 from Components.Converter.Converter import Converter
 from Components.config import config
-from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr
+from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr, eServiceReference, eEPGCache
 from Components.Element import cached
 from ServiceReference import resolveAlternate
 
 class ServiceName(Converter, object):
 	NAME = 0
-	NAME_ONLY = 2
+	NAME_ONLY = 1
+	NAME_EVENT = 2
 	PROVIDER = 3
 	REFERENCE = 4
-	EDITREFERENCE = 4
+	EDITREFERENCE = 5
 
 	def __init__(self, type):
 		Converter.__init__(self, type)
+		self.epgQuery = eEPGCache.getInstance().lookupEventTime
 		if type == "Provider":
 			self.type = self.PROVIDER
 		elif type == "Reference":
@@ -22,6 +24,8 @@ class ServiceName(Converter, object):
 			self.type = self.EDITREFERENCE
 		elif type == "NameOnly":
 			self.type = self.NAME_ONLY
+		elif type == "NameAndEvent":
+			self.type = self.NAME_EVENT
 		else:
 			self.type = self.NAME
 
@@ -36,22 +40,30 @@ class ServiceName(Converter, object):
 			ref = service
 		if not info:
 			return ""
-		if self.type == self.NAME or self.type == self.NAME_ONLY:
-			if self.type != self.NAME_ONLY and config.usage.show_infobar_channel_number.getValue() and hasattr(self.source, "serviceref") and self.source.serviceref.toString().find('0:0:0:0:0:0:0:0:0') == -1:
-				name = ref and info.getName(ref)
+
+		if self.type == self.NAME or self.type == self.NAME_ONLY or self.type == self.NAME_EVENT:
+			name = ref and info.getName(ref)
+			if name is None:
+				name = info.getName()
+			name = name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+			if self.type == self.NAME_EVENT:
+				act_event = info and info.getEvent(0)
+				if not act_event and info:
+					refstr = info.getInfoString(iServiceInformation.sServiceref)
+					act_event = self.epgQuery(eServiceReference(refstr), -1, 0)
+				if act_event is None:
+					return "%s - " % name
+				else:
+					return "%s - %s" % (name, act_event.getEventName())
+			elif self.type != self.NAME_ONLY and config.usage.show_infobar_channel_number.getValue() and hasattr(self.source, "serviceref") and self.source.serviceref.toString().find('0:0:0:0:0:0:0:0:0') == -1:
 				numservice = self.source.serviceref
 				num = numservice and numservice.getChannelNum() or None
-				if name is None:
-					name = info.getName()
 				if num is not None:
-					return str(num) + '   ' + name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+					return str(num) + '   ' + name
 				else:
-					return name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+					return name
 			else:
-				name = ref and info.getName(ref)
-				if name is None:
-					name = info.getName()
-				return name.replace('\xc2\x86', '').replace('\xc2\x87', '')
+				return name
 		elif self.type == self.PROVIDER:
 			return info.getInfoString(iServiceInformation.sProvider)
 		elif self.type == self.REFERENCE or self.type == self.EDITREFERENCE and hasattr(self.source, "editmode") and self.source.editmode:
