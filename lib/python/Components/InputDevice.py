9c907eb8efa6ed5f6a4ed8a409bdd645b8390459
atvcaptain <captain@web.de>
2013-08-11 17:44:44 +0200

    fix input device

--- lib/python/Components/InputDevice.py
+++ lib/python/Components/InputDevice.py
@@ -208,7 +208,7 @@ class RcTypeControl():
 				self.writeRcType(config.plugins.remotecontroltype.rctype.value)
 		else:
 			self.isSupported = False
-		if getBoxType().startswith('gb')
+		if getBoxType().startswith('gb'):
 			self.isSupported = False
 
 	def multipleRcSupported(self):
