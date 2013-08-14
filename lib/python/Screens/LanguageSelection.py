75a4ebfd0c92131853393ee6bb39aad2f0da43cd
Paul In&In <darezik@gmail.com>
2013-08-15 00:00:16 +0200

    LanguageSelection: Close resursive if language is changed
    When you not do that you still see menus that are in the previous
    language, so when the language is realy changed and you perform a
    recursive close you do not see "rests" of menues presenting in the
    previous language. This behavior was anoying me fore a long time - undless I do
    not see this often :D.

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -52,7 +52,7 @@ class LanguageSelection(Screen):
 
 	def save(self):
 		self.commit(self.run())
-		self.close()
+		self.close(self.oldActiveLanguage != config.osd.language.value)
 
 	def cancel(self):
 		language.activateLanguage(self.oldActiveLanguage)
