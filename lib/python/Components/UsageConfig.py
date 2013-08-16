d11679aea2fd37421ed3a1e398988b384c3365eb
Paul In&In <darezik@gmail.com>
2013-08-16 17:57:49 +0200

    Remove obsoleted config.usage.show_vcr_scart
    Sorry as I forgot to remove this in the previous commit.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -210,7 +210,6 @@ def InitUsageConfig():
 	config.usage.hide_ci_messages = ConfigYesNo(default = False)
 	config.usage.show_cryptoinfo = ConfigYesNo(default = True)
 	config.usage.show_eit_nownext = ConfigYesNo(default = True)
-	config.usage.show_vcr_scart = ConfigYesNo(default = False)
 
 	config.epg = ConfigSubsection()
 	config.epg.eit = ConfigYesNo(default = True)
