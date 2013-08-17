83a16009f2fb861b2bb8d67886e0b334b05d43a3
Paul In&In <darezik@gmail.com>
2013-08-17 17:19:54 +0200

    [AVSwitch] fix issues with aspect ratio. (reverse-merged from commit b7ddf78b150c2ecf284798d04ba261d8aa750c78) Reversed as a provisional bugfix.

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -19,50 +19,21 @@ class AVSwitch:
 		eAVSwitch.getInstance().setVideomode(value)
 
 	def getOutputAspect(self):
-		if os.path.exists('/usr/lib/enigma2/python/Plugins/SystemPlugins/Videomode/VideoHardware.pyo'):
-			from Plugins.SystemPlugins.Videomode.VideoHardware import video_hw
-			ret = (16,9)
-			port = config.av.videoport.getValue()
-			if port not in config.av.videomode:
-				print "current port not available in getOutputAspect!!! force 16:9"
-			else:
-				mode = config.av.videomode[port].getValue()
-				force_widescreen = video_hw.isWidescreenMode(port, mode)
-				is_widescreen = force_widescreen or config.av.aspect.getValue() in ("16_9", "16_10")
-				is_auto = config.av.aspect.getValue() == "auto"
-				if is_widescreen:
-					if force_widescreen:
-						pass
-					else:
-						aspect = {"16_9": "16:9", "16_10": "16:10"}[config.av.aspect.getValue()]
-						if aspect == "16:10":
-							ret = (16,10)
-				elif is_auto:
-					try:
-						aspect_str = open("/proc/stb/vmpeg/0/aspect", "r").read()
-						if aspect_str == "1": # 4:3
-							ret = (4,3)
-					except IOError:
-						pass
-				else:  # 4:3
-					ret = (4,3)
-			return ret
-		else:
-			valstr = config.av.aspectratio.getValue()
-			if valstr in ("4_3_letterbox", "4_3_panscan"): # 4:3
-				return (4,3)
-			elif valstr == "16_9": # auto ... 4:3 or 16:9
-				try:
-					aspect_str = open("/proc/stb/vmpeg/0/aspect", "r").read()
-					if aspect_str == "1": # 4:3
-						return (4,3)
-				except IOError:
-					pass
-			elif valstr in ("16_9_always", "16_9_letterbox"): # 16:9
+		valstr = config.av.aspectratio.getValue()
+		if valstr in ("4_3_letterbox", "4_3_panscan"): # 4:3
+			return (4,3)
+		elif valstr == "16_9": # auto ... 4:3 or 16:9
+			try:
+				aspect_str = open("/proc/stb/vmpeg/0/aspect", "r").read()
+				if aspect_str == "1": # 4:3
+					return (4,3)
+			except IOError:
 				pass
-			elif valstr in ("16_10_letterbox", "16_10_panscan"): # 16:10
-				return (16,10)
-			return (16,9)
+		elif valstr in ("16_9_always", "16_9_letterbox"): # 16:9
+			pass
+		elif valstr in ("16_10_letterbox", "16_10_panscan"): # 16:10
+			return (16,10)
+		return (16,9)
 
 	def getFramebufferScale(self):
 		aspect = self.getOutputAspect()
@@ -157,28 +128,27 @@ def InitAVSwitch():
 
 	iAVSwitch = AVSwitch()
 
-	if not os.path.exists('/usr/lib/enigma2/python/Plugins/SystemPlugins/Videomode/VideoHardware.pyo'):
-		def setColorFormat(configElement):
-			map = {"cvbs": 0, "rgb": 1, "svideo": 2, "yuv": 3}
-			iAVSwitch.setColorFormat(map[configElement.getValue()])
-	
-		def setAspectRatio(configElement):
-			map = {"4_3_letterbox": 0, "4_3_panscan": 1, "16_9": 2, "16_9_always": 3, "16_10_letterbox": 4, "16_10_panscan": 5, "16_9_letterbox" : 6}
-			iAVSwitch.setAspectRatio(map[configElement.getValue()])
-	
-		def setSystem(configElement):
-			map = {"pal": 0, "ntsc": 1, "multinorm" : 2}
-			iAVSwitch.setSystem(map[configElement.getValue()])
-	
-		def setWSS(configElement):
-			iAVSwitch.setAspectWSS()
-	
-		# this will call the "setup-val" initial
-		config.av.colorformat.addNotifier(setColorFormat)
-		config.av.aspectratio.addNotifier(setAspectRatio)
-		config.av.tvsystem.addNotifier(setSystem)
-		config.av.wss.addNotifier(setWSS)
-	
+	def setColorFormat(configElement):
+		map = {"cvbs": 0, "rgb": 1, "svideo": 2, "yuv": 3}
+		iAVSwitch.setColorFormat(map[configElement.getValue()])
+
+	def setAspectRatio(configElement):
+		map = {"4_3_letterbox": 0, "4_3_panscan": 1, "16_9": 2, "16_9_always": 3, "16_10_letterbox": 4, "16_10_panscan": 5, "16_9_letterbox" : 6}
+		iAVSwitch.setAspectRatio(map[configElement.getValue()])
+
+	def setSystem(configElement):
+		map = {"pal": 0, "ntsc": 1, "multinorm" : 2}
+		iAVSwitch.setSystem(map[configElement.getValue()])
+
+	def setWSS(configElement):
+		iAVSwitch.setAspectWSS()
+
+	# this will call the "setup-val" initial
+	config.av.colorformat.addNotifier(setColorFormat)
+	config.av.aspectratio.addNotifier(setAspectRatio)
+	config.av.tvsystem.addNotifier(setSystem)
+	config.av.wss.addNotifier(setWSS)
+
 	iAVSwitch.setInput("ENCODER") # init on startup
 	SystemInfo["ScartSwitch"] = eAVSwitch.getInstance().haveScartSwitch()
 
