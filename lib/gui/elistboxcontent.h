ead0921ab19ad32ad653eb0ce233c6cd5bc5a4c3
Littlesat <littlesat99@yahoo.com>
2013-08-18 18:11:17 +0200

    eListboxcontent: cosmetic as tabs and spaces were used

--- lib/gui/elistboxcontent.h
+++ lib/gui/elistboxcontent.h
@@ -97,9 +97,9 @@ private:
 #define RT_VALIGN_BOTTOM 32
 #define RT_WRAP 64
 #define BT_ALPHATEST 1
-#define	BT_ALPHABLEND 2
-#define	BT_SCALE 4
-#define	BT_KEEP_ASPECT_RATIO 8
+#define BT_ALPHABLEND 2
+#define BT_SCALE 4
+#define BT_KEEP_ASPECT_RATIO 8
 #endif // SWIG
 
 #endif
