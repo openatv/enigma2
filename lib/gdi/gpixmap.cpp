706ffb8fed27e5e5e254eb127b112a80b9cd2131
Paul In&In <darezik@gmail.com>
2013-08-26 19:44:27 +0200

    gpixmap: Only use accel fill when worth the overhead
    Like blitting, using the accel fill has a constant cost, so don't
    use it for tiny areas.
    
    A little refactoring in eRect because I wanted a nice "surface"
    function.

--- lib/gdi/gpixmap.cpp
+++ lib/gdi/gpixmap.cpp
@@ -236,9 +236,17 @@ void gPixmap::fill(const gRegion &region, const gRGB &color)
 			col = color.argb();
 			col^=0xFF000000;
 
-			if (surface->data_phys)
-				if (!gAccel::getInstance()->fill(surface,  area, col))
+#ifdef GPIXMAP_DEBUG
+			Stopwatch s;
+#endif
+			if (surface->data_phys && (area.surface() > 20000))
+				if (!gAccel::getInstance()->fill(surface,  area, col)) {
+#ifdef GPIXMAP_DEBUG
+					s.stop();
+					eDebug("[BLITBENCH] accel fill %dx%d took %u us", area.width(), area.height(), s.elapsed_us());
+#endif
 					continue;
+				}
 
 			for (int y=area.top(); y<area.bottom(); y++)
 			{
@@ -247,6 +255,10 @@ void gPixmap::fill(const gRegion &region, const gRGB &color)
 				while (x--)
 					*dst++=col;
 			}
+#ifdef GPIXMAP_DEBUG
+			s.stop();
+			eDebug("[BLITBENCH] cpu fill %dx%d took %u us", area.width(), area.height(), s.elapsed_us());
+#endif
 		} else if (surface->bpp == 16)
 		{
 			__u32 icol = color.argb();
