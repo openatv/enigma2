cee3d6231990a5e8bc3ff9ae22db35bfd8d5224b
Paul In&In <darezik@gmail.com>
2013-09-04 02:57:41 +0100

    [EpgSelection] revert 'getClockTypesForEntry' name change for backward compatibly, required for plugins (EpgSearch etc).

--- lib/python/Components/EpgList.py
+++ lib/python/Components/EpgList.py
@@ -575,7 +575,7 @@ class EPGList(HTMLComponent, GUIComponent):
 		xpos, width = self.calcEntryPosAndWidthHelper(ev_start, ev_duration, time_base, time_base + time_epoch * 60, event_rect.width())
 		return xpos + event_rect.left(), width
 
-	def getClockTypesForEntry(self, service, eventId, beginTime, duration):
+	def getPixmapForEntry(self, service, eventId, beginTime, duration):
 		if not beginTime:
 			return None
 		rec = self.timer.isInTimer(eventId, beginTime, duration, service)
@@ -591,7 +591,7 @@ class EPGList(HTMLComponent, GUIComponent):
 		return rec[2]
 
 	def buildSingleEntry(self, service, eventId, beginTime, duration, EventName):
-		clock_types = self.getClockTypesForEntry(service, eventId, beginTime, duration)
+		clock_types = self.getPixmapForEntry(service, eventId, beginTime, duration)
 		r1 = self.weekday_rect
 		r2 = self.datetime_rect
 		r3 = self.descr_rect
@@ -618,7 +618,7 @@ class EPGList(HTMLComponent, GUIComponent):
  		return res
 
 	def buildSimilarEntry(self, service, eventId, beginTime, service_name, duration):
-		clock_types = self.getClockTypesForEntry(service, eventId, beginTime, duration)
+		clock_types = self.getPixmapForEntry(service, eventId, beginTime, duration)
 		r1 = self.weekday_rect
 		r2 = self.datetime_rect
 		r3 = self.descr_rect
@@ -652,7 +652,7 @@ class EPGList(HTMLComponent, GUIComponent):
 		res = [ None ] # no private data needed
 		res.append((eListboxPythonMultiContent.TYPE_TEXT, r1.x, r1.y, r1.w, r1.h, 0, RT_HALIGN_LEFT|RT_VALIGN_CENTER, service_name))
 		if beginTime is not None:
-			clock_types = self.getClockTypesForEntry(service, eventId, beginTime, duration)
+			clock_types = self.getPixmapForEntry(service, eventId, beginTime, duration)
 			if nowTime < beginTime:
 				begin = localtime(beginTime)
 				end = localtime(beginTime+duration)
@@ -835,7 +835,7 @@ class EPGList(HTMLComponent, GUIComponent):
 				stime = ev[2]
 				duration = ev[3]
 				xpos, ewidth = self.calcEntryPosAndWidthHelper(stime, duration, start, end, width)
-				clock_types = self.getClockTypesForEntry(service, ev[0], stime, duration)
+				clock_types = self.getPixmapForEntry(service, ev[0], stime, duration)
 				if self.eventNameAlign.lower() == 'left':
 					if self.eventNameWrap.lower() == 'yes':
 						alignnment = RT_HALIGN_LEFT | RT_VALIGN_CENTER | RT_WRAP
