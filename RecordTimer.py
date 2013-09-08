14412d8f4df69b3b45992b112beffcb2a4dcf362
Paul In&In <darezik@gmail.com>
2013-09-08 22:54:09 +0200

    Streamline regards to recordings timers when coming from deep standby
    Go to standby not directly buy after 30 seconds and not directly as it seems to make some plugins not compatible and for some hardware the wasWakeupTimer is not
    functioning properly, so put the box in standby after 30 seconds when there is realty a record timer (record, zap&record or only zap) within 6 minutes. It is assumed that within 30 seconds we should have a correct timestamp as this is proven to perform well for years.
    
    Thanks Dima73 for all comments and sorry for not always understanding you.

--- RecordTimer.py
+++ RecordTimer.py
@@ -258,15 +258,11 @@ class RecordTimerEntry(timer.TimerEntry, object):
 		self.log(5, "activating state %d" % next_state)
 
 		if next_state == 1:
-			if Screens.Standby.inStandby:
-				Screens.Standby.inStandby.prev_running_service = NavigationInstance.instance.getCurrentlyPlayingServiceOrGroup()
-				Screens.Standby.inStandby.paused_service = None
-				NavigationInstance.instance.stopService()
-				self.log(5, "stopped service in standby")
 			if self.always_zap:
 				if Screens.Standby.inStandby:
-					self.wasInStandby = True
-					eActionMap.getInstance().bindAction('', -maxint - 1, self.keypress)
+					if not config.misc.standbyCounter.value:
+						self.wasInStandby = True
+						eActionMap.getInstance().bindAction('', -maxint - 1, self.keypress)
 					#set service to zap after standby
 					Screens.Standby.inStandby.prev_running_service = self.service_ref.ref
 					Screens.Standby.inStandby.paused_service = None
@@ -326,8 +322,9 @@ class RecordTimerEntry(timer.TimerEntry, object):
 				return True
 			if self.justplay:
 				if Screens.Standby.inStandby:
-					self.wasInStandby = True
-					eActionMap.getInstance().bindAction('', -maxint - 1, self.keypress)
+					if not config.misc.standbyCounter.value:
+						self.wasInStandby = True
+						eActionMap.getInstance().bindAction('', -maxint - 1, self.keypress)
 					self.log(11, "wakeup and zap")
 					#set service to zap after standby
 					Screens.Standby.inStandby.prev_running_service = self.service_ref.ref
@@ -716,6 +713,15 @@ class RecordTimer(timer.Timer):
 		for timer in self.timer_list:
 			next_act = timer.getNextActivation()
 			if timer.justplay or next_act < now:
+				continue
+			return next_act
+		return -1
+
+	def getNextTimerTime(self):
+		now = time()
+		for timer in self.timer_list:
+			next_act = timer.getNextActivation()
+			if next_act < now:
 				continue
 			return next_act
 		return -1
