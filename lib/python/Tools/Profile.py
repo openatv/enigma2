9d980cc30bfbed336d43a8be3d8a5e80bcc4c6fb
atvcaptain <captain@web.de>
2013-09-19 17:14:54 +0200

    update progress

--- lib/python/Tools/Profile.py
+++ lib/python/Tools/Profile.py
@@ -1,6 +1,7 @@
 # the implementation here is a bit crappy.
 import time
 from Directories import resolveFilename, SCOPE_CONFIG
+from enigma import getBoxType
 
 PERCENTAGE_START = 50
 PERCENTAGE_END = 100
@@ -40,7 +41,22 @@ def profile(id):
 			else:
 				perc = PERCENTAGE_START
 			try:
-				open("/proc/progress", "w").write("%d \n" % perc)
+				if getBoxType() == "odinm7" or getBoxType() == "odinm6" or getBoxType() == "xp1000s":
+					f = open("/dev/dbox/oled0", "w")
+					f.write("%d" % perc)
+				elif getBoxType() == "gb800se" or getBoxType() == "gb800solo":
+					f = open("/dev/dbox/oled0", "w")
+					f.write("%d  \n" % perc)
+				elif getBoxType() == "gb800seplus":
+					f = open("/dev/mcu", "w")
+					f.write("%d  \n" % perc)					
+				elif getBoxType() == "ebox5000":
+					f = open("/proc/progress", "w")
+					f.write("%d" % perc)
+				else:
+					f = open("/proc/progress", "w")
+					f.write("%d \n" % perc)
+				f.close()
 			except IOError:
 				pass
 
