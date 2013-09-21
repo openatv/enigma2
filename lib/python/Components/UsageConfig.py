bcee249d8f1c3bf2ecb1b36d07a4deaf1ff6fc87
Paul In&In <darezik@gmail.com>
2013-09-21 19:49:02 +0200

    Attempt to use the biggest local disk if /hdd/movie does not exist
    Hopefully, this makes it possible to use a USB disk or similar.
    The whole method of selecting a path has become way too complicated
    now, and is a candidate for redesign (without ever using resolvePath
    again)
    
    This may lead to unexpectedly recording onto another disk in some
    extreme situations though.
    
    But at least it will simply record onto whatever happens to be
    handy at the time, which is better than just giving up.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1,6 +1,6 @@
 from Components.Harddisk import harddiskmanager
 from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection, ConfigText, ConfigNumber, ConfigSet, ConfigLocations, ConfigSelectionNumber, ConfigClock
-from Tools.Directories import resolveFilename, SCOPE_HDD
+from Tools.Directories import resolveFilename, SCOPE_HDD, defaultRecordingLocation
 from enigma import setTunerTypePriorityOrder, setPreferredTuner, setSpinnerOnOff, setEnableTtCachingOnOff;
 from enigma import Misc_Options, eEnv;
 from Components.NimManager import nimmanager
@@ -481,4 +481,4 @@ def preferredInstantRecordPath():
 	return preferredPath(config.usage.instantrec_path.value)
 
 def defaultMoviePath():
-	return config.usage.default_path.value
+	return defaultRecordingLocation(config.usage.default_path.value)
