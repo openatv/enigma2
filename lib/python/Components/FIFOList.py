b144329aa73b719d594756ce9d814a5fdc938275
Paul In&In <darezik@gmail.com>
2013-10-20 20:37:54 +0200

    Visually change behaviour of FIFO list to as it was before
    Visually it behaves as it was before and will show only the latest len
    items that were added to the list. When listAll is called the complete
    list will be shown. So during a scan everything was like it was before,
    but after the scan you're able to select a specific service.

--- lib/python/Components/FIFOList.py
+++ lib/python/Components/FIFOList.py
@@ -1,15 +1,14 @@
 from Components.MenuList import MenuList
 
 class FIFOList(MenuList):
-	def __init__(self, list=[]):
+	def __init__(self, list=[], len=10):
 		self.list = list
+		self.len = len
 		MenuList.__init__(self, self.list)
 
 	def addItem(self, item):
 		self.list.append(item)
-		self.l.setList(self.list)
-		self.selectionEnabled(True)
-		self.down()
+		self.l.setList(self.list[-self.len:])
 
 	def clear(self):
 		del self.list[:]
@@ -17,3 +16,7 @@ class FIFOList(MenuList):
 
 	def getCurrentSelection(self):
 		return self.list and self.getCurrent() or None
+
+	def listAll(self):
+		self.l.setList(self.list)
+		self.selectionEnabled(True)
