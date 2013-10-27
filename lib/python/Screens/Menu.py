ed4e03abf7353cbf67161ea95adb3fe0706ccaa4
Andy Blackburn <andrew.blackburn644@btinternet.com>
2013-10-27 15:51:23 +0000

    [Menu] allow recessive close be optional, as not to break every plugin that uses WHERE_MENU.
    example below:
    		return [(_("Satfinder"), SatfinderMain, "satfinder", 35, True)]
    
    last variable allows self.close to be passed to plugin.
    
    if the call code should be as example below:
    
    def SatfinderMain(session, close=None, **kwargs):

--- lib/python/Screens/Menu.py
+++ lib/python/Screens/Menu.py
@@ -218,7 +218,10 @@ class Menu(Screen):
 					if x[2] == plugin_menuid:
 						list.remove(x)
 						break
-				list.append((l[0], boundFunction(l[1], self.session, close=self.close), l[2], l[3] or 50))
+				if len(l) > 4 and l[4]:
+					list.append((l[0], boundFunction(l[1], self.session, self.close), l[2], l[3] or 50))
+				else:
+					list.append((l[0], boundFunction(l[1], self.session), l[2], l[3] or 50))
 
 		# for the skin: first try a menu_<menuID>, then Menu
 		self.skinName = [ ]
