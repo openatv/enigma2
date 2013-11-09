4859220af9b736b4f7125428a82f23df19275b62
Littlesat <littlesat99@yahoo.com>
2013-11-09 23:35:28 +0100

    ScanSetup: select current active NIM
    Thanks Huevos

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -458,6 +458,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 			self.createSetup()
 
 	def createConfig(self, frontendData):
+			defaultNim = "0"
 			defaultSat = {
 				"orbpos": 192,
 				"system": eDVBFrontendParametersSatellite.System_DVB_S,
@@ -490,6 +491,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 
 			if frontendData is not None:
 				ttype = frontendData.get("tuner_type", "UNKNOWN")
+				defaultNim = str(frontendData.get("tuner_number", 0))
 				if ttype == "DVB-S":
 					defaultSat["system"] = frontendData.get("system", eDVBFrontendParametersSatellite.System_DVB_S)
 					defaultSat["frequency"] = frontendData.get("frequency", 0) / 1000
@@ -540,7 +542,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 						continue
 				nim_list.append((str(n.slot), n.friendly_full_description))
 
-			self.scan_nims = ConfigSelection(choices = nim_list)
+			self.scan_nims = ConfigSelection(choices = nim_list, default = defaultNim)
 
 			# status
 			self.scan_snr = ConfigSlider()
