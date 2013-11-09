8ccfa5948bf1eb7780364019b0ec79d2dace7174
Littlesat <littlesat99@yahoo.com>
2013-11-09 22:56:58 +0100

    ServiceInfo: previous patch from Taapat removed i/p indication

--- lib/python/Screens/ServiceInfo.py
+++ lib/python/Screens/ServiceInfo.py
@@ -118,7 +118,8 @@ class ServiceInfo(Screen):
 				width = self.info.getInfo(iServiceInformation.sVideoWidth)
 				height = self.info.getInfo(iServiceInformation.sVideoHeight)
 				if width > 0 and height > 0:
-					resolution = "%dx%d " % (width,height)
+					resolution = "%dx%d" % (width,height)
+					resolution += ("i", "p", "")[self.info.getInfo(iServiceInformation.sProgressive)]
 					resolution += str((self.info.getInfo(iServiceInformation.sFrameRate) + 500) / 1000)
 					aspect = self.getServiceInfoValue(iServiceInformation.sAspect)
 					if aspect in ( 1, 2, 5, 6, 9, 0xA, 0xD, 0xE ):
