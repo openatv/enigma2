d344cb758836818856c55b17f1c4969f7f4fab9a
Littlesat <littlesat99@yahoo.com>
2013-11-09 22:32:00 +0100

    InfobarGenerics: during swapPiP do not stop service
    As this sometimes does freeze one of both services and it seems
    not to be required at all

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1923,7 +1923,6 @@ class InfoBarPiP:
 			currentServicePath = self.servicelist.getCurrentServicePath()
 			self.servicelist.setCurrentServicePath(self.session.pip.servicePath)	
 			self.session.pip.playService(swapservice)
-			self.session.nav.stopService() # stop portal
 			self.session.nav.playService(pipref) # start subservice
 			self.session.pip.servicePath = currentServicePath
 			if self.servicelist.dopipzap:
