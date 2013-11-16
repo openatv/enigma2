c7cf393d8fae29f42817ff14f89014f450931bc4
ims <ims21@users.sourceforge.net>
2013-11-16 22:00:55 +0100

    small fix in australian UHF
    Signed-off-by: Littlesat <littlesat99@yahoo.com>

--- lib/python/Components/Converter/ChannelNumbers.py
+++ lib/python/Components/Converter/ChannelNumbers.py
@@ -26,7 +26,8 @@ class ChannelNumbers:
 			elif 209 <= f < 230: 	# CH10-CH12
 				return str(int(f - 209)/7 + 6 - 1) + ds
 			elif 526 < f < 820: 	# CH28-CH69
-				return str(int(f - 526)/7 + 28) + ds
+				d = (f - 1) % 7
+				return str(int(f - 526)/7 + 28) + (d < 3 and "-" or d > 4 and "+" or "")
 
 		return ""
 
