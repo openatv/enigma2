f360f4d253b252564a4bc9461eaa75353028ffb4
Black_64 <black6464@gmail.com>
2013-12-02 18:53:44 +0100

    [InfoBarGenerics] fix GS modepip on boxes with no lcd

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2850,7 +2850,7 @@ class InfoBarPiP:
 			if self.session.pip.playService(newservice):
 				self.session.pipshown = True
 				self.session.pip.servicePath = self.servicelist.getCurrentServicePath()
-				if config.lcd.modepip:
+				if SystemInfo["FrontpanelDisplay"]:
 					if config.lcd.modepip.value >= "1":
 						f = open("/proc/stb/lcd/mode", "w")
 						f.write(config.lcd.modepip.value)
