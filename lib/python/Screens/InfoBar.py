57ccfca659d254b6e091d79438f9021d0a354235
Littlesat <littlesat99@yahoo.com>
2014-01-01 12:05:19 +0100

    InfoBar: Declare pip.servivePath when opening PIP from Movieplayer
    This avoids a GSOD when exit movieplayer with stop and then swap the PIP
    Thanks Dima73 for reporting

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -381,6 +381,7 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, \
 			self.session.pip.show()
 			self.session.pipshown = True
 			self.session.pip.playService(slist.getCurrentSelection())
+			self.session.pip.servicePath = slist.getCurrentServicePath()
 
 	def swapPiP(self):
 		pass
