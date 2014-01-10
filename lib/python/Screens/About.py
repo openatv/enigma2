ae18de6bbc2fa59c0f4d0b49888872650829974a
Littlesat <littlesat99@yahoo.com>
2014-01-10 20:03:57 +0100

    Revise read latest commitlogs from Source Forge
    Sourceforge made changes to the website that leads to read too many data
    and incorrect data that leads to a segfault of enigma2.
    There is still some work to be done to improve this feature but to solve
    the GSOD I did already made this commit

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -139,20 +139,24 @@ class CommitInfo(Screen):
 		self.Timer.start(50, True)
 
 	def readCommitLogs(self):
-		url = 'http://sourceforge.net/p/openpli/mailman/openpli-git-commits/'
+		urls = [ 'http://sourceforge.net/p/openpli/enigma2/feed',
+			'http://sourceforge.net/p/openpli/openpli-oe-core/feed']
 		commitlog = ""
+		from urllib2 import urlopen
 		try:
-			import urllib2
-			for x in  "".join(urllib2.urlopen(url, timeout=5).read().split('<td class="email-body">')[1:]).split('<pre>'):
-				for y in x.split('Commit diffs:')[0].replace('New commits:','').strip().split('\n'):
-					y = y.strip()
-					if y or commitlog:
-						if y.startswith('Author:') or y.startswith('Signed-off-by:'):
-							commitlog += y.split('&lt;')[0]
-						else:
-							commitlog += y
-						commitlog += "\n"
-				commitlog += 140*'-' + "\n"
+			for url in urls:
+				commitlog += 80 * '-' + '\n'
+				commitlog += url.split('/')[-2] + '\n'
+				commitlog += 80 * '-' + '\n'
+				for x in  urlopen(url, timeout=5).read().split('<title>')[2:]:
+					for y in x.split("><"):
+						if '</title' in y:
+							title = y[:-7]
+						if '</dc:creator' in y:
+							creator = y.split('>')[1].split('<')[0]
+						if '</pubDate' in y:
+							date = y.split('>')[1].split('<')[0][:-6]
+					commitlog += date + ' ' + creator + '\n' + title + 2 * '\n'
 		except:
 			commitlog = _("Currently the commit log cannot be retreived - please try later again")
 		self["AboutScrollLabel"].setText(commitlog)
