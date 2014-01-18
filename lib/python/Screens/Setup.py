9a919aec4295751cf6f466fa15e15a7c28c4e2bf
Andy Blackburn <andrew.blackburn644@btinternet.com>
2014-01-18 15:50:03 +0000

    [Setup] fix typo.

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -9,7 +9,7 @@ from Components.Sources.StaticText import StaticText
 from Components.Label import Label
 from Components.Sources.Boolean import Boolean
 
-from enigma import eEnv, 
+from enigma import eEnv
 from boxbranding import getMachineBrand, getMachineName
 
 import xml.etree.cElementTree
