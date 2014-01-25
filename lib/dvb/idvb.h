c68e9bb5094ace33e1229242aa81dfa26a5a39a8
betacentauri <betacentauri@arcor.de>
2014-01-26 00:31:11 +0100

    Use type 12 for HE-AAC pid lamedb entry
    Types 10 and 11 are already used by dream. So don't break compatibility
    with dreamboxEdit and other tools.
    
    Signed-off-by: Littlesat <littlesat99@yahoo.com>

--- lib/dvb/idvb.h
+++ lib/dvb/idvb.h
@@ -266,7 +266,7 @@ public:
 	{
 		cVPID, cMPEGAPID, cTPID, cPCRPID, cAC3PID,
 		cVTYPE, cACHANNEL, cAC3DELAY, cPCMDELAY,
-		cSUBTITLE, cAACHEAPID, cacheMax
+		cSUBTITLE, cAACHEAPID=12, cacheMax
 	};
 
 	int getCacheEntry(cacheID);
