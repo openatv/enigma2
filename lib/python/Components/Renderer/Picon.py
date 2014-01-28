3324081f51394256f4fcad73ebc288bb416b46ee
Black_64 <black6464@gmail.com>
2014-01-28 20:22:20 +0100

    [Picon] Fallback for IPTV streams

--- lib/python/Components/Renderer/Picon.py
+++ lib/python/Components/Renderer/Picon.py
@@ -68,7 +68,10 @@ def getPiconName(serviceName):
 		if len(fields) > 2 and fields[2] != '2':
 			#fallback to 1 for tv services with nonstandard servicetypes
 			fields[2] = '1'
-			pngname = findPicon('_'.join(fields))
+		if fields[0] == '4097':
+			#fallback to 1 for IPTV streams
+			fields[0] = '1'
+		pngname = findPicon('_'.join(fields))
 	return pngname
 
 class Picon(Renderer):
