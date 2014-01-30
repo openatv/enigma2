1ebbc6f74e6a7442145310e5860d8d79eef14d50
arn354 <arn354@email.de>
2014-01-30 14:15:32 +0100

    Update Wol.py
    ! remove gbquad based on SystemInfo["WOL"]

--- lib/python/Components/Wol.py
+++ lib/python/Components/Wol.py
@@ -1,6 +1,7 @@
 from config import config, ConfigSubsection, ConfigSelection, ConfigNothing
 from Components.SystemInfo import SystemInfo
 from Tools.Directories import fileExists
+from boxbranding import getBoxType
 
 class WOL:
 	def __init__(self):
@@ -14,11 +15,13 @@ class WOL:
 
 def Init():
 	if SystemInfo["WOL"]:
-		def setWOLmode(value):
-			iwol.setWolState(config.network.wol.value);
-		iwol = WOL()
-		config.network.wol = ConfigSelection([("disable", _("No")), ("enable", _("Yes"))], default = "disable")
-		config.network.wol.addNotifier(setWOLmode, initial_call=True)
+		self.BoxType = getBoxType()
+     		if not self.BoxType == 'gbquad' :
+			def setWOLmode(value):
+				iwol.setWolState(config.network.wol.value);
+			iwol = WOL()
+			config.network.wol = ConfigSelection([("disable", _("No")), ("enable", _("Yes"))], default = "disable")
+			config.network.wol.addNotifier(setWOLmode, initial_call=True)
 	else:
 		def doNothing():
 			pass
