d001b349b55387c42db8f8b07377519ded6890b3
Andy Blackburn <andrew.blackburn644@btinternet.com>
2014-02-05 20:00:06 +0000

    remove Debug.

--- lib/python/Plugins/Plugin.py
+++ lib/python/Plugins/Plugin.py
@@ -76,7 +76,6 @@ class PluginDescriptor:
 			self.where = where
 		else:
 			self.where = [ where ]
-		print 'Name: %s    where: %s' % (name, self.where)
 		self.description = description
 
 		if icon is None or isinstance(icon, str):
