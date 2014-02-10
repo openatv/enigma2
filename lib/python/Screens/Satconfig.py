bd27b3130bd2cc352703977b3f0b25012e8e871f
Littlesat <littlesat99@yahoo.com>
2014-02-10 20:13:16 +0100

    SatConfig/Setup: Allow OK button to quickly select satellites/transponders
    The Green button is to save or start scans. This allows to use the
    choicebox as mentioned in the previous commit within these screens

--- lib/python/Screens/Satconfig.py
+++ lib/python/Screens/Satconfig.py
@@ -448,7 +448,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 
 		self["actions"] = ActionMap(["SetupActions", "SatlistShortcutAction"],
 		{
-			"ok": self.keySave,
+			"save": self.keySave,
 			"cancel": self.keyCancel,
 			"nothingconnected": self.nothingConnectedShortcut
 		}, -2)
--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -304,7 +304,6 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 
 		self["actions"] = NumberActionMap(["SetupActions", "MenuActions"],
 		{
-			"ok": self.keyGo,
 			"save": self.keyGo,
 			"cancel": self.keyCancel,
 			"menu": self.doCloseRecursive,
@@ -993,7 +992,6 @@ class ScanSimple(ConfigListScreen, Screen, CableTransponderSearchSupport):
 
 		self["actions"] = ActionMap(["SetupActions", "MenuActions"],
 		{
-			"ok": self.keyGo,
 			"save": self.keyGo,
 			"cancel": self.keyCancel,
 			"menu": self.doCloseRecursive,
