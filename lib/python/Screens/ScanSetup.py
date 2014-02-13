d2def49bcad9db1e9484d0d1a6797fabb333352a
Littlesat <littlesat99@yahoo.com>
2014-02-13 07:57:54 +0100

    ScanSetup: Reassing OK button to start the scan

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -304,6 +304,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport):
 
 		self["actions"] = NumberActionMap(["SetupActions", "MenuActions"],
 		{
+			"ok": self.keyGo,
 			"save": self.keyGo,
 			"cancel": self.keyCancel,
 			"menu": self.doCloseRecursive,
