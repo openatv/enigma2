e2afa4ea9a477476efb955c2f264320b391e1d41
Black_64 <black6464@gmail.com>
2014-02-14 17:28:28 +0100

    [ImageBackup] fix xp1000 usb folder name

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -252,6 +252,7 @@ class ImageBackup(Screen):
 			self.EXTRA = "%s/fullbackup_%s/%s" % (self.DIRECTORY, self.TYPE, self.DATE)
 		elif self.OEM == "xp" and self.MODEL == "xp1000mk":
 			self.TYPE = "MAXDIGITAL"
+			self.MODEL == "xp1000"
 			self.MKUBIFS_ARGS = "-m 2048 -e 126976 -c 4096"
 			self.UBINIZE_ARGS = "-m 2048 -p 128KiB"
 			self.SHOWNAME = "%s %s" %(self.MACHINEBRAND, self.MODEL)
