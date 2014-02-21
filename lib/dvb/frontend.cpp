8f1aa4d7f0a593805f14634a651522c14a44100f
Littlesat <littlesat99@yahoo.com>
2014-02-21 20:43:43 +0100

    frontend: SNR approximation in dB for new Xtrend DVB-S tuners

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -755,6 +755,11 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 		ret = (int)(snr / 40.5);
 		sat_max = 1618;
 	}
+	if (!strcmp(m_description, "AVL6211")) // ET10000
+	{
+		ret = (int)(snr / 38.55);
+		sat_max = 1700;
+	}
 	else if (strstr("Nova-T StickNovaT 500StickDTB03", m_description)) // dib0700
 	{
 		if ( snr > 300 )
