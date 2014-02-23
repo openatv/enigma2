7b1ccb2076e0725cd8b0de00e4ffe1ed580e9b76
Littlesat <littlesat99@yahoo.com>
2014-02-23 12:49:05 +0100

    Put back DVB subtitles patch
    As it was by accident removed

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -378,7 +378,7 @@ def InitUsageConfig():
 
 	config.subtitles.dvb_subtitles_yellow = ConfigYesNo(default = False)
 	config.subtitles.dvb_subtitles_original_position = ConfigSelection(default = "0", choices = [("0", _("Original")), ("1", _("Fixed")), ("2", _("Relative"))])
-	config.subtitles.dvb_subtitles_centered = ConfigYesNo(default = False)
+	config.subtitles.dvb_subtitles_centered = ConfigYesNo(default = True)
 	config.subtitles.subtitle_bad_timing_delay = ConfigSelection(default = "0", choices = subtitle_delay_choicelist)
 	config.subtitles.dvb_subtitles_backtrans = ConfigSelection(default = "0", choices = [
 		("0", _("No transparency")),
