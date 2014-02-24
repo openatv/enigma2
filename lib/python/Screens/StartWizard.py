bae16a65c50bc59c656fda99869870aa06ef139e
Paul <darezik@gmail.com>
2014-02-24 11:31:18 +0100

    Fix #10

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -38,6 +38,6 @@ class StartWizard(WizardLanguage, Rc):
 		configfile.save()
 
 wizardManager.registerWizard(VideoWizard, config.misc.videowizardenabled.getValue(), priority = 0)
-wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.getValue(), priority = -1)
+#wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.getValue(), priority = -1)
 #wizardManager.registerWizard(UserInterfacePositionerWizard, config.misc.firstrun.getValue(), priority = 3)
 wizardManager.registerWizard(StartWizard, config.misc.firstrun.getValue(), priority = 20)
