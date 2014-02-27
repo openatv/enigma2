94784c0884648c9b2a1736418335b1bf95100c39
arn354 <arn354@email.de>
2014-02-27 09:29:48 +0100

    Update HdmiCec.py
    enhance time values

--- lib/python/Components/HdmiCec.py
+++ lib/python/Components/HdmiCec.py
@@ -32,7 +32,7 @@ config.hdmicec.control_receiver_standby = ConfigYesNo(default = False)
 config.hdmicec.handle_deepstandby_events = ConfigYesNo(default = False)
 config.hdmicec.preemphasis = ConfigYesNo(default = False)	
 choicelist = []
-for i in (10, 50, 100, 150, 250):
+for i in (10, 50, 100, 150, 250, 500, 750, 1000, 1500, 2000):
 	choicelist.append(("%d" % i, "%d ms" % i))
 config.hdmicec.minimum_send_interval = ConfigSelection(default = "0", choices = [("0", _("Disabled"))] + choicelist)
 
