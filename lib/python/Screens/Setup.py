91b0f08eda388cc73743ca0dd52e05f5f121792e
Littlesat <littlesat99@yahoo.com>
2014-03-02 12:18:27 +0100

    Put back declaration of on ChangeEntry in Setup.py
    As it seems some plugins (mediainfo) may get a GSOD without this
    declaration.
    
    Thanks ian1095
    
    http://openpli.org/forums/topic/32822-youve-killed-media-info-again-with-the-latest-changes-to-setuppy/

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -85,6 +85,8 @@ class Setup(ConfigListScreen, Screen):
 		# for the skin: first try a setup_<setupID>, then Setup
 		self.skinName = ["setup_" + setup, "Setup" ]
 
+		self.onChangedEntry = [ ]
+
 		self.setup = setup
 		list = []
 		self.refill(list)
