86c986e10b9e9b3ca53611f792e5931d16bd09ca
atvcaptain <captain@web.de>
2014-03-02 12:33:18 +0100

    update flash online

--- lib/python/Plugins/SystemPlugins/SoftwareManager/Flash_online.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/Flash_online.py
@@ -26,8 +26,8 @@ if distro.lower() == "openmips":
 	image = 1
 elif distro.lower() == "openatv":
 	image = 0
-feedurl_atv = 'http://images.mynonpublic.com/openatv/nightly'
-feedurl_om = 'http://image.openmips.com/2.0'
+feedurl_atv = 'http://images.mynonpublic.com/openatv/beta'
+feedurl_om = 'http://image.openmips.com/4.0'
 imagePath = '/hdd/images'
 flashPath = '/hdd/images/flash'
 flashTmp = '/hdd/images/tmp'
