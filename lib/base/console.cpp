d93283321940af791caee859c7ac33dce57da695
Mike Looijmans <milo-software@users.sourceforge.net>
2014-03-08 10:13:34 +0100

    console.cpp: Remove cryptic debug messages

--- lib/base/console.cpp
+++ lib/base/console.cpp
@@ -209,7 +209,6 @@ void eConsoleAppContainer::closePipes()
 		::close(fd[2]);
 		fd[2]=-1;
 	}
-	eDebug("pipes closed");
 	while( outbuf.size() ) // cleanup out buffer
 	{
 		queue_data d = outbuf.front();
@@ -242,7 +241,6 @@ void eConsoleAppContainer::readyRead(int what)
 	readyErrRead(eSocketNotifier::Priority|eSocketNotifier::Read); /* be sure to flush all data which might be already written */
 	if (hungup)
 	{
-		eDebug("child has terminated");
 		int childstatus;
 		int retval = killstate;
 		/*
