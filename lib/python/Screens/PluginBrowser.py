83c9f0875c8bbeeea86204d6ada73d5cdbf9c3d7
Paul <darezik@gmail.com>
2014-03-09 21:10:40 +0100

    fix #30

--- lib/python/Screens/PluginBrowser.py
+++ lib/python/Screens/PluginBrowser.py
@@ -461,3 +461,4 @@ class PluginDownloadBrowser(Screen):
 				list.append(PluginCategoryComponent(x, expandableIcon, self.listWidth))
 		self.list = list
 		self["list"].l.setList(list)
+		self["text"].setText(_("Download of plugin information complete."))
