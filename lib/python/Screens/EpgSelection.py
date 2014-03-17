11b4c71a326bd0ed53b1ffc7c6a745385fd8f0d8
Andy Blackburn <andrew.blackburn644@btinternet.com>
2014-03-17 02:05:39 +0000

    [EpgSelection] cleanup some code.

--- lib/python/Screens/EpgSelection.py
+++ lib/python/Screens/EpgSelection.py
@@ -736,8 +736,11 @@ class EPGSelection(Screen, HelpableScreen):
 		if self.CurrBouquet and self.CurrService and (self.CurrBouquet != self.StartBouquet or self.CurrService != self.StartRef):
 			self.zapToNumber(self.StartRef, self.StartBouquet)
 		if self.session.nav.getCurrentlyPlayingServiceOrGroup() and self.StartRef and self.session.nav.getCurrentlyPlayingServiceOrGroup().toString() != self.StartRef.toString():
-			if self.zapFunc and ((self.type == EPG_TYPE_GRAPH and config.epgselection.graph_preview_mode.getValue()) or (self.type == EPG_TYPE_MULTI and config.epgselection.multi_preview_mode.getValue()) or (
-						self.type == EPG_TYPE_INFOBAR or self.type == EPG_TYPE_INFOBARGRAPH and config.epgselection.infobar_preview_mode.getValue() == '1' or config.epgselection.infobar_preview_mode.getValue() == '2') or (self.type == EPG_TYPE_ENHANCED and config.epgselection.enhanced_preview_mode.getValue())) and self.StartRef and self.StartBouquet:
+			if self.zapFunc and self.StartRef and self.StartBouquet and (
+								(self.type == EPG_TYPE_GRAPH and config.epgselection.graph_preview_mode.getValue()) or 
+								(self.type == EPG_TYPE_MULTI and config.epgselection.multi_preview_mode.getValue()) or 
+								(self.type in (EPG_TYPE_INFOBAR, EPG_TYPE_INFOBARGRAPH) and config.epgselection.infobar_preview_mode.getValue() in ('1', '2')) or 
+								(self.type == EPG_TYPE_ENHANCED and config.epgselection.enhanced_preview_mode.getValue())):
 				if '0:0:0:0:0:0:0:0:0' not in self.StartRef.toString():
 					self.zapFunc(None, zapback = True)
 			elif '0:0:0:0:0:0:0:0:0' in self.StartRef.toString():
