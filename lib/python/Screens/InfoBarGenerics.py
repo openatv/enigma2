b51c33d3047657ca8bcffcc58afb721295570c56
Paul <darezik@gmail.com>
2014-03-24 12:31:39 +0100

    Fix #4

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1079,6 +1079,7 @@ class InfoBarEPG:
 		self.eventView = None
 		self.epglist = []
 		self.defaultEPGType = self.getDefaultEPGtype()
+		self.defaultGuideType = self.getDefaultGuidetype()
 		self.__event_tracker = ServiceEventTracker(screen=self, eventmap=
 			{
 				iPlayableService.evUpdatedEventInfo: self.__evEventInfoChanged,
@@ -1093,6 +1094,33 @@ class InfoBarEPG:
 				"EPGPressed":  (self.showDefaultEPG, _("show EPG...")),
 				"showSingleEPG": (self.openSingleServiceEPG, _("show single EPG...")),
 				"showEventGuidePlugin": self.showEventGuidePlugins,
+				"showInfobarOrEpgWhenInfobarAlreadyVisible": self.showEventInfoWhenNotVisible,
+			})
+
+class InfoBarEPG:
+	""" EPG - Opens an EPG list when the showEPGList action fires """
+	def __init__(self):
+		self.is_now_next = False
+		self.dlg_stack = []
+		self.bouquetSel = None
+		self.eventView = None
+		self.epglist = []
+		self.defaultEPGType = self.getDefaultEPGtype()
+		self.defaultGuideType = self.getDefaultGuidetype()
+		self.__event_tracker = ServiceEventTracker(screen=self, eventmap=
+			{
+				iPlayableService.evUpdatedEventInfo: self.__evEventInfoChanged,
+			})
+
+		self["EPGActions"] = HelpableActionMap(self, "InfobarEPGActions",
+			{
+				"RedPressed": (self.RedPressed, _("Show epg")),
+				"IPressed": (self.IPressed, _("show program information...")),
+				"InfoPressed": (self.InfoPressed, _("show program information...")),
+				"showEventInfoPlugin": (self.showEventInfoPlugins, _("List EPG functions...")),
+				"EPGPressed":  (self.showDefaultEPG, _("show EPG...")),
+				"showSingleEPG": (self.openSingleServiceEPG, _("show single EPG...")),
+				"showEventGuidePlugin": (self.showEventGuidePlugins, _("List EPG functions...")),
 				"showInfobarOrEpgWhenInfobarAlreadyVisible": self.showEventInfoWhenNotVisible,
 			})
 
@@ -1115,20 +1143,6 @@ class InfoBarEPG:
 		return None
 
 	def showEventInfoPlugins(self):
-		if isStandardInfoBar(self):
-			if getBrandOEM() not in ('xtrend', 'odin', 'ini', 'dags' ,'gigablue', 'xp'):
-				pluginlist = self.getEPGPluginList()
-				if pluginlist:
-					pluginlist.append((_("Select default EPG type..."), self.SelectDefaultInfoPlugin))
-					self.session.openWithCallback(self.EventInfoPluginChosen, ChoiceBox, title=_("Please choose an extension..."), list = pluginlist, skin_name = "EPGExtensionsList")
-				else:
-					self.openSingleServiceEPG()
-			else:
-				self.openEventView()
-		elif isMoviePlayerInfoBar(self):
-			self.openEventView()
-
-	def showEventGuidePlugins(self):
 		if isMoviePlayerInfoBar(self):
 			self.openEventView()
 		else:
@@ -1148,6 +1162,38 @@ class InfoBarEPG:
 			config.usage.defaultEPGType.value = answer[0]
 			config.usage.defaultEPGType.save()
 			configfile.save()
+
+	def getDefaultGuidetype(self):
+		pluginlist = self.getEPGPluginList()
+		config.usage.defaultGuideType=ConfigSelection(default = "None", choices = pluginlist)
+		for plugin in pluginlist:
+			if plugin[0] == config.usage.defaultGuideType.value:
+				return plugin[1]
+		return None
+
+	def showEventGuidePlugins(self):
+		if isMoviePlayerInfoBar(self):
+			self.openEventView()
+		else:
+			pluginlist = self.getEPGPluginList()
+			if pluginlist:
+				pluginlist.append((_("Select default EPG type..."), self.SelectDefaultGuidePlugin))
+				self.session.openWithCallback(self.EventGuidePluginChosen, ChoiceBox, title=_("Please choose an extension..."), list = pluginlist, skin_name = "EPGExtensionsList")
+			else:
+				self.openSingleServiceEPG()
+
+	def SelectDefaultGuidePlugin(self):
+		self.session.openWithCallback(self.DefaultGuidePluginChosen, ChoiceBox, title=_("Please select a default EPG type..."), list = self.getEPGPluginList(), skin_name = "EPGExtensionsList")
+
+	def DefaultGuidePluginChosen(self, answer):
+		if answer is not None:
+			self.defaultGuideType = answer[1]
+			config.usage.defaultGuideType.value = answer[0]
+			config.usage.defaultGuideType.save()
+
+	def EventGuidePluginChosen(self, answer):
+		if answer is not None:
+			answer[1]()
 
 	def runPlugin(self, plugin):
 		plugin(session = self.session, servicelist=self.servicelist)
@@ -1177,6 +1223,7 @@ class InfoBarEPG:
 	def EPGPressed(self):
 		if isStandardInfoBar(self) or isMoviePlayerInfoBar(self):
 			self.openGraphEPG()
+			#self.openMultiServiceEPG
 
 	def showEventInfoWhenNotVisible(self):
 		if self.shown:
@@ -1372,7 +1419,7 @@ class InfoBarEPG:
 			self.defaultGuideType()
 			return
 		self.EPGPressed()
-		
+
 	def openEventView(self, simple=False):
 		if self.servicelist is None:
 			return
