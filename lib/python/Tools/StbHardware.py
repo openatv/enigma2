9bab08e417fd616dc1c32ad96296daee0ac91170
atvcaptain <captain@web.de>
2014-04-12 22:02:29 +0200

    remove workaround setrtc for ini too, this fix 0 bytes records

--- lib/python/Tools/StbHardware.py
+++ lib/python/Tools/StbHardware.py
@@ -51,8 +51,6 @@ def setRTCoffset():
 		print "set RTC Offset failed!"
 
 def setRTCtime(wutime):
-	if getBrandOEM().startswith('ini'):
-		setRTCoffset()
 	try:
 		f = open("/proc/stb/fp/rtc", "w")
 		f.write(str(wutime))
