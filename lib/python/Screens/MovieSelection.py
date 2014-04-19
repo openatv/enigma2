54254babe5aad97874d39f9b6f9e5b3a3dbb893d
Littlesat <littlesat99@yahoo.com>
2014-04-19 11:36:56 +0200

    Revert "Add screensaver to MovieSelection for playing MP3s"
    As it seems here to break the EOF detection of MP3s, I still do not have a
    good clarification why
    
    This reverts commit d30a83d75128263d77a4aab5134e9bd3e4470bda.

--- lib/python/Screens/MovieSelection.py
+++ lib/python/Screens/MovieSelection.py
@@ -20,7 +20,6 @@ from Plugins.Plugin import PluginDescriptor
 from Screens.MessageBox import MessageBox
 from Screens.ChoiceBox import ChoiceBox
 from Screens.LocationBox import MovieLocationBox
-from Screens import ScreenSaver
 from Screens.HelpMenu import HelpableScreen
 import Screens.InfoBar
 
@@ -427,19 +426,15 @@ class MovieSelectionSummary(Screen):
 		else:
 			self["name"].text = ""
 
-from Screens.InfoBarGenerics import InfoBarScreenSaver
-
-class MovieSelection(Screen, HelpableScreen, SelectionEventInfo, InfoBarBase, InfoBarScreenSaver):
+class MovieSelection(Screen, HelpableScreen, SelectionEventInfo, InfoBarBase):
 	# SUSPEND_PAUSES actually means "please call my pauseService()"
 	ALLOW_SUSPEND = Screen.SUSPEND_PAUSES
 
 	def __init__(self, session, selectedmovie = None, timeshiftEnabled = False):
 		Screen.__init__(self, session)
 		HelpableScreen.__init__(self)
-		InfoBarScreenSaver.__init__(self)
 		if not timeshiftEnabled:
 			InfoBarBase.__init__(self) # For ServiceEventTracker
-
 		self.initUserDefinedActions()
 		self.tags = {}
 		if selectedmovie:
@@ -991,7 +986,6 @@ class MovieSelection(Screen, HelpableScreen, SelectionEventInfo, InfoBarBase, In
 				ext = os.path.splitext(path)[1].lower()
 				if config.movielist.play_audio_internal.value and (ext in AUDIO_EXTENSIONS):
 					self.preview()
-					self.ScreenSaverTimerStart()
 					return
 				if self.list.playInBackground:
 					# Stop preview, come back later
