c926ed0ef4ff338afa71470d86f200c9255a38d4
Littlesat <littlesat99@yahoo.com>
2014-04-20 20:33:47 +0200

    NimManager: Second try to solve the rotor issue after adding 32 LNBs
    I found another range from 1 to 37 that should be changed from 1 to 69, so
    32 more LNBs

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -269,7 +269,7 @@ class SecConfigure:
 			pass
 
 		lnbSat = {}
-		for x in range(1,69):
+		for x in range(1, 69):
 			lnbSat[x] = []
 
 		#wildcard for all satellites ( for rotor )
@@ -286,7 +286,7 @@ class SecConfigure:
 				print "add", x[0], "to", lnb
 				lnbSat[lnb].append(x[0])
 
-		for x in range(1,37):
+		for x in range(1, 69):
 			if len(lnbSat[x]) > 0:
 				currLnb = config.Nims[slotid].advanced.lnb[x]
 				sec.addLNB()
