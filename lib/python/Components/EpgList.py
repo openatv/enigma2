6e83935756669c83b2c321d340054063d401b23f
Peter Urbanec <bitbucket.org@urbanec.net>
2014-04-22 12:24:47 +1000

    Prevent crash due to out of bounds access.

--- lib/python/Components/EpgList.py
+++ lib/python/Components/EpgList.py
@@ -352,7 +352,7 @@ class EPGList(HTMLComponent, GUIComponent):
 			refstr = self.cur_service[0]
 			print '[EPG DEBUG] self.cur_event:',self.cur_event
 			print '[EPG DEBUG] len(events):',events and len(events)
-			if self.cur_event is None or not events or (self.cur_event and events and self.cur_event > len(events)):
+			if self.cur_event is None or not events or (self.cur_event and events and self.cur_event >= len(events)):
 				return None, ServiceReference(refstr)
 			event = events[self.cur_event] #(event_id, event_title, begin_time, duration)
 			eventid = event[0]
