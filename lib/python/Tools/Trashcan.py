419080edfc6b8dbd960f55b8b56a0a44525dc719
atvcaptain <captain@web.de>
2014-04-22 13:15:44 +0200

    fix GS create trash folder

--- lib/python/Tools/Trashcan.py
+++ lib/python/Tools/Trashcan.py
@@ -26,7 +26,10 @@ def getTrashFolder(path=None):
 def createTrashFolder(path=None):
 	trash = getTrashFolder(path)
 	if not os.path.isdir(trash):
-		os.mkdir(trash)
+		try:
+			os.mkdir(trash)
+		except:
+			pass
 	return trash
 
 def get_size(start_path = '.'):
