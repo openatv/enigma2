9da34953fe4a1a309848aa4761fa93987327cf07
Littlesat <Littlesat99@yahoo.com>
2014-05-07 23:35:09 +0200

    Correctly flag encrypted channels during service scan
    By adding a fake CAID 0000 during the scan when no CAIDs are known during the scan itself.

--- lib/dvb/scan.cpp
+++ lib/dvb/scan.cpp
@@ -1146,29 +1146,26 @@ RESULT eDVBScan::processSDT(eDVBNamespace dvbnamespace, const ServiceDescription
 	{
 		unsigned short service_id = (*s)->getServiceId();
 		SCAN_eDebugNoNewLine("SID %04x: ", service_id);
-		bool add = true;
+		bool is_crypted = false;
 
-		if (m_flags & scanOnlyFree)
+		std::map<unsigned short, service>::iterator it =
+			m_pmts_to_read.find(service_id);
+		if (it != m_pmts_to_read.end())
 		{
-			std::map<unsigned short, service>::iterator it =
-				m_pmts_to_read.find(service_id);
-			if (it != m_pmts_to_read.end())
+			if (it->second.scrambled)
 			{
-				if (it->second.scrambled)
-				{
-					SCAN_eDebug("is scrambled!");
-					add = false;
-				}
-				else
-					SCAN_eDebug("is free");
+				SCAN_eDebug("is scrambled!");
+				is_crypted = true;
 			}
-			else {
-				SCAN_eDebug("not found in PAT.. so we assume it is scrambled!!");
-				add = false;
-			}
+			else
+				SCAN_eDebug("is free");
+		}
+		else {
+			SCAN_eDebug("not found in PAT.. so we assume it is scrambled!!");
+			is_crypted = true;
 		}
 
-		if (add)
+		if (!(m_flags & scanOnlyFree) || !is_crypted)
 		{
 			eServiceReferenceDVB ref;
 			ePtr<eDVBService> service = new eDVBService;
@@ -1233,6 +1230,9 @@ RESULT eDVBScan::processSDT(eDVBNamespace dvbnamespace, const ServiceDescription
 					break;
 				}
 			}
+
+			if (is_crypted and !service->m_ca.size())
+				service->m_ca.push_front(0);
 
 			std::pair<std::map<eServiceReferenceDVB, ePtr<eDVBService> >::iterator, bool> i =
 				m_new_services.insert(std::pair<eServiceReferenceDVB, ePtr<eDVBService> >(ref, service));
