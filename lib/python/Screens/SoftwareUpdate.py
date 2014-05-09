934f5353e0de71bf2820fecd368e3498b5979031
Paul <darezik@gmail.com>
2014-05-09 08:03:49 +0200

    Fixed COLD Upgrade

--- lib/python/Screens/SoftwareUpdate.py
+++ lib/python/Screens/SoftwareUpdate.py
@@ -254,15 +254,15 @@ class UpdatePlugin(Screen):
 				currentTimeoutDefault = socket.getdefaulttimeout()
 				socket.setdefaulttimeout(3)
 				try:
-					config.softwareupdate.updateisunstable.setValue(urlopen("http://enigma2.world-of-satellite.com/feeds/" + getImageVersion() + "/status").read())
+					config.softwareupdate.updateisunstable.value = int(urlopen("http://enigma2.world-of-satellite.com/feeds/" + getImageVersion() + "/status").read())
 				except:
-					config.softwareupdate.updateisunstable.setValue('1')
+					config.softwareupdate.updateisunstable.value = 1
 				socket.setdefaulttimeout(currentTimeoutDefault)
 				self.total_packages = None
-				#if config.softwareupdate.updateisunstable.value == '1' and config.softwareupdate.updatebeta.value:
+				#if config.softwareupdate.updateisunstable.value == 1 and config.softwareupdate.updatebeta.value:
 				#	self.total_packages = len(self.ipkg.getFetchedList())
 				#	message = _("The current update may be unstable") + "\n" + _("Are you sure you want to update your %s %s ?") % (getMachineBrand(), getMachineName()) + "\n(" + (ngettext("%s updated package available", "%s updated packages available", self.total_packages) % self.total_packages) + ")"
-				#elif config.softwareupdate.updateisunstable.value == '0':
+				#elif config.softwareupdate.updateisunstable.value == 0:
 				self.total_packages = len(self.ipkg.getFetchedList())
 				message = _("Do you want to update your %s %s ?") % (getMachineBrand(), getMachineName()) + "\n(" + (ngettext("%s updated package available", "%s updated packages available", self.total_packages) % self.total_packages) + ")"
 				if self.total_packages:
@@ -360,11 +360,11 @@ class UpdatePlugin(Screen):
 			self.slider.setValue(1)
 			self.ipkg.startCmd(IpkgComponent.CMD_LIST, args = {'installed_only': True})
 		elif answer[1] == "cold":
-			if (config.softwareupdate.autosettingsbackup.value and config.backupmanager.backuplocation.value) or (config.softwareupdate.autoimagebackup.value and config.imagemanager.backuplocation.value):
-				self.doAutoBackup()
-			else:
-				self.session.open(TryQuitMainloop,retvalue=42)
-				self.close()
+			#if (config.softwareupdate.autosettingsbackup.value and config.backupmanager.backuplocation.value) or (config.softwareupdate.autoimagebackup.value and config.imagemanager.backuplocation.value):
+				#self.doAutoBackup()
+			#else:
+			self.session.open(TryQuitMainloop,retvalue=42)
+			self.close()
 		else:
 			self.ipkg.startCmd(IpkgComponent.CMD_UPGRADE, args = {'test_only': False})
 
