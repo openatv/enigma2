428d401cb3fa31bc531be598df8dbe6c5f0ab8d4
Littlesat <littlesat99@yahoo.com>
2014-06-02 19:36:19 +0200

    Changes in InfoBarGenerics to correctly switch new PIP modes
    It could be that later I'll find a better way to work-a-round this

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1907,6 +1907,18 @@ class InfoBarPiP:
 				self.addExtension((self.getShowHideName, self.showPiP, self.pipShown), "blue")
 				self.addExtension((self.getMoveName, self.movePiP, self.pipShown), "green")
 
+		self.onExecBegin.append(self.reclocatePiP)
+
+	def reclocatePiP(self):
+		if self.session.pipshown:
+			self.relocatePiPTimer = eTimer()
+			self.relocatePiPTimer.callback.append(self.relocatePiPTimeout)
+			self.relocatePiPTimer.start(100, True)
+
+	def relocatePiPTimeout(self):
+		if self.session.pipshown:
+			self.session.pip.relocate()
+
 	def pipShown(self):
 		return self.session.pipshown
 
@@ -1957,6 +1969,7 @@ class InfoBarPiP:
 			if self.session.pip.playService(newservice):
 				self.session.pipshown = True
 				self.session.pip.servicePath = self.servicelist.getCurrentServicePath()
+				self.reclocatePiP()
 			else:
 				self.session.pipshown = False
 				del self.session.pip
