130d661522452fc70bf97785d25875fb6e5fc594
arn354 <arn354@email.de>
2014-06-28 16:22:07 +0200

    Update SystemInfo.py

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -1,7 +1,7 @@
 from enigma import eDVBResourceManager, Misc_Options
 from Tools.Directories import fileExists, fileCheck, resolveFilename, SCOPE_SKIN
 from Tools.HardwareInfo import HardwareInfo
-from boxbranding import getBoxType
+from boxbranding import getBoxType, getMachineBuild
 
 SystemInfo = { }
 
@@ -39,8 +39,8 @@ SystemInfo["GBWOL"] = fileExists("/usr/bin/gigablue_wol")
 SystemInfo["Fan"] = fileCheck("/proc/stb/fp/fan")
 SystemInfo["FanPWM"] = SystemInfo["Fan"] and fileCheck("/proc/stb/fp/fan_pwm")
 SystemInfo["StandbyLED"] = fileCheck("/proc/stb/power/standbyled")
-SystemInfo["WakeOnLAN"] = fileCheck("/proc/stb/power/wol") or fileCheck("/proc/stb/fp/wol")
-SystemInfo["HasExternalPIP"] = getBoxType() != 'et9x00' and fileCheck("/proc/stb/vmpeg/1/external")
+SystemInfo["WakeOnLAN"] = (fileCheck("/proc/stb/power/wol") or fileCheck("/proc/stb/fp/wol")) and getBoxType() not in ('gbquadplus')
+SystemInfo["HasExternalPIP"] = getMachineBuild() not in ('et9x00', 'et6x00', 'et5x00') and fileCheck("/proc/stb/vmpeg/1/external")
 SystemInfo["VideoDestinationConfigurable"] = fileExists("/proc/stb/vmpeg/0/dst_left")
 SystemInfo["hasPIPVisibleProc"] = fileCheck("/proc/stb/vmpeg/1/visible")
 
