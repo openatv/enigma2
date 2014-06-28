385f6d8e58c3651690287fa8530c8c74bc220f8e
littlesat <littlesat99@yahoo.com>
2014-06-28 16:16:36 +0200

    About: allow via left/right buttons more project commit logs
    previously you could only view enigma2 and oe. Now with left/right you can
    view more of our commit logs

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -131,32 +131,62 @@ class CommitInfo(Screen):
 				"cancel": self.close,
 				"ok": self.close,
 				"up": self["AboutScrollLabel"].pageUp,
-				"down": self["AboutScrollLabel"].pageDown
+				"down": self["AboutScrollLabel"].pageDown,
+				"left": self.left,
+				"right": self.right
 			})
 
+		self.project = 0
+		self.projects = [
+			("enigma2", "Enigma2"),
+			("openpli-oe-core", "Openpli Oe Core"),
+			("enigma2-plugins", "Enigma2 Plugins"),
+			("aio-grab", "Aio Grab"),
+			("gst-plugin-dvbmediasink", "Gst Plugin Dvbmediasink"),
+			("openembedded", "Openembedded"),
+			("plugin-xmltvimport", "Plugin Xmltvimport"),
+			("plugins-enigma2", "Plugins Enigma2"),
+			("skin-magic", "Skin Magic"),
+			("tuxtxt", "Tuxtxt")
+		]
+		self.cachedProjects = {}
 		self.Timer = eTimer()
 		self.Timer.callback.append(self.readCommitLogs)
 		self.Timer.start(50, True)
 
 	def readCommitLogs(self):
-		urls = [ 'http://sourceforge.net/p/openpli/enigma2/feed',
-			'http://sourceforge.net/p/openpli/openpli-oe-core/feed']
+		url = 'http://sourceforge.net/p/openpli/%s/feed' % self.projects[self.project][0]
 		commitlog = ""
 		from urllib2 import urlopen
 		try:
-			for url in urls:
-				commitlog += 80 * '-' + '\n'
-				commitlog += url.split('/')[-2] + '\n'
-				commitlog += 80 * '-' + '\n'
-				for x in  urlopen(url, timeout=5).read().split('<title>')[2:]:
-					for y in x.split("><"):
-						if '</title' in y:
-							title = y[:-7]
-						if '</dc:creator' in y:
-							creator = y.split('>')[1].split('<')[0]
-						if '</pubDate' in y:
-							date = y.split('>')[1].split('<')[0][:-6]
-					commitlog += date + ' ' + creator + '\n' + title + 2 * '\n'
+			commitlog += 80 * '-' + '\n'
+			commitlog += url.split('/')[-2] + '\n'
+			commitlog += 80 * '-' + '\n'
+			for x in  urlopen(url, timeout=5).read().split('<title>')[2:]:
+				for y in x.split("><"):
+					if '</title' in y:
+						title = y[:-7]
+					if '</dc:creator' in y:
+						creator = y.split('>')[1].split('<')[0]
+					if '</pubDate' in y:
+						date = y.split('>')[1].split('<')[0][:-6]
+				commitlog += date + ' ' + creator + '\n' + title + 2 * '\n'
+			self.cachedProjects[self.projects[self.project][1]] = commitlog
 		except:
 			commitlog = _("Currently the commit log cannot be retrieved - please try later again")
 		self["AboutScrollLabel"].setText(commitlog)
+
+	def updateCommitLogs(self):
+		if self.cachedProjects.has_key(self.projects[self.project][1]):
+			self["AboutScrollLabel"].setText(self.cachedProjects[self.projects[self.project][1]])
+		else:
+			self["AboutScrollLabel"].setText(_("Please wait"))
+			self.Timer.start(50, True)
+
+	def left(self):
+		self.project = self.project == 0 and len(self.projects) - 1 or self.project - 1
+		self.updateCommitLogs()
+
+	def right(self):
+		self.project = self.project != len(self.projects) - 1 and self.project + 1 or 0
+		self.updateCommitLogs()
