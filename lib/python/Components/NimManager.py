d57e729c2e46f9d3c51c75c23fe6d5e573612690
Pr2 <dummy@dummy.com>
2014-06-28 19:03:26 +0200

    Strip space before DVB-x in multi_type case
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -781,7 +781,7 @@ class NimManager:
 					# "Mode 0" -> ["Mode", "0"]
 					split2 = split[0].split(" ")
 					modes = entries[current_slot].get("multi_type", {})
-					modes[split2[1]] = split[1]
+					modes[split2[1]] = split[1].strip()
 					entries[current_slot]["multi_type"] = modes
 			elif line.startswith("I2C_Device:"):
 				input = int(line[len("I2C_Device:") + 1:])
