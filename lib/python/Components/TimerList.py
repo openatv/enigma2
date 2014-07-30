e9034f2e9a5e74b06f8fc97fbfb5a66789d66b7c
Littlesat <littlesat99@yahoo.com>
2014-07-30 15:22:38 +0200

    Small changes to TimerList as suggested by DeBeek

--- lib/python/Components/TimerList.py
+++ lib/python/Components/TimerList.py
@@ -70,8 +70,8 @@ class TimerList(HTMLComponent, GUIComponent, object):
 		if timer.disabled:
 			state = _("disabled")
 
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, 24, 25, 126, 20, 1, RT_HALIGN_LEFT|RT_VALIGN_TOP, state))
-		res.append((eListboxPythonMultiContent.TYPE_TEXT, 150,27, 60, 18, 2, RT_HALIGN_LEFT|RT_VALIGN_TOP, self.getOrbitalPos(timer.service_ref)))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, 24, 25, 136, 20, 1, RT_HALIGN_LEFT|RT_VALIGN_TOP, state))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, 165,27, 60, 18, 2, RT_HALIGN_LEFT|RT_VALIGN_TOP, self.getOrbitalPos(timer.service_ref)))
 		if icon:
 			res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHATEST, 0, 0, 20, 20, icon))
 		return res
