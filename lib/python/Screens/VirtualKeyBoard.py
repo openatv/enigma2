98c4bb91a0b5eaeeca24b43fe049f209482a30dd
littlesat <littlesat99@yahoo.com>
2014-08-05 18:54:13 +0200

    Add + and ~ key in Virtual keybord for English language

--- lib/python/Screens/VirtualKeyBoard.py
+++ lib/python/Screens/VirtualKeyBoard.py
@@ -251,13 +251,13 @@ class VirtualKeyBoard(Screen):
 				[u"q", u"w", u"e", u"r", u"t", u"y", u"u", u"i", u"o", u"p", u"-", u"["],
 				[u"a", u"s", u"d", u"f", u"g", u"h", u"j", u"k", u"l", u";", u"'", u"\\"],
 				[u"<", u"z", u"x", u"c", u"v", u"b", u"n", u"m", u",", ".", u"/", u"ALL"],
-				[u"SHIFT", u"SPACE", u"OK", u"LEFT", u"RIGHT"]]
+				[u"SHIFT", u"SPACE", u"OK", u"LEFT", u"RIGHT", u"*"]]
 			self.shiftkeys_list = [
 				[u"EXIT", u"!", u"@", u"#", u"$", u"%", u"^", u"&", u"(", u")", u"=", u"BACKSPACE"],
-				[u"Q", u"W", u"E", u"R", u"T", u"Y", u"U", u"I", u"O", u"P", u"*", u"]"],
+				[u"Q", u"W", u"E", u"R", u"T", u"Y", u"U", u"I", u"O", u"P", u"+", u"]"],
 				[u"A", u"S", u"D", u"F", u"G", u"H", u"J", u"K", u"L", u"?", u'"', u"|"],
 				[u">", u"Z", u"X", u"C", u"V", u"B", u"N", u"M", u";", u":", u"_", u"CLEAR"],
-				[u"SHIFT", u"SPACE", u"OK", u"LEFT", u"RIGHT"]]
+				[u"SHIFT", u"SPACE", u"OK", u"LEFT", u"RIGHT", u"~"]]
 			self.lang = 'en_EN'
 			self.nextLang = 'de_DE'
 		self["country"].setText(self.lang)
