2db31eadf67b58312af2457f4344b3e4e3c4b918
littlesat <littlesat99@yahoo.com>
2014-08-10 17:26:33 +0200

    Move pictureviewer resolution to enigma2 configs
    As it was somewhere hidden and also used in fact in the MoviePlayer

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -235,6 +235,7 @@ def InitUsageConfig():
 	config.usage.show_eit_nownext = ConfigYesNo(default = True)
 	config.usage.show_vcr_scart = ConfigYesNo(default = False)
 	config.usage.show_update_disclaimer = ConfigYesNo(default = True)
+	config.usage.pic_resolution = ConfigSelection(default = None, choices = [(None, _("Same resolution as skin")), ("(720, 576)","720x576"), ("(1280, 720)", "1280x720"), ("(1920, 1080)", "1920x1080")])
 
 	if SystemInfo["Fan"]:
 		choicelist = [('off', _("Off")), ('on', _("On")), ('auto', _("Auto"))]
