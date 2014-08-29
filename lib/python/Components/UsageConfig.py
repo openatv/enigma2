1ab62b2640d21b2a32e0f018cb04f07c808284e2
Littlesat <littlesat99@yahoo.com>
2014-08-29 15:28:11 +0200

    Try to solve WakeOnLan configuration for VU devices
    As I do know own a VU with WakeOnLan I'm not able to test this

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -260,7 +260,10 @@ def InitUsageConfig():
 
 	if SystemInfo["WakeOnLAN"]:
 		def wakeOnLANChanged(configElement):
-			open(SystemInfo["WakeOnLAN"], "w").write(configElement.value and "on" or "off")
+			if "fb" in SystemInfo["WakeOnLAN"]:
+				open(SystemInfo["WakeOnLAN"], "w").write(configElement.value and "1" or "0")
+			else:
+				open(SystemInfo["WakeOnLAN"], "w").write(configElement.value and "on" or "off")
 		config.usage.wakeOnLAN = ConfigYesNo(default = False)
 		config.usage.wakeOnLAN.addNotifier(wakeOnLANChanged)
 
