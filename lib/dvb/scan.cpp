openatv/enigma2

commit c1473bd6a41a356fd6eec3f24be059d4d7437c02

eDVBScan: fix NULL pointer segfault
introduced by 8d8ca4272ee1da26a4edf8867e171b7626f352ae

lib/dvb/scan.cpp
@@ -145,8 +145,7 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 				eDVBFrontendParametersTerrestrial parm;
 				m_ch_current->getDVBT(parm);
 				ePtr<iDVBFrontend> fe;
-				ePtr<eDVBFrontend> frontend;
-				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !m_channel->getFrontend(fe) && frontend->supportsDeliverySystem(SYS_DVBT2, true))
+				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !m_channel->getFrontend(fe) && fe && fe->supportsDeliverySystem(SYS_DVBT2, true))
 				{
 					/* we have to scan T2 as well as T */
 					parm.system = eDVBFrontendParametersTerrestrial::System_DVB_T2;

commit f4b2955c77320a0d66e7a83bbbe323c11a7d4160

eDVBScan: fix compile error
introduced by c1473bd6a41a356fd6eec3f24be059d4d7437c02
use iDVBFrontend::isCompatible instead of eDVBFrontend::supportsDeliverySystem

lib/dvb/scan.cpp
@@ -144,14 +144,23 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 			{
 				eDVBFrontendParametersTerrestrial parm;
 				m_ch_current->getDVBT(parm);
-				ePtr<iDVBFrontend> fe;
-				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2 && !m_channel->getFrontend(fe) && fe && fe->supportsDeliverySystem(SYS_DVBT2, true))
+				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2)
 				{
 					/* we have to scan T2 as well as T */
+					ePtr<iDVBFrontend> fe;
+					eDVBFrontendParameters eparm;
 					parm.system = eDVBFrontendParametersTerrestrial::System_DVB_T2;
-					ePtr<eDVBFrontendParameters> feparm = new eDVBFrontendParameters;
-					feparm->setDVBT(parm);
-					addChannelToScan(feparm);
+					eparm.setDVBT(parm);
+					m_channel->getFrontend(fe);
+					if (fe)
+					{
+						ePtr<iDVBFrontendParameters> feparm = new eDVBFrontendParameters(eparm);
+						/* but only if the frontend supports T2 */
+						if (fe->isCompatibleWith(feparm))
+						{
+							addChannelToScan(feparm);
+						}
+					}
 				}
 			}
 		}