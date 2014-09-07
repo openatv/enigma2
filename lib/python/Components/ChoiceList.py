14c8546f2d3028e0152fcb9976743db66b288268
Mike Looijmans <milo-software@users.sourceforge.net>
2014-09-07 15:06:36 +0200

    ChoiceList: Only load PNG when key is defined
    As suggested here:
    http://openpli.org/forums/topic/28532-merge-requests-for-plis-git/page-32#entry441828
    
    Use a format-string instead of concatenation. This allows any printable
    object to be used as key name (e.g. numbers) instead of forcing key to be
    a string, it's more efficient and it won't crash when key is something else.

--- lib/python/Components/ChoiceList.py
+++ lib/python/Components/ChoiceList.py
@@ -4,17 +4,16 @@ from enigma import RT_HALIGN_LEFT, eListboxPythonMultiContent, gFont
 from Tools.LoadPixmap import LoadPixmap
 import skin
 
-def ChoiceEntryComponent(key = "", text = ["--"]):
+def ChoiceEntryComponent(key = None, text = ["--"]):
 	res = [ text ]
 	if text[0] == "--":
 		res.append((eListboxPythonMultiContent.TYPE_TEXT, 0, 00, 800, 25, 0, RT_HALIGN_LEFT, "-"*200))
 	else:
 		res.append((eListboxPythonMultiContent.TYPE_TEXT, 45, 00, 800, 25, 0, RT_HALIGN_LEFT, text[0]))
-
-		png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "skin_default/buttons/key_" + key + ".png"))
-		if png is not None:
-			res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHATEST, 5, 0, 35, 25, png))
-
+		if key:
+			png = LoadPixmap(resolveFilename(SCOPE_CURRENT_SKIN, "skin_default/buttons/key_%s.png" % key))
+			if png is not None:
+				res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHATEST, 5, 0, 35, 25, png))
 	return res
 
 class ChoiceList(MenuList):
