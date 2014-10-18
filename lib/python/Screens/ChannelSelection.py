4d7ca68018b4f17b85d1d2b107d80fdca1308225
littlesat <littlesat99@yahoo.com>
2014-10-18 14:12:18 +0200

    ChannelSelection: Disable movemode when selecting channel
    Since we do not block all options anymore when we are in movemode this is
    required as when leaving the channelselection it can stay in the movemode
    when selecting a service from a non userbouquet service list.

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1686,6 +1686,7 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
 					self.startServiceRef = None
 					self.startRoot = None
 					self.correctChannelNumber()
+					self.movemode and self.toggleMoveMode()
 					self.editMode = False
 					self.close(ref)
 
