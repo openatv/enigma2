b66ab96ca4b8abd9b7eeffa5f23d1f61de282088
dima73 <dima73@dev.null>
2014-10-26 15:38:32 +0100

    about.py: make two texts translatable
    Signed-off-by: Erik Slagter <erik@openpli.org>

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -55,7 +55,7 @@ def getCPUInfoString():
 		        if line[0] == "cpu MHz":
 		                cpu_speed = "%1.0f" % float(line[1])
 		                cpu_count += 1
-		return "%s %s MHz (%d %s)" % (processor, cpu_speed, cpu_count, cpu_count > 1 and "cores" or "core")
+		return "%s %s MHz (%d %s)" % (processor, cpu_speed, cpu_count, cpu_count > 1 and _("cores") or _("core"))
 	except:
 		return _("undefined")
 
