8069d8019e26b408dd5b8fe35077189b48e39f76
atvcaptain <captain@web.de>
2014-10-28 07:35:02 +0100

    enbale rca for 'formuler3', 'enibox', 'mago'

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -79,8 +79,8 @@ class AVSwitch:
 	# 	del modes["DVI-PC"]
 	if modes.has_key("YPbPr") and getBoxType() in ('mago', 'enibox','mutant1100', 'et4x00', 'et7500', 'et7000', 'et8500', 'xp1000mk', 'xp1000max', 'xp1000plus', 'sf8', 'tm2t', 'tmsingle', 'vusolo2', 'tmnano', 'iqonios300hd', 'iqonios300hdv2', 'classm', 'axodin', 'axodinc', 'genius', 'evo', 'galaxym6', 'geniuse3hd', 'evoe3hd', 'axase3', 'axase3c', 'dm500hdv2', 'dm500hd', 'dm800', 'mixosf7', 'mixoslumi', 'mixosf5mini', 'gi9196lite', 'ixusszero', 'optimussos1', 'enfinity', 'marvel1', 'sezam1000hd', 'mbmini', 'atemio5x00', 'xpeedlx1', 'xpeedlx2', 'vusolose', 'gbipbox', 'formuler3', 'optimussos3plus', 'force1plus') or (about.getModelString() == 'ini-3000'):
 		del modes["YPbPr"]
-	if modes.has_key("Scart") and getBoxType() in ('formuler3', 'enibox', 'mago'):
-		del modes["Scart"]
+	#if modes.has_key("Scart") and getBoxType() in ('formuler3', 'enibox', 'mago'):
+	#	del modes["Scart"]
 
 	def __init__(self):
 		self.last_modes_preferred =  [ ]
