d93dc9a4ae6200fe2847778301bdd8ead468e3c1
littlesat <littlesat99@yahoo.com>
2014-11-05 21:33:48 +0100

    About: Streamline some code around the CPU temperature

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -50,24 +50,19 @@ def getCPUInfoString():
 	try:
 		cpu_count = 0
 		for line in open("/proc/cpuinfo").readlines():
-		        line = [x.strip() for x in line.strip().split(":")]
-		        if line[0] == "system type":
-		                processor = line[1].split()[0]
-		        if line[0] == "cpu MHz":
-		                cpu_speed = "%1.0f" % float(line[1])
-		                cpu_count += 1
+			line = [x.strip() for x in line.strip().split(":")]
+			if line[0] == "system type":
+				processor = line[1].split()[0]
+			if line[0] == "cpu MHz":
+				cpu_speed = "%1.0f" % float(line[1])
+				cpu_count += 1
+		if os.path.isfile('/proc/stb/fp/temp_sensor_avs'):
+			temperature = open("/proc/stb/fp/temp_sensor_avs").readline().replace('\n','')
+			return "%s %s MHz (%s) %s°C" % (processor, cpu_speed, ngettext("%d core", "%d cores", cpu_count) % cpu_count, temperature)
 		return "%s %s MHz (%s)" % (processor, cpu_speed, ngettext("%d core", "%d cores", cpu_count) % cpu_count)
 	except:
 		return _("undefined")
 
-def getCPUTempString():
-	try:
-		if os.path.isfile('/proc/stb/fp/temp_sensor_avs'):
-			temperature = open("/proc/stb/fp/temp_sensor_avs").readline().replace('\n','')
-			return _("%s°C") % temperature
-	except:
-		pass
-	return ""
 
 # For modules that do "from About import about"
 about = sys.modules[__name__]
