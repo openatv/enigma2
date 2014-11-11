3a2e2623f68e6821d4b7ce5306b335ffaa271734
littlesat <littlesat99@yahoo.com>
2014-11-11 19:11:44 +0100

    VideoTune: Video Fine Tuning -> Testscreens
    I suggest mentioning it Testscreen is a much better description than call
    it Video Fine Tuning.

--- lib/python/Plugins/SystemPlugins/VideoTune/plugin.py
+++ lib/python/Plugins/SystemPlugins/VideoTune/plugin.py
@@ -8,7 +8,7 @@ def startSetup(menuid):
 	if menuid != "system":
 		return [ ]
 
-	return [(_("Video fine tuning"), videoFinetuneMain, "video_finetune", None)]
+	return [(_("Testscreens"), videoFinetuneMain, "video_finetune", None)]
 
 def Plugins(**kwargs):
-	return PluginDescriptor(name=_("Video fine tuning"), description=_("fine-tune your display"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc=startSetup)
+	return PluginDescriptor(name=_("Testscreens"), description=_("Testscreens that are helpfull to fine-tune your display"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc=startSetup)
