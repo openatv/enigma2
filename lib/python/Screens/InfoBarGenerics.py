9131d73a5b9944fca8c649c42f02777ffb6f92ad
littlesat <littlesat99@yahoo.com>
2014-11-12 00:12:05 +0100

    InfoBarGenerics: Solve issue with channel EPG after zap timer

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -997,8 +997,7 @@ class InfoBarEPG:
 		self.serviceSel = None
 
 	def openSingleServiceEPG(self):
-		ref = self.servicelist.lastservice.value
-		ref = ref and eServiceReference(ref)
+		ref = self.servicelist.getCurrentSelection()
 		if ref:
 			if self.servicelist.getMutableList(): # bouquet in channellist
 				current_path = self.servicelist.getRoot()
