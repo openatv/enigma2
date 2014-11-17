28c8fe0e689c204bf8993d8ce22845a8eb621e0a
arn354 <arn354@email.de>
2014-11-17 17:28:57 +0100

    ImageBackup.py - change self.EXTRA to gain a zippable folderstructure
    fix cp MAINDEST to EXTRA - removed duplicate folder.
    Blame willy for changes :)

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -163,7 +163,7 @@ class ImageBackup(Screen):
 		self.SHOWNAME = "%s %s" %(self.MACHINEBRAND, self.MODEL)
 		self.MAINDESTOLD = "%s/%s" %(self.DIRECTORY, self.MODEL)
 		self.MAINDEST = "%s/%s" %(self.DIRECTORY,self.IMAGEFOLDER)
-		self.EXTRA = "%s/fullbackup_%s/%s" % (self.DIRECTORY, self.IMAGEFOLDER, self.DATE)
+		self.EXTRA = "%s/fullbackup_%s/%s/%s" % (self.DIRECTORY, self.MODEL, self.DATE, self.IMAGEFOLDER)
 		self.EXTRAOLD = "%s/fullbackup_%s/%s/%s" % (self.DIRECTORY, self.MODEL, self.DATE, self.MODEL)
 
 
@@ -260,7 +260,7 @@ class ImageBackup(Screen):
 		system('mv %s/root.%s %s/%s' %(self.WORKDIR, self.ROOTFSTYPE, self.MAINDEST, self.ROOTFSBIN))
 		system('mv %s/vmlinux.gz %s/%s' %(self.WORKDIR, self.MAINDEST, self.KERNELBIN))
 		cmdlist.append('echo "rename this file to "force" to force an update without confirmation" > %s/noforce' %self.MAINDEST)
-		cmdlist.append('cp -r %s %s' % (self.MAINDEST, self.EXTRA))
+		cmdlist.append('cp -r %s %s/' % (self.MAINDEST, self.EXTRA))
 
 		if self.MODEL in ("gbquad", "gbquadplus", "gb800ue", "gb800ueplus"):
 			lcdwaitkey = '/usr/share/lcdwaitkey.bin'
