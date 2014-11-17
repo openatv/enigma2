85d4d7a66cc8cbcdbdb037f32cfa059a1ed8c856
Peter Urbanec <bitbucket.org@urbanec.net>
2014-11-18 01:54:22 +1100

    PEP8 cleanup

--- lib/python/Components/Task.py
+++ lib/python/Components/Task.py
@@ -5,9 +5,10 @@ from Tools.CList import CList
 
 class Job(object):
 	NOT_STARTED, IN_PROGRESS, FINISHED, FAILED = range(4)
+
 	def __init__(self, name):
-		self.tasks = [ ]
-		self.resident_tasks = [ ]
+		self.tasks = []
+		self.resident_tasks = []
 		self.workspace = "/tmp"
 		self.current_task = 0
 		self.callback = None
@@ -33,12 +34,17 @@ class Job(object):
 			return self.end
 		t = self.tasks[self.current_task]
 		jobprogress = t.weighting * t.progress / float(t.end) + sum([task.weighting for task in self.tasks[:self.current_task]])
-		return int(jobprogress*self.weightScale)
+		return int(jobprogress * self.weightScale)
 
 	progress = property(getProgress)
 
 	def getStatustext(self):
-		return { self.NOT_STARTED: _("Waiting"), self.IN_PROGRESS: _("In progress"), self.FINISHED: _("Finished"), self.FAILED: _("Failed") }[self.status]
+		return {
+			self.NOT_STARTED: _("Waiting"),
+			self.IN_PROGRESS: _("In progress"),
+			self.FINISHED: _("Finished"),
+			self.FAILED: _("Failed")
+		}[self.status]
 
 	def task_progress_changed_CB(self):
 		self.state_changed()
@@ -73,7 +79,7 @@ class Job(object):
 			self.tasks[self.current_task].run(self.taskCallback)
 			self.state_changed()
 
-	def taskCallback(self, task, res, stay_resident = False):
+	def taskCallback(self, task, res, stay_resident=False):
 		cb_idx = self.tasks.index(task)
 		if stay_resident:
 			if cb_idx not in self.resident_tasks:
@@ -115,9 +121,9 @@ class Job(object):
 class Task(object):
 	def __init__(self, job, name):
 		self.name = name
-		self.immediate_preconditions = [ ]
-		self.global_preconditions = [ ]
-		self.postconditions = [ ]
+		self.immediate_preconditions = []
+		self.global_preconditions = []
+		self.postconditions = []
 		self.returncode = None
 		self.initial_input = None
 		self.job = None
@@ -126,7 +132,7 @@ class Task(object):
 		self.__progress = 0
 		self.cmd = None
 		self.cwd = "/tmp"
-		self.args = [ ]
+		self.args = []
 		self.cmdline = None
 		self.task_progress_changed = None
 		self.output_line = ""
@@ -146,8 +152,8 @@ class Task(object):
 	def setCmdline(self, cmdline):
 		self.cmdline = cmdline
 
-	def checkPreconditions(self, immediate = False):
-		not_met = [ ]
+	def checkPreconditions(self, immediate=False):
+		not_met = []
 		if immediate:
 			preconditions = self.immediate_preconditions
 		else:
@@ -210,8 +216,8 @@ class Task(object):
 			i = self.output_line.find('\n')
 			if i == -1:
 				break
-			self.processOutputLine(self.output_line[:i+1])
-			self.output_line = self.output_line[i+1:]
+			self.processOutputLine(self.output_line[:i + 1])
+			self.output_line = self.output_line[i + 1:]
 
 	def processOutputLine(self, line):
 		print "[Task %s]" % self.name, line[:-1]
@@ -224,11 +230,11 @@ class Task(object):
 	def abort(self):
 		if self.container:
 			self.container.kill()
-		self.finish(aborted = True)
+		self.finish(aborted=True)
 
-	def finish(self, aborted = False):
+	def finish(self, aborted=False):
 		self.afterRun()
-		not_met = [ ]
+		not_met = []
 		if aborted:
 			not_met.append(AbortedPostcondition())
 		else:
@@ -241,8 +247,8 @@ class Task(object):
 	def afterRun(self):
 		pass
 
-	def writeInput(self, input):
-		self.container.write(input)
+	def writeInput(self, inp):
+		self.container.write(inp)
 
 	def getProgress(self):
 		return self.__progress
@@ -265,6 +271,7 @@ class LoggingTask(Task):
 	def __init__(self, job, name):
 		Task.__init__(self, job, name)
 		self.log = []
+
 	def processOutput(self, data):
 		print "[%s]" % self.name, data,
 		self.log.append(data)
@@ -280,14 +287,18 @@ class PythonTask(Task):
 		self.timer = eTimer()
 		self.timer.callback.append(self.onTimer)
 		self.timer.start(5)
+
 	def work(self):
-		raise NotImplemented, "work"
+		raise NotImplemented("work")
+
 	def abort(self):
 		self.aborted = True
 		if self.callback is None:
-			self.finish(aborted = True)
+			self.finish(aborted=True)
+
 	def onTimer(self):
 		self.setProgress(self.pos)
+
 	def onComplete(self, result):
 		self.postconditions.append(FailedPostcondition(result))
 		self.timer.stop()
@@ -306,25 +317,30 @@ class ConditionTask(Task):
 	def __init__(self, job, name, timeoutCount=None):
 		Task.__init__(self, job, name)
 		self.timeoutCount = timeoutCount
+
 	def _run(self):
 		self.triggerCount = 0
+
 	def prepare(self):
 		from enigma import eTimer
 		self.timer = eTimer()
 		self.timer.callback.append(self.trigger)
 		self.timer.start(1000)
+
 	def cleanup(self, failed):
 		if hasattr(self, 'timer'):
 			self.timer.stop()
 			del self.timer
+
 	def check(self):
 		# override to return True only when condition triggers
 		return True
+
 	def trigger(self):
 		self.triggerCount += 1
 		try:
 			if (self.timeoutCount is not None) and (self.triggerCount > self.timeoutCount):
-				raise Exception, "Timeout elapsed, sorry"
+				raise Exception("Timeout elapsed, sorry")
 			res = self.check()
 		except Exception, e:
 			self.postconditions.append(FailedPostcondition(e))
@@ -337,9 +353,8 @@ class ConditionTask(Task):
 # It also supports a notification when some error occurred, and possibly a retry.
 class JobManager:
 	def __init__(self):
-		self.active_jobs = [ ]
-		self.failed_jobs = [ ]
-		self.job_classes = [ ]
+		self.active_jobs = []
+		self.failed_jobs = []
 		self.in_background = False
 		self.visible = False
 		self.active_job = None
@@ -368,7 +383,7 @@ class JobManager:
 			Notifications.AddNotificationWithCallback(self.errorCB, MessageBox, _("Error: %s\nRetry?") % (problems[0].getErrorMessage(task)))
 			return True
 		else:
-			Notifications.AddNotification(MessageBox, job.name + "\n" + _("Error") + ': %s' % (problems[0].getErrorMessage(task)), type = MessageBox.TYPE_ERROR )
+			Notifications.AddNotification(MessageBox, job.name + "\n" + _("Error") + ': %s' % (problems[0].getErrorMessage(task)), type=MessageBox.TYPE_ERROR)
 			return False
 
 	def jobDone(self, job, task, problems):
@@ -401,11 +416,11 @@ class JobManager:
 			self.kick()
 
 	def getPendingJobs(self):
-		list = [ ]
+		job_list = []
 		if self.active_job:
-			list.append(self.active_job)
-		list += self.active_jobs
-		return list
+			job_list.append(self.active_job)
+		job_list += self.active_jobs
+		return job_list
 
 # some examples:
 #class PartitionExistsPostcondition:
@@ -481,7 +496,7 @@ class ToolExistsPrecondition(Condition):
 
 	def check(self, task):
 		import os
-		if task.cmd[0]=='/':
+		if task.cmd[0] == '/':
 			self.realpath = task.cmd
 			print "[Task.py][ToolExistsPrecondition] WARNING: usage of absolute paths for tasks should be avoided!"
 			return os.access(self.realpath, os.X_OK)
@@ -489,7 +504,7 @@ class ToolExistsPrecondition(Condition):
 			self.realpath = task.cmd
 			path = os.environ.get('PATH', '').split(os.pathsep)
 			path.append(task.cwd + '/')
-			absolutes = filter(lambda file: os.access(file, os.X_OK), map(lambda directory, file = task.cmd: os.path.join(directory, file), path))
+			absolutes = filter(lambda file: os.access(file, os.X_OK), map(lambda directory, file=task.cmd: os.path.join(directory, file), path))
 			if absolutes:
 				self.realpath = absolutes[0]
 				return True
@@ -524,6 +539,7 @@ class ReturncodePostcondition(Condition):
 class FailedPostcondition(Condition):
 	def __init__(self, exception):
 		self.exception = exception
+
 	def getErrorMessage(self, task):
 		if isinstance(self.exception, int):
 			if hasattr(task, 'log'):
@@ -534,6 +550,7 @@ class FailedPostcondition(Condition):
 			else:
 				return _("Error code") + " %s" % self.exception
 		return str(self.exception)
+
 	def check(self, task):
 		return (self.exception is None) or (self.exception == 0)
 
