a2ed628dd3de4ec6a25557e688eadf4fb769d139
Peter Urbanec <bitbucket.org@urbanec.net>
2014-11-18 04:08:23 +1100

    [Task] Tag debug output.

--- lib/python/Components/Task.py
+++ lib/python/Components/Task.py
@@ -74,7 +74,7 @@ class Job(object):
 				self.callback(self, None, [])
 				self.callback = None
 			else:
-				print "still waiting for %d resident task(s) %s to finish" % (len(self.resident_tasks), str(self.resident_tasks))
+				print "[Task] still waiting for %d resident task(s) %s to finish" % (len(self.resident_tasks), str(self.resident_tasks))
 		else:
 			self.tasks[self.current_task].run(self.taskCallback)
 			self.state_changed()
@@ -84,18 +84,18 @@ class Job(object):
 		if stay_resident:
 			if cb_idx not in self.resident_tasks:
 				self.resident_tasks.append(self.current_task)
-				print "task going resident:", task
+				print "[Task] going resident:", task
 			else:
-				print "task keeps staying resident:", task
+				print "[Task] keep staying resident:", task
 				return
 		if len(res):
-			print ">>> Error:", res
+			print "[Task] >>> Error:", res
 			self.status = self.FAILED
 			self.state_changed()
 			self.callback(self, task, res)
 		if cb_idx != self.current_task:
 			if cb_idx in self.resident_tasks:
-				print "resident task finished:", task
+				print "[Task] resident task finished:", task
 				self.resident_tasks.remove(cb_idx)
 		if not res:
 			self.state_changed()
@@ -175,11 +175,11 @@ class Task(object):
 		if self.cwd is not None:
 			self.container.setCWD(self.cwd)
 		if not self.cmd and self.cmdline:
-			print "execute:", self.container.execute(self.cmdline), self.cmdline
+			print "[Task] execute:", self.container.execute(self.cmdline), self.cmdline
 		else:
 			assert self.cmd is not None
 			assert len(self.args) >= 1
-			print "execute:", self.container.execute(self.cmd, *self.args), ' '.join(self.args)
+			print "[Task] execute:", self.container.execute(self.cmd, *self.args), ' '.join(self.args)
 		if self.initial_input:
 			self.writeInput(self.initial_input)
 
@@ -220,7 +220,7 @@ class Task(object):
 			self.output_line = self.output_line[i + 1:]
 
 	def processOutputLine(self, line):
-		print "[Task %s]" % self.name, line[:-1]
+		print "[Task] [%s]" % self.name, line[:-1]
 		pass
 
 	def processFinished(self, returncode):
@@ -273,7 +273,7 @@ class LoggingTask(Task):
 		self.log = []
 
 	def processOutput(self, data):
-		print "[%s]" % self.name, data,
+		print "[Task] [%s]" % self.name, data,
 		self.log.append(data)
 
 
@@ -387,7 +387,7 @@ class JobManager:
 			return False
 
 	def jobDone(self, job, task, problems):
-		print "job", job, "completed with", problems, "in", task
+		print "[Task] job", job, "completed with", problems, "in", task
 		if problems:
 			if not job.onFail(job, task, problems):
 				self.errorCB(False)
@@ -407,10 +407,10 @@ class JobManager:
 
 	def errorCB(self, answer):
 		if answer:
-			print "retrying job"
+			print "[Task] retrying job"
 			self.active_job.retry()
 		else:
-			print "not retrying job."
+			print "[Task] not retrying job."
 			self.failed_jobs.append(self.active_job)
 			self.active_job = None
 			self.kick()
@@ -498,7 +498,7 @@ class ToolExistsPrecondition(Condition):
 		import os
 		if task.cmd[0] == '/':
 			self.realpath = task.cmd
-			print "[Task.py][ToolExistsPrecondition] WARNING: usage of absolute paths for tasks should be avoided!"
+			print "[Task] [ToolExistsPrecondition] WARNING: usage of absolute paths for tasks should be avoided!"
 			return os.access(self.realpath, os.X_OK)
 		else:
 			self.realpath = task.cmd
