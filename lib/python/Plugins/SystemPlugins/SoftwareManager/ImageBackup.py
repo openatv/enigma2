d357989a326dc259c333e8d555823a8074690bde
arn354 <arn354@email.de>
2014-11-17 18:26:39 +0100

    ImageBackup.py - fix copy 3rd attempt - blame willy :)
    remove useless first copy step - just increases backup time. correct second copy step instead

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -260,7 +260,6 @@ class ImageBackup(Screen):
 		system('mv %s/root.%s %s/%s' %(self.WORKDIR, self.ROOTFSTYPE, self.MAINDEST, self.ROOTFSBIN))
 		system('mv %s/vmlinux.gz %s/%s' %(self.WORKDIR, self.MAINDEST, self.KERNELBIN))
 		cmdlist.append('echo "rename this file to "force" to force an update without confirmation" > %s/noforce' %self.MAINDEST)
-		cmdlist.append('cp -r %s/* %s/' % (self.MAINDEST, self.EXTRA))
 
 		if self.MODEL in ("gbquad", "gbquadplus", "gb800ue", "gb800ueplus"):
 			lcdwaitkey = '/usr/share/lcdwaitkey.bin'
@@ -278,7 +277,8 @@ class ImageBackup(Screen):
 			f.write("'rootfstype=jffs2 bmem=106M@150M root=/dev/mtdblock6 rw '")
 			f.write('"\n')
 			f.close()
-		cmdlist.append('cp -r %s %s' % (self.MAINDEST, self.EXTRA))
+
+		cmdlist.append('cp -r %s/* %s/' % (self.MAINDEST, self.EXTRA))
 
 		cmdlist.append("sync")
 		file_found = True
