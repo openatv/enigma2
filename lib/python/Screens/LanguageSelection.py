de2225724410171c56be1ce2eb1296f6f8d1414f
daTa <data-devil@arcor.de>
2014-11-18 18:51:19 +0100

    [LanguageSelection.py] add summary description for selected Language

--- lib/python/Screens/LanguageSelection.py
+++ lib/python/Screens/LanguageSelection.py
@@ -41,6 +41,7 @@ class LanguageSelection(Screen):
 		self.list = []
 # 		self["flag"] = Pixmap()
 		self["summarylangname"] = StaticText()
+		self["summarylangsel"] = StaticText()
 		self["languages"] = List(self.list)
 		self["languages"].onSelectionChanged.append(self.changed)
 
@@ -136,6 +137,7 @@ class LanguageSelection(Screen):
 
 		self.setTitle(_cached("T2"))
 		self["summarylangname"].setText(_cached("T2"))
+		self["summarylangsel"].setText(self["languages"].getCurrent()[1])
 		self["key_red"].setText(_cached("T3"))
 		self["key_green"].setText(_cached("T4"))
 # 		index = self["languages"].getCurrent()[2]
