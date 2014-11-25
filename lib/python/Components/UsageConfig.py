06d81b9c849bb58f7287d9058616f2d14176c9dd
atvcaptain <captain@web.de>
2014-11-25 12:02:08 +0100

    enable ci message

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -326,7 +326,7 @@ def InitUsageConfig():
 	config.usage.frontend_priority.addNotifier(PreferredTunerChanged)
 
 	config.usage.hide_zap_errors = ConfigYesNo(default = True)
-	config.usage.hide_ci_messages = ConfigYesNo(default = True)
+	config.usage.hide_ci_messages = ConfigYesNo(default = False)
 	config.usage.show_cryptoinfo = ConfigSelection([("0", _("Off")),("1", _("One line")),("2", _("Two lines"))], "2")
 	config.usage.show_eit_nownext = ConfigYesNo(default = True)
 	config.usage.show_vcr_scart = ConfigYesNo(default = False)
