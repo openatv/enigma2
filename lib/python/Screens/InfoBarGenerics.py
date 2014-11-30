df78438751e02e38be5750a115bc412351a69c2b
Littlesat <Littlesat99@yahoo.com>
2014-11-30 09:47:06 +0100

    Also hide buffering status when playing audio (radio) streams
    Previously the detection if a stream was started was by detecting the VideoResolution
    This of course does not work for audio streams. Therefore I added a evGstreamerPlayStarted
    event to detect if a stream is actually starting to play which is now used to hide
    the buffer indicator.

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -373,7 +373,7 @@ class BufferIndicator(Screen):
 			{
 				iPlayableService.evBuffering: self.bufferChanged,
 				iPlayableService.evStart: self.__evStart,
-				iPlayableService.evVideoSizeChanged: self.__evVideoStarted,
+				iPlayableService.evGstreamerPlayStarted: self.__evGstreamerPlayStarted,
 			})
 
 	def bufferChanged(self):
@@ -392,7 +392,7 @@ class BufferIndicator(Screen):
 		self.mayShow = True
 		self.hide()
 
-	def __evVideoStarted(self):
+	def __evGstreamerPlayStarted(self):
 		self.mayShow = False
 		self.hide()
 
