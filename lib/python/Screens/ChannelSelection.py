416ce7fc5e2a76ae3c502d4aeb9a0131655c460e
littlesat <littlesat99@yahoo.com>
2014-12-04 00:29:22 +0100

    Bring back ChannelSelectEPGActions
    As it was required in some plugins. In addition in ChannelSelection use
    this map so not all buttons are "checked" for hotkey anymore

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -513,7 +513,7 @@ class ChannelSelectionEPG(InfoBarHotkey):
 			("Info (EPG)" + " " + _("long"), "info_long", "Infobar/showEventInfoPlugins"),
 			("Epg/Guide", "epg", "Plugins/Extensions/GraphMultiEPG/1"),
 			("Epg/Guide" + " " + _("long"), "epg_long", "Infobar/showEventInfoPlugins")]
-		self["ChannelSelectEPGActions"] = hotkeyActionMap(["HotkeyActions"], dict((x[1], self.hotkeyGlobal) for x in self.hotkeys))
+		self["ChannelSelectEPGActions"] = hotkeyActionMap(["ChannelSelectEPGActions"], dict((x[1], self.hotkeyGlobal) for x in self.hotkeys))
 		self.eventViewEPG = self.start_bouquet = self.epg_bouquet = None
 		self.currentSavedPath = []
 		self.onExecBegin.append(self.clearLongkeyPressed)
