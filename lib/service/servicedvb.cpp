f9ffc2f5d1b72a75c1ce99f518b607a5a136d919
ims <ims21@users.sourceforge.net>
2014-12-05 12:58:18 +0100

    sanity check for fallback service when is wrong service reference in bouquet
    Signed-off-by: Erik Slagter <erik@openpli.org>

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1843,7 +1843,8 @@ RESULT eDVBServicePlay::getName(std::string &name)
 	}
 	else if (m_is_stream)
 	{
-		m_dvb_service->getName(m_reference, name);
+		if (m_dvb_service)
+			m_dvb_service->getName(m_reference, name);
 		if (name.empty())
 			name = m_reference.name;
 			if (name.empty())
