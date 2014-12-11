eec6ad54811373f42920343213c42896401f7c2d
pieterg <pieterg@users.sourceforge.net>
2014-12-11 21:08:58 +0100

    dvb: use runtime DVB_API_VERSION check
    we cannot rely on a compiletime check, because
    the linux-libc-headers we're building against
    might differ from the actual kernel headers.
    So first do a compiletime check for the availability
    of defines and enums (in linux-libc-headers),
    followed by a runtime check for the actual kernel
    support (where applicable).

--- lib/dvb/frontend.h
+++ lib/dvb/frontend.h
@@ -80,6 +80,8 @@ private:
 	int m_dvbid;
 	int m_slotid;
 	int m_fd;
+#define DVB_VERSION(major, minor) ((major << 8) | minor)
+	int m_dvbversion;
 	bool m_rotor_mode;
 	bool m_need_rotor_workaround;
 	std::map<fe_delivery_system_t, bool> m_delsys, m_delsys_whitelist;
