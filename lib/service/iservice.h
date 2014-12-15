1d1cd45453df5fd4b222ee4a16c0558434dad1c5
Paul <darezik@gmail.com>
2014-12-15 23:17:10 +0100

    Fix commpile error try 3

--- lib/service/iservice.h
+++ lib/service/iservice.h
@@ -270,7 +270,7 @@ public:
 	virtual ePtr<iServiceInfoContainer> getInfoObject(int w);
 	virtual ePtr<iDVBTransponderData> getTransponderData(const eServiceReference &ref);
 	virtual long long getFileSize(const eServiceReference &ref);
-	virtual bool isCrypted();
+	virtual bool isCrypted(const eServiceReference &ref);
 
 	virtual int setInfo(const eServiceReference &ref, int w, int v);
 	virtual int setInfoString(const eServiceReference &ref, int w, const char *v);
@@ -637,6 +637,7 @@ public:
 	virtual RESULT startTimeshift()=0;
 	virtual RESULT stopTimeshift(bool swToLive=true)=0;
 	virtual RESULT setNextPlaybackFile(const char *fn)=0; // not needed by our internal timeshift.. but external plugin...
+	virtual void goToNextPlaybackFile()=0;
 
 	virtual int isTimeshiftActive()=0;
 	virtual int isTimeshiftEnabled()=0;
