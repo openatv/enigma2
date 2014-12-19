3f2d4266af71c46c91aeadd2b731f4677f62b45e
ims <ims21@users.sourceforge.net>
2014-12-19 12:44:21 +0100

    Meminfo callable from hotkey
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/Hotkey.py
+++ lib/python/Screens/Hotkey.py
@@ -179,6 +179,7 @@ def getHotkeyFunctions():
 	hotkeyFunctions.append((_("Harddisk Setup"), "Setup/harddisk", "Setup"))
 	hotkeyFunctions.append((_("Subtitles Settings"), "Setup/subtitlesetup", "Setup"))
 	hotkeyFunctions.append((_("Language"), "Module/Screens.LanguageSelection/LanguageSelection", "Setup"))
+	hotkeyFunctions.append((_("Memory Info"), "Module/Screens.About/MemoryInfo", "Setup"))
 	if os.path.isdir("/etc/ppanels"):
 		for x in [x for x in os.listdir("/etc/ppanels") if x.endswith(".xml")]:
 			x = x[:-4]
