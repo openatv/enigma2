3e2db4ea53d477c27c0fafdc29123c51e7ef9f79
Huevos <rowland@moralet.com>
2014-12-27 20:32:10 +0100

    [EPG] Add option to delete EPG cache in cases of corruption. Accessed via EPG Load/Save menu.

--- lib/dvb/epgcache.h
+++ lib/dvb/epgcache.h
@@ -329,7 +329,6 @@ private:
 #endif
 	void sectionRead(const uint8_t *data, int source, channel_data *channel);
 	void gotMessage(const Message &message);
-	void flushEPG(const uniqueEPGKey & s=uniqueEPGKey());
 	void cleanLoop();
 
 // called from main thread
@@ -350,6 +349,7 @@ public:
 	void save();
 	void load();
 	void timeUpdated();
+	void flushEPG(const uniqueEPGKey & s=uniqueEPGKey());
 #ifndef SWIG
 	eEPGCache();
 	~eEPGCache();
