04b5930be7c59f2f1bed17c2ac2dfc3a5568d404
Athanasios Oikonomou <athoik@gmail.com>
2015-01-11 19:40:07 +0200

    AVSwitch: use fileExists instead try catch check file not exists
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -2,6 +2,7 @@ from config import config, ConfigSlider, ConfigSelection, ConfigYesNo, \
 	ConfigEnableDisable, ConfigSubsection, ConfigBoolean, ConfigSelectionNumber, ConfigNothing, NoSave
 from enigma import eAVSwitch, getDesktop
 from SystemInfo import SystemInfo
+from Tools.Directories import fileExists
 import os
 
 class AVSwitch:
@@ -146,58 +147,31 @@ def InitAVSwitch():
 	iAVSwitch.setInput("ENCODER") # init on startup
 	SystemInfo["ScartSwitch"] = eAVSwitch.getInstance().haveScartSwitch()
 
-	try:
-		can_downmix_ac3 = "downmix" in open("/proc/stb/audio/ac3_choices", "r").read()
-	except:
-		can_downmix_ac3 = False
-
-	SystemInfo["CanDownmixAC3"] = can_downmix_ac3
-	if can_downmix_ac3:
+	SystemInfo["CanDownmixAC3"] = fileExists("/proc/stb/audio/ac3_choices") and "downmix" in open("/proc/stb/audio/ac3_choices", "r").read()
+	if SystemInfo["CanDownmixAC3"]:
 		def setAC3Downmix(configElement):
 			open("/proc/stb/audio/ac3", "w").write(configElement.value and "downmix" or "passthrough")
 		config.av.downmix_ac3 = ConfigYesNo(default = True)
 		config.av.downmix_ac3.addNotifier(setAC3Downmix)
 
-	try:
-		can_downmix_dts = "downmix" in open("/proc/stb/audio/dts_choices", "r").read()
-	except:
-		can_downmix_dts = False
-
-	SystemInfo["CanDownmixDTS"] = can_downmix_dts
-	if can_downmix_dts:
+	SystemInfo["CanDownmixDTS"] = fileExists("/proc/stb/audio/dts_choices") and "downmix" in open("/proc/stb/audio/dts_choices", "r").read()
+	if SystemInfo["CanDownmixDTS"]:
 		def setDTSDownmix(configElement):
 			open("/proc/stb/audio/dts", "w").write(configElement.value and "downmix" or "passthrough")
 		config.av.downmix_dts = ConfigYesNo(default = True)
 		config.av.downmix_dts.addNotifier(setDTSDownmix)
 
-	try:
-		can_downmix_aac = "downmix" in open("/proc/stb/audio/aac_choices", "r").read()
-	except:
-		can_downmix_aac = False
-
-	SystemInfo["CanDownmixAAC"] = can_downmix_aac
-	if can_downmix_aac:
+	SystemInfo["CanDownmixAAC"] = fileExists("/proc/stb/audio/aac_choices") and "downmix" in open("/proc/stb/audio/aac_choices", "r").read()
+	if SystemInfo["CanDownmixAAC"]:
 		def setAACDownmix(configElement):
 			open("/proc/stb/audio/aac", "w").write(configElement.value and "downmix" or "passthrough")
 		config.av.downmix_aac = ConfigYesNo(default = True)
 		config.av.downmix_aac.addNotifier(setAACDownmix)
 
-	try:
-		can_osd_alpha = open("/proc/stb/video/alpha", "r") and True or False
-	except:
-		can_osd_alpha = False
-
-	try:
-		can_osd_alpha = open("/proc/stb/video/alpha", "r") and True or False
-	except:
-		can_osd_alpha = False
-
-	SystemInfo["CanChangeOsdAlpha"] = can_osd_alpha
-
-	def setAlpha(config):
-		open("/proc/stb/video/alpha", "w").write(str(config.value))
-
-	if can_osd_alpha:
+	SystemInfo["CanChangeOsdAlpha"] = fileExists("/proc/stb/video/alpha")
+	if SystemInfo["CanChangeOsdAlpha"]:
+		def setAlpha(config):
+			open("/proc/stb/video/alpha", "w").write(str(config.value))
 		config.av.osd_alpha = ConfigSlider(default=255, limits=(0,255))
 		config.av.osd_alpha.addNotifier(setAlpha)
 
