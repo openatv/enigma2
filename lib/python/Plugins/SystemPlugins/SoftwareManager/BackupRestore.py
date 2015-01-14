234d5360378c3cad9e05f2df0c48c3c412b26654
Captain <captain.onboard@web.de>
2015-01-14 19:41:33 +0100

    softwareManager change ipkg to opkg

--- lib/python/Plugins/SystemPlugins/SoftwareManager/BackupRestore.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/BackupRestore.py
@@ -103,8 +103,8 @@ class BackupScreen(Screen, ConfigListScreen):
 			if not "/tmp/changed-configfiles.txt" in self.backupdirs:
 				self.backupdirs = self.backupdirs + " /tmp/changed-configfiles.txt"
 
-			cmd1 = "ipkg list-installed | egrep 'enigma2-plugin-|task-base|packagegroup-base' > /tmp/installed-list.txt"
-			cmd2 = "ipkg list-changed-conffiles > /tmp/changed-configfiles.txt"
+			cmd1 = "opkg list-installed | egrep 'enigma2-plugin-|task-base|packagegroup-base' > /tmp/installed-list.txt"
+			cmd2 = "opkg list-changed-conffiles > /tmp/changed-configfiles.txt"
 			cmd3 = "tar -czvf " + self.fullbackupfilename + " " + self.backupdirs
 			cmd = [cmd1, cmd2, cmd3]
 			if path.exists(self.fullbackupfilename):
