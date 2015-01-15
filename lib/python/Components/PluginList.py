6efe885377e92c094b8a92251945c72438fff9d6
ims <ims21@users.sourceforge.net>
2015-01-15 09:54:47 +0100

    Pluginlist - fixed plugins icon position, class instead globals, skin parameters in comment
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Components/PluginList.py
+++ lib/python/Components/PluginList.py
@@ -8,34 +8,50 @@ from Tools.LoadPixmap import LoadPixmap
 from GUIComponent import GUIComponent
 from skin import parseFont
 
+#
+# SKIN pars:
+# ============================
+# PluginBrowser:
+# setName("y, height")
+# setDescr("y, height")
+# setPluginIcon("x,y,width,height")
+#
+# PluginDownloadBrowser:
+# setName("y, height")
+# setDescr("y,height")
+# setIcon("x,y,width,height")
+#
+# common:
+# setNameFont
+# setDescrFont
+
 def PluginEntryComponent(plugin, width=440):
-	global ny, dy, nh, dh
 	if plugin.icon is None:
 		png = LoadPixmap(resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/icons/plugin.png"))
 	else:
 		png = plugin.icon
-
+	pos = 2 * p.plicon[0] + p.plicon[2]
 	return [
 		plugin,
-		MultiContentEntryText(pos=(120, ny), size=(width-120, nh), font=0, text=plugin.name),
-		MultiContentEntryText(pos=(120, dy), size=(width-120, dh), font=1, text=plugin.description),
-		MultiContentEntryPixmapAlphaTest(pos=(10, 5), size=(100, 40), png = png)
+		MultiContentEntryText(pos=(pos, p.name[0]), size=(width - pos, p.name[1]), font=0, text=plugin.name),
+		MultiContentEntryText(pos=(pos, p.descr[0]), size=(width - pos, p.descr[1]), font=1, text=plugin.description),
+		MultiContentEntryPixmapAlphaTest(pos=(p.plicon[0], p.plicon[1]), size=(p.plicon[2], p.plicon[3]), png = png)
 	]
 
 def PluginCategoryComponent(name, png, width=440):
-	global nx, ny, nh, ix, iy, iw, ih
+	pos = 2* p.icon[0] + p.icon[2]
 	return [
 		name,
-		MultiContentEntryText(pos=(nx, ny), size=(width-nx, nh), font=0, text=name),
-		MultiContentEntryPixmapAlphaTest(pos=(ix, iy), size=(iw, ih), png = png)
+		MultiContentEntryText(pos=(pos, p.name[0]), size=(width-pos, p.name[1]), font=0, text=name),
+		MultiContentEntryPixmapAlphaTest(pos=(p.icon[0], p.icon[1]), size=(p.icon[2], p.icon[3]), png = png)
 	]
 
 def PluginDownloadComponent(plugin, name, version=None, width=440):
-	global nx, ny, nh, dx, dy, dh, ix, iy, iw, ih
 	if plugin.icon is None:
 		png = LoadPixmap(resolveFilename(SCOPE_SKIN_IMAGE, "skin_default/icons/plugin.png"))
 	else:
 		png = plugin.icon
+	pos = 2 * p.icon[0] + p.icon[2]
 	if version:
 		if "+git" in version:
 			# remove git "hash"
@@ -45,46 +61,53 @@ def PluginDownloadComponent(plugin, name, version=None, width=440):
 		name += "  (" + version + ")"
 	return [
 		plugin,
-		MultiContentEntryText(pos=(nx, ny), size=(width-nx, nh), font=0, text=name),
-		MultiContentEntryText(pos=(dx, dy), size=(width-dx, dh), font=1, text=plugin.description),
-		MultiContentEntryPixmapAlphaTest(pos=(ix, iy), size=(iw, ih), png = png)
+		MultiContentEntryText(pos=(pos, p.name[0]), size=(width-pos, p.name[1]), font=0, text=name),
+		MultiContentEntryText(pos=(pos, p.descr[0]), size=(width-pos, p.descr[1]), font=1, text=plugin.description),
+		MultiContentEntryPixmapAlphaTest(pos=(p.icon[0], p.icon[1]), size=(p.icon[2], p.icon[3]), png = png)
 	]
 
 class PluginList(MenuList):
 	def __init__(self, list, enableWrapAround=True):
-		global nx, ny, nh, dx, dy, dh, ix, iy, iw, ih
 		MenuList.__init__(self, list, enableWrapAround, eListboxPythonMultiContent)
-		GUIComponent.__init__(self)
 		self.itemNameFont = gFont("Regular", 20)
 		self.itemDescrFont = gFont("Regular", 14)
-		# n = name, d = description, i = icon, w = width, h = height
-		(nx, ny, nh, dx, dy, dh) = (80, 5, 25, 80, 26, 17 )
-		(ix, iy, iw, ih) = (10, 0, 60, 50)
 		self.l.setItemHeight(50)
 
 	def applySkin(self, desktop, parent):
-		def setItemNameFont(value):
+		def setNameFont(value):
 			self.itemNameFont = parseFont(value, ((1,1),(1,1)))
-		def setItemDescrFont(value):
+		def setDescrFont(value):
 			self.itemDescrFont = parseFont(value, ((1,1),(1,1)))
-		def setNameXYH(value):
-			global nx, ny, nh
-			(n_x, n_y, n_h) = value.split(',')
-			(nx, ny, nh) = (int(n_x),int(n_y),int(n_h))
-		def setDescrXYH(value):
-			global dx, dy, dh
-			(d_x, d_y, d_h) = value.split(',')
-			(dx, dy, dh) = (int(d_x),int(d_y),int(d_h))
-		def setIconXYWH(value):
-			global ix,iy,iw,ih
-			(i_x, i_y, i_w, i_h) = value.split(',')
-			(ix, iy, iw, ih) = (int(i_x),int(i_y),int(i_w),int(i_h))
-		for (attrib, value) in self.skinAttributes[:]:
+		def setName(value):
+			p.name = map(int, value.split(','))
+		def setDescr(value):
+			p.descr = map(int, value.split(','))
+		def setIcon(value):
+			p.icon = map(int, value.split(','))
+		def setPluginIcon(value):
+			p.plicon = map(int, value.split(','))
+		for (attrib, value) in self.skinAttributes:
 			try:
 				locals().get(attrib)(value)
-				self.skinAttributes.remove((attrib, value))
+				self.skinAttributes((attrib, value))
 			except:
 				pass
 		self.l.setFont(0, self.itemNameFont)
 		self.l.setFont(1, self.itemDescrFont)
 		return GUIComponent.applySkin(self, desktop, parent)
+
+class skinParameters():
+	def __init__(self):
+		self.name  =  5, 25
+		self.descr =  26, 17
+		self.icon  =  10, 0, 60, 50
+		self.plicon = 10, 5, 100, 40
+	def name(self):
+		return self.name
+	def descr(self):
+		return self.descr
+	def icon(self):
+		return self.icon
+	def plicon(self):
+		return self.plicon
+p = skinParameters()
