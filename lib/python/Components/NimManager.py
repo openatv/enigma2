c90602bbb194a66910946b28ad6e2e826da26aa2
rossi2000 <rossi_2000@Hotmail.com>
2015-01-16 13:23:53 +0000

    Revert "[NimManager] make sure compatible type exists before remove. Credit OpenPLi."
    This reverts commit ee8476614666a191b39a931cfc98074ab8be4f04.

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -155,10 +155,10 @@ class SecConfigure:
 		for slot in nim_slots:
 			if slot.frontend_id is not None:
 				types = [type for type in ["DVB-C", "DVB-T", "DVB-T2", "DVB-S", "DVB-S2", "ATSC"] if eDVBResourceManager.getInstance().frontendIsCompatible(slot.frontend_id, type)]
-				if "DVB-T2" in types and "DVB-T" in types:
+				if "DVB-T2" in types:
 					# DVB-T2 implies DVB-T support
 					types.remove("DVB-T")
-				if "DVB-S2" in types and "DVB-S" in types:
+				if "DVB-S2" in types:
 					# DVB-S2 implies DVB-S support
 					types.remove("DVB-S")
 				if len(types) > 1:
