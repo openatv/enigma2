507b9bf5895717408ee5cd3d504e385ca392612a
Athanasios Oikonomou <athoik@gmail.com>
2015-02-01 15:29:27 +0200

    InfoBarTimeshift: allow pause/unpause on streams
    When a stream is entered on bouquets pause/unpause doesn't work because
    pause button tries to create timeshift.
    
    Now when time shift fails and service is streamed it will try to pause/unpause
    the stream.
    
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -1712,6 +1712,20 @@ class InfoBarTimeshift:
 		ts = self.getTimeshift()
 		return ts and ts.isTimeshiftActive()
 
+	def playpauseService(self):
+		service = self.session.nav.getCurrentService()
+		if service and service.streamed():
+			pauseable = service.pause()
+			if pauseable:
+				if self.seekstate == self.SEEK_STATE_PLAY:
+					pauseable.pause()
+					self.seekstate = self.SEEK_STATE_PAUSE
+				else:
+					pauseable.unpause()
+					self.seekstate = self.SEEK_STATE_PLAY
+				return
+		return 0
+
 	def startTimeshift(self, pauseService = True):
 		print "enable timeshift"
 		ts = self.getTimeshift()
@@ -1719,7 +1733,7 @@ class InfoBarTimeshift:
 			if not pauseService and not int(config.usage.timeshift_start_delay.value):
 				self.session.open(MessageBox, _("Timeshift not possible!"), MessageBox.TYPE_ERROR, simple = True)
 			print "no ts interface"
-			return 0
+			return self.playpauseService()
 
 		if ts.isTimeshiftEnabled():
 			print "hu, timeshift already enabled?"
