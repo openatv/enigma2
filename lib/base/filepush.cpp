c711fa07a00bf8228538a279b614177ff762ddee
arn354 <arn354@email.de>
2015-02-04 12:44:11 +0100

    Revert "filepush - reduce high cpu load on playback mediafiles and ts files"
    This reverts commit 5711b85a74ff6e5f3d028664a9a20a0a8e794c4b.

--- lib/base/filepush.cpp
+++ lib/base/filepush.cpp
@@ -194,10 +194,7 @@ void eFilePushThread::thread()
 						break;
 					}
 					if (w < 0 && (errno == EINTR || errno == EAGAIN || errno == EBUSY))
-					{
-						sleep(2);
 						continue;
-					}
 					eDebug("eFilePushThread WRITE ERROR");
 					sendEvent(evtWriteError);
 					break;
