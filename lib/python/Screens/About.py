0fa1f16ee0179e9b97279f08afcec5060af35d07
ims <ims21@users.sourceforge.net>
2015-02-07 11:19:15 +0100

    About - do not display GStreamer text in version

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -64,7 +64,7 @@ class About(Screen):
 		AboutText += _("Drivers:\t%s\n") % driversdate
 		
 		AboutText += _("Kernel:\t%s\n") % about.getKernelVersionString()
-		AboutText += _("GStreamer:\t%s\n") % about.getGStreamerVersionString()
+		AboutText += _("GStreamer:\t%s\n") % about.getGStreamerVersionString().replace("GStreamer","")
 		AboutText += _("Python:\t ") + about.getPythonVersionString() + "\n"
 
 		AboutText += _("Installed:\t ") + about.getFlashDateString() + "\n"
