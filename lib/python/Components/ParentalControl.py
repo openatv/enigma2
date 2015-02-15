9e009d7fa29646dbf4a342e6f77e662cc0584f59
Huevos <rowland@moralet.com>
2015-02-15 01:40:56 +0100

    [ParentalControl] missing config. Without this OpenWebIF will not zap.

--- lib/python/Components/ParentalControl.py
+++ lib/python/Components/ParentalControl.py
@@ -21,7 +21,7 @@ def InitParentalControl():
 	config.ParentalControl = ConfigSubsection()
 	config.ParentalControl.storeservicepin = ConfigSelection(default = "never", choices = [("never", _("never")), ("5", _("%d minutes") % 5), ("30", _("%d minutes") % 30), ("60", _("%d minutes") % 60), ("standby", _("until standby/restart"))])
 	config.ParentalControl.servicepinactive = ConfigYesNo(default = False)
-
+	config.ParentalControl.configured = ConfigYesNo(default = True) # without this OpenWebIF is broken
 	config.ParentalControl.retries = ConfigSubsection()
 	config.ParentalControl.retries.servicepin = ConfigSubsection()
 	config.ParentalControl.retries.servicepin.tries = ConfigInteger(default = 3)
