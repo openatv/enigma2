330b9cab751eebffb0756287c31659614637edfc
Mike Looijmans <milo-software@users.sourceforge.net>
2015-02-16 11:02:23 +0100

    mytest.py: Remove error masking code
    The guideline: Don't catch exceptions when you cannot resolve the problem.
    
    Just pass exceptions on, and the top level handler will do the gsod. Remove the
    try..except blocks from the dialog initialization code, so that we get a proper
    trace instead of a halfway interrupted one.
    
    This also gets rid of the weird "dlg.isTmp = True" crash that usually follows
    any exception, with a backtrace divided into two blocks.
    
    And it reduces the code size.

--- mytest.py
+++ mytest.py
@@ -224,16 +224,6 @@ class Session:
 			self.current_dialog.removeSummary(self.summary)
 			self.popSummary()
 
-	def create(self, screen, arguments, **kwargs):
-		# creates an instance of 'screen' (which is a class)
-		try:
-			return screen(self, *arguments, **kwargs)
-		except:
-			errstr = "Screen %s(%s, %s): %s" % (str(screen), str(arguments), str(kwargs), exc_info()[0])
-			print errstr
-			print_exc(file=stdout)
-			enigma.quitMainloop(5)
-
 	def instantiateDialog(self, screen, *arguments, **kwargs):
 		return self.doInstantiateDialog(screen, arguments, kwargs, self.desktop)
 
@@ -251,28 +241,14 @@ class Session:
 
 	def doInstantiateDialog(self, screen, arguments, kwargs, desktop):
 		# create dialog
-
-		try:
-			dlg = self.create(screen, arguments, **kwargs)
-		except:
-			print 'EXCEPTION IN DIALOG INIT CODE, ABORTING:'
-			print '-'*60
-			print_exc(file=stdout)
-			enigma.quitMainloop(5)
-			print '-'*60
-
+		dlg = screen(self, *arguments, **kwargs)
 		if dlg is None:
 			return
-
 		# read skin data
 		readSkin(dlg, None, dlg.skinName, desktop)
-
 		# create GUI view of this dialog
-		assert desktop is not None
-
 		dlg.setDesktop(desktop)
 		dlg.applySkin()
-
 		return dlg
 
 	def pushCurrent(self):
