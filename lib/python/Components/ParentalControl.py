e021b3a9e18f769a4241b70d12999e42dc41e18a
littlesat <littlesat99@yahoo.com>
2015-02-17 11:41:30 +0100

    ParentalControl: Add more config clones (settuppin, settuppinactive)
    There are still some 3rd party plugins which we cannot fix that depend on
    it and these plugins do have users.

--- lib/python/Components/ParentalControl.py
+++ lib/python/Components/ParentalControl.py
@@ -21,10 +21,6 @@ def InitParentalControl():
 	config.ParentalControl = ConfigSubsection()
 	config.ParentalControl.storeservicepin = ConfigSelection(default = "never", choices = [("never", _("never")), ("5", _("%d minutes") % 5), ("30", _("%d minutes") % 30), ("60", _("%d minutes") % 60), ("standby", _("until standby/restart"))])
 	config.ParentalControl.servicepinactive = ConfigYesNo(default = False)
-
-	#Added for backwards compatibility with some 3rd party plygins that depend on this config
-	config.ParentalControl.configured = config.ParentalControl.servicepinactive
-
 	config.ParentalControl.retries = ConfigSubsection()
 	config.ParentalControl.retries.servicepin = ConfigSubsection()
 	config.ParentalControl.retries.servicepin.tries = ConfigInteger(default = 3)
@@ -32,6 +28,10 @@ def InitParentalControl():
 	config.ParentalControl.servicepin = ConfigSubList()
 	config.ParentalControl.servicepin.append(ConfigPIN(default = -1))
 	config.ParentalControl.age = ConfigSelection(default = "18", choices = [("0", _("No age block"))] + list((str(x), "%d+" % x) for x in range(3,19)))
+
+	#Added for backwards compatibility with some 3rd party plygins that depend on this config
+	config.ParentalControl.configured = config.ParentalControl.setuppinactive  = config.ParentalControl.servicepinactive
+	config.ParentalControl.setuppin = config.ParentalControl.servicepin[0]
 
 class ParentalControl:
 	def __init__(self):
