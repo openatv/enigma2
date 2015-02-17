3900d275baacbc2359721c5720089408dd391bf6
littlesat <littlesat99@yahoo.com>
2015-02-17 10:11:16 +0100

    ParentalControl: Add configured config as clone from servicepinactive
    As some plugins seems to need it and there are also some 3rd party plugins
    which we cannot adapt.

--- lib/python/Components/ParentalControl.py
+++ lib/python/Components/ParentalControl.py
@@ -22,6 +22,9 @@ def InitParentalControl():
 	config.ParentalControl.storeservicepin = ConfigSelection(default = "never", choices = [("never", _("never")), ("5", _("%d minutes") % 5), ("30", _("%d minutes") % 30), ("60", _("%d minutes") % 60), ("standby", _("until standby/restart"))])
 	config.ParentalControl.servicepinactive = ConfigYesNo(default = False)
 
+	#Added for backwards compatibility with some 3rd party plygins that depend on this config
+	config.ParentalControl.configured = config.ParentalControl.servicepinactive
+
 	config.ParentalControl.retries = ConfigSubsection()
 	config.ParentalControl.retries.servicepin = ConfigSubsection()
 	config.ParentalControl.retries.servicepin.tries = ConfigInteger(default = 3)
