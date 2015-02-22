565af23e498bade6640b5dbb4826bbf20681d352
ims <ims21@users.sourceforge.net>
2015-02-22 14:59:35 +0100

    Added TransponderInfo parameter for Servicename in skin. It can be limite for using for All services only with optionaly InRootOnly parameter: <convert type="ServiceName">TransponderInfo;InRootOnly</convert>

--- lib/python/Components/Converter/ServiceName.py
+++ lib/python/Components/Converter/ServiceName.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from Components.Converter.Converter import Converter
 from Components.config import config
-from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr, eServiceReference, eEPGCache
+from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr, eServiceReference, eEPGCache, eServiceCenter
 from Components.Element import cached
-from ServiceReference import resolveAlternate
+from ServiceReference import resolveAlternate, ServiceReference
 from Tools.Directories import fileExists
+from Tools.Transponder import ConvertToHumanReadable
+from Components.NimManager import nimmanager
+from Components.Converter.ChannelNumbers import channelnumbers
+import Screens.InfoBar
 
 class ServiceName(Converter, object):
 	NAME = 0
@@ -13,10 +17,14 @@ class ServiceName(Converter, object):
 	PROVIDER = 3
 	REFERENCE = 4
 	EDITREFERENCE = 5
+	TRANSPONDER = 6
 
 	def __init__(self, type):
 		Converter.__init__(self, type)
 		self.epgQuery = eEPGCache.getInstance().lookupEventTime
+		self.mode = ""
+		if ';' in type:
+			type, self.mode = type.split(';')
 		if type == "Provider":
 			self.type = self.PROVIDER
 		elif type == "Reference":
@@ -27,6 +35,8 @@ class ServiceName(Converter, object):
 			self.type = self.NAME_ONLY
 		elif type == "NameAndEvent":
 			self.type = self.NAME_EVENT
+		elif type == "TransponderInfo":
+			self.type = self.TRANSPONDER
 		else:
 			self.type = self.NAME
 
@@ -79,11 +89,66 @@ class ServiceName(Converter, object):
 				return refstr
 			nref = resolveAlternate(service)
 			if nref:
-				service = nref
-			return service.toString()
+				ref = nref
+			return ref.toString()
+		elif self.type == self.TRANSPONDER:
+			if ref:
+				nref = resolveAlternate(ref)
+				if nref:
+					ref = nref
+					info = eServiceCenter.getInstance().info(ref)
+				transponder_info = info.getInfoObject(ref, iServiceInformation.sTransponderData)
+			else:
+				transponder_info = info.getInfoObject(iServiceInformation.sTransponderData)
+			if "InRootOnly" in self.mode and not self.rootBouquet():
+				return ""
+			if transponder_info:
+				self.t_info = ConvertToHumanReadable(transponder_info)
+				if "DVB-T" in self.system():
+					return	self.dvb_t()
+				elif "DVB-C" in self.system():
+					return self.dvb_c()
+				return 	self.dvb_s()
+			return ""
 
 	text = property(getText)
 
 	def changed(self, what):
 		if what[0] != self.CHANGED_SPECIFIC or what[1] in (iPlayableService.evStart,):
 			Converter.changed(self, what)
+
+	def dvb_s(self):
+		return "%s %d %s %d %s %s" % (self.system(), self.freq()/1000, self.polar(), self.s_rate()/1000, self.fec(), self.orb_pos())
+	def dvb_t(self):
+		return "%s %s %d/%s" % (self.system(), self.ch_number(), self.freq()/1000000 + 0.5 , self.bandwidth())
+	def dvb_c(self):
+		return "%s %d %s %s" % (self.system(), self.freq()/1000, _("kHz"), self.bandwidth())
+	def system(self):
+		return self.t_info["system"]
+	def freq(self):
+		return self.t_info["frequency"]
+	def bandwidth(self):
+		return self.t_info["bandwidth"]
+	def s_rate(self):
+		return self.t_info["symbol_rate"]
+	def polar(self):
+		return self.t_info["polarization"][0]
+	def orb_pos(self):
+		op = self.t_info["orbital_position"].split(' ')[0]
+		return "%s°%s" % (op[:-1],op[-1:])
+	def fec(self):
+		return self.t_info["fec_inner"]
+	def ch_number(self):
+		for n in nimmanager.nim_slots:
+			if n.isCompatible("DVB-T"):
+				channel = channelnumbers.getChannelNumber(self.freq(), n.slot)
+				if channel:
+					return _("CH") + "%s" % channel
+		return ""
+
+	def rootBouquet(self):
+		servicelist = Screens.InfoBar.InfoBar.instance.servicelist
+		epg_bouquet = servicelist and servicelist.getRoot()
+		if ServiceReference(epg_bouquet).getServiceName():
+			return False
+		return True
