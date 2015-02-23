dd8df09c2ba74967843e28d6807eeba3f5d012cf
littlesat <littlesat99@yahoo.com>
2015-02-23 08:02:33 +0100

    Only show config hide parental locked service when pin is cached

--- lib/python/Screens/ParentalControlSetup.py
+++ lib/python/Screens/ParentalControlSetup.py
@@ -71,8 +71,9 @@ class ParentalControlSetup(Screen, ConfigListScreen, ProtectedScreen):
 			self.changePin = getConfigListEntry(_("Change service PIN"), NoSave(ConfigNothing()))
 			self.list.append(self.changePin)
 			self.list.append(getConfigListEntry(_("Remember service PIN"), config.ParentalControl.storeservicepin))
+			if config.ParentalControl.storeservicepin.value != "never":
+				self.list.append(getConfigListEntry(_("Hide parentel locked services"), config.ParentalControl.hideBlacklist))
 			self.list.append(getConfigListEntry(_("Protect on epg age"), config.ParentalControl.age))
-			self.list.append(getConfigListEntry(_("Hide parentel locked services"), config.ParentalControl.hideBlacklist))
 			self.reloadLists = getConfigListEntry(_("Reload blacklists"), NoSave(ConfigNothing()))
 			self.list.append(self.reloadLists)
 
