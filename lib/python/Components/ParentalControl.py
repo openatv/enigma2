8d3bff7e630deeaec123f5e317491ab58d124c58
littlesat <littlesat99@yahoo.com>
2015-02-23 07:44:14 +0100

    Only hide parentel locked services when required
    e.g. parental control should be enabled and the pin cache setting should
    not be set to "never"

--- lib/python/Components/ParentalControl.py
+++ lib/python/Components/ParentalControl.py
@@ -240,6 +240,8 @@ class ParentalControl:
 			self.hideBlacklist(config.ParentalControl.hideBlacklist)
 		else:
 			config.ParentalControl.hideBlacklist.addNotifier(self.hideBlacklist)
+			config.ParentalControl.servicepinactive.addNotifier(self.hideBlacklist, initial_call=False)
+			config.ParentalControl.storeservicepin.addNotifier(self.hideBlacklist, initial_call=False)
 		self.filesOpened = True
 
 	def __getattr__(self, name):
@@ -253,7 +255,7 @@ class ParentalControl:
 
 	def hideBlacklist(self, configEntry):
 		if self.blacklist:
-			if configEntry.value and not self.sessionPinCached:
+			if config.ParentalControl.servicepinactive.value and config.ParentalControl.storeservicepin.value != "never" and configEntry.value and not self.sessionPinCached:
 				for ref in self.blacklist:
 					if TYPE_BOUQUET not in ref:
 						eDVBDB.getInstance().addFlag(eServiceReference(ref), 2)
--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -163,7 +163,7 @@ class ChannelContextMenu(Screen):
 							append_when_current_valid(current, menu, (_("add to parental protection"), boundFunction(self.addParentalProtection, csel.getCurrentSelection())), level=0)
 						else:
 							append_when_current_valid(current, menu, (_("remove from parental protection"), boundFunction(self.removeParentalProtection, csel.getCurrentSelection())), level=0)
-						if config.ParentalControl.hideBlacklist.value and not parentalControl.sessionPinCached:
+						if config.ParentalControl.hideBlacklist.value and not parentalControl.sessionPinCached and config.ParentalControl.storeservicepin.value != "never":
 							append_when_current_valid(current, menu, (_("Unhide parental control services"), boundFunction(self.unhideParentalServices)), level=0)
 					if haveBouquets:
 						bouquets = self.csel.getBouquetList()
