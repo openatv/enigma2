cd54c5efe0b8c7675314920185d65f9752ad30b3
Peter Urbanec <bitbucket.org@urbanec.net>
2015-03-01 03:55:34 +1100

    Clean up date and time formatting.

--- lib/python/Tools/FuzzyDate.py
+++ lib/python/Tools/FuzzyDate.py
@@ -1,10 +1,9 @@
-from time import localtime, time
+from time import localtime, time, strftime
 
-def FuzzyTime(t, inPast = False):
+def FuzzyTime(t, inPast=False):
 	d = localtime(t)
 	nt = time()
-	n = localtime()
-	dayOfWeek = (_("Mon"), _("Tue"), _("Wed"), _("Thu"), _("Fri"), _("Sat"), _("Sun"))
+	n = localtime(nt)
 
 	if d[:3] == n[:3]:
 		# same day
@@ -12,27 +11,25 @@ def FuzzyTime(t, inPast = False):
 	elif d[0] == n[0] and d[7] == n[7] - 1 and inPast:
 		# won't work on New Year's day
 		date = _("Yesterday")
-	elif ((t - nt) < 7*86400) and (nt < t) and not inPast:
+	elif ((t - nt) < 7 * 86400) and (nt < t) and not inPast:
 		# same week (must be future)
-		date = dayOfWeek[d[6]]
+		date = strftime("%a", d)
 	elif d[0] == n[0]:
 		# same year
-		if inPast:
-			# I want the day in the movielist
-			date = "%s %02d.%02d." % (dayOfWeek[d[6]], d[2], d[1])
-		else:
-			date = "%02d.%02d." % (d[2], d[1])
+		date = strftime("%a %d %b", d)
 	else:
-		date = "%02d.%02d.%d" % (d[2], d[1], d[0])
+		date = strftime("%d %b %Y", d)
 
-	timeres = "%02d:%02d" % (d[3], d[4])
+	timeres = strftime("%R", d)
 
 	return date, timeres
 
 if __name__ == "__main__":
-	def _(x): return x
-	print "now: %s %s" % FuzzyTime(time())
-	for i in range(1, 14):
-		print "+%2s day(s):  %s " % (i, FuzzyTime(time() + 86400 * i))
-	for i in range(1, 14):
-		print "-%2s day(s):  %s " % (i, FuzzyTime(time() - 86400 * i, True))
+	def _(x):
+		return x
+
+	for i in [365] + range(14, 0, -1):
+		print "%+4.0d day(s): %s" % (i, FuzzyTime(time() + 86400 * i))
+	print "        now: %s" % str(FuzzyTime(time()))
+	for i in range(1, 14) + [365]:
+		print "%+4.0d day(s): %s" % (-i, FuzzyTime(time() - 86400 * i, True))
