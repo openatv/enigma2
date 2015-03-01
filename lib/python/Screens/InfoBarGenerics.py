c9910680be38bb7491e7bf02d6a0dbe0e4e92f6a
ims <ims21@users.sourceforge.net>
2015-03-01 02:27:51 +0100

    changes in service list screen are displayed without restart GUI (language changes, skin parameters changes)
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -714,12 +714,12 @@ class InfoBarChannelSelection:
 	def switchChannelUp(self):
 		if "keep" not in config.usage.servicelist_cursor_behavior.value:
 			self.servicelist.moveUp()
-		self.session.execDialog(self.servicelist)
+		self.openServiceList()
 
 	def switchChannelDown(self):
 		if "keep" not in config.usage.servicelist_cursor_behavior.value:
 			self.servicelist.moveDown()
-		self.session.execDialog(self.servicelist)
+		self.openServiceList()
 
 	def zapUp(self):
 		if self.servicelist.inBouquet():
@@ -770,6 +770,7 @@ class InfoBarChannelSelection:
 		self.openServiceList()
 
 	def openServiceList(self):
+		self.servicelist = self.session.instantiateDialog(ChannelSelection)
 		self.session.execDialog(self.servicelist)
 
 class InfoBarMenu:
