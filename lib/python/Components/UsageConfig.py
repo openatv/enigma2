eb04be700bd7f3fd7c073d8a9ffb5e6d69f78cee
madie <madie1@gmx.de>
2015-03-04 21:25:43 +0100

    add option for blink clock on display

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -296,7 +296,7 @@ def InitUsageConfig():
 	config.usage.show_event_progress_in_servicelist.addNotifier(refreshServiceList)
 	config.usage.show_channel_numbers_in_servicelist.addNotifier(refreshServiceList)
 
-	config.usage.blinking_display_clock_during_recording = ConfigYesNo(default = True)
+	config.usage.blinking_display_clock_during_recording = ConfigYesNo(default = False)
 
 	config.usage.blinking_rec_symbol_during_recording = ConfigYesNo(default = True)
 
