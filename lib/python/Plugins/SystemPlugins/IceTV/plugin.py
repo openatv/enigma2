307ca1f002a5d322cee38863dde66e0b2771837b
Peter Urbanec <bitbucket.org@urbanec.net>
2015-03-14 00:20:30 +1100

    [IceTV] Catch exceptions on fetch and log them.

--- lib/python/Plugins/SystemPlugins/IceTV/plugin.py
+++ lib/python/Plugins/SystemPlugins/IceTV/plugin.py
@@ -562,10 +562,13 @@ class IceTVMain(ChoiceBox):
         _session.open(IceTVUserTypeScreen)
 
     def fetch(self, res=None):
-        if fetcher.doWork():
-            _session.open(MessageBox, _("IceTV update completed OK"), type=MessageBox.TYPE_INFO, timeout=5)
-        else:
-            _session.open(MessageBox, _("IceTV update completed with errors.\n\nPlease check the log for details."), type=MessageBox.TYPE_ERROR, timeout=15)
+        try:
+            if fetcher.doWork():
+                _session.open(MessageBox, _("IceTV update completed OK"), type=MessageBox.TYPE_INFO, timeout=5)
+                return
+        except (Exception) as ex:
+            self.addLog("Error trying to fetch: %s" % str(ex))
+        _session.open(MessageBox, _("IceTV update completed with errors.\n\nPlease check the log for details."), type=MessageBox.TYPE_ERROR, timeout=15)
 
     def login(self, res=None):
         _session.open(IceTVNeedPassword)
