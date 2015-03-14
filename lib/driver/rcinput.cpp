2e37de2274dbcd0882f6887da389df41bc38b1da
Captain <captain.onboard@web.de>
2015-03-14 12:31:22 +0100

    cleanup old rc key hacks

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -427,38 +427,6 @@ void eRCDeviceInputDev::handleCode(long rccode)
 	}
 #endif
 
-#if KEY_INFO_TO_KEY_EPG
- /* INFO to EPG Toggle , which sends KEY_INFO events. Correct this, so we do not have to place hacks in the keymaps. */
- if (ev->code == KEY_INFO)
- {
-  if (ev->value == 0)
-  {
-  ev->code = KEY_EPG;
-  }
-  else
-  {
-  ev->code = KEY_INFO;
-  ev->value == 0;
-  }
- }
-#endif
-
-#if KEY_EPG_TO_KEY_INFO
- /* EPG to INFO Toggle , which sends KEY_EPG events. Correct this, so we do not have to place hacks in the keymaps. */
- if (ev->code == KEY_EPG)
- {
-  if (ev->value == 0)
-  {
-  ev->code = KEY_EPG;
-  }
-  else
-  {
-  ev->code = KEY_INFO;
-  ev->value == 0;
-  }
- }
-#endif
-
 	switch (ev->value)
 	{
 		case 0:
