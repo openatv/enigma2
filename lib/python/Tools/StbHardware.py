2b2e2fb051f390cac16d27d82d5e796e075daff8
daTa <data-devil@arcor.de>
2015-03-29 13:52:35 +0200

    [StbHardware] reverse this commit-->https://github.com/openatv/enigma2/commit/6b998674e10258d4581d9cc77d58cae4678630b7

--- lib/python/Tools/StbHardware.py
+++ lib/python/Tools/StbHardware.py
@@ -37,16 +37,10 @@ def setFPWakeuptime(wutime):
 
 def setRTCoffset():
 	import time
-	if getBrandOEM() in ('fulan'):
-		if time.localtime().tm_isdst == 0:
-			forsleep = 0-time.timezone
-		else:
-			forsleep = 3600+time.timezone
+	if time.localtime().tm_isdst == 0:
+		forsleep = 7200+time.timezone
 	else:
-		if time.localtime().tm_isdst == 0:
-			forsleep = 7200+time.timezone
-		else:
-			forsleep = 3600-time.timezone
+		forsleep = 3600-time.timezone
 
 	t_local = time.localtime(int(time.time()))
 
