fd7fce7077e729dd43225095408388b56f96e5dd
dima73 <dima73@dev.null>
2015-03-30 10:31:15 +0200

    Solve forgotton self.infoBarInstance in Standby.py
    Issue induced in http://sourceforge.net/p/openpli/enigma2/ci/46dfd473d77a8110e0a6d167684b20543cf55f97/
    
    Signed-off-by: littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -82,7 +82,7 @@ class Standby(Screen):
 					self.timeHandler.m_timeUpdated.get().append(self.stopService)
 
 		if self.session.pipshown:
-			infoBarInstance and hasattr(infoBarInstance, "showPiP") and infoBarInstance.showPiP()
+			self.infoBarInstance and hasattr(self.infoBarInstance, "showPiP") and self.infoBarInstance.showPiP()
 
 		#set input to vcr scart
 		if SystemInfo["ScartSwitch"]:
