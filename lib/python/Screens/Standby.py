854f4c85a33b4cefcd9ec5829ecc02857d28e37f
littlesat <littlesat99@yahoo.com>
2015-04-09 22:45:39 +0200

    Simplify parental locked movie list and playing movie when coming from standby
    And also handle it correctly when (re)starting box/enigma2

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -80,14 +80,6 @@ class Standby(Screen):
 				else:
 					self.timeHandler.m_timeUpdated.get().append(self.stopService)
 
-		movie = config.usage.last_movie_played.value
-		movie = movie and movie.rsplit(":", 1)[1]
-		if movie.startswith("/") and [x for x in movie[1:].split("/") if x.startswith(".") and not x.startswith(".Trash")]:
-			from Screens.MovieSelection import defaultMoviePath
-			moviepath = defaultMoviePath()
-			if moviepath:
-				config.movielist.last_videodir.value = moviepath
-
 		if self.session.pipshown:
 			self.infoBarInstance and hasattr(self.infoBarInstance, "showPiP") and self.infoBarInstance.showPiP()
 
