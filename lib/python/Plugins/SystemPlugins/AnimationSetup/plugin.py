362e4b9854407d789cea644178c2ab70ae20413a
Captain <captain.onboard@web.de>
2015-04-14 08:47:46 +0200

    [vu] animation change to fade

--- lib/python/Plugins/SystemPlugins/AnimationSetup/plugin.py
+++ lib/python/Plugins/SystemPlugins/AnimationSetup/plugin.py
@@ -9,9 +9,9 @@ from Plugins.Plugin import PluginDescriptor
 
 from enigma import setAnimation_current, setAnimation_speed
 
-# default = slide to left
+# default = simple fade
 g_default = {
-        "current": 6,
+        "current": 1,
         "speed"  : 20,
 }
 g_max_speed = 30
