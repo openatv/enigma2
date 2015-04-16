cb18d2f9f99718d91417970c7939f1556fd1b4c0
Captain <captain.onboard@web.de>
2015-04-16 23:48:44 +0200

    add jess mode thx adenin

--- lib/python/Screens/Satconfig.py
+++ lib/python/Screens/Satconfig.py
@@ -360,7 +360,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 				self.list.append(getConfigListEntry(_("Increased voltage"), currLnb.increased_voltage))
 				self.list.append(getConfigListEntry(_("Tone mode"), Sat.tonemode))
 
-			if lnbnum < 65:
+			if lnbnum < 65 and not currLnb.unicable.value == "unicable_matrix" and not currLnb.unicable.value == "unicable_lnb":
 				self.advancedDiseqcMode = getConfigListEntry(_("DiSEqC mode"), currLnb.diseqcMode)
 				self.list.append(self.advancedDiseqcMode)
 			if currLnb.diseqcMode.value != "none":
