cc86550067fa734cab0a7203bc0e8db060fd1856
arn354 <arn354@email.de>
2015-04-17 20:37:01 +0200

    NimManager - disable print

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -1674,7 +1674,7 @@ def InitNimManager(nimmgr):
 			nim.multiType.fe_id = x - empty_slots
 			nim.multiType.addNotifier(boundFunction(tunerTypeChanged, nimmgr))
 
-		print"[NimManager] slotname = %s, slotdescription = %s, multitype = %s" % (slot.input_name, slot.description,(slot.isMultiType() and addMultiType))
+		# print"[NimManager] slotname = %s, slotdescription = %s, multitype = %s" % (slot.input_name, slot.description,(slot.isMultiType() and addMultiType))
 
 	empty_slots = 0
 	for slot in nimmgr.nim_slots:
