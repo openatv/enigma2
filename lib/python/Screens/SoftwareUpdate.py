3aa57edc926c6a0a7360830542beca1a24683c33
Andy Blackburn <andrew.blackburn644@btinternet.com>
2015-05-02 15:59:32 +0100

    [Softwareupdate] fix posible BSoD.

--- lib/python/Screens/SoftwareUpdate.py
+++ lib/python/Screens/SoftwareUpdate.py
@@ -174,7 +174,7 @@ class UpdatePlugin(Screen, ProtectedScreen):
 	def checkNetworkState(self):
 		self.trafficLight = feedsstatuscheck.getFeedsBool()
 		if (config.softwareupdate.updateisunstable.value == '1' and config.softwareupdate.updatebeta.value) or config.softwareupdate.updateisunstable.value == '0':
-			status_msgs = {'stable': _('Feeds status:   Stable'), 'unstable': _('Feeds status:   Unstable'), 'updating': _('Feeds status:   Updating'), '-2': _('ERROR:   No network found'), '404': _('ERROR:   No internet found'), 'unknown': _('No connection')}
+			status_msgs = {'stable': _('Feeds status:   Stable'), 'unstable': _('Feeds status:   Unstable'), 'updating': _('Feeds status:   Updating'), '-2': _('ERROR:   No network found'), '404': _('ERROR:   No internet found'), 'inprogress': _('ERROR: Check is already running in background'), 'unknown': _('No connection')}
 			self['tl_red'].hide()
 			self['tl_yellow'].hide()
 			self['tl_green'].hide()
