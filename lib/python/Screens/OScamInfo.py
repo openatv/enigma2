09ed7fba355e67ae6843dd288e255868afadf33c
rossi2000 <rossi_2000@hotmail.com>
2015-05-06 19:23:09 +0100

    Revert "OScamInfo: adjust binary detection"
    This reverts commit 6a0ae2e2e8c2ea7c5aae077f5983f1c46dabe7f2.

--- lib/python/Screens/OScamInfo.py
+++ lib/python/Screens/OScamInfo.py
@@ -48,12 +48,12 @@ class OscamInfo:
 		#search_dirs = [ "/usr", "/var", "/etc" ]
 		#sdirs = " ".join(search_dirs)
 		#cmd = 'find %s -name "oscam.conf"' % sdirs
-		cmd = 'ps -eo command | sort -u | grep -v "grep" | grep -c "OScam"'
+		cmd = 'ps -eo command | sort -u | grep -v "grep" | grep -c "oscam"'
 		res = os.popen(cmd).read()
 		if res:
 			data = res.replace("\n", "")
 			if int(data) == 1:
-				cmd = 'ps -eo command | sort -u | grep -v "grep" | grep "OScam"'
+				cmd = 'ps -eo command | sort -u | grep -v "grep" | grep "oscam"'
 				res = os.popen(cmd).read()
 				if res:
 					data = res.replace("\n", "")
