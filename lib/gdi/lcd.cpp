3bd4f93ddc185b3f849eac332650c1d21daa6092
Captain <captain.onboard@web.de>
2015-05-07 18:46:27 +0200

    [7100] fix BGR again

--- lib/gdi/lcd.cpp
+++ lib/gdi/lcd.cpp
@@ -348,7 +348,15 @@ void eDBoxLCD::update()
 			}
 			else
 			{
-				if (FILE * file = fopen("/proc/stb/info/gbmodel", "r"))
+				FILE *file;
+				FILE *boxtype_file;
+				char boxtype_name[20];
+				if((boxtype_file = fopen("/proc/stb/info/boxtype", "r")) != NULL)
+				{
+					fgets(boxtype_name, sizeof(boxtype_name), boxtype_file);
+					fclose(boxtype_file);
+				}
+				if (((file = fopen("/proc/stb/info/gbmodel", "r")) != NULL ) || (strcmp(boxtype_name, "7100\n") == 0) || (strcmp(boxtype_name, "Ceryon\n") == 0))
 				{
 					unsigned char gb_buffer[_stride * res.height()];
 					for (int offset = 0; offset < _stride * res.height(); offset += 2)
@@ -357,7 +365,10 @@ void eDBoxLCD::update()
 						gb_buffer[offset + 1] = ((_buffer[offset + 1] >> 5) & 0x03) | ((_buffer[offset] >> 3) & 0x1C) | ((_buffer[offset + 1] << 5) & 0x60);
 					}
 					write(lcdfd, gb_buffer, _stride * res.height());
-					fclose(file);
+					if (file != NULL)
+					{
+						fclose(file);
+					}
 				}
 				else
 				{
