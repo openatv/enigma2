6b772b0351c4b73d6ce0780924eec0dea1a5f758
Fischreiher <lukas.bauer5@web.de>
2015-05-17 13:00:24 +0200

    [MovieInfo.py] Fixed rounding, display size in GB from 100GB+ (for the upcoming fix for showing the size of directories)

--- lib/python/Components/Converter/MovieInfo.py
+++ lib/python/Components/Converter/MovieInfo.py
@@ -53,7 +53,10 @@ class MovieInfo(Converter, object):
 					return _("Directory")
 				filesize = info.getInfoObject(service, iServiceInformation.sFileSize)
 				if filesize is not None:
-					return _("%d MB") % (filesize / (1024*1024))
+					if filesize >= 100000*1024*1024:
+						return _("%.0f GB") % (filesize / (1024.0*1024.0*1024.0))
+					else:
+						return _("%.0f MB") % (filesize / (1024.0*1024.0))
 		return ""
 
 	text = property(getText)
