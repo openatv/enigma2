49e2ae6c9a26e84c90d5b05c1c862bcb5cd6bd11
mike-99 <mike99@gmx.net>
2015-05-20 19:01:47 +0200

    [ChoiceBox] add "header" for Title translation

--- lib/python/Screens/ChoiceBox.py
+++ lib/python/Screens/ChoiceBox.py
@@ -14,6 +14,7 @@ config.misc.pluginlist.extension_order = ConfigText(default="")
 
 class ChoiceBox(Screen):
 	def __init__(self, session, title="", list=None, keys=None, selection=0, skin_name=None, text="", reorderConfig="", var=""):
+		self.setTitle(_("Choice Box"))
 		if not list: list = []
 		if not skin_name: skin_name = []
 		Screen.__init__(self, session)
