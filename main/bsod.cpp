b6a7bde3219dcdc4fa4124d1e9fafb4f71df3a75
arn354 <arn354@email.de>
2015-06-06 14:44:36 +0200

    Add backtrace to crashlog when c++ part crashes Use e.g. online demangler or c++filt to get better readable backtrace and addr2line for source code filename and line numbers (non stipped binary is needed for this). thx betacentauri - https://github.com/XTAv2/Enigma2/commit/7ad1a1560098ee59295941d5d0bcaab4452b7e79

--- main/bsod.cpp
+++ main/bsod.cpp
@@ -1,6 +1,8 @@
 #include <csignal>
 #include <fstream>
 #include <sstream>
+#include <execinfo.h>
+#include <dlfcn.h>
 #include <lib/base/eenv.h>
 #include <lib/base/eerror.h>
 #include <lib/base/nconfig.h>
@@ -352,13 +354,39 @@ void oops(const mcontext_t &context)
 }
 #endif
 
+/* Use own backtrace print procedure because backtrace_symbols_fd
+ * only writes to files. backtrace_symbols cannot be used because
+ * it's not async-signal-safe and so must not be used in signal
+ * handlers.
+ */
+void print_backtrace()
+{
+	void *array[15];
+	size_t size;
+	int cnt;
+
+	size = backtrace(array, 15);
+	eDebug("Backtrace:");
+	for (cnt = 1; cnt < size; ++cnt)
+	{
+		Dl_info info;
+
+		if (dladdr(array[cnt], &info)
+			&& info.dli_fname != NULL && info.dli_fname[0] != '\0')
+		{
+			eDebug("%s(%s) [0x%X]", info.dli_fname, info.dli_sname != NULL ? info.dli_sname : "n/a", (unsigned long int) array[cnt]);
+		}
+	}
+}
+
 void handleFatalSignal(int signum, siginfo_t *si, void *ctx)
 {
 #ifndef NO_OOPS_SUPPORT
 	ucontext_t *uc = (ucontext_t*)ctx;
 	oops(uc->uc_mcontext);
 #endif
-	eDebug("-------");
+	print_backtrace();
+	eDebug("-------FATAL SIGNAL");
 	bsodFatal("enigma2, signal");
 }
 
