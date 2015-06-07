920ca01fa1fcb1b14baec9b26f3d3dbb80ec4058
arn354 <arn354@email.de>
2015-06-07 15:05:08 +0200

    SwapManager - improve recommended swapsize

--- lib/python/Plugins/SystemPlugins/SwapManager/plugin.py
+++ lib/python/Plugins/SystemPlugins/SwapManager/plugin.py
@@ -22,6 +22,13 @@ config.plugins.swapmanager.swapautostart = ConfigYesNo(default = False)
 
 startswap = None
 
+try:
+	file = open("/proc/stb/info/chipset", "r")
+	chipset = file.readline().strip()
+	file.close()
+except:
+	chipset = "unknown"
+
 def SwapAutostart(reason, session=None, **kwargs):
 	global startswap
 	if reason == 0:
@@ -280,7 +287,14 @@ class SwapManager(Screen):
 	def doCSplace(self, name):
 		if name:
 			self.new_place = name[1]
-			myoptions = [[_("16 MB"), '16384'],[_("32 MB"), '32768'],[_("64 MB"), '65536'],[_("128 MB (recommended for BCM7325)"), '131072'],[_("256 MB (recommended for BCM7358)"), '262144'],[_("512 MB (recommended for BCM7356 BCM7362)"), '524288'],[_("1024 MB"), '1048576'],[_("2048 MB (maximum)"), '2097152']]
+			if chipset in ('bcm7325'):
+				myoptions = [[_("16 MB"), '16384'],[_("32 MB"), '32768'],[_("64 MB"), '65536'],[_("128 MB (recommended for this chipset)"), '131072'],[_("256 MB"), '262144'],[_("512 MB"), '524288'],[_("1024 MB"), '1048576'],[_("2048 MB (maximum)"), '2097152']]
+			if chipset in ('bcm7358'):
+				myoptions = [[_("16 MB"), '16384'],[_("32 MB"), '32768'],[_("64 MB"), '65536'],[_("128 MB"), '131072'],[_("256 MB (recommended for this chipset)"), '262144'],[_("512 MB"), '524288'],[_("1024 MB"), '1048576'],[_("2048 MB (maximum)"), '2097152']]
+			if chipset in ('bcm7356', 'bcm7362'):
+				myoptions = [[_("16 MB"), '16384'],[_("32 MB"), '32768'],[_("64 MB"), '65536'],[_("128 MB"), '131072'],[_("256 MB"), '262144'],[_("512 MB (recommended for this chipset)"), '524288'],[_("1024 MB"), '1048576'],[_("2048 MB (maximum)"), '2097152']]
+			else:
+				myoptions = [[_("16 MB"), '16384'],[_("32 MB"), '32768'],[_("64 MB"), '65536'],[_("128 MB"), '131072'],[_("256 MB"), '262144'],[_("512 MB"), '524288'],[_("1024 MB"), '1048576'],[_("2048 MB (maximum)"), '2097152']]
 			self.session.openWithCallback(self.doCSsize, ChoiceBox, title=_("Select the Swap File Size:"), list=myoptions)
 
 	def doCSsize(self, swapsize):
