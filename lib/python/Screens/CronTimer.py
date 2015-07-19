4b9ae5a8baa6838b89e1a8c43b43adf99538cbde
madie <madie1@gmx.de>
2015-07-19 19:35:47 +0200

    change script path in crontimer

--- lib/python/Screens/CronTimer.py
+++ lib/python/Screens/CronTimer.py
@@ -15,8 +15,8 @@ from boxbranding import getMachineBrand, getMachineName
 class CronTimers(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
-		if not path.exists('/usr/scripts'):
-			mkdir('/usr/scripts', 0755)
+		if not path.exists('/usr/script'):
+			mkdir('/usr/script', 0755)
 		Screen.setTitle(self, _("Cron Manager"))
 		self.onChangedEntry = [ ]
 		self['lab1'] = Label(_("Autostart:"))
@@ -285,11 +285,11 @@ class CronTimersConfig(Screen, ConfigListScreen):
 
 	def createSetup(self):
 		predefinedlist = []
-		f = listdir('/usr/scripts')
+		f = listdir('/usr/script')
 		if f:
 			for line in f:
 				parts = line.split()
-				path = "/usr/scripts/"
+				path = "/usr/script/"
 				pkg = parts[0]
 				description = path + parts[0]
 				if pkg.find('.sh') >= 0:
