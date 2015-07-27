a02e40c07b06f913489300c3d58c1d5127422ed0
arn354 <arn354@email.de>
2015-07-27 09:13:28 +0200

    SoftwareManager - ImageBackup switch to python image backup

--- lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/ImageBackup.py
@@ -1,10 +1,27 @@
-from enigma import *
+from enigma import getEnigmaVersionString
 from Screens.Screen import Screen
 from Components.Button import Button
 from Components.Label import Label
 from Components.ActionMap import ActionMap
+from Components.About import about
 from Screens.Console import Console
+from Screens.MessageBox import MessageBox
+from time import time, strftime, localtime
+from os import path, system, makedirs, listdir, walk, statvfs
+import commands
+import datetime
 
+from boxbranding import getBoxType, getMachineBrand, getMachineName, getDriverDate, getImageVersion, getImageBuild, getBrandOEM, getMachineBuild, getImageFolder, getMachineUBINIZE, getMachineMKUBIFS, getMachineMtdKernel, getMachineKernelFile, getMachineRootFile, getImageFileSystem
+
+HaveGZkernel = True
+
+VERSION = "Version 1.0 openMips"
+
+def Freespace(dev):
+	statdev = statvfs(dev)
+	space = (statdev.f_bavail * statdev.f_frsize) / 1024
+	print "[ImageBackup] Free space on %s = %i kilobytes" %(dev, space)
+	return space
 
 class ImageBackup(Screen):
 	skin = """
@@ -17,21 +34,43 @@ class ImageBackup(Screen):
 		<widget name="key_green" position="140,360" zPosition="2" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
 		<widget name="key_yellow" position="280,360" zPosition="2" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
 		<widget name="key_blue" position="420,360" zPosition="2" size="140,40" valign="center" halign="center" font="Regular;21" transparent="1" shadowColor="black" shadowOffset="-1,-1" />
-		<widget name="info-hdd" position="10,30" zPosition="1" size="550,100" font="Regular;20" halign="left" valign="top" transparent="1" />
-		<widget name="info-usb" position="10,150" zPosition="1" size="550,200" font="Regular;20" halign="left" valign="top" transparent="1" />
+		<widget name="info-hdd" position="10,30" zPosition="1" size="450,100" font="Regular;20" halign="left" valign="top" transparent="1" />
+		<widget name="info-usb" position="10,150" zPosition="1" size="450,200" font="Regular;20" halign="left" valign="top" transparent="1" />
 	</screen>"""
-		
+
 	def __init__(self, session, args = 0):
 		Screen.__init__(self, session)
 		self.session = session
-		
+		self.MODEL = getBoxType()
+		self.OEM = getBrandOEM()
+		self.MACHINEBUILD = getMachineBuild()
+		self.MACHINENAME = getMachineName()
+		self.MACHINEBRAND = getMachineBrand()
+		self.IMAGEFOLDER = getImageFolder()
+		self.UBINIZE_ARGS = getMachineUBINIZE()
+		self.MKUBIFS_ARGS = getMachineMKUBIFS()
+		self.MTDKERNEL = getMachineMtdKernel()
+		self.ROOTFSBIN = getMachineRootFile()
+		self.KERNELBIN = getMachineKernelFile()
+		self.ROOTFSTYPE = getImageFileSystem()
+		print "[ImageBackup] BOX MACHINEBUILD = >%s<" %self.MACHINEBUILD
+		print "[ImageBackup] BOX MACHINENAME = >%s<" %self.MACHINENAME
+		print "[ImageBackup] BOX MACHINEBRAND = >%s<" %self.MACHINEBRAND
+		print "[ImageBackup] BOX MODEL = >%s<" %self.MODEL
+		print "[ImageBackup] OEM MODEL = >%s<" %self.OEM
+		print "[ImageBackup] IMAGEFOLDER = >%s<" %self.IMAGEFOLDER
+		print "[ImageBackup] UBINIZE = >%s<" %self.UBINIZE_ARGS
+		print "[ImageBackup] MKUBIFS = >%s<" %self.MKUBIFS_ARGS
+		print "[ImageBackup] MTDKERNEL = >%s<" %self.MTDKERNEL
+		print "[ImageBackup] ROOTFSTYPE = >%s<" %self.ROOTFSTYPE
+
 		self["key_green"] = Button("USB")
 		self["key_red"] = Button("HDD")
 		self["key_blue"] = Button(_("Exit"))
 		self["key_yellow"] = Button("")
-		self["info-usb"] = Label(_("USB = Do you want to make a back-up on USB?\nThis will take between 2 and 7 minutes depending on the used filesystem and is fully automatic.\nMake sure you first insert an USB flash drive before you select USB."))
-		self["info-hdd"] = Label(_("HDD = Do you want to make an back-up on HDD? \nThis only takes 2 or 3 minutes and is fully automatic."))
-		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"], 
+		self["info-usb"] = Label(_("USB = Do you want to create a fullbackup on USB?\nThis will take between 4 and 15 minutes depending on the used filesystem and is fully automatic.\nMake sure you first insert an USB flash drive before you select USB."))
+		self["info-hdd"] = Label(_("HDD = Do you want to create a fullbackup on HDD? \nThis only takes 2 or 10 minutes and is fully automatic."))
+		self["actions"] = ActionMap(["OkCancelActions", "ColorActions"],
 		{
 			"blue": self.quit,
 			"yellow": self.yellow,
@@ -40,17 +79,342 @@ class ImageBackup(Screen):
 			"cancel": self.quit,
 		}, -2)
 
-		
+	def check_hdd(self):
+		if not path.exists("/media/hdd"):
+			self.session.open(MessageBox, _("No /hdd found !!\nPlease make sure you have a HDD mounted.\n"), type = MessageBox.TYPE_ERROR)
+			return False
+		if Freespace('/media/hdd') < 300000:
+			self.session.open(MessageBox, _("Not enough free space on /hdd !!\nYou need at least 300Mb free space.\n"), type = MessageBox.TYPE_ERROR)
+			return False
+		return True
+
+	def check_usb(self, dev):
+		if Freespace(dev) < 300000:
+			self.session.open(MessageBox, _("Not enough free space on %s !!\nYou need at least 300Mb free space.\n" % dev), type = MessageBox.TYPE_ERROR)
+			return False
+		return True
+
 	def quit(self):
-		self.close()	
-		
+		self.close()
+
 	def red(self):
-		self.session.open(Console, title = "Full back-up on HDD", cmdlist = ["sh '/usr/lib/enigma2/python/Plugins/SystemPlugins/SoftwareManager/backup-hdd.sh'"])
+		if self.check_hdd():
+			self.doFullBackup("/hdd")
 
 	def green(self):
-		self.session.open(Console, title = "Full Back-up to USB", cmdlist = ["sh '/usr/lib/enigma2/python/Plugins/SystemPlugins/SoftwareManager/backup-usb.sh'"])
+		USB_DEVICE = self.SearchUSBcanidate()
+		if USB_DEVICE == 'XX':
+			text = _("No USB-Device found for fullbackup !!\n\n\n")
+			text += _("To back-up directly to the USB-stick, the USB-stick MUST\n")
+			text += _("contain a file with the name: \n\n")
+			text += _("backupstick or backupstick.txt")
+			self.session.open(MessageBox, text, type = MessageBox.TYPE_ERROR)
+		else:
+			if self.check_usb(USB_DEVICE):
+				self.doFullBackup(USB_DEVICE)
 
 	def yellow(self):
 		#// Not used
-		pass	
+		pass
 
+	def SearchUSBcanidate(self):
+		for paths, subdirs, files in walk("/media"):
+			for dir in subdirs:
+				if not dir == 'hdd' and not dir == 'net':
+					for file in listdir("/media/" + dir):
+						if file.find("backupstick") > -1:
+							print "[ImageBackup] USB-DEVICE found on: /media/%s" % dir
+							return "/media/" + dir
+			break
+		return "XX"
+
+	def doFullBackup(self, DIRECTORY):
+		self.DIRECTORY = DIRECTORY
+		self.TITLE = _("Fullbackup on %s") % (self.DIRECTORY)
+		self.START = time()
+		self.DATE = strftime("%Y%m%d_%H%M", localtime(self.START))
+		self.IMAGEVERSION = self.imageInfo()
+		if "ubi" in self.ROOTFSTYPE.split():
+			self.MKFS = "/usr/sbin/mkfs.ubifs"
+		else:
+			self.MKFS = "/usr/sbin/mkfs.jffs2"
+		self.UBINIZE = "/usr/sbin/ubinize"
+		self.NANDDUMP = "/usr/sbin/nanddump"
+		self.WORKDIR= "%s/bi" %self.DIRECTORY
+		self.TARGET="XX"
+
+		## TESTING IF ALL THE TOOLS FOR THE BUILDING PROCESS ARE PRESENT
+		if not path.exists(self.MKFS):
+			text = "%s not found !!" %self.MKFS
+			self.session.open(MessageBox, _(text), type = MessageBox.TYPE_ERROR)
+			return
+		if not path.exists(self.NANDDUMP):
+			text = "%s not found !!" %self.NANDDUMP
+			self.session.open(MessageBox, _(text), type = MessageBox.TYPE_ERROR)
+			return
+
+		self.SHOWNAME = "%s %s" %(self.MACHINEBRAND, self.MODEL)
+		self.MAINDESTOLD = "%s/%s" %(self.DIRECTORY, self.MODEL)
+		self.MAINDEST = "%s/%s" %(self.DIRECTORY,self.IMAGEFOLDER)
+		self.EXTRA = "%s/fullbackup_%s/%s/%s" % (self.DIRECTORY, self.MODEL, self.DATE, self.IMAGEFOLDER)
+		self.EXTRAOLD = "%s/fullbackup_%s/%s/%s" % (self.DIRECTORY, self.MODEL, self.DATE, self.MODEL)
+		print "[ImageBackup] SHOWNAME: ", self.SHOWNAME
+		print "[ImageBackup] MAINDESTOLD: ", self.MAINDESTOLD
+		print "[ImageBackup] EXTRA: ", self.EXTRA
+		print "[ImageBackup] EXTRAOLD: ", self.EXTRAOLD
+
+		self.message = "echo -e '\n"
+		self.message += (_("Fullback for %s\n" %self.SHOWNAME)).upper()
+		self.message += VERSION + '\n'
+		self.message += "____________________________________________________\n"
+		self.message += _("Please be patient, a fullbackup will now be created.\n")
+		if self.ROOTFSTYPE == "ubi":
+			self.message += _("Due to the used filesystem, the fullbackup\n")
+			self.message += _("will take about 3-12 minutes for this system.\n")
+		else:
+			self.message += _("This will take about 2-9 minutes for this system.\n")
+		self.message += "____________________________________________________\n"
+		self.message += "'"
+
+		## PREPARING THE BUILDING ENVIRONMENT
+		system("rm -rf %s" %self.WORKDIR)
+		if not path.exists(self.WORKDIR):
+			makedirs(self.WORKDIR)
+		if not path.exists("/tmp/bi/root"):
+			makedirs("/tmp/bi/root")
+		system("sync")
+		system("mount --bind / /tmp/bi/root")
+
+		if "jffs2" in self.ROOTFSTYPE.split():
+			cmd1 = "%s --root=/tmp/bi/root --faketime --output=%s/root.jffs2 %s" % (self.MKFS, self.WORKDIR, self.MKUBIFS_ARGS)
+			cmd2 = None
+			print "[ImageBackup] jffs2 cmd1: ", cmd1
+			print "[ImageBackup] jffs2 cmd2: ", cmd2
+		else:
+			f = open("%s/ubinize.cfg" %self.WORKDIR, "w")
+			f.write("[ubifs]\n")
+			f.write("mode=ubi\n")
+			f.write("image=%s/root.ubi\n" %self.WORKDIR)
+			f.write("vol_id=0\n")
+			f.write("vol_type=dynamic\n")
+			f.write("vol_name=rootfs\n")
+			f.write("vol_flags=autoresize\n")
+			f.close()
+			ff = open("%s/root.ubi" %self.WORKDIR, "w")
+			ff.close()
+			cmd1 = "%s -r /tmp/bi/root -o %s/root.ubi %s" % (self.MKFS, self.WORKDIR, self.MKUBIFS_ARGS)
+			cmd2 = "%s -o %s/root.ubifs %s %s/ubinize.cfg" % (self.UBINIZE, self.WORKDIR, self.UBINIZE_ARGS, self.WORKDIR)
+			cmd3 = "mv %s/root.ubifs %s/root.%s" %(self.WORKDIR, self.WORKDIR, self.ROOTFSTYPE)
+			print "[ImageBackup] ubifs cmd1: ", cmd1
+			print "[ImageBackup] ubifs cmd2: ", cmd2
+			print "[ImageBackup] ubifs cmd3: ", cmd3
+
+		cmdlist = []
+		cmdlist.append(self.message)
+		cmdlist.append('echo "Create: root.%s\n"' %self.ROOTFSTYPE)
+		cmdlist.append(cmd1)
+		if cmd2:
+			cmdlist.append(cmd2)
+			cmdlist.append(cmd3)
+		cmdlist.append("chmod 644 %s/root.%s" %(self.WORKDIR, self.ROOTFSTYPE))
+		cmdlist.append('echo " "')
+		cmdlist.append('echo "Create: kerneldump"')
+		cmdlist.append('echo " "')
+		cmdlist.append("nanddump -a -f %s/vmlinux.gz /dev/%s" % (self.WORKDIR, self.MTDKERNEL))
+		cmdlist.append('echo " "')
+
+		if HaveGZkernel:
+			cmdlist.append('echo "Check: kerneldump"')
+		cmdlist.append("sync")
+
+		self.session.open(Console, title = self.TITLE, cmdlist = cmdlist, finishedCallback = self.doFullBackupCB, closeOnSuccess = True)
+
+	def doFullBackupCB(self):
+		if HaveGZkernel:
+			ret = commands.getoutput(' gzip -d %s/vmlinux.gz -c > /tmp/vmlinux.bin' % self.WORKDIR)
+			if ret:
+				text = "Kernel dump error\n"
+				text += "Please Flash your Kernel new and Backup again"
+				system('rm -rf /tmp/vmlinux.bin')
+				self.session.open(MessageBox, _(text), type = MessageBox.TYPE_ERROR)
+				return
+
+		cmdlist = []
+		cmdlist.append(self.message)
+		if HaveGZkernel:
+			cmdlist.append('echo "Kernel dump OK"')
+			cmdlist.append("rm -rf /tmp/vmlinux.bin")
+		cmdlist.append('echo "____________________________________________________\n"')
+		cmdlist.append('echo "Creating the Fullbackup..."')
+
+		system('rm -rf %s' %self.MAINDEST)
+		if not path.exists(self.MAINDEST):
+			makedirs(self.MAINDEST)
+		if not path.exists(self.EXTRA):
+			makedirs(self.EXTRA)
+
+		f = open("%s/imageversion" %self.MAINDEST, "w")
+		f.write(self.IMAGEVERSION)
+		f.close()
+
+		system('mv %s/root.%s %s/%s' %(self.WORKDIR, self.ROOTFSTYPE, self.MAINDEST, self.ROOTFSBIN))
+		system('mv %s/vmlinux.gz %s/%s' %(self.WORKDIR, self.MAINDEST, self.KERNELBIN))
+		cmdlist.append('echo "rename this file to "force" to force an update without confirmation" > %s/noforce' %self.MAINDEST)
+
+		if self.MODEL in ("gbquad", "gbquadplus", "gb800ue", "gb800ueplus", "gbultraue"):
+			lcdwaitkey = '/usr/share/lcdwaitkey.bin'
+			lcdwarning = '/usr/share/lcdwarning.bin'
+			if path.exists(lcdwaitkey):
+				system('cp %s %s/lcdwaitkey.bin' %(lcdwaitkey, self.MAINDEST))
+			if path.exists(lcdwarning):
+				system('cp %s %s/lcdwarning.bin' %(lcdwarning, self.MAINDEST))
+		if self.MODEL == "gb800solo":
+			burnbat = "%s/fullbackup_%s/%s" % (self.DIRECTORY, self.MODEL, self.DATE)
+			f = open("%s/burn.bat" % (burnbat), "w")
+			f.write("flash -noheader usbdisk0:gigablue/solo/kernel.bin flash0.kernel\n")
+			f.write("flash -noheader usbdisk0:gigablue/solo/rootfs.bin flash0.rootfs\n")
+			f.write('setenv -p STARTUP "boot -z -elf flash0.kernel: ')
+			f.write("'rootfstype=jffs2 bmem=106M@150M root=/dev/mtdblock6 rw '")
+			f.write('"\n')
+			f.close()
+
+		cmdlist.append('cp -r %s/* %s/' % (self.MAINDEST, self.EXTRA))
+
+		cmdlist.append("sync")
+		file_found = True
+
+		if not path.exists("%s/%s" % (self.MAINDEST, self.ROOTFSBIN)):
+			print '[ImageBackup] ROOTFS bin file not found'
+			file_found = False
+
+		if not path.exists("%s/%s" % (self.MAINDEST, self.KERNELBIN)):
+			print '[ImageBackup] KERNEL bin file not found'
+			file_found = False
+
+		if path.exists("%s/noforce" % self.MAINDEST):
+			print '[ImageBackup] NOFORCE bin file not found'
+			file_found = False
+
+		if file_found:
+			cmdlist.append('echo "____________________________________________________\n"')
+			cmdlist.append('echo "Fullbackup created on:" %s' %self.MAINDEST)
+			cmdlist.append('echo "Extra copy of fullbackup created on:"')
+			cmdlist.append('echo %s' %self.EXTRA)
+			cmdlist.append('echo "____________________________________________________\n"')
+			cmdlist.append('echo "Please wait...almost ready...\n"')
+			cmdlist.append('echo "To restore the image:"')
+			cmdlist.append('echo "Please check the manual of the receiver "')
+			cmdlist.append('echo "on how to restore the image"')
+		else:
+			cmdlist.append('echo "____________________________________________________\n"')
+			cmdlist.append('echo "Image creation failed - "')
+			cmdlist.append('echo "Possible causes could be"')
+			cmdlist.append('echo "     wrong back-up destination "')
+			cmdlist.append('echo "     no space left on back-up device"')
+			cmdlist.append('echo "     no writing permission on back-up device"')
+			cmdlist.append('echo " "')
+
+		if self.DIRECTORY == "/hdd":
+			self.TARGET = self.SearchUSBcanidate()
+			print "[ImageBackup] TARGET = %s" % self.TARGET
+			if self.TARGET == 'XX':
+				cmdlist.append('echo "\n"')
+				cmdlist.append('echo "\n"')
+				cmdlist.append('echo "Please wait..."')
+			else:
+				cmdlist.append('echo "\n"')
+				cmdlist.append('echo "\n"')
+				cmdlist.append('echo "__________________________________________________\n"')
+				cmdlist.append('echo "There is a valid USB-flash drive detected in one "')
+				cmdlist.append('echo "of the USB-ports, therefor an extra copy of the "')
+				cmdlist.append('echo "back-up image will now be copied to that USB- "')
+				cmdlist.append('echo "flash drive. "')
+				cmdlist.append('echo "This only takes about 1 or 2 minutes"')
+				cmdlist.append('echo "\n"')
+
+				cmdlist.append('mkdir -p %s/%s' % (self.TARGET, self.IMAGEFOLDER))
+				cmdlist.append('cp -r %s %s/' % (self.MAINDEST, self.TARGET))
+
+
+				cmdlist.append("sync")
+				cmdlist.append('echo "Backup finished and copied to your USB-flash drive"')
+
+		cmdlist.append("sleep 2")
+		cmdlist.append("umount /tmp/bi/root")
+		cmdlist.append("sleep 2")
+		cmdlist.append("rmdir /tmp/bi/root")
+		cmdlist.append("rmdir /tmp/bi")
+		cmdlist.append("rm -rf %s" % self.WORKDIR)
+		cmdlist.append("sleep 5")
+		cmdlist.append('echo "\n"')
+		END = time()
+		DIFF = int(END - self.START)
+		TIMELAP = str(datetime.timedelta(seconds=DIFF))
+		cmdlist.append('echo "\n\n"')
+		cmdlist.append('echo "Time required for this process: %s"' %TIMELAP)
+		cmdlist.append('echo "\n"')
+
+		self.session.open(Console, title = self.TITLE, cmdlist = cmdlist, closeOnSuccess = False)
+
+	def imageInfo(self):
+		imageInfotext = _("[Full Image Backup]\n")
+		imageInfotext += _("By openMips Image Team") + "\n"
+		imageInfotext += _("Support at") + " http://gigablue-support.com/\n\n"
+		imageInfotext += _("[Image Info]\n")
+		imageInfotext += _("Model: %s %s\n") % (getMachineBrand(), getMachineName())
+		imageInfotext += _("Backup Date: %s\n") % strftime("%Y-%m-%d", localtime(self.START))
+
+		imageInfotext += _("Chipset: BCM%s") % about.getChipSetString().lower().replace('\n','').replace('bcm','') + "\n"
+		imageInfotext += _("Kernel: %s") % about.getKernelVersionString() + "\n"
+		imageInfotext += _("CPU: %s") % about.getCPUString() + "\n"
+		imageInfotext += _("Threads: %s") % about.getCpuCoresString() + "\n"
+		imageInfotext += _("Enigma2 Version: %s") % getImageVersion() + "\n"
+		imageInfotext += _("Enigma2 Build: %s") % getImageBuild() + "\n"
+
+		string = getDriverDate()
+		year = string[0:4]
+		month = string[4:6]
+		day = string[6:8]
+		driversdate = '-'.join((year, month, day))
+		imageInfotext += _("Drivers: %s") % driversdate + "\n"
+		imageInfotext += _("Last update: %s") % getEnigmaVersionString() + "\n\n"
+		imageInfotext += _("[Enigma2 Settings]\n")
+		imageInfotext += commands.getoutput("cat /etc/enigma2/settings")
+		imageInfotext += _("\n\n[User - bouquets (TV)]\n")
+		try:
+			f = open("/etc/enigma2/bouquets.tv","r")
+			lines = f.readlines()
+			f.close()
+			for line in lines:
+				if line.startswith("#SERVICE"):
+					bouqet = line.split()
+					if len(bouqet) > 3:
+						bouqet[3] = bouqet[3].replace('"','')
+						f = open("/etc/enigma2/" + bouqet[3],"r")
+						userbouqet = f.readline()
+						imageInfotext += userbouqet.replace('#NAME ','')
+						f.close()
+		except:
+			imageInfotext += "Error reading bouquets.tv"
+
+		imageInfotext += _("\n[User - bouquets (RADIO)]\n")
+		try:
+			f = open("/etc/enigma2/bouquets.radio","r")
+			lines = f.readlines()
+			f.close()
+			for line in lines:
+				if line.startswith("#SERVICE"):
+					bouqet = line.split()
+					if len(bouqet) > 3:
+						bouqet[3] = bouqet[3].replace('"','')
+						f = open("/etc/enigma2/" + bouqet[3],"r")
+						userbouqet = f.readline()
+						imageInfotext += userbouqet.replace('#NAME ','')
+						f.close()
+		except:
+			imageInfotext += "Error reading bouquets.radio"
+
+		imageInfotext += _("\n[Installed Plugins]\n")
+		imageInfotext += commands.getoutput("opkg list_installed | grep enigma2-plugin-")
+
+		return imageInfotext
