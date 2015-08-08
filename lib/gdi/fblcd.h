91eba2e0b714f2919d53b4a5560dde69e11087ea
Captain <captain.onboard@web.de>
2015-08-08 23:51:18 +0200

    fix dm800 build fblcd

--- lib/gdi/fblcd.h
+++ lib/gdi/fblcd.h
@@ -6,6 +6,10 @@
 #include <lib/gdi/lcd.h>
 #include "gpixmap.h"
 
+#ifndef FBIO_WAITFORVSYNC
+#define FBIO_WAITFORVSYNC _IOW('F', 0x20, uint32_t)
+#endif
+
 class eFbLCD: public eLCD
 {
 	int m_xRes, m_yRes, m_bpp;
