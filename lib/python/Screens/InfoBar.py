5e430d30da6f089d4b2ffd35738a36732d350e07
arn354 <arn354@email.de>
2015-08-22 18:04:05 +0200

    Radiomode - fix display of picon - thx adenin

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -131,6 +131,8 @@ class InfoBar(InfoBarBase, InfoBarShowHide,
 	def ChannelSelectionRadioClosed(self, *arg):
 		self.rds_display.show()  # in InfoBarRdsDecoder
 		self.servicelist.correctChannelNumber()
+		self.radioTV = 0
+		self.doShow()
 
 	def showMovies(self, defaultRef=None):
 		self.lastservice = self.session.nav.getCurrentlyPlayingServiceOrGroup()
