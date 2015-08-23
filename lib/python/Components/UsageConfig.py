4bbd521930848e60a75cb44f7b98191ec0cfe9fd
Huevos <rowland@moralet.com>
2015-08-24 01:46:01 +0200

    [UsageConfig] Set "Allow unstable (experimental) updates" to default yes in Developer images as this is the only state a Developer image has.

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1,6 +1,6 @@
 import os
 from time import time
-from boxbranding import getBrandOEM, getBoxType
+from boxbranding import getBrandOEM, getBoxType, getImageType
 
 from enigma import eDVBDB, eEPGCache, setTunerTypePriorityOrder, setPreferredTuner, setSpinnerOnOff, setEnableTtCachingOnOff, eEnv, Misc_Options, eBackgroundFileEraser, eServiceEvent
 
@@ -403,7 +403,7 @@ def InitUsageConfig():
 	config.softwareupdate.checktimer = ConfigSelectionNumber(min = 1, max = 48, stepwidth = 1, default = 6, wraparound = True)
 	config.softwareupdate.updatelastcheck = ConfigInteger(default=0)
 	config.softwareupdate.updatefound = NoSave(ConfigBoolean(default = False))
-	config.softwareupdate.updatebeta = ConfigYesNo(default = False)
+	config.softwareupdate.updatebeta = ConfigYesNo(default = getImageType().title() == 'Developer')
 	config.softwareupdate.updateisunstable = ConfigInteger(default=0)
 
 	config.timeshift = ConfigSubsection()
