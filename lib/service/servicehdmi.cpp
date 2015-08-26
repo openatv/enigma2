71736554d10cf43d9f754a58fc7af7dd5520d78a
Peter Urbanec <bitbucket.org@urbanec.net>
2015-08-26 16:23:45 +1000

    Revert "Fix bug #410: Incorrect video mode info shown when viewing HDMI IN live (T4)"
    This reverts commit 1eb2f31ff148b066a96a366b3b7df1420b741b96.

--- lib/service/servicehdmi.cpp
+++ lib/service/servicehdmi.cpp
@@ -169,14 +169,7 @@ int eServiceHDMI::getInfo(int w)
 
 std::string eServiceHDMI::getInfoString(int w)
 {
-	switch (w)
-	{
-	case sServiceref:
-		return m_ref.toString();
-	default:
-		break;
-	}
-	return iServiceInformation::getInfoString(w);
+	return "";
 }
 
 ePtr<iServiceInfoContainer> eServiceHDMI::getInfoObject(int w)
