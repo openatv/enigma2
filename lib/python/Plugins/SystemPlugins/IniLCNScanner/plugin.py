dcaf6cb6132464202d13cd4eed4d67ac083db5d9
prl <prl@ozemail.com.au>
2015-09-12 12:00:19 +1000

    eServiceReference cleanup - 2
    Second stage of a cleanup of the use of eServiceReference, especially
    to reduce the use of numbers for eServiceReference parameters and
    the reduce the use of creation and manipulation of eServiceReferences
    in their string format.
    
    Use eServiceReferences instead of the equivalent string expressions
    where possible.
    
    Use some global eServiceReferences and functions from
    Tools/ServiceReference.py.
    
    Retained now-unused string definitions of service_types_tv and
    service_types_radio for partial backwards compatability, but changing
    them will have no effect. Use
    Tools.ServiceReference.service_types_{tv,radio}_ref instead.
    
    Using Tools.ServiceReference.service_types_radio_ref adds (type ==
    eServiceID.dRadioAvc) to the search path, allowing advanced codec
    radio to be included. This should make no practical difference for
    Australian DVB-T broadcasts.
    
    Use common class variables for common eServiceReferences instead
    of replicating the definitions.
    
    Refactor writeTVBouquet(), writeRadioBouquet(), addInTVBouquets()
    and addInRadioBouquets() to eliminate duplicated code.

--- lib/python/Plugins/SystemPlugins/IniLCNScanner/plugin.py
+++ lib/python/Plugins/SystemPlugins/IniLCNScanner/plugin.py
@@ -1,6 +1,6 @@
 # plugin from Sif Team, with some modds for EGAMI use
 
-from enigma import eDVBDB, eServiceReference, eServiceCenter
+from enigma import eDVBDB, eServiceID, eServiceReference, eServiceCenter
 from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
 from Components.ActionMap import ActionMap
@@ -8,6 +8,7 @@ from Components.Button import Button
 from Components.ConfigList import ConfigListScreen
 from Components.config import getConfigListEntry, config, ConfigSubsection, ConfigYesNo, ConfigSelection, configfile
 from Tools.Directories import resolveFilename, SCOPE_CONFIG
+from Tools.ServiceReference import service_types_tv_ref, service_types_radio_ref, serviceRefAppendPath, hdmiInServiceRef
 from Plugins.Plugin import PluginDescriptor
 from boxbranding import getMachineBuild
 import os
@@ -15,8 +16,33 @@ import sys
 import xml.etree.cElementTree
 
 class LCN():
-	service_types_tv = '1:7:1:0:0:0:0:0:0:0:(type == 1) || (type == 17) || (type == 22) || (type == 25) || (type == 134) || (type == 195)'
-	service_types_radio = '1:7:2:0:0:0:0:0:0:0:(type == 2)'
+	service_types_tv = service_types_tv_ref.toString()
+	service_types_radio = service_types_radio_ref.toString()
+	sr_marker_terr_lcn_tv = eServiceReference(eServiceReference.idDVB, eServiceReference.isMarker)
+	sr_marker_terr_lcn_tv.setName("Terrestrial TV LCN")
+	sr_marker_terr_lcn_radio = eServiceReference(eServiceReference.idDVB, eServiceReference.isMarker)
+	sr_marker_terr_lcn_radio.setName("Terrestrial RADIO LCN")
+	sr_filler = eServiceReference(
+		eServiceReference.idDVB,
+		eServiceReference.isInvisible | eServiceReference.isNumberedMarker | eServiceReference.isMarker,
+		0xD
+	)
+	sr_marker = eServiceReference(
+		eServiceReference.idDVB,
+		eServiceReference.isMarker
+	)
+	sr_tv_bouquet_entry = eServiceReference(
+		eServiceReference.idDVB,
+		eServiceReference.isInvisible | eServiceReference.isNumberedMarker | eServiceReference.isMarker,
+		eServiceID.dTv
+	)
+	sr_tv_bouquet_entry.setPath('FROM BOUQUET "userbouquet.terrestrial_lcn.tv" ORDER BY bouquet')
+	sr_radio_bouquet_entry = eServiceReference(
+		eServiceReference.idDVB,
+		eServiceReference.isInvisible | eServiceReference.isNumberedMarker | eServiceReference.isMarker,
+		eServiceID.dRadio
+	)
+	sr_radio_bouquet_entry.setPath('FROM BOUQUET "userbouquet.terrestrial_lcn.radio" ORDER BY bouquet')
 
 	def __init__(self, dbfile, rulefile, rulename, bouquetfile):
 		self.dbfile = dbfile
@@ -122,10 +148,9 @@ class LCN():
 	def readE2Services(self, serviceType):
 		self.e2services = []
 		if serviceType == "TV":
-			refstr = '%s ORDER BY name' % (self.service_types_tv)
+			ref = serviceRefAppendPath(service_types_tv_ref, ' ORDER BY name')
 		elif serviceType == "RADIO":
-			refstr = '%s ORDER BY name' % (self.service_types_radio)
-		ref = eServiceReference(refstr)
+			ref = serviceRefAppendPath(service_types_radio_ref, ' ORDER BY name')
 		serviceHandler = eServiceCenter.getInstance()
 		servicelist = serviceHandler.list(ref)
 		if servicelist is not None:
@@ -136,11 +161,11 @@ class LCN():
 
 				unsigned_orbpos = service.getUnsignedData(4) >> 16
 				if unsigned_orbpos == 0xEEEE:  # Terrestrial
-					self.e2services.append(service.toString())
+					self.e2services.append(service)
 
-	def writeTVBouquet(self):
+	def writeBouquet(self, filename, name_marker, extras=[]):
 		try:
-			f = open('/etc/enigma2/userbouquet.terrestrial_lcn.tv', "w")
+			f = open('/etc/enigma2/' + filename, "w")
 		except Exception, e:
 			print "[LCNScanner]", e
 			return
@@ -157,140 +182,84 @@ class LCN():
 				self.newlist.append(x)
 
 		#for x in self.e2services:
-			#print "[LCNScanner]", " self.e2services:", x
+			#print "[LCNScanner]", " self.e2services:", x.toString()
 
 		#for x in self.newlist:
 			#print "[LCNScanner]", " NEW LIST LCN :", x
 
 		#print "[LCNScanner]", " New LIST LEN: " , len(self.newlist)
 
-		f.write("#NAME Terrestrial TV LCN\n")
-		f.write("#SERVICE 1:64:0:0:0:0:0:0:0:0::Terrestrial TV LCN\n")
-		f.write("##DESCRIPTION Terrestrial TV LCN\n")
+		print "[LCN] writeTVBouquet write", "#NAME " + name_marker.getName() #ZZ
+		f.write("#NAME " + name_marker.getName() + "\n")
+		f.write("#SERVICE " + name_marker.toString() + "\n")
+		f.write("##DESCRIPTION " + name_marker.getName() + "\n")
 		for x in self.newlist:
 			if int(x[1]) == 11111111:
 				#print "[LCNScanner]", x[0], " Detected 111111111111 service"
-				f.write("#SERVICE 1:832:d:0:0:0:0:0:0:0:\n")
+				f.write("#SERVICE " + self.sr_filler.toString() + "\n")
 				continue
 
 			if len(self.markers) > 0:
 				if x[0] > self.markers[0][0]:
-					f.write("#SERVICE 1:64:0:0:0:0:0:0:0:0:\n")
+					f.write("#SERVICE " + self.sr_marker.toString() + "\n")
 					f.write("#DESCRIPTION ------- " + self.markers[0][1] + " -------\n")
 					self.markers.remove(self.markers[0])
 			refstr = "1:0:1:%x:%x:%x:%x:0:0:0:" % (x[4], x[3], x[2], x[1])  # temporary ref
 			refsplit = eServiceReference(refstr).toString().split(":")
 			added = False
 			for tref in self.e2services:
-				tmp = tref.split(":")
+				tmp = tref.toString().split(":")
 				if tmp[3] == refsplit[3] and tmp[4] == refsplit[4] and tmp[5] == refsplit[5] and tmp[6] == refsplit[6]:
-					f.write("#SERVICE " + tref + "\n")
+					f.write("#SERVICE " + tref.toString() + "\n")
 					added = True
 					break
 
 			if not added:  # no service found? something wrong? a log should be a good idea. Anyway we add an empty line so we keep the numeration
-				f.write("#SERVICE 1:832:d:0:0:0:0:0:0:0:\n")
+				f.write("#SERVICE " + self.sr_filler.toString() + "\n")
 
-		# Add HDMI-IN
-		if getMachineBuild() in ('inihdp'):
-			f.write("#SERVICE 8192:0:1:0:0:0:0:0:0:0:\n")
+		# Add extra services
+		for x in extras:
+			f.write("#SERVICE " + x.toString() + "\n")
 
 		f.close()
+
+	def writeTVBouquet(self):
+		# Add HDMI-IN
+		if getMachineBuild() in ('inihdp'):
+			extras = [hdmiInServiceRef()]
+		else:
+			extras = []
+		self.writeBouquet("userbouquet.terrestrial_lcn.tv", self.sr_marker_terr_lcn_tv, extras)
 		self.addInTVBouquets()
 
-	def addInTVBouquets(self):
-		f = open('/etc/enigma2/bouquets.tv', 'r')
+	def addInBouquets(self, bouquets_filename, bouquet_filename, bouquet_entry):
+		f = open('/etc/enigma2/' + bouquets_filename, 'r')
 		ret = f.read().split("\n")
 		f.close()
 
 		i = 0
 		while i < len(ret):
-			if ret[i].find("userbouquet.terrestrial_lcn.tv") >= 0:
+			if ret[i].find(bouquet_filename) >= 0:
 				return
 			i += 1
 
-		f = open('/etc/enigma2/bouquets.tv', 'w')
+		f = open('/etc/enigma2/' + bouquets_filename, 'w')
 		f.write(ret[0] + "\n")
-		f.write('#SERVICE 1:7:1:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.terrestrial_lcn.tv" ORDER BY bouquet\n')
+		f.write("#SERVICE " + bouquet_entry.toString() + "\n")
 		i = 1
 		while i < len(ret):
 			f.write(ret[i] + "\n")
 			i += 1
 
+	def addInTVBouquets(self):
+		self.addInBouquets("bouquets.tv", "userbouquet.terrestrial_lcn.tv", self.sr_tv_bouquet_entry)
+
 	def writeRadioBouquet(self):
-		try:
-			f = open('/etc/enigma2/userbouquet.terrestrial_lcn.radio', "w")
-		except Exception, e:
-			print "[LCNScanner]", e
-			return
-
-		self.newlist = []
-		count = 0
-
-		for x in self.lcnlist:
-			count += 1
-			while x[0] != count:
-				self.newlist.append([count, 11111111, 11111, 111, 111, 111111])
-				count += 1
-			if x[0] == count:
-				self.newlist.append(x)
-
-		#for x in self.e2services:
-			#print "[LCNScanner]", " self.e2services:", x
-
-		#for x in self.newlist:
-			#print "[LCNScanner]", " NEW LIST LCN :", x
-
-		#print "[LCNScanner]", " New LIST LEN: " , len(self.newlist)
-
-		f.write("#NAME Terrestrial Radio LCN\n")
-		f.write("#SERVICE 1:64:0:0:0:0:0:0:0:0::Terrestrial RADIO LCN\n")
-		f.write("##DESCRIPTION Terrestrial RADIO LCN\n")
-		for x in self.newlist:
-			if int(x[1]) == 11111111:
-				#print "[LCNScanner]", x[0], " Detected 111111111111 service"
-				f.write("#SERVICE 1:832:d:0:0:0:0:0:0:0:\n")
-				continue
-
-			if len(self.markers) > 0:
-				if x[0] > self.markers[0][0]:
-					f.write("#SERVICE 1:64:0:0:0:0:0:0:0:0:\n")
-					f.write("#DESCRIPTION ------- " + self.markers[0][1] + " -------\n")
-					self.markers.remove(self.markers[0])
-			refstr = "1:0:2:%x:%x:%x:%x:0:0:0:" % (x[4], x[3], x[2], x[1])  # temporary ref
-			refsplit = eServiceReference(refstr).toString().split(":")
-			added = False
-			for tref in self.e2services:
-				tmp = tref.split(":")
-				if tmp[3] == refsplit[3] and tmp[4] == refsplit[4] and tmp[5] == refsplit[5] and tmp[6] == refsplit[6]:
-					f.write("#SERVICE " + tref + "\n")
-					added = True
-					break
-
-			if not added:  # no service found? something wrong? a log should be a good idea. Anyway we add an empty line so we keep the numeration
-				f.write("#SERVICE 1:832:d:0:0:0:0:0:0:0:\n")
-
-		f.close()
+		self.writeBouquet("userbouquet.terrestrial_lcn.radio", self.sr_marker_terr_lcn_radio)
 		self.addInRadioBouquets()
 
 	def addInRadioBouquets(self):
-		f = open('/etc/enigma2/bouquets.radio', 'r')
-		ret = f.read().split("\n")
-		f.close()
-
-		i = 0
-		while i < len(ret):
-			if ret[i].find("userbouquet.terrestrial_lcn.radio") >= 0:
-				return
-			i += 1
-
-		f = open('/etc/enigma2/bouquets.radio', 'w')
-		f.write(ret[0] + "\n")
-		f.write('#SERVICE 1:7:2:0:0:0:0:0:0:0:FROM BOUQUET "userbouquet.terrestrial_lcn.radio" ORDER BY bouquet\n')
-		i = 1
-		while i < len(ret):
-			f.write(ret[i] + "\n")
-			i += 1
+		self.addInBouquets("bouquets.radio", "userbouquet.terrestrial_lcn.radio", self.sr_radio_bouquet_entry)
 
 	def reloadBouquets(self):
 		eDVBDB.getInstance().reloadBouquets()
@@ -335,15 +304,15 @@ class LCNBuildHelper():
 			if line[:8] != "#SERVICE":
 				continue
 
-			tmp = line.strip().split(":")
-			line = tmp[len(tmp) - 1]
-
 			filename = None
-			if line[:12] == "FROM BOUQUET":
-				tmp = line[13:].split(" ")
-				filename = tmp[0].strip("\"")
-			else:
-				filename = line
+			bouquetlistref = eServiceReference(line[8:].strip())
+			bqpath = bouquetlistref.getPath()
+			if bqpath.startswith("FROM BOUQUET"):
+				bqparts = bqpath.split('"', 3)
+				if len(bqparts) == 3:
+					filename = bqparts[1]
+				else:
+					filename = bqpath
 
 			if filename:
 				try:
@@ -353,7 +322,7 @@ class LCNBuildHelper():
 					continue
 
 				tmp = fb.readline().strip()
-				if tmp[:6] == "#NAME ":
+				if tmp.startswith("#NAME "):
 					ret.append([filename, tmp[6:]])
 				else:
 					ret.append([filename, filename])
