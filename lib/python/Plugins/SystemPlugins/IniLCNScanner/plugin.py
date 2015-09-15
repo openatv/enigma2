5fee771dfa5eb37bfc5ec15b7ee424536cbae417
prl <prl@ozemail.com.au>
2015-09-15 12:44:28 +1000

    eServiceReference cleanup - 2 rewrite addLcnToList & writeBouquet
    Rewrite addLcnToList() to have it comply with FreeTV OP 41 "Logical
    Channel Descriptor and Allocation of Logical Channel Numbers", Table
    2 overlap cases 2 and 3.
    
    Overlap case 1 seems to be dealt with in the C++ scan code (in
    particular eDVBScan::addKnownGoodChannel()).
    
    Assign duplicate LCNs sequentially from LCN 350 instead of at
    orig_lcn + 350, which goes outside the range for LCN duplicates
    (350..399) if orig_lcn >= 50.
    
    Drop weaker duplicates if keeping them would require the duplicate
    to be assigned an LCN > 399.
    
    The method addLcnToList() was reamed to addLcn() as a function
    nested in LCN.read().
    
    Also simplify it and make it more efficient.
    
    Rewrite writeBouquet() to simplify it. Also add a marker before
    "extra" services are added to the list (like HDMI In).
    
    Change the test for inserting markers so that the "Duplicates"
    marker appears in the correct place if a duplicate is assigned to
    LCN 300 (as can now happen with the new addLcn() code).
    
    Duplicate handling has only been tested on simulated data.

--- lib/python/Plugins/SystemPlugins/IniLCNScanner/plugin.py
+++ lib/python/Plugins/SystemPlugins/IniLCNScanner/plugin.py
@@ -16,6 +16,9 @@ import sys
 import xml.etree.cElementTree
 
 class LCN():
+	DUP_LCN_MIN = 350
+	DUP_LCN_MAX = 399
+
 	service_types_tv = service_types_tv_ref.toString()
 	service_types_radio = service_types_radio_ref.toString()
 	sr_marker_terr_lcn_tv = eServiceReference(eServiceReference.idDVB, eServiceReference.isMarker)
@@ -57,37 +60,6 @@ class LCN():
 				self.root = x
 				return
 
-	def addLcnToList(self, namespace, nid, tsid, sid, lcn, signal):
-		for x in self.lcnlist:
-			if x[0] == lcn and x[1] == namespace and x[2] == nid and x[3] == tsid and x[4] == sid and x[5] == signal:
-				return
-
-		if lcn == 0:
-			return
-
-		for i in range(0, len(self.lcnlist)):
-			if self.lcnlist[i][0] == lcn:
-				if self.lcnlist[i][5] > signal:
-					self.addLcnToList(namespace, nid, tsid, sid, lcn + 350, signal)
-				else:
-					znamespace = self.lcnlist[i][1]
-					znid = self.lcnlist[i][2]
-					ztsid = self.lcnlist[i][3]
-					zsid = self.lcnlist[i][4]
-					zsignal = self.lcnlist[i][5]
-					self.lcnlist[i][1] = namespace
-					self.lcnlist[i][2] = nid
-					self.lcnlist[i][3] = tsid
-					self.lcnlist[i][4] = sid
-					self.lcnlist[i][5] = signal
-					self.addLcnToList(znamespace, znid, ztsid, zsid, lcn + 350, zsignal)
-				return
-			elif self.lcnlist[i][0] > lcn:
-				self.lcnlist.insert(i, [lcn, namespace, nid, tsid, sid, signal])
-				return
-
-		self.lcnlist.append([lcn, namespace, nid, tsid, sid, signal])
-
 	def renumberLcn(self, range, rule):
 		tmp = range.split("-")
 		if len(tmp) != 2:
@@ -117,6 +89,33 @@ class LCN():
 			print "[LCNScanner]", e
 			return
 
+		lcn_next_dup_slot = self.DUP_LCN_MIN
+		lcnlookup = {}
+
+		def addLcn(lcndup, namespace, nid, tsid, sid, lcn, signal):
+			if lcn == 0:
+				return lcndup
+
+			newent = (lcn, namespace, nid, tsid, sid, signal)
+
+			if lcn in lcnlookup:
+				oldent = lcnlookup[lcn]
+				if oldent == newent:
+					return lcndup
+
+				if oldent[5] < signal:
+					lcnlookup[lcn] = newent
+					newent = oldent
+
+				while lcndup in lcnlookup and lcndup <= self.DUP_LCN_MAX:
+					lcndup += 1
+				if lcndup <= self.DUP_LCN_MAX:
+					lcnlookup[lcndup] = (lcndup,) + newent[1:6]
+					lcndup += 1
+			else:
+				lcnlookup[lcn] = newent
+			return lcndup
+
 		while True:
 			line = f.readline()
 			if line == "":
@@ -131,8 +130,11 @@ class LCN():
 			if len(tmp) != 6:
 				continue
 
-			self.addLcnToList(int(tmp[0], 16), int(tmp[1], 16), int(tmp[2], 16), int(tmp[3], 16), int(tmp[4]), int(tmp[5]))
+			lcn_next_dup_slot = addLcn(lcn_next_dup_slot, int(tmp[0], 16), int(tmp[1], 16), int(tmp[2], 16), int(tmp[3], 16), int(tmp[4]), int(tmp[5]))
+
 		f.close()
+
+		self.lcnlist = sorted(lcnlookup.itervalues(), key=lambda z: int(z[0]))
 
 		if self.root is not None:
 			for x in self.root:
@@ -170,56 +172,49 @@ class LCN():
 			print "[LCNScanner]", e
 			return
 
-		self.newlist = []
-		count = 0
-
-		for x in self.lcnlist:
-			count += 1
-			while x[0] != count:
-				self.newlist.append([count, 11111111, 11111, 111, 111, 111111])
-				count += 1
-			if x[0] == count:
-				self.newlist.append(x)
-
-		#for x in self.e2services:
-			#print "[LCNScanner]", " self.e2services:", x.toString()
-
-		#for x in self.newlist:
-			#print "[LCNScanner]", " NEW LIST LCN :", x
-
-		#print "[LCNScanner]", " New LIST LEN: " , len(self.newlist)
-
-		print "[LCN] writeTVBouquet write", "#NAME " + name_marker.getName() #ZZ
 		f.write("#NAME " + name_marker.getName() + "\n")
 		f.write("#SERVICE " + name_marker.toString() + "\n")
 		f.write("##DESCRIPTION " + name_marker.getName() + "\n")
-		for x in self.newlist:
-			if int(x[1]) == 11111111:
-				#print "[LCNScanner]", x[0], " Detected 111111111111 service"
-				f.write("#SERVICE " + self.sr_filler.toString() + "\n")
-				continue
 
-			if len(self.markers) > 0:
-				if x[0] > self.markers[0][0]:
+		if self.lcnlist:
+			# Extract (SID, TSID, ONID, NS) from lcnlist entry
+			def lcnKey(lcn_info):
+				return tuple(lcn_info[4:0:-1])
+
+			# Extract (SID, TSID, ONID, NS) from eServiceReference
+			def svcKey(svc):
+				return tuple([svc.getUnsignedData(i) for i in range(1, 5)])
+
+			svc_lookup = dict([(svcKey(svc), svc) for svc in self.e2services])
+
+			loc = 0
+			nxt_lcn = self.lcnlist[loc]
+			for lcn in range(1, self.lcnlist[-1][0] + 1):
+				marker = None
+				if nxt_lcn[0] == lcn:
+					if len(self.markers) > 0 and lcn >= self.markers[0][0]:
+						marker = self.markers.pop(0)
+					key = lcnKey(nxt_lcn)
+					if key in svc_lookup:
+						svc = svc_lookup[key]
+					else:
+						svc = self.sr_filler
+					loc += 1
+					if loc < len(self.lcnlist):
+						nxt_lcn = self.lcnlist[loc]
+				else:
+					svc = self.sr_filler
+				if marker:
 					f.write("#SERVICE " + self.sr_marker.toString() + "\n")
-					f.write("#DESCRIPTION ------- " + self.markers[0][1] + " -------\n")
-					self.markers.remove(self.markers[0])
-			refstr = "1:0:1:%x:%x:%x:%x:0:0:0:" % (x[4], x[3], x[2], x[1])  # temporary ref
-			refsplit = eServiceReference(refstr).toString().split(":")
-			added = False
-			for tref in self.e2services:
-				tmp = tref.toString().split(":")
-				if tmp[3] == refsplit[3] and tmp[4] == refsplit[4] and tmp[5] == refsplit[5] and tmp[6] == refsplit[6]:
-					f.write("#SERVICE " + tref.toString() + "\n")
-					added = True
-					break
-
-			if not added:  # no service found? something wrong? a log should be a good idea. Anyway we add an empty line so we keep the numeration
-				f.write("#SERVICE " + self.sr_filler.toString() + "\n")
+					f.write("#DESCRIPTION ------- " + marker[1] + " -------\n")
+				f.write("#SERVICE " + svc.toString() + "\n")
 
 		# Add extra services
-		for x in extras:
-			f.write("#SERVICE " + x.toString() + "\n")
+		if extras:
+			f.write("#SERVICE " + self.sr_marker.toString() + "\n")
+			f.write("#DESCRIPTION ------- Other services -------\n")
+			for x in extras:
+				f.write("#SERVICE " + x.toString() + "\n")
 
 		f.close()
 
