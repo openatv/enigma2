2d38494ee80bee7e318a38292e72808af0c5fa88
Captain <captain.onboard@web.de>
2015-09-16 22:12:54 +0200

    [LCNScanner] add LCN Bouget after scan

--- lib/python/Screens/ServiceScan.py
+++ lib/python/Screens/ServiceScan.py
@@ -35,6 +35,13 @@ class ServiceScan(Screen):
 	def ok(self):
 		print "ok"
 		if self["scan"].isDone():
+			try:
+				from Plugins.SystemPlugins.LCNScanner.plugin import LCNBuildHelper
+				lcn = LCNBuildHelper()
+				lcn.buildAfterScan()
+			except Exception, e:
+				print e
+
 			if self.currentInfobar.__class__.__name__ == "InfoBar":
 				selectedService = self["servicelist"].getCurrentSelection()
 				if selectedService and self.currentServiceList is not None:
