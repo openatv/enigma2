212929460a2c2e694f9385d66bc44ca366a04ea7
Captain <captain.onboard@web.de>
2015-09-17 17:33:11 +0200

    update satfinder to last signal finder

--- lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
+++ lib/python/Plugins/SystemPlugins/Satfinder/plugin.py
@@ -11,7 +11,7 @@ from Components.ActionMap import ActionMap
 from Components.NimManager import nimmanager, getConfigSatlist
 from Components.config import config, ConfigSelection, getConfigListEntry
 from Components.TuneTest import Tuner
-from Components.Converter.ChannelNumbers import channelnumbers
+from Tools.Transponder import getChannelNumber, channel2frequency
 
 class Satfinder(ScanSetup, ServiceScan):
 	def __init__(self, session):
@@ -27,17 +27,9 @@ class Satfinder(ScanSetup, ServiceScan):
 		self.satfinderTunerEntry = None
 		self.satEntry = None
 		self.typeOfInputEntry = None
-		self.frequencyEntry = None
-		self.polarizationEntry = None
-		self.symbolrateEntry = None
-		self.inversionEntry = None
-		self.rolloffEntry = None
-		self.pilotEntry = None
-		self.modulationEntry = None
-		self.fecEntry = None
 
 		ScanSetup.__init__(self, session)
-		self.setTitle(_("Satfinder"))
+		self.setTitle(_("Signal Finder"))
 		self["introduction"].setText(_("Press OK to scan"))
 		self["Frontend"] = FrontendStatus(frontend_source = lambda : self.frontend, update_interval = 100)
 
@@ -75,8 +67,6 @@ class Satfinder(ScanSetup, ServiceScan):
 					if not self.openFrontend():
 						self.frontend = None # in normal case this should not happen
 		self.tuner = Tuner(self.frontend)
-		self.updatePreDefTransponders()
-		self.createSetup()
 		self.retune(None)
 
 	def __onClose(self):
@@ -84,7 +74,9 @@ class Satfinder(ScanSetup, ServiceScan):
 
 	def newConfig(self):
 		cur = self["config"].getCurrent()
-		if cur == self.satfinderTunerEntry:
+		if cur in (self.typeOfTuningEntry, self.systemEntry, self.typeOfInputEntry):
+			self.createSetup()
+		elif cur == self.satfinderTunerEntry:
 			self.feid = int(self.satfinder_scan_nims.value)
 			self.createSetup()
 			self.prepareFrontend()
@@ -93,20 +85,9 @@ class Satfinder(ScanSetup, ServiceScan):
 				if self.session.nav.RecordTimer.isRecording():
 					msg += _("\nRecording in progress.")
 				self.session.open(MessageBox, msg, MessageBox.TYPE_ERROR)
-		else:
+		elif cur == self.satEntry:
 			self.createSetup()
-		if cur not in (
-			self.systemEntry,
-			self.satfinderTunerEntry,
-			self.frequencyEntry,
-			self.polarizationEntry,
-			self.symbolrateEntry,
-			self.inversionEntry,
-			self.rolloffEntry,
-			self.fecEntry,
-			self.pilotEntry,
-			self.modulationEntry
-			):
+		else:
 			self.retune(None)
 
 	def createSetup(self):
@@ -132,33 +113,21 @@ class Satfinder(ScanSetup, ServiceScan):
 				else:
 					# downgrade to dvb-s, in case a -s2 config was active
 					self.scan_sat.system.value = eDVBFrontendParametersSatellite.System_DVB_S
-				self.frequencyEntry = getConfigListEntry(_('Frequency'), self.scan_sat.frequency)
-				self.list.append(self.frequencyEntry)
-				self.polarizationEntry = getConfigListEntry(_('Polarization'), self.scan_sat.polarization)
-				self.list.append(self.polarizationEntry)
-				self.symbolrateEntry = (getConfigListEntry(_('Symbol rate'), self.scan_sat.symbolrate))
-				self.list.append(self.symbolrateEntry)
-				self.inversionEntry = getConfigListEntry(_('Inversion'), self.scan_sat.inversion)
-				self.list.append(self.inversionEntry)
-
+				self.list.append(getConfigListEntry(_('Frequency'), self.scan_sat.frequency))
+				self.list.append(getConfigListEntry(_('Polarization'), self.scan_sat.polarization))
+				self.list.append(getConfigListEntry(_('Symbol rate'), self.scan_sat.symbolrate))
+				self.list.append(getConfigListEntry(_('Inversion'), self.scan_sat.inversion))
 				if self.scan_sat.system.value == eDVBFrontendParametersSatellite.System_DVB_S:
-					self.fecEntry = getConfigListEntry(_("FEC"), self.scan_sat.fec)
-					self.list.append(self.fecEntry)
+					self.list.append(getConfigListEntry(_("FEC"), self.scan_sat.fec))
 				elif self.scan_sat.system.value == eDVBFrontendParametersSatellite.System_DVB_S2:
-					self.fecEntry = getConfigListEntry(_("FEC"), self.scan_sat.fec_s2)
-					self.list.append(self.fecEntry)
+					self.list.append(getConfigListEntry(_("FEC"), self.scan_sat.fec_s2))
 					self.modulationEntry = getConfigListEntry(_('Modulation'), self.scan_sat.modulation)
 					self.list.append(self.modulationEntry)
-					self.rolloffEntry = getConfigListEntry(_('Roll-off'), self.scan_sat.rolloff)
-					self.list.append(self.rolloffEntry)
-					self.pilotEntry = getConfigListEntry(_('Pilot'), self.scan_sat.pilot)
-					self.list.append(self.pilotEntry)
+					self.list.append(getConfigListEntry(_('Roll-off'), self.scan_sat.rolloff))
+					self.list.append(getConfigListEntry(_('Pilot'), self.scan_sat.pilot))
 			elif self.tuning_type.value == "predefined_transponder":
-				try:
-					self.list.append(getConfigListEntry(_("Transponder"), self.preDefTransponders))
-				except:
-					self.updatePreDefTransponders()
-					self.list.append(getConfigListEntry(_("Transponder"), self.preDefTransponders))
+				self.updatePreDefTransponders()
+				self.list.append(getConfigListEntry(_("Transponder"), self.preDefTransponders))
 		elif nimmanager.nim_slots[int(self.satfinder_scan_nims.value)].isCompatible("DVB-C"):
 			self.typeOfTuningEntry = getConfigListEntry(_('Tune'), self.tuning_type)
 			if config.Nims[self.feid].cable.scan_type.value != "provider" or len(nimmanager.getTranspondersCable(int(self.satfinder_scan_nims.value))) < 1: # only show 'predefined transponder' if in provider mode and transponders exist
@@ -194,13 +163,13 @@ class Satfinder(ScanSetup, ServiceScan):
 				else:
 					self.scan_input_as.value = self.scan_input_as.choices[0]
 				if self.ter_channel_input and self.scan_input_as.value == "channel":
-					channel = channelnumbers.getChannelNumber(self.scan_ter.frequency.value*1000, self.ter_tnumber)
+					channel = getChannelNumber(self.scan_ter.frequency.value*1000, self.ter_tnumber)
 					if channel:
 						self.scan_ter.channel.value = int(channel.replace("+","").replace("-",""))
 					self.list.append(getConfigListEntry(_("Channel"), self.scan_ter.channel))
 				else:
 					prev_val = self.scan_ter.frequency.value
-					self.scan_ter.frequency.value = channelnumbers.channel2frequency(self.scan_ter.channel.value, self.ter_tnumber)/1000
+					self.scan_ter.frequency.value = channel2frequency(self.scan_ter.channel.value, self.ter_tnumber)/1000
 					if self.scan_ter.frequency.value == 474000:
 						self.scan_ter.frequency.value = prev_val
 					self.list.append(getConfigListEntry(_("Frequency"), self.scan_ter.frequency))
@@ -218,6 +187,7 @@ class Satfinder(ScanSetup, ServiceScan):
 				self.scan_nims.value = self.satfinder_scan_nims.value
 				self.predefinedTerrTranspondersList()
 				self.list.append(getConfigListEntry(_('Transponder'), self.TerrestrialTransponders))
+		self.retune(None)
 		self["config"].list = self.list
 		self["config"].l.setList(self.list)
 
@@ -239,7 +209,7 @@ class Satfinder(ScanSetup, ServiceScan):
 			self.scan_ter.guard, self.scan_ter.hierarchy, self.scan_ter.plp_id,
 			self.scan_cab.frequency, self.scan_cab.inversion, self.scan_cab.symbolrate,
 			self.scan_cab.modulation, self.scan_cab.fec):
-			x.addNotifier(self.retuneTriggeredByAnEvent, initial_call = False)
+			x.addNotifier(self.retune, initial_call = False)
 
 		satfinder_nim_list = []
 		for n in nimmanager.nim_slots:
@@ -318,7 +288,7 @@ class Satfinder(ScanSetup, ServiceScan):
 			return self.retuneCab(configElement)
 		if self.initcomplete:
 			if self.scan_input_as.value == "channel":
-				frequency = channelnumbers.channel2frequency(self.scan_ter.channel.value, self.ter_tnumber)
+				frequency = channel2frequency(self.scan_ter.channel.value, self.ter_tnumber)
 			else:
 				frequency = self.scan_ter.frequency.value * 1000
 			if self.tuning_type.value == "single_transponder":
@@ -347,10 +317,6 @@ class Satfinder(ScanSetup, ServiceScan):
 					if self.initcomplete:
 						self.tuner.tuneTerr(transponder[1], transponder[9], transponder[2], transponder[4], transponder[5], transponder[3], transponder[7], transponder[6], transponder[8], transponder[10], transponder[11])
 					self.transponder = transponder
-
-	def retuneTriggeredByAnEvent(self, configElement):
-		self.createSetup()
-		self.retune(configElement)
 
 	def retune(self, configElement): # satellite
 		if not nimmanager.nim_slots[int(self.satfinder_scan_nims.value)].isCompatible("DVB-S"):
@@ -470,12 +436,12 @@ def SatfinderMain(session, close=None, **kwargs):
 
 def SatfinderStart(menuid, **kwargs):
 	if menuid == "scan":
-		return [(_("Satfinder"), SatfinderMain, "satfinder", 35, True)]
+		return [(_("Signal finder"), SatfinderMain, "satfinder", 35, True)]
 	else:
 		return []
 
 def Plugins(**kwargs):
 	if nimmanager.hasNimType("DVB-S") or nimmanager.hasNimType("DVB-T") or nimmanager.hasNimType("DVB-C"):
-		return PluginDescriptor(name=_("Satfinder"), description=_("Helps setting up your antenna"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc=SatfinderStart)
+		return PluginDescriptor(name=_("Signal finder"), description=_("Helps setting up your antenna"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc=SatfinderStart)
 	else:
 		return []
--- lib/python/Tools/Transponder.py
+++ lib/python/Tools/Transponder.py
@@ -1,6 +1,64 @@
 from enigma import eDVBFrontendParametersSatellite, eDVBFrontendParametersCable, eDVBFrontendParametersTerrestrial
 from Components.NimManager import nimmanager
 
+def orbpos(pos):
+	return pos > 3600 and "N/A" or "%d.%d\xc2\xb0%s" % (pos > 1800 and ((3600 - pos) / 10, (3600 - pos) % 10, "W") or (pos / 10, pos % 10, "E"))
+
+def getTunerDescription(nim):
+	try:
+		return nimmanager.getTerrestrialDescription(nim)
+	except:
+		print "[ChannelNumber] nimmanager.getTerrestrialDescription(nim) failed, nim:", nim
+	return ""
+
+def getMHz(frequency):
+	if str(frequency).endswith('MHz'):
+		return frequency.split()[0]
+	return (frequency+50000)/100000/10.
+
+def getChannelNumber(frequency, nim):
+	if nim == "DVB-T":
+		for n in nimmanager.nim_slots:
+			if n.isCompatible("DVB-T"):
+				nim = n.slot
+				break
+	f = int(getMHz(frequency))
+	descr = getTunerDescription(nim)
+	if "DVB-T" in descr:
+		if "Europe" in descr:
+			if 174 < f < 230: 	# III
+				d = (f + 1) % 7
+				return str(int(f - 174)/7 + 5) + (d < 3 and "-" or d > 4 and "+" or "")
+			elif 470 <= f < 863: 	# IV,V
+				d = (f + 2) % 8
+				return str(int(f - 470) / 8 + 21) + (d < 3.5 and "-" or d > 4.5 and "+" or "")
+		elif "Australia" in descr:
+			d = (f + 1) % 7
+			ds = (d < 3 and "-" or d > 4 and "+" or "")
+			if 174 < f < 202: 	# CH6-CH9
+				return str(int(f - 174)/7 + 6) + ds
+			elif 202 <= f < 209: 	# CH9A
+				return "9A" + ds
+			elif 209 <= f < 230: 	# CH10-CH12
+				return str(int(f - 209)/7 + 10) + ds
+			elif 526 < f < 820: 	# CH28-CH69
+				d = (f - 1) % 7
+				return str(int(f - 526)/7 + 28) + (d < 3 and "-" or d > 4 and "+" or "")
+	return ""
+
+def supportedChannels(nim):
+	descr = getTunerDescription(nim)
+	return "Europe" in descr and "DVB-T" in descr
+
+def channel2frequency(channel, nim):
+	descr = getTunerDescription(nim)
+	if "Europe" in descr and "DVB-T" in descr:
+		if 5 <= channel <= 12:
+			return (177500 + 7000*(channel- 5))*1000
+		elif 21 <= channel <= 69:
+			return (474000 + 8000*(channel-21))*1000
+	return 474000000
+
 def ConvertToHumanReadable(tp, tunertype = None):
 	ret = { }
 	if tunertype is None:
@@ -30,6 +88,7 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersSatellite.Modulation_QAM16 : "QAM16",
 			eDVBFrontendParametersSatellite.Modulation_8PSK : "8PSK"}.get(tp.get("modulation"))
 		ret["orbital_position"] = nimmanager.getSatName(int(tp.get("orbital_position")))
+		ret["orb_pos"] = orbpos(int(tp.get("orbital_position")))
 		ret["polarization"] = {
 			eDVBFrontendParametersSatellite.Polarisation_Horizontal : _("Horizontal"),
 			eDVBFrontendParametersSatellite.Polarisation_Vertical : _("Vertical"),
@@ -94,7 +153,7 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			6000000 : "6 MHz",
 			5000000 : "5 MHz",
 			1712000 : "1.712 MHz"}.get(tp.get("bandwidth"))
-		print 'bandwidth:',tp.get("bandwidth")
+		#print 'bandwidth:',tp.get("bandwidth")
 		ret["code_rate_lp"] = {
 			eDVBFrontendParametersTerrestrial.FEC_Auto : _("Auto"),
 			eDVBFrontendParametersTerrestrial.FEC_1_2 : "1/2",
@@ -104,7 +163,7 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersTerrestrial.FEC_6_7 : "6/7",
 			eDVBFrontendParametersTerrestrial.FEC_7_8 : "7/8",
 			eDVBFrontendParametersTerrestrial.FEC_8_9 : "8/9"}.get(tp.get("code_rate_lp"))
-		print 'code_rate_lp:',tp.get("code_rate_lp")
+		#print 'code_rate_lp:',tp.get("code_rate_lp")
 		ret["code_rate_hp"] = {
 			eDVBFrontendParametersTerrestrial.FEC_Auto : _("Auto"),
 			eDVBFrontendParametersTerrestrial.FEC_1_2 : "1/2",
@@ -114,14 +173,14 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersTerrestrial.FEC_6_7 : "6/7",
 			eDVBFrontendParametersTerrestrial.FEC_7_8 : "7/8",
 			eDVBFrontendParametersTerrestrial.FEC_8_9 : "8/9"}.get(tp.get("code_rate_hp"))
-		print 'code_rate_hp:',tp.get("code_rate_hp")
+		#print 'code_rate_hp:',tp.get("code_rate_hp")
 		ret["constellation"] = {
 			eDVBFrontendParametersTerrestrial.Modulation_Auto : _("Auto"),
 			eDVBFrontendParametersTerrestrial.Modulation_QPSK : "QPSK",
 			eDVBFrontendParametersTerrestrial.Modulation_QAM16 : "QAM16",
 			eDVBFrontendParametersTerrestrial.Modulation_QAM64 : "QAM64",
 			eDVBFrontendParametersTerrestrial.Modulation_QAM256 : "QAM256"}.get(tp.get("constellation"))
-		print 'constellation:',tp.get("constellation")
+		#print 'constellation:',tp.get("constellation")
 		ret["transmission_mode"] = {
 			eDVBFrontendParametersTerrestrial.TransmissionMode_Auto : _("Auto"),
 			eDVBFrontendParametersTerrestrial.TransmissionMode_1k : "1k",
@@ -130,7 +189,7 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersTerrestrial.TransmissionMode_8k : "8k",
 			eDVBFrontendParametersTerrestrial.TransmissionMode_16k : "16k",
 			eDVBFrontendParametersTerrestrial.TransmissionMode_32k : "32k"}.get(tp.get("transmission_mode"))
-		print 'transmission_mode:',tp.get("transmission_mode")
+		#print 'transmission_mode:',tp.get("transmission_mode")
 		ret["guard_interval"] = {
 			eDVBFrontendParametersTerrestrial.GuardInterval_Auto : _("Auto"),
 			eDVBFrontendParametersTerrestrial.GuardInterval_19_256 : "19/256",
@@ -140,26 +199,27 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersTerrestrial.GuardInterval_1_16 : "1/16",
 			eDVBFrontendParametersTerrestrial.GuardInterval_1_8 : "1/8",
 			eDVBFrontendParametersTerrestrial.GuardInterval_1_4 : "1/4"}.get(tp.get("guard_interval"))
-		print 'guard_interval:',tp.get("guard_interval")
+		#print 'guard_interval:',tp.get("guard_interval")
 		ret["hierarchy_information"] = {
 			eDVBFrontendParametersTerrestrial.Hierarchy_Auto : _("Auto"),
 			eDVBFrontendParametersTerrestrial.Hierarchy_None : _("None"),
 			eDVBFrontendParametersTerrestrial.Hierarchy_1 : "1",
 			eDVBFrontendParametersTerrestrial.Hierarchy_2 : "2",
 			eDVBFrontendParametersTerrestrial.Hierarchy_4 : "4"}.get(tp.get("hierarchy_information"))
-		print 'hierarchy_information:',tp.get("hierarchy_information")
+		#print 'hierarchy_information:',tp.get("hierarchy_information")
 		ret["inversion"] = {
 			eDVBFrontendParametersTerrestrial.Inversion_Unknown : _("Auto"),
 			eDVBFrontendParametersTerrestrial.Inversion_On : _("On"),
 			eDVBFrontendParametersTerrestrial.Inversion_Off : _("Off")}.get(tp.get("inversion"))
-		print 'inversion:',tp.get("inversion")
+		#print 'inversion:',tp.get("inversion")
 		ret["system"] = {
 			eDVBFrontendParametersTerrestrial.System_DVB_T_T2 : "DVB-T/T2",
 			eDVBFrontendParametersTerrestrial.System_DVB_T : "DVB-T",
 			eDVBFrontendParametersTerrestrial.System_DVB_T2 : "DVB-T2"}.get(tp.get("system"))
-		print 'system:',tp.get("system")
+#		print 'system:',tp.get("system")
 		ret["frequency"] = (tp.get("frequency") and ('%i MHz' % int(round(tp.get("frequency"), -6)/1000000))) or '0 MHz'
-		print 'frequency:',tp.get("frequency")
+#		print 'frequency:',tp.get("frequency")
+		ret["channel"] = _("CH%s") % getChannelNumber(tp.get("frequency"), "DVB-T")
 	elif tunertype == "ATSC":
 		ret["tuner_type"] = "ATSC"
 		ret["modulation"] = {
