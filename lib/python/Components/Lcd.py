40ea5950b09a838171de64745266c1b1464d4d7b
TwolDE <awhalley@waitrose.com>
2015-09-25 12:31:16 +0200

    fix defs for dimbright

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -299,7 +299,16 @@ def InitLcd():
 			("noscrolling", _("off"))])
 
 		def setLCDbright(configElement):
-			ilcd.setBright(configElement.value)
+			ilcd.setBright(configElement.value);
+
+		def setLCDstandbybright(configElement):
+			ilcd.setStandbyBright(configElement.value);
+
+		def setLCDdimbright(configElement):
+			ilcd.setDimBright(configElement.value);
+
+		def setLCDdimdelay(configElement):
+			ilcd.setDimDelay(configElement.value);
 
 		def setLCDcontrast(configElement):
 			ilcd.setContrast(configElement.value)
@@ -342,6 +351,21 @@ def InitLcd():
 		config.lcd.bright.addNotifier(setLCDbright)
 		config.lcd.bright.apply = lambda : setLCDbright(config.lcd.bright)
 		config.lcd.bright.callNotifiersOnSaveAndCancel = True
+
+		config.lcd.dimbright = ConfigSlider(default=standby_default, limits=(0, 10))
+		config.lcd.dimbright.addNotifier(setLCDdimbright);
+		config.lcd.dimbright.apply = lambda : setLCDdimbright(config.lcd.dimbright)
+		config.lcd.dimdelay = ConfigSelection(default = "0", choices = [
+			("5", "5 " + _("seconds")),
+			("10", "10 " + _("seconds")),
+			("15", "15 " + _("seconds")),
+			("20", "20 " + _("seconds")),
+			("30", "30 " + _("seconds")),
+			("60", "1 " + _("minute")),
+			("120", "2 " + _("minutes")),
+			("300", "5 " + _("minutes")),
+			("0", _("off"))])
+		config.lcd.dimdelay.addNotifier(setLCDdimdelay);
 
 		config.lcd.invert = ConfigYesNo(default=False)
 		config.lcd.invert.addNotifier(setLCDinverted)
