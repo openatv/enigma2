da7e13df0523e6b870f88b4943654a0f5dd22c4b
dvb-adenin <dvb-adenin@gmx.de>
2015-10-09 19:23:51 +0200

    [debug] new modul BugHunting.py     first version     provide just two methods at time: printCallSequence(deep) and printCallSequenceRawData(deep)     printCallSequence(2) placed in check() generates this debug output:     19:52:00.580 /usr/lib/enigma2/python/Components/TimerSanityCheck.py:20 check <-- record(RecordTimer.py:1197) <-- startInstantRecording(InfoBarGenerics.py:3683)

--- lib/python/Tools/RedirectOutput.py
+++ lib/python/Tools/RedirectOutput.py
@@ -10,14 +10,19 @@ class EnigmaOutput:
 		if isinstance(data, unicode):
 			data = data.encode("UTF-8")
 		if '\n' in data:
+			frame = sys._getframe(1)
+			filename = frame.f_code.co_filename
 			self.buf += data
-			filename = sys._getframe(1).f_code.co_filename
-			if '/usr/lib/enigma2/python/' in filename:
-				filename = filename.replace('/usr/lib/enigma2/python/', '')
-			elif '/git/' in filename:
-				filename = filename.split('/git/')[1]
-			ePythonOutput(filename, sys._getframe(1).f_lineno, sys._getframe(1).f_code.co_name, self.buf)
+			if 'BugHunting' in filename:
+				ePythonOutput('',0,'',self.buf)
+			else:
+				if '/usr/lib/enigma2/python/' in filename:
+					filename = filename.replace('/usr/lib/enigma2/python/', '')
+				elif '/git/' in filename:
+					filename = filename.split('/git/')[1]
+				ePythonOutput(filename, frame.f_lineno, frame.f_code.co_name, self.buf)
 			self.buf = ''
+
 		else:
 			self.buf += data
 
