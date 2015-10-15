7e34e13675cf68b4a19e8a313734c13c2ada1fd8
madie <madie1@gmx.de>
2015-10-15 22:19:27 +0200

    rm videoclipping from pluginmenu

--- lib/python/Plugins/SystemPlugins/VideoClippingSetup/plugin.py
+++ lib/python/Plugins/SystemPlugins/VideoClippingSetup/plugin.py
@@ -130,7 +130,6 @@ def Plugins(**kwargs):
 	from os import path
 	if path.exists("/proc/stb/vmpeg/0/clip_left"):
 		from Plugins.Plugin import PluginDescriptor
-		return [PluginDescriptor(name = "Video clipping setup", description = "clip overscan / letterbox borders", where = PluginDescriptor.WHERE_PLUGINMENU, fnc = main),
-					PluginDescriptor(name = "Video clipping setup", description = "", where = PluginDescriptor.WHERE_SESSIONSTART, fnc = startup),
+		return [PluginDescriptor(name = "Video clipping setup", description = "", where = PluginDescriptor.WHERE_SESSIONSTART, fnc = startup),
 					PluginDescriptor(name=_("Video clipping"), description=_("clip overscan / letterbox borders"), where = PluginDescriptor.WHERE_MENU, needsRestart = False, fnc=startMain)]
 	return []
