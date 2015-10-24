566d534aeb0f2c357f8478567440b04bd1dab561
littlesat <littlesat99@yahoo.com>
2015-10-24 13:31:02 +0200

    Add option to enable or disable the timer conflict detection
    When you use the remote fallback tuner to do recording, the timer conflict detection can't handle remote fallback tuners. This option allows you to turn off the timer confict detection

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -204,6 +204,7 @@ def InitUsageConfig():
 
 	config.usage.remote_fallback_enabled = ConfigYesNo(default = False)
 	config.usage.remote_fallback = ConfigText(default = "http://IP-ADRESS:8001", fixed_size = False)
+	config.usage.timer_sanity_check_enabled = ConfigYesNo(default = True);
 
 	dvbs_nims = [("-2", _("Disabled"))]
 	dvbt_nims = [("-2", _("Disabled"))]
