60a71e71b73f278631b4d22f57e66297b578d835
dvb-adenin <dvb-adenin@gmx.de>
2015-10-30 16:16:17 +0100

    [debug] fix missing traceback in log files

--- lib/python/Tools/RedirectOutput.py
+++ lib/python/Tools/RedirectOutput.py
@@ -1,30 +1,36 @@
 import sys
 from enigma import ePythonOutput
 
+buff=''
+
 class EnigmaOutput:
 	def __init__(self):
 		self.buf = ''
-		pass
+		self.isTraceback = False
 
 	def write(self, data):
 		if isinstance(data, unicode):
 			data = data.encode("UTF-8")
+		self.buf += data
 		if '\n' in data:
-			frame = sys._getframe(1)
-			filename = frame.f_code.co_filename
-			self.buf += data
-			if 'BugHunting' in filename:
-				ePythonOutput('',0,'',self.buf)
+			if not self.isTraceback and 'Traceback (most recent call last):\n' == self.buf:
+				self.isTraceback = True
+			if self.isTraceback == False:
+				frame = sys._getframe(1)
+				filename = frame.f_code.co_filename
+				if 'BugHunting' in filename:
+					ePythonOutput('',0,'',self.buf)
+				else:
+					if '/usr/lib/enigma2/python/' in filename:
+						filename = filename.replace('/usr/lib/enigma2/python/', '')
+					elif '/git/' in filename:
+						filename = filename.split('/git/')[1]
+					ePythonOutput(filename, frame.f_lineno, frame.f_code.co_name, self.buf)
 			else:
-				if '/usr/lib/enigma2/python/' in filename:
-					filename = filename.replace('/usr/lib/enigma2/python/', '')
-				elif '/git/' in filename:
-					filename = filename.split('/git/')[1]
-				ePythonOutput(filename, frame.f_lineno, frame.f_code.co_name, self.buf)
+				ePythonOutput('',0,'',self.buf)
+			if self.isTraceback and self.buf[0] != ' ' and 'Traceback (most recent call last):\n' != self.buf:
+				self.isTraceback = False
 			self.buf = ''
-
-		else:
-			self.buf += data
 
 	def flush(self):
 		pass
