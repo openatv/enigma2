5cfaa8d34e085db62f94bc2550ffefc39fea6081
Athanasios Oikonomou <athoik@gmail.com>
2015-11-09 00:34:38 +0200

    CA: When descramling iterate demuxes in ascending order
    We missed that on commit 4c12411

--- lib/dvb/dvb.cpp
+++ lib/dvb/dvb.cpp
@@ -1096,7 +1096,7 @@ RESULT eDVBResourceManager::allocateDemux(eDVBRegisteredFrontend *fe, ePtr<eDVBA
 					}
 				}
 			}
-			if (fe)
+			if (fe || (cap & iDVBChannel::capDecode))
 			{
 				++i;
 			}
