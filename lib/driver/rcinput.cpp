67c31a62822485c106e2668b937933d570398a0b
Andrew Blackburn <andyblac@icloud.com>
2015-11-09 09:15:42 +0000

    [rcinput] tweak debug.

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -19,6 +19,8 @@ void eRCDeviceInputDev::handleCode(long rccode)
 
 	if (ev->type != EV_KEY)
 		return;
+		
+	eDebug("[eInputDeviceInit] %x %x %x", ev->value, ev->code, ev->type);
 
 	int km = iskeyboard ? input->getKeyboardMode() : eRCInput::kmNone;
 
@@ -192,7 +194,7 @@ public:
 				return;
 			}
 		}
-		eDebug("Remove '%s', not found", filename);
+		eDebug("[eInputDeviceInit] Remove '%s', not found", filename);
 	}
 
 	void addAll(void)
