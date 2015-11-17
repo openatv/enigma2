f59fc3101e28a39851ae0779dc94078f923949e5
Athanasios Oikonomou <athoik@gmail.com>
2015-11-17 19:40:22 +0200

    ServiceMP3: send evUpdatedInfo only the first time bitrate changes
    Send evUpdatedInfo only the first time bitrate changes in order
    to reduce events. This is required with GStreamer >= 1.6.0.
    
    More info: http://forums.openpli.org/topic/29501-gstreamer-10/page-96
    
    (cherry picked from commit a4c053e67632855e94d58ff9798ea9fea5d27edd)
    Signed-off-by: Erik Slagter <erik@openpli.org>

--- lib/service/servicemp3.h
+++ lib/service/servicemp3.h
@@ -322,6 +322,7 @@ private:
 	int m_state;
 	GstElement *m_gst_playbin, *audioSink, *videoSink;
 	GstTagList *m_stream_tags;
+	guint m_bitrate;
 
 	eFixedMessagePump<ePtr<GstMessageContainer> > m_pump;
 
