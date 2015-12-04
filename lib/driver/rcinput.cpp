db2376ab6e38e4723fc7fb32d6b74dfb313e35d5
Mirakels <mirakels@openpli.org>
2015-12-05 00:35:49 +0100

    rcinput: cosmetic debug

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -205,7 +205,8 @@ public:
 		{
 			char filename[32];
 			sprintf(filename, "/dev/input/event%d", i);
-			if (::access(filename, R_OK) < 0) break;
+			if (::access(filename, R_OK) < 0)
+				break;
 			add(filename);
 			++i;
 		}
@@ -223,6 +224,7 @@ public:
 
 	void add(const char* filename)
 	{
+		eDebug("[eInputDeviceInit] adding device %s", filename);
 		eRCInputEventDriver *p = new eRCInputEventDriver(filename);
 		items.push_back(new element(filename, p, new eRCDeviceInputDev(p, consoleFd)));
 	}
