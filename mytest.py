fa26efd157d0a5fa01033bd6eabcc051d3e6f51d
skl <skl@skl.net>
2015-12-11 17:50:31 +0100

    Remove sh4 showiframe hack

--- mytest.py
+++ mytest.py
@@ -621,9 +621,6 @@ def runScreenTest():
 	if not RestoreSettings:
 		configfile.save()
 
-	# kill showiframe if it is running (sh4 hack...)
-	os.system("killall -9 showiframe")
-
 	runReactor()
 
 	print "[mytest.py] normal shutdown"
