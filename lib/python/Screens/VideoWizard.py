9a3339a22b8705b56929f8f0e5755ff8d19b3aef
rossi2000 <rossi_2000@hotmail.com>
2015-12-16 18:44:16 +0000

    VideoMode: add 2160p mode if hardware supports it Add 2160p video mode to VideoWizard.

--- lib/python/Screens/VideoWizard.py
+++ lib/python/Screens/VideoWizard.py
@@ -145,7 +145,7 @@ class VideoWizard(WizardLanguage, Rc):
 	def modeSelect(self, mode):
 		ratesList = self.listRates(mode)
 		print "[VideoWizard] ratesList:", ratesList
-		if self.port == "HDMI" and mode in ("720p", "1080i", "1080p"):
+		if self.port == "HDMI" and mode in ("720p", "1080i", "1080p", "2160p"):
 			self.rate = "multi"
 			self.hw.setMode(port = self.port, mode = mode, rate = "multi")
 		else:
