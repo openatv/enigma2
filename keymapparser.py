2ad628299c45d48f43db81326fe16275c16bd70e
Mirakels <mirakels@openpli.org>
2015-12-18 13:28:05 +0100

    actionmap: Implement key translations
    Some remotes lack support for certian buttons or have buttons
    with different key codes. This translation feature allows to
    translate specific keys into a different one reducing to need
    to create bloated keymaps to map several buttons to the same function.
    
    Some remotes are very small and lack certain keys. Using a toggle
    feature based on a specific button, buttons can be multi functional
    
    The definitions are like a keymap.xml and the translations can be added
    to the default keymap.xml. But to keep keymap.xml clean and to let it serve
    as a standard keymap, enigma2 will look for a keytranslation.xml file.
    In is just like a keymap.xml but can be used for STB specific mappings
    and translations.
    
    An example keytranslation.xml in which the number keys are mapped
    to specific other keys is shown below.  The toggle="1" means that
    translation will only be done after the toggle key has been pressed. The
    toggle element defines the toggle button, in this case the BACKSPACE key.
    key definitions without the toggle= attribute (or with a value not equal
    to "1") will always be translated regardless the toggle state.
    
    The device name is taken from the /dev/input identifications. Check your
    enigma2 log to find the proper names.
    
    <keymap>
          <translate>
                  <device name="Small remote device name">
                          <toggle from="KEY_BACKSPACE"/>
                          <key from="KEY_1" to="KEY_RED" toggle="1"/>
                          <key from="KEY_2" to="KEY_GREEN" toggle="1"/>
                          <key from="KEY_3" to="KEY_YELLOW" toggle="1"/>
                          <key from="KEY_4" to="KEY_BLUE" toggle="1"/>
                          <key from="KEY_5" to="KEY_PREVIOUS" toggle="1"/>
                          <key from="KEY_6" to="KEY_NEXT" toggle="1"/>
                          <key from="KEY_7" to="KEY_REWIND" toggle="1"/>
                          <key from="KEY_8" to="KEY_STOP" toggle="1"/>
                          <key from="KEY_9" to="KEY_FASTFORWARD" toggle="1"/>
                          <key from="KEY_0" to="KEY_PLAYPAUSE" toggle="1"/>
    
                          <key from="KEY_F7" to="KEY_MENU"/>
                          <key from="KEY_F1" to="KEY_VIDEO"/>
                          <key from="KEY_HOME" to="KEY_INFO"/>
                          <key from="KEY_BACK" to="KEY_EXIT"/>
                          <key from="KEY_F2" to="KEY_EPG"/>
                          <key from="KEY_ENTER" to="KEY_OK"/>
                  </device>
          </translate>
          <translate>
                  <device name="dreambox advanced remote control (native)">
                          <key from="KEY_PLAY" to="KEY_PLAYPAUSE"/>
                  </device>
          </translate>
    </keymap>

--- keymapparser.py
+++ keymapparser.py
@@ -13,6 +13,25 @@ class KeymapError(Exception):
     def __str__(self):
         return self.msg
 
+def getKeyId(id):
+	if len(id) == 1:
+		keyid = ord(id) | 0x8000
+	elif id[0] == '\\':
+		if id[1] == 'x':
+			keyid = int(id[2:], 0x10) | 0x8000
+		elif id[1] == 'd':
+			keyid = int(id[2:]) | 0x8000
+		else:
+			raise KeymapError("[keymapparser] key id '" + str(id) + "' is neither hex nor dec")
+	else:
+		try:
+			keyid = KEYIDS[id]
+		except:
+			raise KeymapError("[keymapparser] key id '" + str(id) + "' is illegal")
+
+	return keyid
+
+
 def parseKeys(context, filename, actionmap, device, keys):
 	for x in keys.findall("key"):
 		get_attr = x.attrib.get
@@ -24,49 +43,66 @@ def parseKeys(context, filename, actionmap, device, keys):
 
 		flags = sum(map(flag_ascii_to_id, flags))
 
-		assert mapto, "%s: must specify mapto in context %s, id '%s'" % (filename, context, id)
-		assert id, "%s: must specify id in context %s, mapto '%s'" % (filename, context, mapto)
-		assert flags, "%s: must specify at least one flag in context %s, id '%s'" % (filename, context, id)
+		assert mapto, "[keymapparser] %s: must specify mapto in context %s, id '%s'" % (filename, context, id)
+		assert id, "[keymapparser] %s: must specify id in context %s, mapto '%s'" % (filename, context, mapto)
+		assert flags, "[keymapparser] %s: must specify at least one flag in context %s, id '%s'" % (filename, context, id)
 
-		if len(id) == 1:
-			keyid = ord(id) | 0x8000
-		elif id[0] == '\\':
-			if id[1] == 'x':
-				keyid = int(id[2:], 0x10) | 0x8000
-			elif id[1] == 'd':
-				keyid = int(id[2:]) | 0x8000
-			else:
-				raise KeymapError("key id '" + str(id) + "' is neither hex nor dec")
-		else:
-			try:
-				keyid = KEYIDS[id]
-			except:
-				raise KeymapError("key id '" + str(id) + "' is illegal")
-#				print context + "::" + mapto + " -> " + device + "." + hex(keyid)
+		keyid = getKeyId(id)
+#				print "[keymapparser] " + context + "::" + mapto + " -> " + device + "." + hex(keyid)
 		actionmap.bindKey(filename, device, keyid, flags, context, mapto)
 		addKeyBinding(filename, keyid, context, mapto, flags)
+
+
+def parseTrans(filename, actionmap, device, keys):
+	for x in keys.findall("toggle"):
+		get_attr = x.attrib.get
+		toggle_key = get_attr("from")
+		toggle_key = getKeyId(toggle_key)
+                actionmap.bindToggle(filename, device, toggle_key)
+
+	for x in keys.findall("key"):
+		get_attr = x.attrib.get
+		keyin = get_attr("from")
+		keyout = get_attr("to")
+		toggle = get_attr("toggle") or "0"
+		assert keyin, "[keymapparser] %s: must specify key to translate from '%s'" % (filename, keyin)
+		assert keyout, "[keymapparser] %s: must specify key to translate to '%s'" % (filename, keyout)
+
+		keyin  = getKeyId(keyin)
+		keyout = getKeyId(keyout)
+		toggle = int(toggle)
+                actionmap.bindTranslation(filename, device, keyin, keyout, toggle)
+
 
 def readKeymap(filename):
 	p = enigma.eActionMap.getInstance()
 	assert p
 
-	source = open(filename)
+	try:
+		source = open(filename)
+	except:
+		print "[keymapparser] keymap file " + filename + " not found"
+		return
 
 	try:
 		dom = xml.etree.cElementTree.parse(source)
 	except:
-		raise KeymapError("keymap %s not well-formed." % filename)
+		raise KeymapError("[keymapparser] keymap %s not well-formed." % filename)
 
 	keymap = dom.getroot()
 
 	for cmap in keymap.findall("map"):
 		context = cmap.attrib.get("context")
-		assert context, "map must have context"
+		assert context, "[keymapparser] map must have context"
 
 		parseKeys(context, filename, p, "generic", cmap)
 
 		for device in cmap.findall("device"):
 			parseKeys(context, filename, p, device.attrib.get("name"), device)
+
+	for ctrans in keymap.findall("translate"):
+		for device in ctrans.findall("device"):
+			parseTrans(filename, p, device.attrib.get("name"), device)
 
 def removeKeymap(filename):
 	p = enigma.eActionMap.getInstance()
