865f9e70b440784434e745551b3a63de983691fd
Erik Slagter <erik@openpli.org>
2016-01-03 12:57:02 +0100

    HEVC: Add HEVC/h.265 support. This is based on original VU+ code.

--- lib/dvb/tstools.cpp
+++ lib/dvb/tstools.cpp
@@ -856,7 +856,7 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 		}
 		data = ((unsigned int)longdata);
 	}
-	while (((data & 0xFF) != 0x09) && ((data & 0xFF) != 0x00) && ((data & 0x7E) != 0x46)); /* next frame */
+	while (((data & 0xff) != 0x09) && ((data & 0xff) != 0x00) && ((data & 0x7E) != 0x46)); /* next frame */
 
 	if (is_mpeg2)
 	{
