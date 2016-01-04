57585872dde35955426e1e4c74e5d1612e50a48c
IanSav <IS.OzPVR@gmail.com>
2016-01-04 08:07:01 +0000

    [ChoiceList.py] Use ChoiceList font and skin parameters rather than static numbers
    This change is to address an issue where the ChoiceBox screen does not adjust its size to account for a change in the underlying ChoiceList font alias.  Also the skin ChoiceBox screen width was ignored in preparing the layout.
    
    - Use the ChoiceList font as a basis for the layout
    
    - Adjust formatting to use skin.py (hard coded) or skin based ChoiceList font parameters
    
    - Parametrise settings to optimise code
    
    - Use current screen width rather that a hard coded number
    
    - Optimise and enhance expandable icon image location and size

--- lib/python/Components/ChoiceList.py
+++ lib/python/Components/ChoiceList.py
@@ -1,40 +1,61 @@
 from MenuList import MenuList
 from Tools.Directories import SCOPE_ACTIVE_SKIN, resolveFilename
-from enigma import RT_HALIGN_LEFT, eListboxPythonMultiContent, gFont, getDesktop
+from enigma import RT_HALIGN_LEFT, RT_VALIGN_CENTER, eListboxPythonMultiContent, gFont, getDesktop
 from Tools.LoadPixmap import LoadPixmap
 from Tools.Directories import fileExists
 import skin
 
 def ChoiceEntryComponent(key="", text=None):
 	screenwidth = getDesktop(0).size().width()
+	font = skin.fonts["ChoiceList"]
+	top = 0
+	if screenwidth:
+		if screenwidth == 1920:
+			left = 100
+			height = 50
+		else:
+			left = 50
+			height = 25
+		width = screenwidth
+	else:
+		left = 50
+		width = 720
+		height = 25
+	if font:
+		if font[2] > height:
+			height = font[2]
+		top = int((height - font[2]) / 2)
 	if not text: text = ["--"]
 	res = [ text ]
 	if text[0] == "--":
-		if screenwidth and screenwidth == 1920:
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, 0, 00, 900, 50, 0, RT_HALIGN_LEFT, "-"*200))
-		else:
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, 0, 00, 800, 25, 0, RT_HALIGN_LEFT, "-"*200))
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, 0, 0, width, height, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, "-"*200))
 	else:
-		if screenwidth and screenwidth == 1920:
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, 100, 5, 900, 50, 0, RT_HALIGN_LEFT, text[0]))
-		else:
-			res.append((eListboxPythonMultiContent.TYPE_TEXT, 45, 00, 800, 25, 0, RT_HALIGN_LEFT, text[0]))
-		
+		res.append((eListboxPythonMultiContent.TYPE_TEXT, left, top, width, height, 0, RT_HALIGN_LEFT | RT_VALIGN_CENTER, text[0]))
 		if key:
+			if screenwidth and screenwidth == 1920:
+				left = 10
+				width = 48
+				size = 48
+			else:
+				left = 5;
+				width = 35
+				size = 25
+			top = int((height - size) / 2)
 			if key == "expandable":
+				height = size
 				pngfile = resolveFilename(SCOPE_ACTIVE_SKIN, "icons/expandable.png")
 			elif key == "expanded":
+				height = size + top
 				pngfile = resolveFilename(SCOPE_ACTIVE_SKIN, "icons/expanded.png")
 			elif key == "verticalline":
+				top = 0
 				pngfile = resolveFilename(SCOPE_ACTIVE_SKIN, "icons/verticalline.png")
 			else:
+				height = size
 				pngfile = resolveFilename(SCOPE_ACTIVE_SKIN, "buttons/key_%s.png" % key)
 			if fileExists(pngfile):
 				png = LoadPixmap(pngfile)
-				if screenwidth and screenwidth == 1920:
-					res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, 10, 5, 63, 48, png))
-				else:
-					res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, 5, 0, 35, 25, png))
+				res.append((eListboxPythonMultiContent.TYPE_PIXMAP_ALPHABLEND, left, top, width, height, png))
 	return res
 
 class ChoiceList(MenuList):
