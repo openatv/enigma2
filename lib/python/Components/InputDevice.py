608fc3fd9771c2c43dabfc90a8443ac1b85da82b
Athanasios Oikonomou <athoik@gmail.com>
2016-01-07 09:44:34 +0200

    InputDevice: Do not force default RC
    By default Enigma2 has default RC value 0 and that changes only
    by user.
    
    The commit 10d3a8108688f2e387bed06924a77ee0fdcb6344 forced to
    write default RC always,
    
    So do not force default RC unless config changed by user.
    
    (cherry picked from commit 1b8f7e89e62d0955a446c52f77cdc71cf800531d)
    Signed-off-by: Erik Slagter <erik@openpli.org>

--- lib/python/Components/InputDevice.py
+++ lib/python/Components/InputDevice.py
@@ -200,7 +200,8 @@ class RcTypeControl():
 		if SystemInfo["RcTypeChangable"] and os.path.exists('/proc/stb/info/boxtype'):
 			self.isSupported = True
 			self.boxType = open('/proc/stb/info/boxtype', 'r').read().strip()
-			self.writeRcType(config.plugins.remotecontroltype.rctype.value)
+			if config.plugins.remotecontroltype.rctype.value != 0:
+				self.writeRcType(config.plugins.remotecontroltype.rctype.value)
 		else:
 			self.isSupported = False
 		if getBoxType().startswith('gb'):
