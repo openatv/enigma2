03d39ca3c5ea088409483575358784333720a4f2
Mirakels <mirakels@openpli.org>
2016-01-13 23:27:25 +0100

    cosmetic eDebugNoNewLine fixes
    issues were only in inactive code...

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -24,8 +24,8 @@
 		else \
 		{ \
 			eDebugNoNewLineStart("SIMULATE:"); \
-			eDebugNoNewline(x); \
-			eDebugNoNewline("\n"); \
+			eDebugNoNewLine(x); \
+			eDebugNoNewLine("\n"); \
 		}
 #endif
 
