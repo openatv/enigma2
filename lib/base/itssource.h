9b1a620e2f1415f0fd47853e8fd4b94d8249824a
Athanasios Oikonomou <athoik@gmail.com>
2016-01-24 12:37:32 +0200

    iTsSource: Change  packetSize to unsigned int
    Changing packetSize to unsigned int prevents warnings about sign compare:
    
    .../lib/base/httpstream.cpp:257:40: warning: comparison between signed and unsigned integer expressions [-Werror=sign-compare]
       if (partialPktSz > 0 && partialPktSz < packetSize)
                                            ^
    Signed-off-by: Mike Looijmans <milo@openpli.org>

--- lib/base/itssource.h
+++ lib/base/itssource.h
@@ -6,10 +6,10 @@
 class iTsSource: public iObject
 {
 protected:
-	int packetSize;
+	unsigned int packetSize;
 
 public:
-	iTsSource(int packetsize = 188) : packetSize(packetsize) {}
+	iTsSource(unsigned int packetsize = 188) : packetSize(packetsize) {}
 
 	/* NOTE: you must be able to handle short reads! */
 	virtual ssize_t read(off_t offset, void *buf, size_t count)=0; /* NOTE: this is what you in normal case have to use!! */
@@ -20,7 +20,7 @@ public:
 	virtual off_t offset() = 0;
 	virtual bool isStream() { return false; }
 	virtual int reconnect() { return 0; }
-	int getPacketSize() const { return packetSize; }
+	unsigned int getPacketSize() const { return packetSize; }
 };
 
 #endif
