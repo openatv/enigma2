63e1f978bbb50be297012ec3ad14736588b8fd79
madie <madie1@gmx.de>
2016-01-24 12:47:28 +0100

    [hotkey] add cccaminfo

--- lib/python/Screens/ButtonSetup.py
+++ lib/python/Screens/ButtonSetup.py
@@ -208,6 +208,7 @@ def getButtonSetupFunctions():
 	ButtonSetupFunctions.append((_("Subtitles Settings"), "Setup/subtitlesetup", "Setup"))
 	ButtonSetupFunctions.append((_("Language"), "Module/Screens.LanguageSelection/LanguageSelection", "Setup"))
 	ButtonSetupFunctions.append((_("OscamInfo Mainmenu"), "Module/Screens.OScamInfo/OscamInfoMenu", "Plugins"))
+	ButtonSetupFunctions.append((_("CCcamInfo Mainmenu"), "Module/Screens.CCcamInfo/CCcamInfoMain", "Plugins"))
 	if os.path.isdir("/etc/ppanels"):
 		for x in [x for x in os.listdir("/etc/ppanels") if x.endswith(".xml")]:
 			x = x[:-4]
