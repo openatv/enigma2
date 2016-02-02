dbc9e7a9715c4fbcd1d510f282e58937e55fcdcc
littlesat <littlesat99@yahoo.com>
2016-02-02 01:10:40 +0100

    Solve issue when adding alternatives to previous last service
    When you want to add alternatives to the previous last service within a
    userbouquet all services were loaded with all marked. This issue at least
    solves this topic.
    
    Please note I'm not sure if this is the real solution. So it might be we
    have to look to the real issue later on. But at least for now this issue
    is solved

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1101,8 +1101,9 @@ class ChannelSelectionEdit:
 				new_title += ' ' + _("[favourite edit]")
 		self.setTitle(new_title)
 		self.__marked = self.servicelist.getRootServices()
-		for x in self.__marked:
-			self.servicelist.addMarked(eServiceReference(x))
+		if type == EDIT_ALTERNATIVES:
+			for x in self.__marked:
+				self.servicelist.addMarked(eServiceReference(x))
 		self["Service"].editmode = True
 
 	def endMarkedEdit(self, abort):
