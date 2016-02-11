12396cb769aabb8bd6ea4b52c18ffd7dd5792c36
Erik Slagter <erik@openpli.org>
2016-02-11 18:50:36 +0100

    Add new info screen "Streaming clients info".

---
+++ lib/python/Screens/StreamingClientsInfo.py
@@ -0,0 +1,22 @@
+from Screen import Screen
+from Components.ActionMap import ActionMap
+from Components.ScrollLabel import ScrollLabel
+from Components.Converter.ClientsStreaming import ClientsStreaming
+import skin
+
+class StreamingClientsInfo(Screen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		self.setTitle(_("StreamingClientsInfo"))
+                clients = ClientsStreaming("INFO_RESOLVE")
+                text = clients.getText()
+
+		self["ScrollLabel"] = ScrollLabel(text)
+
+		self["actions"] = ActionMap(["ColorActions", "SetupActions", "DirectionActions"],
+		{
+			"cancel": self.close,
+			"ok": self.close,
+			"up": self["ScrollLabel"].pageUp,
+			"down": self["ScrollLabel"].pageDown
+		})
