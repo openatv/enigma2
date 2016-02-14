26dab757f5047557f20f508acf9adbded23f4151
rossi2000 <rossi_2000@hotmail.com>
2016-02-14 00:14:00 +0000

    update enigma2.cpp debug

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -226,16 +226,16 @@ int main(int argc, char **argv)
 	gst_init(&argc, &argv);
 
 	// set pythonpath if unset
-	setenv("PYTHONPATH", eEnv::resolve("${libdir}/enigma2/python").c_str(), 0);
-	printf("PYTHONPATH: %s\n", getenv("PYTHONPATH"));
-	printf("DVB_API_VERSION %d DVB_API_VERSION_MINOR %d\n", DVB_API_VERSION, DVB_API_VERSION_MINOR);
+	setenv("[Enigma2] PYTHONPATH", eEnv::resolve("${libdir}/enigma2/python").c_str(), 0);
+	printf("[Enigma2] PYTHONPATH: %s\n", getenv("PYTHONPATH"));
+	printf("[Enigma2] DVB_API_VERSION %d DVB_API_VERSION_MINOR %d\n", DVB_API_VERSION, DVB_API_VERSION_MINOR);
 
 	// get enigma2 debug level
 	debugLvl = getenv("ENIGMA_DEBUG_LVL") ? atoi(getenv("ENIGMA_DEBUG_LVL")) : 4;
 	if (debugLvl < 0)
 		debugLvl = 0;
 	debugTag = getenv("ENIGMA_DEBUG_TAG");
-	printf("ENIGMA2_DEBUG settings: Level=%d, Tag=%s\n", debugLvl, debugTag ? debugTag : "");
+	printf("[Enigma2] ENIGMA2_DEBUG settings: Level=%d, Tag=%s\n", debugLvl, debugTag ? debugTag : "");
 
 	bsodLogInit();
 
