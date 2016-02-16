3ca7eb70e0233251d5855ec8a7767c26f450f486
undertaker <spichala@gmx.de>
2016-02-16 23:23:56 +0100

    add Screen and extra_info

--- lib/python/Screens/StreamingClientsInfo.py
+++ lib/python/Screens/StreamingClientsInfo.py
@@ -1,23 +1,30 @@
 from Screen import Screen
 from Components.ActionMap import ActionMap
-from Components.ScrollLabel import ScrollLabel
 from Components.Converter.ClientsStreaming import ClientsStreaming
 import skin
-
+import gettext
+from Components.Sources.StaticText import StaticText
 
 class StreamingClientsInfo(Screen):
-	def __init__(self, session):
-		Screen.__init__(self, session)
-		self.setTitle(_("Streaming clients info"))
-		clients = ClientsStreaming("INFO_RESOLVE")
-		text = clients.getText()
-
-		self["ScrollLabel"] = ScrollLabel(text or _("No stream clients"))
-
-		self["actions"] = ActionMap(["ColorActions", "SetupActions", "DirectionActions"],
-		{
-			"cancel": self.close,
-			"ok": self.close,
-			"up": self["ScrollLabel"].pageUp,
-			"down": self["ScrollLabel"].pageDown
-		})
+		skin ="""<screen name="StreamingClientsInfo" position="0,0" size="1280,720" flags="wfNoBorder" backgroundColor="#90000000">
+	<eLabel position="center,117" zPosition="-2" size="600,500" backgroundColor="layer-a-background" />
+	<widget source="Title" render="Label" position="center,126" size="580,44" font="Regular; 35" transparent="1" valign="top" zPosition="0" backgroundColor="layer-b-background" halign="center" />
+	<widget source="total" render="Label" position="center,174" size="580,50" zPosition="1" font="Regular; 22" halign="left" backgroundColor="layer-a-background" transparent="1" valign="center" />
+	<widget source="liste" render="Label" position="center,234" size="580,370" zPosition="1" transparent="1" noWrap="1" font="Regular; 20" valign="top" />
+</screen>"""
+		def __init__(self, session):
+			Screen.__init__(self, session)
+			self.setTitle(_("Streaming clients info"))
+			if ClientsStreaming("NUMBER").getText() == "0":
+				self["total"] = StaticText( _("No streaming Channel from this STB at this moment") )
+				text = ""
+			else:
+				self["total"] = StaticText( _("Total Clients streaming: ") + ClientsStreaming("NUMBER").getText())
+				text =  ClientsStreaming("EXTRA_INFO").getText()
+			
+			self["liste"] = StaticText(text)
+			self["actions"] = ActionMap(["ColorActions", "SetupActions", "DirectionActions"],
+			{
+				"cancel": self.close,
+				"ok": self.close
+			})
