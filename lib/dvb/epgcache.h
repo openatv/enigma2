8de4f1bb4f18dda34f171739c95bde3445a6476b
Cofybreak <Cofybreak@users.noreply.github.com>
2016-03-02 16:05:09 +0200

    Update epgcache.h

--- lib/dvb/epgcache.h
+++ lib/dvb/epgcache.h
@@ -258,7 +258,7 @@ private:
 	unsigned int historySeconds;
 
 	std::vector<int> onid_blacklist;
-	std::map<std::string,int> customeitpid;
+	std::map<std::string,int> customeitpids;
 	eventCache eventDB;
 	updateMap channelLastUpdated;
 	std::string m_filename;
