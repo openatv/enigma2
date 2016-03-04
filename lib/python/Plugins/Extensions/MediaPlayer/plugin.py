aa983658a443b26df88ae55663cae50b3dda12b6
Dima73 <Dima-73@inbox.lv>
2016-03-04 19:40:28 +0200

    MediaPlayer: fix zap to old service
    getCurrentlyPlayingServiceReference -->
    getCurrentlyPlayingServiceOrGroup

--- lib/python/Plugins/Extensions/MediaPlayer/plugin.py
+++ lib/python/Plugins/Extensions/MediaPlayer/plugin.py
@@ -110,7 +110,7 @@ class MediaPlayer(Screen, InfoBarBase, InfoBarScreenSaver, InfoBarSeek, InfoBarA
 		InfoBarSubtitleSupport.__init__(self)
 		HelpableScreen.__init__(self)
 		self.summary = None
-		self.oldService = self.session.nav.getCurrentlyPlayingServiceReference()
+		self.oldService = self.session.nav.getCurrentlyPlayingServiceOrGroup()
 		self.session.nav.stopService()
 
 		self.setTitle(_("Media player"))
