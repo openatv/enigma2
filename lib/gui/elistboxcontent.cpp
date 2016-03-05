9b6c01e3cdc97c3474e0690632b62d92ae0f8883
arn354 <arn354@email.de>
2016-03-05 12:12:45 +0100

    elistboxcontent - reduce length of slider

--- lib/gui/elistboxcontent.cpp
+++ lib/gui/elistboxcontent.cpp
@@ -461,7 +461,7 @@ void eListboxPythonConfigContent::paint(gPainter &painter, eWindowStyle &style, 
 						int size = (pvalue && PyInt_Check(psize)) ? PyInt_AsLong(psize) : 100;
 
 							/* calc. slider length */
-						valueWidth = (itemsize.width() - m_seperation) * value / size;
+						valueWidth = (itemsize.width() - m_seperation -40) * value / size;
 						int height = itemsize.height();
 
 							/* draw slider */
