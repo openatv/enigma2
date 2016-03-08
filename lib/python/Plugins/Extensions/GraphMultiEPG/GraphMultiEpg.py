a413d889cc82c3b6852531327b3af77790c2e759
littlesat <littlesat99@yahoo.com>
2016-03-08 22:47:14 +0100

    GMEPG: removed some not required code
    As the menu option has always at least two options

--- lib/python/Plugins/Extensions/GraphMultiEPG/GraphMultiEpg.py
+++ lib/python/Plugins/Extensions/GraphMultiEPG/GraphMultiEpg.py
@@ -1055,13 +1055,10 @@ class GraphMultiEPG(Screen, HelpableScreen):
 				text += _(": %s") % event.getEventName()
 		menu.append((_("Timer Overview"), self.openTimerOverview))
 		menu.append((_("Setup menu"), self.showSetup, "menu"))
-		if len(menu) == 1:
-			menu and menu[0][1]()
-		elif len(menu) > 1:
-			def boxAction(choice):
-				if choice:
-					choice[1]()
-			self.session.openWithCallback(boxAction, ChoiceBox, title=text, list=menu, windowTitle=_("Further options"))
+		def boxAction(choice):
+			if choice:
+				choice[1]()
+		self.session.openWithCallback(boxAction, ChoiceBox, title=text, list=menu, windowTitle=_("Further options"))
 
 	def runPlugin(self, plugin):
 		event = self["list"].getCurrent()
