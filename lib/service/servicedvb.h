5ab2d7b9a5a60f0d892799cf5c99201ff692c2cb
Erik Slagter <erik@openpli.org>
2016-03-14 19:06:37 +0100

    Revert "Enabled/disabled fallback tuner for recordings seperately"
    This reverts commit 65250a287a6aa860fcc47266fef5283ee93eaaa3.
    
     - it's confusing
     - it doesn't actually do what it suggests
     - there is no advantage really
    
    When you use this option and set it to "no", the only difference
    is that a recording that already couldn't be made in the first place,
    now also won't be made using the fallback tuner, so it always fails.
    
    This option works at the wrong level, at this point the user cannot
    be informed so the only options are:
    
     - make the recording using the fallback tuner
     - don't make the recording at all (and thereby failing)
    
    So this option is useless. It should be implemented at user-interface
    level, i.e. in the timer conflict detection. If one doesn't disable
    the conflict detection explicitly, fallback tuners won't be considered
    for timer recordings anyway.

--- lib/service/servicedvb.h
+++ lib/service/servicedvb.h
@@ -71,7 +71,7 @@ class eDVBServiceBase: public iFrontendInformation
 {
 protected:
 	static bool tryFallbackTuner(eServiceReferenceDVB &service,
-			bool &is_stream, bool is_pvr, bool simulate, bool is_recording=false);
+			bool &is_stream, bool is_pvr, bool simulate);
 
 	eDVBServicePMTHandler m_service_handler;
 public:
--- lib/service/servicedvbrecord.cpp
+++ lib/service/servicedvbrecord.cpp
@@ -219,7 +219,7 @@ int eDVBServiceRecord::doPrepare()
 	{
 		eDVBServicePMTHandler::serviceType servicetype;
 
-		if(tryFallbackTuner(/*REF*/m_ref, /*REF*/m_is_stream_client, m_is_pvr, m_simulate, /*is_recording*/ true))
+		if(tryFallbackTuner(/*REF*/m_ref, /*REF*/m_is_stream_client, m_is_pvr, m_simulate))
 			eDebug("ServiceRecord: fallback tuner selected");
 
 		if (m_streaming)
