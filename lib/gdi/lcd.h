38815218ba7dc33bcd6567de05e20ee66819b0ba
madie <madie1@gmx.de>
2016-03-19 18:03:23 +0100

    fix typo

--- lib/gdi/lcd.h
+++ lib/gdi/lcd.h
@@ -82,7 +82,7 @@ public:
 	int setLED(int value, int option);
 	void setInverted( unsigned char );
 	void setFlipped(bool);
-	void setDump(bool)
+	void setDump(bool);
 	bool isOled() const { return !!lcd_type; };
 	void setPalette(gUnmanagedSurface) {};
 	void update();
