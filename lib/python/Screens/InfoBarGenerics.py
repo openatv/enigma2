aa39b98fab2f5f166a3952a7541d520eacd65c6b
Dima73 <Dima-73@inbox.lv>
2016-03-21 21:36:31 +0200

    InfoBarGenerics/addSubserviceToBouquetCallback: add sanity check

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -3335,7 +3335,7 @@ class InfoBarSubserviceSelection:
 				self.session.nav.playService(service[1], False)
 
 	def addSubserviceToBouquetCallback(self, service):
-		if len(service) > 1 and isinstance(service[1], eServiceReference):
+		if service and len(service) > 1 and isinstance(service[1], eServiceReference):
 			self.selectedSubservice = service
 			if self.bouquets is None:
 				cnt = 0
