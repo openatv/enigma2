6bc2a35748c0759909b6b0ea05cb35073c557446
pizzelnet <pizzel@email.com>
2016-03-24 21:14:42 +0100

    File size in GB with 2 digits when file is +1024MB
    Shows the size of the file in gigabytes (with two digits for megabytes) when it is higher than 1024 MB. (E.g. 4.35GB)

--- lib/python/Components/Converter/MovieInfo.py
+++ lib/python/Components/Converter/MovieInfo.py
@@ -55,6 +55,8 @@ class MovieInfo(Converter, object):
 				if filesize is not None:
 					if filesize >= 100000*1024*1024:
 						return _("%.0f GB") % (filesize / (1024.0*1024.0*1024.0))
+					elif filesize >= 100000*1024:
+						return _("%.2f GB") % (filesize / (1024.0*1024.0*1024.0))
 					else:
 						return _("%.0f MB") % (filesize / (1024.0*1024.0))
 		return ""
