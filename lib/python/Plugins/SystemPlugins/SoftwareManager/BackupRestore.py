90e26ebb52e70170450a5d050e812c219bf78d35
madie <madie1@gmx.de>
2016-03-24 15:28:19 +0100

    [softwaremanager] add skinpart folder to default backup

--- lib/python/Plugins/SystemPlugins/SoftwareManager/BackupRestore.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/BackupRestore.py
@@ -41,6 +41,7 @@ def InitConfig():
 																							'/etc/default/dropbear', '/home/root/.ssh/', '/etc/samba/', '/etc/fstab', '/etc/inadyn.conf', 
 																							'/etc/network/interfaces', '/etc/wpa_supplicant.conf', '/etc/wpa_supplicant.ath0.conf', '/etc/opkg/secret-feed.conf',
 																							'/etc/wpa_supplicant.wlan0.conf', '/etc/resolv.conf', '/etc/default_gw', '/etc/hostname', '/usr/lib/enigma2/python/Plugins/Extensions/VMC/DB/',
+																							'/usr/share/enigma2/MetrixHD/skinparts/',
 																							'/usr/lib/enigma2/python/Plugins/Extensions/MP3Browser/db',
 																							'/usr/lib/enigma2/python/Plugins/Extensions/MovieBrowser/db',
 																							'/usr/lib/enigma2/python/Plugins/Extensions/TVSpielfilm/db', '/etc/ConfFS',
