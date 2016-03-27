d6d4188f7d349aa3e324a94614e507d424478b5c
littlesat <littlesat99@yahoo.com>
2016-03-27 14:55:31 +0200

    Cosmetics

--- lib/python/Plugins/Extensions/GraphMultiEPG/plugin.py
+++ lib/python/Plugins/Extensions/GraphMultiEPG/plugin.py
@@ -74,9 +74,7 @@ def onSelectBouquetClose(*args):
 		epg["list"].moveToService(serviceref)
 
 def changeBouquetCB(direction, epgcall):
-	global epg
-	epg = epgcall
-	Session.openWithCallback(onSelectBouquetClose, SimpleChannelSelection, _("Select channel"), True, True, epg["list"].getCurrent()[1].ref )
+	Session.openWithCallback(onSelectBouquetClose, SimpleChannelSelection, _("Select channel"), True, True, epg["list"].getCurrent()[1].ref)
 
 def main(session, servicelist = None, **kwargs):
 	global Session
