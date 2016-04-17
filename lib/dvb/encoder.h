8fade7b3f5405f4fb5d122e0cac406627fd0347b
prl <prl@ozemail.com.au>
2016-04-17 13:56:42 +1000

    Fix Bug #478: No .ap file created for HDMI In recordings - causes Execute cuts to fail
    [eEncoder]
    
    Add definitions for fixed PIDs used by the HDMI IN encoder,
    and use them in methods to return corresponding the PIDs.
    
    [eServiceHDMIRecord]
    
    In doRecord(), add code to set the video PID for the HDMI IN encoder
    as the "timing pid" for the recording, from which the access points
    for the .ap file are saved.
    
    Because it's not possible to tell what streams are presend in the
    output of the decoder, if the HUDM IN has no video stream,
    only audio, no access points will be found.
    
    This has the side-effect of filling and preserving the .sc file
    for the recording.

--- lib/dvb/encoder.h
+++ lib/dvb/encoder.h
@@ -14,6 +14,13 @@ class eEncoder
 
 	static eEncoder *instance;
 
+	enum {
+		hdmiPatPid = 0x00, // 0
+		hdmiPcrPid = 0x13, // 19
+		hdmiPmtPid = 0x55, // 85
+		hdmiAudioPid = 0xC0, // 196
+		hdmiVideoPid = 0xE0, // 224
+	};
 public:
 	eEncoder();
 	~eEncoder();
@@ -21,6 +28,12 @@ public:
 	int allocateEncoder(const std::string &serviceref, const int bitrate, const int width, const int height, const int framerate, const int interlaced, const int aspectratio);
 	void freeEncoder(int encoderfd);
 	int getUsedEncoderCount();
+
+	int getPatPid() { return hdmiPatPid; }
+	int getPcrPid() { return hdmiPcrPid; }
+	int getPmtPid() { return hdmiPmtPid; }
+	int getAudioPid() { return hdmiAudioPid; }
+	int getVideoPid() { return hdmiVideoPid; }
 
 	static eEncoder *getInstance();
 };
