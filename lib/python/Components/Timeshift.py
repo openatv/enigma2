708000bd96a3468762555dcdfc9175486762ae09
Andrew Blackburn <andyblac@icloud.com>
2016-05-07 17:59:36 +0100

    [TimeShift] add option to hide stream warning message.

--- lib/python/Components/Timeshift.py
+++ lib/python/Components/Timeshift.py
@@ -322,7 +322,8 @@ class InfoBarTimeshift:
 
 	def startTimeshift(self):
 		if self.session.nav.getCurrentlyPlayingServiceReference() and 'http' in self.session.nav.getCurrentlyPlayingServiceReference().toString():
-			self.session.open(MessageBox, _("Timeshift on a stream is not supported!"), MessageBox.TYPE_ERROR, timeout=5)
+			if config.timeshift.stream_warning.value:
+				self.session.open(MessageBox, _("Timeshift on a stream is not supported!"), MessageBox.TYPE_ERROR, timeout=5)
 			print '[Timeshift] unable to activate, due being on a stream.'
 			return
 		ts = self.getTimeshift()
@@ -476,7 +477,8 @@ class InfoBarTimeshift:
 	def autostartPermanentTimeshift(self):
 		self["TimeshiftActions"].setEnabled(True)
 		if self.session.nav.getCurrentlyPlayingServiceReference() and 'http' in self.session.nav.getCurrentlyPlayingServiceReference().toString() and int(config.timeshift.startdelay.value):
-			self.session.open(MessageBox, _("Timeshift on a stream is not supported!"), MessageBox.TYPE_ERROR, timeout=5)
+			if config.timeshift.stream_warning.value:
+				self.session.open(MessageBox, _("Timeshift on a stream is not supported!"), MessageBox.TYPE_ERROR, timeout=5)
 			print '[Timeshift] unable to activate, due being on a stream.'
 			return
 		ts = self.getTimeshift()
