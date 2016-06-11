048d10399d7e5a30917f65f203923949d4b2b4bb
Andrew Blackburn <andyblac@icloud.com>
2016-06-11 11:43:13 +0100

    [SystemInfo] add some boxes to led display.

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -32,7 +32,7 @@ SystemInfo["NumFrontpanelLEDs"] = countFrontpanelLEDs()
 SystemInfo["FrontpanelDisplay"] = fileExists("/dev/dbox/oled0") or fileExists("/dev/dbox/lcd0")
 SystemInfo["OledDisplay"] = fileExists("/dev/dbox/oled0")
 SystemInfo["LcdDisplay"] = fileExists("/dev/dbox/lcd0")
-SystemInfo["DisplayLED"] = getBoxType() in ('gb800se', 'gb800solo')
+SystemInfo["DisplayLED"] = getBoxType() in ('gb800se', 'gb800solo', 'gbx1', 'gbx3')
 SystemInfo["LEDButtons"] = getBoxType() == 'vuultimo'
 SystemInfo["DeepstandbySupport"] = HardwareInfo().has_deepstandby()
 SystemInfo["Fan"] = fileCheck("/proc/stb/fp/fan")
