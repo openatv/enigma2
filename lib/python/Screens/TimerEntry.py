c50be956341f5108681bf9dbb501e113e39153d3
arn354 <arn354@email.de>
2016-06-28 11:31:57 +0200

    Add option to configure default "timer type" (record, zap+record, zap)

--- lib/python/Screens/TimerEntry.py
+++ lib/python/Screens/TimerEntry.py
@@ -131,9 +131,10 @@ class TimerEntry(Screen, ConfigListScreen):
 			weekday = int(strftime("%u", localtime(self.timer.begin))) - 1
 			day[weekday] = 1
 
-		self.timerentry_justplay = ConfigSelection(choices = [
-			("zap", _("zap")), ("record", _("record")), ("zap+record", _("zap and record"))],
-			default = {0: "record", 1: "zap", 2: "zap+record"}[justplay + 2*always_zap])
+		self.timerentry_justplay = config.recording.timerentry_justplay
+		#self.timerentry_justplay = ConfigSelection(choices = [
+		#	("zap", _("zap")), ("record", _("record")), ("zap+record", _("zap and record"))],
+		#	default = {0: "record", 1: "zap", 2: "zap+record"}[justplay + 2*always_zap])
 		if SystemInfo["DeepstandbySupport"]:
 			shutdownString = _("go to deep standby")
 		else:
