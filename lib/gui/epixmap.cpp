85e93675222ce3a8239bb20bbff6297f086399ee
dvb-adenin <dvb-adenin@users.noreply.github.com>
2016-08-04 20:45:27 +0200

    [epixmap] an incorrect parameter is passed
    accel or nothing is expected
    but m_scale is passed

--- lib/gui/epixmap.cpp
+++ lib/gui/epixmap.cpp
@@ -36,7 +36,7 @@ void ePixmap::setPixmap(ePtr<gPixmap> &pixmap)
 
 void ePixmap::setPixmapFromFile(const char *filename)
 {
-	loadPNG(m_pixmap, filename, m_scale);
+	loadPNG(m_pixmap, filename);
 
 	if (!m_pixmap)
 	{
