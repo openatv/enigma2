ed91f6a88e51fcaab31804a248c7e2c2ee6a24af
nx111 <gd.zhangdz@gmail.com>
2016-08-15 15:34:22 +0800

    Support convert UTF16-LE/UTF16-BE,and Chinese GB18030/BIG5 string to UTF-8 string.
    changes:
        1. support convert utf16-le/utf16be/gb18030/big5 string to utf-8 string.
        2. support return converted length for convertDVBUTF8.
        3. support set custom default encoding for strings.
        4. function replace_all can replace by string encoding correctly.
    
    Signed-off-by: nx111 <gd.zhangdz@gmail.com>

--- lib/base/estring.h
+++ lib/base/estring.h
@@ -12,18 +12,21 @@ int strnicmp(const char*, const char*, int);
 
 std::string getNum(int num, int base=10);
 
-std::string convertDVBUTF8(const unsigned char *data, int len, int table=1, int tsidonid=1); // with default ISO8859-1 / Latin1
+std::string GB18030ToUTF8(const char *szIn, int len,int *pconvertedLen=0);
+std::string Big5ToUTF8(const char *szIn, int len,int *pconvertedLen=0);
+
+std::string convertDVBUTF8(const unsigned char *data, int len, int table=0, int tsidonid=1,int *pconvertedLen=0); // with default encoding
 std::string convertLatin1UTF8(const std::string &string);
 int isUTF8(const std::string &string);
 unsigned int truncateUTF8(std::string &s, unsigned int newsize);
 
 std::string removeDVBChars(const std::string &s);
 void makeUpper(std::string &s);
-std::string replace_all(const std::string &in, const std::string &entity, const std::string &symbol);
+std::string replace_all(const std::string &in, const std::string &entity, const std::string &symbol,int table=-1);
 
-inline std::string convertDVBUTF8(const std::string &string, int table=1, int tsidonid=1) // with default ISO8859-1 / Latin1
+inline std::string convertDVBUTF8(const std::string &string, int table=0, int tsidonid=1,int *pconvertedLen=0) // with default encoding
 {
-	return convertDVBUTF8((const unsigned char*)string.c_str(), string.length(), table, tsidonid);
+	return convertDVBUTF8((const unsigned char*)string.c_str(), string.length(), table, tsidonid,pconvertedLen);
 }
 
 std::string urlDecode(const std::string &s);
