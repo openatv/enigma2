f936212eac0cb200fa6c84d4a7286643596ceb3b
Huevos <rowland@moralet.com>
2016-08-27 12:58:56 +0200

    Revert "multituner - initialize m_type as feSatellite if not m_multitype."
    This reverts commit ac0cb095f1aebac9ec68c30e98801037fe3a18ad.

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -677,11 +677,7 @@ int eDVBFrontend::openFrontend()
 
 	m_multitype = m_delsys[SYS_DVBS] && (m_delsys[SYS_DVBT] || m_delsys[SYS_DVBC_ANNEX_A]);
 
-	if (!m_multitype)
-		m_type = feSatellite;
-
-	if(m_type == feSatellite)
-		setTone(iDVBFrontend::toneOff);
+	setTone(iDVBFrontend::toneOff);
 	setVoltage(iDVBFrontend::voltageOff);
 
 	return 0;
