d3fa62d40998b92cfbb8a7634d87077bafd5d747
ViX-Sicilian <stefd@hotmail.co.uk>
2016-08-31 18:36:32 +0100

    [AVSWITCH] Add BCM7251s CPU.

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -64,7 +64,7 @@ class AVSwitch:
 	modes["Scart"] = ["PAL", "NTSC", "Multi"]
 	# modes["DVI-PC"] = ["PC"]
 
-	if about.getChipSetString() in ('7366', '7376', '5272s','7252'):
+	if about.getChipSetString() in ('7366', '7376', '5272s','7252','7251s'):
 		modes["HDMI"] = ["720p", "1080p", "2160p", "1080i", "576p", "576i", "480p", "480i"]
 		widescreen_modes = {"720p", "1080p", "2160p", "1080i"}
 	elif about.getChipSetString() in ('7358', '7356', '7362', '7424', '7425', '7241', '7552'):
