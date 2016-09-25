0d68a2cdc62d3c836e765bc4e6ed0f3b8de1f95c
captain <openatv@gmail.com>
2016-09-25 18:05:25 +0200

    about fix cpu speed

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -33,7 +33,7 @@ def getAboutText():
 		AboutText += _("Chipset:\t%s") % about.getChipSetString() + "\n"
 
 	cpuMHz = ""
-	if getMachineBuild() in ('vusolo4k') or bootloader in ('v29'):
+	if getMachineBuild() in ('vusolo4k') or bootloader == 'v29':
 		cpuMHz = "   (1,5 GHz)"
 	elif getMachineBuild() in ('hd52','hd51'):
 		cpuMHz = "   (1,7 GHz)"
