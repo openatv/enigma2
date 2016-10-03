8931ec7bafbf21da00da25d7eb9d4c7a9c98de35
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2016-10-03 12:55:56 +0100

    [EpsSelection] White space removal

--- lib/python/Screens/EpgSelection.py
+++ lib/python/Screens/EpgSelection.py
@@ -459,7 +459,7 @@ class EPGSelection(Screen, HelpableScreen):
 			self['bouquetlist'].recalcEntrySize()
 			self['bouquetlist'].fillBouquetList(self.bouquets)
 			self['bouquetlist'].moveToService(self.StartBouquet)
-			self['bouquetlist'].setCurrentBouquet(self.StartBouquet	)
+			self['bouquetlist'].setCurrentBouquet(self.StartBouquet)
 			self.setTitle(self['bouquetlist'].getCurrentBouquet())
 			if self.type == EPG_TYPE_GRAPH:
 				self['list'].setShowServiceMode(config.epgselection.graph_servicetitle_mode.value)
