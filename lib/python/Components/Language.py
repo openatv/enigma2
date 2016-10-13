fb0c902daa37c70002a02c6bc7730a1d0ded9a99
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2016-10-13 09:44:11 +0200

    [Language] Fix global text domain not set
    This leads to untranslated texts in plugins when they rely on default translations from Enigma2.
    
    If this really happened or not was unpredictable because some plugins already contained the above statement and fixed the whole system that way as a side effect ;) .
    If NO such plugin was installed, default translations where broken for ALL plugins.

--- lib/python/Components/Language.py
+++ lib/python/Components/Language.py
@@ -8,6 +8,7 @@ from Tools.Directories import SCOPE_LANGUAGE, resolveFilename
 class Language:
 	def __init__(self):
 		gettext.install('enigma2', resolveFilename(SCOPE_LANGUAGE, ""), unicode=0, codeset="utf-8")
+		gettext.textdomain("enigma2")
 		self.activeLanguage = 0
 		self.catalog = None
 		self.lang = {}
