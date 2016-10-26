027e332bd15b19bc753071f93de72d47aab5b575
RobvanderDoes <rlvddoes@gmail.com>
2016-10-26 09:42:52 +0200

    [About] Added FP-version for supporting boxes.

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -138,6 +138,13 @@ class About(Screen):
 			AboutText += _("Processor temperature:\t%s") % tempinfo.replace('\n', '').replace(' ','') + mark + "C\n"
 		AboutLcdText = AboutText.replace('\t', ' ')
 
+		fp_version = getFPVersion()
+		if fp_version is None:
+			fp_version = ""
+		elif fp_version != 0:
+			fp_version = _("Frontprocessor version: %s") % fp_version
+			AboutText += fp_version + "\n"
+
 		self["AboutScrollLabel"] = ScrollLabel(AboutText)
 
 	def showTranslationInfo(self):
