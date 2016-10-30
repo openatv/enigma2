408ecc6f481c0c33e18303eac975169f467078d0
Athanasios Oikonomou <athoik@gmail.com>
2016-10-30 16:21:10 +0200

    servicedvb: do not try to record non pid values from cache (#504)
    In cache we are storing cVTYPE, cACHANNEL, cAC3DELAY and cPCMDELAY.
    
    None of then is PID so exclude their values from recording pids.

--- lib/service/servicedvbrecord.cpp
+++ lib/service/servicedvbrecord.cpp
@@ -349,6 +349,11 @@ int eDVBServiceRecord::doRecord()
 				// cached pids
 				for (int x = 0; x < eDVBService::cacheMax; ++x)
 				{
+					if (x == 5)
+					{
+						x += 3; // ignore cVTYPE, cACHANNEL, cAC3DELAY, cPCMDELAY
+						continue;
+					}
 					int entry = service->getCacheEntry((eDVBService::cacheID)x);
 					if (entry != -1)
 					{
