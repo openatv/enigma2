be2aa7c8cc01aa6743abe484cdd55d716b279b21
pieterg <pieterg@users.sourceforge.net>
2016-11-01 19:30:03 +0100

    atsc: add EIT support
    providing now/next information
    
    Currently, only EIT-0 is parsed.
    The matching ETT-0 is used to provide event descriptions.
    Both titles and descriptions are selected based on the preferred EPG
    languages.
    
    For both EIT and ETT, only plain messages with UTF-16 encoding are
    parsed.
    Huffman and SCSU are not yet supported by the current multi-string-
    object implementation.
    
    Side-effect:
    eDVBServiceEITHandler contained a ePtr<iDVBDemux> member, which was
    never used. (it should have been used to make sure an additional ref was
    held on the demux, while the EIT reader was running)
    The current implementation uses the member, thus adding refs on the
    demux.
    If resource refcount issues are starting to occur, this might need a
    closer look, there might be an issue explaining why the ePtr<iDVBDemux>
    member was never assigned.

--- lib/dvb/atsc.h
+++ lib/dvb/atsc.h
@@ -2,6 +2,7 @@
 #define __ATSC_H__
 
 #include <sys/types.h>
+#include <lib/dvb/specs.h>
 
 #include <dvbsi++/long_crc_section.h>
 #include <dvbsi++/descriptor_container.h>
@@ -138,6 +139,113 @@ public:
 	uint8_t getGPSOffset(void) const;
 };
 
+class MasterGuideTable : public DescriptorContainer
+{
+protected:
+	unsigned PID : 16;
+	unsigned tableType : 16;
+	unsigned numberBytes : 32;
+	unsigned descriptorsLoopLength : 10;
+
+public:
+	MasterGuideTable(const uint8_t *const buffer);
+	~MasterGuideTable(void);
+
+	uint16_t getPID(void) const;
+	uint16_t getTableType(void) const;
+	uint32_t getNumberBytes(void) const;
+	uint16_t getDescriptorsLoopLength(void) const;
+};
+
+typedef std::list<MasterGuideTable*> MasterGuideTableList;
+typedef MasterGuideTableList::iterator MasterGuideTableListIterator;
+typedef MasterGuideTableList::const_iterator MasterGuideTableListConstIterator;
+
+class MasterGuideTableSection : public LongCrcSection
+{
+protected:
+	unsigned versionNumber : 5;
+	MasterGuideTableList tables;
+
+public:
+	MasterGuideTableSection(const uint8_t * const buffer);
+	~MasterGuideTableSection(void);
+
+	static const uint16_t LENGTH = 4096;
+	static const uint16_t PID = 0x1ffb;
+	static const int TID = 0xc7;
+	static const uint32_t TIMEOUT = 1000;
+
+	uint8_t getVersion(void) const;
+	const MasterGuideTableList *getTables(void) const;
+};
+
+class ATSCEvent : public DescriptorContainer
+{
+protected:
+	MultipleStringStructure *title;
+	unsigned eventId : 14;
+	unsigned startTime : 32;
+	unsigned ETMLocation : 2;
+	unsigned lengthInSeconds : 20;
+	unsigned titleLength : 8;
+	unsigned descriptorsLoopLength : 12;
+
+public:
+	ATSCEvent(const uint8_t *const buffer);
+	~ATSCEvent(void);
+
+	const std::string getTitle(const std::string &language) const;
+	uint16_t getEventId(void) const;
+	uint32_t getStartTime(void) const;
+	uint8_t getETMLocation(void) const;
+	uint32_t getLengthInSeconds(void) const;
+	uint16_t getTitleLength(void) const;
+	uint16_t getDescriptorsLoopLength(void) const;
+};
+
+typedef std::list<ATSCEvent*> ATSCEventList;
+typedef ATSCEventList::iterator ATSCEventListIterator;
+typedef ATSCEventList::const_iterator ATSCEventListConstIterator;
+
+class ATSCEventInformationSection : public LongCrcSection
+{
+protected:
+	unsigned versionNumber : 5;
+	ATSCEventList events;
+
+public:
+	ATSCEventInformationSection(const uint8_t * const buffer);
+	~ATSCEventInformationSection(void);
+
+	static const uint16_t LENGTH = 4096;
+	static const int TID = 0xcb;
+	static const uint32_t TIMEOUT = 5000;
+
+	uint8_t getVersion(void) const;
+	const ATSCEventList *getEvents(void) const;
+};
+
+class ExtendedTextTableSection : public LongCrcSection
+{
+protected:
+	unsigned versionNumber : 5;
+	unsigned ETMId : 32;
+	MultipleStringStructure *message;
+
+public:
+	ExtendedTextTableSection(const uint8_t * const buffer);
+	~ExtendedTextTableSection(void);
+
+	static const uint16_t LENGTH = 4096;
+	static const int TID = 0xcc;
+	static const uint32_t TIMEOUT = 10000;
+
+	uint8_t getVersion(void) const;
+	uint32_t getETMId(void) const;
+	const std::string getMessage(const std::string &language) const;
+};
+
 struct eDVBVCTSpec
 {
 	eDVBTableSpec m_spec;
@@ -167,6 +275,64 @@ public:
 		m_spec.pid      = SystemTimeTableSection::PID;
 		m_spec.tid      = SystemTimeTableSection::TID;
 		m_spec.timeout  = SystemTimeTableSection::TIMEOUT;
+		m_spec.flags    = eDVBTableSpec::tfAnyVersion |
+			eDVBTableSpec::tfHaveTID |
+			eDVBTableSpec::tfCheckCRC | eDVBTableSpec::tfHaveTimeout;
+	}
+	operator eDVBTableSpec &()
+	{
+		return m_spec;
+	}
+};
+
+struct eATSCMGTSpec
+{
+	eDVBTableSpec m_spec;
+public:
+	eATSCMGTSpec()
+	{
+		m_spec.pid      = MasterGuideTableSection::PID;
+		m_spec.tid      = MasterGuideTableSection::TID;
+		m_spec.timeout  = MasterGuideTableSection::TIMEOUT;
+		m_spec.flags    = eDVBTableSpec::tfAnyVersion |
+			eDVBTableSpec::tfHaveTID |
+			eDVBTableSpec::tfCheckCRC | eDVBTableSpec::tfHaveTimeout;
+	}
+	operator eDVBTableSpec &()
+	{
+		return m_spec;
+	}
+};
+
+struct eATSCEITSpec
+{
+	eDVBTableSpec m_spec;
+public:
+	eATSCEITSpec(int pid, int source_id)
+	{
+		m_spec.pid      = pid;
+		m_spec.tid      = ATSCEventInformationSection::TID;
+		m_spec.tidext   = source_id;
+		m_spec.timeout  = ATSCEventInformationSection::TIMEOUT;
+		m_spec.flags    = eDVBTableSpec::tfAnyVersion |
+			eDVBTableSpec::tfHaveTID | eDVBTableSpec::tfHaveTIDExt |
+			eDVBTableSpec::tfCheckCRC | eDVBTableSpec::tfHaveTimeout;
+	}
+	operator eDVBTableSpec &()
+	{
+		return m_spec;
+	}
+};
+
+struct eATSCETTSpec
+{
+	eDVBTableSpec m_spec;
+public:
+	eATSCETTSpec(int pid)
+	{
+		m_spec.pid      = pid;
+		m_spec.tid      = ExtendedTextTableSection::TID;
+		m_spec.timeout  = ExtendedTextTableSection::TIMEOUT;
 		m_spec.flags    = eDVBTableSpec::tfAnyVersion |
 			eDVBTableSpec::tfHaveTID |
 			eDVBTableSpec::tfCheckCRC | eDVBTableSpec::tfHaveTimeout;
