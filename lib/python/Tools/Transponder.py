2044222e4152b82cf9525e4095d63abdd527006c
Peter Urbanec <bitbucket.org@urbanec.net>
2016-11-07 11:39:36 +1100

    [Transponder] Fix up debug tags

--- lib/python/Tools/Transponder.py
+++ lib/python/Tools/Transponder.py
@@ -8,7 +8,7 @@ def getTunerDescription(nim):
 	try:
 		return nimmanager.getTerrestrialDescription(nim)
 	except:
-		print "[ChannelNumber] nimmanager.getTerrestrialDescription(nim) failed, nim:", nim
+		print "[Transponder] nimmanager.getTerrestrialDescription(nim) failed, nim:", nim
 	return ""
 
 def getMHz(frequency):
@@ -255,7 +255,7 @@ def ConvertToHumanReadable(tp, tunertype = None):
 			eDVBFrontendParametersATSC.System_ATSC : "ATSC",
 			eDVBFrontendParametersATSC.System_DVB_C_ANNEX_B : "DVB-C ANNEX B"}.get(tp.get("system"))
 	elif tunertype != "None":
-		print "ConvertToHumanReadable: no or unknown tunertype in tpdata dict for tunertype:", tunertype
+		print "[Transponder] ConvertToHumanReadable: no or unknown tunertype in tpdata dict for tunertype:", tunertype
 	for k,v in tp.items():
 		if k not in ret:
 			ret[k] = v
