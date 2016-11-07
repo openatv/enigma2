87ac21686bad3675c6610e87d518ac1745b34b04
IanSav <IS.OzPVR@gmail.com>
2016-11-07 17:46:51 +0000

    [PluginList.py] Revert default width change
    I still maintain that a width of 440 pixels is a random choice and that there is logic to change the fall-back default to 1280.
    
    Having checked the PluginBrowser screen that calls this module I note that all calls do provide an appropriate width that relates to the current skin control width configuration.
    
    To aid in future merge efforts I will revert this change.

--- lib/python/Components/PluginList.py
+++ lib/python/Components/PluginList.py
@@ -7,7 +7,7 @@ from enigma import eListboxPythonMultiContent, gFont, BT_SCALE, BT_KEEP_ASPECT_R
 from Tools.LoadPixmap import LoadPixmap
 import skin
 
-def PluginEntryComponent(plugin, width=1280):
+def PluginEntryComponent(plugin, width=440):
 	if plugin.icon is None:
 		png = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "icons/plugin.png"))
 	else:
@@ -22,7 +22,7 @@ def PluginEntryComponent(plugin, width=1280):
 		MultiContentEntryPixmapAlphaBlend(pos=(ix, iy), size=(iw, ih), png = png, flags = BT_SCALE | BT_KEEP_ASPECT_RATIO)
 	]
 
-def PluginCategoryComponent(name, png, width=1280):
+def PluginCategoryComponent(name, png, width=440):
 	x, y, h = skin.parameters.get("PluginBrowserCategoryName", (80, 10, 50))
 	ix, iy, iw, ih = skin.parameters.get("PluginBrowserDownloadIcon", (10, 0, 60, 50))
 	return [
@@ -31,7 +31,7 @@ def PluginCategoryComponent(name, png, width=1280):
 		MultiContentEntryPixmapAlphaBlend(pos=(ix, iy), size=(iw, ih), png=png)
 	]
 
-def PluginDownloadComponent(plugin, name, version=None, width=1280):
+def PluginDownloadComponent(plugin, name, version=None, width=440):
 	if plugin.icon is None:
 		png = LoadPixmap(resolveFilename(SCOPE_ACTIVE_SKIN, "icons/plugin.png"))
 	else:
