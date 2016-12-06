986d9c46aaa8bce3ef9e7476b47758dae0679721
captain <openatv@gmail.com>
2016-12-06 08:45:48 +0100

    azbox audio fix

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -2885,14 +2885,8 @@ void eDVBServicePlay::updateDecoder(bool sendSeekableStateChanged)
 
 		selectAudioStream();
 
-#if HAVE_AMLOGIC
-			m_decoder->setSyncPCR(pcrpid);
-#else
-			if (!(m_is_pvr || m_is_stream || m_timeshift_active))
-				m_decoder->setSyncPCR(pcrpid);
-			else
-				m_decoder->setSyncPCR(-1);
-#endif
+		m_decoder->setSyncPCR(pcrpid);
+
 
 		if (m_decoder_index == 0)
 		{
