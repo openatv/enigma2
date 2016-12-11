50249adb524e53a68121c9fbc9416b814551f53c
rossi2000 <rossi_2000@hotmail.com>
2016-12-11 09:13:31 +0000

    Infobargenerics: add HDMI-IN for ultimo4k

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -4027,7 +4027,7 @@ class InfoBarHdmi:
 		self.hdmi_enabled_full = False
 		self.hdmi_enabled_pip = False
 
-		if getMachineBuild() in ('inihdp', 'hd2400'):
+		if getMachineBuild() in ('hd2400', 'inihdp', 'vuultimo4k'):
 			if not self.hdmi_enabled_full:
 				self.addExtension((self.getHDMIInFullScreen, self.HDMIInFull, lambda: True), "blue")
 			if not self.hdmi_enabled_pip:
