ea463288a887a68f8dde0f8459c7d662d1604ac6
captain <openatv@gmail.com>
2016-12-15 12:53:56 +0100

    add nextv rc_type setup

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -9,7 +9,7 @@ from Components.ConfigList import ConfigListScreen
 from Components.ActionMap import ActionMap, HelpableActionMap
 from Tools.Directories import resolveFilename, SCOPE_ACTIVE_SKIN
 from Tools.LoadPixmap import LoadPixmap
-from boxbranding import getBoxType, getMachineBrand, getMachineName
+from boxbranding import getBoxType, getMachineBrand, getMachineName, getBrandOEM
 
 boxtype = getBoxType()
 
@@ -258,60 +258,124 @@ class InputDeviceSetup(Screen, ConfigListScreen):
 
 
 class RemoteControlType(Screen, ConfigListScreen):
-	
-	rcList = [
-			("0", _("Default")),
-			("3", _("MaraM9")),
-			("4", _("DMM normal")),
-			("5", _("et9000/et9100")),
-			("6", _("DMM advanced")),
-			("7", _("et5000/6000")),
-			("8", _("VU+")),
-			("9", _("et8000/et10000")),
-			("11", _("et9200/9500/6500")),
-			("13", _("et4000")),
-			("14", _("XP1000")),
-			("16", _("HD11/HD51/HD1100/HD1200/HD1265/HD1500/HD500C/HD530C/et7x00/et8500/et7000mini")),
-			("17", _("XP3000")),
-			("18", _("F1/F3/F4/F4-TURBO/TRIPLEX")),
-			("19", _("HD2400")),
-			("20", _("Zgemma Star S/2S/H1/H2")),
-			("21", _("Zgemma H.S/H.2S/H.2H/H5"))
-			]
-
-	defaultRcList = [
-			("et4000", 13),
-			("et5000", 7),
-			("et6000", 7),
-			("et6500", 11),
-			("et7x00",16),
-			("et7000mini",16),
-			("et8000", 9),
-			("et8500",16),
-			("et9000", 5),
-			("et9100", 5),
-			("et9200", 11),
-			("et9500", 11),
-			("et10000", 9),
-			("formuler1",18),
-			("formuler3",18),
-			("formuler4",18),
-			("formuler4turbo",18),
-			("hd11",16),
-			("hd51",16),
-			("hd1100",16),
-			("hd1200",16),
-			("hd1265",16),
-			("hd500c",16),
-			("hd530c",16),
-			("hd2400",19),
-			("triplex",18),
-			("xp1000", 14),
-			("xp3000", 17),
-			("sh1", 20),
-			("h3", 21),
-			("h5", 21)
-			]
+	if getBrandOEM() in ('broadmedia','octagon','odin','protek','ultramini','ultramini') or getBoxType() in ('et7x00','et8500'):
+		rcList = [
+				("3", _("MaraM9")),
+				("4", _("DMM normal")),
+				("5", _("et9000/et9100")),
+				("6", _("DMM advanced")),
+				("7", _("et5000/6000")),
+				("8", _("VU+")),
+				("9", _("et8000/et10000")),
+				("11", _("et9200/9500/6500")),
+				("13", _("et4000")),
+				("14", _("XP1000")),
+				("16", _("HD11/HD51/HD1100/HD1200/HD1265/HD1500/HD500C/HD530C/et7x00/et8500")),
+				("17", _("XP3000")),
+				("18", _("F1/F3/F4/F4-TURBO/TRIPLEX")),
+				("19", _("HD2400")),
+				("20", _("Zgemma Star S/2S/H1/H2")),
+				("21", _("Zgemma H.S/H.2S/H.2H/H5")),
+				("500", _("WWIO_BRE2ZE_TC")),
+				("501", _("OCTAGON_SFXXX8")),
+				("502", _("GIGABLUE Black")),
+				("503", _("MIRACLEBOX_TWINPLUS")),
+				("504", _("E3HD/XPEEDLX ")),
+				("505", _("ODIN_M7"))
+				]
+		defaultRcList = [
+				("et4000", 13),
+				("et5000", 7),
+				("et6000", 7),
+				("et6500", 11),
+				("et7x00",16),
+				("et7000mini",16),
+				("et8000", 9),
+				("et8500",16),
+				("et9000", 5),
+				("et9100", 5),
+				("et9200", 11),
+				("et9500", 11),
+				("et10000", 9),
+				("formuler1",18),
+				("formuler3",18),
+				("formuler4",18),
+				("formuler4turbo",18),
+				("hd11",16),
+				("hd51",16),
+				("hd1100",16),
+				("hd1200",16),
+				("hd1265",16),
+				("hd500c",16),
+				("hd530c",16),
+				("hd2400",19),
+				("triplex",18),
+				("xp1000", 14),
+				("xp3000", 17),
+				("sh1", 20),
+				("h3", 21),
+				("h5", 21),
+				("bre2ze_tc", 500),
+				("sf4008", 501),
+				("g100", 501),
+				("sf4018", 501),
+				("gbquadplus", 502),
+				("g300", 503),
+				("e3hd", 504),
+				("et7000mini", 504),
+				("xpeedc.", 504),
+				("odinm7", 505)
+				]
+	else:
+		rcList = [
+				("0", _("Default")),
+				("3", _("MaraM9")),
+				("4", _("DMM normal")),
+				("5", _("et9000/et9100")),
+				("6", _("DMM advanced")),
+				("7", _("et5000/6000")),
+				("8", _("VU+")),
+				("9", _("et8000/et10000")),
+				("11", _("et9200/9500/6500")),
+				("13", _("et4000")),
+				("14", _("XP1000")),
+				("16", _("HD11/HD51/HD1100/HD1200/HD1265/HD1500/HD500C/HD530C")),
+				("17", _("XP3000")),
+				("18", _("F1/F3/F4/F4-TURBO/TRIPLEX")),
+				("19", _("HD2400")),
+				("20", _("Zgemma Star S/2S/H1/H2")),
+				("21", _("Zgemma H.S/H.2S/H.2H/H5"))
+				]
+		defaultRcList = [
+				("et4000", 13),
+				("et5000", 7),
+				("et6000", 7),
+				("et6500", 11),
+				("et8000", 9),
+				("et9000", 5),
+				("et9100", 5),
+				("et9200", 11),
+				("et9500", 11),
+				("et10000", 9),
+				("formuler1",18),
+				("formuler3",18),
+				("formuler4",18),
+				("formuler4turbo",18),
+				("hd11",16),
+				("hd51",16),
+				("hd1100",16),
+				("hd1200",16),
+				("hd1265",16),
+				("hd500c",16),
+				("hd530c",16),
+				("hd2400",19),
+				("triplex",18),
+				("xp1000", 14),
+				("xp3000", 17),
+				("sh1", 20),
+				("h3", 21),
+				("h5", 21)
+				]
 
 	def __init__(self, session):
 		Screen.__init__(self, session)
