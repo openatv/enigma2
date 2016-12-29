b0eb24b705b02b05b22e5d1065c89e9ec76698a1
Mike Looijmans <milo@openpli.org>
2016-12-29 21:19:59 +0100

    e2avahi: Integrate Avahi interface to Enigma2 main event loop
    Integrate the event loop. This allows avahi to interact with the system.
    Calls should thus be made from the main gui thread. This will enable
    browsing for services too.

--- lib/base/e2avahi.cpp
+++ lib/base/e2avahi.cpp
@@ -1,20 +1,66 @@
 #include "e2avahi.h"
-#include <avahi-common/simple-watch.h>
+#include "ebase.h"
 #include <avahi-common/error.h>
 #include <avahi-client/client.h>
 #include <avahi-client/publish.h>
 #include <avahi-client/lookup.h>
 #include <avahi-common/malloc.h>
+#include <avahi-common/timeval.h>
 
-/* For now, instantiate a poll that we'll never actually start. To
- * be replaced with the E2 mainloop */
-static AvahiSimplePoll *avahi_poll = NULL;
 /* Our link to avahi */
 static AvahiClient *avahi_client = NULL;
 
+/* API to the E2 event loop */
+static AvahiPoll avahi_poll_api;
+
+struct AvahiTimeout: public Object
+{
+	ePtr<eTimer> timer;
+    AvahiTimeoutCallback callback;
+    void *userdata;
+
+    void timeout()
+	{
+		eDebug("[Avahi] timeout elapsed");
+		callback(this, userdata);
+	}
+    
+    AvahiTimeout(eMainloop *mainloop, AvahiTimeoutCallback _callback, void *_userdata):
+		timer(eTimer::create(mainloop)),
+		callback(_callback),
+		userdata(_userdata)
+    {
+		CONNECT(timer->timeout, AvahiTimeout::timeout);
+	}
+};
+
+struct AvahiWatch: public Object
+{
+	ePtr<eSocketNotifier> sn;
+	AvahiWatchCallback callback;
+	void *userdata;
+	int lastEvent;
+	
+	void activated(int event)
+	{
+		eDebug("[Avahi] watch activated: %#x", event);
+		lastEvent = event;
+		callback(this, sn->getFD(), (AvahiWatchEvent)event, userdata);
+	}
+
+	AvahiWatch(eMainloop *mainloop, int fd, int req, AvahiWatchCallback cb, void *ud):
+		sn(eSocketNotifier::create(mainloop, fd, req)),
+		callback(cb),
+		userdata(ud),
+		lastEvent(0)
+	{
+		CONNECT(sn->activated, AvahiWatch::activated);
+	}
+};
+
 static void avahi_client_callback(AvahiClient *client, AvahiClientState state, void *d)
 {
-	eDebug("[Avahi] client state: %d\n", state);
+	eDebug("[Avahi] client state: %d", state);
 }
 
 static void avahi_group_callback(AvahiEntryGroup *group,
@@ -22,15 +68,97 @@ static void avahi_group_callback(AvahiEntryGroup *group,
 {
 }
 
+/** Create a new watch for the specified file descriptor and for
+ * the specified events. The API will call the callback function
+ * whenever any of the events happens. */
+static AvahiWatch* avahi_watch_new(const AvahiPoll *api, int fd, AvahiWatchEvent event, AvahiWatchCallback callback, void *userdata)
+{
+	eDebug("[Avahi] %s(%d %#x)", __func__, fd, event);
+	
+	return new AvahiWatch((eMainloop*)api->userdata, fd, event, callback, userdata);
+}
+    
+
+/** Update the events to wait for. It is safe to call this function from an AvahiWatchCallback */
+static void avahi_watch_update(AvahiWatch *w, AvahiWatchEvent event)
+{
+	eDebug("[Avahi] %s(%#x)", __func__, event);
+	w->sn->setRequested(event);
+}
+
+/** Return the events that happened. It is safe to call this function from an AvahiWatchCallback  */
+AvahiWatchEvent avahi_watch_get_events(AvahiWatch *w)
+{
+	eDebug("[Avahi] %s", __func__);
+	return (AvahiWatchEvent)w->lastEvent;
+}
+
+/** Free a watch. It is safe to call this function from an AvahiWatchCallback */
+void avahi_watch_free(AvahiWatch *w)
+{
+	eDebug("[Avahi] %s", __func__);
+	delete w;
+}
+
+static void avahi_set_timer(AvahiTimeout *t, const struct timeval *tv)
+{
+	if (tv)
+	{
+		/*struct timeval now;
+		gettimeofday(&now, NULL);*/
+		AvahiUsec usec = (- avahi_age(tv));
+		if (usec < 0)
+			usec = 0;
+		t->timer->start((usec + 999) / 1000, true);
+	}
+}
+
+/** Set a wakeup time for the polling loop. The API will call the
+callback function when the absolute time *tv is reached. If tv is
+NULL, the timeout is disabled. After the timeout expired the
+callback function will be called and the timeout is disabled. You
+can reenable it by calling timeout_update()  */
+AvahiTimeout* avahi_timeout_new(const AvahiPoll *api, const struct timeval *tv, AvahiTimeoutCallback callback, void *userdata)
+{
+	eDebug("[Avahi] %s", __func__);
+
+	AvahiTimeout* result = new AvahiTimeout((eMainloop*)api->userdata, callback, userdata);
+	avahi_set_timer(result, tv);
+	
+	return result;
+}
+
+/** Update the absolute expiration time for a timeout, If tv is
+ * NULL, the timeout is disabled. It is safe to call this function from an AvahiTimeoutCallback */
+void avahi_timeout_update(AvahiTimeout *t, const struct timeval *tv)
+{
+	eDebug("[Avahi] %s\n", __func__);
+	t->timer->stop();
+	avahi_set_timer(t, tv);
+}
+
+/** Free a timeout. It is safe to call this function from an AvahiTimeoutCallback */
+void avahi_timeout_free(AvahiTimeout *t)
+{
+	eDebug("[Avahi] %s\n", __func__);
+	t->timer->stop();
+	delete t;
+}
+
+
 /* In future, this will connect the mainloop to avahi... */
 void e2avahi_init(eMainloop* reactor)
 {
-	avahi_poll = avahi_simple_poll_new();
-	if (!avahi_poll) {
-		eDebug("avahi_simple_poll_new failed\n");
-		return;
-	}
-	avahi_client = avahi_client_new(avahi_simple_poll_get(avahi_poll),
+	avahi_poll_api.userdata = reactor;
+	avahi_poll_api.watch_new = avahi_watch_new;
+	avahi_poll_api.watch_update = avahi_watch_update;
+	avahi_poll_api.watch_get_events = avahi_watch_get_events;
+	avahi_poll_api.watch_free = avahi_watch_free;
+	avahi_poll_api.timeout_new = avahi_timeout_new;
+	avahi_poll_api.timeout_update = avahi_timeout_update;
+	avahi_poll_api.timeout_free = avahi_timeout_free;
+
+	avahi_client = avahi_client_new(&avahi_poll_api,
 		AVAHI_CLIENT_NO_FAIL, avahi_client_callback, NULL, NULL);
 }
 
--- lib/base/e2avahi.h
+++ lib/base/e2avahi.h
@@ -1,10 +1,12 @@
 #pragma once
 
-#include "ebase.h"
+#ifndef SWIG
+class eMainloop;
 
 /* Initialization and shutdown */
 void e2avahi_init(eMainloop* reactor);
 void e2avahi_close();
+#endif
 
 /* Offer a service. There's currently no way to withdraw it. */
 void e2avahi_announce(const char* service_name, const char* service_type, unsigned short port_num);
