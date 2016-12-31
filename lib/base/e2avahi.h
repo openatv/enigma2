eb19ddf5ef55ebfce32578a358539e804c4977bd
Mike Looijmans <milo@openpli.org>
2016-12-31 11:59:38 +0100

    e2avahi: Use hostname as service name
    It's rather unclear what service name is supposed to be, but other
    services like NFS and Samba use the host name there. Apparently the
    name+type must be unique on the network, or you'll get a collision.
    Implement this by defaulting to the local hostname when the name
    is blank or NULL.
    
    Advertise the streamserver as such.

--- lib/base/e2avahi.h
+++ lib/base/e2avahi.h
@@ -8,6 +8,8 @@ void e2avahi_init(eMainloop* reactor);
 void e2avahi_close();
 #endif
 
-/* Offer a service. There's currently no way to withdraw it. */
+/* Offer a service. There's currently no way to withdraw it. Leave
+ * service_name NULL or blank to use avahi's local hostname as service
+ * name (recommended, since it must be unique on the network) */
 void e2avahi_announce(const char* service_name, const char* service_type, unsigned short port_num);
 
