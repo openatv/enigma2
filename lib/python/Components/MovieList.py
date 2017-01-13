728aa37e8ffc1b8b23c55f3bccb5d98c60e07b6d
Persian Professionals <persianpros@yahoo.com>
2017-01-13 16:53:51 +0330

    Use frozenset for DVD_EXTENSIONS (#599)
    As suggested by littlesat.

--- lib/python/Components/MovieList.py
+++ lib/python/Components/MovieList.py
@@ -18,7 +18,7 @@ import skin
 from enigma import eListboxPythonMultiContent, eListbox, gFont, iServiceInformation, eSize, RT_HALIGN_LEFT, RT_HALIGN_RIGHT, RT_VALIGN_CENTER, eServiceReference, eServiceCenter, eTimer
 
 AUDIO_EXTENSIONS = frozenset((".dts", ".mp3", ".wav", ".wave", ".ogg", ".flac", ".m4a", ".mp2", ".m2a", ".wma", ".ac3", ".mka"))
-DVD_EXTENSIONS = ('.iso', '.img', '.nrg')
+DVD_EXTENSIONS = frozenset((".iso", ".img", ".nrg"))
 IMAGE_EXTENSIONS = frozenset((".jpg", ".png", ".gif", ".bmp", ".jpeg"))
 MOVIE_EXTENSIONS = frozenset((".mpg", ".vob", ".m4v", ".mkv", ".avi", ".divx", ".dat", ".flv", ".mp4", ".mov", ".wmv", ".m2ts", ".asf", ".3gp", ".3g2"))
 KNOWN_EXTENSIONS = MOVIE_EXTENSIONS.union(IMAGE_EXTENSIONS, DVD_EXTENSIONS, AUDIO_EXTENSIONS)
