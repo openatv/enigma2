d7db2c3903036b0a6017a976dbe1cec1a320425f
Huevos <rowland@moralet.com>
2017-01-28 16:48:16 +0100

    [GitLog] Use new branding module method "getFeedsUrl()" to get the feeds URL, rather than using hard coded values in the .py.

--- lib/python/Components/GitLog.py
+++ lib/python/Components/GitLog.py
@@ -1,15 +1,12 @@
 import urllib, socket
 from sys import modules
 from os import path
-from boxbranding import getImageDistro, getImageType, getImageVersion
+from boxbranding import getImageType, getFeedsUrl
 
 def fetchlog(logtype):
 	releasenotes = ""
 	try:
-		if getImageType() == 'release':
-			sourceurl = 'http://www.openvix.co.uk/feeds/%s/%s/%s/%s-git.log' % (getImageDistro(), getImageType(), getImageVersion(), logtype)
-		else:
-			sourceurl = 'http://openvixdev.dyndns.tv/feeds/%s/%s/%s/%s-git.log' % (getImageDistro(), getImageType(), getImageVersion(), logtype)
+		sourceurl = "%s/%s-git.log" % (getFeedsUrl().rsplit("/", 1)[0], logtype)
 		print "[GitLog]",sourceurl
 		sourcefile,headers = urllib.urlretrieve(sourceurl)
 		fd = open(sourcefile, 'r')
