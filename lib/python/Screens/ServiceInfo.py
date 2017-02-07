99bc3f0304b9ee3c63e604ef6defd6065f9c0161
Huevos <rowland@moralet.com>
2017-02-08 00:24:06 +0100

    [ServiceInfo] Amend last commit.
    Related commit: https://github.com/OpenViX/enigma2/commit/d19ebe43c611f398dc78ee41795dfc62a48db934

--- lib/python/Screens/ServiceInfo.py
+++ lib/python/Screens/ServiceInfo.py
@@ -280,14 +280,7 @@ class ServiceInfo(Screen):
 			else:
 				tuner = (_("NIM & Type"), chr(ord('A') + frontendData["tuner_number"]) + " - " + frontendData["tuner_type"], TYPE_TEXT)
 			if frontendDataOrg["tuner_type"] == "DVB-S":
-				if frontendData.get("is_id", -1) == -1: # not multistream
-					return (tuner,
-						(_("System & Modulation"), "%s %s" % (frontendData["system"], frontendData["modulation"]), TYPE_TEXT),
-						(_("Orbital position"), "%s" % frontendData["orbital_position"], TYPE_TEXT),
-						(_("Frequency & Polarization"), "%s - %s" % (frontendData.get("frequency", 0), frontendData["polarization"]), TYPE_TEXT),
-						(_("Symbol rate & FEC"), "%s - %s" % (frontendData.get("symbol_rate", 0), frontendData["fec_inner"]), TYPE_TEXT),
-						(_("Inversion, Pilot & Roll-off"), "%s - %s - %s" % (frontendData["inversion"], frontendData.get("pilot", None), str(frontendData.get("rolloff", None))), TYPE_TEXT))
-				else: # multistream
+				if frontendData.get("is_id", -1) > -1: # multistream
 					return (tuner,
 						(_("System & Modulation"), "%s %s" % (frontendData["system"], frontendData["modulation"]), TYPE_TEXT),
 						(_("Orbital position"), "%s" % frontendData["orbital_position"], TYPE_TEXT),
@@ -295,6 +288,13 @@ class ServiceInfo(Screen):
 						(_("Symbol rate & FEC"), "%s - %s" % (frontendData.get("symbol_rate", 0), frontendData["fec_inner"]), TYPE_TEXT),
 						(_("Input Stream ID"), "%s" % (frontendData.get("is_id", -1)), TYPE_TEXT),
 						(_("PLS Mode & PLS Code"), "%s - %s" % (frontendData["pls_mode"], frontendData["pls_code"]), TYPE_TEXT),
+						(_("Inversion, Pilot & Roll-off"), "%s - %s - %s" % (frontendData["inversion"], frontendData.get("pilot", None), str(frontendData.get("rolloff", None))), TYPE_TEXT))
+				else: # not multistream
+					return (tuner,
+						(_("System & Modulation"), "%s %s" % (frontendData["system"], frontendData["modulation"]), TYPE_TEXT),
+						(_("Orbital position"), "%s" % frontendData["orbital_position"], TYPE_TEXT),
+						(_("Frequency & Polarization"), "%s - %s" % (frontendData.get("frequency", 0), frontendData["polarization"]), TYPE_TEXT),
+						(_("Symbol rate & FEC"), "%s - %s" % (frontendData.get("symbol_rate", 0), frontendData["fec_inner"]), TYPE_TEXT),
 						(_("Inversion, Pilot & Roll-off"), "%s - %s - %s" % (frontendData["inversion"], frontendData.get("pilot", None), str(frontendData.get("rolloff", None))), TYPE_TEXT))
 			elif frontendDataOrg["tuner_type"] == "DVB-C":
 				return (tuner,
