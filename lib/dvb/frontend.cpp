c90aad6f3a113a03d5fcdeee0449fdc47416f695
dvb-adenin <dvb-adenin@gmx.de>
2017-02-08 22:30:37 +0100

    Early setting of the tuner type for non multitype tuner

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -855,6 +855,21 @@ int eDVBFrontend::openFrontend()
 		|| (m_delsys[SYS_DVBC_ANNEX_AC] && m_delsys[SYS_DVBS])
 		|| (m_delsys[SYS_DVBC_ANNEX_AC] && m_delsys[SYS_DVBT]);
 #endif
+	if(!m_multitype)
+	{
+		if(m_delsys[SYS_DVBS])
+			m_type = feSatellite;
+		else if(m_delsys[SYS_DVBT])
+			m_type = feTerrestrial;
+#if DVB_API_VERSION > 5 || DVB_API_VERSION == 5 && DVB_API_VERSION_MINOR >= 6
+		else if(m_delsys[SYS_DVBC_ANNEX_A])
+#else
+		else if(m_delsys[SYS_DVBC_ANNEX_AC])
+#endif
+			m_type = feCable;
+		else if(m_delsys[SYS_ATSC])
+			m_type = feATSC;
+	}
 	if(m_type == feSatellite)
 		setTone(iDVBFrontend::toneOff);
 	setVoltage(iDVBFrontend::voltageOff);
