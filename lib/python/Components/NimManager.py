d276593427095921aff8d3e587ff226b6336eedf
Huevos <rowland@moralet.com>
2017-02-09 12:45:28 +0100

    [NimManager] Bug fix: isMultistream()
    This check was returning False on frontend "0".

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -604,7 +604,7 @@ class NIM(object):
 		return (self.frontend_id is not None) or self.__is_empty
 
 	def isMultistream(self):
-		multistream = self.frontend_id and eDVBResourceManager.getInstance().frontendIsMultistream(self.frontend_id) or False
+		multistream = self.frontend_id is not None and eDVBResourceManager.getInstance().frontendIsMultistream(self.frontend_id) or False
 		# HACK due to poor support for VTUNER_SET_FE_INFO
 		# When vtuner does not accept fe_info we have to fallback to detection using tuner name
 		# More tuner names will be added when confirmed as multistream (FE_CAN_MULTISTREAM)
