5b7ac3e13a7f828f8a07afa70a25b4cf6372f033
Dimitrij <Dima-73@inbox.lv>
2017-02-11 16:26:42 +0200

    [frontend] add snr calculation Astrometa/os mega (#637)

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1066,10 +1066,9 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 	else if (!strcmp(m_description, "Broadcom BCM73XX") ||
 			 !strcmp(m_description, "FTS-260 (Montage RS6000)") ||
 			 !strcmp(m_description, "Panasonic MN88472") ||
-			 !strcmp(m_description, "Panasonic MN88473")) // xcore
+			 !strcmp(m_description, "Panasonic MN88473"))
 	{
 		ret = snr * 100 / 256;
-
 		if (!strcmp(m_description, "FTS-260 (Montage RS6000)"))
 			sat_max = 1490;
 	}
