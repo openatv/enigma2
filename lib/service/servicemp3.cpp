0714bf74308ad0e6fdf4f7b30b9daa7c697c3cd0
christophecvr <stefansat@telenet.be>
2017-02-22 11:22:21 +0100

    Reduce debug output
    	modified:   lib/service/servicemp3.cpp

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -2964,13 +2964,13 @@ void eServiceMP3::pushSubtitles()
 
 		if (m_decoder_time_valid_state < 4)
 		{
-			eDebug("[eServiceMP3] *** push subtitles, waiting for clock to stabilise");
+			//eDebug("[eServiceMP3] *** push subtitles, waiting for clock to stabilise");
 			m_prev_decoder_time = running_pts;
 			next_timer = 100;
 			goto exit;
 		}
 
-		eDebug("[eServiceMP3] *** push subtitles, clock stable");
+		//eDebug("[eServiceMP3] *** push subtitles, clock stable");
 	}
 
 	decoder_ms = running_pts / 90;
