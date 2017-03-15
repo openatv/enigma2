fcda7284349fd2351efd39798fb61cf6eb4e83c9
littlesat <littlesat99@yahoo.com>
2017-03-15 21:57:25 +0100

    Solve issue induced in dvbci.cpp for e.g. the VU+ Solo2
    When it has the old DMM7025 CI interface it should be limited to just 2
    tuners

--- lib/dvb_ci/dvbci.cpp
+++ lib/dvb_ci/dvbci.cpp
@@ -177,7 +177,7 @@ eDVBCIInterfaces::eDVBCIInterfaces()
 	for (eSmartPtrList<eDVBCISlot>::iterator it(m_slots.begin()); it != m_slots.end(); ++it)
 		it->setSource("A");
 
-	for (int tuner_no = 0; tuner_no < 26; ++tuner_no) // NOTE: this assumes tuners are A .. Z max.
+	for (int tuner_no = 0; tuner_no < (num_ci > 1 ? 26 : 2); ++tuner_no) // NOTE: this assumes tuners are A .. Z max.
 	{
 		path.str("");
 		path.clear();
