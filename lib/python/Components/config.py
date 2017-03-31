338a98ed48a63070fd1a436a770f7434d4782e7b
Andrew Blackburn <andyblac@icloud.com>
2017-03-31 12:48:52 +0100

    [config] fix bug that was causing ‘none immediate notifiers’ not to be called on save cancel.

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -91,12 +91,12 @@ class ConfigElement(object):
 		else:
 			self.saved_value = self.tostring(self.value)
 		if self.callNotifiersOnSaveAndCancel:
-			self.changed()
+			self.changedFinal()  # call none immediate_feedback notifiers, immediate_feedback Notifiers are called as they are chanaged, so do not need to be called here.
 
 	def cancel(self):
 		self.load()
 		if self.callNotifiersOnSaveAndCancel:
-			self.changed()
+			self.changedFinal()  # call none immediate_feedback notifiers, immediate_feedback Notifiers are called as they are chanaged, so do not need to be called here.
 
 	def isChanged(self):
 # NOTE - sv should already be stringified!
