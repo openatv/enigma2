9da551f6daacdcda7f7be53ee932e1e9485afd12
captain <openatv@gmail.com>
2017-04-04 16:34:21 +0200

    update socketmmi, on req from gutemine

--- lib/python/Plugins/Extensions/SocketMMI/SocketMMI.py
+++ lib/python/Plugins/Extensions/SocketMMI/SocketMMI.py
@@ -10,14 +10,22 @@ class SocketMMIMessageHandler:
 	def setSession(self, session):
 		self.session = session
 
-	def connected(self):
-		return socketmmi.getState(0)
+	def connected(self, slot=0):
+		return socketmmi.getState(slot)
 
-	def getName(self):
-		return socketmmi.getName(0)
+	def numConnections(self):                  
+		if socketmmi.getState(0):
+			return 1
+		else:
+			return 0
 
-	def startMMI(self):
-		slot = 0
+	def getState(self, slot=0):                                        
+		return socketmmi.getState(slot)
+
+	def getName(self, slot=0):
+		return socketmmi.getName(slot)
+
+	def startMMI(self, slot=0):
 		self.dlgs[slot] = self.session.openWithCallback(self.dlgClosed, MMIDialog, slot, 2, socketmmi, _("wait for mmi..."))
 
 	def socketStateChanged(self, slot):
@@ -30,4 +38,3 @@ class SocketMMIMessageHandler:
 	def dlgClosed(self, slot):
 		if slot in self.dlgs:
 			del self.dlgs[slot]
-
