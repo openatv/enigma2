854a2e5690ee512ff70898706ef4ab3b9385052d
Huevos <rowland@moralet.com>
2017-04-22 19:22:31 +0200

    [Multistream:ScanSetup.py] Clean up... ...so out-of-bounds "-1" values are not shown in the GUI.
    Test results:
    Non-multistream transport stream:
    t:011a0000:0802:0002,s:10847000:23000000:1:2:282:2:0:1:2:1:2
    
    Multistream transport stream
    t:0dde0000:0001:20fa,s:11509000:29500000:0:6:-50:2:0:1:2:0:2,MIS/PLS:1:50416:1
    
    This also fixes the same problem in satfinder.

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -887,7 +887,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 			"fec": eDVBFrontendParametersSatellite.FEC_Auto,
 			"fec_s2": eDVBFrontendParametersSatellite.FEC_9_10,
 			"modulation": eDVBFrontendParametersSatellite.Modulation_QPSK,
-			"is_id": -1,
+			"is_id": 0,
 			"pls_mode": eDVBFrontendParametersSatellite.PLS_Root,
 			"pls_code": 1 }
 		defaultCab = {
@@ -928,7 +928,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 					defaultSat["fec_s2"] = frontendData.get("fec_inner", eDVBFrontendParametersSatellite.FEC_Auto)
 					defaultSat["rolloff"] = frontendData.get("rolloff", eDVBFrontendParametersSatellite.RollOff_alpha_0_35)
 					defaultSat["pilot"] = frontendData.get("pilot", eDVBFrontendParametersSatellite.Pilot_Unknown)
-					defaultSat["is_id"] = frontendData.get("is_id", defaultSat["is_id"])
+					defaultSat["is_id"] = frontendData.get("is_id", defaultSat["is_id"]) > defaultSat["is_id"] and frontendData.get("is_id", defaultSat["is_id"]) or defaultSat["is_id"]
 					defaultSat["pls_mode"] = frontendData.get("pls_mode", eDVBFrontendParametersSatellite.PLS_Root)
 					defaultSat["pls_code"] = frontendData.get("pls_code", defaultSat["pls_code"])
 				else:
