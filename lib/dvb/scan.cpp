ea8f861d2608e4f58942d31bf15b9db4a2217b14
Athanasios Oikonomou <athoik@gmail.com>
2017-05-12 19:13:48 +0300

    scan: allow filtering network_id from NIT (#926)

--- lib/dvb/scan.cpp
+++ lib/dvb/scan.cpp
@@ -709,6 +709,12 @@ void eDVBScan::channelDone()
 		std::vector<NetworkInformationSection*>::const_iterator i;
 		for (i = m_NIT->getSections().begin(); i != m_NIT->getSections().end(); ++i)
 		{
+			if (m_networkid && m_networkid != (*i)->getNetworkId())
+			{
+				SCAN_eDebug("[eDVBScan] ignoring NetworkId %d!", (*i)->getNetworkId());
+				continue;
+			}
+
 			const TransportStreamInfoList &tsinfovec = *(*i)->getTsInfo();
 
 			for (TransportStreamInfoConstIterator tsinfo(tsinfovec.begin());
