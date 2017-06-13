3a613c19fa3f10574864ba01116ff5ed3cb742f4
rossi2000 <rossi_2000@hotmail.com>
2017-06-13 13:56:31 +0100

    [PIP] merge PLi Cosmetics.

--- lib/python/Screens/PictureInPicture.py
+++ lib/python/Screens/PictureInPicture.py
@@ -194,7 +194,7 @@ class PictureInPicture(Screen):
 				print "[PictureInPicture] playing pip service", ref and ref.toString()
 			else:
 				if not config.usage.hide_zap_errors.value:
-					Notifications.AddPopup(text = _("No free tuner available!"), type = MessageBox.TYPE_ERROR, timeout = 5, id = "ZapPipError")
+					Notifications.AddPopup(text = "PiP...\n" + _("No free tuner!"), type = MessageBox.TYPE_ERROR, timeout = 5, id = "ZapPipError")
 				return False
 			self.pipservice = eServiceCenter.getInstance().play(ref)
 			if self.pipservice and not self.pipservice.setTarget(1, True):
