b9851e7b6b776f0a6f54a47c50f91e5831055c9e
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2017-08-24 04:05:40 +0100

    [Picon] allow for different IPTV streams

--- lib/python/Components/Renderer/LcdPicon.py
+++ lib/python/Components/Renderer/LcdPicon.py
@@ -88,7 +88,7 @@ def getLcdPiconName(serviceName):
 		fields = sname.split('_', 3)
 		if len(fields) > 2 and fields[2] != '1': #fallback to 1 for services with different service types
 			fields[2] = '1'
-		if len(fields) > 0 and fields[0] == '4097': #fallback to 1 for IPTV streams
+		if len(fields) > 0 and fields[0] != '1': #fallback to 1 for IPTV streams
 			fields[0] = '1'
 		pngname = findLcdPicon('_'.join(fields))
 	if not pngname: # picon by channel name
--- lib/python/Components/Renderer/Picon.py
+++ lib/python/Components/Renderer/Picon.py
@@ -83,7 +83,7 @@ def getPiconName(serviceName):
 		fields = sname.split('_', 3)
 		if len(fields) > 2 and fields[2] != '1': #fallback to 1 for services with different service types
 			fields[2] = '1'
-		if len(fields) > 0 and fields[0] == '4097': #fallback to 1 for IPTV streams
+		if len(fields) > 0 and fields[0] != '1': #fallback to 1 for IPTV streams
 			fields[0] = '1'
 		pngname = findPicon('_'.join(fields))
 	if not pngname: # picon by channel name
