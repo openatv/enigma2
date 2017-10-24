62735afc3aa92aa9eb7ad2ad0a8609c1a9afa84c
Schimmelreiter <Ziggy.SpaceRat@gmx.de>
2017-10-24 14:51:30 +0200

    [networksetup] Tryfix for corrupted Samba installations Ever since OpenV... introduced silly new packagegroups, users manage to mess up their Samba installations by reinstalling what's already pre-installed.

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -2381,7 +2381,7 @@ class NetworkSamba(Screen):
 		self.my_Samba_active = False
 		self.my_Samba_run = False
 		self['actions'] = ActionMap(['WizardActions', 'ColorActions'], {'ok': self.close, 'back': self.close, 'red': self.UninstallCheck, 'green': self.SambaStartStop, 'yellow': self.activateSamba, 'blue': self.Sambashowlog})
-		self.service_name = basegroup + '-samba'
+		self.service_name = basegroup + '-smbfs-server'
 		self.onLayoutFinish.append(self.InstallCheck)
 
 	def InstallCheck(self):
