1fdb1ccb2c4701ed585ae8a6fdeedcdd2c9c9550
Athanasios Oikonomou <athoik@gmail.com>
2017-11-11 09:19:31 +0200

    Make public_str local variable in notifier
    Also change it to std::string.

--- lib/dvb/rtspstreamserver.cpp
+++ lib/dvb/rtspstreamserver.cpp
@@ -68,7 +68,6 @@
 
 static int global_stream_id = 0;
 
-char public_str[] = "Public: OPTIONS, DESCRIBE, SETUP, PLAY, TEARDOWN";
 const char *app_name = "enigma2";
 const char *version = "0.1";
 
@@ -1055,6 +1054,7 @@ void eRTSPStreamClient::notifier(int what)
 
 	ePtr<eRTSPStreamClient> ref = this;
 	std::string transport_reply;
+	std::string public_str = "Public: OPTIONS, DESCRIBE, SETUP, PLAY, TEARDOWN";
 
 	char tmpbuf[4096], buf[8192];
 	int len;
