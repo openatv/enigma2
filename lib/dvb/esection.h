8480643f269ea6ad694c1d23e237d86979748be4
Athanasios Oikonomou <athoik@gmail.com>
2018-02-07 22:34:53 +0200

    eAUGTable: add an empty virtual dtor
    It seems to silence the following warnings:
    dvb/eit.cpp:131:10: warning: deleting object of polymorphic class type "AUTable<eTable<ATSCEventInformationSection> >"
    which has non-virtual destructor might cause undefined behaviour [-Wdelete-non-virtual-dtor]

--- lib/dvb/esection.h
+++ lib/dvb/esection.h
@@ -94,6 +94,7 @@ class eAUGTable: public sigc::trackable
 protected:
 	void slotTableReady(int);
 public:
+	virtual ~eAUGTable(){};
 	sigc::signal1<void, int> tableReady;
 	virtual void getNext(int err)=0;
 };
