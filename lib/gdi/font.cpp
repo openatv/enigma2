openatv/enigma2

commit ccd9a562d7be6a526dc35dddd76980874898e145

Fixed bug in finding last nonspace in a rendered line
Caused problems with centered alignments.

Thanks to adoxa for the fix.

lib/gdi/font.cpp
@@ -1168,13 +1168,13 @@ void eTextPara::realign(int dir)	// der code hier ist ein wenig merkwuerdig.
 
 		ASSERT( end != glyphs.end());
 
-		glyphString::iterator nonspace_end(begin),  last_fixed(begin);
+		glyphString::iterator nonspace_end(begin);
 
 			// zeilenende suchen
 		do {
 			last=end;
 			++end;
-			if(!(last->flags&GS_ISSPACE) && (end != glyphs.end() || end->flags&GS_ISSPACE))
+			if(!(last->flags&GS_ISSPACE) && (end == glyphs.end() || end->flags&GS_ISSPACE))
 				nonspace_end = end;
 		} while ((end != glyphs.end()) && (!(end->flags&GS_ISFIRST)));
 			// end zeigt jetzt auf begin der naechsten zeile
@@ -1212,7 +1212,7 @@ void eTextPara::realign(int dir)	// der code hier ist ein wenig merkwuerdig.
 			int offset=area.width()-linelength;
 			if (dir==dirCenter)
 				offset/=2;
-			while (begin != end)
+			while (begin != nonspace_end)
 			{
 				begin->bbox.moveBy(offset,0);
 				begin->x += offset;

commit 1717cb20c314dd3dec5fdb0f586b6be89f0550a4

Fix realignment of trailing spaces
Fix a trailing space bug introduced in commit ccd9a56 - trailing
spaces need to be moved when realigning for dirCenterIfFits, dirRight,
dirCenter and dirBidi alignment types.

This was the cause of odd highlighting issues in the MinuteInput
popup box as reported in the forum, because un-moved trailing spaces
were highlighted.

There were also similar issues with not moving trailing spaces for
dirBlock.

lib/gdi/font.cpp
@@ -1212,7 +1212,7 @@ void eTextPara::realign(int dir)	// der code hier ist ein wenig merkwuerdig.
 			int offset=area.width()-linelength;
 			if (dir==dirCenter)
 				offset/=2;
-			while (begin != nonspace_end)
+			while (begin != end)
 			{
 				begin->bbox.moveBy(offset,0);
 				begin->x += offset;
@@ -1252,6 +1252,12 @@ void eTextPara::realign(int dir)	// der code hier ist ein wenig merkwuerdig.
 				begin->bbox.moveBy(curoff>>8,0);
 				if (doadd)
 					curoff+=off;
+				++begin;
+			}
+			curoff = (curoff+255) >> 8;
+			while (begin != end) {
+				begin->x+=curoff;
+				begin->bbox.moveBy(curoff,0);
 				++begin;
 			}
 			break;