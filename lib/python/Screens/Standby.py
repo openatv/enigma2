90d061e40488018bea1f46ae70c4bc42294d23eb
Captain <captain.onboard@web.de>
2018-08-20 19:07:03 +0200

    [clap] close HDMI when enter standy

--- lib/python/Screens/Standby.py
+++ lib/python/Screens/Standby.py
@@ -32,7 +32,7 @@ class Standby2(Screen):
 		if os.path.exists("/usr/script/StandbyLeave.sh"):
 			Console().ePopen("/usr/script/StandbyLeave.sh &")
 
-		if (getBrandOEM() in ('fulan')):
+		if (getBrandOEM() in ('fulan','clap')):
 			open("/proc/stb/hdmi/output", "w").write("on")
 		#set input to encoder
 		self.avswitch.setInput("ENCODER")
@@ -148,7 +148,7 @@ class Standby2(Screen):
 			self.avswitch.setInput("SCART")
 		else:
 			self.avswitch.setInput("AUX")
-		if (getBrandOEM() in ('fulan')):
+		if (getBrandOEM() in ('fulan','clap')):
 			open("/proc/stb/hdmi/output", "w").write("off")
 
 		if int(config.usage.hdd_standby_in_standby.value) != -1: # HDD standby timer value (box in standby) / -1 = same as when box is active
