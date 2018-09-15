4602b14f26873e2d56c0aca94bf5e69deb61a165
iPkay <iPKay@gmx.com>
2018-09-15 19:20:46 +0200

    [EpgSelection] don't zap when play a movie

--- lib/python/Screens/EpgSelection.py
+++ lib/python/Screens/EpgSelection.py
@@ -1846,6 +1846,8 @@ class EPGSelection(Screen, HelpableScreen):
 		self.close(True)
 
 	def zap(self):
+		if self.session.nav.getCurrentlyPlayingServiceOrGroup() and '0:0:0:0:0:0:0:0:0' in self.session.nav.getCurrentlyPlayingServiceOrGroup().toString():
+			return
 		if self.zapFunc:
 			self.zapSelectedService()
 			self.closeEventViewDialog()
@@ -1899,8 +1901,9 @@ class EPGSelection(Screen, HelpableScreen):
 
 	def zapTo(self):
 		if self.session.nav.getCurrentlyPlayingServiceOrGroup() and '0:0:0:0:0:0:0:0:0' in self.session.nav.getCurrentlyPlayingServiceOrGroup().toString():
-			from Screens.InfoBarGenerics import setResumePoint
-			setResumePoint(self.session)
+			#from Screens.InfoBarGenerics import setResumePoint
+			#setResumePoint(self.session)
+			return
 		if self.zapFunc:
 			self.zapSelectedService(True)
 			self.refreshTimer.start(2000)
