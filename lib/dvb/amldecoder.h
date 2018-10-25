fd595670d7c475f76beca13d25ae61416f0ab428
Captain <captain.onboard@web.de>
2018-10-25 07:11:30 +0200

    add new getMPEGDecode virtual function

--- lib/dvb/amldecoder.h
+++ lib/dvb/amldecoder.h
@@ -125,6 +125,7 @@ public:
 	int getVideoProgressive();
 	int getVideoFrameRate();
 	int getVideoAspect();
+	int getVideoGamma();
 	static RESULT setHwPCMDelay(int delay);
 	static RESULT setHwAC3Delay(int delay);
 };
