6db930276521371f2dc7233a69ebb83769fd8001
original-birdman <gordon.lack@dsl.pipex.com>
2018-11-29 03:18:56 +0000

    Use the correct variable in test (sv, not the non-existant val). (#336)
    (cherry picked from commit b197bb11acdfa5e8fd8011a906845a14363069fe)
    (cherry picked from commit fcf4e7a65f60864617960b71c7680dd0db63c352)

--- lib/python/Components/config.py
+++ lib/python/Components/config.py
@@ -1732,7 +1732,7 @@ class ConfigLocations(ConfigElement):
 	def isChanged(self):
 		sv = self.saved_value
 		locations = self.locations
-		if val is None and not locations:
+		if sv is None and not locations:
 			return False
 		return self.tostring([x[0] for x in locations]) != sv
 
