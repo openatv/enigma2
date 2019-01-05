631f794f77fe99cc1fe02e49669ca30a4b5c6a73
captain <captain.onboard@web.de>
2019-01-05 10:09:18 +0100

    servicemp3: handle sVideoType info thx athoik
    This commit handles the sVideoType information by using the new signal get-video-codec from dvbvideosink.
    Please note that enigma2 should include the extra stream types now (eg on lib/python/Screens/ServiceInfo.py).

--- lib/service/servicemp3.cpp
+++ lib/service/servicemp3.cpp
@@ -1731,6 +1731,14 @@ int eServiceMP3::getInfo(int w)
 		tag = "has-crc";
 		break;
 	case sBuffer: return m_bufferInfo.bufferPercent;
+	case sVideoType:
+	{
+		if (!dvb_videosink) return -1;
+		guint64 v = -1;
+		g_signal_emit_by_name(dvb_videosink, "get-video-codec", &v);
+		return (int) v;
+		break;
+	}
 	default:
 		return resNA;
 	}
