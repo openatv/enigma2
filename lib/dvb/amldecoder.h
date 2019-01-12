99b3d6120d895bdf7f1dd343fd0287b28f44a88c
captain <captain.onboard@web.de>
2019-01-12 15:59:00 +0100

    alien5 fix enum h265 too

--- lib/dvb/amldecoder.h
+++ lib/dvb/amldecoder.h
@@ -73,7 +73,7 @@ private:
 	codec_para_t m_codec;
 
 public:
-	enum { aMPEG, aAC3, aDTS, aAAC, aAACHE, aLPCM, aDTSHD, aDDP, MPEG2 = 0, MPEG4_H264, MPEG1, MPEG4_Part2, VC1, VC1_SM, H265_HEVC };
+	enum { aMPEG, aAC3, aDTS, aAAC, aAACHE, aLPCM, aDTSHD, aDDP,UNKNOWN = -1, MPEG2=0, MPEG4_H264, VC1 = 3, MPEG4_Part2, VC1_SM, MPEG1, H265_HEVC, AVS = 16 };
 	enum { pidNone = -1 };
 	eAMLTSMPEGDecoder(eDVBDemux *demux, int decoder);
 	virtual ~eAMLTSMPEGDecoder();
