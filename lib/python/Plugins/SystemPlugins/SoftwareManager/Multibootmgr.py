103f8524edc9aecb2da08976603c90b418f80da8
hd75hd <hd75hd.hd75hd@gmail.com>
2019-05-03 09:31:46 +0200

    [Multibootmgr.py] fix for translation

--- lib/python/Plugins/SystemPlugins/SoftwareManager/Multibootmgr.py
+++ lib/python/Plugins/SystemPlugins/SoftwareManager/Multibootmgr.py
@@ -112,11 +112,11 @@ class MultiBootWizard(Screen):
 		self.currentSelected = self["config"].l.getCurrentSelection()
 		if self.currentSelected[0][1] != "Queued":
 			if SystemInfo["HasRootSubdir"]:
-				message = _("Removal of this slot will not show in %s Gui.  Are you sure you want to delete image slot %s ?" %(getMachineBuild(), self.currentSelected[0][1]))
+				message = _("Removal of this slot will not show in %s Gui.  Are you sure you want to delete image slot %s ?") %(getMachineBuild(), self.currentSelected[0][1])
 				ybox = self.session.openWithCallback(self.doErase, MessageBox, message, MessageBox.TYPE_YESNO, default=True)
 				ybox.setTitle(_("Remove confirmation"))
 			else:
-				message = _("Are you sure you want to delete image slot %s ?" %self.currentSelected[0][1])
+				message = _("Are you sure you want to delete image slot %s ?") %self.currentSelected[0][1]
 				ybox = self.session.openWithCallback(self.doErase, MessageBox, message, MessageBox.TYPE_YESNO, default=True)
 				ybox.setTitle(_("Remove confirmation"))
 
