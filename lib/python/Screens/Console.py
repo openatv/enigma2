109784cd7ae1623d426dda6b0dec3943ba1e27bf
Jason Hood <jadoxa@yahoo.com.au>
2019-06-18 22:10:34 +1000

    Screens.Console: Add stop and hide options
    Use RED to ask if execution should be stopped (useful to terminate a
    rogue script from File Commander).  Use GREEN to hide the window (if
    expecting a script to run a while); any key (other than VOL+/- and
    MUTE) will show the window again (and otherwise ignore the key).
    (Requires corresponding skin changes.)

--- lib/python/Screens/Console.py
+++ lib/python/Screens/Console.py
@@ -1,7 +1,11 @@
-from enigma import eConsoleAppContainer
+from enigma import eConsoleAppContainer, eActionMap
 from Screens.Screen import Screen
 from Components.ActionMap import ActionMap
 from Components.ScrollLabel import ScrollLabel
+from Components.Sources.StaticText import StaticText
+from Screens.MessageBox import MessageBox
+from sys import maxint
+from keyids import KEYIDS
 
 class Console(Screen):
 
@@ -20,7 +24,9 @@ class Console(Screen):
 
 		self.title = title
 		self["text"] = ScrollLabel()
-		self["actions"] = ActionMap(["OkCancelActions", "DirectionActions"], {
+		self["key_red"] = StaticText(_("Stop"))
+		self["key_green"] = StaticText(_("Hide"))
+		self["actions"] = ActionMap(["OkCancelActions", "DirectionActions", "ColorActions"], {
 			"ok": self.cancel,
 			"cancel": self.cancel,
 			"up": self["text"].pageUp,
@@ -29,12 +35,17 @@ class Console(Screen):
 			"right": self["text"].pageDown,
 			"chplus": self.firstPage,
 			"chminus": self.lastPage,
+			"red": self.key_red,
+			"green": self.key_green,
 		}, -1)
+
+		self.run = -1
+		self.stop_msg = None
+		self.hidden = False
 
 		self.container = eConsoleAppContainer()
 		self.container.appClosed.append(self.runFinished)
 		self.container.dataAvail.append(self.dataAvail)
-		self.run = -1
 		self.onLayoutFinish.append(self.startRun)	# don't start before gui is finished
 
 	def firstPage(self):
@@ -57,7 +68,9 @@ class Console(Screen):
 		self.runFinished(0)
 
 	def runFinished(self, retval):
-		if retval:
+		if retval == "stop":
+			self.run = len(self.cmdlist) - 1
+		elif retval:
 			self.errorOccurred = True
 		self.run += 1
 		if self.run != len(self.cmdlist):
@@ -71,17 +84,55 @@ class Console(Screen):
 				end = "\n"
 			else:
 				end = "\n\n"
-			self["text"].appendText(end + _("Execution finished!"))
+			self["text"].appendText(end + (retval == "stop" and _("Execution stopped!") or _("Execution finished!")))
+			self["key_red"].text = _("Close")
+			self["key_green"].text = ""
+			if self.stop_msg:
+				self.stop_msg.close()
+			self.unhide()
+			self.hidden = None
 			if self.finishedCallback is not None:
 				self.finishedCallback()
 			if not self.errorOccurred and self.closeOnSuccess:
 				self.cancel()
+
+	def key_green(self):
+		if self.hidden is False:
+			self.hidden = True
+			self.hide()
+			eActionMap.getInstance().bindAction("", -maxint - 1, self.key_any)
+
+	def key_any(self, key, flag):
+		if key not in (KEYIDS["KEY_MUTE"], KEYIDS["KEY_VOLUMEUP"], KEYIDS["KEY_VOLUMEDOWN"]):
+			if flag == 1:	# break
+				self.unhide()
+			return 1
+
+	def unhide(self):
+		if self.hidden:
+			self.hidden = False
+			self.show()
+			eActionMap.getInstance().unbindAction("", self.key_any)
+
+	def key_red(self):
+		if self.run == len(self.cmdlist):
+			self.cancel()
+		else:
+			self.stop_msg = self.session.openWithCallback(self.cancelCB, MessageBox, _("Stop execution?"), type=MessageBox.TYPE_YESNO, default=False)
+
+	def cancelCB(self, ret=None):
+		self.stop_msg = None
+		if ret:
+			self.cancel()
 
 	def cancel(self):
 		if self.run == len(self.cmdlist):
 			self.close()
 			self.container.appClosed.remove(self.runFinished)
 			self.container.dataAvail.remove(self.dataAvail)
+		else:
+			self.container.kill()
+			self.runFinished("stop")
 
 	def dataAvail(self, str):
 		self["text"].appendText(str)
