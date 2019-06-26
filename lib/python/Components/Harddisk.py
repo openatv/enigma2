6c91c018c5e38983e02e1e5586b6d30fd8bdb2b0
badsystem <46809978+badsystem@users.noreply.github.com>
2019-06-26 11:59:28 +0300

    Update Harddisk.py

--- lib/python/Components/Harddisk.py
+++ lib/python/Components/Harddisk.py
@@ -203,8 +203,8 @@ class Harddisk:
 		if cap == 0:
 			return ""
 		if cap < 1000:
-			return "%03d MB" % cap
-		return "%d.%03d GB" % (cap/1000, cap%1000)
+			return _("%03d MB") % cap
+		return _("%d.%03d GB") % (cap/1000, cap%1000)
 
 	def model(self):
 		try:
