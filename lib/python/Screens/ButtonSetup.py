61b490d1df0d3e053461e25f2a9f2e1b302d4aa4
Littlesat <reijng1@medtronic.com>
2019-07-01 16:00:24 +0200

    ButtonSetup: Do not use from x import * as this may import a lot

--- lib/python/Screens/ButtonSetup.py
+++ lib/python/Screens/ButtonSetup.py
@@ -613,13 +613,13 @@ class InfoBarButtonSetup():
 					return 0
 			elif selected[0] == "Module":
 				try:
-					exec "from " + selected[1] + " import *"
-					exec "self.session.open(" + ",".join(selected[2:]) + ")"
+					exec "from %s import %s" % (selected[1], selected[2])
+					exec "self.session.open(%s)" %  ",".join(selected[2:])
 				except:
 					print "[ButtonSetup] error during executing module %s, screen %s" % (selected[1], selected[2])
 			elif selected[0] == "Setup":
-				exec "from Screens.Setup import *"
-				exec "self.session.open(Setup, \"" + selected[1] + "\")"
+				from Screens.Setup import Setup
+				exec "self.session.open(Setup, \"%s\")" % selected[1]
 			elif selected[0].startswith("Zap"):
 				if selected[0] == "ZapPanic":
 					self.servicelist.history = []
