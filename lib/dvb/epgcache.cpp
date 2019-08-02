8725c814f180121a56108bf6a5f10799d4243523
LraiZer <lbound@hotmail.com>
2019-08-02 14:33:31 +0100

    epgcache: opentv promote variable declaration
    default dictionary read check should always be declared at beginning of start EPG
    
    (cherry picked from commit bdfe4202b7953b050253b9d5981229910f76f33e)

--- lib/dvb/epgcache.cpp
+++ lib/dvb/epgcache.cpp
@@ -1640,6 +1640,7 @@ void eEPGCache::channel_data::startEPG()
 		cleanupFreeSat();
 #endif
 #ifdef ENABLE_OPENTV
+		huffman_dictionary_read = false;
 		cleanupOPENTV();
 #endif
 	eDVBSectionFilterMask mask;
@@ -1807,7 +1808,6 @@ void eEPGCache::channel_data::startEPG()
 	{
 		char dictionary[256];
 		memset(dictionary, '\0', 256);
-		huffman_dictionary_read = false;
 
 		//load correct EPG dictionary data "otv_namespace_onid_tsid.dict"
 		sprintf (dictionary, "/usr/share/enigma2/otv_%08x_%04x_%04x.dict",
