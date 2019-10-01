b7e3c1ca26c655f98c17d2a993761448c275aae1
RAED <rrrr53@hotmail.com>
2019-10-01 21:13:13 +0300

    Update PluginComponent.py

--- lib/python/Components/PluginComponent.py
+++ lib/python/Components/PluginComponent.py
@@ -132,6 +132,12 @@ class PluginComponent:
 		for p in self.getPlugins(PluginDescriptor.WHERE_MENU):
 			res += p(menuid)
 		return res
+	
+	def getDescriptionForMenuEntryID(self, menuid, entryid):
+		 for p in self.getPlugins(PluginDescriptor.WHERE_MENU):
+			if p(menuid) and isinstance(p(menuid), (list, tuple)):
+				if p(menuid)[0][2] == entryid:
+					return p.description
 
 	def clearPluginList(self):
 		self.pluginList = []
