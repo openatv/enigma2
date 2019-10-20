c3a9203a0884215bbcb86b5ef5e694e267b30193
captain <captain.onboard@web.de>
2019-10-20 08:51:55 +0200

    Revert "[icetv] add openATV PVR ID"
    This reverts commit 24cc110e0fdf22c27257760c53db3fe3c0430332.
    
    use 39 as fallback for Enigma2 PVR

--- lib/python/Plugins/SystemPlugins/IceTV/__init__.py
+++ lib/python/Plugins/SystemPlugins/IceTV/__init__.py
@@ -8,22 +8,19 @@ License: Proprietary / Commercial - contact enigma.licensing (at) urbanec.net
 '''
 
 from enigma import eEPGCache
-from boxbranding import getMachineBrand, getMachineName, getImageDistro
+from boxbranding import getMachineBrand, getMachineName
 from Components.config import config, ConfigSubsection, ConfigNumber, ConfigText, \
     ConfigPassword, ConfigSelection, NoSave, configfile, ConfigYesNo, \
     ConfigSelectionNumber
 
 def getIceTVDeviceType():
-    if getImageDistro() == "openatv":
-        return "39"
-    else:
-        return {
-            ("Beyonwiz", "T2"): 31,
-            ("Beyonwiz", "T3"): 22,
-            ("Beyonwiz", "T4"): 30,
-            ("Beyonwiz", "U4"): 36,
-            ("Beyonwiz", "V2"): 38,
-        }.get((getMachineBrand(), getMachineName()), 22)
+    return {
+        ("Beyonwiz", "T2"): 31,
+        ("Beyonwiz", "T3"): 22,
+        ("Beyonwiz", "T4"): 30,
+        ("Beyonwiz", "U4"): 36,
+        ("Beyonwiz", "V2"): 38,
+    }.get((getMachineBrand(), getMachineName()), 39)
 
 config.plugins.icetv = ConfigSubsection()
 
