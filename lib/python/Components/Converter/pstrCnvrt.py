648a1007bbe2cbdf4421c79969b580a08f40ff47
arn354 <arn354@ich.ms>
2020-01-06 16:22:48 +0100

    Update pstrCnvrt.py
    convert special characters - thx 4l3x2k - see https://www.opena.tv/skins/50390-poster-imdb-rating-event-info-source-files-post425129.html#post425129

--- lib/python/Components/Converter/pstrCnvrt.py
+++ lib/python/Components/Converter/pstrCnvrt.py
@@ -7,6 +7,7 @@ import json
 import re
 import os
 import urllib2
+from urllib import quote
 
 if not os.path.isdir('/tmp/poster'):
 	os.mkdir('/tmp/poster')
@@ -52,7 +53,7 @@ class pstrCnvrt(Converter, object):
 	text = property(getText)
 
 	def searchPoster(self):
-		url_json = "https://api.themoviedb.org/3/search/%s?api_key=3c3efcf47c3577558812bb9d64019d65&query=%s"%(self.srch, self.evntNm)
+		url_json = "https://api.themoviedb.org/3/search/%s?api_key=3c3efcf47c3577558812bb9d64019d65&query=%s"%(self.srch, quote(self.evntNm))
 		jp = json.load(urllib2.urlopen(url_json))
 
 		imgP = (jp['results'][0]['poster_path'])
@@ -74,4 +75,4 @@ class pstrCnvrt(Converter, object):
 					return "S"+seg.group(2).zfill(2)+"E"+seg.group(1).zfill(2)
 				else :
 					return "S"+seg.group(1).zfill(2)+"E"+seg.group(2).zfill(2)
-		return ""
+		return ""
