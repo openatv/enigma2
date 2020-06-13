167be85287f46dcc270ec3760dcbcd097e7e1a6c
Betacentauri <betacentauri@arcor.de>
2020-06-13 14:36:58 +0200

    Fix parseactions.py
    Without it actionid.h is empty. This causes a malfunction
    in listboxes.

--- lib/actions/parseactions.py
+++ lib/actions/parseactions.py
@@ -4,13 +4,13 @@ import tokenize, sys
 
 def filter(g):
 	while True:
-		t = g.next()
+		t = next(g)
 		if t[1] == "/*":
-			while g.next()[1] != "*/":
+			while next(g)[1] != "*/":
 				pass
 			continue
 		if t[1] == "//":
-			while g.next()[1] != "\n":
+			while next(g)[1] != "\n":
 				pass
 			continue
 
@@ -31,12 +31,12 @@ def do_file(f, mode):
 
 	while True:
 		try:
-			t = tokens.next()
-		except:
+			t = next(tokens)
+		except Exception as e:
 			break
 
 		if t == "class":
-			classname = tokens.next()
+			classname = next(tokens)
 			classstate = state
 
 		if t == "{":
@@ -46,15 +46,15 @@ def do_file(f, mode):
 			state -= 1
 
 		if t == "enum" and state == classstate + 1:
-			actionname = tokens.next()
+			actionname = next(tokens)
 
 			if actionname == "{":
-				while tokens.next() != "}":
+				while next(tokens) != "}":
 					pass
 				continue
 
 			if actionname[-7:] == "Actions":
-				if tokens.next() != "{":
+				if next(tokens) != "{":
 					try:
 						print(classname)
 					except:
@@ -71,11 +71,11 @@ def do_file(f, mode):
 
 				while True:
 
-					t = tokens.next()
+					t = next(tokens)
 
 					if t == "=":
-						tokens.next()
-						t = tokens.next()
+						next(tokens)
+						t = next(tokens)
 
 					if t == "}":
 						break
@@ -83,7 +83,7 @@ def do_file(f, mode):
 					if counter:
 						if t != ",":
 							raise Exception("no comma")
-						t = tokens.next()
+						t = next(tokens)
 
 					if firsthit:
 
