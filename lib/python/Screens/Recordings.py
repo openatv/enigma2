d30455cd4ea93f0bde83988f3b766fc61b4ca963
Jörg Bleyel <jbleyel@gmx.net>
2020-06-18 20:30:19 +0200

    Fix encoding

--- lib/python/Screens/Recordings.py
+++ lib/python/Screens/Recordings.py
@@ -12,7 +12,7 @@ from Components.UsageConfig import preferredPath
 from Components.Sources.Boolean import Boolean
 from Components.Sources.StaticText import StaticText
 from Components.SystemInfo import SystemInfo
-
+import six
 
 class SetupSummary(Screen):
 	def __init__(self, session, parent):
@@ -38,7 +38,7 @@ class SetupSummary(Screen):
 		if hasattr(self.parent, "getCurrentDescription"):
 			self.parent["description"].text = self.parent.getCurrentDescription()
 		if 'footnote' in self.parent:
-			if self.parent.getCurrentEntry().endswith('*'):
+			if six.ensure_str(self.parent.getCurrentEntry()).endswith('*'):
 				self.parent['footnote'].text = (_("* = Restart Required"))
 			else:
 				self.parent['footnote'].text = (_(" "))
@@ -63,7 +63,7 @@ class RecordingSettings(Screen, ConfigListScreen):
 			if x.get("key") != self.setup:
 				continue
 			self.addItems(list, x)
-			self.setup_title = x.get("title", "").encode("UTF-8")
+			self.setup_title = six.ensure_str(x.get("title", ""))
 			self.seperation = int(x.get('separation', '0'))
 
 	def __init__(self, session):
@@ -310,8 +310,8 @@ class RecordingSettings(Screen, ConfigListScreen):
 				if requires and not SystemInfo.get(requires, False):
 					continue
 
-				item_text = _(x.get("text", "??").encode("UTF-8"))
-				item_description = _(x.get("description", " ").encode("UTF-8"))
+				item_text = _(six.ensure_str(x.get("text", "??")))
+				item_description = _(six.ensure_str(x.get("description", " ")))
 				b = eval(x.text or "")
 				if b == "":
 					continue
