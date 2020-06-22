8362e8c71f5ec3b553e345a18d405a49fac9e71e
Jörg Bleyel <jbleyel@gmx.net>
2020-06-22 13:55:49 +0200

    Fix timerlist

--- lib/python/Screens/TimerEdit.py
+++ lib/python/Screens/TimerEdit.py
@@ -237,19 +237,21 @@ class TimerEditList(Screen):
 
 	def fillTimerList(self):
 		#helper function to move finished timers to end of list
+		def _cmp(a, b):
+			return (a > b) - (a < b)
 		def eol_compare(x, y):
 			if x[0].state != y[0].state and x[0].state == RealTimerEntry.StateEnded or y[0].state == RealTimerEntry.StateEnded:
-				return cmp(x[0].state, y[0].state)
-			return cmp(x[0].begin, y[0].begin)
+				return _cmp(x[0].state, y[0].state)
+			return _cmp(x[0].begin, y[0].begin)
 
-		list = self.list
-		del list[:]
-		list.extend([(timer, False) for timer in self.session.nav.RecordTimer.timer_list])
-		list.extend([(timer, True) for timer in self.session.nav.RecordTimer.processed_timers])
+		_list = self.list
+		del _list[:]
+		_list.extend([(timer, False) for timer in self.session.nav.RecordTimer.timer_list])
+		_list.extend([(timer, True) for timer in self.session.nav.RecordTimer.processed_timers])
 		if config.usage.timerlist_finished_timer_position.index: #end of list
-			list.sort(key=cmp_to_key(eol_compare))
+			_list.sort(key=cmp_to_key(eol_compare))
 		else:
-			list.sort(key = lambda x: x[0].begin)
+			_list.sort(key = lambda x: x[0].begin)
 
 	def getEPGEvent(self, timer):
 		event = None
