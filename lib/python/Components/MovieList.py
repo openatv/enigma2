16a23d2917992c4737e38348b02f8dedb72227d9
Captain <captain.onboard@web.de>
2020-08-09 14:28:54 +0200

    movielist fix TypeError: '<' not supported between instances of 'float' and 'NoneType'

--- lib/python/Components/MovieList.py
+++ lib/python/Components/MovieList.py
@@ -782,7 +782,7 @@ class MovieList(GUIComponent):
 	def buildBeginTimeSortKey(self, x):
 		ref = x[0]
 		if ref.flags & eServiceReference.mustDescent:
-			return 0, x[1] and -os.stat(ref.getPath()).st_mtime
+			return (0, x[1] and x[1].getName(ref).lower() or "")
 		return 1, "", -x[2]
 
 	def buildGroupwiseSortkey(self, x):
