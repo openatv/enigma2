3628269c11d690e33e73be84cedd7966779a3c93
wedebe <9741693+wedebe@users.noreply.github.com>
2020-08-21 22:04:02 +0100

    This change prevents non-translatable files being added to po file refs

--- po/tools/cleanUpPoFiles.py
+++ po/tools/cleanUpPoFiles.py
@@ -129,14 +129,14 @@ def getIncludedExcludedPaths(root, dirs, files):
 
 def indicateProgress():
   global idx
-  print "\r" + prgChars[idx],
+  print("\r" + prgChars[idx]),
   idx = (idx + 1) % len(prgChars)
 
 nonTranslatableFiles = []
 
 def isFileTranslatable(fPath, data):
   isFound = False
-  regex_gettext = r'([^_]_\(|gettext\()'
+  regex_gettext = r'([^_]_\(|gettext\(|text=)'
   if re.search(regex_gettext, data):
     isFound = True
   else:
@@ -174,22 +174,22 @@ def searchCodebaseForOccurrences(poFile):
   if prefs['searchCodebaseForOccurrences']:
     unCachedEntries = getUncachedEntries(poFile)
     if len(unCachedEntries) > 0: 
-      print "Searching for %d occurrences..." % len(unCachedEntries)
+      print("Searching for %d occurrences..." % len(unCachedEntries))
       for root, dirs, files in os.walk(codeBasePath, topdown=True, onerror=None):
         for fPath in getIncludedExcludedPaths(root, dirs, files):
+          fPath = os.path.abspath(fPath)
           if fPath in nonTranslatableFiles:
-            next
+            continue
 
           indicateProgress()
           sys.stdout.flush()
-          fPath = os.path.abspath(fPath)
           try:
             size = os.stat(fPath).st_size
             f2 = open(fPath)
             data = mmap.mmap(f2.fileno(), size, access=mmap.ACCESS_READ)
 
             if not isFileTranslatable(fPath, data):
-              next
+              continue
 
             entryIndex = 0
             for poEntry in unCachedEntries:
@@ -209,6 +209,8 @@ def searchCodebaseForOccurrences(poFile):
                   fPathLastPart = fPathSplit[len(fPathSplit) - 2] + "/" + fPathLastPart
                 occurrencesCache[encodedMsgid] = occurrencesCache[encodedMsgid] + [(fPathLastPart, None)]
                 poEntry.occurrences = occurrencesCache[encodedMsgid]
+              else:
+                tcomment = "not found in source code"
           except UnicodeDecodeError:
             # found non-text data
             pass
@@ -255,7 +257,7 @@ def normaliseAllPoFiles(filesGlob):
       poFile = polib.pofile(fileName)
       poFile.wrapwidth = 1024 # avoid re-wrapping
       poFile.check_for_duplicates = True
-      print ("\rNormalising translation files..." + " {0:.0%}".format(float(fileIndex) / len(sorted(filesGlob)))),
+      print("\rNormalising translation files..." + " {0:.0%}".format(float(fileIndex) / len(sorted(filesGlob)))),
       sys.stdout.flush()
       
       for cacheEntry in sorted(occurrencesCache, key=lambda r: r[0]):
@@ -270,7 +272,6 @@ def normaliseAllPoFiles(filesGlob):
             )
             poFile.append(newEntry)
           except:
-            print "error adding"
             pass
         elif len(matchedEntries) == 1 and matchedEntries[0].obsolete:
           # matchedEntries[0].obsolete = False
@@ -285,29 +286,27 @@ def main():
     poFilesGlob = sorted(filesGlob, key=os.path.getsize, reverse=True)
     fileIndex = 0
     os.system('clear')
-    print "Running... (first file will take substantially longer as there's no cache)"
+    print("Running... (first file will take substantially longer as there's no cache)")
 
     for fileName in poFilesGlob:
       fileIndex = fileIndex + 1
       baseFileName = os.path.basename(fileName)
-      print ("Processing file %3d" % fileIndex) + "/" + str(fileCountStr) + " (" + baseFileName + ")"
+      print(("\nProcessing file %3d" % fileIndex) + "/" + str(fileCountStr) + " (" + baseFileName + ")")
       poFile = processPoFile(fileName)
       poFile.save(fileName + prefs['newFileExt'])
       addToPoStats(baseFileName, poFile)
     if prefs['outputFinalStats']:
-      print ""
       rowFormat = "{:>12} " * (len(poStats['columnHeadings']) + 1)
-      print(rowFormat.format("File", *poStats['columnHeadings']))
+      print("\n" + rowFormat.format("File", *poStats['columnHeadings']))
       rowsByName = sorted([line for line in zip(poStats['rowTitles'], poStats['data'])], key=lambda r: r[0])
       for pfs, row in rowsByName:
-        print rowFormat.format(pfs, *row)
-    print ""
+        print(rowFormat.format(pfs, *row))
     normaliseAllPoFiles(poFilesGlob)
     hours, remainder = divmod(timedelta(seconds = time.time() - startTime).seconds, 3600)
     minutes, seconds = divmod(remainder, 60)
-    print "\nComplete in " + '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds)) + "!\n"
+    print("\nComplete in " + '{:02}:{:02}:{:02}'.format(int(hours), int(minutes), int(seconds)) + "!\n")
   except KeyboardInterrupt:
-    print "\nBye!"
+    print("\nBye!")
 
 
 if __name__ == "__main__":
