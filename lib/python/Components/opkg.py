e4e6340123cf4eed481b215613e95fd2c168a1fc
Captain <captain.onboard@web.de>
2020-08-25 07:00:32 +0200

    Opkg hide  '--pycache--' files

--- lib/python/Components/opkg.py
+++ lib/python/Components/opkg.py
@@ -26,7 +26,7 @@ def enumPlugins(filter_start=''):
 					package = line.split(":", 1)[1].strip()
 					version = ''
 					description = ''
-					if package.startswith(filter_start) and not package.endswith('-dev') and not package.endswith('-staticdev') and not package.endswith('-dbg') and not package.endswith('-doc') and not package.endswith('-src'):
+					if package.startswith(filter_start) and not package.endswith('-dev') and not package.endswith('-staticdev') and not package.endswith('-dbg') and not package.endswith('-doc') and not package.endswith('-src') and not package.endswith('--pycache--'):
 						continue
 					package = None
 				if package is None:
