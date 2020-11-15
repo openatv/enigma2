eb8e0d6b47bce0b59a7866cc9b12b33963047754
Peter Urbanec <bitbucket.org@urbanec.net>
2020-11-16 00:10:14 +1100

    pmt: Add ASSERT checks to help track crash reports
    Forum user reports the following crash:
    
    {2090}<  4757.797> [eDVBScan] tuner data from frontend: freq 226500000, ber 0x0, snr 0x11a, pwr 0x72c0
    {2090}<  4757.802> Backtrace:
    {2090}<  4757.803> /usr/bin/enigma2(_Z17handleFatalSignaliP9siginfo_tPv) [0x6ebac]
    {2090}<  4757.803> /lib/libc.so.6(__default_rt_sa_restorer) [0xb51d5da0]
    {2090}<  4757.803> /usr/bin/enigma2(_ZN21eDVBServicePMTHandler12SDTScanEventEi) [0x108c80]
    {2090}<  4757.803> /usr/bin/enigma2(_ZN8eDVBScan11nextChannelEv) [0x11e3c4]
    {2090}<  4757.803> /usr/bin/enigma2(_ZN8eDVBScan11channelDoneEv) [0x123aa8]
    {2090}<  4757.803> /usr/bin/enigma2(_ZN7eGTable11sectionReadEPKh) [0xe9084]
    {2090}<  4757.803> /usr/bin/enigma2(_ZN17eDVBSectionReader4dataEi) [0xb06b8]
    {2090}<  4757.805> /usr/bin/enigma2(_ZN9eMainloop15processOneEventEjPP7_object9ePyObject) [0x79984]
    {2090}<  4757.805> /usr/bin/enigma2(_ZN9eMainloop7iterateEjPP7_object9ePyObject) [0x79f30]
    {2090}<  4757.805> /usr/bin/enigma2(_ZN9eMainloop4pollE9ePyObjectS0_) [0x7a2f8]
    {2090}<  4757.806> /usr/bin/enigma2(n/a) [0x1ac990]
    {2090}<  4757.806> -------FATAL SIGNAL (11)
    
    Demangled stack trace:
    
    {2090}<  4757.803> /usr/bin/enigma2(handleFatalSignal(int, siginfo_t*, void*)) [0x6ebac]
    {2090}<  4757.803> /lib/libc.so.6(__default_rt_sa_restorer) [0xb51d5da0]
    {2090}<  4757.803> /usr/bin/enigma2(eDVBServicePMTHandler::SDTScanEvent(int)) [0x108c80]
    {2090}<  4757.803> /usr/bin/enigma2(eDVBScan::nextChannel()) [0x11e3c4]
    {2090}<  4757.803> /usr/bin/enigma2(eDVBScan::channelDone()) [0x123aa8]
    {2090}<  4757.803> /usr/bin/enigma2(eGTable::sectionRead(unsigned char const*)) [0xe9084]
    {2090}<  4757.803> /usr/bin/enigma2(eDVBSectionReader::data(int)) [0xb06b8]
    {2090}<  4757.805> /usr/bin/enigma2(eMainloop::processOneEvent(unsigned int, _object**, ePyObject)) [0x79984]
    {2090}<  4757.805> /usr/bin/enigma2(eMainloop::iterate(unsigned int, _object**, ePyObject)) [0x79f30]
    {2090}<  4757.805> /usr/bin/enigma2(eMainloop::poll(ePyObject, ePyObject)) [0x7a2f8]
    {2090}<  4757.806> /usr/bin/enigma2(n/a) [0x1ac990]

--- lib/dvb/pmt.cpp
+++ lib/dvb/pmt.cpp
@@ -792,12 +792,15 @@ void eDVBServicePMTHandler::SDTScanEvent(int event)
 		case eDVBScan::evtFinish:
 		{
 			ePtr<iDVBChannelList> db;
+			ASSERT(m_resourceManager != NULL);
 			if (m_resourceManager->getChannelList(db) != 0)
 				eDebug("[eDVBServicePMTHandler] no channel list");
 			else
 			{
 				eDVBChannelID chid, curr_chid;
+				ASSERT(m_reference != NULL);
 				m_reference.getChannelID(chid);
+				ASSERT(m_dvb_scan != NULL);
 				curr_chid = m_dvb_scan->getCurrentChannelID();
 				if (chid == curr_chid)
 				{
