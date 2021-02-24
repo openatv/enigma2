2f00acf2aa35f7684ca812e5fa981c2134abe391
prl <prl@ozemail.com.au>
2021-02-24 12:34:58 +1100

    [various Converters,ServiceInfo] Fix string colour escapes
    Change string "\c" colour escape sequences to use "A".."F" and
    "a".."f" for hex values 0xA..0xF, instead of using :;<=>? to represent
    0xA..0xF.
    
    This also takes code from OpenViX to allow the colours for the
    STRING and USE_TUNERS_STRING conversions in Converter.FrontendInfo
    and the CryptoInfo conversion in Converter.PliExtraInfo to be
    defined as skin <parameter/> elements.

--- lib/python/Screens/ServiceInfo.py
+++ lib/python/Screens/ServiceInfo.py
@@ -287,7 +287,7 @@ class ServiceInfo(AboutBase):
 					extra_info = "extra data=%s" % caid[2]
 			from Tools.GetEcmInfo import GetEcmInfo
 			ecmdata = GetEcmInfo().getEcmData()
-			color = "\c00??;?00" if caid[1] == int(ecmdata[3], 16) and caid[0] == int(ecmdata[1], 16) else ""
+			color = "\c00ffbf00" if caid[1] == int(ecmdata[3], 16) and caid[0] == int(ecmdata[1], 16) else ""
 			tlist.append(ServiceInfoListEntry("%sECMPid %04X (%d) %04X-%s %s" % (color, caid[1], caid[1], caid[0], CaIdDescription, extra_info)))
 		if not tlist:
 			tlist.append(ServiceInfoListEntry(_("No ECMPids available (FTA Service)")))
