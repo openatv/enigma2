20dc8a7b94d1bc3581db4626ede03356ec103e8d
Captain <captain.onboard@web.de>
2021-04-04 16:41:44 +0200

    trackinfo update imports

--- lib/python/Components/Converter/TrackInfo.py
+++ lib/python/Components/Converter/TrackInfo.py
@@ -1,6 +1,7 @@
+from __future__ import absolute_import
 from Components.Converter.Converter import Converter
 from Components.Element import cached
-from Poll import Poll
+from Components.Converter.Poll import Poll
 from Tools.ISO639 import LanguageCodes
 from Tools.Directories import fileExists
 
