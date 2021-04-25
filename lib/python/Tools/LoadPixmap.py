8f63b4fef580d602607068b803a1e0802b9bc516
Jörg Bleyel <jbleyel@gmx.net>
2021-04-25 16:41:48 +0200

    switch to pixmapcache

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,28 +1,28 @@
 from enigma import loadPNG, loadJPG, loadSVG
 
-pixmap_cache = {}
 
-
-def LoadPixmap(path, desktop=None, cached=False,  width=0, height=0):
-	if path in pixmap_cache:
-		return pixmap_cache[path]
+# If cached is not supplied, LoadPixmap defaults to caching PNGs and not caching JPGs
+# Split alpha channel JPGs are never cached as the C++ layer's caching is based on
+# a single file per image in the cache
+def LoadPixmap(path, desktop=None, cached=None, width=0, height=0):
 	if path[-4:] == ".png":
-		ptr = loadPNG(path)
+		# cache unless caller explicity requests to not cache
+		ptr = loadPNG(path, 0, 0 if cached is False else 1)
 	elif path[-4:] == ".jpg":
-		ptr = loadJPG(path)
+		# don't cache unless caller explicity requests caching
+		ptr = loadJPG(path, 1 if cached is True else 0)
 	elif path[-4:] == ".svg":
 		from skin import getSkinFactor # imported here to avoid circular import
 		# autoscale = int(parameters.get("AutoscaleSVG", -1)) # skin_default only == -1, disabled == 0 or enabled == 1
 		autoscale = -1
 		scale = height == 0 and (autoscale == -1 and "/skin_default/" in path or autoscale == 1) and getSkinFactor() or 0
-		ptr = loadSVG(path, 0, width, height, scale)
+		ptr = loadSVG(path, 0 if cached is False else 1, width, height, scale)
 	elif path[-1:] == ".":
-		alpha = loadPNG(path + "a.png")
-		ptr = loadJPG(path + "rgb.jpg", alpha)
+		# caching mechanism isn't suitable for multi file images, so it's explicitly disabled
+		alpha = loadPNG(path + "a.png", 0, 0)
+		ptr = loadJPG(path + "rgb.jpg", alpha, 0)
 	else:
-		raise Exception("neither .png nor .jpg, please fix file extension")
+		raise Exception("Neither .png nor .jpg nor .svg, please fix file extension")
 	if ptr and desktop:
 		desktop.makeCompatiblePixmap(ptr)
-	if cached:
-		pixmap_cache[path] = ptr
 	return ptr
