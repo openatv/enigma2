04bc84efb64d3b7e93c2ebce50fd090ce8691d70
Jörg Bleyel <jbleyel@gmx.net>
2021-04-25 15:58:06 +0200

    introduce loadSVG and loadImage / ePixmap allow PNG,JPG and SVG

--- lib/gui/epixmap.cpp
+++ lib/gui/epixmap.cpp
@@ -36,11 +36,11 @@ void ePixmap::setPixmap(ePtr<gPixmap> &pixmap)
 
 void ePixmap::setPixmapFromFile(const char *filename)
 {
-	loadPNG(m_pixmap, filename);
+	loadImage(m_pixmap, filename, m_scale, m_scale ? size().width() : 0, m_scale ? size().height() : 0);
 
 	if (!m_pixmap)
 	{
-		eDebug("[ePixmap] setPixmapFromFile: loadPNG failed");
+		eDebug("[ePixmap] setPixmapFromFile: load %s failed", filename);
 		return;
 	}
 
