91d724a048c5d08b1a3f46ff59c3b4674244259a
jbleyel <jbleyel@gmx.net>
2021-06-15 20:28:41 +0200

    fix ASCIItranslit #1945

--- lib/python/Tools/ASCIItranslit.py
+++ lib/python/Tools/ASCIItranslit.py
@@ -269,9 +269,11 @@ def legacyEncode(string):
 			string2 += ASCIItranslit[i]
 		else:
 			try:
-				string2 += char.encode('ascii', 'strict')
+				_s = char.encode('ascii', 'strict')
+				if six.PY3:
+					string2 += _s.decode("utf-8")
+				else:
+					string2 += _s
 			except:
 				string2 += "_"
-	if six.PY3:
-		string2 = string2.decode("utf-8")
 	return string2.upper()
