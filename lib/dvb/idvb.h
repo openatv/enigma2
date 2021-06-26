562037cb38a03b7be9b1480ea495312b9a04f012
jbleyel <jbleyel@gmx.net>
2021-06-26 08:35:11 +0200

    Add servicetype 0x32 -> ETSI EN 300 468 V1.16.1 (2019-08)
    (cherry picked from commit cd65254194bd3fb9511c5de48dc21102ec0dfb3f)

--- lib/dvb/idvb.h
+++ lib/dvb/idvb.h
@@ -229,8 +229,9 @@ public:
 		avcHdStereo         = 0x1C, // 28             H.264/AVC frame compatible plano - stereoscopic HD digital television service (see note 3)
 		nvodAvcHdStereoTs   = 0x1D, // 29             H.264/AVC frame compatible plano - stereoscopic HD NVOD time - shifted service (see note 3)
 		nvodAvcHdStereoRef  = 0x1E, // 30             H.264/AVC frame compatible plano - stereoscopic HD NVOD reference service (see note 3)
-		nvecTv              = 0x1F, // 31             HEVC digital television service
-				    //0x20, // 32 to 0x7F/127 reserved for future use
+		nvecTv              = 0x1F, // 31             HEVC digital television service (see note 4) 
+		nvecTv20            = 0x20, // 32             HEVC UHD digital television service with HDR and/or a frame rate of 100 Hz, 120 000/1 001 Hz, or 120 Hz, or any combination of HDR and these frame rates (see note 5) 
+				    //0x21, // 33 to 0x7F/127 reserved for future use
 				    //0x80, //128 to 0xFE/254 user defined
 		user134             = 0x86, //134             ???
 		user195             = 0xC3, //195             ???
