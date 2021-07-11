f1a7b7eca6fe46c2cfa3ca8ff67f63c23cc75c42
jbleyel <jbleyel@gmx.net>
2021-07-11 23:50:07 +0200

    Redesign keymap based on OV Thanks to @IanSav

--- lib/python/Components/ActionMap.py
+++ lib/python/Components/ActionMap.py
@@ -1,8 +1,149 @@
 from __future__ import print_function
 from enigma import eActionMap
+from keyids import KEYIDS
+from Components.config import config
+from Tools.Directories import fileReadXML
 import six
 
-from Tools.KeyBindings import queryKeyBinding
+MODULE_NAME = __name__.split(".")[-1]
+
+keyBindings = {}
+unmapDict = {}
+
+
+
+class KeymapError(Exception):
+	def __init__(self, message):
+		self.msg = message
+
+	def __str__(self):
+		return self.msg
+
+
+def addKeyBinding(filename, keyId, context, mapto, flags):
+	keyBindings.setdefault((context, mapto), []).append((keyId, filename, flags))
+
+
+def queryKeyBinding(context, mapto):  # Returns a list of (keyId, flags) for a specified mapto action in a context.
+	if (context, mapto) in keyBindings:
+		return [(x[0], x[2]) for x in keyBindings[(context, mapto)]]
+	return []
+
+
+def getKeyBindingKeys(filterFunction=lambda key: True):
+	return filter(filterFunction, keyBindings)
+
+
+def removeKeyBinding(keyId, context, mapto, wild=True):
+	if wild and mapto == "*":
+		for context, mapto in keyBindings.keys():
+			if context == context:
+				removeKeyBinding(keyId, context, mapto, False)
+		return
+	contextAction = (context, mapto)
+	if contextAction in keyBindings:
+		bind = [x for x in keyBindings[contextAction] if x[0] != keyId]
+		if bind:
+			keyBindings[contextAction] = bind
+		else:
+			del keyBindings[contextAction]
+
+
+def removeKeyBindings(filename):  # Remove all entries of filename "domain".
+	for keyBinding in keyBindings:
+		keyBindings[keyBinding] = filter(lambda x: x[1] != filename, keyBindings[keyBinding])
+
+def getKeyId(id):
+	if len(id) == 1:
+		keyid = ord(id) | 0x8000
+	elif id[0] == '\\':
+		if id[1] == 'x':
+			keyid = int(id[2:], 0x10) | 0x8000
+		elif id[1] == 'd':
+			keyid = int(id[2:]) | 0x8000
+		else:
+			raise KeymapError("[keymapparser] key id '" + str(id) + "' is neither hex nor dec")
+	else:
+		try:
+			keyid = KEYIDS[id]
+		except:
+			raise KeymapError("[keymapparser] key id '" + str(id) + "' is illegal")
+	return keyid
+
+
+def parseTrans(filename, actionmap, device, keys):
+	for x in keys.findall("toggle"):
+		get_attr = x.attrib.get
+		toggle_key = get_attr("from")
+		toggle_key = getKeyId(toggle_key)
+		actionmap.bindToggle(filename, device, toggle_key)
+
+	for x in keys.findall("key"):
+		get_attr = x.attrib.get
+		keyin = get_attr("from")
+		keyout = get_attr("to")
+		toggle = get_attr("toggle") or "0"
+		assert keyin, "[keymapparser] %s: must specify key to translate from '%s'" % (filename, keyin)
+		assert keyout, "[keymapparser] %s: must specify key to translate to '%s'" % (filename, keyout)
+
+		keyin = getKeyId(keyin)
+		keyout = getKeyId(keyout)
+		toggle = int(toggle)
+		actionmap.bindTranslation(filename, device, keyin, keyout, toggle)
+
+
+def parseKeymap(filename, context, actionmap, device, keys):
+	for x in keys.findall("key"):
+		get_attr = x.attrib.get
+		mapto = get_attr("mapto")
+		unmap = get_attr("unmap")
+		id = get_attr("id")
+		flags = get_attr("flags")
+
+		if unmap is not None:
+			assert id, "[keymapparser] %s: must specify id in context %s, unmap '%s'" % (filename, context, unmap)
+			keyid = getKeyId(id)
+			actionmap.unbindPythonKey(context, keyid, unmap)
+			unmapDict.update({(context, id, unmap): filename})
+		else:
+			assert mapto, "[keymapparser] %s: must specify mapto (or unmap) in context %s, id '%s'" % (filename, context, id)
+			assert id, "[keymapparser] %s: must specify id in context %s, mapto '%s'" % (filename, context, mapto)
+			keyid = getKeyId(id)
+
+			flag_ascii_to_id = lambda x: {'m': 1, 'b': 2, 'r': 4, 'l': 8}[x]
+
+			flags = sum(map(flag_ascii_to_id, flags))
+
+			assert flags, "[keymapparser] %s: must specify at least one flag in context %s, id '%s'" % (filename, context, id)
+
+			# if a key was unmapped, it can only be assigned a new function in the same keymap file (avoid file parsing sequence dependency)
+			if unmapDict.get((context, id, mapto)) in [filename, None]:
+#				print "[keymapparser] " + context + "::" + mapto + " -> " + device + "." + hex(keyid)
+				actionmap.bindKey(filename, device, keyid, flags, context, mapto)
+				addKeyBinding(filename, keyid, context, mapto, flags)
+
+
+def loadKeymap(filename):
+	actionMapInstance = eActionMap.getInstance()
+	domKeymap = fileReadXML(filename, source=MODULE_NAME)
+	if domKeymap:
+		for domMap in domKeymap.findall("map"):
+			context = domMap.attrib.get("context")
+			if context is None:
+				print("ActionMap] Error: All keymap action maps in '%s' must have a context!" % filename)
+			else:
+				parseKeymap(filename, context, actionMapInstance, "generic", domMap)
+				for domDevice in domMap.findall("device"):
+					parseKeymap(filename, context, actionMapInstance, domDevice.attrib.get("name"), domDevice)
+
+		for domMap in domKeymap.findall("translate"):
+			for domDevice in domMap.findall("device"):
+				parseTrans(filename, actionMapInstance, domDevice.attrib.get("name"), domDevice)
+
+
+def removeKeymap(filename):
+	actionMapInstance = eActionMap.getInstance()
+	actionMapInstance.unbindKeyDomain(filename)
 
 
 class ActionMap:
@@ -10,17 +151,17 @@ class ActionMap:
 		self.contexts = contexts or []
 		self.actions = actions or {}
 		self.prio = prio
-		self.p = eActionMap.getInstance()
+		self.actionMapInstance = eActionMap.getInstance()
 		self.bound = False
-		self.exec_active = False
+		self.execActive = False
 		self.enabled = True
-		unknown = list(self.actions.keys())
-		for action in unknown:
+		undefinedAction = list(self.actions.keys())
+		for action in undefinedAction:
 			for context in self.contexts:
 				if queryKeyBinding(context, action):
-					unknown.remove(action)
+					undefinedAction.remove(action)
 					break
-		if unknown:
+		if undefinedAction:
 			print("[ActionMap] Keymap(s) '%s' -> Undefined action(s) '%s'." % (", ".join(contexts), ", ".join(unknown)))
 
 	def setEnabled(self, enabled):
@@ -30,39 +171,38 @@ class ActionMap:
 	def doBind(self):
 		if not self.bound:
 			for context in self.contexts:
-				self.p.bindAction(context, self.prio, self.action)
+				self.actionMapInstance.bindAction(context, self.prio, self.action)
 			self.bound = True
 
 	def doUnbind(self):
 		if self.bound:
 			for context in self.contexts:
-				self.p.unbindAction(context, self.action)
+				self.actionMapInstance.unbindAction(context, self.action)
 			self.bound = False
 
 	def checkBind(self):
-		if self.exec_active and self.enabled:
+		if self.execActive and self.enabled:
 			self.doBind()
 		else:
 			self.doUnbind()
 
 	def execBegin(self):
-		self.exec_active = True
+		self.execActive = True
 		self.checkBind()
 
 	def execEnd(self):
-		self.exec_active = False
+		self.execActive = False
 		self.checkBind()
 
 	def action(self, context, action):
 		if action in self.actions:
-			print("[ActionMap] Keymap '%s' -> Action = '%s'." % (context, action))
-			res = self.actions[action]()
-			if res is not None:
-				return res
+			print("[ActionMap] Keymap '%s' -> Action '%s'." % (context, action))
+			response = self.actions[action]()
+			if response is not None:
+				return response
 			return 1
-		else:
-			print("[ActionMap] Keymap '%s' -> Unknown action '%s'! (Typo in keymap?)" % (context, action))
-			return 0
+		print("[ActionMap] Keymap '%s' -> Unknown action '%s'! (Typo in keymap?)" % (context, action))
+		return 0
 
 	def destroy(self):
 		pass
@@ -71,12 +211,11 @@ class ActionMap:
 class NumberActionMap(ActionMap):
 	def action(self, contexts, action):
 		if action in ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9") and action in self.actions:
-			res = self.actions[action](int(action))
-			if res is not None:
-				return res
+			response = self.actions[action](int(action))
+			if response is not None:
+				return response
 			return 1
-		else:
-			return ActionMap.action(self, contexts, action)
+		return ActionMap.action(self, contexts, action)
 
 
 class HelpableActionMap(ActionMap):
@@ -92,6 +231,7 @@ class HelpableActionMap(ActionMap):
 	# ActionMapconstructor,	the collected helpstrings (with correct
 	# context, action) is added to the screen's "helpList", which will
 	# be picked up by the "HelpableScreen".
+	#
 	def __init__(self, parent, contexts, actions=None, prio=0, description=None):
 		def exists(record):
 			for context in parent.helpList:
@@ -103,23 +243,23 @@ class HelpableActionMap(ActionMap):
 			contexts = [contexts]
 		actions = actions or {}
 		self.description = description
-		adict = {}
+		actionDict = {}
 		for context in contexts:
-			alist = []
-			for (action, funchelp) in six.iteritems(actions):
+			actionList = []
+			for (action, response) in six.iteritems(actions):
 				# Check if this is a tuple.
-				if isinstance(funchelp, tuple):
+				if isinstance(response, tuple):
 					if queryKeyBinding(context, action):
-						if not exists((action, funchelp[1])):
-							alist.append((action, funchelp[1]))
-					adict[action] = funchelp[0]
+						if not exists((action, response[1])):
+							actionList.append((action, response[1]))
+					actionDict[action] = response[0]
 				else:
 					if queryKeyBinding(context, action):
 						if not exists((action, None)):
-							alist.append((action, None))
-					adict[action] = funchelp
-			parent.helpList.append((self, context, alist))
-		ActionMap.__init__(self, contexts, adict, prio)
+							actionList.append((action, None))
+					actionDict[action] = response
+			parent.helpList.append((self, context, actionList))
+		ActionMap.__init__(self, contexts, actionDict, prio)
 
 
 class HelpableNumberActionMap(NumberActionMap, HelpableActionMap):
@@ -127,5 +267,6 @@ class HelpableNumberActionMap(NumberActionMap, HelpableActionMap):
 		# Initialise NumberActionMap with empty context and actions
 		# so that the underlying ActionMap is only initialised with
 		# these once, via the HelpableActionMap.
+		#
 		NumberActionMap.__init__(self, [], {})
 		HelpableActionMap.__init__(self, parent, contexts, actions, prio, description)
