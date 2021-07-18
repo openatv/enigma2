35d6bba02ae064c560111a1f98c77f9470bf2686
jbleyel <jbleyel@gmx.net>
2021-07-18 13:08:11 +0200

    [VtiTempFan] cosmetics

--- lib/python/Components/Converter/VtiTempFan.py
+++ lib/python/Components/Converter/VtiTempFan.py
@@ -2,66 +2,62 @@ from __future__ import absolute_import
 from Components.Converter.Converter import Converter
 from Components.Element import cached
 from Components.Converter.Poll import Poll
-import six
+from six import PY3
 
-SIGN = ' °' if six.PY3 else str(' \xc2\xb0')
+SIGN = ' °' if PY3 else str(' \xc2\xb0')
 
 
 class VtiTempFan(Poll, Converter, object):
-    TEMPINFO = 1
-    FANINFO = 2
-    ALL = 5
+	TEMPINFO = 1
+	FANINFO = 2
+	ALL = 5
 
-    def __init__(self, type):
-        Poll.__init__(self)
-        Converter.__init__(self, type)
-        self.type = type
-        self.poll_interval = 30000
-        self.poll_enabled = True
-        if type == 'TempInfo':
-            self.type = self.TEMPINFO
-        elif type == 'FanInfo':
-            self.type = self.FANINFO
-        else:
-            self.type = self.ALL
+	def __init__(self, type):
+		Poll.__init__(self)
+		Converter.__init__(self, type)
+		self.type = type
+		self.poll_interval = 30000
+		self.poll_enabled = True
+		if type == 'TempInfo':
+			self.type = self.TEMPINFO
+		elif type == 'FanInfo':
+			self.type = self.FANINFO
+		else:
+			self.type = self.ALL
 
-    @cached
-    def getText(self):
-        textvalue = ''
-        if self.type == self.TEMPINFO:
-            textvalue = self.tempfile()
-        elif self.type == self.FANINFO:
-            textvalue = self.fanfile()
-        return textvalue
+	@cached
+	def getText(self):
+		textvalue = ''
+		if self.type == self.TEMPINFO:
+			textvalue = self.tempfile()
+		elif self.type == self.FANINFO:
+			textvalue = self.fanfile()
+		return textvalue
 
-    text = property(getText)
+	text = property(getText)
 
-    def tempfile(self):
-        temp = ''
-        unit = ''
-        try:
-            f = open('/proc/stb/sensors/temp0/value', 'rb')
-            temp = f.readline().strip()
-            f.close()
-            f = open('/proc/stb/sensors/temp0/unit', 'rb')
-            unit = f.readline().strip()
-            f.close()
-            tempinfo = 'TEMP: ' + str(temp) + SIGN + str(unit)
-            return tempinfo
-        except:
-            pass
+	def tempfile(self):
+		temp = ''
+		unit = ''
+		try:
+			with open('/proc/stb/sensors/temp0/value', 'rb') as fd:
+				temp = fd.readline().strip()
+			with open('/proc/stb/sensors/temp0/unit', 'rb') as fd:
+				unit = fd.readline().strip()
+			return 'TEMP: %s%s%s' % (str(temp), SIGN, str(unit))
+		except:
+			pass
 
-    def fanfile(self):
-        fan = ''
-        try:
-            f = open('/proc/stb/fp/fan_speed', 'rb')
-            fan = f.readline().strip()
-            f.close()
-            faninfo = 'FAN: ' + str(fan)
-            return faninfo
-        except:
-            pass
+	def fanfile(self):
+		fan = ''
+		try:
+			with open('/proc/stb/fp/fan_speed', 'rb') as fd:
+				fan = fd.readline().strip()
+			faninfo = 'FAN: %s' % (str(fan))
+			return faninfo
+		except:
+			pass
 
-    def changed(self, what):
-        if what[0] == self.CHANGED_POLL:
-            Converter.changed(self, what)
+	def changed(self, what):
+		if what[0] == self.CHANGED_POLL:
+			Converter.changed(self, what)
