5cd76a208a8b021326d86436aebbe6765818e3a3
jbleyel <jbleyel@gmx.net>
2021-07-29 22:23:06 +0200

    [HelpMenu] move HelpMenuList to HelpMenu

--- lib/python/Screens/HelpMenu.py
+++ lib/python/Screens/HelpMenu.py
@@ -1,9 +1,33 @@
 from __future__ import print_function
-from Screens.Screen import Screen
+from Components.GUIComponent import GUIComponent
 from Components.Label import Label
-from Components.ActionMap import ActionMap
-from Components.HelpMenuList import HelpMenuList
+from Components.ActionMap import ActionMap, queryKeyBinding
 from Screens.Rc import Rc
+from Screens.Screen import Screen
+from skin import parameters, fonts
+from Tools.KeyBindings import getKeyDescription
+from enigma import eListboxPythonMultiContent, eListbox, gFont
+
+
+class HelpableScreen:
+	def __init__(self):
+		self["helpActions"] = ActionMap(["HelpActions"],
+			{
+				"displayHelp": self.showHelp,
+			})
+
+	def showHelp(self):
+		try:
+			if self.secondInfoBarScreen and self.secondInfoBarScreen.shown:
+				self.secondInfoBarScreen.hide()
+		except:
+			pass
+		self.session.openWithCallback(self.callHelpAction, HelpMenu, self.helpList)
+
+	def callHelpAction(self, *args):
+		if args:
+			(actionmap, context, action) = args
+			actionmap.action(context, action)
 
 
 class HelpMenu(Screen, Rc):
@@ -30,7 +54,7 @@ class HelpMenu(Screen, Rc):
 		if selection:
 			selection = selection[3]
 		#arrow = self["arrowup"]
-		print("selection:", selection)
+		print("[HelpMenu] selection:", selection)
 
 		longText = ""
 		if selection and len(selection) > 1:
@@ -42,28 +66,101 @@ class HelpMenu(Screen, Rc):
 
 		self.selectKey(selection[0])
 		#if selection is None:
-		print("select arrow")
+		print("[HelpMenu] select arrow")
 		#	arrow.moveTo(selection[1], selection[2], 1)
 		#	arrow.startMoving()
 		#	arrow.show()
 
 
-class HelpableScreen:
-	def __init__(self):
-		self["helpActions"] = ActionMap(["HelpActions"],
-			{
-				"displayHelp": self.showHelp,
-			})
+class HelpMenuList(GUIComponent):
+	def __init__(self, helplist, callback):
+		GUIComponent.__init__(self)
+		self.onSelChanged = []
+		self.l = eListboxPythonMultiContent()
+		self.callback = callback
+		self.extendedHelp = False
 
-	def showHelp(self):
-		try:
-			if self.secondInfoBarScreen and self.secondInfoBarScreen.shown:
-				self.secondInfoBarScreen.hide()
-		except:
-			pass
-		self.session.openWithCallback(self.callHelpAction, HelpMenu, self.helpList)
+		l = []
+		for (actionmap, context, actions) in helplist:
+			for (action, help) in actions:
+				if hasattr(help, '__call__'):
+					help = help()
+				if not help:
+					continue
+				buttons = queryKeyBinding(context, action)
 
-	def callHelpAction(self, *args):
-		if args:
-			(actionmap, context, action) = args
-			actionmap.action(context, action)
+				# do not display entries which are not accessible from keys
+				if not len(buttons):
+					continue
+
+				name = None
+				flags = 0
+
+				for n in buttons:
+					(name, flags) = (getKeyDescription(n[0]), n[1])
+					if name is not None:
+						break
+
+				# only show entries with keys that are available on the used rc
+				if name is None:
+					continue
+
+				if flags & 8: # for long keypresses, prepend l_ into the key name.
+					name = (name[0], "long")
+
+				entry = [(actionmap, context, action, name)]
+
+				if isinstance(help, list):
+					self.extendedHelp = True
+					print("[HelpMenuList] extendedHelpEntry found")
+					x, y, w, h = parameters.get("HelpMenuListExtHlp0", (0, 0, 600, 26))
+					x1, y1, w1, h1 = parameters.get("HelpMenuListExtHlp1", (0, 28, 600, 20))
+					entry.extend((
+						(eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, 0, help[0]),
+						(eListboxPythonMultiContent.TYPE_TEXT, x1, y1, w1, h1, 1, 0, help[1])
+					))
+				else:
+					x, y, w, h = parameters.get("HelpMenuListHlp", (0, 0, 600, 28))
+					entry.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, 0, help))
+
+				l.append(entry)
+
+		self.l.setList(l)
+		if self.extendedHelp is True:
+			font = fonts.get("HelpMenuListExt0", ("Regular", 24, 50))
+			self.l.setFont(0, gFont(font[0], font[1]))
+			self.l.setItemHeight(font[2])
+			font = fonts.get("HelpMenuListExt1", ("Regular", 18))
+			self.l.setFont(1, gFont(font[0], font[1]))
+		else:
+			font = fonts.get("HelpMenuList", ("Regular", 24, 38))
+			self.l.setFont(0, gFont(font[0], font[1]))
+			self.l.setItemHeight(font[2])
+
+	def ok(self):
+		# a list entry has a "private" tuple as first entry...
+		l = self.getCurrent()
+		if l is None:
+			return
+		# ...containing (Actionmap, Context, Action, keydata).
+		# we returns this tuple to the callback.
+		self.callback(l[0], l[1], l[2])
+
+	def getCurrent(self):
+		sel = self.l.getCurrentSelection()
+		return sel and sel[0]
+
+	GUI_WIDGET = eListbox
+
+	def postWidgetCreate(self, instance):
+		instance.setContent(self.l)
+		instance.selectionChanged.get().append(self.selectionChanged)
+		self.instance.setWrapAround(True)
+
+	def preWidgetRemove(self, instance):
+		instance.setContent(None)
+		instance.selectionChanged.get().remove(self.selectionChanged)
+
+	def selectionChanged(self):
+		for x in self.onSelChanged:
+			x()
