2e5a1a380f93bb9d9f721d523fa8d8cf3bdbeb2d
jbleyel <jbleyel@gmx.net>
2021-08-01 08:30:02 +0200

    [Pixmap] * refactor code * Add callback option for movement * prepare new HELP display engine

--- lib/python/Components/Pixmap.py
+++ lib/python/Components/Pixmap.py
@@ -1,21 +1,55 @@
-from __future__ import print_function
-from __future__ import absolute_import
+from enigma import ePixmap, eTimer
+from os.path import isfile
+
+from skin import domScreens, loadPixmap
 from Components.ConditionalWidget import ConditionalWidget
 from Components.GUIComponent import GUIComponent
-
-from enigma import ePixmap, eTimer
-
-from Tools.Directories import resolveFilename, fileExists, SCOPE_CURRENT_SKIN, SCOPE_CURRENT_LCDSKIN
-from os import path
-from skin import loadPixmap
+from Tools.Directories import SCOPE_CURRENT_LCDSKIN, SCOPE_CURRENT_SKIN, fileExists, resolveFilename
 
 
 class Pixmap(GUIComponent):
 	GUI_WIDGET = ePixmap
 
+	def __init__(self):
+		GUIComponent.__init__(self)
+		self.xOffset = 0
+		self.yOffset = 0
+
 	def getSize(self):
-		s = self.instance.size()
-		return s.width(), s.height()
+		size = self.instance.size()
+		return size.width(), size.height()
+
+	def applySkin(self, desktop, screen):
+		if self.skinAttributes is not None:
+			attribs = []
+			for (attrib, value) in self.skinAttributes:
+				if attrib == "offset":
+					self.xOffset, self.yOffset = map(int, value.split(","))
+				else:
+					attribs.append((attrib, value))
+			self.skinAttributes = attribs
+		return GUIComponent.applySkin(self, desktop, screen)
+
+	def move(self, x, y=None):
+		if y is None:
+			y = x.y()
+			x = x.x()
+		GUIComponent.move(self, x - self.xOffset, y - self.yOffset)
+
+	def setPosition(self, x, y):
+		self.move(x, y)
+
+	def getPosition(self):
+		x, y = GUIComponent.getPosition(self)
+		return x + self.xOffset, y + self.yOffset
+
+	def setOffset(self, x, y):
+		oldx, oldy = self.getPosition()
+		self.xOffset, self.yOffset = x, y
+		self.move(oldx, oldy)
+
+	def getOffset(self):
+		return self.xOffset, self.yOffset
 
 
 class PixmapConditional(ConditionalWidget, Pixmap):
@@ -27,23 +61,23 @@ class PixmapConditional(ConditionalWidget, Pixmap):
 class MovingPixmap(Pixmap):
 	def __init__(self):
 		Pixmap.__init__(self)
-
 		self.moving = False
-
-		# TODO: get real values
-		self.x = 0.0
-		self.y = 0.0
-
+		self.x = 0  # Get actual value after skin applied.
+		self.y = 0  # Get actual value after skin applied.
 		self.clearPath()
-
 		self.moveTimer = eTimer()
 		self.moveTimer.callback.append(self.doMove)
+		self.callback = None
+
+	def applySkin(self, desktop, screen):
+		ret = Pixmap.applySkin(self, desktop, screen)
+		self.x, self.y = self.getPosition()
+		return ret
 
 	def clearPath(self, repeated=False):
 		if self.moving:
 			self.moving = False
 			self.moveTimer.stop()
-
 		self.path = []
 		self.currDest = 0
 		self.repeated = repeated
@@ -55,37 +89,48 @@ class MovingPixmap(Pixmap):
 		self.clearPath()
 		self.addMovePoint(x, y, time)
 
-	def startMoving(self):
+	def startMoving(self, callback=None):
+		if callable(callback):
+			self.callback = callback
 		if not self.moving:
-			self.time = self.path[self.currDest][2]
-			self.stepX = (self.path[self.currDest][0] - self.x) / float(self.time)
-			self.stepY = (self.path[self.currDest][1] - self.y) / float(self.time)
-
-			self.moving = True
-			self.moveTimer.start(100)
+			try:
+				self.time = self.path[self.currDest][2]
+				self.x, self.y = self.getPosition()
+				self.stepX = (self.path[self.currDest][0] - self.x) / float(self.time)
+				self.stepY = (self.path[self.currDest][1] - self.y) / float(self.time)
+				self.moving = True
+				self.moveTimer.start(100)
+			except Exception:  # Moving not possible.  Widget not there yet/any more.  Stop moving.
+				self.stopMoving()
 
 	def stopMoving(self):
 		self.moving = False
 		self.moveTimer.stop()
+		if self.callback:
+			self.callback()
 
 	def doMove(self):
-		self.x += self.stepX
-		self.y += self.stepY
 		self.time -= 1
+		if self.time == 0:
+			self.x, self.y = self.path[self.currDest][0:2]
+		else:
+			self.x += self.stepX
+			self.y += self.stepY
 		try:
 			self.move(int(self.x), int(self.y))
-		except: # moving not possible... widget not there any more... stop moving
+		except Exception:  # Moving not possible.  Widget not there any more.  Stop moving.
 			self.stopMoving()
-
 		if self.time == 0:
 			self.currDest += 1
 			self.moveTimer.stop()
 			self.moving = False
-			if self.currDest >= len(self.path): # end of path
+			if self.currDest >= len(self.path):  # End of path.
 				if self.repeated:
 					self.currDest = 0
 					self.moving = False
 					self.startMoving()
+				elif self.callback:
+					self.callback()
 			else:
 				self.moving = False
 				self.startMoving()
@@ -98,26 +143,32 @@ class MultiPixmap(Pixmap):
 
 	def applySkin(self, desktop, screen):
 		if self.skinAttributes is not None:
-			skin_path_prefix = getattr(screen, "skin_path", path)
+			myScreen, path = domScreens.get(screen.__class__.__name__, (None, None))
+			skinPathPrefix = getattr(screen, "skin_path", path)
 			pixmap = None
 			attribs = []
 			for (attrib, value) in self.skinAttributes:
 				if attrib == "pixmaps":
-					pixmaps = value.split(',')
-					for p in pixmaps:
-						pngfile = resolveFilename(SCOPE_CURRENT_SKIN, p, path_prefix=skin_path_prefix)
-						if not fileExists(pngfile) and fileExists(resolveFilename(SCOPE_CURRENT_LCDSKIN, p, path_prefix=skin_path_prefix)):
-							pngfile = resolveFilename(SCOPE_CURRENT_LCDSKIN, p, path_prefix=skin_path_prefix)
-						if path.exists(pngfile):
+					pixmaps = value.split(",")
+					for pix in pixmaps:
+						if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, pix, path_prefix=skinPathPrefix)):
+							pngfile = resolveFilename(SCOPE_CURRENT_SKIN, pix, path_prefix=skinPathPrefix)
+						elif fileExists(resolveFilename(SCOPE_CURRENT_LCDSKIN, pix, path_prefix=skinPathPrefix)):
+							pngfile = resolveFilename(SCOPE_CURRENT_LCDSKIN, pix, path_prefix=skinPathPrefix)
+						else:
+							pngfile = ""
+						if pngfile and isfile(pngfile):
 							self.pixmaps.append(loadPixmap(pngfile, desktop))
 					if not pixmap:
-						pixmap = resolveFilename(SCOPE_CURRENT_SKIN, pixmaps[0], path_prefix=skin_path_prefix)
-						if not fileExists(pixmap) and fileExists(resolveFilename(SCOPE_CURRENT_LCDSKIN, pixmaps[0], path_prefix=skin_path_prefix)):
-							pixmap = resolveFilename(SCOPE_CURRENT_LCDSKIN, pixmaps[0], path_prefix=skin_path_prefix)
+						if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, pixmaps[0], path_prefix=skinPathPrefix)):
+							pixmap = resolveFilename(SCOPE_CURRENT_SKIN, pixmaps[0], path_prefix=skinPathPrefix)
+						elif fileExists(resolveFilename(SCOPE_CURRENT_LCDSKIN, pixmaps[0], path_prefix=skinPathPrefix)):
+							pixmap = resolveFilename(SCOPE_CURRENT_LCDSKIN, pixmaps[0], path_prefix=skinPathPrefix)
 				elif attrib == "pixmap":
-					pixmap = resolveFilename(SCOPE_CURRENT_SKIN, value, path_prefix=skin_path_prefix)
-					if not fileExists(pixmap) and fileExists(resolveFilename(SCOPE_CURRENT_LCDSKIN, value, path_prefix=skin_path_prefix)):
-						pixmap = resolveFilename(SCOPE_CURRENT_LCDSKIN, value, path_prefix=skin_path_prefix)
+					if fileExists(resolveFilename(SCOPE_CURRENT_SKIN, value, path_prefix=skinPathPrefix)):
+						pixmap = resolveFilename(SCOPE_CURRENT_SKIN, value, path_prefix=skinPathPrefix)
+					elif fileExists(resolveFilename(SCOPE_CURRENT_LCDSKIN, value, path_prefix=skinPathPrefix)):
+						pixmap = resolveFilename(SCOPE_CURRENT_LCDSKIN, value, path_prefix=skinPathPrefix)
 				else:
 					attribs.append((attrib, value))
 			if pixmap:
@@ -125,9 +176,9 @@ class MultiPixmap(Pixmap):
 			self.skinAttributes = attribs
 		return GUIComponent.applySkin(self, desktop, screen)
 
-	def setPixmapNum(self, x):
+	def setPixmapNum(self, index):
 		if self.instance:
-			if len(self.pixmaps) > x:
-				self.instance.setPixmap(self.pixmaps[x])
+			if len(self.pixmaps) > index:
+				self.instance.setPixmap(self.pixmaps[index])
 			else:
-				print("setPixmapNum(%d) failed! defined pixmaps:" % x, self.pixmaps)
+				print("[Pixmap] setPixmapNum(%d) failed!  Defined pixmaps: %s." % (index, str(self.pixmaps)))
