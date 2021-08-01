e07a4e7cb1e3b92119dd423f1d274cc24fb40d95
jbleyel <jbleyel@gmx.net>
2021-08-01 21:16:33 +0200

    [Console.py] Refactor code
    - Fix logic to properly allow commands submitted as an argument list as well as simple text command lines.
    - Sort and optimize imports.
    - Log all commands being executed.
    - Use camelCase variables.
    - Improve logging text.
    
    Thanks @IanSav

--- lib/python/Components/Console.py
+++ lib/python/Components/Console.py
@@ -1,64 +1,67 @@
-from __future__ import print_function
-import enigma
-import ctypes
-import os
+from os import waitpid
+
+from enigma import eConsoleAppContainer
 import six
 
 
 class ConsoleItem:
-	def __init__(self, containers, cmd, callback, extra_args):
-		self.extra_args = extra_args
-		self.callback = callback
-		self.container = enigma.eConsoleAppContainer()
+	def __init__(self, containers, cmd, callback, extraArgs):
 		self.containers = containers
-		# Create a unique name
-		name = cmd
-		if name in containers:
-			name = str(cmd) + '@' + hex(id(self))
+		if isinstance(cmd, str):  # Until .execute supports a better api.
+			cmd = [cmd]
+		name = cmd[0]
+		if name in self.containers:  # Create a unique name.
+			name = "%s@%s" % (str(cmd), hex(id(self)))
 		self.name = name
-		containers[name] = self
-		# If the caller isn't interested in our results, we don't need
-		# to store the output either.
+		self.callback = callback
+		self.extraArgs = extraArgs
+		self.container = eConsoleAppContainer()
+		self.containers[name] = self
+		# If the caller isn't interested in our results, we don't need to store the output either.
 		if callback is not None:
 			self.appResults = []
 			self.container.dataAvail.append(self.dataAvailCB)
 		self.container.appClosed.append(self.finishedCB)
-		if isinstance(cmd, str): # until .execute supports a better api
-			cmd = [cmd]
-		retval = self.container.execute(*cmd)
-		if retval:
-			self.finishedCB(retval)
+		if len(cmd) > 1:
+			print("[Console] Processing command '%s' with arguments %s." % (cmd[0], str(cmd[1:])))
+		else:
+			print("[Console] Processing command line '%s'." % cmd[0])
+		retVal = self.container.execute(*cmd)
+		if retVal:
+			self.finishedCB(retVal)
 		if callback is None:
 			try:
-				os.waitpid(self.container.getPID(), 0)
-			except:
+				waitpid(self.container.getPID(), 0)
+			except (IOError, OSError):
 				pass
 
 	def dataAvailCB(self, data):
 		self.appResults.append(data)
 
-	def finishedCB(self, retval):
-		print("[Console] finished:", self.name)
+	def finishedCB(self, retVal):
+		print("[Console] Command '%s' finished." % self.name)
 		del self.containers[self.name]
 		del self.container.dataAvail[:]
 		del self.container.appClosed[:]
-		self.container = None
+		del self.container
 		callback = self.callback
 		if callback is not None:
-			data = b''.join(self.appResults)
-			callback(data, retval, self.extra_args)
+			data = b"".join(self.appResults)
+			callback(data, retVal, self.extraArgs)
 
 
 class Console(object):
 	def __init__(self):
-		# Still called appContainers and appResults because Network.py accesses it to
-		# know if there's still stuff running
+		# Still called appContainers because Network.py, SoftwareTools.py
+		# and WirelessLan/Wlan.py accesses it to know if there's still
+		# stuff running.
 		self.appContainers = {}
-		self.appResults = {}
+		self.appResults = {} # FIXME : Do ee need this
 
 	def ePopen(self, cmd, callback=None, extra_args=None):
-		extra_args = [] if extra_args is None else extra_args
-		print("[Console] command:", cmd)
+		if not extra_args:
+			extra_args = []
+		print("[Console] command: %s" % cmd)
 		return ConsoleItem(self.appContainers, cmd, callback, extra_args)
 
 	def eBatch(self, cmds, callback, extra_args=None, debug=False):
@@ -68,23 +71,23 @@ class Console(object):
 		cmd = cmds.pop(0)
 		self.ePopen(cmd, self.eBatchCB, [cmds, callback, extra_args])
 
-	def eBatchCB(self, data, retval, _extra_args):
-		(cmds, callback, extra_args) = _extra_args
+	def eBatchCB(self, data, retVal, extraArg):
+		(cmds, callback, extraArgs) = extraArg
 		if self.debug:
 			data = six.ensure_str(data)
-			print('[eBatch] retval=%s, cmds left=%d, data:\n%s' % (retval, len(cmds), data))
+			print("[Console] eBatch DEBUG: retVal=%s, cmds left=%d, data:\n%s" % (retVal, len(cmds), data))
 		if cmds:
 			cmd = cmds.pop(0)
-			self.ePopen(cmd, self.eBatchCB, [cmds, callback, extra_args])
+			self.ePopen(cmd, self.eBatchCB, [cmds, callback, extraArgs])
 		else:
-			callback(extra_args)
+			callback(extraArgs)
 
 	def kill(self, name):
 		if name in self.appContainers:
-			print("[Console] killing: ", name)
+			print("[Console] Killing command '%s'." % name)
 			self.appContainers[name].container.kill()
 
 	def killAll(self):
-		for name, item in list(self.appContainers.items()):
-			print("[Console] killing: ", name)
+		for name, item in list(self.appContainers.items()):  # FIXME : Do ee need list
+			print("[Console] Killing all commands '%s'." % name)
 			item.container.kill()
