83953dea3ea3d0b334cb455373790936eff5b358
jbleyel <jbleyel@gmx.net>
2021-08-24 20:46:13 +0200

    [CamControl] refactor

--- lib/python/Tools/camcontrol.py
+++ lib/python/Tools/camcontrol.py
@@ -1,6 +1,6 @@
-from __future__ import print_function
-import os
-import enigma
+from enigma import eConsoleAppContainer
+from os import listdir, readlink, symlink, unlink
+from os.path import exists, split as pathsplit
 
 
 class CamControl:
@@ -10,46 +10,47 @@ class CamControl:
 	def __init__(self, name):
 		self.name = name
 		self.link = '/etc/init.d/' + name
-		if not os.path.exists(self.link):
-			print("[CamControl] No softcam link?", self.link)
+		if not exists(self.link):
+			print("[CamControl] No softcam link: '%s'" % self.link)
 
 	def getList(self):
 		result = []
 		prefix = self.name + '.'
-		for f in os.listdir("/etc/init.d"):
+		for f in listdir("/etc/init.d"):
 			if f.startswith(prefix):
 				result.append(f[len(prefix):])
 		return result
 
 	def current(self):
 		try:
-			l = os.readlink(self.link)
+			l = readlink(self.link)
 			prefix = self.name + '.'
-			return os.path.split(l)[1].split(prefix, 2)[1]
+			return pathsplit(l)[1].split(prefix, 2)[1]
 		except:
 			pass
 		return None
 
 	def command(self, cmd):
-		if os.path.exists(self.link):
-			print("Executing", self.link + ' ' + cmd)
-			enigma.eConsoleAppContainer().execute(self.link + ' ' + cmd)
+		if exists(self.link):
+			cmd = "%s %s" % (self.link, cmd)
+			print("[CamControl] Executing Command '%s'" % cmd)
+			eConsoleAppContainer().execute(cmd)
 
 	def select(self, which):
-		print("Selecting CAM:", which)
+		print("[CamControl] Select Cam: %s" % which)
 		if not which:
 			which = "None"
-		dst = self.name + '.' + which
-		if not os.path.exists('/etc/init.d/' + dst):
-			print("[CamControl] init script does not exist:", dst)
+		dst = "%s.%s" % (self.name, which)
+		if not exists('/etc/init.d/%s' % dst):
+			print("[CamControl] init script '%s' does not exist" % dst)
 			return
 		try:
-			os.unlink(self.link)
+			unlink(self.link)
 		except:
 			pass
 		try:
-			os.symlink(dst, self.link)
+			symlink(dst, self.link)
 		except:
-			print("Failed to create symlink for softcam:", dst)
+			print("[CamControl] Failed to create symlink for softcam: %s" % dst)
 			import sys
 			print(sys.exc_info()[:2])
