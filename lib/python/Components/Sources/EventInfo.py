7b47f865691dbbb701e1e69d390c7a65a21c7c26
Alexander Meinke <4l3x2k@users.noreply.github.com>
2021-09-07 06:58:59 +0200

    Update implementation of servicemp3's mp3 and aac stream titles (#2065)
    * EventInfo do not refresh downstream elements
    This change is to reduce load for normal dvb service reference types
    because evUpdatedInfo for dvb is called more often. For example if the
    video size has changed which can be the case for old tv series with
    4:3 resolution but advertising is broadcast in 16:9.
    MP3 and AAC streams advertise their title change via evUpdatedInfo
    instead of evUpdatedEventInfo.
    
    * Implement getExtraEventData in Converter and Source
    This is implemented in python's fake class and not in servicemp3's
    impementation of event.h's c++ interface.
    
    * Show infobar if servicemp3's service info changed
    This change is to show the infobar if the service info changed and there
    could be a new title for ip radio in mp3 or aac streams.

--- lib/python/Components/Sources/EventInfo.py
+++ lib/python/Components/Sources/EventInfo.py
@@ -3,6 +3,7 @@ from Components.PerServiceDisplay import PerServiceBase
 from Components.Element import cached
 from enigma import iPlayableService, iServiceInformation, eServiceReference, eEPGCache
 from Components.Sources.Source import Source
+import NavigationInstance
 
 # Fake eServiceEvent to fill Event_Now and Event_Next in Infobar for Streams
 #
@@ -71,8 +72,10 @@ class pServiceEvent(object):
 	def getEventId(self):
 		return 0
 
+	# eServiceEvent::getExtraEventData from lib/service/event.h ruturns std::string
+	# this fake class also return string
 	def getExtraEventData(self):
-		return None
+		return self.m_EventNameNext
 
 	def getBeginTimeString(self):
 		return ""
@@ -113,6 +116,13 @@ class EventInfo(PerServiceBase, Source, object):
 	def gotEvent(self, what):
 		if what == iPlayableService.evEnd:
 			self.changed((self.CHANGED_CLEAR,))
+		elif what == iPlayableService.evUpdatedInfo:
+			nav = NavigationInstance.instance
+			if nav:
+				service = nav.getCurrentlyPlayingServiceReference()
+				servicestring = service.toString()
+				if servicestring.split(':')[0] in ['4097', '5001', '5002', '5003']:
+					self.changed((self.CHANGED_ALL,))
 		else:
 			self.changed((self.CHANGED_ALL,))
 
