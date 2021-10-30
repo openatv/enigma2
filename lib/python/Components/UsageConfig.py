c2302538489b24132bcf39d6382965217eb62ecc
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2021-10-30 12:59:06 +0100

    [Usageconfig] language options (#2114)
    Add "Visual impaired commentary". "NAR" in UK. "qad" used by French national channels.
    
    Add "oth" to "Original". Used by Rai (Italian national channels)
    
    "qaa" is not English. This is used for original language and already exists in that section.
    
    Copied form commits by Huevos from Vix

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1330,7 +1330,7 @@ def InitUsageConfig():
 	audio_language_choices = [
 		("", _("None")),
 		("und", _("Undetermined")),
-		("orj dos ory org esl qaa und mis mul ORY ORJ Audio_ORJ", _("Original")),
+		("orj dos ory org esl qaa und mis mul ORY ORJ Audio_ORJ oth", _("Original")),
 		("ara", _("Arabic")),
 		("eus baq", _("Basque")),
 		("bul", _("Bulgarian")),
@@ -1340,7 +1340,7 @@ def InitUsageConfig():
 		("ces cze", _("Czech")),
 		("dan", _("Danish")),
 		("dut ndl nld Dutch", _("Dutch")),
-		("eng qaa Englisch", _("English")),
+		("eng Englisch", _("English")),
 		("est", _("Estonian")),
 		("fin", _("Finnish")),
 		("fra fre", _("French")),
@@ -1366,7 +1366,8 @@ def InitUsageConfig():
 		("swe", _("Swedish")),
 		("tha", _("Thai")),
 		("tur Audio_TUR", _("Turkish")),
-		("ukr Ukr", _("Ukrainian"))]
+		("ukr Ukr", _("Ukrainian")),
+		("NAR qad", _("Visual impaired commentary"))]
 
 	epg_language_choices = audio_language_choices[:1] + audio_language_choices[2:]
 
