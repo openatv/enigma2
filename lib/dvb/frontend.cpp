8b4de86446f01b95bc64c804989d1c6ff6f943cc
Jörg Bleyel <jbleyel@gmx.net>
2021-12-24 19:17:50 +0100

    [eDVBFrontendParameters::calculateDifference] remove dead code

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -504,7 +504,6 @@ RESULT eDVBFrontendParameters::calculateDifference(const iDVBFrontendParameters 
 		default:
 			return -1;
 	}
-	return 0;
 }
 
 RESULT eDVBFrontendParameters::getHash(unsigned long &hash) const
