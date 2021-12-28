a2945207da28632f703c6293625d31cf00cd12f4
Jörg Bleyel <jbleyel@gmx.net>
2021-12-28 13:26:27 +0100

    [Config.py] fix print parameter

--- lib/python/Components/Sources/Config.py
+++ lib/python/Components/Sources/Config.py
@@ -12,9 +12,9 @@ class Config(Source):
 	config = property(getConfig)
 
 	def getHTML(self, id):
-		print("[Config] getHTML %s %s", (str(self), str(id)))
+		print("[Config] getHTML %s %s" % (str(self), str(id)))
 		return self.__config.getHTML(id)
 
 	def handleCommand(self, cmd):
-		print("[Config] ASSIGN: %s", str(cmd))
+		print("[Config] ASSIGN: %s" % str(cmd))
 		self.__config.unsafeAssign(cmd)
