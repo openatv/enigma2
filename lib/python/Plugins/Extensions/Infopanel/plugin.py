61a0c5a34a22075ee0895da8164989dad88d8877
Jörg Bleyel <jbleyel@gmx.net>
2022-01-06 14:29:35 +0100

    [Infopanel] fix intentions

--- lib/python/Plugins/Extensions/Infopanel/plugin.py
+++ lib/python/Plugins/Extensions/Infopanel/plugin.py
@@ -135,26 +135,26 @@ def setDefaultKeymap():
 
 
 def command(comandline, strip=1):
-  comandline = comandline + " >/tmp/command.txt"
-  os.system(comandline)
-  text = ""
-  if os.path.exists("/tmp/command.txt") is True:
-    file = open("/tmp/command.txt", "r")
-    if strip == 1:
-      for line in file:
-        text = text + line.strip() + '\n'
-    else:
-      for line in file:
-        text = text + line
-        if text[-1:] != '\n':
-        	text = text + "\n"
-    file.close()
-  # if one or last line then remove linefeed
-  if text[-1:] == '\n':
-  	text = text[:-1]
-  comandline = text
-  os.system("rm /tmp/command.txt")
-  return comandline
+	comandline = comandline + " >/tmp/command.txt"
+	os.system(comandline)
+	text = ""
+	if os.path.exists("/tmp/command.txt") is True:
+		file = open("/tmp/command.txt", "r")
+		if strip == 1:
+			for line in file:
+				text = text + line.strip() + '\n'
+		else:
+			for line in file:
+				text = text + line
+				if text[-1:] != '\n':
+					text = text + "\n"
+		file.close()
+	# if one or last line then remove linefeed
+	if text[-1:] == '\n':
+		text = text[:-1]
+	comandline = text
+	os.system("rm /tmp/command.txt")
+	return comandline
 
 
 boxversion = BoxInfo.getItem("model")
