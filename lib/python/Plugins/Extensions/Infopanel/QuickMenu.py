6460a30f8765912f9b45f4d2580edf8bf9f1c26a
geeeeeeeeeee <82714245+geeeeeeeeeee@users.noreply.github.com>
2022-01-15 17:58:54 +0100

    Update QuickMenu.py
    I can't uninstall NetworkWizard because this

--- lib/python/Plugins/Extensions/Infopanel/QuickMenu.py
+++ lib/python/Plugins/Extensions/Infopanel/QuickMenu.py
@@ -433,7 +433,11 @@ class QuickMenu(Screen, ProtectedScreen):
 
 ######## Select Network Menu ##############################
 		if item[0] == _("Network Wizard"):
-			self.session.open(NetworkWizard)
+			try:
+				from Plugins.SystemPlugins.NetworkWizard.NetworkWizard import NetworkWizard
+				self.session.open(NetworkWizard)
+			except:
+				self.session.open(MessageBox, _("Sorry NetworkWizard is not installed!"), MessageBox.TYPE_INFO, timeout=10)
 		elif item[0] == _("Network Adapter Selection"):
 			self.session.open(NetworkAdapterSelection)
 		elif item[0] == _("Network Interface"):
