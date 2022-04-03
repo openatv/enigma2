f85797bf574ff3419e8351b35a66343c8b7cec84
Captain <captain.onboard@web.de>
2022-04-03 06:46:06 +0200

    cherry from https://github.com/OpenPLi/enigma2/pull/3376/commits/3978cccd302d9e726335b766335b05b14a585ba1

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -2245,8 +2245,8 @@ int eDVBServicePlay::selectAudioStream(int i)
 
 	int rdsPid = apid;
 
-		/* if we are not in PVR mode, timeshift is not active and we are not in pip mode, check if we need to enable the rds reader */
-	if (!(m_is_pvr || m_timeshift_active || m_decoder_index || m_have_video_pid || !m_is_primary))
+	/* if we are not in PVR mode, timeshift is not active and we are not in pip mode, check if we need to enable the rds reader */
+	if (!(m_timeshift_active || m_decoder_index || m_have_video_pid || !m_is_primary))
 	{
 		int different_pid = program.videoStreams.empty() && program.audioStreams.size() == 1 && program.audioStreams[stream].rdsPid != -1;
 		if (different_pid)
