dc54294b1139adf5085618a757d148415cf3999f
IanSav <IanSav@users.noreply.github.com>
2022-05-23 23:23:01 +1000

    [UnhandledKey.py] Add ability to display UnhandledKey
    This is a code proposal to allow any module or method to invoke the UnhandledKey icon without needing that supporting code to be replicated.
    
    For any code to display the UnhandledKey icon simply use code like:
    	from Screens.UnhandledKey import UnhandledKey
    	UnhandledKey.displayUnhandledKey()
    
    This will trigger the timed display of the UnhandledKey icon.

--- lib/python/Screens/UnhandledKey.py
+++ lib/python/Screens/UnhandledKey.py
@@ -1,8 +1,18 @@
-from Screens.Screen import Screen
+from enigma import eTimer
+
 from Components.Pixmap import Pixmap
+from Screens.Screen import Screen
 
 
 class UnhandledKey(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		self["UnhandledKeyPixmap"] = Pixmap()
+		self.unhandledKeyTimer = eTimer()
+		self.unhandledKeyTimer.callback.append(self.hide)
+		# if BoxInfo.getItem("OSDAnimation"):
+		# 	self.unhandledKeyDialog.setAnimationMode(0)
+
+	def displayUnhandledKey(self):
+		self.show()
+		self.unhandledKeyTimer.start(2000, True)
