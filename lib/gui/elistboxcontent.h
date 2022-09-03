2cac56590bbf0cdc5259664da80491e75acb54b2
jbleyel <jbleyel@gmx.net>
2022-09-03 10:08:58 +0200

    [eListboxPythonStringContent] remove unused m_font

--- lib/gui/elistboxcontent.h
+++ lib/gui/elistboxcontent.h
@@ -46,9 +46,8 @@ protected:
 
 protected:
 	ePyObject m_list;
-	int m_cursor, m_saved_cursor, m_saved_cursor_line; // NOSONAR
+	int m_cursor, m_saved_cursor, m_saved_cursor_line;
 	eSize m_itemsize;
-	ePtr<gFont> m_font;
 	int m_itemheight;
 #endif
 };
