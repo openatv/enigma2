02940c304292761f8048912e5ccc5cd973ab22e7
IanSav <IanSav@users.noreply.github.com>
2022-09-06 14:28:57 +1000

    [Setup.py] Clear a SONAR warning

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -151,7 +151,7 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 		else:
 			itemText = _(element.get("text", "??"))
 			itemDescription = _(element.get("description", " "))
-		item = eval(element.text or "") if element.text else ""
+		item = eval(element.text) if element.text else ""
 		if item == "":
 			self.list.append((self.formatItemText(itemText),))  # Add the comment line to the config list.
 		elif not isinstance(item, ConfigNothing):
