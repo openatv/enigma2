7f91ee8cbbf0b58723148b4d57597611f1c130f3
jbleyel <jbleyel@gmx.net>
2022-09-12 09:57:36 +0200

    * improve time shift settings migration

--- lib/python/Tools/Migration.py
+++ lib/python/Tools/Migration.py
@@ -1,4 +1,4 @@
-from Components.config import ConfigBoolean, ConfigText, ConfigSelectionNumber, ConfigYesNo, config
+from Components.config import ConfigBoolean, ConfigText, config
 
 menuMappings = {
 	"info_screen": "information",
@@ -19,8 +19,12 @@ menuMappings = {
 
 
 def migrateSettings():
-	migrateMenuSort()
-	migrateTimeshift()
+	config.misc.settingsversion = ConfigText("0")
+	if config.misc.settingsversion.value == "0":
+		migrateMenuSort()
+		migrateTimeshift()
+		config.misc.settingsversion.value = "1"
+		config.misc.settingsversion.save()
 
 
 def migrateMenuSort():  # This function needs to called in StartEnigma after init of config.usage.menu_sort_weight!
@@ -64,69 +68,38 @@ def migrateMenuSort():  # This function needs to called in StartEnigma after ini
 
 
 def migrateTimeshift():
-	# Time shift settings
+	# Update time shift settings.
+	stringList = (
+		("startdelay", "startDelay", "0"),
+		("timeshiftCheckEvents", "checkEvents", "0"),
+		("timeshiftCheckFreeSpace", "checkFreeSpace", "0"),
+		("timeshiftMaxHours", "maxHours", "12"),
+		("timeshiftMaxEvents", "maxEvents", "12")
+	)
+	booleanList = (
+		("showinfobar", "showInfoBar", True),
+		("showlivetvmsg", "showLiveTVMsg", True),
+		("filesplitting", "fileSplitting", True),
+		("stopwhilerecording", "stopWhileRecording", False),
+	)
 	try:
-		config.timeshift.startdelay = ConfigText(default="0")
-		if config.timeshift.startdelay.value != "0":
-			config.timeshift.startDelay.value = int(config.timeshift.startdelay.value)
-			config.timeshift.startDelay.save()
-			config.timeshift.startdelay.value = "0"
-			config.timeshift.startdelay.save()
-
-		config.timeshift.timeshiftCheckEvents = ConfigText(default="0")
-		if config.timeshift.timeshiftCheckEvents.value != "0":
-			config.timeshift.checkEvents.value = int(config.timeshift.timeshiftCheckEvents.value)
-			config.timeshift.checkEvents.save()
-			config.timeshift.timeshiftCheckEvents.value = "0"
-			config.timeshift.timeshiftCheckEvents.save()
-
-		config.timeshift.timeshiftCheckFreeSpace = ConfigText(default="0")
-		if config.timeshift.timeshiftCheckFreeSpace.value != "0":
-			config.timeshift.checkFreeSpace.value = int(config.timeshift.timeshiftCheckFreeSpace.value)
-			config.timeshift.checkFreeSpace.save()
-			config.timeshift.timeshiftCheckFreeSpace.value = "0"
-			config.timeshift.timeshiftCheckFreeSpace.save()
-
-		config.timeshift.timeshiftMaxHours = ConfigSelectionNumber(min=1, max=999, stepwidth=1, default=12, wraparound=True)
-		if config.timeshift.timeshiftMaxHours.value != 12:
-			config.timeshift.maxHours.value = config.timeshift.timeshiftMaxHours.value
-			config.timeshift.maxHours.save()
-			config.timeshift.timeshiftMaxHours.value = 12
-			config.timeshift.timeshiftMaxHours.save()
-
-		config.timeshift.timeshiftMaxEvents = ConfigSelectionNumber(min=1, max=999, stepwidth=1, default=12, wraparound=True)
-		if config.timeshift.timeshiftMaxEvents.value != 12:
-			config.timeshift.maxEvents.value = config.timeshift.timeshiftMaxEvents.value
-			config.timeshift.maxEvents.save()
-			config.timeshift.timeshiftMaxEvents.value = 12
-			config.timeshift.timeshiftMaxEvents.save()
-
-		config.timeshift.showinfobar = ConfigYesNo(default=True)
-		if not config.timeshift.showinfobar.value:
-			config.timeshift.showInfoBar.value = config.timeshift.showinfobar.value
-			config.timeshift.showInfoBar.save()
-			config.timeshift.showinfobar.value = True
-			config.timeshift.showinfobar.save()
-
-		config.timeshift.showlivetvmsg = ConfigYesNo(default=True)
-		if not config.timeshift.showlivetvmsg.value:
-			config.timeshift.showLiveTVMsg.value = config.timeshift.showlivetvmsg.value
-			config.timeshift.showLiveTVMsg.save()
-			config.timeshift.showlivetvmsg.value = True
-			config.timeshift.showlivetvmsg.save()
-
-		config.timeshift.filesplitting = ConfigYesNo(default=True)
-		if not config.timeshift.filesplitting.value:
-			config.timeshift.fileSplitting.value = config.timeshift.filesplitting.value
-			config.timeshift.fileSplitting.save()
-			config.timeshift.filesplitting.value = True
-			config.timeshift.filesplitting.save()
-
-		config.timeshift.stopwhilerecording = ConfigYesNo(default=False)
-		if config.timeshift.stopwhilerecording.value:
-			config.timeshift.stopWhileRecording.value = config.timeshift.stopwhilerecording.value
-			config.timeshift.stopWhileRecording.save()
-			config.timeshift.stopwhilerecording.value = False
-			config.timeshift.stopwhilerecording.save()
+		for item in stringList:
+			setattr(config.timeshift, item[0], ConfigText(default=item[2]))
+			value = getattr(config.timeshift, item[0]).value
+			default = getattr(config.timeshift, item[0]).default
+			if value != default:
+				getattr(config.timeshift, item[1]).value = value
+				getattr(config.timeshift, item[0]).value = default
+				getattr(config.timeshift, item[0]).save()
+				getattr(config.timeshift, item[1]).save()
+		for item in booleanList:
+			setattr(config.timeshift, item[0], ConfigBoolean(default=item[2]))
+			value = getattr(config.timeshift, item[0]).value
+			default = getattr(config.timeshift, item[0]).default
+			if value != default:
+				getattr(config.timeshift, item[1]).value = value
+				getattr(config.timeshift, item[0]).value = default
+				getattr(config.timeshift, item[0]).save()
+				getattr(config.timeshift, item[1]).save()
 	except Exception as err:
 		print("[Migration] migrateTimeshift Error: %s!" % str(err))
