6d940585b703b4c9ba1b87d5b12180fadc3aef7d
jbleyel <jbleyel@gmx.net>
2022-09-20 19:37:10 +0200

    remove old help screen and old remote control image code

--- lib/python/Screens/HelpMenu.py
+++ lib/python/Screens/HelpMenu.py
@@ -1,22 +1,17 @@
-from __future__ import print_function
 from collections import defaultdict
 from sys import maxsize
-from enigma import eActionMap, eListboxPythonMultiContent, eListbox, gFont
+from enigma import eActionMap
 from keyids import KEYIDS
 from Components.ActionMap import ActionMap, queryKeyBinding
 from Components.config import config
-from Components.GUIComponent import GUIComponent
 from Components.InputDevice import remoteControl
 from Components.Label import Label
 from Components.Pixmap import MovingPixmap, Pixmap
 from Components.SystemInfo import BoxInfo
 from Components.Sources.List import List
 from Components.Sources.StaticText import StaticText
-from Screens.Rc import Rc
 from Screens.Screen import Screen
-from Tools.KeyBindings import getKeyDescription
 from Tools.LoadPixmap import LoadPixmap
-from skin import parameters, fonts
 from functools import cmp_to_key
 
 
@@ -168,140 +163,6 @@ class ShowRemoteControl:
 			pixmap.addMovePoint(rcPos[0] + pos[0], rcPos[1] + pos[1], time=10)
 		pixmap.startMoving(callback)
 
-
-class HelpMenuOld(Screen, Rc):
-	def __init__(self, session, list):
-		Screen.__init__(self, session)
-		Screen.setTitle(self, _("Help"))
-		self.onSelChanged = []
-		self["list"] = HelpMenuListOld(list, self.close)
-		self["list"].onSelChanged.append(self.SelectionChanged)
-		Rc.__init__(self)
-		self["long_key"] = Label("")
-
-		self["actions"] = ActionMap(["WizardActions"],
-		{
-			"ok": self["list"].ok,
-			"back": self.close,
-		}, -1)
-
-		self.onLayoutFinish.append(self.SelectionChanged)
-
-	def SelectionChanged(self):
-		self.clearSelectedKeys()
-		selection = self["list"].getCurrent()
-		if selection:
-			selection = selection[3]
-		#arrow = self["arrowup"]
-		print("[HelpMenu] selection:", selection)
-
-		longText = ""
-		if selection and len(selection) > 1:
-			if selection[1] == "SHIFT":
-				self.selectKey("SHIFT")
-			elif selection[1] == "long":
-				longText = _("Long key press")
-		self["long_key"].setText(longText)
-
-		self.selectKey(selection[0])
-		#if selection is None:
-		print("[HelpMenu] select arrow")
-		#	arrow.moveTo(selection[1], selection[2], 1)
-		#	arrow.startMoving()
-		#	arrow.show()
-
-
-class HelpMenuListOld(GUIComponent):
-	def __init__(self, helplist, callback):
-		GUIComponent.__init__(self)
-		self.onSelChanged = []
-		self.l = eListboxPythonMultiContent()
-		self.callback = callback
-		self.extendedHelp = False
-
-		l = []
-		for (actionmap, context, actions) in helplist:
-			for (action, help) in actions:
-				if hasattr(help, '__call__'):
-					help = help()
-				if not help:
-					continue
-				buttons = queryKeyBinding(context, action)
-
-				# do not display entries which are not accessible from keys
-				if not len(buttons):
-					continue
-
-				name = None
-				flags = 0
-
-				for n in buttons:
-					(name, flags) = (getKeyDescription(n[0]), n[1])
-					if name is not None:
-						break
-
-				# only show entries with keys that are available on the used rc
-				if name is None:
-					continue
-
-				if flags & 8: # for long keypresses, prepend l_ into the key name.
-					name = (name[0], "long")
-
-				entry = [(actionmap, context, action, name)]
-
-				if isinstance(help, list):
-					self.extendedHelp = True
-					print("[HelpMenuList] extendedHelpEntry found")
-					x, y, w, h = parameters.get("HelpMenuListExtHlp0", (0, 0, 600, 26))
-					x1, y1, w1, h1 = parameters.get("HelpMenuListExtHlp1", (0, 28, 600, 20))
-					entry.extend((
-						(eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, 0, help[0]),
-						(eListboxPythonMultiContent.TYPE_TEXT, x1, y1, w1, h1, 1, 0, help[1])
-					))
-				else:
-					x, y, w, h = parameters.get("HelpMenuListHlp", (0, 0, 600, 28))
-					entry.append((eListboxPythonMultiContent.TYPE_TEXT, x, y, w, h, 0, 0, help))
-
-				l.append(entry)
-
-		self.l.setList(l)
-		if self.extendedHelp is True:
-			font = fonts.get("HelpMenuListExt0", ("Regular", 24, 50))
-			self.l.setFont(0, gFont(font[0], font[1]))
-			self.l.setItemHeight(font[2])
-			font = fonts.get("HelpMenuListExt1", ("Regular", 18))
-			self.l.setFont(1, gFont(font[0], font[1]))
-		else:
-			font = fonts.get("HelpMenuList", ("Regular", 24, 38))
-			self.l.setFont(0, gFont(font[0], font[1]))
-			self.l.setItemHeight(font[2])
-
-	def ok(self):
-		# a list entry has a "private" tuple as first entry...
-		l = self.getCurrent()
-		if l is None:
-			return
-		# ...containing (Actionmap, Context, Action, keydata).
-		# we returns this tuple to the callback.
-		self.callback(l[0], l[1], l[2])
-
-	def getCurrent(self):
-		sel = self.l.getCurrentSelection()
-		return sel and sel[0]
-
-	GUI_WIDGET = eListbox
-
-	def postWidgetCreate(self, instance):
-		instance.setContent(self.l)
-		instance.selectionChanged.get().append(self.selectionChanged)
-
-	def preWidgetRemove(self, instance):
-		instance.setContent(None)
-		instance.selectionChanged.get().remove(self.selectionChanged)
-
-	def selectionChanged(self):
-		for x in self.onSelChanged:
-			x()
 
 class HelpMenu(Screen, ShowRemoteControl):
 	def __init__(self, session, helpList):
