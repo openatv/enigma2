97e9d51cac24eb0d92b04aecd1f5b3dcfb737a80
jbleyel <jbleyel@gmx.net>
2022-11-02 11:47:00 +0100

    [UnhandledKey] (#2641)
    * [UnhandledKey.py]
    - Add config.usage.unhandledKeyTimeout setting to allow for different window pop up timeouts.
    - Move self/auto hide timer functionality to UnhandledKey screen code.
    - Move the setAnimationMode() here so it is not required elsewhere on invocation.
    - Sort imports.
    
    * [UsageConfig.py] Add config.usage.unhandledKeyTimeout
    
    * [setup.xml] Add config.usage.unhandledKeyTimeout
    
    * [InfoBarGenerics.py] 
    - Rework InfoBarUnhandledKey class
    - Move setAnimationMode() to the UnhandledKey screen code.
    - Simplify the show/hide logic.
    - PEP8 clean up the area of the changed code.
    - Remove the unconditional forced hide of the "UnhandledKey" error pop up screen and only hide it when a new key is pressed.  This stops the "UnhandledKey" pop up screen from being hidden by an unknown long press and any following repeat events.  (The problem is that when a user holds down a button to generate a long press the long press event is triggered but this code immediately sees a valid button press for the repeat mode and immediately clears the error pop up so that the user never gets to see it.)
    - Improve the method and variable names to be more clear of their function and content.
    - Add a new method, "showUnhandledKey()", to allow external code to trigger the display of the "UnhandledKey" error pop up screen.
    - Optimize the code.
    - Add a comment to explain the "flag" values.
    
    Co-authored-by: IanSav <iansav@users.noreply.github.com>

--- lib/python/Screens/UnhandledKey.py
+++ lib/python/Screens/UnhandledKey.py
@@ -1,8 +1,19 @@
-from Screens.Screen import Screen
+from enigma import eTimer
+
+from Components.config import config
 from Components.Pixmap import Pixmap
+from Screens.Screen import Screen
 
 
 class UnhandledKey(Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
 		self["UnhandledKeyPixmap"] = Pixmap()
+		self.unhandledKeyTimer = eTimer()
+		self.unhandledKeyTimer.callback.append(self.hide)
+		self.setAnimationMode(0)
+		self.hide()
+		self.onShow.append(self.startAutoHide)
+
+	def startAutoHide(self):
+		self.unhandledKeyTimer.start(config.usage.unhandledKeyTimeout.value * 1000, True)
