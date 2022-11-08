93ca153b4a2f182880f75712df2943fd681bd839
Captain <captain.onboard@web.de>
2022-11-08 09:29:21 +0100

    [frontend] update snr value for new uclan Model

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1644,6 +1644,10 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 				break;
 		}
 	}
+	else if (!strcmp(m_description, "AVL62X1")) // UCLAN
+	{
+		ret = snr;
+	}
 
 	signalqualitydb = ret;
 	if (ret == 0x12345678) // no snr db calculation avail.. return untouched snr value..
