32ac73237992644cb75eb9a85e92b26ef9a24da1
IanSav <IanSav@users.noreply.github.com>
2022-12-26 23:28:47 +1100

    [ActionMap.py] Improve legacy navigation code (#2728)
    * [ActionMap.py] Improve legacy navigation code
    
    - Allow the legacy navigation code enhancement to work on all action maps.
    - Improve the logging comments.
    
    Co-authored-by: jbleyel <jbleyel@gmx.net>

--- lib/python/Components/ActionMap.py
+++ lib/python/Components/ActionMap.py
@@ -1,3 +1,5 @@
+from sys import maxsize
+
 from enigma import eActionMap
 
 from keyids import KEYIDS
@@ -51,7 +53,7 @@ def parseKeymap(filename, context, actionMapInstance, device, domKeys):
 	for key in domKeys.findall("key"):
 		keyName = key.attrib.get("id")
 		if keyName is None:
-			print("[ActionMap] Error: Keymap attribute 'id' in context '%s' in file '%s' must be specified!" % (context, filename))
+			print("[ActionMap] Error: Key map attribute 'id' in context '%s' in file '%s' must be specified!" % (context, filename))
 			error = True
 		else:
 			try:
@@ -67,15 +69,15 @@ def parseKeymap(filename, context, actionMapInstance, device, domKeys):
 					elif keyName[1].lower() == "b":
 						keyId = int(keyName[2:], 2) | 0x8000
 					else:
-						print("[ActionMap] Error: Keymap id '%s' in context '%s' in file '%s' is not a hex, decimal, octal or binary number!" % (keyName, context, filename))
+						print("[ActionMap] Error: Key map id '%s' in context '%s' in file '%s' is not a hex, decimal, octal or binary number!" % (keyName, context, filename))
 						error = True
 				else:
 					keyId = KEYIDS.get(keyName, -1)
 					if keyId is None:
-						print("[ActionMap] Error: Keymap id '%s' in context '%s' in file '%s' is undefined/invalid!" % (keyName, context, filename))
+						print("[ActionMap] Error: Key map id '%s' in context '%s' in file '%s' is undefined/invalid!" % (keyName, context, filename))
 						error = True
 			except ValueError:
-				print("[ActionMap] Error: Keymap id '%s' in context '%s' in file '%s' can not be evaluated!" % (keyName, context, filename))
+				print("[ActionMap] Error: Key map id '%s' in context '%s' in file '%s' can not be evaluated!" % (keyName, context, filename))
 				keyId = -1
 				error = True
 		mapto = key.attrib.get("mapto")
@@ -100,7 +102,7 @@ def parseKeymap(filename, context, actionMapInstance, device, domKeys):
 				error = True
 			flags = newFlags
 		if not error:
-			if unmap is None:  # If a key was unmapped, it can only be assigned a new function in the same keymap file (avoid file parsing sequence dependency).
+			if unmap is None:  # If a key was unmapped, it can only be assigned a new function in the same key map file (avoid file parsing sequence dependency).
 				if unmapDict.get((context, keyName, mapto)) in [filename, None]:
 					if config.crash.debugActionMaps.value:
 						print("[ActionMap] Context '%s' keyName '%s' (%d) mapped to '%s' (Device: %s)." % (context, keyName, keyId, mapto, device.capitalize()))
@@ -155,7 +157,7 @@ def loadKeymap(filename):
 		for domMap in domKeymap.findall("map"):
 			context = domMap.attrib.get("context")
 			if context is None:
-				print("ActionMap] Error: All keymap action maps in '%s' must have a context!" % filename)
+				print("ActionMap] Error: All key map action maps in '%s' must have a context!" % filename)
 			else:
 				parseKeymap(filename, context, actionMapInstance, "generic", domMap)
 				for domDevice in domMap.findall("device"):
@@ -179,14 +181,39 @@ class ActionMap:
 		self.bound = False
 		self.execActive = False
 		self.enabled = True
+		self.legacyBound = False
 		undefinedAction = list(self.actions.keys())
+		leftActionDefined = "left" in undefinedAction
+		rightActionDefined = "right" in undefinedAction
+		leftAction = None
+		rightAction = None
 		for action in undefinedAction[:]:
 			for context in self.contexts:
+				if context == "NavigationActions":
+					if action == "pageUp" and not leftActionDefined:
+						leftAction = self.actions[action]
+					if action == "pageDown" and not rightActionDefined:
+						rightAction = self.actions[action]
 				if queryKeyBinding(context, action):
 					undefinedAction.remove(action)
 					break
+		if leftAction and rightAction and config.misc.actionLeftRightToPageUpPageDown.value:
+			if config.crash.debugActionMaps.value:
+				print("[ActionMap] DEBUG: Creating legacy navigation action map.")
+				print(leftAction)
+				print(rightAction)
+			self.legacyActions = {
+				"left": leftAction,
+				"right": rightAction
+			}
+		else:
+			self.legacyActions = {}
 		if undefinedAction:
-			print("[ActionMap] Keymap(s) '%s': Undefined action(s) '%s'." % ("', '".join(sorted(contexts)), "', '".join(sorted(undefinedAction))))
+			context = "', '".join(sorted(self.contexts))
+			contextPlural = "s" if len(self.contexts) > 1 else ""
+			action = "', '".join(sorted(undefinedAction))
+			actionPlural = "s" if len(undefinedAction) > 1 else ""
+			print("[ActionMap] Map context%s '%s': Undefined action%s '%s'." % (contextPlural, context, actionPlural, action))
 
 	def getEnabled(self):
 		return self.enabled
@@ -196,12 +223,18 @@ class ActionMap:
 		self.checkBind()
 
 	def doBind(self):
+		if not self.legacyBound and self.legacyActions:
+			self.actionMapInstance.bindAction("NavigationActions", maxsize - 1, self.legacyAction)
+			self.legacyBound = True
 		if not self.bound:
 			for context in self.contexts:
 				self.actionMapInstance.bindAction(context, self.prio, self.action)
 			self.bound = True
 
 	def doUnbind(self):
+		if self.legacyBound and self.legacyActions:
+			self.actionMapInstance.unbindAction("NavigationActions", self.legacyAction)
+			self.legacyBound = False
 		if self.bound:
 			for context in self.contexts:
 				self.actionMapInstance.unbindAction(context, self.action)
@@ -223,12 +256,23 @@ class ActionMap:
 
 	def action(self, context, action):
 		if action in self.actions:
-			print("[ActionMap] Keymap '%s' -> Action '%s'." % (context, action))
+			print("[ActionMap] Map context '%s' -> Action '%s'." % (context, action))
 			response = self.actions[action]()
 			if response is not None:
 				return response
 			return 1
-		print("[ActionMap] Keymap '%s' -> Unknown action '%s'! (Typo in keymap?)" % (context, action))
+		print("[ActionMap] Map context '%s' -> Unknown action '%s'!  (Typo in map?)" % (context, action))
+		return 0
+
+	def legacyAction(self, context, action):
+		if action in self.legacyActions:
+			print("[ActionMap] Map context '%s' -> Legacy action '%s'." % (context, action))
+			print(self.legacyActions[action])
+			response = self.legacyActions[action]()
+			if response is not None:
+				return response
+			return 1
+		print("[ActionMap] Map context '%s' -> Unknown legacy action '%s'!  (Typo in map?)" % (context, action))
 		return 0
 
 	def destroy(self):
@@ -265,14 +309,7 @@ class HelpableActionMap(ActionMap):
 		actions = actions or {}
 		self.description = description
 		actionDict = {}
-		legacyMode = config.usage.actionLeftRightToPageUpPageDown.value
 		for context in contexts:
-			if legacyMode and context == "NavigationActions":
-				copyLeft = "left" not in actions and "pageUp" in actions
-				copyRight = "right" not in actions and "pageDown" in actions
-			else:
-				copyLeft = False
-				copyRight = False
 			actionList = []
 			for (action, response) in actions.items():
 				if not isinstance(response, (list, tuple)):
@@ -280,12 +317,6 @@ class HelpableActionMap(ActionMap):
 				if queryKeyBinding(context, action):
 					actionList.append((action, response[1]))
 				actionDict[action] = response[0]
-				if copyLeft and action == "pageUp":
-					actionList.append(("left", response[1]))
-					actionDict["left"] = response[0]
-				if copyRight and action == "pageDown":
-					actionList.append(("right", response[1]))
-					actionDict["right"] = response[0]
 			parent.helpList.append((self, context, actionList))
 		ActionMap.__init__(self, contexts, actionDict, prio)
 
