938c35ad111682b13abf7a12904ae862c325b06d
jbleyel <jbleyel@gmx.net>
2023-02-18 22:23:47 +0100

    [PositionGauge] * Fix cutlist reference issue, taken from EMCPositionGauge.
    [DisplaySkin]
    * Use enigma core PositionGauge instead of the EMC duplicate.
    
    (cherry picked from commit 046c0a315933d85b79b584847e2a34d3d20e9921)

--- lib/python/Components/Renderer/PositionGauge.py
+++ lib/python/Components/Renderer/PositionGauge.py
@@ -52,7 +52,8 @@ class PositionGauge(Renderer):
 
 	def setCutlist(self, cutlist):
 		if self.__cutlist != cutlist:
-			self.__cutlist = cutlist
+			# E2 Bug: Use a list copy instead of a reference
+			self.__cutlist = cutlist[:]
 			if self.instance is not None:
 				self.instance.setInOutList(cutlist)
 
