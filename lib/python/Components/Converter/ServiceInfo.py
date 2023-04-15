c942ceb22b9341d2c6a49405e1c5c4b7c7f48eb4
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2023-04-15 08:06:32 +0100

    Tweak last commit

--- lib/python/Components/Converter/ServiceInfo.py
+++ lib/python/Components/Converter/ServiceInfo.py
@@ -254,7 +254,7 @@ class ServiceInfo(Poll, Converter):
 			if info.getInfo(iServiceInformation.sGamma) > 0:
 				return False
 			else:
-				return video_width >= 720 and video_width < 2160
+				return video_width >= 721 and video_width < 2160
 		elif self.type == self.IS_1080:
 			return video_height > 1000 and video_height <= 1080
 		elif self.type == self.IS_720:
