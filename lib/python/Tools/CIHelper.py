8cc2a959108c211819f03e0fb1632845faf72eab
jbleyel <jbleyel@gmx.net>
2023-08-29 15:58:29 +0200

    [CIHelper] * remove six * reorder imports * tiny cleanups

--- lib/python/Tools/CIHelper.py
+++ lib/python/Tools/CIHelper.py
@@ -1,10 +1,8 @@
+from os.path import exists
 from xml.etree.ElementTree import parse
 from enigma import eDVBCIInterfaces, eDVBCI_UI, eEnv, eServiceCenter, eServiceReference
 from timer import TimerEntry
 import NavigationInstance
-
-import os
-import six
 
 
 class CIHelper:
@@ -20,14 +18,13 @@ class CIHelper:
 			self.CI_ASSIGNMENT_LIST = []
 
 			def getValue(definitions, default):
-				ret = ""
 				Len = len(definitions)
 				return Len > 0 and definitions[Len - 1].text or default
 
-			for ci in list(range(NUM_CI)):
-				filename = eEnv.resolve("${sysconfdir}/enigma2/ci") + str(ci) + ".xml"
+			for ci in range(NUM_CI):
+				filename = eEnv.resolve("${sysconfdir}/enigma2/ci") + f"{ci}.xml"
 
-				if not os.path.exists(filename):
+				if not exists(filename):
 					continue
 
 				try:
@@ -36,44 +33,44 @@ class CIHelper:
 					read_providers = []
 					usingcaid = []
 					for slot in tree.findall("slot"):
-						read_slot = six.ensure_str(getValue(slot.findall("id"), False))
+						read_slot = getValue(slot.findall("id"), False)
 
 						for caid in slot.findall("caid"):
-							read_caid = six.ensure_str(caid.get("id"))
+							read_caid = caid.get("id")
 							usingcaid.append(int(read_caid, 16))
 
 						for service in slot.findall("service"):
-							read_service_ref = six.ensure_str(service.get("ref"))
+							read_service_ref = service.get("ref")
 							read_services.append(read_service_ref)
 
 						for provider in slot.findall("provider"):
-							read_provider_name = six.ensure_str(provider.get("name"))
-							read_provider_dvbname = six.ensure_str(provider.get("dvbnamespace"))
+							read_provider_name = provider.get("name")
+							read_provider_dvbname = provider.get("dvbnamespace")
 							read_providers.append((read_provider_name, int(read_provider_dvbname, 16)))
 
 						self.CI_ASSIGNMENT_LIST.append((int(read_slot), (read_services, read_providers, usingcaid)))
-				except:
-					print("[CI_ASSIGNMENT %d] error parsing xml..." % ci)
+				except Exception:
+					print(f"[CI_ASSIGNMENT {ci}] error parsing xml...")
 
 			services = []
 			providers = []
 			for item in self.CI_ASSIGNMENT_LIST:
-				print("[CI_Activate] activate CI%d with following settings:" % item[0])
+				print(f"[CI_Activate] activate CI{item[0]} with following settings:")
 				print(item[1])
 				try:
 					eDVBCIInterfaces.getInstance().setDescrambleRules(item[0], item[1])
-				except:
-					print("[CI_Activate_Config_CI%d] error setting DescrambleRules..." % item[0])
+				except Exception:
+					print(f"[CI_Activate_Config_CI{item[0]}] error setting DescrambleRules...")
 				for x in item[1][0]:
 					services.append(x)
 				for x in item[1][1]:
 					providers.append(x[0])
 			service_refs = []
-			if len(services):
+			if services:
 				for x in services:
 					service_refs.append(eServiceReference(x))
 			provider_services_refs = []
-			if len(providers):
+			if providers:
 				provider_services_refs = self.getProivderServices(providers)
 			self.CI_ASSIGNMENT_SERVICES_LIST = [service_refs, provider_services_refs]
 
@@ -86,7 +83,7 @@ class CIHelper:
 		if len(providers):
 			serviceHandler = eServiceCenter.getInstance()
 			for x in providers:
-				refstr = '1:7:0:0:0:0:0:0:0:0:(provider == "%s") && (type == 1) || (type == 17) || (type == 22) || (type == 25) || (type == 31) || (type == 134) || (type == 195) ORDER BY name:%s' % (x, x)
+				refstr = f'1:7:0:0:0:0:0:0:0:0:(provider == "{x}") && (type == 1) || (type == 17) || (type == 22) || (type == 25) || (type == 31) || (type == 134) || (type == 195) ORDER BY name:{x}'
 				myref = eServiceReference(refstr)
 				servicelist = serviceHandler.list(myref)
 				if servicelist is not None:
@@ -102,7 +99,7 @@ class CIHelper:
 
 		if self.CI_ASSIGNMENT_SERVICES_LIST:
 			for x in self.CI_ASSIGNMENT_SERVICES_LIST:
-				if len(x) and ref in x:
+				if x and ref in x:
 					return True
 		return False
 
@@ -111,7 +108,7 @@ class CIHelper:
 			no_ci = eDVBCIInterfaces.getInstance().getNumOfSlots()
 			if no_ci > 0:
 				self.CI_MULTIDESCRAMBLE = False
-				for ci in list(range(no_ci)):
+				for ci in range(no_ci):
 					appname = eDVBCI_UI.getInstance().getAppName(ci)
 					if appname in self.CI_MULTIDESCRAMBLE_MODULES:
 						self.CI_MULTIDESCRAMBLE = True
