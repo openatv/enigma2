90ec5a7107bb38c91b0de12303beaba8decd8e5e
jbleyel <jbleyel@gmx.net>
2023-09-16 15:53:06 +0200

    [eRectangle] * remove border properties because they are now in parent class

--- lib/gui/erectangle.h
+++ lib/gui/erectangle.h
@@ -8,16 +8,10 @@ class eRectangle : public eWidget
 public:
 	eRectangle(eWidget *parent);
 
-	void setBorderWidth(int pixel);
-	void setBorderColor(const gRGB &color);
 
 protected:
 	int event(int event, void *data = 0, void *data2 = 0);
 
-private:
-	bool m_have_border_color;
-	int m_border_width;
-	gRGB m_border_color;
 };
 
 #endif
