bb8b26254a6b5bc4388a97f68c13dc9b4ead8de6
jbleyel <jbleyel@gmx.net>
2023-10-28 20:01:38 +0200

    [VReference/Renderer] * small cleanups

--- lib/python/Components/Renderer/VReference.py
+++ lib/python/Components/Renderer/VReference.py
@@ -15,8 +15,8 @@
 #
 #######################################################################
 
-from Components.Renderer.Renderer import Renderer
 from enigma import eLabel
+from Components.Renderer.Renderer import Renderer
 from Components.VariableText import VariableText
 
 
@@ -39,8 +39,5 @@ class VReference(VariableText, Renderer):
 			else:
 				service = self.source.service
 				sname = service.toString()
-				pos = sname.rfind(':')
-				if pos != -1:
-					self.text = "Reference: " + sname[:-1]
-				else:
-					self.text = "Reference reading error !"
+				pos = sname.rfind(":")
+				self.text = f"Reference: {sname[:-1]}" if pos != -1 else "Reference reading error !"
