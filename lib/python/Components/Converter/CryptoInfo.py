7be79a6c26f2f44ffe7f7ed2dea4614171c39f63
jbleyel <jbleyel@gmx.net>
2023-10-28 22:04:17 +0200

    [CryptoInfo/Converter] * don't start the Poll if show_cryptoinfo is off

--- lib/python/Components/Converter/CryptoInfo.py
+++ lib/python/Components/Converter/CryptoInfo.py
@@ -1,8 +1,8 @@
 from Components.Converter.Converter import Converter
+from Components.Converter.Poll import Poll
 from Components.Element import cached
 from Components.config import config
 from Tools.GetEcmInfo import GetEcmInfo
-from Components.Converter.Poll import Poll
 
 
 class CryptoInfo(Poll, Converter):
@@ -11,12 +11,19 @@ class CryptoInfo(Poll, Converter):
 		Poll.__init__(self)
 
 		self.type = type
-		self.active = False
+		self.active = False  # TODO what's this
 		self.visible = config.usage.show_cryptoinfo.value > 0
-		self.textvalue = ""
-		self.poll_interval = 1000
-		self.poll_enabled = True
+		self.textvalue = ""  # TODO what's this
 		self.ecmdata = GetEcmInfo()
+
+		def ShowCryptoInfo(configElement):
+			if configElement.value > 0:
+				self.poll_interval = 1000
+				self.poll_enabled = True
+			else:
+				self.poll_enabled = False
+
+		config.usage.show_cryptoinfo.addNotifier(ShowCryptoInfo)
 
 	@cached
 	def getText(self):
