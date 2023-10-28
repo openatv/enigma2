a14d99720f7e9d56a5edd5586db221a087f33340
jbleyel <jbleyel@gmx.net>
2023-10-28 21:37:21 +0200

    [ClientsStreaming/Converter] * use f-strings * use dict get instead of branches

--- lib/python/Components/Converter/ClientsStreaming.py
+++ lib/python/Components/Converter/ClientsStreaming.py
@@ -1,9 +1,9 @@
+from socket import gethostbyaddr
+from enigma import eStreamServer
 from Components.Converter.Converter import Converter
 from Components.Converter.Poll import Poll
 from Components.Element import cached
-from enigma import eStreamServer
 from ServiceReference import ServiceReference
-import socket
 
 
 class ClientsStreaming(Converter, Poll):
@@ -26,32 +26,21 @@ class ClientsStreaming(Converter, Poll):
 		Poll.__init__(self)
 		self.poll_interval = 30000
 		self.poll_enabled = True
-		if type == "REF":
-			self.type = self.REF
-		elif type == "IP":
-			self.type = self.IP
-		elif type == "NAME":
-			self.type = self.NAME
-		elif type == "ENCODER":
-			self.type = self.ENCODER
-		elif type == "NUMBER":
-			self.type = self.NUMBER
-		elif type == "SHORT_ALL":
-			self.type = self.SHORT_ALL
-		elif type == "ALL":
-			self.type = self.ALL
-		elif type == "INFO":
-			self.type = self.INFO
-		elif type == "INFO_RESOLVE":
-			self.type = self.INFO_RESOLVE
-		elif type == "INFO_RESOLVE_SHORT":
-			self.type = self.INFO_RESOLVE_SHORT
-		elif type == "EXTRA_INFO":
-			self.type = self.EXTRA_INFO
-		elif type == "DATA":
-			self.type = self.DATA
-		else:
-			self.type = self.UNKNOWN
+
+		self.type = {
+			"REF": self.REF,
+			"IP": self.IP,
+			"NAME": self.NAME,
+			"ENCODER": self.ENCODER,
+			"NUMBER": self.NUMBER,
+			"SHORT_ALL": self.SHORT_ALL,
+			"ALL": self.ALL,
+			"INFO": self.INFO,
+			"INFO_RESOLVE": self.INFO_RESOLVE,
+			"INFO_RESOLVE_SHORT": self.INFO_RESOLVE_SHORT,
+			"EXTRA_INFO": self.EXTRA_INFO,
+			"DATA": self.DATA,
+		}.get(type, self.UNKNOWN)
 
 		self.streamServer = eStreamServer.getInstance()
 
@@ -65,7 +54,7 @@ class ClientsStreaming(Converter, Poll):
 		ips = []
 		names = []
 		encoders = []
-		extrainfo = _("ClientIP") + "\t\t" + _("Transcode") + "\t" + _("Channel") + "\n\n"
+		extrainfo = f'{_("ClientIP")}\t\t{_("Transcode")}\t{_("Channel")}\n\n'
 		info = ""
 
 		for x in self.streamServer.getConnectedClients():
@@ -79,45 +68,45 @@ class ClientsStreaming(Converter, Poll):
 
 			if int(x[2]) == 0:
 				strtype = "S"
-				encoder = _('No')
+				encoder = _("No")
 			else:
 				strtype = "T"
-				encoder = _('Yes')
+				encoder = _("Yes")
 
 			encoders.append((encoder))
 
 			if self.type == self.INFO_RESOLVE or self.type == self.INFO_RESOLVE_SHORT:
 				try:
-					raw = socket.gethostbyaddr(ip)
+					raw = gethostbyaddr(ip)
 					ip = raw[0]
-				except:
+				except Exception:
 					pass
 
 				if self.type == self.INFO_RESOLVE_SHORT:
-					ip, sep, tail = ip.partition('.')
+					ip, sep, tail = ip.partition(".")
 
-			info += ("%s %-8s %s\n") % (strtype, ip, service_name)
+			info = f"{info}{strtype} {ip:8s} {service_name}\n"
 
 			clients.append((ip, service_name, encoder))
 
-			extrainfo += ("%-8s\t%s\t%s") % (ip, encoder, service_name) + "\n"
+			extrainfo = f"{extrainfo}{ip:8s}\t{encoder}\t{service_name}\n"
 
 		if self.type == self.REF:
-			return ' '.join(refs)
+			return " ".join(refs)
 		elif self.type == self.IP:
-			return ' '.join(ips)
+			return " ".join(ips)
 		elif self.type == self.NAME:
-			return ' '.join(names)
+			return " ".join(names)
 		elif self.type == self.ENCODER:
-			return _("Transcoding: ") + ' '.join(encoders)
+			return _("Transcoding: ") + " ".join(encoders)
 		elif self.type == self.NUMBER:
 			return str(len(clients))
 		elif self.type == self.EXTRA_INFO:
 			return extrainfo
 		elif self.type == self.SHORT_ALL:
-			return _("Total clients streaming: %d ( %s )") % (len(clients), ' '.join(names))
+			return _("Total clients streaming: %d ( %s )") % (len(clients), " ".join(names))
 		elif self.type == self.ALL:
-			return '\n'.join(' '.join(elems) for elems in clients)
+			return "\n".join(" ".join(elems) for elems in clients)
 		elif self.type == self.INFO or self.type == self.INFO_RESOLVE or self.type == self.INFO_RESOLVE_SHORT:
 			return info
 		elif self.type == self.DATA:
