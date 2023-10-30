ff5451526629a4d5a023fe755e6758103d411182
jbleyel <jbleyel@gmx.net>
2023-10-30 17:49:06 +0100

    [ChannelNumber/Renderer] * cleanup

--- lib/python/Components/Renderer/ChannelNumber.py
+++ lib/python/Components/Renderer/ChannelNumber.py
@@ -1,20 +1,18 @@
-from Components.VariableText import VariableText
 from enigma import eLabel, iPlayableService
 from Components.Renderer.Renderer import Renderer
+from Components.VariableText import VariableText
 
 
 class ChannelNumber(Renderer, VariableText):
+	GUI_WIDGET = eLabel
+
 	def __init__(self):
 		Renderer.__init__(self)
 		VariableText.__init__(self)
 		self.text = "---"
-	GUI_WIDGET = eLabel
 
 	def changed(self, what):
 		if what is True or what[0] == self.CHANGED_SPECIFIC and what[1] == iPlayableService.evStart:
 			service = self.source.serviceref
 			num = service and service.getChannelNum() or None
-			if num:
-				self.text = str(num)
-			else:
-				self.text = '---'
+			self.text = str(num) if num else "---"
