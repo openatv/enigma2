9ae8dd692724db6639401f92bbd200bb418d3098
jbleyel <jbleyel@gmx.net>
2023-10-30 18:01:31 +0100

    [Cover/Renderer] * cleanup

--- lib/python/Components/Renderer/Cover.py
+++ lib/python/Components/Renderer/Cover.py
@@ -1,27 +1,24 @@
-from Components.Renderer.Renderer import Renderer
+from os.path import basename, dirname, exists, isdir, isfile, join, split, splitext
 from enigma import ePixmap, ePicLoad
 from Components.config import config
-from Components.Sources.ServiceEvent import ServiceEvent
+from Components.Renderer.Renderer import Renderer
 from Components.Sources.CurrentService import CurrentService
-from os import path as os_path
+from Components.Sources.ServiceEvent import ServiceEvent
 
 
 class Cover(Renderer):
-	exts = ('.jpg', '.png', '.jpeg')
+	EXTENSIONS = (".jpg", ".png", ".jpeg")
+	GUI_WIDGET = ePixmap
 
 	def __init__(self):
 		Renderer.__init__(self)
 		self.nameCache = {}
-		self.picname = ''
-
-	GUI_WIDGET = ePixmap
+		self.picname = ""
 
 	def changed(self, what):
-		if not self.instance:
-			return
-		else:
-			picname = ''
-			sname = ''
+		if self.instance:
+			picname = ""
+			sname = ""
 			if what[0] != self.CHANGED_CLEAR:
 				service = None
 				if isinstance(self.source, ServiceEvent):
@@ -32,41 +29,34 @@ class Cover(Renderer):
 					sname = service.getPath()
 				else:
 					return
-				picname = self.nameCache.get(sname, '')
-				if picname == '':
+				picname = self.nameCache.get(sname, "")
+				if picname == "":
 					picname = self.findCover(sname)[1]
-				if picname == '':
+				if picname == "":
 					path = sname
-					if service.toString().endswith == '..':
+					if service.toString().endswith == "..":
 						path = config.movielist.last_videodir.value
-					for ext in self.exts:
-						p = os_path.dirname(path) + '/folder' + ext
-						picname = os_path.exists(p) and p or ''
+					for ext in self.EXTENSIONS:
+						p = dirname(path) + "/folder" + ext
+						picname = exists(p) and p or ""
 						if picname:
 							break
 
-				if picname != '':
+				if picname != "":
 					self.nameCache[sname] = picname
 				if picname == self.picname:
 					return
 				self.picname = picname
-				if picname != '' and os_path.exists(picname):
+				if picname != "" and exists(picname):
 					size = self.instance.size()
 					self.picload = ePicLoad()
 					self.picload.PictureData.get().append(self.showCoverCallback)
 					if self.picload:
-						self.picload.setPara((size.width(),
-						size.height(),
-						1,
-						1,
-						False,
-						1,
-						'#00000000'))
+						self.picload.setPara((size.width(), size.height(), 1, 1, False, 1, "#00000000"))
 						if self.picload.startDecode(picname) != 0:
 							del self.picload
 				else:
 					self.instance.hide()
-		return
 
 	def showCoverCallback(self, picInfo=None):
 		if self.picload:
@@ -75,39 +65,37 @@ class Cover(Renderer):
 				self.instance.setPixmap(ptr)
 				self.instance.show()
 			del self.picload
-		return
 
 	def findCover(self, path):
-		fpath = p1 = p2 = p3 = ''
-		name, ext = os_path.splitext(path)
+		fpath = p1 = p2 = p3 = ""
+		name, ext = splitext(path)
 		ext = ext.lower()
-		if os_path.isfile(path):
-			dir = os_path.dirname(path)
+		if isfile(path):
+			directory = dirname(path)
 			p1 = name
-			p2 = os_path.join(dir, os_path.basename(dir))
-		elif os_path.isdir(path):
-			if path.lower().endswith('/bdmv'):
-				dir = path[:-5]
-				if dir.lower().endswith('/brd'):
-					dir = dir[:-4]
-			elif path.lower().endswith('video_ts'):
-				dir = path[:-9]
-				if dir.lower().endswith('/dvd'):
-					dir = dir[:-4]
+			p2 = join(directory, basename(directory))
+		elif isdir(path):
+			if path.lower().endswith("/bdmv"):
+				directory = path[:-5]
+				if directory.lower().endswith("/brd"):
+					directory = directory[:-4]
+			elif path.lower().endswith("video_ts"):
+				directory = path[:-9]
+				if directory.lower().endswith("/dvd"):
+					directory = directory[:-4]
 			else:
-				dir = path
-				p2 = os_path.join(dir, 'folder')
-			prtdir, dirname = os_path.split(dir)
-			p1 = os_path.join(dir, dirname)
-			p3 = os_path.join(prtdir, dirname)
+				directory = path
+				p2 = join(directory, "folder")
+			prtdir, directory = split(directory)
+			p1 = join(dir, directory)
+			p3 = join(prtdir, directory)
 		pathes = (p1, p2, p3)
 		for p in pathes:
-			for ext in self.exts:
+			for ext in self.EXTENSIONS:
 				path = p + ext
-				if os_path.exists(path):
+				if exists(path):
 					break
-
-			if os_path.exists(path):
+			if exists(path):
 				fpath = path
 				break
 
