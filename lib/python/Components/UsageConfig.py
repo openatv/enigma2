f6b0799f0f48405f6a5ab002b4a901a4ac8fd0cf
madhouse <35741027+m4dhouse@users.noreply.github.com>
2023-11-30 09:38:58 +0100

    [NetworkSetup/UsageConfig] (#3159)
    Added NordVpn to UsageConfig

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -143,6 +143,7 @@ def InitUsageConfig():
 		("google", _("Google DNS")),
 		("cloudflare", _("Cloudflare DNS")),
 		("quad9", _("Quad9 DNS")),
+		("nordvpn", _("NordVpn DNS")),
 		("opendns-familyshield", _("OpenDNS FamilyShield")),
 		("opendns-home", _("OpenDNS Home"))
 	])
