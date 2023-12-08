f7a4fc4b2a9ca7b63e109a86b74b9965eb3cb889
jbleyel <jbleyel@gmx.net>
2023-12-08 20:11:05 +0100

    [ChannelSelection] * force save root on zap

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -2471,6 +2471,7 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
 		elif ref is None or ref != nref:
 			Screens.InfoBar.InfoBar.instance.checkTimeshiftRunning(boundFunction(self.zapCheckTimeshiftCallback, enable_pipzap, preview_zap, nref))
 		elif not preview_zap:
+			self.lastroot.value = ""  # force save root
 			self.saveRoot()
 			self.saveChannel(nref)
 			config.servicelist.lastmode.save()
@@ -2485,6 +2486,7 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
 			self.new_service_played = True
 			self.session.nav.playService(nref)
 			if not preview_zap:
+				self.lastroot.value = ""  # force save root
 				self.saveRoot()
 				self.saveChannel(nref)
 				config.servicelist.lastmode.save()
