788fc3fa29b784259df759de934675773358ea5a
jbleyel <jbleyel@gmx.net>
2023-12-13 17:57:32 +0100

    [bsod] * tweak message text / remove config.crash.debug_text
    Close #3189

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1404,13 +1404,6 @@ def InitUsageConfig():
 			choiceList.append((pathjoin(partition.mountpoint, "logs", ""), normpath(partition.mountpoint)))
 	config.crash.debug_path = ConfigSelection(default="/home/root/logs/", choices=choiceList)
 	config.crash.debug_path.addNotifier(updateDebugPath, immediate_feedback=False)
-
-	crashlogheader = _("We are really sorry. Your receiver encountered "
-		"a software problem, and needs to be restarted.\n"
-		"Please send the logfile %senigma2_crash_xxxxxx.log to www.opena.tv.\n"
-		"Your receiver restarts in 10 seconds!\n"
-		"Component: enigma2") % config.crash.debug_path.value
-	config.crash.debug_text = ConfigText(default=crashlogheader, fixed_size=False)
 	config.crash.skin_error_crash = ConfigYesNo(default=True)
 
 	def updateStackTracePrinter(configElement):
