eb650380ebcf8394f2eae917ab17d48d721aacc5
jbleyel <jbleyel@gmx.net>
2023-12-23 12:43:19 +0100

    [TemplatedMultiContent.py] Fix and improve code
    - Convert the "assert" commands into logged errors.
    - Correct the "templates" "default" conversion to "template".
    - Correct the initialization scaling of simple templates.
    - Reduce the scope of single use methods.
    - Move some imports to the top of the module.
    - Improve some variable names.
    
    Co-authored-by: @IanSav

--- lib/python/Components/Converter/TemplatedMultiContent.py
+++ lib/python/Components/Converter/TemplatedMultiContent.py
@@ -1,3 +1,5 @@
+from enigma import eListboxPythonMultiContent, gFont
+
 from Components.Converter.StringList import StringList
 
 
@@ -13,107 +15,112 @@ class TemplatedMultiContent(StringList):
 		loc = locals()
 		del loc["self"]  # Cleanup locals a bit.
 		del loc["args"]
-		self.active_style = None
+		self.activeStyle = None
 		self.template = eval(args, {}, loc)
 		self.scale = None
-		assert "fonts" in self.template, "templates must include a 'fonts' entry"
-		assert "itemHeight" in self.template or "itemSize" in self.template, "templates must include an 'itemHeight' or 'itemSize' entry"
-		assert "template" in self.template or "templates" in self.template, "templates must include a 'template' or 'templates' entry"
-		assert "template" in self.template or "default" in self.template["templates"], "templates must include a 'default' template"  # We need to have a default template.
-		if "template" not in self.template:  # Default template can be ["template"] or ["templates"]["default"].
-			self.template["template"] = self.template["templates"]["default"][1]
-			self.template["itemSize"] = self.template["template"][0]
+		if "template" in self.template or "templates" in self.template:
+			if "template" in self.template or "default" in self.template["templates"]:
+				if "template" not in self.template:  # Default template can be ["template"] or ["templates"]["default"].
+					self.template["itemSize"] = self.template["templates"]["default"][0]
+					self.template["template"] = self.template["templates"]["default"][1]
+				if "fonts" not in self.template:
+					print("[TemplatedMultiContent] Error: All templates must include a 'fonts' entry!")
+				if "itemHeight" in self.template and "itemSize" not in self.template:
+					self.template["itemSize"] = self.template["itemHeight"]
+				if "itemSize" not in self.template:
+					print("[TemplatedMultiContent] Error: All 'template' entries must include an 'itemHeight' or 'itemSize' entry!")
+			else:
+				print("[TemplatedMultiContent] Error: All 'templates' must include a 'default' template!")
+		else:
+			print("[TemplatedMultiContent] Error: All TemplatedMultiContent converters must include either a 'template' or 'templates' entry!")
 
 	def changed(self, what):
+		def setTemplate():
+			def scaleTemplate(template, itemWidth, itemHeight):
+				scaleFactorVertical = self.scale[1][0] / self.scale[1][1]
+				scaleFactorHorizontal = self.scale[0][0] / self.scale[0][1]
+				itemWidth = int(itemWidth * scaleFactorHorizontal)
+				itemHeight = int(itemHeight * scaleFactorVertical)
+				scaledTemplate = []
+				fonts = []
+				for font in self.template["fonts"]:
+					fonts.append(gFont(font.family, int(font.pointSize * scaleFactorVertical)))
+				for content in template:
+					elements = list(content)
+					elements[1] = int(elements[1] * scaleFactorVertical)
+					elements[2] = int(elements[2] * scaleFactorHorizontal)
+					elements[3] = int(elements[3] * scaleFactorVertical)
+					elements[4] = int(elements[4] * scaleFactorHorizontal)
+					scaledTemplate.append(tuple(elements))
+				return scaledTemplate, itemHeight, itemWidth, fonts
+
+			if self.source:
+				style = self.source.style
+				if style == self.activeStyle:
+					return
+				templates = self.template.get("templates")  # If skin defined "templates", that means that it defines multiple styles in a dictionary but template should still be a default.
+				template = self.template.get("template")
+				if "itemHeight" in self.template:
+					itemHeight = self.template["itemHeight"]
+					itemWidth = itemHeight
+				if "itemWidth" in self.template:
+					itemWidth = self.template["itemWidth"]
+				if "itemSize" in self.template:
+					itemWidth = self.template["itemSize"]
+					itemHeight = itemWidth
+				selectionEnabled = self.template.get("selectionEnabled", None)
+				scrollbarMode = self.template.get("scrollbarMode", None)
+				if templates and style and style in templates:  # If we have a custom style defined in the source, and different templates in the skin, look the template up.
+					template = templates[style][1]
+					if isinstance(templates[style][0], tuple):
+						itemWidth = templates[style][0][0]
+						itemHeight = templates[style][0][1]
+					else:
+						itemWidth = templates[style][0]
+						itemHeight = itemWidth
+					if len(templates[style]) > 2:
+						selectionEnabled = templates[style][2]
+					if len(templates[style]) > 3:
+						scrollbarMode = templates[style][3]
+				if self.scale:
+					template, itemWidth, itemHeight, fonts = scaleTemplate(template, itemWidth, itemHeight)
+					for index, font in enumerate(fonts):
+						self.content.setFont(index, font)
+				self.content.setTemplate(template)
+				self.content.setItemWidth(int(itemWidth))
+				self.content.setItemHeight(int(itemHeight))
+				if selectionEnabled is not None:
+					self.selectionEnabled = selectionEnabled
+				if scrollbarMode is not None:
+					self.scrollbarMode = scrollbarMode
+				self.activeStyle = style
+
 		if not self.content:
-			from enigma import eListboxPythonMultiContent
 			self.content = eListboxPythonMultiContent()
 			for index, font in enumerate(self.template["fonts"]):  # Setup fonts (also given by source).
 				self.content.setFont(index, font)
-		if what[0] == self.CHANGED_SPECIFIC and what[1] == "style":  # If only template changed, don't reload list.
-			if self.downstream_elements:
+		if what[0] == self.CHANGED_SPECIFIC and what[1] == "style" and self.scale is not None:  # If only template changed, don't reload list.
+			pass
+		elif self.source:
+			if self.scale is None and self.downstream_elements:
 				listBoxRenderer = self.downstream_elements[0]
 				if str(listBoxRenderer.__class__.__name__) == "Listbox":
 					if hasattr(listBoxRenderer, "scale"):
 						scale = listBoxRenderer.scale
 						if scale and (scale[0][0] != scale[0][1] or scale[1][0] != scale[1][1]):
 							self.scale = scale
-							self.active_style = None
-			pass
-		elif self.source:
+							self.activeStyle = None
 			try:
-				tmp = []
-				src = self.source.list
-				for x in range(len(src)):
-					if not isinstance(src[x], (list, tuple)):
-						tmp.append((src[x],))
+				contentList = []
+				sourceList = self.source.list
+				for item in range(len(sourceList)):
+					if not isinstance(sourceList[item], (list, tuple)):
+						contentList.append((sourceList[item],))
 					else:
-						tmp.append(src[x])
+						contentList.append(sourceList[item])
 			except Exception as error:
-				print("[TemplatedMultiContent] Error: %s." % error)
-				tmp = self.source.list
-			self.content.setList(tmp)
-		self.setTemplate()
+				print(f"[TemplatedMultiContent] Error: {error}.")
+				contentList = self.source.list
+			self.content.setList(contentList)
+		setTemplate()
 		self.downstream_elements.changed(what)
-
-	def scaleTemplate(self, template, itemWidth, itemHeight):
-		from enigma import gFont
-		print("[TemplatedMultiContent] DEBUG: Scale template.")
-		scaleFactorVertical = self.scale[1][0] / self.scale[1][1]
-		scaleFactorHorizontal = self.scale[0][0] / self.scale[0][1]
-		itemWidth = int(itemWidth * scaleFactorHorizontal)
-		itemHeight = int(itemHeight * scaleFactorVertical)
-		scaledTemplate = []
-		fonts = []
-		for font in self.template["fonts"]:
-			fonts.append(gFont(font.family, int(font.pointSize * scaleFactorVertical)))
-		for content in template:
-			elements = list(content)
-			elements[1] = int(elements[1] * scaleFactorVertical)
-			elements[2] = int(elements[2] * scaleFactorHorizontal)
-			elements[3] = int(elements[3] * scaleFactorVertical)
-			elements[4] = int(elements[4] * scaleFactorHorizontal)
-			scaledTemplate.append(tuple(elements))
-		return scaledTemplate, itemHeight, itemWidth, fonts
-
-	def setTemplate(self):
-		if self.source:
-			style = self.source.style
-			if style == self.active_style:
-				return
-			templates = self.template.get("templates")  # If skin defined "templates", that means that it defines multiple styles in a dictionary but template should still be a default.
-			template = self.template.get("template")
-			if "itemHeight" in self.template:
-				itemHeight = self.template["itemHeight"]
-				itemWidth = itemHeight
-			if "itemWidth" in self.template:
-				itemWidth = self.template["itemWidth"]
-			if "itemSize" in self.template:
-				itemWidth = self.template["itemSize"]
-				itemHeight = itemWidth
-			selectionEnabled = self.template.get("selectionEnabled", None)
-			scrollbarMode = self.template.get("scrollbarMode", None)
-			if templates and style and style in templates:  # If we have a custom style defined in the source, and different templates in the skin, look the template up.
-				template = templates[style][1]
-				if isinstance(templates[style][0], tuple):
-					itemWidth = templates[style][0][0]
-					itemHeight = templates[style][0][1]
-				else:
-					itemWidth = templates[style][0]
-					itemHeight = itemWidth
-				if len(templates[style]) > 2:
-					selectionEnabled = templates[style][2]
-				if len(templates[style]) > 3:
-					scrollbarMode = templates[style][3]
-			if self.scale:
-				template, itemWidth, itemHeight, fonts = self.scaleTemplate(template, itemWidth, itemHeight)
-				for index, font in enumerate(fonts):
-					self.content.setFont(index, font)
-			self.content.setTemplate(template)
-			self.content.setItemWidth(int(itemWidth))
-			self.content.setItemHeight(int(itemHeight))
-			if selectionEnabled is not None:
-				self.selectionEnabled = selectionEnabled
-			if scrollbarMode is not None:
-				self.scrollbarMode = scrollbarMode
-			self.active_style = style
