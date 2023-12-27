5a43345292922c03b24429463136b936f3286c0c
IanSav <IanSav@users.noreply.github.com>
2023-12-27 23:22:54 +1100

    [SystemInfo.py] Small code optimization
    Remove an unused condition in "processValue()".

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -76,9 +76,7 @@ class BoxInformation:  # To maintain data integrity class variables should not b
 
 	def processValue(self, value):
 		valueTest = value.upper() if value else ""
-		if value is None:
-			pass
-		elif (value.startswith("\"") or value.startswith("'")) and value.endswith(value[0]):
+		if (value.startswith("\"") or value.startswith("'")) and value.endswith(value[0]):
 			value = value[1:-1]
 		elif value.startswith("(") and value.endswith(")"):
 			data = []
