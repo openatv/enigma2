436f4a5ebfd205d3b96c4592ec776039671039b4
GhostofGeeeee <130693485+GhostofGeeeee@users.noreply.github.com>
2024-05-05 14:45:12 +0000

    [ChannelSelection] fix green button text text wasn't reset

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -2754,6 +2754,7 @@ class ChannelSelection(ChannelSelectionBase, ChannelSelectionEdit, ChannelSelect
 		self.correctChannelNumber()
 		self.editMode = False
 		self.protectContextMenu = True
+		self["key_green"].setText(_("Reception Lists"))
 		self.close(None)
 
 	def zapBack(self):
