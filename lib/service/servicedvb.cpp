2469ffef0523999457c1e310d4836529a72dda28
jbleyel <jbleyel@gmx.net>
2024-10-04 16:27:16 +0200

    Cherry pick -> https://github.com/OpenViX/enigma2/commit/ef04c891a07de3bb36273ed1161e3053b3540b8a https://github.com/OpenViX/enigma2/commit/2e040db07e6ec3008ada9520b3be2db3235fa43e

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1526,7 +1526,8 @@ RESULT eDVBServicePlay::pause(ePtr<iPauseableService> &ptr)
 		/* note: we check for time shift to be enabled,
 		   not neccessary active. if you pause when timeshift
 		   is not active, you should activate it when unpausing */
-	if ((!m_is_pvr) && (!m_timeshift_enabled))
+
+	if (!m_is_pvr && !m_timeshift_enabled && (m_reference.path.empty() || !m_reference.alternativeurl.empty()))
 	{
 		ptr = nullptr;
 		return -1;
@@ -1631,7 +1632,8 @@ RESULT eDVBServicePlay::setFastForward_internal(int ratio, bool final_seek)
 
 RESULT eDVBServicePlay::seek(ePtr<iSeekableService> &ptr)
 {
-	if (m_is_pvr || m_timeshift_enabled)
+
+	if (m_is_pvr || m_timeshift_enabled || (!m_reference.path.empty() && m_reference.alternativeurl.empty()))
 	{
 		ptr = this;
 		return 0;
