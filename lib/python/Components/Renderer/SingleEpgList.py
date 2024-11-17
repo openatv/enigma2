f57f68b926596d1dafa8c7a0d432c6afeb58b945
jbleyel <jbleyel@gmx.net>
2024-11-17 12:47:23 +0100

    [SingleEpgList] * add maxEvents attribute * tiny refactor
    (cherry picked from commit 746159704a32c586e13f72a2043623efa8651b92)

--- lib/python/Components/Renderer/SingleEpgList.py
+++ lib/python/Components/Renderer/SingleEpgList.py
@@ -8,36 +8,39 @@ class SingleEpgList(Renderer, VariableText):
 	GUI_WIDGET = eLabel
 
 	def __init__(self):
+		self.maxEvents = 4
 		Renderer.__init__(self)
 		VariableText.__init__(self)
 		self.epgcache = eEPGCache.getInstance()
 
+	def applySkin(self, desktop, parent):
+		attribs = self.skinAttributes[:]
+		for (attrib, value) in self.skinAttributes:
+			if attrib == "maxEvents":
+				self.maxEvents = int(value)
+				attribs.remove((attrib, value))
+		self.skinAttributes = attribs
+		return Renderer.applySkin(self, desktop, parent)
+
 	def changed(self, what):
 		event = self.source.event
+		text = []
+		if event:
+			service = self.source.service
+			if self.epgcache:
+				events = self.epgcache.lookupEvent(["IBDCT", (service.toString(), 0, -1, -1)])
+				if events:
+					eventCount = 0
+					for event in events:
+						if eventCount > 0:
+							if event[4]:
+								localTime = localtime(event[1])
+								text.append(f"{localTime[3]:02d}:{localTime[4]:02d} {event[4]}")
+							else:
+								text.append(_("N/A)"))
 
-		if event is None:
-			self.text = ""
-			return
+						eventCount += 1
+						if eventCount > self.maxEvents:
+							break
 
-		service = self.source.service
-		text = ""
-		evt = None
-
-		if self.epgcache is not None:
-			evt = self.epgcache.lookupEvent(["IBDCT", (service.toString(), 0, -1, -1)])
-
-		if evt:
-			maxx = 0
-			for x in evt:
-				if maxx > 0:
-					if x[4]:
-						t = localtime(x[1])
-						text = text + "%02d:%02d %s\n" % (t[3], t[4], x[4])
-					else:
-						text = text + "n/a\n"
-
-				maxx += 1
-				if maxx > 4:
-					break
-
-		self.text = text
+		self.text = "\n".join(text)
