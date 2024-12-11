fd17bd45d79d4f7fa3d33a261e0100fb274a103e
jbleyel <jbleyel@gmx.net>
2024-12-11 12:02:20 +0100

    fix hardwareDB

--- include/hardwaredb.h
+++ include/hardwaredb.h
@@ -134,40 +134,40 @@ static std::unordered_map<std::string, std::string> HardwareDB{
 	{"/devices/pci0000:01/0000:01:00.0/host0/target0:0:0/0:0:0:0", "eSATA"}
 #elif HWVUULTIMO4K
 	{"/devices/platform/brcmstb-ahci.0/ata1/host0/target0:0:0/0:0:0:0", "SATA"},
-	{"/devices/rdb.3/f0470300.ehci_v2/usb3/3-1/3-1:1.0/host5/target5:0:0/5:0:0:0", "Back, lower USB"},
-	{"/devices/rdb.3/f0470500.ehci_v2/usb4/4-1/4-1:1.0/host2/target2:0:0/2:0:0:0", "Back, upper USB"},
-	{"/devices/rdb.3/f0480600.ohci_v2/usb10/10-1/10-1:1.0/host3/target3:0:0/3:0:0:0", "Front USB"}
+	{"/devices/rdb.3/f0470300.ehci_v2/usb3/", "Back, lower USB"},
+	{"/devices/rdb.3/f0470500.ehci_v2/usb4/", "Back, upper USB"},
+	{"/devices/rdb.3/f0480600.ohci_v2/usb10/", "Front USB"}
 #elif HWVUSOLO4K
 	{"/devices/platform/strict-ahci.0/ata1/host0/target0:0:0/0:0:0:0", "SATA"},
-	{"/devices/f0490600.ohci/usb10/10-1/10-1:1.0/host2/target2:0:0/2:0:0:0", "Front USB"},
-	{"/devices/f0480400.ohci/usb7/7-1/7-1:1.0/host3/target3:0:0/3:0:0:0", "Back, lower USB"},
-	{"/devices/f0480600.ohci/usb8/8-1/8-1:1.0/host4/target4:0:0/4:0:0:0", "Back, upper USB"}
+	{"/devices/f0490600.ohci/usb10/", "Front USB"},
+	{"/devices/f0480400.ohci/usb7/", "Back, lower USB"},
+	{"/devices/f0480600.ohci/usb8/", "Back, upper USB"}
 #elif HWH7
 	{"/devices/platform/rdb/f045a000.sata/ata1/host0/target0:0:0/0:0:0:0", "SATA"},
-	{"/devices/platform/f0470500.ehci/usb2/2-1/2-1:1.0/host2/target2:0:0/2:0:0:0", "Back USB 3.0"},
-	{"/devices/platform/f0470300.ehci/usb1/1-1/1-1.2/1-1.2:1.0/host3/target3:0:0/3:0:0:0", "Back, lower USB 2.0"},
-	{"/devices/platform/f0470300.ehci/usb1/1-1/1-1.1/1-1.1:1.0/host2/target2:0:0/2:0:0:0", "Back, upper USB 2.0"}
+	{"/devices/platform/f0470500.ehci/usb2/", "Back USB 3.0"},
+	{"/devices/platform/f0470300.ehci/usb1/1-1/1-1.2/", "Back, lower USB 2.0"},
+	{"/devices/platform/f0470300.ehci/usb1/1-1/1-1.1/", "Back, upper USB 2.0"}
 #elif HWH17
 	{"/devices/platform/f0470300.ehci/usb1/", "Front USB"},
-	{"/devices/platform/f0470500.ehci/usb2/", "Back USB"}
+	{"/devices/platform/f0471000.xhci/usb6/", "Back USB"}
 #elif HWGBUE4K
 	{"/devices/platform/rdb/f045a000.sata/ata2/host1/target1:0:0/1:0:0:0", "SATA"},
-	{"/devices/platform/rdb/f0470300.ehci_v2/usb3/3-1/3-1.1/3-1.1:1.0/host2/target2:0:0/2:0:0:0", "Front USB"},
-	{"/devices/platform/rdb/f0470500.ehci_v2/usb4/4-1/4-1:1.0/host3/target3:0:0/3:0:0:0", "Back USB 3.0"},
-	{"/devices/platform/rdb/f0470300.ehci_v2/usb3/3-1/3-1.2/3-1.2:1.0/host4/target4:0:0/4:0:0:0", "Back, upper USB 2.0"},
-	{"/devices/platform/rdb/f0470300.ehci_v2/usb3/3-1/3-1.3/3-1.3:1.0/host5/target5:0:0/5:0:0:0", "Back, lower USB 2.0"}
+	{"/devices/platform/rdb/f0470300.ehci_v2/usb3/3-1/3-1.1/3-1.1:", "Front USB"},
+	{"/devices/platform/rdb/f0470500.ehci_v2/usb4/", "Back USB 3.0"},
+	{"/devices/platform/rdb/f0470300.ehci_v2/usb3/3-1/3-1.2/3-1.2:", "Back, upper USB 2.0"},
+	{"/devices/platform/rdb/f0470300.ehci_v2/usb3/3-1/3-1.3/3-1.3:", "Back, lower USB 2.0"}
 #elif HWPULSE4K
 	{"/devices/platform/soc/f9900000.hiahci/ata1/host0/target0:0:0/0:0:0:0", "SATA"},
-	{"/devices/platform/soc/f98a0000.xhci/usb3/3-1/3-1:1.0/host1/target1:0:0/1:0:0:0", "Front USB"},
-	{"/devices/platform/soc/f9890000.ehci/usb1/1-1/1-1:1.0/host1/target1:0:0/1:0:0:0", "Back USB"}
+	{"/devices/platform/soc/f98a0000.xhci/usb3/", "Front USB"},
+	{"/devices/platform/soc/f9890000.ehci/usb1/", "Back USB"}
 #elif HWPULSE4KMINI
 	{"/devices/platform/soc/f9820000.himciv200.SD/mmc_host/mmc1/mmc1:59b4", "Micro SD"},
-	{"/devices/platform/soc/f9890000.ehci/usb1/1-1/1-1:1.0/host1/target1:0:0/1:0:0:0", "Back, upper USB 2.0"},
-	{"/devices/platform/soc/f98a0000.xhci/usb4/4-1/4-1:1.0/host0/target0:0:0/0:0:0:0", "Back, lower USB 3.0"}
+	{"/devices/platform/soc/f9890000.ehci/usb1/", "Back, upper USB 2.0"},
+	{"/devices/platform/soc/f98a0000.xhci/usb4/", "Back, lower USB 3.0"}
 #elif HWGBTRIO4KPRO
-	{"/devices/platform/soc/f9890000.ehci/usb1/1-2/1-2:1.0/host0/target0:0:0/0:0:0:0", "Micro SD"},
-	{"/devices/platform/soc/f9880000.ohci/usb2/2-1/2-1:1.0/host1/target1:0:0/1:0:0:0", "Back USB 2.0"},
-	{"/devices/platform/soc/f98a0000.xhci/usb3/3-1/3-1:1.0/host2/target2:0:0/2:0:0:0", "Back USB 3.0"}
+	{"/devices/platform/soc/f9890000.ehci/usb1/", "Micro SD"},
+	{"/devices/platform/soc/f9880000.ohci/usb2/", "Back USB 2.0"},
+	{"/devices/platform/soc/f98a0000.xhci/usb3/", "Back USB 3.0"}
 #else
 #endif
 
