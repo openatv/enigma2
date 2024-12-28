03e93c465e011ecc0ece32d06138aa60143ae9ad
jbleyel <jbleyel@gmx.net>
2024-12-28 23:02:12 +0100

    add new ModalMessageBox

--- lib/python/StartEnigma.py
+++ lib/python/StartEnigma.py
@@ -434,6 +434,8 @@ def runScreenTest():
 	vol = VolumeControl(session)
 	enigma.eProfileWrite("Processing Screen")
 	processing = Processing(session)
+	enigma.eProfileWrite("Global MessageBox Screen")
+	modalmessagebox = ModalMessageBox(session)
 	enigma.eProfileWrite("PowerKey")
 	power = PowerKey(session)
 	if enigma.getVFDSymbolsPoll():
@@ -845,6 +847,9 @@ from Components.VolumeControl import VolumeControl
 
 enigma.eProfileWrite("Processing")
 from Screens.Processing import Processing
+
+enigma.eProfileWrite("ModalMessageBox")
+from Screens.MessageBox import ModalMessageBox
 
 enigma.eProfileWrite("StackTracePrinter")
 from Components.StackTrace import StackTracePrinter
