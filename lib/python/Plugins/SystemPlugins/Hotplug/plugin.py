0b6aab1be22763c119d1878d447ee724b200e814
jbleyel <jbleyel@gmx.net>
2025-01-08 12:13:32 +0100

    [Hotplug] * use config.crash.debugStorage for debug output

--- lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
+++ lib/python/Plugins/SystemPlugins/Hotplug/plugin.py
@@ -5,6 +5,7 @@ from twisted.internet.protocol import Factory, Protocol
 
 from enigma import getDeviceDB, eTimer
 
+from Components.config import config
 from Components.Console import Console
 from Components.Harddisk import harddiskmanager
 from Components.Storage import EXPANDER_MOUNT, cleanMediaDirs
@@ -78,6 +79,11 @@ class HotPlugManager:
 		self.deviceData = []
 		self.addedDevice = []
 		self.callMount = False
+		self.debug = False
+
+		def debugStorageChanged(configElement):
+			self.debug = configElement.value
+		config.crash.debugStorage.addNotifier(debugStorageChanged)
 
 	def processAddDevice(self):
 		self.addTimer.stop()
@@ -119,7 +125,8 @@ class HotPlugManager:
 				for device in knownDevices:
 					deviceData = device.split(":")
 					if len(deviceData) == 2 and deviceData[0] == ID_FS_UUID:
-						print("[Hotplug] UUID found in known_devices")
+						if self.debug:
+							print("[Hotplug] UUID found in known_devices")
 						knownDevice = deviceData[1]
 						notFound = knownDevice != "None"  # Ignore this device
 						break
@@ -225,7 +232,8 @@ class HotPlugManager:
 
 	def processHotplugData(self, eventData):
 		mode = eventData.get("mode")
-		print("[Hotplug] DEBUG: ", eventData)
+		if self.debug:
+			print("[Hotplug] DEBUG: ", eventData)
 		action = eventData.get("ACTION")
 		if mode == 1:
 			if action == "add":
