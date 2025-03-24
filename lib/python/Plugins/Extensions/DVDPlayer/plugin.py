c0e4f77fb31ac67f08ba08033a063314e81bd1a0
jbleyel <jbleyel@gmx.net>
2025-03-24 10:51:51 +0100

    [DVDPlayer] * refactor imports * comment out some debugs

--- lib/python/Plugins/Extensions/DVDPlayer/plugin.py
+++ lib/python/Plugins/Extensions/DVDPlayer/plugin.py
@@ -1,45 +1,41 @@
-import os
-from Tools.Directories import fileExists
-from Plugins.Plugin import PluginDescriptor
+from os.path import exists, join
+
 from Components.Harddisk import harddiskmanager
+from Components.Scanner import Scanner, ScanPath
+from Plugins.Plugin import PluginDescriptor
+from Screens.DVD import DVDPlayer as _DVDPlayer, DVDOverlay as _DVDOverlay
+from Tools.Directories import fileExists
 
 detected_DVD = None
 
 
 def main(session, **kwargs):
-	from Screens import DVD
-	session.open(DVD.DVDPlayer)
+	session.open(_DVDPlayer)
 
 
 def play(session, **kwargs):
-	from Screens import DVD
-	if (os.path.exists(os.path.join(harddiskmanager.getAutofsMountpoint(harddiskmanager.getCD()), "VIDEO_TS"))
-			or os.path.exists(os.path.join(harddiskmanager.getAutofsMountpoint(harddiskmanager.getCD()), "video_ts"))):
-		session.open(DVD.DVDPlayer, dvd_device=harddiskmanager.getAutofsMountpoint(harddiskmanager.getCD()))
+	if (exists(join(harddiskmanager.getAutofsMountpoint(harddiskmanager.getCD()), "VIDEO_TS"))
+			or exists(join(harddiskmanager.getAutofsMountpoint(harddiskmanager.getCD()), "video_ts"))):
+		session.open(DVDPlayer, dvd_device=harddiskmanager.getAutofsMountpoint(harddiskmanager.getCD()))
 	else:
 		return
 
 
-def DVDPlayer(*args, **kwargs):
-	# for backward compatibility with plugins that do "from DVDPlayer.plugin import DVDPlayer"
-	from Screens import DVD
-	return DVD.DVDPlayer(*args, **kwargs)
+def DVDPlayer(*args, **kwargs):  # for backward compatibility with plugins that do "from DVDPlayer.plugin import DVDPlayer"
+	return _DVDPlayer(*args, **kwargs)
 
 
-def DVDOverlay(*args, **kwargs):
-	# for backward compatibility with plugins that do "from DVDPlayer.plugin import DVDOverlay"
-	from Screens import DVD
-	return DVD.DVDOverlay(*args, **kwargs)
+def DVDOverlay(*args, **kwargs):  # for backward compatibility with plugins that do "from DVDPlayer.plugin import DVDOverlay"
+	return _DVDOverlay(*args, **kwargs)
 
 
 def filescan_open(list, session, **kwargs):
-	from Screens import DVD
 	if len(list) == 1 and list[0].mimetype == "video/x-dvd":
 		cd = harddiskmanager.getCD()
-		if cd and (os.path.exists(os.path.join(harddiskmanager.getAutofsMountpoint(cd), "VIDEO_TS"))
-				or os.path.exists(os.path.join(harddiskmanager.getAutofsMountpoint(cd), "video_ts"))):
+		if cd and (exists(join(harddiskmanager.getAutofsMountpoint(cd), "VIDEO_TS"))
+				or exists(join(harddiskmanager.getAutofsMountpoint(cd), "video_ts"))):
 			print("[DVDplayer] found device /dev/%s", " mount path ", harddiskmanager.getAutofsMountpoint(cd))
-			session.open(DVD.DVDPlayer, dvd_device="/dev/%s" % (harddiskmanager.getAutofsMountpoint(cd)))
+			session.open(DVDPlayer, dvd_device="/dev/%s" % (harddiskmanager.getAutofsMountpoint(cd)))
 			return
 	else:
 		dvd_filelist = []
@@ -48,11 +44,10 @@ def filescan_open(list, session, **kwargs):
 				dvd_filelist.append(x.path)
 			if x.mimetype == "video/x-dvd":
 				dvd_filelist.append(x.path.rsplit('/', 1)[0])
-		session.open(DVD.DVDPlayer, dvd_filelist=dvd_filelist)
+		session.open(DVDPlayer, dvd_filelist=dvd_filelist)
 
 
 def filescan(**kwargs):
-	from Components.Scanner import Scanner, ScanPath
 
 	# Overwrite checkFile to only detect local
 	class LocalScanner(Scanner):
@@ -73,14 +68,14 @@ def filescan(**kwargs):
 
 
 def onPartitionChange(action, partition):
-	print("[@] onPartitionChange", action, partition)
+	# print("[@] onPartitionChange", action, partition)
 	if partition != harddiskmanager.getCD():
 		global detected_DVD
 		if action == 'remove':
-			print("[DVDplayer] DVD removed")
+			# print("[DVDplayer] DVD removed")
 			detected_DVD = False
 		elif action == 'add':
-			print("[DVDplayer] DVD Inserted")
+			# print("[DVDplayer] DVD Inserted")
 			detected_DVD = None
 
 
@@ -89,8 +84,8 @@ def menu(menuid, **kwargs):
 		global detected_DVD
 		if detected_DVD is None or detected_DVD:
 			cd = harddiskmanager.getCD()
-			if cd and (os.path.exists(os.path.join(harddiskmanager.getAutofsMountpoint(cd), "VIDEO_TS"))
-					or os.path.exists(os.path.join(harddiskmanager.getAutofsMountpoint(cd), "video_ts"))):
+			if cd and (exists(join(harddiskmanager.getAutofsMountpoint(cd), "VIDEO_TS"))
+					or exists(join(harddiskmanager.getAutofsMountpoint(cd), "video_ts"))):
 				print("[DVDplayer] Mountpoint is present and is", harddiskmanager.getAutofsMountpoint(cd))
 				detected_DVD = True
 			else:
