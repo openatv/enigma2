504b50008e29dd288de2f62e5bdb1d50f7441c71
jbleyel <jbleyel@gmx.net>
2025-05-30 11:24:17 +0200

    [NumericalTextInputHelpDialog] * remove six

--- lib/python/Screens/NumericalTextInputHelpDialog.py
+++ lib/python/Screens/NumericalTextInputHelpDialog.py
@@ -1,6 +1,5 @@
 from Screens.Screen import Screen
 from Components.Label import Label
-import six
 
 
 class NumericalTextInputHelpDialog(Screen):
@@ -8,8 +7,14 @@ class NumericalTextInputHelpDialog(Screen):
 		Screen.__init__(self, session)
 		self["help1"] = Label(text="<")
 		self["help2"] = Label(text=">")
+
+		def ensure_str(result):
+			if isinstance(result, bytes):
+				result = result.decode(encoding='utf-8', errors='strict')
+			return result
+
 		for x in (1, 2, 3, 4, 5, 6, 7, 8, 9, 0):
-			self["key%d" % x] = Label(text=six.ensure_str(textinput.mapping[x]))
+			self["key%d" % x] = Label(text=ensure_str(textinput.mapping[x]))
 		self.last_marked = 0
 
 	def update(self, textinput):
