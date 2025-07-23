c99af1f15a1ad8ef2b259a35ab0d345c2c343021
jbleyel <jbleyel@gmx.net>
2025-07-23 16:09:56 +0200

    Text updates

--- lib/python/Screens/ButtonSetup.py
+++ lib/python/Screens/ButtonSetup.py
@@ -239,7 +239,7 @@ def getButtonSetupFunctions():
 	ButtonSetupFunctions.append((_("Show subtitle selection"), "Infobar/subtitleSelection", textInfoBar))
 	ButtonSetupFunctions.append((_("Show subtitle quick menu"), "Infobar/subtitleQuickMenu", textInfoBar))
 	ButtonSetupFunctions.append((_("LetterBox zoom"), "Infobar/vmodeSelection", textInfoBar))
-	ButtonSetupFunctions.append((_("Seekbar"), "Infobar/seekFwdVod", textInfoBar))
+	ButtonSetupFunctions.append((_("SeekBar"), "Infobar/seekFwdVod", textInfoBar))
 	if BoxInfo.getItem("PIPAvailable"):
 		ButtonSetupFunctions.append((_("Show PiP"), "Infobar/showPiP", textInfoBar))
 		ButtonSetupFunctions.append((_("Swap PiP"), "Infobar/swapPiP", textInfoBar))
