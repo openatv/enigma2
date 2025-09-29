7108070c5b803c953a86f6ac6c3240c4913c0495
jbleyel <jbleyel@gmx.net>
2025-09-29 12:14:35 +0200

    Fix license header because CC BY-NC-SA 4.0 cannot mix with GPL

--- lib/python/Components/ServiceList.py
+++ lib/python/Components/ServiceList.py
@@ -1,32 +1,7 @@
-#Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License
-#
-#Copyright (c) 2024-2025 jbleyel
+# Copyright (c) 2024-2025 jbleyel
 
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
-#1. Non-Commercial Use: You may not use the Software or any derivative works
-#   for commercial purposes without obtaining explicit permission from the
-#   copyright holder.
-#2. Share Alike: If you distribute or publicly perform the Software or any
-#   derivative works, you must do so under the same license terms, and you
-#   must make the source code of any derivative works available to the
-#   public.
-#3. Attribution: You must give appropriate credit to the original author(s)
-#   of the Software by including a prominent notice in your derivative works.
-#THE SOFTWARE IS PROVIDED "AS IS," WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE, AND NONINFRINGEMENT. IN NO EVENT SHALL
-#THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES, OR
-#OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT, OR OTHERWISE,
-#ARISING FROM, OUT OF, OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-#OTHER DEALINGS IN THE SOFTWARE.
-#
-#For more details about the CC BY-NC-SA 4.0 License, please visit:
-#https://creativecommons.org/licenses/by-nc-sa/4.0/
+# This code may be used commercially. Attribution must be given to the original author.
+# Licensed under GPLv2.
 
 # This file also contains the previous code of ServiceList ( ServiceListLegacy ) based on multiple authors.
 
@@ -816,7 +791,7 @@ class ServiceListLegacy(ServiceListBase):
 		numberHeight = self.ItemHeight // 2 if twoLines and config.usage.servicelist_servicenumber_valign.value == "1" else self.ItemHeight
 		self.l.setElementPosition(self.l.celServiceNumber, eRect(self.listMarginLeft, 0, channelNumberWidth, numberHeight))
 
-		#progress view modes for two lines
+		# progress view modes for two lines
 		#  0 - single, centered
 		# 10 - single, upper line
 		#  1 - dual, bar upper line, value lower line
@@ -1164,7 +1139,7 @@ class ServiceList(ServiceListBase, ServiceListTemplateParser):
 	def resetReloadTimer(self):
 		self.reloadTimer.stop()
 		# TODO enable this if code is finshed
-		#self.reloadTimer.startLongTimer(60 - datetime.now().second)
+		# self.reloadTimer.startLongTimer(60 - datetime.now().second)
 
 	def postWidgetCreate(self, instance):
 		instance.setContent(self.l)
