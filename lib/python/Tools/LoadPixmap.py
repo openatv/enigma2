f3376f65e5bef076b19d074240715a1f6a9f022b
jbleyel <jbleyel@gmx.net>
2025-09-30 10:45:44 +0200

    [LoadPixmap] * add scaletoFit and align for SVG

--- lib/python/Tools/LoadPixmap.py
+++ lib/python/Tools/LoadPixmap.py
@@ -1,10 +1,10 @@
-from enigma import loadPNG, loadJPG, loadSVG, loadGIF
+from enigma import loadPNG, loadJPG, loadSVG, loadGIF, RT_HALIGN_CENTER
 
 
 # If cached is not supplied, LoadPixmap defaults to caching PNGs and not caching JPGs
 # Split alpha channel JPGs are never cached as the C++ layer's caching is based on
 # a single file per image in the cache
-def LoadPixmap(path, desktop=None, cached=None, width=0, height=0):
+def LoadPixmap(path, desktop=None, cached=None, width=0, height=0, scaletoFit=0, align=RT_HALIGN_CENTER):
 	if path[-4:] == ".png":
 		# cache unless caller explicity requests to not cache
 		ptr = loadPNG(path, 0, 0 if cached is False else 1)
@@ -15,11 +15,10 @@ def LoadPixmap(path, desktop=None, cached=None, width=0, height=0):
 		# don't cache unless caller explicity requests caching
 		ptr = loadGIF(path, 1 if cached is True else 0)
 	elif path[-4:] == ".svg":
-		from skin import getSkinFactor  # imported here to avoid circular import
-		# autoscale = int(parameters.get("AutoscaleSVG", -1)) # skin_default only == -1, disabled == 0 or enabled == 1
-		autoscale = -1
+		from skin import parameters, getSkinFactor  # imported here to avoid circular import
+		autoscale = int(parameters.get("AutoscaleSVG", -1))  # skin_default only == -1, disabled == 0 or enabled == 1
 		scale = height == 0 and (autoscale == -1 and "/skin_default/" in path or autoscale == 1) and getSkinFactor() or 0
-		ptr = loadSVG(path, 0 if cached is False else 1, width, height, scale)
+		ptr = loadSVG(path, 0 if cached is False else 1, width, height, scale, scaletoFit, align)
 	elif path[-1:] == ".":
 		# caching mechanism isn't suitable for multi file images, so it's explicitly disabled
 		alpha = loadPNG(path + "a.png", 0, 0)
