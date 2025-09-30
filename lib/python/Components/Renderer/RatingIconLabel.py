7e2c2c82ac1e699d833390df1c73b9c2f55d5478
jbleyel <jbleyel@gmx.net>
2025-09-30 11:41:17 +0200

    [RatingIconLabel] * fix copy paste error * small improvements

--- lib/python/Components/Renderer/RatingIconLabel.py
+++ lib/python/Components/Renderer/RatingIconLabel.py
@@ -1,3 +1,7 @@
+# This Renderer is used in E2-DarkOS-skin and it's temporary.
+# The Renderer can changed or removed.
+# We need to find a better solution for the ratings.
+
 from Components.Renderer.Renderer import Renderer
 from enigma import eLabel, gRGB
 from skin import parseColor
@@ -14,26 +18,30 @@ class RatingIconLabel(Renderer):
 		self.changed((self.CHANGED_DEFAULT,))
 
 	def applySkin(self, desktop, parent):
-		attribs = self.skinAttributes[:]
+		attribs = []
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "colors":
 				self.colors = {int(k): parseColor(v) for k, v in (item.split(":") for item in value.split(","))}
+			else:
+				attribs.append((attrib, value))
 		self.skinAttributes = attribs
 		rc = Renderer.applySkin(self, desktop, parent)
 		self.changed((self.CHANGED_DEFAULT,))
 		return rc
 
+	def hideLabel(self):
+		self.instance.setText("")
+		self.instance.hide()
+
 	def changed(self, what):
 		if self.source and hasattr(self.source, "text") and self.instance:
 			if what[0] == self.CHANGED_CLEAR:
-				self.instance.setText("")
-				self.instance.setPixmap(None)
+				self.hideLabel()
 			else:
 				if self.source.text:
 					age = int(self.source.text.replace("+", ""))
 					if age == 0:
-						self.instance.setText("")
-						self.instance.hide()
+						self.hideLabel()
 						return
 					if age <= 15:
 						age += 3
@@ -42,5 +50,4 @@ class RatingIconLabel(Renderer):
 					self.instance.setBackgroundColor(gRGB(self.colors.get(age, 0x10000000)))
 					self.instance.show()
 				else:
-					self.instance.setText("")
-					self.instance.hide()
+					self.hideLabel()
