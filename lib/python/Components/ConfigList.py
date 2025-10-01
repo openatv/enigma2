42d3a2bcdc65a86064377145efc1bcc88af767d3
jbleyel <jbleyel@gmx.net>
2025-10-01 11:01:53 +0200

    [ConfigList] * revert applySkin

--- lib/python/Components/ConfigList.py
+++ lib/python/Components/ConfigList.py
@@ -172,20 +172,6 @@ class ConfigList(GUIComponent):
 	def moveBottom(self):
 		self.goBottom()
 
-	def applySkin(self, desktop, screen):
-		if self.skinAttributes is not None:
-			attribs = []
-			for (attrib, value) in self.skinAttributes:
-				if attrib == "sepLineColor":
-					attribs.append(("separatorColor", value))
-				elif attrib == "sepLineThickness":
-					pass
-					# self.sepLineThickness = int(value) # This is currently not working
-				else:
-					attribs.append((attrib, value))
-			self.skinAttributes = attribs
-		return GUIComponent.applySkin(self, desktop, screen)
-
 
 class ConfigListScreen:
 	def __init__(self, list, session=None, on_change=None, fullUI=False, allowDefault=False):
