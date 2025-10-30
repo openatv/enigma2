5423e93bce3e5b2c46a1d18d30ab8aff8340b9d0
jbleyel <jbleyel@gmx.net>
2025-10-30 16:17:17 +0100

    [gPixmap] * fix hisi scale issues

--- lib/gdi/gpixmap.cpp
+++ lib/gdi/gpixmap.cpp
@@ -2108,14 +2108,8 @@ void gPixmap::blit(const gPixmap& src, const eRect& _pos, const gRegion& clip, i
 #endif
 
 #ifdef FORCE_NO_ACCELERATION_SCALE
-		if (accel && (flag & blitScale)) {
-			// Reset width in case of round issue
-			if (src.size().width() != srcarea.width())
-				srcarea.setWidth(src.size().width());
-
-			// Reset height in case of round issue
-			if (src.size().height() != srcarea.height())
-				srcarea.setHeight(src.size().height());
+		if (accel && (flag & blitScale) && (src.size().width() != srcarea.width() || src.size().height() != srcarea.height())) {
+			accel = false;
 		}
 #endif
 		if (accel) {
