08d55fff3afbe6dfb6db161a872b556b77d21249
jbleyel <jbleyel@gmx.net>
2025-12-12 21:34:04 +0100

    [Screensaver] * add option to choose picon, blank screen and logo for screensaver

--- lib/python/Screens/ScreenSaver.py
+++ lib/python/Screens/ScreenSaver.py
@@ -1,12 +1,13 @@
+from os.path import exists, splitext
 from random import randrange
-from os.path import splitext
 
-from enigma import ePoint, eTimer, iPlayableService
+from enigma import ePoint, eServiceReference, eTimer, iPlayableService, iPlayableServicePtr, iServiceInformation
 
 from Components.config import config
 from Components.MovieList import AUDIO_EXTENSIONS
 from Components.Pixmap import Pixmap
 from Components.ServiceEventTracker import ServiceEventTracker
+from Components.Renderer.Picon import getPiconName
 from Screens.Screen import Screen
 
 
@@ -15,34 +16,49 @@ class ScreenSaver(Screen):
 		Screen.__init__(self, session)
 		self.skinName = ["ScreenSaver", "Screensaver"]
 		self["picture"] = Pixmap()
-		self.moveLogoTimer = eTimer()
-		self.moveLogoTimer.callback.append(self.movePicture)
+		self.moveTimer = eTimer()
+		self.moveTimer.callback.append(self.movePicture)
 		self.__event_tracker = ServiceEventTracker(screen=self, eventmap={
 			iPlayableService.evStart: self.serviceStarted
 		})
 		self.onShow.append(self.showScreenSaver)
 		self.onHide.append(self.hideScreenSaver)
-		self.onLayoutFinish.append(self.layoutFinished)
 
 	def showScreenSaver(self):
-		self.moveTimer = config.usage.screenSaverMoveTimer.value
-		self.moveLogoTimer.startLongTimer(self.moveTimer)
+		if config.usage.screenSaverMode.value > 0:  # Show logo or picon
+			self.moveTiming = config.usage.screenSaverMoveTimer.value
+
+		if config.usage.screenSaverMode.value == 2:  # Show picon
+			pictureSize = ()
+			try:
+				service = self.session.screen["CurrentService"].service
+				sref = None
+				if isinstance(service, eServiceReference):
+					sref = service.toString()
+				elif isinstance(service, iPlayableServicePtr):
+					info = service and service.info()
+					sref = info.getInfoString(iServiceInformation.sServiceref)
+				if sref:
+					picon = getPiconName(sref)
+					if exists(picon):
+						self["picture"].instance.setPixmapFromFile(picon)
+						pictureSize = (220, 132)
+			except Exception:
+				pass
+			pictureSize = pictureSize or self["picture"].getSize()
+			scaleFactor = 1.5 if self.instance.size().width() > 1300 else 1.0
+			self.maxX = int(self.instance.size().width() - pictureSize[0] * scaleFactor)
+			self.maxY = int(self.instance.size().height() - pictureSize[1] * scaleFactor)
+			self.movePicture()
+		else:
+			self["picture"].hide()
 
 	def hideScreenSaver(self):
-		self.moveLogoTimer.stop()
+		self.moveTimer.stop()
 
-	def layoutFinished(self):
-		pictureSize = self["picture"].getSize()
-		self.maxX = self.instance.size().width() - pictureSize[0]
-		self.maxY = self.instance.size().height() - pictureSize[1]
-		self.movePicture(timerActive=False)
-
-	def movePicture(self, timerActive=True):
-		self.posX = randrange(self.maxX)
-		self.posY = randrange(self.maxY)
-		self["picture"].instance.move(ePoint(self.posX, self.posY))
-		if timerActive:
-			self.moveLogoTimer.startLongTimer(self.moveTimer)
+	def movePicture(self):
+		self["picture"].instance.move(ePoint(randrange(self.maxX), randrange(self.maxY)))
+		self.moveTimer.startLongTimer(self.moveTiming)
 
 	def serviceStarted(self):
 		if self.shown:
