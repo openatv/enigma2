e660e4729e841b2134acdbd53732d31ff2040ed6
jbleyel <jbleyel@gmx.net>
2026-06-15 08:57:18 +0200

    [elistboxcontent]
    * remove deprecated getScollPos

--- lib/gui/elistboxcontent.h
+++ lib/gui/elistboxcontent.h
@@ -54,11 +54,6 @@ protected:
 	int getItemWidth() { return m_itemwidth; }
 	int getScrollPos() { return m_scroll_pos; } // Returns the current scroll position in pixels.
 
-	// Deprecated: use getScrollPos() instead (typo in original name)
-	[[deprecated("Use getScrollPos() instead")]]
-	int getScollPos() { return getScrollPos(); }
-
-
 private:
 	int m_saved_cursor_line;
 	ePtr<gFont> m_font_zoomed;
