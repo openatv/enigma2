1f29caf9247a30b22906691120de9684923a5d45
jbleyel <jbleyel@gmx.net>
2026-07-03 15:10:52 +0200

    [skin] * add support multiple keys for menus, setups and screens

--- lib/python/skin.py
+++ lib/python/skin.py
@@ -1512,31 +1512,40 @@ def loadSingleSkinData(desktop, screenID, domSkin, pathSkin, scope=SCOPE_GUISKIN
 				skinError(f"Tag 'parameter' needs a name and value, got name='{name}' and size='{value}'")
 	for tag in domSkin.findall("screens"):
 		for screen in tag.findall("screen"):
-			key = screen.attrib.get("key")
 			image = screen.attrib.get("image")
-			if key and image is not None:
-				screens[key] = image
-				# print(f"[Skin] DEBUG: Screen key='{key}', image='{image}'.")
+			key = screen.attrib.get("key", "")
+			keys = screen.attrib.get("keys", "")
+			if image is not None and (key or keys):
+				keys = [x.strip() for x in keys.split(",")] if keys else [key]
+				for key in keys:
+					screens[key] = image
+					# print(f"[Skin] DEBUG: Screen key='{key}', image='{image}'.")
 			else:
-				skinError(f"Tag 'screen' needs key and image, got key='{key}' and image='{image}'")
+				skinError(f"Tag 'screen' needs key or keys and image, got key='{key}' keys='{keys}' and image='{image}'")
 	for tag in domSkin.findall("menus"):
 		for menu in tag.findall("menu"):
-			key = menu.attrib.get("key")
 			image = menu.attrib.get("image")
-			if key and image is not None:
-				menus[key] = image
-				# print(f"[Skin] DEBUG: Menu key='{key}', image='{image}'.")
+			key = menu.attrib.get("key", "")
+			keys = menu.attrib.get("keys", "")
+			if image is not None and (key or keys):
+				keys = [x.strip() for x in keys.split(",")] if keys else [key]
+				for key in keys:
+					menus[key] = image
+					# print(f"[Skin] DEBUG: Menu key='{key}', image='{image}'.")
 			else:
-				skinError(f"Tag 'menu' needs key and image, got key='{key}' and image='{image}'")
+				skinError(f"Tag 'menu' needs key or keys and image, got key='{key}' keys='{keys}' and image='{image}'")
 	for tag in domSkin.findall("setups"):
 		for setup in tag.findall("setup"):
-			key = setup.attrib.get("key")
 			image = setup.attrib.get("image")
-			if key and image is not None:
-				setups[key] = image
-				# print(f"[Skin] DEBUG: Setup key='{key}', image='{image}'.")
+			key = setup.attrib.get("key", "")
+			keys = setup.attrib.get("keys", "")
+			if image is not None and (key or keys):
+				keys = [x.strip() for x in keys.split(",")] if keys else [key]
+				for key in keys:
+					setups[key] = image
+					# print(f"[Skin] DEBUG: Setup key='{key}', image='{image}'.")
 			else:
-				skinError(f"Tag 'setup' needs key and image, got key='{key}' and image='{image}'")
+				skinError(f"Tag 'setup' needs key or keys and image, got key='{key}' keys='{keys}' and image='{image}'")
 	for tag in domSkin.findall("constant-widgets"):
 		for constant_widget in tag.findall("constant-widget"):
 			name = constant_widget.attrib.get("name")
